e92bdd12736acc52624921a32a0d4d6526a8b513
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-27 17:19:01 +0900

    refactor: 매직 넘버 상수 전환

--- src/main/java/woowacourse/shoppingcart/domain/customer/Username.java
+++ src/main/java/woowacourse/shoppingcart/domain/customer/Username.java
@@ -2,6 +2,9 @@ package woowacourse.shoppingcart.domain.customer;
 
 public class Username {
 
+    private static final int MINIMUM_LENGTH = 3;
+    private static final int MAXIMUM_LENGTH = 15;
+
     private final String username;
 
     public Username(final String username) {
@@ -10,8 +13,9 @@ public class Username {
     }
 
     private void validateLength(final String username) {
-        if (username.length() < 3 || username.length() > 15) {
-            throw new IllegalArgumentException("유저 이름은 3자 이상 15자 이하입니다.");
+        if (username.length() < MINIMUM_LENGTH || username.length() > MAXIMUM_LENGTH) {
+            throw new IllegalArgumentException(
+                    String.format("유저 이름은 %d자 이상 %d자 이하입니다.", MINIMUM_LENGTH, MAXIMUM_LENGTH));
         }
     }
 }
