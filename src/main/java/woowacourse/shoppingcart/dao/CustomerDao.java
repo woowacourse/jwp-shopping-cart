0c32f15321af23aeed436a13f5176ea790c4c2ee
Juhyung990122 <harukawa99@naver.com>
2022-05-27 21:44:18 +0900

    warning: fatal sql exception 발생
    ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ

--- src/main/java/woowacourse/shoppingcart/dao/CustomerDao.java
+++ src/main/java/woowacourse/shoppingcart/dao/CustomerDao.java
@@ -1,27 +1,65 @@
 package woowacourse.shoppingcart.dao;
 
+import java.sql.PreparedStatement;
+import java.util.Locale;
+import java.util.Optional;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+import woowacourse.shoppingcart.domain.Member;
 import woowacourse.shoppingcart.exception.InvalidCustomerException;
-
-import java.util.Locale;
 
 @Repository
 public class CustomerDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Member> memberRowMapper = (rs, rn) -> new Member(
+            rs.getLong("id"),
+            rs.getString("email"),
+            rs.getString("password"),
+            rs.getString("nickname")
+    );
+
 
     public CustomerDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public Long findIdByUserName(final String userName) {
+
+    public Long save(final Member member) {
+        final String sql = "INSERT INTO CUSTOMER(email, password, nickname) values(?, ?, ?)";
+        final KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            final PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, member.getEmail());
+            preparedStatement.setString(2, member.getPassword());
+            preparedStatement.setString(3, member.getNickname());
+            return preparedStatement;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public Optional<Member> findById(final Long createdMemberId) {
+        final String sql = "SELECT id, email, password, nickname FROM CUSTOMER WHERE id = ?";
         try {
-            final String query = "SELECT id FROM customer WHERE username = ?";
-            return jdbcTemplate.queryForObject(query, Long.class, userName.toLowerCase(Locale.ROOT));
+            return Optional.ofNullable(jdbcTemplate.queryForObject(sql, memberRowMapper, createdMemberId));
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    public Long findIdByNickName(final String nickname) {
+        try {
+            final String query = "SELECT id FROM customer WHERE nickname = ?";
+            return jdbcTemplate.queryForObject(query, Long.class, nickname.toLowerCase(Locale.ROOT));
         } catch (final EmptyResultDataAccessException e) {
             throw new InvalidCustomerException();
         }
     }
 }
+
--- src/test/java/woowacourse/shoppingcart/dao/CustomerDaoTest.java
+++ src/test/java/woowacourse/shoppingcart/dao/CustomerDaoTest.java
@@ -1,26 +1,28 @@
 package woowacourse.shoppingcart.dao;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.TestConstructor;
-import org.springframework.test.context.jdbc.Sql;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import woowacourse.shoppingcart.domain.Member;
 
 @JdbcTest
-@AutoConfigureTestDatabase(replace = Replace.NONE)
-@Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-public class CustomerDaoTest {
+class CustomerDaoTest {
 
-    private final CustomerDao customerDao;
+    @Autowired
+    private CustomerDao customerDao;
 
-    public CustomerDaoTest(JdbcTemplate jdbcTemplate) {
-        customerDao = new CustomerDao(jdbcTemplate);
+    @Test
+    @DisplayName("이메일, 암호화 된 패스워드, 닉네임을 받아서 Member 테이블에 저장한다.")
+    void save() {
+        final Member testMember = Member.createWithoutId("test@test.com", "testtest", "테스트");
+        final Long createdMemberId = customerDao.save(testMember);
+
+        final Member findMember = customerDao.findById(createdMemberId).get();
+
+        assertThat(findMember.getId()).isEqualTo(createdMemberId);
     }
 
     @DisplayName("username을 통해 아이디를 찾으면, id를 반환한다.")
@@ -31,7 +33,7 @@ public class CustomerDaoTest {
         final String userName = "puterism";
 
         // when
-        final Long customerId = customerDao.findIdByUserName(userName);
+        final Long customerId = customerDao.findIdByNickName(userName);
 
         // then
         assertThat(customerId).isEqualTo(1L);
@@ -45,7 +47,7 @@ public class CustomerDaoTest {
         final String userName = "gwangyeol-iM";
 
         // when
-        final Long customerId = customerDao.findIdByUserName(userName);
+        final Long customerId = customerDao.findIdByNickName(userName);
 
         // then
         assertThat(customerId).isEqualTo(16L);
