bcc3dc5d4bcde2c551781f717985e4b15d3ba7a6
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-27 19:46:02 +0900

    feat: 유저 이름이 존재 할 경우 예외 발생 및 테스트

--- src/main/java/woowacourse/shoppingcart/application/CustomerService.java
+++ src/main/java/woowacourse/shoppingcart/application/CustomerService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import woowacourse.shoppingcart.dao.CustomerDao;
 import woowacourse.shoppingcart.dto.CustomerSignUpRequest;
+import woowacourse.shoppingcart.exception.InvalidCustomerException;
 
 @Service
 @Transactional(readOnly = true)
@@ -15,7 +16,11 @@ public class CustomerService {
         this.customerDao = customerDao;
     }
 
+    @Transactional
     public void save(final CustomerSignUpRequest request) {
+        if (customerDao.existCustomerByUsername(request.getUsername())) {
+            throw new InvalidCustomerException("이미 존재하는 유저 이름입니다.");
+        }
         customerDao.save(request.toCustomer());
     }
 }
--- src/test/java/woowacourse/shoppingcart/application/CustomerServiceTest.java
+++ src/test/java/woowacourse/shoppingcart/application/CustomerServiceTest.java
@@ -1,5 +1,6 @@
 package woowacourse.shoppingcart.application;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import woowacourse.shoppingcart.dto.CustomerSignUpRequest;
+import woowacourse.shoppingcart.exception.InvalidCustomerException;
 
 @SpringBootTest
 @Transactional
@@ -21,5 +23,16 @@ class CustomerServiceTest {
     void save() {
         CustomerSignUpRequest request = new CustomerSignUpRequest("username", "password123", "01012345678", "성담빌딩");
         assertDoesNotThrow(() -> customerService.save(request));
+    }
+
+    @Test
+    @DisplayName("이미 존재하는 유저 이름이면 예외 발생")
+    void save_duplicatedUserName_throwException() {
+        CustomerSignUpRequest request = new CustomerSignUpRequest("username", "password123", "01012345678", "성담빌딩");
+        customerService.save(request);
+
+        assertThatThrownBy(() -> customerService.save(request))
+                .isInstanceOf(InvalidCustomerException.class)
+                .hasMessage("이미 존재하는 유저 이름입니다.");
     }
 }
