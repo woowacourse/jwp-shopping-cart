3767930253ce82826f6d3d2d9643afeba9c1495f
woong7 <driedseas@gmail.com>
2022-05-27 17:37:57 +0900

    feat: 내 정보 조회 기능 구현

--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -16,7 +16,6 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원가입")
     @Test
     void addCustomer() {
-
         ExtractableResponse<Response> createResponse = RestAssured
                 .given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -32,6 +31,25 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("내 정보 조회")
     @Test
     void getMe() {
+        // given
+        ExtractableResponse<Response> createResponse = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new CustomerRequest("test", "1234"))
+                .when().post("/api/customers")
+                .then().log().all()
+                .extract();
+
+        // when
+        ExtractableResponse<Response> getResponse = RestAssured
+                .given().log().all()
+                .when().get(createResponse.header("Location"))
+                .then().log().all()
+                .extract();
+
+        // then
+        assertThat(getResponse.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(getResponse.body().jsonPath().getString("name")).isEqualTo("test");
     }
 
     @DisplayName("내 정보 수정")
