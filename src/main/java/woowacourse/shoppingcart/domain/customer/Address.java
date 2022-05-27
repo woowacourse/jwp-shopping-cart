e3522d7aa6a19a0010a534a4b2d918678f6fbbc5
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-27 17:48:13 +0900

    feat: 주소 vo 추가

---
+++ src/main/java/woowacourse/shoppingcart/domain/customer/Address.java
@@ -0,0 +1,29 @@
+package woowacourse.shoppingcart.domain.customer;
+
+import java.util.Objects;
+
+public class Address {
+
+    private final String address;
+
+    public Address(final String address) {
+        this.address = address;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Address address1 = (Address) o;
+        return Objects.equals(address, address1.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address);
+    }
+}
