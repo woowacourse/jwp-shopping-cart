d0cfd6a26d3c1e220cce07785bb1f0a0477671c8
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-27 19:51:17 +0900

    feat: 회원가입 api 추가

--- src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
+++ src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
@@ -1,4 +1,27 @@
 package woowacourse.shoppingcart.ui;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import woowacourse.shoppingcart.application.CustomerService;
+import woowacourse.shoppingcart.dto.CustomerSignUpRequest;
+
+@RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    public CustomerController(final CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signUp(@RequestBody CustomerSignUpRequest request) {
+        customerService.save(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -1,13 +1,25 @@
 package woowacourse.shoppingcart.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import woowacourse.shoppingcart.dto.CustomerSignUpRequest;
 
 @DisplayName("회원 관련 기능")
 public class CustomerAcceptanceTest extends AcceptanceTest {
+
     @DisplayName("회원가입")
     @Test
     void addCustomer() {
+        CustomerSignUpRequest request = new CustomerSignUpRequest("username", "password123", "01012345678", "성담빌딩");
+        ExtractableResponse<Response> response = 회원_가입_요청(request);
+        회원_가입됨(response);
     }
 
     @DisplayName("내 정보 조회")
@@ -23,5 +35,18 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원탈퇴")
     @Test
     void deleteMe() {
+    }
+
+    public static ExtractableResponse<Response> 회원_가입_요청(final CustomerSignUpRequest request) {
+        return RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/api/customers/signup")
+                .then().log().all()
+                .extract();
+    }
+
+    private void 회원_가입됨(final ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 }
