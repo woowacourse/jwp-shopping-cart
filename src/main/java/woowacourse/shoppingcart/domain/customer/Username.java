7435de849989ea6132e87362a3ac1719cd3feba0
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-27 17:25:36 +0900

    feat: 유저 이름에 equals, hashCode 재정의

--- src/main/java/woowacourse/shoppingcart/domain/customer/Username.java
+++ src/main/java/woowacourse/shoppingcart/domain/customer/Username.java
@@ -1,5 +1,6 @@
 package woowacourse.shoppingcart.domain.customer;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Username {
@@ -29,4 +30,20 @@ public class Username {
         }
     }
 
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Username username1 = (Username) o;
+        return Objects.equals(username, username1.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
 }
