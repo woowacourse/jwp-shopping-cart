a4bf548fe096cc9d8e848bc78cc7a7867319636a
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-27 19:57:07 +0900

    test: 중복된 유저 이름 가입 시나리오 추가

--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -5,7 +5,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,11 +17,31 @@ import woowacourse.shoppingcart.dto.CustomerSignUpRequest;
 public class CustomerAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("회원가입")
-    @Test
-    void addCustomer() {
-        CustomerSignUpRequest request = new CustomerSignUpRequest("username", "password123", "01012345678", "성담빌딩");
-        ExtractableResponse<Response> response = 회원_가입_요청(request);
-        회원_가입됨(response);
+    @Nested
+    class AddCustomer extends AcceptanceTest {
+
+        private CustomerSignUpRequest request;
+
+        @BeforeEach
+            void prepare() {
+                request = new CustomerSignUpRequest("username", "password123", "01012345678", "성담빌딩");
+        }
+
+        @Test
+        @DisplayName("성공한다.")
+        void success() {
+            ExtractableResponse<Response> response = 회원_가입_요청(request);
+            회원_가입됨(response);
+        }
+
+        @Test
+        @DisplayName("중복된 유저 이름으로 가입하여 실패")
+        void uplicatedUsername_fail() {
+            회원_가입_요청(request);
+
+            ExtractableResponse<Response> response = 회원_가입_요청(request);
+            회원_가입_실패(response);
+        }
     }
 
     @DisplayName("내 정보 조회")
@@ -48,5 +70,9 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
 
     private void 회원_가입됨(final ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    private void 회원_가입_실패(final ExtractableResponse<Response> response) {
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
