1269b8a296b0aa002dcec23e46233b3259764268
devHudi <devhudi@gmail.com>
2022-05-28 15:18:15 +0900

    feat: 생년월일 도메인 구현

---
+++ src/main/java/woowacourse/auth/domain/user/BirthDay.java
@@ -0,0 +1,41 @@
+package woowacourse.auth.domain.user;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
+
+public class BirthDay {
+    private final LocalDate value;
+
+    private BirthDay(LocalDate value) {
+        this.value = value;
+    }
+
+    public static BirthDay from(String value) {
+        return new BirthDay(LocalDate.parse(value, DateTimeFormatter.ISO_DATE));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        BirthDay birthDay = (BirthDay) o;
+        return Objects.equals(value, birthDay.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return "BirthDay{" +
+                "value='" + value + '\'' +
+                '}';
+    }
+}
