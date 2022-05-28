646f655fd22093768c2052c33f2aa2155e5229c6
Hyunta <mohai2618@gmail.com>
2022-05-28 17:12:29 +0900

    chore: SQL 스크립트 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,12 +11,12 @@ drop table if exists customer;
 create table customer
 (
     id       bigint       not null auto_increment,
+    loginid  varchar(255) not null unique,
     username varchar(255) not null,
+    password varchar(255) not null,
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
 
-alter table customer
-    add unique key (username);
 
 create table product
 (
