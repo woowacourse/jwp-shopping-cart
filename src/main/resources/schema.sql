8de747b6755c8da79636044627f1b30fa515d72a
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-05-28 16:21:46 +0900

    refactor(sql): 스키마 및 ddl 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,6 +11,8 @@ drop table if exists customer;
 create table customer
 (
     id       bigint       not null auto_increment,
+    loginId varchar(20) not null,
+    password varchar(255) not null,
     username varchar(255) not null,
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
