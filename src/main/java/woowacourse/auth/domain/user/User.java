a90e088da91d929ab717321b63dc49b7618a19f7
devHudi <devhudi@gmail.com>
2022-05-28 16:32:39 +0900

    feat: 유저 도메인 구현

---
+++ src/main/java/woowacourse/auth/domain/user/User.java
@@ -0,0 +1,50 @@
+package woowacourse.auth.domain.user;
+
+import java.util.Objects;
+import woowacourse.auth.exception.DisagreeToTermsException;
+
+public class User {
+    private final Email email;
+    private final Password password;
+    private final ProfileImageUrl profileImageUrl;
+    private final Privacy privacy;
+    private final boolean terms;
+
+    private User(Email email, Password password, ProfileImageUrl profileImageUrl, Privacy privacy, boolean terms) {
+        this.email = email;
+        this.password = password;
+        this.profileImageUrl = profileImageUrl;
+        this.privacy = privacy;
+        this.terms = terms;
+    }
+
+    public static User of(String email, String password, String profileImageUrl, Privacy privacy, boolean terms) {
+        validateTerms(terms);
+        return new User(new Email(email), new Password(password), new ProfileImageUrl(profileImageUrl), privacy, terms);
+    }
+
+    private static void validateTerms(boolean terms) {
+        if (!terms) {
+            throw new DisagreeToTermsException();
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return terms == user.terms && Objects.equals(email, user.email) && Objects.equals(password,
+                user.password) && Objects.equals(profileImageUrl, user.profileImageUrl)
+                && Objects.equals(privacy, user.privacy);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email, password, profileImageUrl, privacy, terms);
+    }
+}
---
+++ src/test/java/woowacourse/auth/domain/user/UserTest.java
@@ -0,0 +1,42 @@
+package woowacourse.auth.domain.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import woowacourse.auth.exception.DisagreeToTermsException;
+
+class UserTest {
+    @DisplayName("유저 정보를 전달하여 유저를 생성한다.")
+    @Test
+    void of() {
+        // given
+        String email = "devhudi@gmail.com";
+        String password = "a!123456";
+        String profileImageUrl = "http://gravatar.com/avatar/1?d=identicon";
+        Privacy privacy = Privacy.of("조동현", "male", "1998-12-21", "01011111111", "서울특별시 강남구 선릉역", "이디야 1층", "12345");
+        boolean terms = true;
+
+        // when
+        User actual = User.of(email, password, profileImageUrl, privacy, terms);
+
+        // then
+        assertThat(actual).isNotNull();
+    }
+
+    @DisplayName("약관 동의 여부가 거짓이라면 예외가 발생한다.")
+    @Test
+    void of_termsIsFalse() {
+        // given
+        String email = "devhudi@gmail.com";
+        String password = "a!123456";
+        String profileImageUrl = "http://gravatar.com/avatar/1?d=identicon";
+        Privacy privacy = Privacy.of("조동현", "male", "1998-12-21", "01011111111", "서울특별시 강남구 선릉역", "이디야 1층", "12345");
+        boolean terms = false;
+
+        // when & then
+        assertThatThrownBy(() -> User.of(email, password, profileImageUrl, privacy, terms))
+                .isInstanceOf(DisagreeToTermsException.class);
+    }
+}
