4958fb758d88b30f2d9b9a0baeed806b4275c941
Richard JEON <ztzy1907@gmail.com>
2022-05-29 21:42:48 +0900

    test(customer-acceptance): 회원 관리 ATDD 작성

--- src/test/java/woowacourse/ShoppingCartFixture.java
+++ src/test/java/woowacourse/ShoppingCartFixture.java
@@ -1,11 +1,17 @@
 package woowacourse;
 
+import woowacourse.auth.dto.TokenRequest;
+import woowacourse.shoppingcart.ui.dto.request.CustomerDeleteRequest;
 import woowacourse.shoppingcart.ui.dto.request.CustomerRequest;
 import woowacourse.shoppingcart.ui.dto.request.CustomerUpdateRequest;
 
 public class ShoppingCartFixture {
     public static final CustomerRequest 잉_회원생성요청 = new CustomerRequest("잉", "ing@woowahan.com", "ing_woowahan");
     public static final CustomerUpdateRequest 잉_회원수정요청 = new CustomerUpdateRequest("잉", "ing_woowahan");
+
+    public static final TokenRequest 잉_로그인요청 = new TokenRequest(잉_회원생성요청.getEmail(), 잉_회원생성요청.getPassword());
+
+    public static final CustomerDeleteRequest 잉_회원탈퇴요청 = new CustomerDeleteRequest("ing_woowahan");
     public static final String CUSTOMER_URI = "/api/customer";
 
     public static final String LOGIN_URI = "/api/login";
--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -1,27 +1,104 @@
 package woowacourse.shoppingcart.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static woowacourse.ShoppingCartFixture.CUSTOMER_URI;
+import static woowacourse.ShoppingCartFixture.잉_로그인요청;
+import static woowacourse.ShoppingCartFixture.잉_회원생성요청;
+import static woowacourse.ShoppingCartFixture.잉_회원수정요청;
+import static woowacourse.ShoppingCartFixture.잉_회원탈퇴요청;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+import woowacourse.shoppingcart.ui.dto.request.CustomerDeleteRequest;
+import woowacourse.shoppingcart.ui.dto.request.CustomerRequest;
+import woowacourse.shoppingcart.ui.dto.request.CustomerResponse;
+import woowacourse.shoppingcart.ui.dto.request.CustomerUpdateRequest;
+import woowacourse.shoppingcart.ui.dto.response.ExceptionResponse;
 
 @DisplayName("회원 관련 기능")
+@Sql("/truncate.sql")
 public class CustomerAcceptanceTest extends AcceptanceTest {
-    @DisplayName("회원가입")
+
+    @DisplayName("이름, 이메일, 비밀번호를 입력해서 회원 등록 요청한다")
     @Test
     void addCustomer() {
+        // given
+        final CustomerRequest 회원생성요청 = 잉_회원생성요청;
+
+        // when
+        final ExtractableResponse<Response> 회원생성응답 = post(CUSTOMER_URI, 회원생성요청);
+
+        // then
+        assertAll(
+                () -> assertThat(회원생성응답.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(회원생성응답.header("Location")).isNotBlank()
+        );
     }
 
-    @DisplayName("내 정보 조회")
+    @DisplayName("회원 이름과 이메일을 확인할 수 있다")
     @Test
     void getMe() {
+        // given
+        final CustomerRequest 회원생성요청 = 잉_회원생성요청;
+        post(CUSTOMER_URI, 회원생성요청);
+
+        // when
+        final String token = getToken(잉_로그인요청);
+        final ExtractableResponse<Response> 회원조회응답 = get(CUSTOMER_URI, token);
+        final CustomerResponse 회원응답 = 회원조회응답.as(CustomerResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(회원조회응답.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(회원응답.getName()).isEqualTo(회원생성요청.getName()),
+                () -> assertThat(회원응답.getName()).isEqualTo(회원생성요청.getEmail())
+        );
     }
 
     @DisplayName("내 정보 수정")
     @Test
     void updateMe() {
+        // given
+        final CustomerRequest 회원생성요청 = 잉_회원생성요청;
+        post(CUSTOMER_URI, 회원생성요청);
+        final CustomerUpdateRequest 회원수정요청 = 잉_회원수정요청;
+
+        // when
+        final String token = getToken(잉_로그인요청);
+        final ExtractableResponse<Response> 회원수정응답 = put(CUSTOMER_URI, 회원수정요청, token);
+        final CustomerResponse 회원조회응답 = get(CUSTOMER_URI, token).as(CustomerResponse.class);
+
+        // then
+        assertAll(
+                () -> assertThat(회원수정응답.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                () -> assertThat(회원조회응답.getName()).isEqualTo(회원수정요청.getName()),
+                () -> assertThat(회원조회응답.getEmail()).isEqualTo(회원조회응답.getEmail())
+        );
     }
 
     @DisplayName("회원탈퇴")
     @Test
     void deleteMe() {
+        //given
+        final CustomerRequest 회원생성요청 = 잉_회원생성요청;
+        post(CUSTOMER_URI, 회원생성요청);
+        final CustomerDeleteRequest 회원탈퇴요청 = 잉_회원탈퇴요청;
+
+        //when
+        final String token = getToken(잉_로그인요청);
+        final ExtractableResponse<Response> 회원탈퇴응답 = delete(CUSTOMER_URI, 회원탈퇴요청, token);
+
+        //then
+        assertAll(
+                () -> assertThat(회원탈퇴응답.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
+                () -> assertThatCode(() -> get(CUSTOMER_URI, token).as(ExceptionResponse.class))
+                        .doesNotThrowAnyException()
+        );
     }
 }
