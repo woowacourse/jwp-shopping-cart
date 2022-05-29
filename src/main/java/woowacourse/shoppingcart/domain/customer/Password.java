313158ea1df3e00c19ff259c3f10511035cf4f78
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-29 14:22:26 +0900

    feat: 비밀번호 패스워드 패턴 구현 및 테스트

--- src/main/java/woowacourse/shoppingcart/domain/customer/Password.java
+++ src/main/java/woowacourse/shoppingcart/domain/customer/Password.java
@@ -1,17 +1,20 @@
 package woowacourse.shoppingcart.domain.customer;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 import woowacourse.auth.exception.InvalidAuthException;
 
 public class Password {
 
     private static final int MINIMUM_LENGTH = 8;
     private static final int MAXIMUM_LENGTH = 20;
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[0-9])(?=.*[a-zA-Z])[A-Za-z0-9]*$");
 
     private final String password;
 
     public Password(final String password) {
         validateLength(password);
+        validatePattern(password);
         this.password = password;
     }
 
@@ -19,6 +22,12 @@ public class Password {
         if (password.length() < MINIMUM_LENGTH || password.length() > MAXIMUM_LENGTH) {
             throw new IllegalArgumentException(
                     String.format("비밀번호는 %d자 이상 %d자 이하입니다.", MINIMUM_LENGTH, MAXIMUM_LENGTH));
+        }
+    }
+
+    private void validatePattern(final String password) {
+        if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            throw new IllegalArgumentException("패스워드는 숫자와 영어를 포함해야합니다.");
         }
     }
 
--- src/test/java/woowacourse/shoppingcart/domain/customer/PasswordTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/customer/PasswordTest.java
@@ -1,6 +1,8 @@
 package woowacourse.shoppingcart.domain.customer;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,7 +17,23 @@ public class PasswordTest {
     @DisplayName("비밀번호가 8자 미만 20자 초과 시 예외 발생")
     void invalidPasswordLength_throwException(String password) {
         assertThatThrownBy(() -> new Password(password))
-                .isInstanceOf(IllegalArgumentException.class);
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호는 8자 이상 20자 이하입니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abcdefgh", "12345678", "abcdefg123@"})
+    @DisplayName("비밀번호 영어, 숫자 미포함 시 예외 발생")
+    void invalidPasswordPattern_thrownException(String password) {
+        assertThatThrownBy(() -> new Password(password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("패스워드는 숫자와 영어를 포함해야합니다.");
+    }
+
+    @Test
+    @DisplayName("정상적인 패스워드 생성")
+    void createPassword() {
+        assertDoesNotThrow(() -> new Password("password123"));
     }
 
     @Test
