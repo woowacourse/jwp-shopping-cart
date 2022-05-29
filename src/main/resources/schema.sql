e3dcc7939899bf030357e40d01b13d4003a16339
brorae <jhr3239@gmail.com>
2022-05-29 13:57:08 +0900

    refactor: DB table 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,6 +12,8 @@ create table customer
 (
     id       bigint       not null auto_increment,
     username varchar(255) not null,
+    email varchar(255) not null,
+    password varchar(255) not null,
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
 
