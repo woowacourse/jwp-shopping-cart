e8d554a22bbdd30d2890a4d84fa088b89ec86bf4
Richard JEON <ztzy1907@gmail.com>
2022-05-29 21:01:06 +0900

    feat(acceptance-test): get, post, put, delete, getToken 공통 메서드 부모 클래스에 구현

--- src/test/java/woowacourse/shoppingcart/acceptance/AcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/AcceptanceTest.java
@@ -1,10 +1,17 @@
 package woowacourse.shoppingcart.acceptance;
 
+import static woowacourse.ShoppingCartFixture.LOGIN_URI;
+
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
@@ -15,5 +22,51 @@ public class AcceptanceTest {
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+    }
+
+    protected ExtractableResponse<Response> post(String uri, Object object) {
+        return RestAssured.given().log().all()
+                .body(object)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    protected ExtractableResponse<Response> get(String uri, String token) {
+        return RestAssured.given().log().all()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    protected ExtractableResponse<Response> put(String uri, Object object, String token) {
+        return RestAssured.given().log().all()
+                .header("Authorization", "Bearer " + token)
+                .body(object)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .put(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    protected ExtractableResponse<Response> delete(String uri, Object object, String token) {
+        return RestAssured.given().log().all()
+                .header("Authorization", "Bearer " + token)
+                .body(object)
+                .when()
+                .delete(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    protected String getToken(TokenRequest tokenRequest) {
+        return post(LOGIN_URI, tokenRequest)
+                .as(TokenResponse.class)
+                .getAccessToken();
     }
 }
