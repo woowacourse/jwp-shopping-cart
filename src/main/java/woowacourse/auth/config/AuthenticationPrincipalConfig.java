423f62ba54726e369e60879e81cb38d23633c8d9
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-05-29 16:04:06 +0900

    feat: ArgumentResolver component 변경

--- src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
+++ src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
@@ -1,28 +1,22 @@
 package woowacourse.auth.config;
 
 import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import woowacourse.auth.ui.AuthenticationContext;
 import woowacourse.auth.ui.AuthenticationPrincipalArgumentResolver;
 
 @Configuration
 public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
-    private final AuthenticationContext authenticationContext;
+    private final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver;
 
-    public AuthenticationPrincipalConfig(final AuthenticationContext authenticationContext) {
-        this.authenticationContext = authenticationContext;
+    public AuthenticationPrincipalConfig(
+            final AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver) {
+        this.authenticationPrincipalArgumentResolver = authenticationPrincipalArgumentResolver;
     }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(createAuthenticationPrincipalArgumentResolver());
-    }
-
-    @Bean
-    public AuthenticationPrincipalArgumentResolver createAuthenticationPrincipalArgumentResolver() {
-        return new AuthenticationPrincipalArgumentResolver(authenticationContext);
+        resolvers.add(authenticationPrincipalArgumentResolver);
     }
 }
--- src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -1,12 +1,14 @@
 package woowacourse.auth.ui;
 
 import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import woowacourse.auth.support.AuthenticationPrincipal;
 
+@Component
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
     private final AuthenticationContext authenticationContext;
 
