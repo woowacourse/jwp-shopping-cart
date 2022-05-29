197e31d15b127d2a2b63ca3704c15487507f21b0
jayjaehunchoi <wogns0108@naver.com>
2022-05-29 13:27:35 +0900

    chore: test 환경 설정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,17 +6,19 @@ drop table if exists cart_item;
 
 drop table if exists product;
 
-drop table if exists customer;
+drop table if exists member;
 
-create table customer
+create table member
 (
     id       bigint       not null auto_increment,
-    username varchar(255) not null,
+    email    varchar(255) not null,
+    password varchar(20)  not null,
+    name     varchar(10)  not null,
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
 
-alter table customer
-    add unique key (username);
+alter table member
+    add unique key (email);
 
 create table product
 (
@@ -29,15 +31,15 @@ create table product
 
 create table cart_item
 (
-    id          bigint not null auto_increment,
-    customer_id bigint not null,
-    product_id  bigint not null,
+    id         bigint not null auto_increment,
+    member_id  bigint not null,
+    product_id bigint not null,
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
 
 alter table cart_item
-    add constraint fk_cart_item_to_customer
-        foreign key (customer_id) references customer (id);
+    add constraint fk_cart_item_to_member
+        foreign key (member_id) references member (id);
 
 alter table cart_item
     add constraint fk_cart_item_to_product
@@ -45,14 +47,14 @@ alter table cart_item
 
 create table orders
 (
-    id          bigint not null auto_increment,
-    customer_id bigint not null,
+    id        bigint not null auto_increment,
+    member_id bigint not null,
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
 
 alter table orders
-    add constraint fk_orders_to_customer
-        foreign key (customer_id) references customer (id);
+    add constraint fk_orders_to_member
+        foreign key (member_id) references member (id);
 
 create table orders_detail
 (
