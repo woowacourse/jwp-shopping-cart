9e8a66abb9a313985acffa63962c3cacbb34b7e8
brorae <jhr3239@gmail.com>
2022-05-29 15:41:48 +0900

    refactor: dao 제거

--- src/main/java/woowacourse/shoppingcart/dao/CustomerDao.java
+++ src/main/java/woowacourse/shoppingcart/dao/CustomerDao.java
@@ -1,8 +1,13 @@
 package woowacourse.shoppingcart.dao;
 
+import javax.sql.DataSource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
+import woowacourse.auth.domain.Customer;
 import woowacourse.shoppingcart.exception.InvalidCustomerException;
 
 import java.util.Locale;
@@ -11,9 +16,27 @@ import java.util.Locale;
 public class CustomerDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
 
-    public CustomerDao(final JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public CustomerDao(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
+                .withTableName("CUSTOMER")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    Customer save(Customer customer) {
+        String username = customer.getUsername();
+        String email = customer.getEmail();
+        String password = customer.getPassword();
+
+        SqlParameterSource params = new MapSqlParameterSource()
+                .addValue("username", username)
+                .addValue("email", email)
+                .addValue("password", password);
+
+        long id = simpleJdbcInsert.executeAndReturnKey(params).longValue();
+        return new Customer(id, username, email, password);
     }
 
     public Long findIdByUserName(final String userName) {
--- src/test/java/woowacourse/shoppingcart/dao/CustomerDaoTest.java
+++ src/test/java/woowacourse/shoppingcart/dao/CustomerDaoTest.java
@@ -1,13 +1,17 @@
 package woowacourse.shoppingcart.dao;
 
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.jdbc.Sql;
+import woowacourse.auth.domain.Customer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,8 +23,16 @@ public class CustomerDaoTest {
 
     private final CustomerDao customerDao;
 
-    public CustomerDaoTest(JdbcTemplate jdbcTemplate) {
-        customerDao = new CustomerDao(jdbcTemplate);
+    public CustomerDaoTest(DataSource dataSource) {
+        customerDao = new CustomerDao(dataSource);
+    }
+
+    @Test
+    @DisplayName("회원을 저장한다.")
+    void save() {
+        Customer customer = customerDao.save(new Customer("레넌", "rennon@woowa.com", "1234"));
+
+        assertThat(customer.getId()).isEqualTo(1L);
     }
 
     @DisplayName("username을 통해 아이디를 찾으면, id를 반환한다.")
