2e23f40119866a359bb1787f29470781a1e8371a
hyeonic <dev.hyeonic@gmail.com>
2022-05-30 16:24:23 +0900

    feat: CustomerContoller 자신의 정보 조회 기능 추가
    인수 테스트 추가

--- src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
+++ src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
@@ -2,12 +2,15 @@ package woowacourse.shoppingcart.ui;
 
 import java.net.URI;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import woowacourse.auth.support.AuthenticationPrincipal;
 import woowacourse.shoppingcart.application.CustomerService;
 import woowacourse.shoppingcart.dto.CustomerResponse;
 import woowacourse.shoppingcart.dto.CustomerSaveRequest;
+import woowacourse.shoppingcart.dto.LoginCustomer;
 
 @RestController
 public class CustomerController {
@@ -22,5 +25,12 @@ public class CustomerController {
     public ResponseEntity<Void> save(@RequestBody CustomerSaveRequest request) {
         CustomerResponse response = customerService.save(request);
         return ResponseEntity.created(URI.create("/api/customers/" + response.getId())).build();
+    }
+
+    @GetMapping("/api/customers/me")
+    public ResponseEntity<CustomerResponse> findCustomer(@AuthenticationPrincipal LoginCustomer loginCustomer) {
+        CustomerResponse customerResponse = new CustomerResponse(1L, "puterism", "puterism@email.com", "test",
+                "000-0000-0000");
+        return ResponseEntity.ok(customerResponse);
     }
 }
--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,46 @@
 package woowacourse.auth.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
 import woowacourse.shoppingcart.acceptance.AcceptanceTest;
+import woowacourse.shoppingcart.dto.CustomerResponse;
 
 @DisplayName("인증 관련 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
+
     @DisplayName("Bearer Auth 로그인 성공")
     @Test
     void myInfoWithBearerAuth() {
         // given
-        // 회원이 등록되어 있고
-        // id, password를 사용해 토큰을 발급받고
+        String accessToken = RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new TokenRequest("puterism", "1234567890"))
+                .when().post("/api/auth/token")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(TokenResponse.class)
+                .getAccessToken();
 
         // when
-        // 발급 받은 토큰을 사용하여 내 정보 조회를 요청하면
+        CustomerResponse customerResponse = RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/api/customers/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract()
+                .as(CustomerResponse.class);
 
         // then
-        // 내 정보가 조회된다
+        assertThat(customerResponse.getUsername()).isEqualTo("puterism");
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
