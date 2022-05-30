d9a89d5bf8cc0135030e49132c1b87da5a3687f0
Seongwon97 <osw970325@naver.com>
2022-05-30 20:17:25 +0900

    feat(AuthService): 토큰 생성 로직 구현

--- src/main/java/woowacourse/auth/application/AuthService.java
+++ src/main/java/woowacourse/auth/application/AuthService.java
@@ -1,7 +1,20 @@
 package woowacourse.auth.application;
 
 import org.springframework.stereotype.Service;
+import woowacourse.auth.dto.LonginRequest;
+import woowacourse.auth.dto.TokenResponse;
+import woowacourse.auth.support.JwtTokenProvider;
 
 @Service
 public class AuthService {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public TokenResponse createToken(LonginRequest request) {
+        return new TokenResponse(jwtTokenProvider.createToken(request.getEmail()));
+    }
 }
---
+++ src/test/java/woowacourse/auth/application/AuthServiceTest.java
@@ -0,0 +1,35 @@
+package woowacourse.auth.application;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestConstructor;
+import org.springframework.test.context.jdbc.Sql;
+import woowacourse.auth.dto.LonginRequest;
+import woowacourse.auth.dto.TokenResponse;
+import woowacourse.auth.support.JwtTokenProvider;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Sql(scripts = {"classpath:schema.sql", "classpath:data.sql"})
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+class AuthServiceTest {
+
+    private final AuthService authService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthServiceTest(AuthService authService, JwtTokenProvider jwtTokenProvider) {
+        this.authService = authService;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @DisplayName("토큰을 생성한다.")
+    @Test
+    void createToken() {
+        TokenResponse token = authService.createToken(new LonginRequest("ari@wooteco.com","Wooteco!"));
+        boolean result = jwtTokenProvider.validateToken(token.getAccessToken());
+        assertThat(result).isTrue();
+    }
+}
