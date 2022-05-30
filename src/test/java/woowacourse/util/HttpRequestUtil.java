5781f8d7a0a2a1a06222c20f48e8f3a5e8af140e
Chris <bjuuuu98@gmail.com>
2022-05-30 17:33:39 +0900

    test: http 리퀘스트를 보내는 메서드 작성

---
+++ src/test/java/woowacourse/util/HttpRequestUtil.java
@@ -0,0 +1,45 @@
+package woowacourse.util;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.MediaType;
+
+public class HttpRequestUtil {
+
+    public static ExtractableResponse<Response> get(String uri) {
+        return RestAssured.given().log().all()
+                .when()
+                .get(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> post(String uri, Object requestBody) {
+        return RestAssured.given().log().all()
+                .body(requestBody)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> put(String uri, Object requestBody) {
+        return RestAssured.given().log().all()
+                .body(requestBody)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .put(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public static ExtractableResponse<Response> delete(String uri) {
+        return RestAssured.given().log().all()
+                .when()
+                .delete(uri)
+                .then().log().all()
+                .extract();
+    }
+}
