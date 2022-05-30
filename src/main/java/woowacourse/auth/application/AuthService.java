5d1c2c6c4780816fa48ae0db562875be199b1bb5
kth990303 <kth990303@naver.com>
2022-05-30 22:24:30 +0900

    feat: 토큰에서 사용자 정보를 추출하는 `ArgumentResolver` 기능 구현
    비즈니스 로직과 인증 로직 책임 분리

--- src/main/java/woowacourse/auth/application/AuthService.java
+++ src/main/java/woowacourse/auth/application/AuthService.java
@@ -7,7 +7,6 @@ import woowacourse.auth.dto.TokenResponse;
 import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.shoppingcart.dao.CustomerDao;
 import woowacourse.shoppingcart.domain.customer.Customer;
-import woowacourse.shoppingcart.dto.CustomerResponse;
 
 @Transactional
 @Service
@@ -31,9 +30,8 @@ public class AuthService {
         customer.validatePassword(credentials);
     }
 
-    public CustomerResponse findCustomerByToken(String token) {
+    public Customer findCustomer(String token) {
         String userName = jwtTokenProvider.getPayload(token);
-        Customer customer = customerDao.findCustomerByUserName(userName);
-        return new CustomerResponse(customer.getUserName(), customer.getNickName(), customer.getAge());
+        return customerDao.findCustomerByUserName(userName);
     }
 }
