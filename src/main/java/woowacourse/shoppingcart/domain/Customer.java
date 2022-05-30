beda1a910bcb338621e3d0eae0b254d80c240407
awesomeo184 <awesomeo184@gmail.com>
2022-05-30 17:38:15 +0900

    feat: 회원 생성 시, 패스워드 검증 기능

--- src/main/java/woowacourse/shoppingcart/domain/Customer.java
+++ src/main/java/woowacourse/shoppingcart/domain/Customer.java
@@ -5,7 +5,12 @@ import java.util.regex.Pattern;
 
 public class Customer {
 
-    private static final Pattern emailPattern = Pattern.compile("^(.+)@(.+)$");
+    private static final Pattern LOWER_CASE_PATTERN = Pattern.compile(".*[a-z].*");
+    private static final Pattern UPPER_CASE_PATTERN = Pattern.compile(".*[A-Z].*");
+    private static final Pattern SPECIAL_CHARACTERS_PATTERN = Pattern.compile(".*[!@#$%^&*].*");
+    private static final Pattern NUMBERS_PATTERN = Pattern.compile(".*[0-9].*");
+    private static final Pattern SIZE_PATTERN = Pattern.compile("^(?=.{8,20}$).*");
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^(.+)@(.+)$");
 
     private final String email;
     private final String nickname;
@@ -13,15 +18,29 @@ public class Customer {
 
     public Customer(String email, String nickname, String password) {
         validateEmailFormat(email);
+        validatePasswordFormat(password);
         this.email = email;
         this.nickname = nickname;
         this.password = password;
     }
 
     private void validateEmailFormat(String email) {
-        Matcher matcher = emailPattern.matcher(email);
+        Matcher matcher = EMAIL_PATTERN.matcher(email);
         if (!matcher.matches()) {
             throw new IllegalArgumentException("이메일 형식이 맞지 않습니다.");
+        }
+    }
+
+    private void validatePasswordFormat(String password) {
+        Matcher lowerCaseMatcher = LOWER_CASE_PATTERN.matcher(password);
+        Matcher upperCaseMatcher = UPPER_CASE_PATTERN.matcher(password);
+        Matcher specialCharactersMatcher = SPECIAL_CHARACTERS_PATTERN.matcher(password);
+        Matcher numbersMatcher = NUMBERS_PATTERN.matcher(password);
+        Matcher sizeMatcher = SIZE_PATTERN.matcher(password);
+
+        if (!(lowerCaseMatcher.matches() && upperCaseMatcher.matches() && specialCharactersMatcher.matches() &&
+                numbersMatcher.matches() && sizeMatcher.matches())) {
+            throw new IllegalArgumentException("패스워드 형식이 맞지 않습니다.");
         }
     }
 }
--- src/test/java/woowacourse/shoppingcart/domain/CustomerTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/CustomerTest.java
@@ -1,22 +1,40 @@
 package woowacourse.shoppingcart.domain;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CustomerTest {
 
-    @Test
     @DisplayName("이메일 형식이 맞지 않는 경우 예외가 발생한다.")
-    void validateEmail() {
+    @Test
+    void throwsExceptionWhenInvalidEmailFormat() {
         // given
         String email = "beomWhale";
         String nickname = "범고래";
         String password = "Password12345!";
 
-        // when
-        assertThatThrownBy(() -> new Customer(email, nickname, password)).isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("이메일 형식이 맞지 않습니다.");
+        // when && then
+        assertThatThrownBy(() -> new Customer(email, nickname, password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이메일 형식이 맞지 않습니다.");
+    }
+
+    @DisplayName("패스워드 형식이 맞지 않는 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"password123!", "PASSWORD123!", "Password123", "Password!@#", "Aa1!123",
+            "Password123412341234!"})
+    void throwsExceptionWhenInvalidPasswordFormat(String password) {
+        // given
+        String email = "beomWhale@gmail.com";
+        String nickname = "범고래";
+
+        // when && then
+        assertThatThrownBy(() -> new Customer(email, nickname, password))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("패스워드 형식이 맞지 않습니다.");
     }
 }
