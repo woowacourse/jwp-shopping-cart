c432e9eaff464dba6920ec16ad9f2d9c98520e7a
yhh1056 <yhh1056@naver.com>
2022-05-30 17:09:07 +0900

    feat: 비밀번호 암호화

--- src/main/java/woowacourse/auth/domain/Password.java
+++ src/main/java/woowacourse/auth/domain/Password.java
@@ -1,5 +1,8 @@
 package woowacourse.auth.domain;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Password {
@@ -11,7 +14,7 @@ public class Password {
 
     public Password(String value) {
         validate(value);
-        this.value = value;
+        this.value = encrypt(value);
     }
 
     private void validate(String value) {
@@ -21,5 +24,44 @@ public class Password {
         if (value.length() < 8 || value.length() > 20) {
             throw new IllegalArgumentException("비밀번호는 8 ~ 20자로 생성 가능합니다.");
         }
+    }
+
+    private String encrypt(String password) {
+        try {
+            MessageDigest digestAlgorithm = MessageDigest.getInstance("SHA-256");
+            digestAlgorithm.update(password.getBytes());
+            return bytesToHex(digestAlgorithm.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("적합한 알고리즘을 찾을 수 없습니다.");
+        }
+    }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder builder = new StringBuilder();
+        for (byte it : bytes) {
+            builder.append(String.format("%02x", it));
+        }
+        return builder.toString();
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Password)) {
+            return false;
+        }
+        Password password = (Password) o;
+        return Objects.equals(value, password.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
--- src/test/java/woowacourse/auth/domain/PasswordTest.java
+++ src/test/java/woowacourse/auth/domain/PasswordTest.java
@@ -1,9 +1,11 @@
 package woowacourse.auth.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -29,4 +31,20 @@ class PasswordTest {
                 .withMessage("비밀번호는 8 ~ 20자로 생성 가능합니다.");
     }
 
+    @Test
+    void 비밀번호가_생성될_때_암호화() {
+        String input = "dusfhrmWkd12!";
+        Password password = new Password(input);
+
+        assertThat(password.getValue()).isNotEqualTo(input);
+    }
+
+    @Test
+    void 암호화된_비밀번호_일치_여부() {
+        String input = "dusfhrmWkd12!";
+        Password actual = new Password(input);
+        Password expected = new Password(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
 }
