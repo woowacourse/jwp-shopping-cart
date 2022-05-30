a1da33b80456dffd5801a25fd0e26065fd7f753a
Seongwon97 <osw970325@naver.com>
2022-05-30 15:45:29 +0900

    refactor(data.sql): 테스트 데이터 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into member (email, name, password)
-values ('ari', '아리', 'Wooteco1!'),
-       ('rex', '렉스', 'Wooteco1!'),
-       ('huni', '후니', 'Wooteco1!'),
-       ('maru', '마루', 'Wooteco1!'),
-       ('cocacola', '코카콜라', 'Wooteco1!'),
-       ('hope', '호프', 'Wooteco1!')
+values ('ari@wooteco.com', '아리', 'Wooteco1!'),
+       ('rex@wooteco.com', '렉스', 'Wooteco1!'),
+       ('huni@wooteco.com', '후니', 'Wooteco1!'),
+       ('maru@wooteco.com', '마루', 'Wooteco1!'),
+       ('cocacola@wooteco.com', '코카콜라', 'Wooteco1!'),
+       ('hope@wooteco.com', '호프', 'Wooteco1!')
 ;
