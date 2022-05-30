b447843cd4e6b4c50a116cca2376250ebd45ffde
Hongdonggeon <ghd700@naver.com>
2022-05-30 18:27:30 +0900

    feat: 로그인 기능

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -1,12 +1,49 @@
 package woowacourse.auth.acceptance;
 
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import woowacourse.auth.dto.TokenRequest;
 import woowacourse.shoppingcart.acceptance.AcceptanceTest;
+import woowacourse.shoppingcart.dto.CustomerCreationRequest;
 
 @DisplayName("인증 관련 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    @DisplayName("로그인 성공")
+    @Test
+    void login() {
+        // given
+        String email = "email@email.com";
+        String password = "1q2w3e4r";
+
+        CustomerCreationRequest signUpRequest = new CustomerCreationRequest(email, password, "kun");
+        RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(signUpRequest)
+                .when().post("/users")
+                .then().log().all();
+
+        TokenRequest request = new TokenRequest(email, password);
+
+        // when
+        ValidatableResponse response = RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/login")
+                .then().log().all();
+
+        // then
+        response.statusCode(HttpStatus.OK.value())
+                .body("accessToken", Matchers.notNullValue(String.class));
+    }
+
     @DisplayName("Bearer Auth 로그인 성공")
     @Test
     void myInfoWithBearerAuth() {
