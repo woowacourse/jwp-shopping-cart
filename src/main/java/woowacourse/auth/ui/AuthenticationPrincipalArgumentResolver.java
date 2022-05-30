e641dc22958aaef52099d681b12a7686df43da49
hyeonic <dev.hyeonic@gmail.com>
2022-05-30 16:24:11 +0900

    feat: AuthenticationPrincipalArgumentResolver 추가 및 등록

--- src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -6,13 +6,18 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 import woowacourse.auth.support.AuthenticationPrincipal;
-import woowacourse.auth.application.AuthService;
+import woowacourse.auth.support.AuthorizationExtractor;
+import woowacourse.auth.support.JwtTokenProvider;
+import woowacourse.shoppingcart.dto.LoginCustomer;
 
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
-    private AuthService authService;
 
-    public AuthenticationPrincipalArgumentResolver(AuthService authService) {
-        this.authService = authService;
+    private static final String AUTHORIZATION_HEADER_NAME = "Authorization";
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthenticationPrincipalArgumentResolver(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
@@ -20,10 +25,12 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
         return parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
     }
 
-    // parameter에 @AuthenticationPrincipal이 붙어있는 경우 동작
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
-        // TODO: 유효한 로그인인 경우 로그인한 사용자 객체를 만들어서 응답하기
-        return null;
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        String authorizationHeader = webRequest.getHeader(AUTHORIZATION_HEADER_NAME);
+        String accessToken = AuthorizationExtractor.extract(authorizationHeader);
+        String username = jwtTokenProvider.getPayload(accessToken);
+        return new LoginCustomer(username);
     }
 }
