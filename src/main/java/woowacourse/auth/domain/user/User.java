db9333bb43ac0e10588c0dba7f3cb4982758f58d
devHudi <devhudi@gmail.com>
2022-05-30 16:43:11 +0900

    feat: User에 Id 도메인 추가

--- src/main/java/woowacourse/auth/domain/user/User.java
+++ src/main/java/woowacourse/auth/domain/user/User.java
@@ -5,6 +5,7 @@ import woowacourse.auth.domain.user.privacy.Privacy;
 import woowacourse.auth.exception.DisagreeToTermsException;
 
 public class User {
+    private final Id id;
     private final Email email;
     private final Password password;
     private final ProfileImageUrl profileImageUrl;
@@ -12,7 +13,13 @@ public class User {
     private final boolean terms;
 
     public User(Email email, Password password, ProfileImageUrl profileImageUrl, Privacy privacy, boolean terms) {
+        this(Id.empty(), email, password, profileImageUrl, privacy, terms);
+    }
+
+    public User(Id id, Email email, Password password, ProfileImageUrl profileImageUrl, Privacy privacy,
+                boolean terms) {
         validateTerms(terms);
+        this.id = id;
         this.email = email;
         this.password = password;
         this.profileImageUrl = profileImageUrl;
@@ -35,13 +42,25 @@ public class User {
             return false;
         }
         User user = (User) o;
-        return terms == user.terms && Objects.equals(email, user.email) && Objects.equals(password,
-                user.password) && Objects.equals(profileImageUrl, user.profileImageUrl)
-                && Objects.equals(privacy, user.privacy);
+        return terms == user.terms && Objects.equals(id, user.id) && Objects.equals(email, user.email)
+                && Objects.equals(password, user.password) && Objects.equals(profileImageUrl,
+                user.profileImageUrl) && Objects.equals(privacy, user.privacy);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(email, password, profileImageUrl, privacy, terms);
+        return Objects.hash(id, email, password, profileImageUrl, privacy, terms);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email=" + email +
+                ", password=" + password +
+                ", profileImageUrl=" + profileImageUrl +
+                ", privacy=" + privacy +
+                ", terms=" + terms +
+                '}';
     }
 }
