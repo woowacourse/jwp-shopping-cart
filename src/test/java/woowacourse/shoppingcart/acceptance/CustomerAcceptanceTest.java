2b78eac92f26ed1c877ef094979bbf4b7336abe0
beomWhale <cndqjacndqja@gmail.com>
2022-05-30 22:06:54 +0900

    test: 회원 가입 인수 테스트 추가

--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -1,14 +1,38 @@
 package woowacourse.shoppingcart.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import woowacourse.shoppingcart.dto.CustomerCreateRequest;
 
 @DisplayName("회원 관련 기능")
 public class CustomerAcceptanceTest extends AcceptanceTest {
-    @DisplayName("회원가입")
+
+    @DisplayName("이메일, 패스워드, 닉네임을 입력 받아 회원가입을 한다.")
     @Test
     void addCustomer() {
+        CustomerCreateRequest customerCreateRequest = new CustomerCreateRequest(
+            "beomWhale@naver.com", "범고래", "Password12345!");
 
+        ExtractableResponse<Response> response = RestAssured.given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(customerCreateRequest)
+            .when()
+            .post("/api/customers")
+            .then().log().all()
+            .extract();
+
+        assertAll(
+            () -> assertThat(response.header("Location")).isNotEmpty(),
+            () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+        );
     }
 
     @DisplayName("내 정보 조회")
