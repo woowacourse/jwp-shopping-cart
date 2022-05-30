5cc5ab7fee0fa529d71706be850b64abfcc62479
hyeonic <dev.hyeonic@gmail.com>
2022-05-30 16:21:36 +0900

    feat: AuthorizationExtractor Authorization 헤더에서 Token 정보 추출 기능 추가

--- src/main/java/woowacourse/auth/support/AuthorizationExtractor.java
+++ src/main/java/woowacourse/auth/support/AuthorizationExtractor.java
@@ -1,28 +1,18 @@
 package woowacourse.auth.support;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
-
 public class AuthorizationExtractor {
-    public static final String AUTHORIZATION = "Authorization";
-    public static String BEARER_TYPE = "Bearer";
-    public static final String ACCESS_TOKEN_TYPE = AuthorizationExtractor.class.getSimpleName() + ".ACCESS_TOKEN_TYPE";
 
-    public static String extract(HttpServletRequest request) {
-        Enumeration<String> headers = request.getHeaders(AUTHORIZATION);
-        while (headers.hasMoreElements()) {
-            String value = headers.nextElement();
-            if ((value.toLowerCase().startsWith(BEARER_TYPE.toLowerCase()))) {
-                String authHeaderValue = value.substring(BEARER_TYPE.length()).trim();
-                request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(0, BEARER_TYPE.length()).trim());
-                int commaIndex = authHeaderValue.indexOf(',');
-                if (commaIndex > 0) {
-                    authHeaderValue = authHeaderValue.substring(0, commaIndex);
-                }
-                return authHeaderValue;
-            }
+    private static final String BEARER_TYPE = "Bearer";
+
+    public static String extract(String authorizationHeader) {
+        validateAuthorizationFormat(authorizationHeader);
+
+        return authorizationHeader.substring(BEARER_TYPE.length()).trim();
+    }
+
+    private static void validateAuthorizationFormat(String authorizationHeader) {
+        if (!authorizationHeader.toLowerCase().startsWith(BEARER_TYPE.toLowerCase())) {
+            throw new IllegalArgumentException("token 형식이 잘못 되었습니다. (형식: Bearer aaaaaaaa.bbbbbbbb.cccccccc)");
         }
-
-        return null;
     }
 }
