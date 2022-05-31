e8f6bff7e953b85445a08d1093b6957dc141ee80
sinb57 <sinb57@naver.com>
2022-05-31 14:54:03 +0900

    refactor: jwt 라이브러리 변경으로 인한 JwtTokenProvider 로직 수정

--- src/main/java/woowacourse/auth/support/JwtTokenProvider.java
+++ src/main/java/woowacourse/auth/support/JwtTokenProvider.java
@@ -1,5 +1,6 @@
 package woowacourse.auth.support;
 
+import java.security.Key;
 import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -10,13 +11,23 @@ import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import javax.annotation.PostConstruct;
 
 @Component
 public class JwtTokenProvider {
     @Value("${security.jwt.token.secret-key}")
     private String secretKey;
+    private Key key;
     @Value("${security.jwt.token.expire-length}")
     private long validityInMilliseconds;
+
+    @PostConstruct
+    private void initializeKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
     public String createToken(String payload) {
         Claims claims = Jwts.claims().setSubject(payload);
@@ -27,12 +38,12 @@ public class JwtTokenProvider {
                 .setClaims(claims)
                 .setIssuedAt(now)
                 .setExpiration(validity)
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String getPayload(String token) {
-        return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody().getSubject();
     }
 
     public boolean validateToken(String token) {
