20c9fb84a7f7f74e267ee8c2f210f68e64d54570
yxxnghwan <younghwan960@gmail.com>
2022-05-31 21:10:10 +0900

    feat: Customer 검증로직 구현

--- src/main/java/woowacourse/shoppingcart/dto/SignUpDto.java
+++ src/main/java/woowacourse/shoppingcart/dto/SignUpDto.java
@@ -9,15 +9,15 @@ import javax.validation.constraints.Size;
 
 public class SignUpDto {
 
-    @Email
-    @NotBlank
-    @Size(min = 8, max = 50)
+    @Email(message = "이메일 형식을 지켜야합니다.")
+    @NotBlank(message = "이메일에는 공백이 들어가면 안됩니다.")
+    @Size(min = 8, max = 50, message = "이메일은 8자 이상 50자 이하여야합니다.")
     private final String email;
-    @NotBlank
-    @Size(min = 8, max = 20)
+    @NotBlank(message = "비밀번호에는 공백이 들어가면 안됩니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하여야합니다.")
     private final String password;
-    @NotBlank
-    @Size(min = 1, max = 10)
+    @NotBlank(message = "닉네임에는 공백이 들어가면 안됩니다.")
+    @Size(min = 1, max = 10, message = "닉네임은 1자 이상 10자 이하여야합니다.")
     private final String username;
 
     public SignUpDto(final String email, final String password, final String username) {
---
+++ src/main/java/woowacourse/utils/StringValidator.java
@@ -0,0 +1,15 @@
+package woowacourse.utils;
+
+import woowacourse.shoppingcart.exception.ValidationException;
+
+public class StringValidator {
+
+    public static boolean validateLength(final int min, final int max, final String target, final ValidationException e){
+        return !(target.length() < min || target.length() > max);
+    }
+
+    public static boolean validateNullOrBlank(final String target, final ValidationException e) {
+        return !(target.isEmpty() || target.isBlank());
+    }
+
+}
