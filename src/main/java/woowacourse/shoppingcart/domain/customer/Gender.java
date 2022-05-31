9ec764842851c0d063f64bb87d0747bfdbde14ce
skullKim <kyk990328@gmail.com>
2022-05-31 16:43:29 +0900

    feat(Gender.java): 성별 기능 완료
    입력된 성별을 상수로 변환하는 기능 완료

---
+++ src/main/java/woowacourse/shoppingcart/domain/customer/Gender.java
@@ -0,0 +1,23 @@
+package woowacourse.shoppingcart.domain.customer;
+
+import java.util.Arrays;
+
+public enum Gender {
+
+    MALE("male"),
+    FEMALE("female"),
+    UNDEFINED("undefined");
+
+    private final String value;
+
+    Gender(final String value) {
+        this.value = value;
+    }
+
+    public static Gender form(String target) {
+        return Arrays.stream(values())
+                .filter(gender -> gender.value.equals(target))
+                .findFirst()
+                .orElse(UNDEFINED);
+    }
+}
