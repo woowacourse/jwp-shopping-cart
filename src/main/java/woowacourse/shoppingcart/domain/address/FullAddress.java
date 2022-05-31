86bec96070a148f2e3c0b88f19a6aef81f9bb13b
skullKim <kyk990328@gmail.com>
2022-05-31 19:52:04 +0900

    feat(FullAddress.java): 주소 도메인 검증 기능 구현 완료
    주소 중 우편변호 5글자를 검증하는 기능 완료

---
+++ src/main/java/woowacourse/shoppingcart/domain/address/FullAddress.java
@@ -0,0 +1,40 @@
+package woowacourse.shoppingcart.domain.address;
+
+public class FullAddress {
+
+    static final String INVALID_ZONE_CODE_FORMAT = "유효하지 않은 우편번호입니다.";
+
+    private static final int PROPER_ZONE_CODE_LENGTH = 5;
+
+    private String address;
+    private String DetailAddress;
+    private String zoneCode;
+
+    public FullAddress() {
+    }
+
+    public FullAddress(String address, String detailAddress, String zoneCode) {
+        validateZoneCode(zoneCode);
+        this.address = address;
+        this.DetailAddress = detailAddress;
+        this.zoneCode = zoneCode;
+    }
+
+    private void validateZoneCode(String zoneCode) {
+        if (zoneCode.length() != PROPER_ZONE_CODE_LENGTH) {
+            throw new IllegalArgumentException(INVALID_ZONE_CODE_FORMAT);
+        }
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDetailAddress() {
+        return DetailAddress;
+    }
+
+    public String getZoneCode() {
+        return zoneCode;
+    }
+}
