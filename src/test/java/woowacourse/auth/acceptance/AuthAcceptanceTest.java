ffa2d3e975ce305b62ed83ace3aee24acc12da93
sinb57 <sinb57@naver.com>
2022-05-31 18:37:21 +0900

    test: @Nested 제거

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -3,7 +3,6 @@ package woowacourse.auth.acceptance;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 
@@ -41,45 +40,40 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         assertThat(tokenResponse.getUserName()).isEqualTo(CUSTOMER_NAME);
     }
 
-    @DisplayName("로그인을 실패")
-    @Nested
-    class LoginFailedTest {
+    @DisplayName("로그인 실패 - 이메일이 존재하지 않는 경우")
+    @Test
+    void loginFailedWithNoSuchEmail() {
+        // given
+        RequestHandler.postRequest("/customers", new CustomerRegisterRequest(
+                CUSTOMER_EMAIL, CUSTOMER_NAME, CUSTOMER_PASSWORD));
 
-        @DisplayName("이메일이 존재하지 않는 경우")
-        @Test
-        void loginFailedWithNoSuchEmail() {
-            // given
-            RequestHandler.postRequest("/customers", new CustomerRegisterRequest(
-                    CUSTOMER_EMAIL, CUSTOMER_NAME, CUSTOMER_PASSWORD));
+        // when
+        final ExtractableResponse<Response> response = RequestHandler.postRequest(
+                "/auth/login", new TokenRequest("noGuest@woowa.com", CUSTOMER_PASSWORD));
 
-            // when
-            final ExtractableResponse<Response> response = RequestHandler.postRequest(
-                    "/auth/login", new TokenRequest("noGuest@woowa.com", CUSTOMER_PASSWORD));
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
 
-            // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        final ExceptionResponse exceptionResponse = response.jsonPath().getObject(".", ExceptionResponse.class);
+        assertThat(exceptionResponse.getMessage()).isEqualTo("이메일에 해당하는 회원이 존재하지 않거나 비밀번호가 일치하지 않습니다.");
+    }
 
-            final ExceptionResponse exceptionResponse = response.jsonPath().getObject(".", ExceptionResponse.class);
-            assertThat(exceptionResponse.getMessage()).isEqualTo("이메일에 해당하는 회원이 존재하지 않거나 비밀번호가 일치하지 않습니다.");
-        }
+    @DisplayName("로그인 실패 - 비밀번호가 일치하지 않는 경우")
+    @Test
+    void loginFailedWithWrongPassword() {
+        // given
+        RequestHandler.postRequest("/customers", new CustomerRegisterRequest(
+                CUSTOMER_EMAIL, CUSTOMER_NAME, CUSTOMER_PASSWORD));
 
-        @DisplayName("비밀번호가 일치하지 않는 경우")
-        @Test
-        void loginFailedWithWrongPassword() {
-            // given
-            RequestHandler.postRequest("/customers", new CustomerRegisterRequest(
-                    CUSTOMER_EMAIL, CUSTOMER_NAME, CUSTOMER_PASSWORD));
+        // when
+        final ExtractableResponse<Response> response = RequestHandler.postRequest(
+                "/auth/login", new TokenRequest(CUSTOMER_EMAIL, "wrongPassword"));
 
-            // when
-            final ExtractableResponse<Response> response = RequestHandler.postRequest(
-                    "/auth/login", new TokenRequest(CUSTOMER_EMAIL, "wrongPassword"));
+        // then
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
 
-            // then
-            assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
-
-            final ExceptionResponse exceptionResponse = response.jsonPath().getObject(".", ExceptionResponse.class);
-            assertThat(exceptionResponse.getMessage()).isEqualTo("이메일에 해당하는 회원이 존재하지 않거나 비밀번호가 일치하지 않습니다.");
-        }
+        final ExceptionResponse exceptionResponse = response.jsonPath().getObject(".", ExceptionResponse.class);
+        assertThat(exceptionResponse.getMessage()).isEqualTo("이메일에 해당하는 회원이 존재하지 않거나 비밀번호가 일치하지 않습니다.");
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
