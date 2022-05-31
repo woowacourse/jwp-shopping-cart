a369a9d5f82a303d86db23ca06d7e75779e8bd90
jurlring <78091011+jurlring@users.noreply.github.com>
2022-05-31 19:23:16 +0900

    test: 회원 수정 인수테스트 구현

--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -9,7 +9,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
 import woowacourse.shoppingcart.domain.Customer;
+import woowacourse.shoppingcart.dto.CustomerRequest;
 import woowacourse.shoppingcart.dto.CustomerResponse;
 
 @DisplayName("회원 관련 기능")
@@ -45,6 +48,45 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("내 정보 수정")
     @Test
     void updateMe() {
+        // given
+        Customer customer = new Customer("email", "Pw123456!", "name", "010-1234-5678", "address");
+        RestAssured.given().log().all()
+                .body(customer)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/customers")
+                .then().log().all()
+                .extract();
+
+        String accessToken = RestAssured.given().log().all()
+                .body(new TokenRequest("email", "Pw123456!"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/customers/login")
+                .then().log().all()
+                .extract().as(TokenResponse.class).getAccessToken();
+        //when
+        RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .body(new CustomerRequest("email", "Pw123456!", "judy", "010-1111-2222", "address2"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .put("/customers")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+        //then
+        CustomerResponse customerResponse = RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/customers")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract().as(CustomerResponse.class);
+
+        assertThat(customerResponse).extracting("email", "name", "phone", "address")
+                .containsExactly("email", "judy", "010-1111-2222", "address2");
     }
 
     @DisplayName("회원탈퇴")
