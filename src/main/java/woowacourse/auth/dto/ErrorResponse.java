918e03e6fe8e58c35b64493fc8f988ab77db6551
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-31 20:30:16 +0900

    refactor(ControllerAdvice): 예외 응답 포맷 적용

---
+++ src/main/java/woowacourse/auth/dto/ErrorResponse.java
@@ -0,0 +1,17 @@
+package woowacourse.auth.dto;
+
+public class ErrorResponse {
+
+    private String message;
+
+    public ErrorResponse() {
+    }
+
+    public ErrorResponse(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
