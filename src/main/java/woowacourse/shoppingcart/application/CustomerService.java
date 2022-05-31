06d0367c1089b81afa265f8e6d6048aea83a9fab
meatsby <meatsby@gmail.com>
2022-05-31 15:43:02 +0900

    feat: 이메일 중복 체크 기능 서비스 구현

---
+++ src/main/java/woowacourse/shoppingcart/application/CustomerService.java
@@ -0,0 +1,20 @@
+package woowacourse.shoppingcart.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import woowacourse.shoppingcart.dao.CustomerDao;
+
+@Service
+@Transactional(readOnly = true)
+public class CustomerService {
+
+    private final CustomerDao customerDao;
+
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public boolean isDistinctEmail(String email) {
+        return !customerDao.existEmail(email);
+    }
+}
---
+++ src/test/java/woowacourse/shoppingcart/application/CustomerServiceTest.java
@@ -0,0 +1,27 @@
+package woowacourse.shoppingcart.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceTest {
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerServiceTest(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @DisplayName("이메일이 중복되는지 확인한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"email@email.com, false", "distinctemail@email.com, true"})
+    void isDistinctEmail(final String email, final Boolean expected) {
+        assertThat(customerService.isDistinctEmail(email)).isEqualTo(expected);
+    }
+}
