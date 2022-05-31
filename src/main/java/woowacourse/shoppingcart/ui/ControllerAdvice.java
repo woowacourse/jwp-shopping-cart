6c0cc26fb33ac4c98b663b4b176ba9d11eead212
nailseong <nnis9857@gmail.com>
2022-06-01 01:42:30 +0900

    refactor: 불필요한 예외 핸들러 삭제

--- src/main/java/woowacourse/shoppingcart/ui/ControllerAdvice.java
+++ src/main/java/woowacourse/shoppingcart/ui/ControllerAdvice.java
@@ -4,7 +4,6 @@ import java.util.List;
 import javax.validation.ConstraintViolationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
@@ -27,30 +26,23 @@ public class ControllerAdvice {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity handleUnhandledException() {
-        return ResponseEntity.badRequest().body("Unhandled Exception");
-    }
-
-    @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity handle() {
-        return ResponseEntity.badRequest().body("존재하지 않는 데이터 요청입니다.");
-    }
-
-    @ExceptionHandler({MethodArgumentNotValidException.class})
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleInvalidRequest(final BindingResult bindingResult) {
         final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         final FieldError mainError = fieldErrors.get(0);
 
-        return ResponseEntity.badRequest().body(new ErrorResponse("1000", mainError.getDefaultMessage()));
+        final ErrorResponse response = new ErrorResponse("1000", mainError.getDefaultMessage());
+        return ResponseEntity
+                .badRequest()
+                .body(response);
     }
 
-    @ExceptionHandler({
-            HttpMessageNotReadableException.class,
-            ConstraintViolationException.class,
-    })
-    public ResponseEntity handleInvalidRequest(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    @ExceptionHandler({HttpMessageNotReadableException.class, ConstraintViolationException.class})
+    public ResponseEntity<ErrorResponse> handleInvalidRequest(final RuntimeException e) {
+        final ErrorResponse response = new ErrorResponse("1000", e.getMessage());
+        return ResponseEntity
+                .badRequest()
+                .body(response);
     }
 
     @ExceptionHandler({
@@ -64,9 +56,7 @@ public class ControllerAdvice {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
-    @ExceptionHandler({
-            ShoppingCartException.class
-    })
+    @ExceptionHandler(ShoppingCartException.class)
     public ResponseEntity<ErrorResponse> handleInvalidAccess(final ShoppingCartException exception) {
         return ResponseEntity
                 .status(exception.getHttpStatus())
