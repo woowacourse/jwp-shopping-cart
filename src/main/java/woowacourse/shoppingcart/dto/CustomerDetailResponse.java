7e59594ddc1b9e842cba36eb42b7ae2d0863bf60
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-31 22:36:44 +0900

    feat(CustomerController): 회원 정보 조회 API 구현

---
+++ src/main/java/woowacourse/shoppingcart/dto/CustomerDetailResponse.java
@@ -0,0 +1,27 @@
+package woowacourse.shoppingcart.dto;
+
+public class CustomerDetailResponse {
+
+    private String name;
+    private String email;
+
+    public CustomerDetailResponse() {
+    }
+
+    public CustomerDetailResponse(final String name, final String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public static CustomerDetailResponse from(final CustomerDetailServiceResponse serviceResponse) {
+        return new CustomerDetailResponse(serviceResponse.getName(), serviceResponse.getEmail());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
--- src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
+++ src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
@@ -2,11 +2,15 @@ package woowacourse.shoppingcart.ui;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import woowacourse.auth.support.AuthenticationPrincipal;
 import woowacourse.shoppingcart.application.CustomerService;
+import woowacourse.shoppingcart.dto.CustomerDetailResponse;
+import woowacourse.shoppingcart.dto.CustomerDetailServiceResponse;
 import woowacourse.shoppingcart.dto.CustomerRegisterRequest;
 
 @RestController
@@ -23,5 +27,12 @@ public class CustomerController {
     public ResponseEntity<Void> register(@RequestBody final CustomerRegisterRequest request) {
         customerService.save(request.toServiceDto());
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<CustomerDetailResponse> showMyDetail(@AuthenticationPrincipal final Long id) {
+        final CustomerDetailServiceResponse serviceResponse = customerService.findById(id);
+        final CustomerDetailResponse customerDetailResponse = CustomerDetailResponse.from(serviceResponse);
+        return ResponseEntity.ok(customerDetailResponse);
     }
 }
