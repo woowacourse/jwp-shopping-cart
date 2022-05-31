42c20749c257224ce21c80cda8547f11c2217298
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-31 18:36:21 +0900

    feat(AuthController): 로그인 API 구현

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -1,24 +1,54 @@
 package woowacourse.auth.acceptance;
 
+import io.restassured.RestAssured;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
+import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.shoppingcart.acceptance.AcceptanceTest;
+import woowacourse.shoppingcart.dto.CustomerRegisterRequest;
 
 @DisplayName("인증 관련 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
+
+    private static final String NAME = "클레이";
+    private static final String EMAIL = "djwhy5510@naver.com";
+    private static final String PASSWORD = "12345678";
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
     @DisplayName("Bearer Auth 로그인 성공")
     @Test
     void myInfoWithBearerAuth() {
         // given
         // 회원이 등록되어 있고
-        // id, password를 사용해 토큰을 발급받고
+        CustomerRegisterRequest request = new CustomerRegisterRequest(NAME, EMAIL, PASSWORD);
+        RestAssured.given().log().all()
+                .body(request)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/api/customer")
+                .then().log().all()
+                .extract();
 
         // when
-        // 발급 받은 토큰을 사용하여 내 정보 조회를 요청하면
+        // id, password를 사용해 로그인 시도를 하면
+        String accessToken = RestAssured
+                .given().log().all()
+                .body(new TokenRequest(EMAIL, PASSWORD))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/api/login")
+                .then().log().all().extract().as(TokenResponse.class).getAccessToken();
 
         // then
-        // 내 정보가 조회된다
+        // 요청이 성공하고 토큰이 발급된다.
+        Assertions.assertThat(jwtTokenProvider.validateToken(accessToken)).isTrue();
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
