29b17894f1d2b654a439579da11aebc08b041fc0
skullKim <kyk990328@gmail.com>
2022-05-31 18:03:46 +0900

    test(EmailTest.java): 이메일 도메인에 대한 테스트 추가

---
+++ src/test/java/woowacourse/shoppingcart/domain/customer/EmailTest.java
@@ -0,0 +1,32 @@
+package woowacourse.shoppingcart.domain.customer;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Email 은")
+public class EmailTest {
+
+    @DisplayName("이메일 주소가 입력되었을 때")
+    @Nested
+    class EmailFormTest {
+
+        @Test
+        @DisplayName("이메일 주소가 올바르면 이메일 주소를을 저장한다.")
+        void validEmailForm() {
+            assertThatNoException().isThrownBy(() -> new Email("her0807@naver.com"));
+        }
+
+        @Test
+        @DisplayName("이메일 주소가 올바르지 않으면 예외를 던진다.")
+        void invalidEmailForm() {
+            assertThatThrownBy(() -> new Email("her0807naver.com"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(Email.INVALID_EMAIL_FORMAT);
+        }
+    }
+
+}
