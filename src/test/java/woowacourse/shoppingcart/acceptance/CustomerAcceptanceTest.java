ea8c7e153989e548504946a63f1f37260fd96b38
yhh1056 <yhh1056@naver.com>
2022-05-31 13:55:23 +0900

    test: 회원 가입 요청 테스트 메서드 분리

--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -8,36 +8,26 @@ import io.restassured.response.Response;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
 @DisplayName("회원 관련 기능")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SuppressWarnings("NonAsciiCharacters")
 public class CustomerAcceptanceTest extends AcceptanceTest {
 
-    @DisplayName("회원가입")
     @Test
-    void addCustomer() {
-        Map<String, Object> request = new HashMap<>();
-        request.put("account", "loe0842");
-        request.put("nickname", "에덴");
-        request.put("password", "dpepsWkd12!");
-        request.put("address", "에덴 동산");
-
-        Map<String, String> phoneNumber = new HashMap<>();
-        phoneNumber.put("start", "010");
-        phoneNumber.put("middle", "1234");
-        phoneNumber.put("last", "5678");
-        request.put("phoneNumber", phoneNumber);
-
-        ExtractableResponse<Response> response = RestAssured.given()
-                .log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/signup")
-                .then()
-                .log().all()
-                .extract();
+    void 회원_가입_정상_요청() {
+        ExtractableResponse<Response> response = 회원_가입(
+                회원_정보("loe0842",
+                        "에덴",
+                        "dpepsWkd12!",
+                        "에덴 동산",
+                        "010",
+                        "1234",
+                        "5678"));
 
         assertThat(response.statusCode()).isEqualTo(201);
         assertThat(response.header("Location")).isEqualTo("/signin");
@@ -56,5 +46,36 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원탈퇴")
     @Test
     void deleteMe() {
+    }
+
+    private Map<String, Object> 회원_정보(String account, String nickname, String password,
+            String address, String start, String middle, String last) {
+        Map<String, Object> request = new HashMap<>();
+        request.put("account", account);
+        request.put("nickname", nickname);
+        request.put("password", password);
+        request.put("address", address);
+        request.put("phoneNumber", 휴대폰_정보(start, middle, last));
+        return request;
+    }
+
+    private Map<String, String> 휴대폰_정보(String start, String middle, String last) {
+        Map<String, String> phoneNumber = new HashMap<>();
+        phoneNumber.put("start", start);
+        phoneNumber.put("middle", middle);
+        phoneNumber.put("last", last);
+        return phoneNumber;
+    }
+
+    private ExtractableResponse<Response> 회원_가입(Map<String, Object> request) {
+        return RestAssured.given()
+                .log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/signup")
+                .then()
+                .log().all()
+                .extract();
     }
 }
