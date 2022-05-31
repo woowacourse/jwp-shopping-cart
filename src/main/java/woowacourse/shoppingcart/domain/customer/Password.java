2aaa42fdf9df3b7b85552a965086c2d75c5593f4
skullKim <kyk990328@gmail.com>
2022-05-31 15:38:46 +0900

    feat(Password.java): 비밀번호 도메인 구현 완료
     비밀번호 유효 검증과 해시화를 담당하는 도메인 구현 완료

---
+++ src/main/java/woowacourse/shoppingcart/domain/customer/Password.java
@@ -0,0 +1,42 @@
+package woowacourse.shoppingcart.domain.customer;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.regex.Pattern;
+
+public class Password {
+    private static final String REGULAR_EXPRESSION = "^(?=.*[0-9])(?=.*[a-z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
+    private static final Pattern compiledPattern = Pattern.compile(REGULAR_EXPRESSION);
+    static final String INVALID_PASSWORD_FORMAT = "올바르지 않은 비밀번호입니다.";
+
+    private final String value;
+
+    public Password(String rawPassword) {
+        validatePassword(rawPassword);
+        value = encryptPassword(rawPassword);
+    }
+
+    private void validatePassword(String rawPassword) {
+        if (!compiledPattern.matcher(rawPassword).matches()) {
+            throw new IllegalArgumentException(INVALID_PASSWORD_FORMAT);
+        }
+    }
+
+    private String encryptPassword(String rawPassword) {
+        try {
+            final MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+            messageDigest.update(rawPassword.getBytes());
+            return bytesToHex(messageDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private String bytesToHex(final byte[] bytes) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (byte b : bytes) {
+            stringBuilder.append(String.format("%02x", b));
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ src/test/java/woowacourse/shoppingcart/domain/customer/PasswordTest.java
@@ -0,0 +1,31 @@
+package woowacourse.shoppingcart.domain.customer;
+
+import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Password 는")
+public class PasswordTest {
+
+    @DisplayName("비밀번호가 입력되었을 때")
+    @Nested
+    class PasswordValidationTest {
+
+        @DisplayName("올바른 비밀번호가 입력되었으면 비밀번호를 해시화하여 저장한디.")
+        @Test
+        void validPassword() {
+            assertThatNoException().isThrownBy(() -> new Password("password&1"));
+        }
+
+        @DisplayName("비밀번호가 올바르지 않으면 에러를 던진다")
+        @Test
+        void invalidPassword() {
+            assertThatThrownBy(() -> new Password("invalid"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage(Password.INVALID_PASSWORD_FORMAT);
+        }
+    }
+}
