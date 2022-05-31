55efbfcf0272900ff94e50b5b5a920bdf6772727
Jeong Jinwoo <jwjeong96@gmail.com>
2022-05-31 20:02:45 +0900

    refactor(CustomerService): remove CustomerRepository
    - use CustomerDao directly

--- src/main/java/woowacourse/shoppingcart/application/CustomerService.java
+++ src/main/java/woowacourse/shoppingcart/application/CustomerService.java
@@ -2,68 +2,69 @@ package woowacourse.shoppingcart.application;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import woowacourse.shoppingcart.dao.CustomerDao;
 import woowacourse.shoppingcart.domain.Customer;
-import woowacourse.shoppingcart.dto.request.UniqueUsernameRequest;
-import woowacourse.shoppingcart.dto.response.GetMeResponse;
 import woowacourse.shoppingcart.dto.request.SignUpRequest;
+import woowacourse.shoppingcart.dto.request.UniqueUsernameRequest;
 import woowacourse.shoppingcart.dto.request.UpdateMeRequest;
 import woowacourse.shoppingcart.dto.request.UpdatePasswordRequest;
+import woowacourse.shoppingcart.dto.response.GetMeResponse;
 import woowacourse.shoppingcart.dto.response.UniqueUsernameResponse;
-import woowacourse.shoppingcart.repository.CustomerRepository;
+import woowacourse.shoppingcart.exception.NotFoundException;
 
 @Service
 public class CustomerService {
 
-    private final CustomerRepository customerRepository;
+    private final CustomerDao customerDao;
 
-    public CustomerService(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
     }
 
     @Transactional
     public Long signUp(SignUpRequest request) {
         Customer customer = new Customer(request.getUsername(),
                 request.getPassword(), request.getNickname(), request.getAge());
-        return customerRepository.save(customer);
+        return customerDao.save(customer);
     }
 
     public GetMeResponse getMe(Long id) {
-        return new GetMeResponse(customerRepository.find(id));
+        return new GetMeResponse(findCustomer(id));
     }
 
     public UniqueUsernameResponse checkUniqueUsername(UniqueUsernameRequest request) {
-        boolean isUnique = !customerRepository.checkUsernameExistence(request.getUsername());
+        String username = request.getUsername();
+        boolean isUnique =  customerDao.findByUserName(username).isEmpty();
         return new UniqueUsernameResponse(isUnique);
     }
 
     @Transactional
     public void updateMe(Long id, UpdateMeRequest request) {
-        Customer customer = customerRepository.find(id);
-        Customer updatedCustomer = new Customer(id,
-                request.getUsername(),
-                customer.getPassword(),
-                request.getNickname(),
-                request.getAge());
-        customerRepository.update(updatedCustomer);
+        Customer customer = findCustomer(id);
+        Customer updatedCustomer = new Customer(id, request.getUsername(),
+                customer.getPassword(), request.getNickname(), request.getAge());
+        customerDao.update(updatedCustomer);
     }
 
     @Transactional
     public void updatePassword(Long id, UpdatePasswordRequest request) {
-        Customer customer = customerRepository.find(id);
+        Customer customer = findCustomer(id);
         if (!customer.hasSamePassword(request.getOldPassword())) {
             throw new IllegalArgumentException("현재 비밀번호를 잘못 입력하였습니다.");
         }
-        Customer updatedCustomer = new Customer(id,
-                customer.getUsername(),
-                request.getNewPassword(),
-                customer.getNickname(),
-                customer.getAge());
-        customerRepository.update(updatedCustomer);
+        Customer updatedCustomer = new Customer(id, customer.getUsername(),
+                request.getNewPassword(), customer.getNickname(), customer.getAge());
+        customerDao.update(updatedCustomer);
     }
 
     @Transactional
     public void deleteMe(Long id) {
-        Customer customer = customerRepository.find(id);
-        customerRepository.delete(customer);
+        Customer customer = findCustomer(id);
+        customerDao.delete(customer);
+    }
+
+    private Customer findCustomer(Long id) {
+        return customerDao.findById(id)
+                .orElseThrow(() -> new NotFoundException("존재하지 않는 고객입니다."));
     }
 }
