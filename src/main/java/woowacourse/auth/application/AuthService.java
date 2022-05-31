f84877531b819d060b9242b6678e53faadf967bd
sinb57 <sinb57@naver.com>
2022-05-31 14:55:44 +0900

    feat: AuthService 로그인-토큰발행 기능 구현

--- src/main/java/woowacourse/auth/application/AuthService.java
+++ src/main/java/woowacourse/auth/application/AuthService.java
@@ -2,6 +2,38 @@ package woowacourse.auth.application;
 
 import org.springframework.stereotype.Service;
 
+import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
+import woowacourse.auth.support.JwtTokenProvider;
+import woowacourse.shoppingcart.domain.Customer;
+import woowacourse.shoppingcart.exception.InvalidCustomerException;
+import woowacourse.shoppingcart.exception.WrongPasswordException;
+import woowacourse.shoppingcart.infrastructure.jdbc.dao.CustomerDao;
+
 @Service
 public class AuthService {
+
+    private final CustomerDao customerDao;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    public AuthService(final CustomerDao customerDao, final JwtTokenProvider jwtTokenProvider) {
+        this.customerDao = customerDao;
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    public TokenResponse login(final TokenRequest tokenRequest) {
+        Customer customer = getCustomer(tokenRequest.getEmail());
+
+        if (!customer.equalsPassword(tokenRequest.getPassword())) {
+            throw new WrongPasswordException();
+        }
+
+        return new TokenResponse(jwtTokenProvider.createToken(String.valueOf(customer.getId())));
+    }
+
+    private Customer getCustomer(final String email) {
+        return customerDao.findByEmail(email)
+                .orElseThrow(InvalidCustomerException::new);
+    }
+
 }
