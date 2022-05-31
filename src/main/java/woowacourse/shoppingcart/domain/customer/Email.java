0fe9c2cc2b7376da7a9ca822628f102a77ee5942
skullKim <kyk990328@gmail.com>
2022-05-31 14:36:36 +0900

    feat(Email.java): 이메일 도메인 구현 완료
    입력한 이메일을 검증하는 도메인 완료

---
+++ src/main/java/woowacourse/shoppingcart/domain/customer/Email.java
@@ -0,0 +1,27 @@
+package woowacourse.shoppingcart.domain.customer;
+
+import java.util.regex.Pattern;
+
+public class Email {
+
+    private static final String REGULAR_EXPRESSION = "[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\\.([a-zA-Z])+";
+    private static final Pattern COMPILED_PATTERN = Pattern.compile(REGULAR_EXPRESSION);
+    static final String INVALID_EMAIL_FORMAT = "입력하신 이메일 주소가 올바르지 않습니다.";
+
+    private final String value;
+
+    public Email(final String value) {
+        validateEmail(value);
+        this.value = value;
+    }
+
+    private void validateEmail(final String target) {
+        if (!COMPILED_PATTERN.matcher(target).matches()) {
+            throw new IllegalArgumentException(INVALID_EMAIL_FORMAT);
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
