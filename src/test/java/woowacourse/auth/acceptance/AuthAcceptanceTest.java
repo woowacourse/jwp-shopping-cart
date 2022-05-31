f3f28ea7bc64307f8b5cff5e02298c551e820298
yangdongjue5510 <djwhy5510@gmail.com>
2022-05-31 20:23:50 +0900

    test(AuthAcceptanceTest): 로그인 실패 테스트 추가

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -1,10 +1,15 @@
 package woowacourse.auth.acceptance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import io.restassured.RestAssured;
-import org.assertj.core.api.Assertions;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
@@ -13,7 +18,7 @@ import woowacourse.shoppingcart.acceptance.AcceptanceTest;
 import woowacourse.shoppingcart.dto.CustomerRegisterRequest;
 
 @DisplayName("인증 관련 기능")
-public class AuthAcceptanceTest extends AcceptanceTest {
+class AuthAcceptanceTest extends AcceptanceTest {
 
     private static final String NAME = "클레이";
     private static final String EMAIL = "djwhy5510@naver.com";
@@ -24,9 +29,8 @@ public class AuthAcceptanceTest extends AcceptanceTest {
 
     @DisplayName("Bearer Auth 로그인 성공")
     @Test
-    void myInfoWithBearerAuth() {
-        // given
-        // 회원이 등록되어 있고
+    void login() {
+        // given 회원이 등록되어 있고
         CustomerRegisterRequest request = new CustomerRegisterRequest(NAME, EMAIL, PASSWORD);
         RestAssured.given().log().all()
                 .body(request)
@@ -36,8 +40,7 @@ public class AuthAcceptanceTest extends AcceptanceTest {
                 .then().log().all()
                 .extract();
 
-        // when
-        // id, password를 사용해 로그인 시도를 하면
+        // when id, password를 사용해 로그인 시도를 하면
         String accessToken = RestAssured
                 .given().log().all()
                 .body(new TokenRequest(EMAIL, PASSWORD))
@@ -46,31 +49,73 @@ public class AuthAcceptanceTest extends AcceptanceTest {
                 .when().post("/api/login")
                 .then().log().all().extract().as(TokenResponse.class).getAccessToken();
 
-        // then
-        // 요청이 성공하고 토큰이 발급된다.
-        Assertions.assertThat(jwtTokenProvider.validateToken(accessToken)).isTrue();
+        // then 요청이 성공하고 토큰이 발급된다.
+        assertThat(jwtTokenProvider.validateToken(accessToken)).isTrue();
     }
 
-    @DisplayName("Bearer Auth 로그인 실패")
+    @DisplayName("Bearer Auth 로그인 실패 - 존재하지 않는 이메일")
     @Test
-    void myInfoWithBadBearerAuth() {
-        // given
-        // 회원이 등록되어 있고
+    void login_wrongEmail_badRequest() {
+        // given 회원이 등록되어 있고
+        CustomerRegisterRequest request = new CustomerRegisterRequest(NAME, EMAIL, PASSWORD);
+        RestAssured.given().log().all()
+                .body(request)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/api/customer")
+                .then().log().all()
+                .extract();
 
-        // when
-        // 잘못된 id, password를 사용해 토큰을 요청하면
+        // when 존재하지 않는 이메일을 사용해 토큰을 요청하면
+        CustomerRegisterRequest invalidRequest = new CustomerRegisterRequest(NAME, "lalala@gamil.com", PASSWORD);
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(invalidRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/api/login")
+                .then().log().all().extract();
 
-        // then
-        // 토큰 발급 요청이 거부된다
+        // then 토큰 발급 요청이 거부된다
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(response.body().asString()).isEqualTo("존재하지 않는 이메일입니다.")
+        );
+    }
+
+    @DisplayName("Bearer Auth 로그인 실패 - 일치하지 않는 비밀번호")
+    @Test
+    void login_wrongPassword_badRequest() {
+        // given 회원이 등록되어 있고
+        CustomerRegisterRequest request = new CustomerRegisterRequest(NAME, EMAIL, PASSWORD);
+        RestAssured.given().log().all()
+                .body(request)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .post("/api/customer")
+                .then().log().all()
+                .extract();
+
+        // when 이메일에 일치하지 않는 비밀번호를 사용해 토큰을 요청하면
+        CustomerRegisterRequest invalidRequest = new CustomerRegisterRequest(NAME, EMAIL, "1111111111");
+        final ExtractableResponse<Response> response = RestAssured.given().log().all()
+                .body(invalidRequest)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().post("/api/login")
+                .then().log().all().extract();
+
+        // then 토큰 발급 요청이 거부된다
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(response.body().asString()).isEqualTo("비밀번호가 일치하지 않습니다.")
+        );
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
-        // when
-        // 유효하지 않은 토큰을 사용하여 내 정보 조회를 요청하면
+        // when 유효하지 않은 토큰을 사용하여 내 정보 조회를 요청하면
 
-        // then
-        // 내 정보 조회 요청이 거부된다
+        // then 내 정보 조회 요청이 거부된다
     }
 }
