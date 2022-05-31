dd0ff7e1461e2fe8860f49521ef0735ab09351da
jurlring <78091011+jurlring@users.noreply.github.com>
2022-05-31 16:21:13 +0900

    feat: AuthService Token 발급 기능 구현

--- src/main/java/woowacourse/auth/application/AuthService.java
+++ src/main/java/woowacourse/auth/application/AuthService.java
@@ -3,6 +3,8 @@ package woowacourse.auth.application;
 import java.util.Optional;
 import org.springframework.stereotype.Service;
 import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
+import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.shoppingcart.dao.CustomerDao;
 import woowacourse.shoppingcart.domain.Customer;
 import woowacourse.shoppingcart.dto.CustomerRequest;
@@ -13,10 +15,13 @@ import woowacourse.shoppingcart.exception.InvalidCustomerException;
 public class AuthService {
 
     private final CustomerDao customerDao;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthService(CustomerDao customerDao) {
+    public AuthService(CustomerDao customerDao, JwtTokenProvider jwtTokenProvider) {
         this.customerDao = customerDao;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
+
 
     public CustomerResponse save(CustomerRequest customerRequest) {
         validateDuplicationEmail(customerRequest);
@@ -36,5 +41,10 @@ public class AuthService {
                 tokenRequest.getPassword());
         return idByEmailAndPassword.orElseThrow(
                 () -> new InvalidCustomerException("Email 또는 Password가 일치하지 않습니다."));
+    }
+
+    public TokenResponse createToken(Long customerId) {
+        String accessToken = jwtTokenProvider.createToken(String.valueOf(customerId));
+        return new TokenResponse(accessToken);
     }
 }
--- src/test/java/woowacourse/auth/application/AuthServiceTest.java
+++ src/test/java/woowacourse/auth/application/AuthServiceTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
 import woowacourse.shoppingcart.dto.CustomerRequest;
 import woowacourse.shoppingcart.dto.CustomerResponse;
 import woowacourse.shoppingcart.exception.InvalidCustomerException;
@@ -69,6 +70,15 @@ class AuthServiceTest {
     @Test
     @DisplayName("customer id를 이용하여 token을 발급한다.")
     void createToken() {
+        CustomerRequest customer =
+                new CustomerRequest("email", "Pw123456!", "name", "010-2222-3333", "address");
+        authService.save(customer);
 
+        TokenRequest tokenRequest = new TokenRequest("email", "Pw123456!");
+        Long customerId = authService.loginCustomer(tokenRequest);
+
+        TokenResponse accessToken = authService.createToken(customerId);
+
+        assertThat(accessToken.getAccessToken()).isNotBlank();
     }
 }
