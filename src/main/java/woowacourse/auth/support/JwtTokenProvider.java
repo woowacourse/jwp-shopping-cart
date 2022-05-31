e2ecb8d603fd331980946ec2783ff6e7214e8e05
Ohzzi <clover7even@naver.com>
2022-05-31 20:29:29 +0900

    refactor: JwtTokenProvider에서 TokenProvider를 인터페이스로 분리

--- src/main/java/woowacourse/auth/support/JwtTokenProvider.java
+++ src/main/java/woowacourse/auth/support/JwtTokenProvider.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 import woowacourse.auth.exception.AuthorizationException;
 
 @Component
-public class JwtTokenProvider {
+public class JwtTokenProvider implements TokenProvider {
 
     private final Key signingKey;
     private final long validityInMilliseconds;
@@ -25,6 +25,7 @@ public class JwtTokenProvider {
         this.validityInMilliseconds = validityInMilliseconds;
     }
 
+    @Override
     public String createToken(String payload) {
         Claims claims = Jwts.claims().setSubject(payload);
         Date now = new Date();
@@ -38,29 +39,31 @@ public class JwtTokenProvider {
                 .compact();
     }
 
+    @Override
     public String getPayload(String token) {
         if (!validateToken(token)) {
             throw new AuthorizationException("유효하지 않은 토큰입니다.");
         }
-        return Jwts.parserBuilder()
-                .setSigningKey(signingKey)
-                .build()
-                .parseClaimsJws(token)
+        return getClaimsJws(token)
                 .getBody()
                 .getSubject();
     }
 
+    @Override
     public boolean validateToken(String token) {
         try {
-            Jws<Claims> claims = Jwts.parserBuilder()
-                    .setSigningKey(signingKey)
-                    .build()
-                    .parseClaimsJws(token);
-
+            Jws<Claims> claims = getClaimsJws(token);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (JwtException | IllegalArgumentException e) {
             return false;
         }
+    }
+
+    private Jws<Claims> getClaimsJws(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(signingKey)
+                .build()
+                .parseClaimsJws(token);
     }
 }
 
