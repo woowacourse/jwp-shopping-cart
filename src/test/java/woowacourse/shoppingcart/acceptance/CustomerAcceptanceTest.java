3471efeb6ee1eadad66e322e11126e017f1fa3e5
dongho <dongho108@naver.com>
2022-05-31 15:23:27 +0900

    test: 토큰 관련된 테스트는 AuthAcceptanceTest로 이동

--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -12,7 +12,6 @@ import org.springframework.http.MediaType;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import io.restassured.response.ValidatableResponse;
 import woowacourse.auth.dto.CustomerResponse;
 import woowacourse.auth.dto.LoginRequest;
 import woowacourse.auth.dto.TokenResponse;
@@ -96,34 +95,6 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
         );
     }
 
-    @DisplayName("로그인")
-    @Test
-    void login() {
-        // given
-        SignupRequest signupRequest = new SignupRequest("dongho108", "ehdgh1234", "01022728572", "인천 서구 검단로");
-
-        RestAssured.given().log().all()
-            .body(signupRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/customers/signup")
-            .then().log().all()
-            .extract();
-
-        // when
-        LoginRequest loginRequest = new LoginRequest("dongho108", "ehdgh1234");
-        String accessToken = RestAssured.given().log().all()
-            .body(loginRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/customers/login")
-            .then().log().all()
-            .extract().as(TokenResponse.class).getAccessToken();
-
-        // then
-        assertThat(jwtTokenProvider.validateToken(accessToken)).isTrue();
-    }
-
     @DisplayName("내 정보 조회")
     @Test
     void getMe() {
@@ -160,109 +131,6 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
             () -> assertThat(customerResponse.getUsername()).isEqualTo(signupRequest.getUsername()),
             () -> assertThat(customerResponse.getPhoneNumber()).isEqualTo(signupRequest.getPhoneNumber()),
             () -> assertThat(customerResponse.getAddress()).isEqualTo(signupRequest.getAddress())
-        );
-    }
-
-    @DisplayName("잘못된 토큰으로 내 정보 조회를 하면 401 에러를 반환해야 한다.")
-    @Test
-    void getMeByInvalidToken() {
-        // given
-        SignupRequest signupRequest = new SignupRequest("dongho108", "ehdgh1234", "01022728572", "인천 서구 검단로");
-
-        RestAssured.given().log().all()
-            .body(signupRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/customers/signup")
-            .then().log().all()
-            .extract();
-
-        // when
-        RestAssured
-            .given().log().all()
-            .body(new LoginRequest("dongho108", "ehdgh1234"))
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when().post("/api/customers/login")
-            .then().log().all().extract();
-
-        // then
-        ValidatableResponse validatableResponse = RestAssured
-            .given().log().all()
-            .auth().oauth2(INVALID_ACCESS_TOKEN)
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when().get("/api/customers")
-            .then().log().all();
-        ExceptionResponse response = validatableResponse.extract().as(ExceptionResponse.class);
-
-        assertAll(
-            () -> validatableResponse.statusCode(HttpStatus.UNAUTHORIZED.value()),
-            () -> assertThat(response.getMessages())
-                .containsExactly("유효하지 않은 토큰입니다.")
-        );
-    }
-
-    @DisplayName("해당 아이디가 존재하지 않으면 (401)unauthorized 를 반환해야 한다.")
-    @Test
-    void loginByInvalidUsername() {
-        // given
-        SignupRequest signupRequest = new SignupRequest("dongho108", "ehdgh1234", "01022728572", "인천 서구 검단로");
-
-        RestAssured.given().log().all()
-            .body(signupRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/customers/signup")
-            .then().log().all()
-            .extract();
-
-        // when
-        ValidatableResponse validatableResponse = RestAssured
-            .given().log().all()
-            .body(new LoginRequest("dongho109", "ehdgh1234"))
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when().post("/api/customers/login")
-            .then().log().all();
-        ExceptionResponse exceptionResponse = validatableResponse.extract().as(ExceptionResponse.class);
-
-        // then
-        assertAll(
-            () -> validatableResponse.statusCode(HttpStatus.UNAUTHORIZED.value()),
-            () -> assertThat(exceptionResponse.getMessages())
-                .containsExactly("해당하는 username이 없습니다.")
-        );
-    }
-
-    @DisplayName("비밀번호가 일치하지 않으면 (401)unauthorized 를 반환해야 한다.")
-    @Test
-    void loginByInvalidPassword() {
-        // given
-        SignupRequest signupRequest = new SignupRequest("dongho108", "ehdgh1234", "01022728572", "인천 서구 검단로");
-
-        RestAssured.given().log().all()
-            .body(signupRequest)
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .when()
-            .post("/api/customers/signup")
-            .then().log().all()
-            .extract();
-
-        // when
-        ValidatableResponse validatableResponse = RestAssured
-            .given().log().all()
-            .body(new LoginRequest("dongho108", "password"))
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaType.APPLICATION_JSON_VALUE)
-            .when().post("/api/customers/login")
-            .then().log().all();
-        ExceptionResponse exceptionResponse = validatableResponse.extract().as(ExceptionResponse.class);
-
-        // then
-        assertAll(
-            () -> validatableResponse.statusCode(HttpStatus.UNAUTHORIZED.value()),
-            () -> assertThat(exceptionResponse.getMessages())
-                .containsExactly("비밀번호가 일치하지 않습니다.")
         );
     }
 
