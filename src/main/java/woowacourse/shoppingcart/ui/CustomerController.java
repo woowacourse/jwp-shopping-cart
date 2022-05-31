ecc8c4d1d40498fb2e992c1bf319f6daf6013e51
sinb57 <sinb57@naver.com>
2022-05-31 17:38:02 +0900

    feat: CustomerController 회원정보 수정 API 구현

--- src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
+++ src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
@@ -3,14 +3,18 @@ package woowacourse.shoppingcart.ui;
 import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import woowacourse.auth.support.AuthenticationPrincipal;
 import woowacourse.shoppingcart.application.CustomerService;
 import woowacourse.shoppingcart.dto.CustomerRegisterRequest;
 import woowacourse.shoppingcart.dto.CustomerResponse;
+import woowacourse.shoppingcart.dto.CustomerUpdateRequest;
+import woowacourse.shoppingcart.dto.CustomerUpdateResponse;
 
 @RestController
 @RequestMapping("/customers")
@@ -29,5 +33,12 @@ public class CustomerController {
         final CustomerResponse customerResponse = customerService.findById(customerId);
 
         return ResponseEntity.created(URI.create("/customers/" + customerId)).body(customerResponse);
+    }
+
+    @PatchMapping
+    public ResponseEntity<CustomerUpdateResponse> updateCustomer(
+            @AuthenticationPrincipal Long id, @RequestBody CustomerUpdateRequest customerUpdateRequest) {
+        final CustomerUpdateResponse customerUpdateResponse = customerService.updateCustomer(id, customerUpdateRequest);
+        return ResponseEntity.ok(customerUpdateResponse);
     }
 }
