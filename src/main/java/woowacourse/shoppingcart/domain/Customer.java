b98ff5dd192e04ecbb12e3e683cd1fa1dc88a370
hyeonic <dev.hyeonic@gmail.com>
2022-05-31 23:55:13 +0900

    feat: Customer equals, hashCode 추가

--- src/main/java/woowacourse/shoppingcart/domain/Customer.java
+++ src/main/java/woowacourse/shoppingcart/domain/Customer.java
@@ -96,4 +96,21 @@ public class Customer {
     public String getPhoneNumber() {
         return phoneNumber;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Customer customer = (Customer) o;
+        return Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
