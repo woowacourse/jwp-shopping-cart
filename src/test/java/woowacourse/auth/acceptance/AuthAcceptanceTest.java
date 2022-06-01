e5069925a9cfb89f8215c4a8cd0de498bc5fae5c
이한울 <dkfghg3@naver.com>
2022-06-01 16:30:36 +0900

    test: 회원 인수 테스트 픽스쳐 추가

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -2,17 +2,18 @@ package woowacourse.auth.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import woowacourse.auth.dto.TokenRequest;
+import woowacourse.auth.dto.TokenResponse;
 import woowacourse.shoppingcart.acceptance.AcceptanceTest;
-import woowacourse.shoppingcart.dto.CustomerRequest;
+import woowacourse.shoppingcart.acceptance.fixture.CustomerAcceptanceFixture;
+import woowacourse.shoppingcart.dto.CustomerResponse;
+import woowacourse.support.SimpleRestAssured;
 
 @DisplayName("인증 관련 기능")
 public class AuthAcceptanceTest extends AcceptanceTest {
@@ -21,70 +22,50 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @Test
     void myInfoWithBearerAuth() {
         // given
-        // 회원이 등록되어 있고
-        // id, password를 사용해 토큰을 발급받고
+        CustomerAcceptanceFixture.saveCustomer();
+        TokenRequest request = new TokenRequest("username", "password12!@");
+
+        TokenResponse tokenResponse = SimpleRestAssured.toObject(
+                SimpleRestAssured.post("/api/auth/token", request),
+                TokenResponse.class
+        );
 
         // when
-        // 발급 받은 토큰을 사용하여 내 정보 조회를 요청하면
+        String accessToken = "Bearer " + tokenResponse.getAccessToken();
+        CustomerResponse customerResponse = SimpleRestAssured.toObject(
+                SimpleRestAssured.get("/api/customers/me", new Header("Authorization", accessToken)),
+                CustomerResponse.class
+        );
 
         // then
-        // 내 정보가 조회된다
+        assertThat(customerResponse.getName()).isEqualTo("username");
     }
 
     @DisplayName("Bearer Auth 로그인 실패")
     @Test
     void myInfoWithBadBearerAuth() {
         // given
-        CustomerRequest customerRequest = new CustomerRequest(
-                "username",
-                "password12!@",
-                "example@example.com",
-                "some-address",
-                "010-0000-0001"
-        );
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/api/customers")
-                .then().log().all();
+        CustomerAcceptanceFixture.saveCustomer();
 
         // when
         TokenRequest request = new TokenRequest("username", "wrongPassword12@");
-        ExtractableResponse<Response> extracted = RestAssured.given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when().post("/api/auth/token")
-                .then().log().all().extract();
+        ExtractableResponse<Response> tokenResponse =
+                SimpleRestAssured.post("/api/auth/token", request);
 
         // then
-        assertThat(extracted.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        assertThat(tokenResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
     @Test
     void myInfoWithWrongBearerAuth() {
         // given
-        CustomerRequest customerRequest = new CustomerRequest(
-                "username",
-                "password12!@",
-                "example@example.com",
-                "some-address",
-                "010-0000-0001"
-        );
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/api/customers")
-                .then().log().all();
+        CustomerAcceptanceFixture.saveCustomer();
 
         // when
         String invalidToken = "Bearer aaaaaa.bbbbbb.ccccc";
-        ExtractableResponse<Response> authResponse = RestAssured.given().log().all()
-                .header(new Header("Authorization", invalidToken))
-                .when().get("/api/customers/me")
-                .then().log().all().extract();
+        ExtractableResponse<Response> authResponse =
+                SimpleRestAssured.get("/api/customers/me", new Header("Authorization", invalidToken));
 
         // then
         assertThat(authResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -3,7 +3,6 @@ package woowacourse.shoppingcart.acceptance;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import io.restassured.RestAssured;
 import io.restassured.http.Header;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
@@ -11,11 +10,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import woowacourse.auth.support.JwtTokenProvider;
-import woowacourse.shoppingcart.dto.CustomerRequest;
+import woowacourse.shoppingcart.acceptance.fixture.CustomerAcceptanceFixture;
 import woowacourse.shoppingcart.dto.CustomerResponse;
 import woowacourse.shoppingcart.dto.UpdateCustomerRequest;
+import woowacourse.support.SimpleRestAssured;
 
 @DisplayName("회원 관련 기능")
 public class CustomerAcceptanceTest extends AcceptanceTest {
@@ -26,51 +25,25 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원가입")
     @Test
     void addCustomer() {
-        CustomerRequest request = new CustomerRequest(
-                "username",
-                "password12!@",
-                "example@example.com",
-                "some-address",
-                "010-0000-0001"
-        );
-        ExtractableResponse<Response> extract = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when().post("/api/customers")
-                .then().log().all()
-                .extract();
+        ExtractableResponse<Response> saveResponse = CustomerAcceptanceFixture.saveCustomer();
 
         assertAll(
-                () -> assertThat(extract.header("Location")).startsWith("/api/customers"),
-                () -> assertThat(extract.statusCode()).isEqualTo(HttpStatus.CREATED.value())
+                () -> assertThat(saveResponse.header("Location")).startsWith("/api/customers"),
+                () -> assertThat(saveResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value())
         );
     }
 
     @DisplayName("내 정보 조회")
     @Test
     void getMe() {
-        CustomerRequest customerRequest = new CustomerRequest(
-                "username",
-                "password12!@",
-                "example@example.com",
-                "some-address",
-                "010-0000-0001"
-        );
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/api/customers")
-                .then().log().all();
+        CustomerAcceptanceFixture.saveCustomer();
 
         String token = "Bearer " + tokenProvider.createToken("username");
-        ExtractableResponse<Response> extractedResponse = RestAssured.given().log().all()
-                .header(new Header("Authorization", token))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/api/customers/me")
-                .then().log().all().extract();
-        CustomerResponse customerResponse = extractedResponse.jsonPath().getObject(".", CustomerResponse.class);
+
+        ExtractableResponse<Response> extractedResponse =
+                SimpleRestAssured.get("/api/customers/me", new Header("Authorization", token));
+        CustomerResponse customerResponse =
+                SimpleRestAssured.toObject(extractedResponse, CustomerResponse.class);
 
         assertThat(customerResponse.getName()).isEqualTo("username");
     }
@@ -78,35 +51,16 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("내 정보 수정")
     @Test
     void updateMe() {
-        CustomerRequest customerRequest = new CustomerRequest(
-                "username",
-                "password12!@",
-                "example@example.com",
-                "some-address",
-                "010-0000-0001"
-        );
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/api/customers")
-                .then().log().all();
+        CustomerAcceptanceFixture.saveCustomer();
 
         String token = "Bearer " + tokenProvider.createToken("username");
         UpdateCustomerRequest updateCustomerRequest = new UpdateCustomerRequest("another-address", "010-9999-9998");
-        ExtractableResponse<Response> updatedResponse = RestAssured.given().log().all()
-                .header(new Header("Authorization", token))
-                .body(updateCustomerRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().put("/api/customers/me")
-                .then().log().all().extract();
+        ExtractableResponse<Response> updatedResponse =
+                SimpleRestAssured.put("/api/customers/me", new Header("Authorization", token), updateCustomerRequest);
 
-        ExtractableResponse<Response> foundResponse = RestAssured.given().log().all()
-                .header(new Header("Authorization", token))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/api/customers/me")
-                .then().log().all().extract();
-        CustomerResponse customerResponse = foundResponse.jsonPath().getObject(".", CustomerResponse.class);
+        ExtractableResponse<Response> foundResponse =
+                SimpleRestAssured.get("/api/customers/me", new Header("Authorization", token));
+        CustomerResponse customerResponse = SimpleRestAssured.toObject(foundResponse, CustomerResponse.class);
 
         assertAll(
                 () -> assertThat(updatedResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value()),
@@ -118,32 +72,14 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
     @DisplayName("회원탈퇴")
     @Test
     void deleteMe() {
-        CustomerRequest customerRequest = new CustomerRequest(
-                "username",
-                "password12!@",
-                "example@example.com",
-                "some-address",
-                "010-0000-0001"
-        );
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/api/customers")
-                .then().log().all();
+        CustomerAcceptanceFixture.saveCustomer();
 
         String token = "Bearer " + tokenProvider.createToken("username");
-        ExtractableResponse<Response> deletedResponse = RestAssured.given().log().all()
-                .header(new Header("Authorization", token))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().delete("/api/customers/me")
-                .then().log().all().extract();
+        ExtractableResponse<Response> deletedResponse =
+                SimpleRestAssured.delete("/api/customers/me", new Header("Authorization", token));
 
-        ExtractableResponse<Response> foundResponse = RestAssured.given().log().all()
-                .header(new Header("Authorization", token))
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/api/customers/me")
-                .then().log().all().extract();
+        ExtractableResponse<Response> foundResponse =
+                SimpleRestAssured.get("/api/customers/me", new Header("Authorization", token));
 
         assertThat(deletedResponse.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
         assertThat(foundResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
