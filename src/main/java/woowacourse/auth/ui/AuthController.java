15e21d37c88429a84c498435f5d0e4fee58fe6cc
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-02 00:10:25 +0900

    feat(swagger): add @RestController at AuthController for swagger-ui
    - move SwaggerConfig to config package

--- src/main/java/woowacourse/auth/ui/AuthController.java
+++ src/main/java/woowacourse/auth/ui/AuthController.java
@@ -1,14 +1,14 @@
 package woowacourse.auth.ui;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 import woowacourse.auth.application.AuthService;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
 
-@Controller
+@RestController
 public class AuthController {
 
     private final AuthService authService;
