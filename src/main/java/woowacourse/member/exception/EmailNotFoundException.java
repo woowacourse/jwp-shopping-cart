bbc71ad26a1e89e67f3df6f8a50ed6de3a7f975e
Seongwon97 <osw970325@naver.com>
2022-06-01 19:07:29 +0900

    refactor(EmailNotFoundException): 사용하지 않는 예외 클래스 제거

--- src/main/java/woowacourse/member/exception/EmailNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package woowacourse.member.exception;
-
-public class EmailNotFoundException extends MemberException {
-
-    public EmailNotFoundException(String message) {
-        super(message);
-    }
-}
