04cc30f8f3ed6302f4dc31debcb49bf9df84b52d
hyunrrr <whgusrms96@gmail.com>
2022-06-01 14:14:17 +0900

    feat(Customer): add password validation

--- src/main/java/woowacourse/shoppingcart/domain/Customer.java
+++ src/main/java/woowacourse/shoppingcart/domain/Customer.java
@@ -1,6 +1,17 @@
 package woowacourse.shoppingcart.domain;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Customer {
+
+    private static final Pattern PASSWORD_REGEX = Pattern.compile("^[a-zA-Z\\d!@#$%^*]+$");
+    private static final int MIN_USERNAME_LENGTH = 4;
+    private static final int MAX_USERNAME_LENGTH = 20;
+    private static final int MIN_PASSWORD_LENGTH = 8;
+    private static final int MAX_PASSWORD_LENGTH = 20;
+    private static final int MIN_NICKNAME_LENGTH = 8;
+    private static final int MAX_NICKNAME_LENGTH = 20;
 
     private final String username;
     private final String password;
@@ -8,34 +19,48 @@ public class Customer {
     private final int age;
 
     public Customer(String username, String password, String nickname, int age) {
-        validate(username, nickname, age);
+        validate(username, password, nickname, age);
         this.username = username;
         this.password = password;
         this.nickname = nickname;
         this.age = age;
     }
 
-    private void validate(String username, String nickname, int age) {
+    private void validate(String username, String password, String nickname, int age) {
         validateUsername(username);
+        validatePassword(password);
         validateNickname(nickname);
         validateAge(age);
     }
 
     private void validateUsername(String username) {
-        if (username.length() < 4 || username.length() > 20) {
-            throw new IllegalArgumentException("아이디는 최소 4글자, 최대 20글자여야 합니다.");
+        validateLength(username, MIN_USERNAME_LENGTH, MAX_USERNAME_LENGTH);
+    }
+
+    private void validatePassword(String password) {
+        validateLength(password, MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH);
+        Matcher matcher = PASSWORD_REGEX.matcher(password);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("비밀번호는 영문 대소문자, 숫자, 특수문자(!@#$%^*) 조합이여야 합니다.");
         }
     }
 
     private void validateNickname(String nickname) {
-        if (nickname.isEmpty() || nickname.length() > 10) {
-            throw new IllegalArgumentException("닉네임은 최소 1글자, 최대 10글자여야 합니다.");
-        }
+        validateLength(nickname, MIN_NICKNAME_LENGTH, MAX_NICKNAME_LENGTH);
     }
 
     private void validateAge(int age) {
         if (age < 1) {
             throw new IllegalArgumentException("나이는 최소 1살이어야 합니다.");
+        }
+    }
+
+    private void validateLength(String target, int min, int max) {
+        if (min != 0 && target.isBlank()) {
+            throw new IllegalArgumentException("공백은 허용되지 않습니다.");
+        }
+        if (target.length() < min || target.length() > max) {
+            throw new IllegalArgumentException("길이는 " + min + "이상, " + max + "이하여야 합니다.");
         }
     }
 
