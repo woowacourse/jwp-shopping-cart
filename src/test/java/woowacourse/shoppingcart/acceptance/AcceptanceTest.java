b2c356f50835b87bfbf478a31cdbc63a414b88d6
tonic523 <diddmlvkf@gmail.com>
2022-06-01 15:28:57 +0900

    refactor: `@DirtiesContext` 제거

--- src/test/java/woowacourse/shoppingcart/acceptance/AcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/AcceptanceTest.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@Sql("/init.sql")
 @ActiveProfiles("test")
 public class AcceptanceTest {
     @LocalServerPort
