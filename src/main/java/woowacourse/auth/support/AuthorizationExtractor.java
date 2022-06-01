c3c98fab38881acb6358fbf699517ffcbe69ede6
leokim <kunga077@gmail.com>
2022-06-02 00:00:37 +0900

    refactor: AuthorizationExtractor 불필요한 메서드 제거

--- src/main/java/woowacourse/auth/support/AuthorizationExtractor.java
+++ src/main/java/woowacourse/auth/support/AuthorizationExtractor.java
@@ -11,7 +11,6 @@ public class AuthorizationExtractor {
     private static final String BEARER_TYPE = "Bearer";
     private static final String ACCESS_TOKEN_TYPE = AuthorizationExtractor.class.getSimpleName() + ".ACCESS_TOKEN_TYPE";
     private static final int HEADER_VALUE_BEGIN_INDEX = 0;
-    private static final int NOT_EXIST_COMMA_SIZE = 0;
 
     public static String extract(HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaders(AUTHORIZATION);
@@ -20,10 +19,6 @@ public class AuthorizationExtractor {
             if ((value.toLowerCase().startsWith(BEARER_TYPE.toLowerCase()))) {
                 String authHeaderValue = value.substring(BEARER_TYPE.length()).trim();
                 request.setAttribute(ACCESS_TOKEN_TYPE, value.substring(HEADER_VALUE_BEGIN_INDEX, BEARER_TYPE.length()).trim());
-                int commaIndex = authHeaderValue.indexOf(',');
-                if (commaIndex > NOT_EXIST_COMMA_SIZE) {
-                    authHeaderValue = authHeaderValue.substring(HEADER_VALUE_BEGIN_INDEX, commaIndex);
-                }
                 return authHeaderValue;
             }
         }
