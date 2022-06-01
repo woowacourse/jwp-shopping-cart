e68793c51fe2645773cb6abaac215a1cce9e46aa
yxxnghwan <younghwan960@gmail.com>
2022-06-01 20:29:22 +0900

    refactor: AuthenticationPrincipalArgumentResolver 2중으로 등록하던 것 수정

--- src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
+++ src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
@@ -3,6 +3,7 @@ package woowacourse.auth.config;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import woowacourse.auth.application.AuthService;
 import woowacourse.auth.ui.AuthenticationPrincipalArgumentResolver;
@@ -16,7 +17,7 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addArgumentResolvers(List argumentResolvers) {
+    public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(createAuthenticationPrincipalArgumentResolver());
     }
 
