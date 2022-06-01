ed93be69b9a8b9657579e291b8d9fed2d6b97fff
Hyunta <mohai2618@gmail.com>
2022-06-01 11:15:28 +0900

    refactor: AuthenticationPrincipalConfig 인터셉터 주입받는 방식 변경

--- src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
+++ src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
@@ -1,40 +1,35 @@
 package woowacourse.auth.config;
 
 import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import woowacourse.auth.application.AuthService;
+import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.auth.ui.AuthenticationPrincipalArgumentResolver;
 import woowacourse.auth.ui.LoginInterceptor;
 
 @Configuration
 public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     private final AuthService authService;
-    private final LoginInterceptor loginInterceptor;
+    private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthenticationPrincipalConfig(AuthService authService, LoginInterceptor loginInterceptor) {
+    public AuthenticationPrincipalConfig(AuthService authService, JwtTokenProvider jwtTokenProvider) {
         this.authService = authService;
-        this.loginInterceptor = loginInterceptor;
+        this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(createAuthenticationPrincipalArgumentResolver());
+        resolvers.add(new AuthenticationPrincipalArgumentResolver(authService));
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
+        registry.addInterceptor(new LoginInterceptor(jwtTokenProvider))
                 .order(1)
                 .addPathPatterns("/customers/me")
                 .excludePathPatterns("/css/**", "/*.ico", "/error");
-    }
-
-    @Bean
-    public AuthenticationPrincipalArgumentResolver createAuthenticationPrincipalArgumentResolver() {
-        return new AuthenticationPrincipalArgumentResolver(authService);
     }
 }
