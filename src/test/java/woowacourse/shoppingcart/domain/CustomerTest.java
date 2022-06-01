945e73302e87b428cc703ebf322882c1543dfe34
hyunrrr <whgusrms96@gmail.com>
2022-06-01 15:46:47 +0900

    test(customer): amend test
    - delete unrelated test
    - add hasSamePassword test

--- src/test/java/woowacourse/shoppingcart/domain/CustomerTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/CustomerTest.java
@@ -1,58 +1,24 @@
 package woowacourse.shoppingcart.domain;
 
-import static org.assertj.core.api.Assertions.assertThatNoException;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @SuppressWarnings("NonAsciiCharacters")
 class CustomerTest {
 
-    @DisplayName("초기화 테스트")
-    @Nested
-    class InitTest {
+    @DisplayName("비밀번호 같으면 true 반환")
+    @Test
+    void hasSamePasswordTest() {
+        Username username = new Username("유효한_아이디");
+        Password password = new Password("validPassword");
+        Nickname nickname = new Nickname("닉네임");
+        Age age = new Age(10);
+        Customer customer = new Customer(username, password, nickname, age);
 
-        @Test
-        void 값이_유효한_경우_성공() {
-            Username username = new Username("유효한_아이디");
-            Password password = new Password("비밀번호");
-            Nickname nickname = new Nickname("닉네임");
-            Age age = new Age(10);
-            assertThatNoException()
-                    .isThrownBy(() -> new Customer(username, password, nickname, age));
-        }
-        
-        @Test
-        void 닉네임이_0글자인_경우_예외발생() {
-            Username username = new Username("유효한_아이디");
-            Password password = new Password("비밀번호");
-            Nickname nickname = new Nickname("");
-            Age age = new Age(10);
-            assertThatThrownBy(() -> new Customer(username, password, nickname, age))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+        boolean result = customer.hasSamePassword(password);
 
-        @Test
-        void 닉네임이_10글자_초과인_경우_예외발생() {
-            Username username = new Username("유효한_아이디");
-            Password password = new Password("비밀번호");
-            Nickname nickname = new Nickname("12345678901");
-            Age age = new Age(10);
-            String tooLongNickname = "12345678901";
-            assertThatThrownBy(() -> new Customer(username, password, nickname, age))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
-
-        @Test
-        void 나이가_0이하인_경우_예외발생() {
-            Username username = new Username("유효한_아이디");
-            Password password = new Password("비밀번호");
-            Nickname nickname = new Nickname("닉네임");
-            Age age = new Age(0);
-            assertThatThrownBy(() -> new Customer(username, password, nickname, age))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+        assertThat(result).isTrue();
     }
 }
