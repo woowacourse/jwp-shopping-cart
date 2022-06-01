ad7892cdb75251c023bbc7a3ead0d3ce9574e569
Juhyung990122 <harukawa99@naver.com>
2022-06-01 21:12:57 +0900

    refactor: 개행 컨벤션 적용

--- src/main/java/woowacourse/shoppingcart/exception/DuplicateNameException.java
+++ src/main/java/woowacourse/shoppingcart/exception/DuplicateNameException.java
@@ -3,6 +3,7 @@ package woowacourse.shoppingcart.exception;
 import org.springframework.http.HttpStatus;
 
 public class DuplicateNameException extends CartException {
+
     public DuplicateNameException(final String message) {
         super(message, HttpStatus.BAD_REQUEST);
     }
