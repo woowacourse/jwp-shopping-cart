73c3aaaa6d998425054ed4b4ff855d0d1bb14e12
hyeonic <dev.hyeonic@gmail.com>
2022-06-01 14:28:29 +0900

    feat: CustomerService Transactional 애노테이션 추가

--- src/main/java/woowacourse/shoppingcart/application/CustomerService.java
+++ src/main/java/woowacourse/shoppingcart/application/CustomerService.java
@@ -1,6 +1,7 @@
 package woowacourse.shoppingcart.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import woowacourse.shoppingcart.dao.CustomerDao;
 import woowacourse.shoppingcart.domain.Customer;
 import woowacourse.shoppingcart.dto.customer.CustomerResponse;
@@ -10,6 +11,7 @@ import woowacourse.shoppingcart.dto.customer.LoginCustomer;
 import woowacourse.shoppingcart.exception.InvalidCustomerException;
 
 @Service
+@Transactional(readOnly = true)
 public class CustomerService {
 
     private final CustomerDao customerDao;
@@ -18,26 +20,25 @@ public class CustomerService {
         this.customerDao = customerDao;
     }
 
+    @Transactional
     public CustomerResponse save(CustomerSaveRequest customerSaveRequest) {
         Customer customer = customerDao.save(customerSaveRequest.toCustomer());
-
         return new CustomerResponse(customer);
     }
 
     public CustomerResponse find(LoginCustomer loginCustomer) {
         Customer customer = getCustomer(loginCustomer.getUsername());
-
         return new CustomerResponse(customer);
     }
 
+    @Transactional
     public void update(LoginCustomer loginCustomer, CustomerUpdateRequest customerUpdateRequest) {
         Customer customer = getCustomer(loginCustomer.getUsername());
-
         customer.modify(customerUpdateRequest.getAddress(), customerUpdateRequest.getPhoneNumber());
-
         customerDao.update(customer);
     }
 
+    @Transactional
     public void delete(LoginCustomer loginCustomer) {
         Customer customer = getCustomer(loginCustomer.getUsername());
         customerDao.delete(customer);
