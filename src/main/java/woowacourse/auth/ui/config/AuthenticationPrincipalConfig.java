1c07ef0db2b9cebc6806800410fab7b91b5a2e20
injoon2019 <ison@sfu.ca>
2022-06-02 20:54:33 +0900

    feat: cors 관련 설정 추가

--- src/main/java/woowacourse/auth/ui/config/AuthenticationPrincipalConfig.java
+++ src/main/java/woowacourse/auth/ui/config/AuthenticationPrincipalConfig.java
@@ -3,7 +3,9 @@ package woowacourse.auth.ui.config;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import woowacourse.auth.application.CustomerService;
@@ -11,6 +13,8 @@ import woowacourse.auth.support.JwtTokenProvider;
 
 @Configuration
 public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
+
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     private final JwtTokenProvider jwtTokenProvider;
     private final CustomerService customerService;
@@ -39,5 +43,16 @@ public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
     @Bean
     public AuthenticationPrincipalArgumentResolver authenticationPrincipalArgumentResolver() {
         return new AuthenticationPrincipalArgumentResolver(jwtTokenProvider, customerService);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/customers/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION);
+
+        registry.addMapping("/auth/**")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
