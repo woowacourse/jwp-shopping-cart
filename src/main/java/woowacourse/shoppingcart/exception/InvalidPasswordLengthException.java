3a5cd9eed8ef327d12ad09f414238b296beabbf7
yangdongjue5510 <djwhy5510@gmail.com>
2022-06-02 11:40:12 +0900

    feat(Password): 비밀번호 길이 검증 기능 구현

---
+++ src/main/java/woowacourse/shoppingcart/exception/InvalidPasswordLengthException.java
@@ -0,0 +1,10 @@
+package woowacourse.shoppingcart.exception;
+
+public class InvalidPasswordLengthException extends RuntimeException {
+
+    private static final String MESSAGE = "비밀번호는 8자리 이상이어야 합니다.";
+
+    public InvalidPasswordLengthException() {
+        super(MESSAGE);
+    }
+}
