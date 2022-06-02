95966b6fbb78ac5dc4ead3296d9387189fe20321
jayjaehunchoi <wogns0108@naver.com>
2022-06-02 12:47:59 +0900

    refactor: interceptor options 허용

--- src/main/java/woowacourse/auth/support/LoginInterceptor.java
+++ src/main/java/woowacourse/auth/support/LoginInterceptor.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import woowacourse.auth.exception.WrongTokenException;
 
@@ -21,11 +22,18 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+        if(isPreflight(request)) {
+            return true;
+        }
         logger.info("request uri = {}, request method = {} ", request.getRequestURI(), request.getMethod());
         String token = AuthorizationExtractor.extract(request);
         if (!jwtTokenProvider.validateToken(token)) {
             throw new WrongTokenException();
         }
         return true;
+    }
+
+    private boolean isPreflight(HttpServletRequest request) {
+        return request.getMethod().equals(HttpMethod.OPTIONS.name());
     }
 }
