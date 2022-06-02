c2902be09cde9cd2d2b51fef0dda9e4ba4b5c676
dongho <dongho108@naver.com>
2022-06-02 13:10:50 +0900

    refactor: cors 매핑 변경

--- src/main/java/woowacourse/shoppingcart/config/WebConfig.java
+++ src/main/java/woowacourse/shoppingcart/config/WebConfig.java
@@ -1,6 +1,7 @@
 package woowacourse.shoppingcart.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedMethods("*")
-                .allowedOriginPatterns("*");
+            .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+            .exposedHeaders(HttpHeaders.LOCATION);
     }
 }
