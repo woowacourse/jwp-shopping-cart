10f281d9795d9d31aef6adb447bd7c12d26324fe
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-02 15:12:09 +0900

    feat(Token): catch and throw exceptions on extracting payload
    - add all test cases

--- src/main/java/woowacourse/auth/domain/Token.java
+++ src/main/java/woowacourse/auth/domain/Token.java
@@ -1,9 +1,9 @@
 package woowacourse.auth.domain;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.DecodingException;
-import java.util.Date;
 import javax.crypto.SecretKey;
 import woowacourse.auth.exception.AuthenticationException;
 import woowacourse.auth.exception.ForbiddenException;
@@ -18,11 +18,11 @@ public class Token {
 
     public String getPayload(SecretKey tokenKey) {
         try {
-            Claims claims = extractClaims(tokenKey);
-            validateExpiration(claims);
-            return claims.getSubject();
-        } catch (DecodingException e) {
-            throw new ForbiddenException("권한이 없습니다.");
+            return extractClaims(tokenKey).getSubject();
+        } catch (ExpiredJwtException e) {
+            throw new AuthenticationException("다시 로그인해주세요.");
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new ForbiddenException("로그인이 필요합니다.");
         }
     }
 
@@ -32,13 +32,6 @@ public class Token {
                 .build()
                 .parseClaimsJws(value)
                 .getBody();
-    }
-
-    private void validateExpiration(Claims claims) {
-        Date now = new Date();
-        if (claims.getExpiration().before(now)) {
-            throw new AuthenticationException("다시 로그인해주세요.");
-        }
     }
 
     public String getValue() {
