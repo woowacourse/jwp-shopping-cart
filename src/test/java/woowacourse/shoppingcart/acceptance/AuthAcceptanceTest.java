53be97173252f3940db7baafe24137a3fd7d5476
jojogreen91 <iawbg13@gmail.com>
2022-06-02 16:51:23 +0900

    refactor: import optimize 적용

--- src/test/java/woowacourse/shoppingcart/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/AuthAcceptanceTest.java
@@ -1,4 +1,4 @@
-package woowacourse.shoppingcart.acceptance.auth;
+package woowacourse.shoppingcart.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -18,7 +18,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.jdbc.Sql;
-import woowacourse.shoppingcart.acceptance.AcceptanceTest;
 import woowacourse.shoppingcart.dto.CustomerLoginRequest;
 import woowacourse.shoppingcart.dto.CustomerLoginResponse;
 import woowacourse.shoppingcart.dto.CustomerRequest;
