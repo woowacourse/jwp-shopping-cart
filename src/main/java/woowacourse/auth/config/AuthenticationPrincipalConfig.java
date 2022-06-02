619904b7d3b68588ba79a4bec0fe9ccc0f172f54
EastHShin <xldk78@gmail.com>
2022-06-02 12:04:42 +0900

    refactor: jjwt 의존성 업그레이드에 따른 코드 변경

--- src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
+++ src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
@@ -1,13 +1,12 @@
 package woowacourse.auth.config;
 
-import woowacourse.auth.support.JwtTokenProvider;
-import woowacourse.auth.ui.AuthenticationPrincipalArgumentResolver;
-import woowacourse.auth.application.AuthService;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
+import woowacourse.auth.application.AuthService;
+import woowacourse.auth.support.JwtTokenProvider;
+import woowacourse.auth.ui.AuthenticationPrincipalArgumentResolver;
 
 @Configuration
 public class AuthenticationPrincipalConfig implements WebMvcConfigurer {
