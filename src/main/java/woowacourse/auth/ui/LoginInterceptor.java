82c291bdaccf6d29356c94b1ca431c40d1010f5f
dongho <dongho108@naver.com>
2022-06-02 14:12:02 +0900

    refactor: Option 요청은 모두 true를 반환하도록 설정

--- src/main/java/woowacourse/auth/ui/LoginInterceptor.java
+++ src/main/java/woowacourse/auth/ui/LoginInterceptor.java
@@ -3,6 +3,7 @@ package woowacourse.auth.ui;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import woowacourse.auth.support.AuthorizationExtractor;
@@ -20,10 +21,18 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         String token = AuthorizationExtractor.extract(request);
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            setUsernameAndReturn(request, token);
+        }
+
         if (!jwtTokenProvider.validateToken(token) || token == null) {
             throw new AuthorizationException();
         }
 
+        return setUsernameAndReturn(request, token);
+    }
+
+    private boolean setUsernameAndReturn(HttpServletRequest request, String token) {
         request.setAttribute("username", jwtTokenProvider.getPayload(token));
         return true;
     }
