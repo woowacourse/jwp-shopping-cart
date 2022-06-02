884256093db12cf1c913c7eabc78991ae6fb2cc9
jurlring <78091011+jurlring@users.noreply.github.com>
2022-06-02 16:36:57 +0900

    test: 상태코드 변경 및 dto 생성으로 인한 test 수정

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -3,12 +3,11 @@ package woowacourse.auth.acceptance;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import io.restassured.RestAssured;
-import io.restassured.response.ExtractableResponse;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import woowacourse.auth.dto.ExceptionResponse;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
 import woowacourse.shoppingcart.acceptance.AcceptanceTest;
@@ -59,16 +58,16 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     @Test
     void myInfoWithBadBearerAuth() {
         // when
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+        ExceptionResponse response = RestAssured.given().log().all()
                 .body(new TokenRequest("email", "Pw123456!"))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .post("/customers/login")
                 .then().log().all()
-                .extract();
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract().as(ExceptionResponse.class);
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat(response.body().asString()).isEqualTo("Email 또는 Password가 일치하지 않습니다.");
+        assertThat(response.getMessage()).isEqualTo("Email 또는 Password가 일치하지 않습니다.");
     }
 
     @DisplayName("Bearer Auth 유효하지 않은 토큰")
@@ -76,15 +75,15 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     void myInfoWithWrongBearerAuth() {
         // when
         String accessToken = "aaaaaaaaa";
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+        ExceptionResponse response = RestAssured.given().log().all()
                 .auth().oauth2(accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .get("/customers")
                 .then().log().all()
-                .extract();
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .extract().as(ExceptionResponse.class);
         // then
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat(response.body().asString()).isEqualTo("유효하지 않거나 만료된 토큰입니다.");
+        assertThat(response.getMessage()).isEqualTo("유효하지 않거나 만료된 토큰입니다.");
     }
 }
--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import woowacourse.auth.dto.ExceptionResponse;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
 import woowacourse.shoppingcart.dto.CustomerRequest;
@@ -37,11 +38,6 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(customerResponse).extracting("email", "name", "phone", "address")
                 .containsExactly("email", "name", "010-1234-5678", "address");
-    }
-
-    @DisplayName("내 정보 조회")
-    @Test
-    void getMe() {
     }
 
     @DisplayName("내 정보 수정")
@@ -119,15 +115,15 @@ public class CustomerAcceptanceTest extends AcceptanceTest {
                 .statusCode(HttpStatus.NO_CONTENT.value())
                 .extract();
         //then
-        ExtractableResponse<Response> response = RestAssured.given().log().all()
+        ExceptionResponse response = RestAssured.given().log().all()
                 .auth().oauth2(accessToken)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
                 .get("/customers")
                 .then().log().all()
-                .extract();
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .extract().as(ExceptionResponse.class);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
-        assertThat(response.body().asString()).isEqualTo("존재하지 않는 유저입니다.");
+        assertThat(response.getMessage()).isEqualTo("존재하지 않는 유저입니다.");
     }
 }
