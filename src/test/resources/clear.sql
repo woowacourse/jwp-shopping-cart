b486ce891cb3fee25f1d4cda875f396d8f4e77e3
progress0407 <progress0407@gmail.com>
2022-06-02 12:58:17 +0900

    refactor(application): properties 에서 yml 파일로 변경

--- src/test/resources/clear.sql
+++ src/test/resources/clear.sql
@@ -1,19 +1,19 @@
 SET foreign_key_checks = 0;
 
 truncate table orders_detail;
---alter table orders_detail alter column id restart with 1;
+alter table orders_detail alter column id restart with 1;
 
 truncate table orders;
---alter table orders alter column id restart with 1;
+alter table orders alter column id restart with 1;
 
 truncate table cart_item;
---alter table cart_item alter column id restart with 1;
+alter table cart_item alter column id restart with 1;
 
 truncate table product;
---alter table product alter column id restart with 1;
+alter table product alter column id restart with 1;
 
 truncate table customer;
---alter table customer alter column id restart with 1;
+alter table customer alter column id restart with 1;
 
 insert into customer (email, username, password)
 values ('puterism@naver.com', 'puterism', '12349053145'),
