0473b7e9110ee960bf1fdb6ec5aa4491c8f53d96
Chris <bjuuuu98@gmail.com>
2022-06-02 14:22:30 +0900

    refactor: 불필요한 생성자 삭제

--- src/main/java/woowacourse/auth/exception/AuthorizationException.java
+++ src/main/java/woowacourse/auth/exception/AuthorizationException.java
@@ -2,9 +2,6 @@ package woowacourse.auth.exception;
 
 public class AuthorizationException extends RuntimeException {
 
-    public AuthorizationException() {
-    }
-
     public AuthorizationException(String message) {
         super(message);
     }
