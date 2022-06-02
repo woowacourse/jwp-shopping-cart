41479600a6b6ff369567fe3b4fe6871d5da41fa5
hyeonbinsa <shb1833@gmail.com>
2022-06-02 14:37:56 +0900

    refactor: AuthAcceptanceTest 코드 리팩터링

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -3,13 +3,11 @@ package woowacourse.auth.acceptance;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import woowacourse.auth.dto.CustomerRequest;
 import woowacourse.auth.dto.CustomerResponse;
 import woowacourse.auth.dto.TokenRequest;
@@ -25,70 +23,55 @@ public class AuthAcceptanceTest extends AcceptanceTest {
     private static final String PHONE = "010-1234-5678";
     private static final String ADDRESS = "Seoul";
 
-    @DisplayName("회원 가입 성공")
+    @DisplayName("회원 가입 성공하면 상태코드 201과 저장된 회원 정보를 반환한다.")
     @Test
     void registerCustomers() {
         // when
-        // 회원 정보를 담은 요청이 오면
         CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
-        ExtractableResponse<Response> registerCustomerResponse = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/customers")
-                .then().log().all()
-                .extract();
-
+        ExtractableResponse<Response> registerCustomerResponse = postWithBody("/customers", customerRequest);
         // then
-        // 회원가입에 성공하고 상태코드 201과 회원정보를 반환한다.
         CustomerResponse customerResponse = registerCustomerResponse.body().as(CustomerResponse.class);
+
         assertAll(
+                () -> assertThat(registerCustomerResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value()),
                 () -> assertThat(customerResponse.getId()).isEqualTo(1L),
                 () -> assertThat(customerResponse.getEmail()).isEqualTo(EMAIL),
                 () -> assertThat(customerResponse.getName()).isEqualTo(NAME),
                 () -> assertThat(customerResponse.getPhone()).isEqualTo(PHONE),
                 () -> assertThat(customerResponse.getAddress()).isEqualTo(ADDRESS)
         );
-        assertThat(registerCustomerResponse.statusCode()).isEqualTo(HttpStatus.CREATED.value());
     }
 
-    @DisplayName("Bearer Auth 로그인 성공")
+    @DisplayName("회원 가입 전 이메일 검증 시 중복된 이메일으로 검증 요청을 보낸다면 상태코드 200과 false를 반환한다.")
+    @Test
+    void validateDuplicateEmail() {
+        // when
+        CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
+        ExtractableResponse<Response> registerCustomerResponse = postWithBody("/customers", customerRequest);
+        CustomerResponse customerResponse = registerCustomerResponse.body().as(CustomerResponse.class);
+        // then
+        ExtractableResponse<Response> response = postWithBody("/customers/email", customerRequest.getEmail());
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(200),
+                () -> assertThat(response.body().asString()).isEqualTo("false")
+        );
+    }
+
+    @DisplayName("Bearer Auth : 회원 로그인에 성공하면 Token을 통해 회원 정보를 조회할 수 있다.")
     @Test
     void myInfoWithBearerAuth() {
         // given
-        // 회원이 등록되어 있고
-        // id, password를 사용해 토큰을 발급받고
         CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/customers")
-                .then().log().all()
-                .extract();
+        postWithBody("/customers", customerRequest);
 
         TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
-        String accessToken = RestAssured
-                .given().log().all()
-                .body(tokenRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/customers/login")
-                .then().log().all()
-                .extract().as(TokenResponse.class).getAccessToken();
-
+        String accessToken = postWithBody("/customers/login", tokenRequest)
+                .as(TokenResponse.class)
+                .getAccessToken();
         // when
-        // 발급 받은 토큰을 사용하여 내 정보 조회를 요청하면
-        CustomerResponse customerResponse = RestAssured
-                .given().log().all()
-                .auth().oauth2(accessToken)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/customers")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value()).extract().as(CustomerResponse.class);
+        CustomerResponse customerResponse = getByToken("/customers", accessToken).as(CustomerResponse.class);
 
         // then
-        // 내 정보가 조회된다
         assertAll(
                 () -> assertThat(customerResponse.getId()).isEqualTo(1L),
                 () -> assertThat(customerResponse.getEmail()).isEqualTo(EMAIL),
@@ -98,52 +81,20 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         );
     }
 
-    @DisplayName("회원 정보 수정")
+    @DisplayName("토큰을 활용해 회원 정보 수정을 요청하면 수정된 회원 정보를 조회할 수 있다.")
     @Test
     void editCustomerInformation() {
         // given
-        // 회원이 등록되어 있고
         CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/customers")
-                .then().log().all()
-                .extract();
+        postWithBody("/customers", customerRequest);
 
         TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
-        String accessToken = RestAssured
-                .given().log().all()
-                .body(tokenRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/customers/login")
-                .then().log().all()
-                .extract().as(TokenResponse.class).getAccessToken();
-
+        String accessToken = postWithBody("/customers/login", tokenRequest).as(TokenResponse.class).getAccessToken();
         // when
-        // 회원 정보를 수정하고 내 정보 조회를 요청하면
         CustomerRequest updateCustomerRequest = new CustomerRequest(EMAIL, PASSWORD, "bani", PHONE, ADDRESS);
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().oauth2(accessToken)
-                .body(updateCustomerRequest)
-                .when().put("/customers")
-                .then().log().all()
-                .extract();
-
-        CustomerResponse customerResponse = RestAssured
-                .given().log().all()
-                .auth().oauth2(accessToken)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/customers")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value()).extract().as(CustomerResponse.class);
-
+        putByTokenWithBody("/customers", accessToken, updateCustomerRequest);
         // then
-        // 수정된 내 정보가 조회된다.
+        CustomerResponse customerResponse = getByToken("/customers", accessToken).as(CustomerResponse.class);
         assertAll(
                 () -> assertThat(customerResponse.getId()).isEqualTo(1L),
                 () -> assertThat(customerResponse.getEmail()).isEqualTo(EMAIL),
@@ -153,144 +104,68 @@ public class AuthAcceptanceTest extends AcceptanceTest {
         );
     }
 
-    @DisplayName("회원 정보 삭제")
+    @DisplayName("토큰을 활용해 회원 정보 삭제를 요청하면 상태 코드 204를 반환한다.")
     @Test
     void deleteCustomer() {
         // given
-        // 회원이 등록되어 있고
         CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/customers")
-                .then().log().all()
-                .extract();
+        postWithBody("/customers", customerRequest);
 
         TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
-        String accessToken = RestAssured
-                .given().log().all()
-                .body(tokenRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/customers/login")
-                .then().log().all()
-                .extract().as(TokenResponse.class).getAccessToken();
-
+        String accessToken = postWithBody("/customers/login", tokenRequest).as(TokenResponse.class).getAccessToken();
         // when
-        // 패스워드와 함께 회원 삭제를 요청하면
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .auth().oauth2(accessToken)
-                .when().delete("/customers")
-                .then().log().all()
-                .extract();
-
+        ExtractableResponse<Response> response = deleteWithToken("/customers", accessToken);
         // then
-        // 204 상태코드를 반환한다.
         assertThat(response.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
-    @DisplayName("Bearer Auth 로그인 실패 - 이메일 불 일치")
+    @DisplayName("Bearer Auth 로그인 : 일치하지 않는 이메일을 통해 로그인 시도를 하면 토큰 발급 요청이 거부되며 상태 코드 400이 반환된다.")
     @Test
-    void myInfoWithBadBearerAuthInvalidEmail() {
+    void loginCustomer_WhenInvalidEmail() {
         // given
-        // 회원이 등록되어 있고
         CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/customers")
-                .then().log().all()
-                .extract();
-
+        postWithBody("/customers", customerRequest);
         // when
-        // 잘못된 id, password를 사용해 토큰을 요청하면
         TokenRequest tokenRequest = new TokenRequest("test@test.com", PASSWORD);
-        ExtractableResponse<Response> loginResponse = RestAssured
-                .given().log().all()
-                .body(tokenRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/customers/login")
-                .then().log().all()
-                .extract();
-
+        ExtractableResponse<Response> loginResponse = postWithBody("/customers/login", tokenRequest);
         // then
-        // 토큰 발급 요청이 거부된다
-        assertAll(() -> assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-                () -> assertThat(loginResponse.jsonPath().getString("message")).isEqualTo("존재하지 않는 이메일 입니다."));
+        assertAll(
+                () -> assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(loginResponse.jsonPath().getString("message")).isEqualTo("존재하지 않는 이메일 입니다.")
+        );
     }
 
-    @DisplayName("Bearer Auth 로그인 실패 - 비밀번호 불 일치")
+    @DisplayName("Bearer Auth 로그인 : 일치하지 않는 비밀번호를 통해 로그인 시도를 하면 토큰 발급 요청이 거부되며 상태 코드 400이 반환된다.\"")
     @Test
-    void myInfoWithBadBearerAuthInvalidPassword() {
+    void loginCustomer_WhenInvalidPassword() {
         // given
-        // 회원이 등록되어 있고
         CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/customers")
-                .then().log().all()
-                .extract();
-
+        postWithBody("/customers", customerRequest);
         // when
-        // 잘못된 id, password를 사용해 토큰을 요청하면
         TokenRequest tokenRequest = new TokenRequest(EMAIL, "Bunny1234!");
-        ExtractableResponse<Response> loginResponse = RestAssured
-                .given().log().all()
-                .body(tokenRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/customers/login")
-                .then().log().all()
-                .extract();
-
+        ExtractableResponse<Response> loginResponse = postWithBody("/customers/login", tokenRequest);
         // then
-        // 토큰 발급 요청이 거부된다
-        assertAll(() -> assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
-                () -> assertThat(loginResponse.jsonPath().getString("message")).isEqualTo("비밀번호가 일치하지 않습니다."));
+        assertAll(
+                () -> assertThat(loginResponse.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value()),
+                () -> assertThat(loginResponse.jsonPath().getString("message")).isEqualTo("비밀번호가 일치하지 않습니다.")
+        );
     }
 
-    @DisplayName("Bearer Auth 유효하지 않은 토큰")
+    @DisplayName("Bearer Auth : 유효하지 않은 토큰을 통해 정보 조회를 시도하면 정보 조회가 거부되며 상태 코드 401을 반환한다.")
     @Test
-    void myInfoWithWrongBearerAuth() {
-        // when
-        // 유효하지 않은 토큰을 사용하여 내 정보 조회를 요청하면
+    void findCustomerByToken_WhenIllegalToken() {
+        // given
         CustomerRequest customerRequest = new CustomerRequest(EMAIL, PASSWORD, NAME, PHONE, ADDRESS);
-        RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(customerRequest)
-                .when().post("/customers")
-                .then().log().all()
-                .extract();
+        postWithBody("/customers", customerRequest);
 
         TokenRequest tokenRequest = new TokenRequest(EMAIL, PASSWORD);
-        String accessToken = RestAssured
-                .given().log().all()
-                .body(tokenRequest)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().post("/customers/login")
-                .then().log().all()
-                .extract().as(TokenResponse.class).getAccessToken();
-
-        ExtractableResponse<Response> customerResponse = RestAssured
-                .given().log().all()
-                .auth().oauth2(accessToken + "l")
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/customers")
-                .then().log().all()
-                .extract();
-
+        String accessToken = postWithBody("/customers/login", tokenRequest).as(TokenResponse.class).getAccessToken();
+        // when
+        ExtractableResponse<Response> customerResponse = getByToken("/customers", accessToken + "l");
         // then
-        // 내 정보 조회 요청이 거부된다
-        assertAll(() -> assertThat(customerResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value()),
-                () -> assertThat(customerResponse.jsonPath().getString("message")).isEqualTo("유효하지 않은 토큰입니다."));
+        assertAll(
+                () -> assertThat(customerResponse.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value()),
+                () -> assertThat(customerResponse.jsonPath().getString("message")).isEqualTo("유효하지 않은 토큰입니다.")
+        );
     }
 }
--- src/test/java/woowacourse/shoppingcart/acceptance/AcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/AcceptanceTest.java
@@ -1,9 +1,12 @@
 package woowacourse.shoppingcart.acceptance;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -11,11 +14,53 @@ import org.springframework.test.context.ActiveProfiles;
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 @ActiveProfiles("test")
 public class AcceptanceTest {
+
     @LocalServerPort
     int port;
 
     @BeforeEach
     public void setUp() {
         RestAssured.port = port;
+    }
+
+    public ExtractableResponse<Response> postWithBody(String uri, Object body) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(body)
+                .when().post(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> getByToken(String uri, String accessToken) {
+        return RestAssured
+                .given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> putByTokenWithBody(String uri, String accessToken, Object body) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(accessToken)
+                .body(body)
+                .when().put(uri)
+                .then().log().all()
+                .extract();
+    }
+
+    public ExtractableResponse<Response> deleteWithToken(String uri, String accessToken) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .auth().oauth2(accessToken)
+                .when().delete(uri)
+                .then().log().all()
+                .extract();
     }
 }
