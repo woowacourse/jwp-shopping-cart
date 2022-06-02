fc6e3989a1c5bee673964b5f6a40c100d0ff60b8
progress0407 <progress0407@gmail.com>
2022-06-02 18:36:08 +0900

    feat(ControllerAdvice): `ResponseEntityExceptionHandler`를 상속하여 스프링의 예외처리 지원을 받게 수정

--- src/main/java/woowacourse/shoppingcart/ui/ApplicationExceptionHandler.java
+++ src/main/java/woowacourse/shoppingcart/ui/ApplicationExceptionHandler.java
@@ -3,14 +3,17 @@ package woowacourse.shoppingcart.ui;
 import java.util.List;
 import javax.validation.ConstraintViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import woowacourse.auth.exception.InvalidTokenException;
 import woowacourse.auth.exception.LoginFailException;
 import woowacourse.shoppingcart.dto.ErrorResponse;
@@ -26,56 +29,47 @@ import woowacourse.shoppingcart.exception.InvalidProductException;
 import woowacourse.shoppingcart.exception.NotInCustomerCartItemException;
 
 @RestControllerAdvice
-public class ControllerAdvice {
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(RuntimeException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(RuntimeException.class)
     public ErrorResponse handleUnhandledException() {
         return new ErrorResponse("Unhandled Exception");
     }
 
-    @ExceptionHandler(EmptyResultDataAccessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(EmptyResultDataAccessException.class)
     public ErrorResponse handle() {
         return new ErrorResponse("존재하지 않는 데이터 요청입니다.");
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public FieldErrorResponse handleInvalidRequest(final BindingResult bindingResult) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
+        BindingResult bindingResult = ex.getBindingResult();
         final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         final FieldError mainError = fieldErrors.get(0);
+        FieldErrorResponse response = new FieldErrorResponse(mainError.getField(), mainError.getDefaultMessage());
 
-        return new FieldErrorResponse(mainError.getField(), mainError.getDefaultMessage());
+        return ResponseEntity
+                .status(status)
+                .body(response);
     }
 
-    @ExceptionHandler({DuplicateEmailException.class, DuplicateUsernameException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({DuplicateEmailException.class, DuplicateUsernameException.class})
     public FieldErrorResponse handleDuplicatedRequest(DuplicateDomainException exception) {
         return new FieldErrorResponse(exception.getField(), exception.getMessage());
     }
 
-    @ExceptionHandler({LoginFailException.class, InvalidTokenException.class})
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public ErrorResponse handleLoginFailException(Exception exception) {
-        return new ErrorResponse(exception.getMessage());
-    }
-
-    @ExceptionHandler(ForbiddenAccessException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorResponse handleForbiddenAccessException(Exception exception) {
-        return new ErrorResponse(exception.getMessage());
-    }
-
-    @ExceptionHandler({
-            HttpMessageNotReadableException.class,
-            ConstraintViolationException.class,
-    })
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
     public ErrorResponse handleInvalidRequest(final RuntimeException e) {
         return new ErrorResponse(e.getMessage());
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({
             InvalidCustomerException.class,
             InvalidCartItemException.class,
@@ -83,8 +77,34 @@ public class ControllerAdvice {
             InvalidOrderException.class,
             NotInCustomerCartItemException.class,
     })
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleInvalidAccess(final RuntimeException e) {
         return new ErrorResponse(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ExceptionHandler({LoginFailException.class, InvalidTokenException.class})
+    public ErrorResponse handleLoginFailException(Exception exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(ForbiddenAccessException.class)
+    public ErrorResponse handleForbiddenAccessException(Exception exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleAllOtherErrors(final Exception e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers,
+                                                             HttpStatus status, WebRequest request) {
+        ErrorResponse response = new ErrorResponse(ex.getMessage());
+        return ResponseEntity
+                .status(status)
+                .body(response);
     }
 }
