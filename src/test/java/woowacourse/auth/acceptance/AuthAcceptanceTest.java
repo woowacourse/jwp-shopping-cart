0eddad33b62b74050df8fab81c00e6552b3ca4ca
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-02 16:44:32 +0900

    test(assurance): add assurance tests on invalid request body

--- src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
+++ src/test/java/woowacourse/auth/acceptance/AuthAcceptanceTest.java
@@ -5,8 +5,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -27,52 +30,73 @@ class AuthAcceptanceTest extends AcceptanceTest {
         회원가입_요청();
     }
 
-    @Test
-    void 로그인_성공() {
-        TokenRequest 유효한_인증정보 = new TokenRequest(USERNAME, PASSWORD);
+    @DisplayName("POST /login - 로그인 요청에 대한 검증")
+    @Nested
+    class AuthenticationTest {
 
-        ExtractableResponse<Response> response = 로그인_요청(유효한_인증정보);
+        @Test
+        void 로그인_성공시_200() {
+            TokenRequest 유효한_인증정보 = new TokenRequest(USERNAME, PASSWORD);
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+            ExtractableResponse<Response> response = 로그인_요청(유효한_인증정보);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        }
+
+        @Test
+        void 로그인_실패시_401() {
+            TokenRequest 비밀번호가_잘못된_정보 = new TokenRequest(USERNAME, "wrong123@#$%");
+
+            ExtractableResponse<Response> response = 로그인_요청(비밀번호가_잘못된_정보);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
+        }
+
+        @Test
+        void 로그인에_필요한_정보가_하나라도_누락된_경우_400() {
+            Map<String, Object> 비밀번호_누락 = new HashMap<>() {{
+                put("username", USERNAME);
+            }};
+
+            ExtractableResponse<Response> response = 로그인_요청(비밀번호_누락);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
     }
 
-    @Test
-    void 로그인_실패() {
-        TokenRequest 잘못된_인증정보 = new TokenRequest(USERNAME, "wrong123@#$%");
+    @DisplayName("로그인 성공 후 Bearer 토큰을 활용한 인가 과정")
+    @Nested
+    class AuthorizationTest {
 
-        ExtractableResponse<Response> response = 로그인_요청(잘못된_인증정보);
+        @Test
+        void 토큰이_유효한_경우_인가_성공() {
+            String 유효한_토큰 = 유효한_로그인_요청();
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.UNAUTHORIZED.value());
-    }
+            ExtractableResponse<Response> response = 내_정보_조회_요청(유효한_토큰);
 
-    @Test
-    void 토큰이_유효한_경우_인가_성공() {
-        String 유효한_토큰 = 유효한_로그인_요청();
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        }
 
-        ExtractableResponse<Response> response = 내_정보_조회_요청(유효한_토큰);
+        @Test
+        void 토큰이_유효하지_않은_경우_인가_실패() {
+            String 잘못된_토큰 = "안녕하세요.해커입니다.";
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
+            ExtractableResponse<Response> response = 내_정보_조회_요청(잘못된_토큰);
 
-    @Test
-    void 토큰이_유효하지_않은_경우_인가_실패() {
-        String 잘못된_토큰 = "안녕하세요.해커입니다.";
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+        }
 
-        ExtractableResponse<Response> response = 내_정보_조회_요청(잘못된_토큰);
+        @Test
+        void 토큰이_없는_경우_인증_실패() {
+            ExtractableResponse<Response> response = RestAssured
+                    .given().log().all()
+                    .accept(MediaType.APPLICATION_JSON_VALUE)
+                    .when().get("/customers/me")
+                    .then().log().all()
+                    .extract();
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
-    }
-
-    @Test
-    void 토큰이_없는_경우_인증_실패() {
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .when().get("/customers/me")
-                .then().log().all()
-                .extract();
-
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.FORBIDDEN.value());
+        }
     }
 
     private void 회원가입_요청() {
@@ -90,10 +114,10 @@ class AuthAcceptanceTest extends AcceptanceTest {
                 .getAccessToken();
     }
 
-    private ExtractableResponse<Response> 로그인_요청(TokenRequest request) {
+    private ExtractableResponse<Response> 로그인_요청(Object json) {
         return RestAssured
                 .given().log().all()
-                .body(request)
+                .body(json)
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .when().post("/login")
--- src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CustomerAcceptanceTest.java
@@ -5,19 +5,21 @@ import static org.assertj.core.api.Assertions.assertThat;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import woowacourse.setup.AcceptanceTest;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
-import woowacourse.shoppingcart.dto.request.UniqueUsernameRequest;
-import woowacourse.shoppingcart.dto.response.GetMeResponse;
+import woowacourse.setup.AcceptanceTest;
 import woowacourse.shoppingcart.dto.request.SignUpRequest;
+import woowacourse.shoppingcart.dto.request.UniqueUsernameRequest;
 import woowacourse.shoppingcart.dto.request.UpdateMeRequest;
 import woowacourse.shoppingcart.dto.request.UpdatePasswordRequest;
+import woowacourse.shoppingcart.dto.response.GetMeResponse;
 import woowacourse.shoppingcart.dto.response.UniqueUsernameResponse;
 
 @SuppressWarnings("NonAsciiCharacters")
@@ -25,14 +27,32 @@ import woowacourse.shoppingcart.dto.response.UniqueUsernameResponse;
 class CustomerAcceptanceTest extends AcceptanceTest {
 
     private static final String 유효한_아이디 = "username";
+    private static final String 유효한_비밀번호 = "password1@";
     private static final String 유효한_닉네임 = "닉네임";
-    private static final SignUpRequest 유효한_사용자 = new SignUpRequest(유효한_아이디, "password1@", 유효한_닉네임, 15);
+    private static final SignUpRequest 유효한_사용자 = new SignUpRequest(유효한_아이디, 유효한_비밀번호, 유효한_닉네임, 15);
 
-    @Test
-    void 회원가입() {
-        ExtractableResponse<Response> response = 회원가입_요청(유효한_사용자);
+    @DisplayName("POST /customers - 회원가입")
+    @Nested
+    class SignUpTest {
 
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        @Test
+        void 회원가입_성공() {
+            ExtractableResponse<Response> response = 회원가입_요청(유효한_사용자);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        }
+
+        @Test
+        void 필요한_정보가_하나라도_누락된_경우_400() {
+            Map<String, Object> 아이디와_나이_누락_요청 = new HashMap<>() {{
+                put("password", 유효한_비밀번호);
+                put("nickname", 유효한_닉네임);
+            }};
+
+            ExtractableResponse<Response> response = 회원가입_요청(아이디와_나이_누락_요청);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
     }
 
     @DisplayName("GET /customers/me - 고객 본인의 정보 조회")
@@ -44,7 +64,7 @@ class CustomerAcceptanceTest extends AcceptanceTest {
             String 유효한_토큰 = 회원가입_요청_후_토큰_반환(유효한_사용자);
 
             ExtractableResponse<Response> response = 내_정보_조회_요청(유효한_토큰);
-            GetMeResponse actualBody = response.body().jsonPath().getObject(".", GetMeResponse.class);
+            GetMeResponse actualBody = response.as(GetMeResponse.class);
             GetMeResponse expectedBody = new GetMeResponse(유효한_아이디, 유효한_닉네임, 15);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
@@ -64,18 +84,32 @@ class CustomerAcceptanceTest extends AcceptanceTest {
     @Nested
     class UpdateMeTest {
 
+        private SignUpRequest 고객 = new SignUpRequest(유효한_아이디, "password123@", 유효한_닉네임, 15);
+
         @Test
         void 로그인된_경우_200() {
-            SignUpRequest 고객 = new SignUpRequest(유효한_아이디, "password123@", 유효한_닉네임, 15);
-            UpdateMeRequest 수정된_고객 = new UpdateMeRequest(유효한_아이디, "새로운닉네임", 20);
             String 유효한_토큰 = 회원가입_요청_후_토큰_반환(고객);
+            UpdateMeRequest 수정된_고객 = new UpdateMeRequest(유효한_아이디, "새로운닉네임", 20);
 
             ExtractableResponse<Response> response = 내_정보_수정_요청(수정된_고객, 유효한_토큰);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
         }
 
-        private ExtractableResponse<Response> 내_정보_수정_요청(UpdateMeRequest json, String accessToken) {
+        @Test
+        void 필요한_정보가_하나라도_누락된_경우_400() {
+            String 유효한_토큰 = 회원가입_요청_후_토큰_반환(고객);
+            Map<String, Object> 아이디_누락_요청 = new HashMap<>() {{
+                put("nickname", 유효한_닉네임);
+                put("age", 15);
+            }};
+
+            ExtractableResponse<Response> response = 내_정보_수정_요청(아이디_누락_요청, 유효한_토큰);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        private ExtractableResponse<Response> 내_정보_수정_요청(Object json, String accessToken) {
             return RestAssured.given().log().all()
                     .auth().oauth2(accessToken)
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -124,7 +158,7 @@ class CustomerAcceptanceTest extends AcceptanceTest {
 
         @Test
         void 로그인되었고_현재_비밀번호를_제대로_입력한_경우_200() {
-            String 기존_비밀번호 = "password1@";
+            String 기존_비밀번호 = 유효한_비밀번호;
             SignUpRequest customer = new SignUpRequest(유효한_아이디, 기존_비밀번호, 유효한_닉네임, 15);
             UpdatePasswordRequest updatePasswordRequest = new UpdatePasswordRequest(기존_비밀번호, "newpw123@#$");
             String 유효한_토큰 = 회원가입_요청_후_토큰_반환(customer);
@@ -136,9 +170,8 @@ class CustomerAcceptanceTest extends AcceptanceTest {
 
         @Test
         void 로그인되었으나_현재_비밀번호를_잘못_입력한_경우_400() {
-            String 기존_비밀번호 = "password1@";
             String 틀린_비밀번호 = "wrong123@#";
-            SignUpRequest customer = new SignUpRequest(유효한_아이디, 기존_비밀번호, 유효한_닉네임, 15);
+            SignUpRequest customer = new SignUpRequest(유효한_아이디, 유효한_비밀번호, 유효한_닉네임, 15);
             UpdatePasswordRequest updatePasswordRequest = new UpdatePasswordRequest(틀린_비밀번호, "새로운_비밀번호");
             String 유효한_토큰 = 회원가입_요청_후_토큰_반환(customer);
 
@@ -147,7 +180,19 @@ class CustomerAcceptanceTest extends AcceptanceTest {
             assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
         }
 
-        private ExtractableResponse<Response> 내_비밀번호_수정_요청(UpdatePasswordRequest json, String accessToken) {
+        @Test
+        void 필요한_정보가_하나라도_누락된_경우_400() {
+            String 유효한_토큰 = 회원가입_요청_후_토큰_반환(new SignUpRequest(유효한_아이디, 유효한_비밀번호, 유효한_닉네임, 15));
+            Map<String, Object> 기존_비밀번호_누락_요청 = new HashMap<>() {{
+                put("newPassword", "asdfg1234@");
+            }};
+
+            ExtractableResponse<Response> response = 내_비밀번호_수정_요청(기존_비밀번호_누락_요청, 유효한_토큰);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        private ExtractableResponse<Response> 내_비밀번호_수정_요청(Object json, String accessToken) {
             return RestAssured.given().log().all()
                     .auth().oauth2(accessToken)
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -167,7 +212,7 @@ class CustomerAcceptanceTest extends AcceptanceTest {
             UniqueUsernameRequest 존재하지_않는_아이디 = new UniqueUsernameRequest("존재하지_않는_아이디");
 
             ExtractableResponse<Response> response = 아이디_중복_조회_요청(존재하지_않는_아이디);
-            UniqueUsernameResponse actualBody = response.body().jsonPath().getObject(".", UniqueUsernameResponse.class);
+            UniqueUsernameResponse actualBody = response.as(UniqueUsernameResponse.class);
             UniqueUsernameResponse expectedBody = new UniqueUsernameResponse(true);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
@@ -180,14 +225,23 @@ class CustomerAcceptanceTest extends AcceptanceTest {
             UniqueUsernameRequest 이미_저장된_아이디 = new UniqueUsernameRequest(유효한_아이디);
 
             ExtractableResponse<Response> response = 아이디_중복_조회_요청(이미_저장된_아이디);
-            UniqueUsernameResponse actualBody = response.body().jsonPath().getObject(".", UniqueUsernameResponse.class);
+            UniqueUsernameResponse actualBody = response.as(UniqueUsernameResponse.class);
             UniqueUsernameResponse expectedBody = new UniqueUsernameResponse(false);
 
             assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
             assertThat(actualBody).isEqualTo(expectedBody);
         }
 
-        private ExtractableResponse<Response> 아이디_중복_조회_요청(UniqueUsernameRequest json) {
+        @Test
+        void 필요한_정보가_누락된_경우_400() {
+            Map<String, Object> 비어있는_요청 = new HashMap<>();
+
+            ExtractableResponse<Response> response = 아이디_중복_조회_요청(비어있는_요청);
+
+            assertThat(response.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+        }
+
+        private ExtractableResponse<Response> 아이디_중복_조회_요청(Object json) {
             return RestAssured.given().log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .body(json)
@@ -197,13 +251,17 @@ class CustomerAcceptanceTest extends AcceptanceTest {
         }
     }
 
-    private ExtractableResponse<Response> 회원가입_요청(SignUpRequest json) {
+    private ExtractableResponse<Response> 회원가입_요청(Object json) {
         return RestAssured.given().log().all()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(json)
                 .when().post("/customers")
                 .then().log().all()
                 .extract();
+    }
+
+    private ExtractableResponse<Response> 회원가입_요청(SignUpRequest json) {
+        return 회원가입_요청((Object) json);
     }
 
     private String 회원가입_요청_후_토큰_반환(SignUpRequest signupRequest) {
