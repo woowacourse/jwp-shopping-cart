8dfcbc5f91a0e1675efc80f5a2820cb5c633e6c2
김찬 <gmelddl514@gmail.com>
2022-06-02 16:11:13 +0900

    feat: 회원 탈퇴시 비밀번호 검증 로직 구현

---
+++ src/main/java/woowacourse/shoppingcart/dto/CustomerRemoveRequest.java
@@ -0,0 +1,17 @@
+package woowacourse.shoppingcart.dto;
+
+public class CustomerRemoveRequest {
+
+    private String password;
+
+    public CustomerRemoveRequest() {
+    }
+
+    public CustomerRemoveRequest(final String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
