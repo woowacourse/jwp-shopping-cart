4494fbab38b6faf2828a29981efd4448b1dfdce7
hyewoncc <hyewoncc@gmail.com>
2022-06-02 17:24:48 +0900

    fix: DisplayName과 메서드 명의 오타 정정

--- src/test/java/woowacourse/auth/support/EncryptionTest.java
+++ src/test/java/woowacourse/auth/support/EncryptionTest.java
@@ -9,7 +9,7 @@ class EncryptionTest {
 
     private Encryption encryption = new Encryption();
 
-    @DisplayName("암호회 된 문장이 원본과 일치하면 true를 반환한다.")
+    @DisplayName("암호화 된 문장이 원본과 일치하면 true를 반환한다.")
     @Test
     void isSame_true() {
         String plainText = "password123";
@@ -18,7 +18,7 @@ class EncryptionTest {
         assertThat(encryption.isSame(encryptedText, plainText)).isTrue();
     }
 
-    @DisplayName("암호회 된 문장이 원본과 일치하지 않으면 false를 반환한다")
+    @DisplayName("암호화 된 문장이 원본과 일치하지 않으면 false를 반환한다")
     @Test
     void isSame_false() {
         String plainText = "password123";
--- src/test/java/woowacourse/auth/ui/AuthenticationContextTest.java
+++ src/test/java/woowacourse/auth/ui/AuthenticationContextTest.java
@@ -10,7 +10,7 @@ class AuthenticationContextTest {
 
     @Test
     @DisplayName("principal이 없는데 get할 경우 예외 발생")
-    void getNullPrinciapl_throwException() {
+    void getNullPrincipal_throwException() {
         assertThatThrownBy(() -> new AuthenticationContext().getPrincipal())
                 .isInstanceOf(InvalidAuthException.class)
                 .hasMessage("principal은 null값이 반환될 수 없습니다.");
