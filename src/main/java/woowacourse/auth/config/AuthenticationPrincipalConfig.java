f2241b6ac9dc9cb215cdb41fe45680ed2131939b
jurlring <78091011+jurlring@users.noreply.github.com>
2022-06-02 17:23:17 +0900

    refactor: 컨벤션 적용

--- src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
+++ src/main/java/woowacourse/auth/config/AuthenticationPrincipalConfig.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import woowacourse.auth.application.AuthService;
 import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.auth.ui.AuthenticationPrincipalArgumentResolver;
 
