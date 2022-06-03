6c8dd0b1e883ff5c70849677b1fcd787592129ec
Hyunta <mohai2618@gmail.com>
2022-06-03 15:28:53 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/woowacourse/auth/dto/LoginCustomer.java
+++ src/main/java/woowacourse/auth/dto/LoginCustomer.java
@@ -10,10 +10,6 @@ public class LoginCustomer {
         this.loginId = loginId;
     }
 
-    public static LoginCustomer of(Customer customer) {
-        return new LoginCustomer(customer.getLoginId());
-    }
-
     public String getLoginId() {
         return loginId;
     }
