1f22ad289ef134a08b38458b06cdae5829902c75
awesomeo184 <awesomeo184@gmail.com>
2022-06-03 13:19:35 +0900

    fix: VARCHAR 사이즈를 50에서 255로 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,9 +11,9 @@ drop table if exists customer;
 create table customer
 (
     id       bigint      not null auto_increment,
-    email    varchar(50) not null,
-    nickname varchar(50) not null,
-    password varchar(50) not null,
+    email    varchar(255) not null,
+    nickname varchar(255) not null,
+    password varchar(255) not null,
     primary key (id)
 ) engine=InnoDB default charset=utf8mb4;
 
