12c3eeb39fc9691a28460b68fc802060b2118c12
shindong96 <2006peter@naver.com>
2022-06-04 00:34:42 +0900

    refactor: 예외 상황에 적절한 메세지 던지기

--- src/main/java/woowacourse/shoppingcart/exception/NotExistException.java
+++ src/main/java/woowacourse/shoppingcart/exception/NotExistException.java
@@ -2,11 +2,9 @@ package woowacourse.shoppingcart.exception;
 
 public class NotExistException extends IllegalArgumentException {
 
-    public NotExistException() {
-        super();
-    }
+    public static final String UNKNOWN_ERROR = "알 수 없는 예외입니다.";
 
-    public NotExistException(String message) {
-        super(message);
+    public NotExistException() {
+        super(UNKNOWN_ERROR);
     }
 }
