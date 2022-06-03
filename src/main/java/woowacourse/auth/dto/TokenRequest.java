7c8832998017133b2518c210351d5d415164a830
giron <ggyucheol0@gmail.com>
2022-06-03 18:20:56 +0900

    refactor: 비밀번호 규약 추가

--- src/main/java/woowacourse/auth/dto/TokenRequest.java
+++ src/main/java/woowacourse/auth/dto/TokenRequest.java
@@ -6,9 +6,10 @@ import javax.validation.constraints.Size;
 public class TokenRequest {
     @NotBlank(message = "유저 이름은 빈칸일 수 없습니다.")
     @Size(min = 5, max = 20)
-
     private String userName;
+
     @NotBlank(message = "비밀번호는 빈칸일 수 없습니다.")
+    @Size(min = 8, max = 16)
     private String password;
 
     private TokenRequest() {
