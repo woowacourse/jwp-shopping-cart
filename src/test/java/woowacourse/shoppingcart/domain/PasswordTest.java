f741e87d417dacda4161e82cecaddc9c9bcd8632
yeongunheo <gjduddns@gmail.com>
2022-06-04 22:07:29 +0900

    test: 비밀번호가 알파벳, 숫자, 특수문자의 조합으로 되어있는 경우에만 정상적으로 생성되는지 검증하는 테스트 추가

--- src/test/java/woowacourse/shoppingcart/domain/PasswordTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/PasswordTest.java
@@ -1,18 +1,45 @@
 package woowacourse.shoppingcart.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import woowacourse.exception.InputFormatException;
 
 public class PasswordTest {
 
-    @DisplayName("패스워드는 10글자 이상이어야 한다.")
-    @Test
-    void password() {
-        assertThatThrownBy(()-> Password.from("pwd")).isInstanceOf(InputFormatException.class);
+    @ParameterizedTest
+    @DisplayName("패스워드가 8~12글자가 아니라면 에러를 발생한다.")
+    @EmptySource
+    @ValueSource(strings = {"asdasd", "password0", "password01234"})
+    void InputWrongLengthPassword(String value) {
+        //then
+        assertThatThrownBy(() -> Password.from(value))
+                .isInstanceOf(InputFormatException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("패스워드가 8~12글자이고 알파벳, 숫자, 특수문자의 조합이라면 정상적으로 생성된다.")
+    @ValueSource(strings = {"password1!", "Password012="})
+    void InputWrightPassword(String value) {
+        //then
+        assertThatCode(() -> Password.from(value))
+                .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @DisplayName("패턴에 맞지 않는 비밀번호라면 에러를 발생한다.")
+    @EmptySource
+    @ValueSource(strings = {"pwdpwdpwd", "pwdPWD12", "PWDpwd!!!"})
+    void InputWrongPatternPassword(String value) {
+        //then
+        assertThatThrownBy(() -> Password.from(value))
+                .isInstanceOf(InputFormatException.class);
     }
 
     @DisplayName("같은 문자열로 암호화하였을 경우 두 암호화된 패스워드는 같다.")
