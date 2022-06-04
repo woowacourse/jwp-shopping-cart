e58467f615b7be06cac4e36070f4186dd579e2fd
yeon-06 <rnjstldus2@gmail.com>
2022-06-04 14:22:35 +0900

    refactor: 불필요한 ResponseEntity 제거

--- src/main/java/woowacourse/shoppingcart/ui/ControllerAdvice.java
+++ src/main/java/woowacourse/shoppingcart/ui/ControllerAdvice.java
@@ -4,7 +4,6 @@ import java.util.List;
 import javax.validation.ConstraintViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -26,29 +25,33 @@ import woowacourse.shoppingcart.exception.NotInCustomerCartItemException;
 public class ControllerAdvice {
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity handleUnhandledException() {
-        return ResponseEntity.badRequest().body("Unhandled Exception");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleUnhandledException(Exception e) {
+        return "Unhandled Exception";
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity handle() {
-        return ResponseEntity.badRequest().body("존재하지 않는 데이터 요청입니다.");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handle() {
+        return "존재하지 않는 데이터 요청입니다.";
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class})
-    public ResponseEntity handleInvalidRequest(final BindingResult bindingResult) {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidRequest(final BindingResult bindingResult) {
         final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         final FieldError mainError = fieldErrors.get(0);
 
-        return ResponseEntity.badRequest().body(mainError.getDefaultMessage());
+        return mainError.getDefaultMessage();
     }
 
     @ExceptionHandler({
             HttpMessageNotReadableException.class,
             ConstraintViolationException.class,
     })
-    public ResponseEntity handleInvalidRequest(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidRequest(final RuntimeException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler({
@@ -58,8 +61,9 @@ public class ControllerAdvice {
             InvalidOrderException.class,
             NotInCustomerCartItemException.class,
     })
-    public ResponseEntity handleInvalidAccess(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidAccess(final RuntimeException e) {
+        return e.getMessage();
     }
 
     @ExceptionHandler
