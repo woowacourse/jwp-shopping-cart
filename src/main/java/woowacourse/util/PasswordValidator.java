96d981a4705be6e01727bc25631602e022c36de2
yoon <duddbs774@gmail.com>
2022-06-04 16:27:39 +0900

    refactor: 정규식 상수화

--- src/main/java/woowacourse/util/PasswordValidator.java
+++ src/main/java/woowacourse/util/PasswordValidator.java
@@ -5,12 +5,15 @@ import javax.validation.ConstraintValidatorContext;
 
 public class PasswordValidator implements ConstraintValidator<PasswordCheck, String> {
 
+    private static final String KOREAN_REGEX = ".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*";
+    private static final String BLANK_REGEX = "\\S+";
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null || value.isEmpty()) {
             return true;
         }
-        if (value.matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*") || !value.matches("\\S+")) {
+        if (value.matches(KOREAN_REGEX) || !value.matches(BLANK_REGEX)) {
             return false;
         }
         return true;
--- src/main/java/woowacourse/util/UserNameValidator.java
+++ src/main/java/woowacourse/util/UserNameValidator.java
@@ -5,12 +5,14 @@ import javax.validation.ConstraintValidatorContext;
 
 public class UserNameValidator implements ConstraintValidator<UserNameCheck, String> {
 
+    private static final String BLANK_REGEX = "\\S+";
+
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null || value.isEmpty()) {
             return true;
         }
-        if (!value.matches("\\S+")) {
+        if (!value.matches(BLANK_REGEX)) {
             return false;
         }
         return true;
