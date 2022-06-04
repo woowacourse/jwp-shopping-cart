9d66ef9e9308daeddc2424146461abfce8e21a11
sinb57 <sinb57@naver.com>
2022-06-04 15:40:05 +0900

    chore: schema.sql - customer 테이블의 unique 속성 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,8 +11,8 @@ drop table if exists customer;
 create table customer
 (
     id       bigint       not null auto_increment,
-    email    varchar(255) not null,
-    nickname varchar(255) not null unique,
+    email    varchar(255) not null unique,
+    nickname varchar(255) not null,
     password varchar(255) not null,
     primary key (id)
 ) engine = InnoDB
