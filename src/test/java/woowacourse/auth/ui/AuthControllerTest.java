f88f59a881c64464112a62d43faaccbebda4978e
Hongdonggeon <ghd700@naver.com>
2022-06-05 01:09:20 +0900

    test: import문 삭제

--- src/test/java/woowacourse/auth/ui/AuthControllerTest.java
+++ src/test/java/woowacourse/auth/ui/AuthControllerTest.java
@@ -12,17 +12,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import woowacourse.auth.application.AuthService;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.shoppingcart.config.LoginCustomerResolver;
 import woowacourse.shoppingcart.exception.InvalidLoginException;
-import woowacourse.shoppingcart.ui.ControllerAdvice;
 
 @WebMvcTest(controllers = {AuthController.class})
-@Import(ControllerAdvice.class)
 @MockBean(value = {LoginCustomerResolver.class})
 public class AuthControllerTest {
 
