6df684073769292a19405ba920282b6497bdadfd
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-04 20:40:03 +0900

    feat(swagger): implement Bearer Authentication button
     - hide authUser argument from swagger-ui

--- src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
+++ src/main/java/woowacourse/shoppingcart/ui/CustomerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 import woowacourse.auth.domain.User;
 import woowacourse.auth.support.AuthenticatedUser;
 import woowacourse.shoppingcart.application.CustomerService;
@@ -39,26 +40,26 @@ public class CustomerController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<GetMeResponse> getMe(@AuthenticatedUser User authUser) {
+    public ResponseEntity<GetMeResponse> getMe(@ApiIgnore @AuthenticatedUser User authUser) {
         GetMeResponse currentCustomer = customerService.getCustomer(authUser);
         return ResponseEntity.ok(currentCustomer);
     }
 
     @PutMapping("/me")
-    public ResponseEntity<Void> updateMe(@AuthenticatedUser User authUser,
+    public ResponseEntity<Void> updateMe(@ApiIgnore @AuthenticatedUser User authUser,
                                          @Validated @RequestBody UpdateMeRequest updateMeRequest) {
         customerService.updateNicknameAndAge(authUser, updateMeRequest);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/me")
-    public ResponseEntity<Void> deleteMe(@AuthenticatedUser User authUser) {
+    public ResponseEntity<Void> deleteMe(@ApiIgnore @AuthenticatedUser User authUser) {
         customerService.deleteCustomer(authUser);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/me/password")
-    public ResponseEntity<Void> updatePassword(@AuthenticatedUser User authUser,
+    public ResponseEntity<Void> updatePassword(@ApiIgnore @AuthenticatedUser User authUser,
                                                @Validated @RequestBody UpdatePasswordRequest updatePasswordRequest) {
         customerService.updatePassword(authUser, updatePasswordRequest);
         return ResponseEntity.ok().build();
