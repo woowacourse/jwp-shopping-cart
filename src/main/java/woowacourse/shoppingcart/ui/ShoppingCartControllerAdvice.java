4618abe66df523075502c94ad998f87b1d3440d0
cjlee38 <cjlee38@github.com>
2022-06-04 15:00:37 +0900

    feat: exception message를 dto로 포장

--- src/main/java/woowacourse/shoppingcart/ui/ShoppingCartControllerAdvice.java
+++ src/main/java/woowacourse/shoppingcart/ui/ShoppingCartControllerAdvice.java
@@ -16,43 +16,44 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import woowacourse.shoppingcart.exception.ShoppingCartException;
 import woowacourse.shoppingcart.exception.domain.NotFoundException;
+import woowacourse.shoppingcart.ui.dto.ExceptionResponse;
 
 @RestControllerAdvice
 public class ShoppingCartControllerAdvice {
 
     @ExceptionHandler(ShoppingCartException.class)
-    public ResponseEntity<String> handleInvalidAccess(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ExceptionResponse> handleInvalidAccess(final RuntimeException e) {
+        return ResponseEntity.badRequest().body(ExceptionResponse.from(e));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFound(final RuntimeException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    public ResponseEntity<ExceptionResponse> handleNotFound(final RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ExceptionResponse.from(e));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleInvalidRequest(final BindingResult bindingResult) {
+    public ResponseEntity<ExceptionResponse> handleInvalidRequest(final BindingResult bindingResult) {
         final List<FieldError> fieldErrors = bindingResult.getFieldErrors();
         final FieldError mainError = fieldErrors.get(0);
 
-        return ResponseEntity.badRequest().body(mainError.getDefaultMessage());
+        return ResponseEntity.badRequest().body(new ExceptionResponse(mainError.getDefaultMessage()));
     }
 
     @ExceptionHandler({
         HttpMessageNotReadableException.class,
         ConstraintViolationException.class,
     })
-    public ResponseEntity<String> handleInvalidRequest(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<ExceptionResponse> handleInvalidRequest(final RuntimeException e) {
+        return ResponseEntity.badRequest().body(ExceptionResponse.from(e));
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity<String> handle() {
-        return ResponseEntity.badRequest().body("존재하지 않는 데이터 요청입니다.");
+    public ResponseEntity<ExceptionResponse> handle() {
+        return ResponseEntity.badRequest().body(new ExceptionResponse("존재하지 않는 데이터 요청입니다."));
     }
 
     @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleUnhandledException() {
-        return ResponseEntity.internalServerError().body("알 수 없는 에러입니다.");
+    public ResponseEntity<ExceptionResponse> handleUnhandledException(RuntimeException e) {
+        return ResponseEntity.internalServerError().body(new ExceptionResponse("알 수 없는 에러입니다."));
     }
 }
