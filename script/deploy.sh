e04f24621dc27092317c02ca79ef3d1a06069474
nailseong <nnis9857@gmail.com>
2022-06-04 20:01:45 +0900

    refactor: 배포 스크립트 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,4 +1,13 @@
+# 환경 변수 적용
+cd
+source env.sh
+echo host = $MYSQL_HOST, port = $MYSQL_PORT, db = $MYSQL_DATABASE
+
+# 애플리케이션 빌드
+cd -
 cd ..
 ./gradlew bootJar
+
+# 애플리케이션 실행
 cd build/libs/
 nohup java -jar jwp-shopping-cart-0.0.1-SNAPSHOT.jar >> application.log 2>&1 &
