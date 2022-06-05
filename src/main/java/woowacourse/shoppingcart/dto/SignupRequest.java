50c6a4fb0cff685279ac36ebfc1a7f66438c5be4
corinne <yoo77hyeon@gmail.com>
2022-06-05 23:30:26 +0900

    refactor: gradle h2 의존성 추가

--- src/main/java/woowacourse/shoppingcart/dto/SignupRequest.java
+++ src/main/java/woowacourse/shoppingcart/dto/SignupRequest.java
@@ -4,19 +4,24 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import woowacourse.auth.dto.PhoneNumber;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class SignupRequest {
 
-    @Size(min = 4, max = 15, message = "아이디 길이는 4~15자를 만족해야 합니다.")
+//    @Size(min = 4, max = 15, message = "아이디 길이는 4~15자를 만족해야 합니다.")
+    @NotBlank
     private final String account;
-    @Size(min = 2, max = 10, message = "닉네임 길이는 2~10자를 만족해야 합니다.")
+//    @Size(min = 2, max = 10, message = "닉네임 길이는 2~10자를 만족해야 합니다.")
+    @NotBlank
     private final String nickname;
+    @NotBlank
     @Size(min = 8, max = 20, message = "비밀번호 길이는 8~20자를 만족해야 합니다.")
     @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)).*", message = "비밀번호는 대소문자, 숫자, 특수문자가 반드시 1개 이상 포함되어야 합니다.")
     private final String password;
-    @Size(max = 255, message = "주소 길이는 255자를 초과할 수 없습니다.")
+//    @Size(max = 255, message = "주소 길이는 255자를 초과할 수 없습니다.")
+    @NotBlank
     private final String address;
     @Valid
     private final PhoneNumber phoneNumber;
