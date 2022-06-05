267d761e2ee73ad85112eed7c08bedd98bd0d753
Hongdonggeon <ghd700@naver.com>
2022-06-05 14:52:06 +0900

    refactor: 비밃번호 규칙 변경

--- src/main/java/woowacourse/shoppingcart/dto/CustomerCreationRequest.java
+++ src/main/java/woowacourse/shoppingcart/dto/CustomerCreationRequest.java
@@ -11,7 +11,7 @@ public class CustomerCreationRequest {
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}", message = "비밀번호 양식이 잘못 되었습니다.")
     private String password;
 
-    @Pattern(regexp = "[a-zA-Z0-9가-힣]{2,}", message = "닉네임 양식이 잘못 되었습니다.")
+    @Pattern(regexp = "[a-zA-Z0-9가-힣]{2,8}", message = "닉네임 양식이 잘못 되었습니다.")
     private String nickname;
 
     private CustomerCreationRequest() {
