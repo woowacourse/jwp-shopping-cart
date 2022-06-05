9c09d22b813379a3e8cec00f1b931c5005b8a09d
beomWhale <cndqjacndqja@gmail.com>
2022-06-05 17:14:08 +0900

    refactor: 불필요한 import 제거

--- src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -1,18 +1,16 @@
 package woowacourse.auth.ui;
 
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.core.MethodParameter;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
-import woowacourse.auth.support.AuthenticationPrincipal;
 import woowacourse.auth.application.AuthService;
+import woowacourse.auth.support.AuthenticationPrincipal;
 import woowacourse.auth.support.AuthorizationExtractor;
 import woowacourse.auth.support.JwtTokenProvider;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
 
 public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
     private final JwtTokenProvider jwtTokenProvider;
