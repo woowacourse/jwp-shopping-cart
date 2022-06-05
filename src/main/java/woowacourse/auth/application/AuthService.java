cdd55c3df5e35ecfbb99a9e201ca414100119115
SeungCheol <ssc6839@gmail.com>
2022-06-05 23:02:04 +0900

    refactor: 로그인 검증 로직 수정

--- src/main/java/woowacourse/auth/application/AuthService.java
+++ src/main/java/woowacourse/auth/application/AuthService.java
@@ -7,8 +7,6 @@ import woowacourse.auth.dto.LoginRequest;
 import woowacourse.auth.dto.TokenResponse;
 import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.customer.application.CustomerService;
-import woowacourse.customer.domain.Customer;
-import woowacourse.customer.support.passwordencoder.PasswordEncoder;
 
 @Transactional
 @Service
@@ -16,16 +14,13 @@ public class AuthService {
 
     private final JwtTokenProvider jwtTokenProvider;
     private final CustomerService customerService;
-    private final PasswordEncoder passwordEncoder;
 
     public AuthService(
         final JwtTokenProvider jwtTokenProvider,
-        final CustomerService customerService,
-        final PasswordEncoder passwordEncoder
+        final CustomerService customerService
     ) {
         this.jwtTokenProvider = jwtTokenProvider;
         this.customerService = customerService;
-        this.passwordEncoder = passwordEncoder;
     }
 
     public TokenResponse createToken(final LoginRequest loginRequest) {
@@ -35,7 +30,6 @@ public class AuthService {
     }
 
     private void validateLogin(final LoginRequest loginRequest) {
-        final Customer customer = customerService.findByUsername(loginRequest.getUsername());
-        customer.getPassword().matches(passwordEncoder, loginRequest.getPassword());
+        customerService.confirmPassword(loginRequest.getUsername(), loginRequest.getPassword());
     }
 }
