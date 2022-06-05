8c2f089752624d101bb2b8c4edfe5b56a133e83a
summerlunaa <summerlunaa9@gmail.com>
2022-06-06 02:58:30 +0900

    refactor: 주석 및 예외 처리 추가

--- src/main/java/woowacourse/auth/support/AuthorizationExtractor.java
+++ src/main/java/woowacourse/auth/support/AuthorizationExtractor.java
@@ -2,12 +2,19 @@ package woowacourse.auth.support;
 
 import java.util.Enumeration;
 import javax.servlet.http.HttpServletRequest;
+import woowacourse.auth.exception.AuthException;
 
 public class AuthorizationExtractor {
     public static final String AUTHORIZATION = "Authorization";
     public static String BEARER_TYPE = "Bearer";
     public static final String ACCESS_TOKEN_TYPE = AuthorizationExtractor.class.getSimpleName() + ".ACCESS_TOKEN_TYPE";
 
+    /**
+     * request header의 authorization에 존재하는 토큰을 가져온다.
+     * token의 형식은 "Bearer xxx.yyy.zzz"이다.
+     *
+     * @return header에 존재하는 accessToken(xxx.yyy.zzz) 값
+     */
     public static String extract(HttpServletRequest request) {
         Enumeration<String> headers = request.getHeaders(AUTHORIZATION);
         while (headers.hasMoreElements()) {
@@ -22,7 +29,6 @@ public class AuthorizationExtractor {
                 return authHeaderValue;
             }
         }
-
-        return null;
+        throw new AuthException("토큰 값이 잘못되었습니다.");
     }
 }
