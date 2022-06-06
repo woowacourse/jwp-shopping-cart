1d80edb928748a08dd2692915d6cd98efe7e886e
youngwooyoo <fbduddn97@naver.com>
2022-06-06 14:22:08 +0900

    test: test 가독성위해 auth.sql 삭제하고 setUp에서 직접 넣어주도록 처리

--- src/test/java/woowacourse/auth/application/AuthServiceTest.java
+++ src/test/java/woowacourse/auth/application/AuthServiceTest.java
@@ -3,7 +3,9 @@ package woowacourse.auth.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static woowacourse.ShoppingCartFixture.잉_로그인요청;
+import static woowacourse.ShoppingCartFixture.잉_회원생성요청;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +15,26 @@ import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
 import woowacourse.auth.support.JwtTokenProvider;
 import woowacourse.exception.auth.LoginFailureException;
+import woowacourse.shoppingcart.application.CustomerService;
+import woowacourse.shoppingcart.exception.CustomerNotFoundException;
 
 @SpringBootTest
-@Sql({"/truncate.sql", "/auth.sql"})
+@Sql({"/truncate.sql"})
 class AuthServiceTest {
 
     @Autowired
     private AuthService authService;
 
     @Autowired
+    private CustomerService customerService;
+
+    @Autowired
     private JwtTokenProvider jwtTokenProvider;
+
+    @BeforeEach
+    void setUp() {
+        customerService.create(잉_회원생성요청);
+    }
 
     @DisplayName("로그인 성공시 토큰을 발급한다.")
     @Test
@@ -46,5 +58,16 @@ class AuthServiceTest {
         // when then
         assertThatThrownBy(() -> authService.getToken(로그인요청))
                 .isInstanceOf(LoginFailureException.class);
+    }
+
+    @DisplayName("로그인시 이메일이 일치하지 않으면 예외를 발생한다.")
+    @Test
+    void getTokenWithEmailInCorrectException() {
+        // given
+        final TokenRequest 로그인요청 = new TokenRequest(잉_로그인요청.getEmail() + "stranger", 잉_로그인요청.getPassword());
+
+        // when then
+        assertThatThrownBy(() -> authService.getToken(로그인요청))
+                .isInstanceOf(CustomerNotFoundException.class);
     }
 }
