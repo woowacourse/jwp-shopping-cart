d352f9681823ca54c89ea1eb685318b47432dd6c
yukong <kyb062666@gmail.com>
2022-06-06 22:12:42 +0900

    test: 암호화 테스트 시 입력값 수정

--- src/test/java/woowacourse/shoppingcart/domain/customer/password/EncryptedPasswordTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/customer/password/EncryptedPasswordTest.java
@@ -5,9 +5,6 @@ import org.junit.jupiter.api.Test;
 import woowacourse.shoppingcart.exception.datanotmatch.CustomerDataNotMatchException;
 import woowacourse.shoppingcart.exception.datanotmatch.LoginDataNotMatchException;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("EncryptedPassword 도메인 테스트")
@@ -17,7 +14,7 @@ class EncryptedPasswordTest {
     @Test
     void validateMatchingLoginPassword() {
         // given
-        Password password = new EncryptedPassword(encrypt("1234asdf!"));
+        Password password = new EncryptedPassword("1338ad00357397e37ec3990310efd04f767ab485fa8e69f2d06df186f9327372");
 
         // when & then
         assertThatThrownBy(() -> password.validateMatchingLoginPassword("invalidPassword"))
@@ -29,29 +26,11 @@ class EncryptedPasswordTest {
     @Test
     void validateMatchingOriginalPassword() {
         // given
-        Password password = new EncryptedPassword(encrypt("1234asdf!"));
+        Password password = new EncryptedPassword("1338ad00357397e37ec3990310efd04f767ab485fa8e69f2d06df186f9327372");
 
         // when & then
         assertThatThrownBy(() -> password.validateMatchingOriginalPassword("invalidPassword"))
                 .isInstanceOf(CustomerDataNotMatchException.class)
                 .hasMessage("기존 비밀번호와 입력한 비밀번호가 일치하지 않습니다.");
-    }
-
-    private String encrypt(final String text) {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update(text.getBytes());
-            return bytesToHex(md.digest());
-        } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException();
-        }
-    }
-
-    private String bytesToHex(final byte[] bytes) {
-        StringBuilder builder = new StringBuilder();
-        for (byte b : bytes) {
-            builder.append(String.format("%02x", b));
-        }
-        return builder.toString();
     }
 }
--- src/test/java/woowacourse/shoppingcart/domain/customer/password/PlainPasswordTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/customer/password/PlainPasswordTest.java
@@ -1,10 +1,9 @@
-package woowacourse.shoppingcart.domain.customer;
+package woowacourse.shoppingcart.domain.customer.password;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import woowacourse.shoppingcart.domain.customer.password.PlainPassword;
 import woowacourse.shoppingcart.exception.dataempty.CustomerDataEmptyException;
 import woowacourse.shoppingcart.exception.dataformat.CustomerDataFormatException;
 import woowacourse.shoppingcart.exception.datanotmatch.CustomerDataNotMatchException;
