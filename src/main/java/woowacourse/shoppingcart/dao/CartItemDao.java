27d0d68fef83288858d6f0964c1994332805cc8f
skullKim <kyk990328@gmail.com>
2022-06-06 16:30:36 +0900

    chore(src/): 코딩 스타일 수정
    코딩 컨벤션에 맞게 코딩 스타일 수정

--- src/main/java/woowacourse/shoppingcart/dao/CartItemDao.java
+++ src/main/java/woowacourse/shoppingcart/dao/CartItemDao.java
@@ -1,12 +1,10 @@
 package woowacourse.shoppingcart.dao;
 
-import java.sql.PreparedStatement;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
--- src/main/java/woowacourse/shoppingcart/dao/ProductDao.java
+++ src/main/java/woowacourse/shoppingcart/dao/ProductDao.java
@@ -1,17 +1,13 @@
 package woowacourse.shoppingcart.dao;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 import javax.sql.DataSource;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import woowacourse.shoppingcart.domain.Product;
 import woowacourse.shoppingcart.exception.InvalidProductException;
