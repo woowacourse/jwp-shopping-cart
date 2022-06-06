386e2845649ed621a4c5ff15c573878103fc484f
Chris <bjuuuu98@gmail.com>
2022-06-06 13:41:49 +0900

    chore: import.sql 수정

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,3 @@
-drop table product if exists;
-create table product
-(
-    id        bigint       not null auto_increment,
-    name      varchar(255) not null,
-    price     integer      not null,
-    quantity  integer      not null,
-    image_url varchar(255),
-    primary key (id)
-);
-
 insert into product (name, price, quantity, image_url)
 values ('캠핑 의자', 35000, 100, 'https://thawing-fortress-83192.herokuapp.com/static/images/camping-chair.jpg');
 insert into product (name, price, quantity, image_url)
