c53eb0922744f61a927034e96130fdbb7208b7b3
nailseong <nnis9857@gmail.com>
2022-06-07 02:17:44 +0900

    test(CartAcceptanceTest): 장바구니 상품 목록 조회 기능

--- src/test/java/woowacourse/shoppingcart/acceptance/CartAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CartAcceptanceTest.java
@@ -1,28 +1,26 @@
 package woowacourse.shoppingcart.acceptance;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
 
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import woowacourse.shoppingcart.cart.domain.Cart;
 import woowacourse.shoppingcart.cart.dto.CartItemAdditionRequest;
 
 @DisplayName("장바구니 관련 기능")
 public class CartAcceptanceTest extends AcceptanceTest {
 
     private final Long productId1 = 1L;
-    private final Long productId2 = 2L;
 
     public static ExtractableResponse<Response> 장바구니_아이템_추가_요청(final String token, final Long productId) {
         final Map<String, Object> requestBody = new HashMap<>();
@@ -38,16 +36,6 @@ public class CartAcceptanceTest extends AcceptanceTest {
                 .extract();
     }
 
-    public static ExtractableResponse<Response> 장바구니_아이템_목록_조회_요청(final String token) {
-        return RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .when().get("/customers/me/carts")
-                .then().log().all()
-                .extract();
-    }
-
     public static ExtractableResponse<Response> 장바구니_삭제_요청(final String token, final Long cartId) {
         return RestAssured
                 .given().log().all()
@@ -58,25 +46,9 @@ public class CartAcceptanceTest extends AcceptanceTest {
                 .extract();
     }
 
-    public static void 장바구니_아이템_추가됨(final ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(response.header("Location")).isNotBlank();
-    }
-
     public static Long 장바구니_아이템_추가되어_있음(final String token, final Long productId) {
         final ExtractableResponse<Response> response = 장바구니_아이템_추가_요청(token, productId);
         return Long.parseLong(response.header("Location").split("/carts/")[1]);
-    }
-
-    public static void 장바구니_아이템_목록_응답됨(final ExtractableResponse<Response> response) {
-        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
-    }
-
-    public static void 장바구니_아이템_목록_포함됨(final ExtractableResponse<Response> response, final Long... productIds) {
-        final List<Long> resultProductIds = response.jsonPath().getList(".", Cart.class).stream()
-                .map(Cart::getProductId)
-                .collect(Collectors.toList());
-        assertThat(resultProductIds).contains(productIds);
     }
 
     public static void 장바구니_삭제됨(final ExtractableResponse<Response> response) {
@@ -90,15 +62,7 @@ public class CartAcceptanceTest extends AcceptanceTest {
         final CartItemAdditionRequest request = new CartItemAdditionRequest(1L);
 
         // when
-        final ValidatableResponse response = RestAssured
-                .given().log().all()
-                .accept(MediaType.APPLICATION_JSON_VALUE)
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(request)
-                .when()
-                .post("/users/me/carts")
-                .then().log().all();
+        final ValidatableResponse response = postCartItem(request);
 
         // then
         response.statusCode(HttpStatus.NO_CONTENT.value());
@@ -107,13 +71,27 @@ public class CartAcceptanceTest extends AcceptanceTest {
     @DisplayName("장바구니 아이템 목록 조회")
     @Test
     void getCartItems() {
-        장바구니_아이템_추가되어_있음(token, productId1);
-        장바구니_아이템_추가되어_있음(token, productId2);
+        // given
+        postCartItem(new CartItemAdditionRequest(2L));
+        postCartItem(new CartItemAdditionRequest(1L));
 
-        final ExtractableResponse<Response> response = 장바구니_아이템_목록_조회_요청(token);
+        // when
+        final ValidatableResponse response = RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/users/me/carts")
+                .then().log().all();
 
-        장바구니_아이템_목록_응답됨(response);
-        장바구니_아이템_목록_포함됨(response, productId1, productId2);
+        // then
+        response.statusCode(HttpStatus.OK.value())
+                .body("products.id", contains(2, 1))
+                .body("products.name", contains("규조토 치약 꽂이 스탠드", "붙이는 치약 홀더 / 걸이"))
+                .body("products.image_url", hasSize(2))
+                .body("products.price", contains(4300, 1600))
+                .body("products.quantity", contains(1, 1));
     }
 
     @DisplayName("장바구니 삭제")
@@ -124,5 +102,17 @@ public class CartAcceptanceTest extends AcceptanceTest {
         final ExtractableResponse<Response> response = 장바구니_삭제_요청(token, cartId);
 
         장바구니_삭제됨(response);
+    }
+
+    private ValidatableResponse postCartItem(CartItemAdditionRequest request) {
+        return RestAssured
+                .given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when()
+                .post("/users/me/carts")
+                .then().log().all();
     }
 }
