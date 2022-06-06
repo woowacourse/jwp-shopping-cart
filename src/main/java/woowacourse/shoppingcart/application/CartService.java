97fd60a0192eeac4aaf16ccc09fce2b3c535419f
skullKim <kyk990328@gmail.com>
2022-06-06 22:47:07 +0900

    feat(CartRepository.java): 레파지토리 레이어 생성
    Cart에 대한 Repository 레이어 생성

--- src/main/java/woowacourse/shoppingcart/application/CartService.java
+++ src/main/java/woowacourse/shoppingcart/application/CartService.java
@@ -1,68 +1,42 @@
 package woowacourse.shoppingcart.application;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import woowacourse.shoppingcart.dao.CartItemDao;
-import woowacourse.shoppingcart.dao.CustomerDao;
-import woowacourse.shoppingcart.dao.ProductDao;
 import woowacourse.shoppingcart.domain.Cart;
-import woowacourse.shoppingcart.domain.Product;
 import woowacourse.shoppingcart.dto.response.CartResponse;
-import woowacourse.shoppingcart.exception.InvalidProductException;
 import woowacourse.shoppingcart.exception.NotInCustomerCartItemException;
+import woowacourse.shoppingcart.repository.CartRepository;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class CartService {
 
-    private final CartItemDao cartItemDao;
-    private final CustomerDao customerDao;
-    private final ProductDao productDao;
+    private final CartRepository cartRepository;
 
-    public CartService(final CartItemDao cartItemDao, final CustomerDao customerDao, final ProductDao productDao) {
-        this.cartItemDao = cartItemDao;
-        this.customerDao = customerDao;
-        this.productDao = productDao;
+    public CartService(final CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
     }
 
     public List<CartResponse> findCartsByCustomerName(final String customerName) {
-        final List<Long> cartIds = findCartIdsByCustomerName(customerName);
-
-        final List<Cart> carts = new ArrayList<>();
-        for (final Long cartId : cartIds) {
-            final Long productId = cartItemDao.findProductIdById(cartId);
-            final Product product = productDao.findProductById(productId);
-            carts.add(new Cart(cartId, product));
-        }
+        final List<Cart> carts = cartRepository.findCartsByCustomerName(customerName);
         return carts.stream()
                 .map(CartResponse::from)
                 .collect(Collectors.toList());
     }
 
-    private List<Long> findCartIdsByCustomerName(final String customerName) {
-        final Long customerId = customerDao.findIdByUserName(customerName);
-        return cartItemDao.findIdsByCustomerId(customerId);
-    }
-
     public Long addCart(final Long productId, final String customerName) {
-        final Long customerId = customerDao.findIdByUserName(customerName);
-        try {
-            return cartItemDao.addCartItem(customerId, productId);
-        } catch (Exception e) {
-            throw new InvalidProductException();
-        }
+        return cartRepository.addCart(productId, customerName);
     }
 
     public void deleteCart(final String customerName, final Long cartId) {
         validateCustomerCart(cartId, customerName);
-        cartItemDao.deleteCartItem(cartId);
+        cartRepository.deleteCart(cartId);
     }
 
     private void validateCustomerCart(final Long cartId, final String customerName) {
-        final List<Long> cartIds = findCartIdsByCustomerName(customerName);
+        final List<Long> cartIds = cartRepository.findCartIdsByCustomerName(customerName);
         if (cartIds.contains(cartId)) {
             return;
         }
---
+++ src/main/java/woowacourse/shoppingcart/repository/CartRepository.java
@@ -0,0 +1,58 @@
+package woowacourse.shoppingcart.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import woowacourse.shoppingcart.dao.CartItemDao;
+import woowacourse.shoppingcart.dao.CustomerDao;
+import woowacourse.shoppingcart.dao.ProductDao;
+import woowacourse.shoppingcart.domain.Cart;
+import woowacourse.shoppingcart.domain.Product;
+import woowacourse.shoppingcart.exception.InvalidProductException;
+
+@Repository
+public class CartRepository {
+
+
+    private final CartItemDao cartItemDao;
+    private final CustomerDao customerDao;
+    private final ProductDao productDao;
+
+    public CartRepository(CartItemDao cartItemDao, CustomerDao customerDao,
+                          ProductDao productDao) {
+        this.cartItemDao = cartItemDao;
+        this.customerDao = customerDao;
+        this.productDao = productDao;
+    }
+
+    public List<Cart> findCartsByCustomerName(final String customerName) {
+        final List<Long> cartIds = findCartIdsByCustomerName(customerName);
+
+        final List<Cart> carts = new ArrayList<>();
+        for (final Long cartId : cartIds) {
+            final Long productId = cartItemDao.findProductIdById(cartId);
+            final Product product = productDao.findProductById(productId);
+            carts.add(new Cart(cartId, product));
+        }
+
+        return carts;
+    }
+
+    public List<Long> findCartIdsByCustomerName(final String customerName) {
+        final Long customerId = customerDao.findIdByUserName(customerName);
+        return cartItemDao.findIdsByCustomerId(customerId);
+    }
+
+    public Long addCart(final Long productId, final String customerName) {
+        final Long customerId = customerDao.findIdByUserName(customerName);
+        try {
+            return cartItemDao.addCartItem(customerId, productId);
+        } catch (Exception e) {
+            throw new InvalidProductException();
+        }
+    }
+
+    public void deleteCart(final Long cartId) {
+        cartItemDao.deleteCartItem(cartId);
+    }
+}
