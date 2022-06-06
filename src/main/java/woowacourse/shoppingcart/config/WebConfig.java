fdfede2651f6bc464bdea12e1a64254304bdec46
corinne <yoo77hyeon@gmail.com>
2022-06-06 15:41:40 +0900

    refactor: cors maxAge 설정

--- src/main/java/woowacourse/shoppingcart/config/WebConfig.java
+++ src/main/java/woowacourse/shoppingcart/config/WebConfig.java
@@ -14,6 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
                 .allowedOrigins("*")
-                .exposedHeaders(HttpHeaders.LOCATION);
+                .exposedHeaders(HttpHeaders.LOCATION)
+                .maxAge(3600);
     }
 }
