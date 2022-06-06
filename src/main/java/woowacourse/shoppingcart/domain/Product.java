5397618d9d2d7fa6bcab9e9cd3c07f5fc74b1155
jojogreen91 <iawbg13@gmail.com>
2022-06-06 21:52:47 +0900

    feat: Product 도메인 클래스 구현

--- src/main/java/woowacourse/shoppingcart/domain/Product.java
+++ src/main/java/woowacourse/shoppingcart/domain/Product.java
@@ -1,23 +1,40 @@
 package woowacourse.shoppingcart.domain;
 
+import java.net.URL;
+import woowacourse.shoppingcart.exception.InvalidInputException;
+
 public class Product {
     private Long id;
     private String name;
     private Integer price;
     private String imageUrl;
 
-    public Product() {
-    }
-
     public Product(final Long id, final String name, final int price, final String imageUrl) {
+        validateName(name);
+        validatePrice(price);
+        validateName(imageUrl);
         this.id = id;
         this.name = name;
         this.price = price;
         this.imageUrl = imageUrl;
     }
 
-    public Product(final String name, final int price, final String imageUrl) {
-        this(null, name, price, imageUrl);
+    private void validateName(final String name) {
+        if (name == null || name.isBlank()) {
+            throw new InvalidInputException("부적절한 상품 이름입니다.");
+        }
+    }
+
+    private void validatePrice(final int price) {
+        if (price <= 0) {
+            throw new InvalidInputException("부적절한 상품 가격입니다.");
+        }
+    }
+
+    private void validateImageUrl(final String imageUrl) {
+        if (imageUrl == null || imageUrl.isBlank()) {
+            throw new InvalidInputException("부적절한 이미지 URL 입니다.");
+        }
     }
 
     public String getName() {
