ea8a88b5ffc2668b84b99eafb28d1be8bbf27230
RunaNam <nmj5012@naver.com>
2022-06-07 00:56:11 +0900

    refactor(CartItemResponse): 결정한 규약에 맞도록 response 값 수정

--- src/main/java/woowacourse/shoppingcart/dto/CartItemResponse.java
+++ src/main/java/woowacourse/shoppingcart/dto/CartItemResponse.java
@@ -1,25 +1,49 @@
 package woowacourse.shoppingcart.dto;
 
 import woowacourse.shoppingcart.domain.Cart;
-import woowacourse.shoppingcart.domain.Product;
+import woowacourse.shoppingcart.domain.ThumbnailImage;
 
 public class CartItemResponse {
     private Long id;
-    private Product product;
+    private Long productId;
+    private String name;
+    private int price;
+    private int quantity;
+    private ThumbnailImage thumbnailImage;
 
     public CartItemResponse() {
     }
 
     public CartItemResponse(Cart cart) {
         this.id = cart.getId();
-        this.product = cart.getProduct();
+        this.productId = cart.getProduct().getId();
+        this.name = cart.getProduct().getName();
+        this.price = cart.getProduct().getPrice();
+        this.quantity = cart.getProduct().getStockQuantity();
+        this.thumbnailImage = cart.getProduct().getThumbnailImage();
     }
 
     public Long getId() {
         return id;
     }
 
-    public Product getProduct() {
-        return product;
+    public Long getProductId() {
+        return productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public ThumbnailImage getThumbnailImage() {
+        return thumbnailImage;
     }
 }
--- src/test/java/woowacourse/shoppingcart/acceptance/CartAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/CartAcceptanceTest.java
@@ -110,7 +110,7 @@ public class CartAcceptanceTest extends AcceptanceTest {
 
     public static void 장바구니_아이템_목록_포함됨(ExtractableResponse<Response> response, Long... productIds) {
         List<Long> resultProductIds = response.jsonPath().getList(".", CartItemResponse.class).stream()
-                .map(cartItemResponse -> cartItemResponse.getProduct().getId())
+                .map(CartItemResponse::getProductId)
                 .collect(Collectors.toList());
         assertThat(resultProductIds).contains(productIds);
     }
