7350cfcefa7dcff41fbbdf5103c41e3b34cf81ef
Chris <bjuuuu98@gmail.com>
2022-06-06 21:50:09 +0900

    chore: 불필요한 스키마 삭제

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,7 +11,6 @@ create table member
     primary key (id)
 );
 
-
 create table product
 (
     id        bigint       not null auto_increment,
@@ -32,28 +31,3 @@ create table cart_item
     foreign key (member_id) references member (id) ON DELETE CASCADE,
     foreign key (product_id) references product (id) ON DELETE CASCADE
 );
-
--- create table orders
--- (
---     id        bigint not null auto_increment,
---     member_id bigint not null,
---     primary key (id)
--- ) engine=InnoDB default charset=utf8mb4;
---
--- alter table orders
---     add constraint fk_orders_to_member foreign key (member_id) references member (id);
---
--- create table orders_detail
--- (
---     id         bigint  not null auto_increment,
---     orders_id  bigint  not null,
---     product_id bigint  not null,
---     quantity   integer not null,
---     primary key (id)
--- ) engine=InnoDB default charset=utf8mb4;
---
--- alter table orders_detail
---     add constraint fk_orders_detail_to_orders foreign key (orders_id) references orders (id);
---
--- alter table orders_detail
---     add constraint fk_orders_detail_to_product foreign key (product_id) references product (id);
