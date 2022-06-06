97b5e0d219dda14eb75a56676f4908acaee5a676
skullKim <kyk990328@gmail.com>
2022-06-06 16:29:31 +0900

    refactor(CustoerDao.java): 디비 접근 방식 수정
    사용하면 JdbcTemplate을 NamedJdbcTemplate으로 변경

--- src/main/java/woowacourse/shoppingcart/dao/OrderDao.java
+++ src/main/java/woowacourse/shoppingcart/dao/OrderDao.java
@@ -1,16 +1,12 @@
 package woowacourse.shoppingcart.dao;
 
-import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Map;
 import javax.sql.DataSource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
