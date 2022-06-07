f1ccd4519a3b4936f404eda14279049cf228bcba
= <rerub0831@gmail.com>
2022-06-07 14:12:07 +0900

    feat: Data.sql에 유저 목록 삭제

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,31 +1,3 @@
-insert into customer (username, password, nickname, withdrawal)
-values ('puterism', '1234', 'nick1', false),
-       ('tanney-102', '1234', 'nick2', false),
-       ('jho2301', '1234', 'nick3', false),
-       ('365kim', '1234', 'nick4', false),
-       ('dudtjr913', '1234', 'nick5', false),
-       ('jum0', '1234', 'nick6', false),
-       ('hyuuunjukim', '1234', 'nick7', false),
-       ('zereight', '1234', 'nick8', false),
-       ('devhyun637', '1234', 'nick9', false),
-       ('swon3210', '1234', 'nick10', false),
-       ('bigsaigon333', '1234', 'nick11', false),
-       ('yungo1846', '1234', 'nick12', false),
-       ('zigsong', '1234', 'nick13', false),
-       ('iborymagic', '1234', 'nick14', false),
-       ('0307kwon', '1234', 'nick15', false),
-       ('gwangyeol-im', '1234', 'nick16', false),
-       ('shinsehantan', '1234', 'nick17', false),
-       ('ddongule', '1234', 'nick18', false),
-       ('seojihwan', '1234', 'nick19', false),
-       ('0imbean0', '1234', 'nick20', false),
-       ('sunyoungkwon', '1234', 'nick21', false),
-       ('hchayan', '1234', 'nick22', false),
-       ('2sooy', '1234', 'nick23', false),
-       ('yujo11', '1234', 'nick24', false),
-       ('sunhpark42', '1234', 'nick25', false)
-;
-
 insert into product (name, price, image_url)
 values ('치킨', 10000, 'http://example.com/chicken.jpg'),
        ('맥주', 20000, 'http://example.com/beer.jpg'),
