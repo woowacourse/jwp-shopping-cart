1f4b2747069a15ab78c9a754327bb1ce29625384
jojogreen91 <iawbg13@gmail.com>
2022-06-07 14:33:50 +0900

    feat: 찾은 상품 범위 만큼의 상품 정보들을 반환하는 기능 구현

--- src/test/java/woowacourse/shoppingcart/application/ProductServiceTest.java
+++ src/test/java/woowacourse/shoppingcart/application/ProductServiceTest.java
@@ -3,13 +3,17 @@ package woowacourse.shoppingcart.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
 import woowacourse.shoppingcart.dto.ProductResponse;
 
 @SpringBootTest
+@Sql("/init.sql")
 class ProductServiceTest {
 
     @Autowired
@@ -32,6 +36,24 @@ class ProductServiceTest {
                 () -> assertThat(productResponse.getImageUrl()).isEqualTo(
                         "https://cdn-mart.baemin.com/sellergoods/main/678bd8ec-e5fa-4ae2-be55-2cd290b3f10f.jpg"
                 )
+        );
+    }
+
+    @DisplayName("찾은 상품 범위 만큼의 상품 정보들을 반환하는 기능")
+    @Test
+    void findProductsOfPage() {
+        // given
+        int page = 2;
+        int limit = 5;
+
+        // when
+        List<ProductResponse> productsOfPage = productService.findProductsOfPage(page, limit);
+
+        // then
+        assertAll(
+                () -> assertThat(productsOfPage.size()).isEqualTo(5),
+                () -> assertThat(productsOfPage.stream().map(ProductResponse::getId).collect(Collectors.toList()))
+                        .containsExactly(6L, 7L, 8L, 9L, 10L)
         );
     }
 }
