a6471308b19f5cffb2c32bcde823da99b451505c
Kim Seunglae <obey1342@gmail.com>
2022-06-07 15:51:58 +0900

    test(ProductApiTest): restdocs 구현

---
+++ src/test/java/woowacourse/ProductApiTest.java
@@ -0,0 +1,94 @@
+package woowacourse;
+
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.delete;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+
+class ProductApiTest extends TestSupport {
+
+    @Test
+    void add_test() throws Exception {
+        mockMvc.perform(
+                post("/api/products")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(readJson("/json/products/products-create.json"))
+            )
+            .andExpect(status().isCreated())
+            .andDo(
+                restDocs.document(
+                    requestFields(
+                        fieldWithPath("id").description("id"),
+                        fieldWithPath("name").description("name"),
+                        fieldWithPath("price").description("price"),
+                        fieldWithPath("imageUrl").description("imageUrl")
+                    )
+                )
+            );
+    }
+
+    @Test
+    void products_test() throws Exception {
+        mockMvc.perform(
+                get("/api/products")
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andDo(
+                restDocs.document(
+                    responseFields(
+                        fieldWithPath("[].id").description("product id"),
+                        fieldWithPath("[].name").description("product name"),
+                        fieldWithPath("[].price").description("product price"),
+                        fieldWithPath("[].imageUrl").description("product imageUrl")
+                    )
+                )
+            );
+    }
+
+    @Test
+    void product_test() throws Exception {
+        mockMvc.perform(
+                get("/api/products/{productId}", 1L)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+            .andExpect(status().isOk())
+            .andDo(
+                restDocs.document(
+                    pathParameters(
+                        parameterWithName("productId").description("Product Id")
+                    ),
+                    responseFields(
+                        fieldWithPath("id").description("product id"),
+                        fieldWithPath("name").description("product name"),
+                        fieldWithPath("price").description("product price"),
+                        fieldWithPath("imageUrl").description("product imageUrl")
+                    )
+                )
+            );
+    }
+
+    @Test
+    void delete_test() throws Exception {
+        mockMvc.perform(
+                delete("/api/products/{productId}", 2L)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+            )
+            .andExpect(status().isNoContent())
+            .andDo(
+                restDocs.document(
+                    pathParameters(
+                        parameterWithName("productId").description("Product Id")
+                    )
+                )
+            );
+    }
+}
