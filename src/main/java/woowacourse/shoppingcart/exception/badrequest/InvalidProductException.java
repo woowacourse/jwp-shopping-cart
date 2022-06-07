0ecd66f9003ce2d0c4444c57a6959a4079a172d4
nailseong <nnis9857@gmail.com>
2022-06-07 20:39:14 +0900

    refactor(Order): 주문 관련 코드 삭제

--- src/main/java/woowacourse/shoppingcart/exception/badrequest/InvalidProductException.java
+++
@@ -1,8 +0,0 @@
-package woowacourse.shoppingcart.exception.badrequest;
-
-public class InvalidProductException extends BadRequestException {
-
-    public InvalidProductException() {
-        super("1004", "올바르지 않은 사용자 이름이거나 상품 아이디 입니다.");
-    }
-}
