9557fd8ebebd44253bcff86273aeeb0ac4b93eb0
meatsby <meatsby@gmail.com>
2022-06-07 15:52:15 +0900

    feat: CORS 관련 기능 구현

--- src/main/java/woowacourse/auth/ui/AuthInterceptor.java
+++ src/main/java/woowacourse/auth/ui/AuthInterceptor.java
@@ -3,6 +3,7 @@ package woowacourse.auth.ui;
 import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import woowacourse.auth.support.AuthorizationExtractor;
@@ -13,14 +14,22 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    public AuthInterceptor(JwtTokenProvider jwtTokenProvider) {
+    public AuthInterceptor(final JwtTokenProvider jwtTokenProvider) {
         this.jwtTokenProvider = jwtTokenProvider;
     }
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
+                             final Object handler) {
+        if (isPreflight(request)) {
+            return true;
+        }
         String accessToken = AuthorizationExtractor.extract(Objects.requireNonNull(request));
         jwtTokenProvider.validateToken(accessToken);
         return true;
+    }
+
+    private boolean isPreflight(final HttpServletRequest request) {
+        return HttpMethod.OPTIONS.matches(request.getMethod());
     }
 }
