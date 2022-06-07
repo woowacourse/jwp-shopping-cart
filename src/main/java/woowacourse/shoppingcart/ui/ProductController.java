05379e60ba7ef2f2f1120c1880fd5671d2d9c189
Ohzzi <clover7even@naver.com>
2022-06-08 01:15:13 +0900

    feat: 상품 조회 기능 api 매핑

--- src/main/java/woowacourse/shoppingcart/ui/ProductController.java
+++ src/main/java/woowacourse/shoppingcart/ui/ProductController.java
@@ -1,20 +1,12 @@
 package woowacourse.shoppingcart.ui;
 
-import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import woowacourse.shoppingcart.application.ProductService;
-import woowacourse.shoppingcart.dto.Request;
-import woowacourse.shoppingcart.dto.request.ProductRequest;
 import woowacourse.shoppingcart.dto.response.ProductResponse;
 
 @RestController
@@ -28,28 +20,10 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductResponse>> products() {
-        return ResponseEntity.ok(productService.findProducts());
-    }
-
-    @PostMapping
-    public ResponseEntity<Void> add(@Validated(Request.allProperties.class) @RequestBody final ProductRequest productRequest) {
-        final Long productId = productService.save(productRequest);
-        final URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/" + productId)
-                .build().toUri();
-        return ResponseEntity.created(uri).build();
-    }
-
-    @GetMapping("/{productId}")
-    public ResponseEntity<ProductResponse> product(@PathVariable final Long productId) {
-        return ResponseEntity.ok(productService.findProductById(productId));
-    }
-
-    @DeleteMapping("/{productId}")
-    public ResponseEntity<Void> delete(@PathVariable final Long productId) {
-        productService.deleteProductById(productId);
-        return ResponseEntity.noContent().build();
+    public ResponseEntity<List<ProductResponse>> showSelectedPage(@RequestParam int page, @RequestParam int limit) {
+        int totalCount = productService.countAll();
+        return ResponseEntity.ok()
+                .header("x-total-count", String.valueOf(totalCount))
+                .body(productService.findProducts(page, limit));
     }
 }
