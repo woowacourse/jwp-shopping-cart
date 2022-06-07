2682893ce41fd43776dbcdffd903ea0d6bbae812
Kim Seunglae <obey1342@gmail.com>
2022-06-07 16:02:51 +0900

    refactor(Password): 길이 조건 제거

--- src/main/java/woowacourse/shoppingcart/domain/Password.java
+++ src/main/java/woowacourse/shoppingcart/domain/Password.java
@@ -6,8 +6,6 @@ public class Password {
 
     private static final Pattern PASSWORD_PATTERN = Pattern.compile(
         "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()]).+$");
-    private static final int MIN_PASSWORD_LENGTH = 8;
-    private static final int MAX_PASSWORD_LENGTH = 15;
 
     private final String value;
 
@@ -17,17 +15,6 @@ public class Password {
     }
 
     private void validate(String value) {
-        validateLength(value);
-        validateForm(value);
-    }
-
-    private void validateLength(String value) {
-        if (value.length() >= MIN_PASSWORD_LENGTH || value.length() <= MAX_PASSWORD_LENGTH) {
-            throw new IllegalArgumentException("비밀번호는 최소 8자 이상 15자 이하여야 합니다.");
-        }
-    }
-
-    private void validateForm(String value) {
         if (!PASSWORD_PATTERN.matcher(value).matches()) {
             throw new IllegalArgumentException("비밀번호 형식이 유효하지 않습니다.");
         }
