4421b986397edef26c482d5bf808ab9c85b69502
cjlee38 <cjlee38@github.com>
2022-06-07 14:54:15 +0900

    test: 테스트 customer 패키지 정리

--- src/test/java/woowacourse/shoppingcart/domain/customer/PasswordEncryptorTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/customer/PasswordEncryptorTest.java
@@ -1,4 +1,4 @@
-package woowacourse.shoppingcart.domain;
+package woowacourse.shoppingcart.domain.customer;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
--- src/test/java/woowacourse/shoppingcart/domain/customer/PhoneNumberTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/customer/PhoneNumberTest.java
@@ -1,4 +1,4 @@
-package woowacourse.shoppingcart.domain;
+package woowacourse.shoppingcart.domain.customer;
 
 import static org.assertj.core.api.Assertions.*;
 
