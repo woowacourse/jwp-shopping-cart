046165476babef822065ecef4897b5ea7bbb13d1
Jeong Jinwoo <jwjeong96@gmail.com>
2022-06-07 22:52:35 +0900

    feat(db): add data fixture for product data at h2 db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO product(name, price, image_url)
+VALUES ('감자', 1000, 'https://storybook.takealook.kr/image/potato.jpg'),
+       ('고구마', 1500, 'https://storybook.takealook.kr/image/potato.jpg'),
+       ('호박고구마', 3000, 'https://storybook.takealook.kr/image/potato.jpg'),
+       ('최강 감자', 100000, 'https://storybook.takealook.kr/image/potato.jpg'),
+       ('무료 감자', 0, 'https://storybook.takealook.kr/image/potato.jpg'),
+       ('세일 중인 감자', 500, 'https://storybook.takealook.kr/image/potato.jpg'),
+       ('회오리 감자', 100000, 'https://storybook.takealook.kr/image/potato.jpg')
+;
