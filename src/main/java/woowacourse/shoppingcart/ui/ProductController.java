966d144ee41b0d57c4b11f7ef256cfcae9ed392f
Ohzzi <clover7even@naver.com>
2022-06-08 02:12:03 +0900

    feat: 상품 추가 기능 api 매핑

--- src/main/java/woowacourse/shoppingcart/ui/ProductController.java
+++ src/main/java/woowacourse/shoppingcart/ui/ProductController.java
@@ -1,12 +1,19 @@
 package woowacourse.shoppingcart.ui;
 
+import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import woowacourse.shoppingcart.application.ProductService;
+import woowacourse.shoppingcart.dto.Request;
+import woowacourse.shoppingcart.dto.request.ProductRequest;
 import woowacourse.shoppingcart.dto.response.ProductResponse;
 
 @RestController
@@ -25,5 +32,15 @@ public class ProductController {
         return ResponseEntity.ok()
                 .header("x-total-count", String.valueOf(totalCount))
                 .body(productService.findProducts(page, limit));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> add(@Validated(Request.allProperties.class) @RequestBody final ProductRequest productRequest) {
+        final Long productId = productService.save(productRequest);
+        final URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/" + productId)
+                .build().toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/test/java/woowacourse/shoppingcart/ui/ProductControllerTest.java
+++ src/test/java/woowacourse/shoppingcart/ui/ProductControllerTest.java
@@ -1,22 +1,27 @@
 package woowacourse.shoppingcart.ui;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import woowacourse.shoppingcart.application.ProductService;
+import woowacourse.shoppingcart.dto.request.ProductRequest;
 import woowacourse.shoppingcart.dto.response.ProductResponse;
 
 @WebMvcTest(ProductController.class)
@@ -31,6 +36,8 @@ class ProductControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @DisplayName("조회할 페이지와 페이지 당 상품의 숫자로 상품을 조회한다. - 200 Ok")
     @Test
@@ -52,5 +59,21 @@ class ProductControllerTest {
                 .findProducts(1, 10);
         verify(productService, times(1))
                 .countAll();
+    }
+
+    @DisplayName("상품을 등록한다. - 200 Ok")
+    @Test
+    void add_Ok() throws Exception {
+        ProductRequest productRequest = new ProductRequest("아이스크림", 1_000, 1, "www.test.com");
+
+        given(productService.save(any()))
+                .willReturn(1L);
+
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productRequest)))
+                .andExpect(status().isCreated());
+        verify(productService, times(1))
+                .save(any());
     }
 }
