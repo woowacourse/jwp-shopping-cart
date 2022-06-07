15975008671d8daa42749d7c4073c83a384f8124
jurlring <78091011+jurlring@users.noreply.github.com>
2022-06-08 02:13:14 +0900

    refactor: argument resolver에서 비회원일 경우 예외 반환하지 않도록 수정

--- src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
+++ src/main/java/woowacourse/auth/ui/AuthenticationPrincipalArgumentResolver.java
@@ -30,11 +30,13 @@ public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArg
         String token = AuthorizationExtractor.extract(webRequest.getNativeRequest(HttpServletRequest.class));
 
         if (token == null || token.isEmpty()) {
-            throw new InvalidTokenException("토큰 정보가 존재하지 않습니다.");
+            //throw new InvalidTokenException("토큰 정보가 존재하지 않습니다.");
+            return 0L;
         }
 
         if (!jwtTokenProvider.validateToken(token)) {
-            throw new InvalidTokenException("유효하지 않거나 만료된 토큰입니다.");
+            //throw new InvalidTokenException("유효하지 않거나 만료된 토큰입니다.");
+            return 0L;
         }
 
         return Long.valueOf(jwtTokenProvider.getPayload(token));
