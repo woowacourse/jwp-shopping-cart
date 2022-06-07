a53b3ebc0384b68eefa0fb04e3d582a0d50e6604
Hongdonggeon <ghd700@naver.com>
2022-06-08 00:44:35 +0900

    refactor: dto @Valid 조건 변경

--- src/main/java/woowacourse/shoppingcart/dto/CustomerUpdationRequest.java
+++ src/main/java/woowacourse/shoppingcart/dto/CustomerUpdationRequest.java
@@ -4,10 +4,10 @@ import javax.validation.constraints.Pattern;
 
 public class CustomerUpdationRequest {
 
-    @Pattern(regexp = "[a-zA-Z0-9가-힣]{2,}", message = "닉네임 양식이 잘못 되었습니다.")
+    @Pattern(regexp = "[a-zA-Z0-9가-힣]{2,8}", message = "1000:닉네임 양식이 잘못 되었습니다.")
     private String nickname;
 
-    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}", message = "비밀번호 양식이 잘못 되었습니다.")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}", message = "1000:비밀번호 양식이 잘못 되었습니다.")
     private String password;
 
     private CustomerUpdationRequest() {
