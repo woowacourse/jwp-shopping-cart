ac1361aaac4089db51b63499b0d28a7b638c6284
meatsby <meatsby@gmail.com>
2022-06-07 22:34:16 +0900

    feat: CartService 레거시 수정 및 추가 기능 구현, Cart 도메인 수량 추가
    - 장바구니 조회 시 재고가 부족할 경우 최대 수량으로 변경하는 기능
    - 장바구니 상품의 수량을 변경하는 기능

--- src/main/java/woowacourse/shoppingcart/application/CartService.java
+++ src/main/java/woowacourse/shoppingcart/application/CartService.java
@@ -10,7 +10,8 @@ import woowacourse.shoppingcart.dao.ProductDao;
 import woowacourse.shoppingcart.domain.Cart;
 import woowacourse.shoppingcart.domain.Product;
 import woowacourse.shoppingcart.domain.customer.Email;
-import woowacourse.shoppingcart.exception.InvalidProductException;
+import woowacourse.shoppingcart.dto.CartRequest;
+import woowacourse.shoppingcart.exception.InvalidCartItemException;
 import woowacourse.shoppingcart.exception.NotInCustomerCartItemException;
 
 @Service
@@ -27,39 +28,59 @@ public class CartService {
         this.productDao = productDao;
     }
 
-    public List<Cart> findCartsByCustomerName(final String customerName) {
-        final List<Long> cartIds = findCartIdsByCustomerName(customerName);
+    public List<Cart> findCartsByEmail(final String email) {
+        final List<Long> cartIds = findCartIdsByEmail(email);
 
         final List<Cart> carts = new ArrayList<>();
         for (final Long cartId : cartIds) {
             final Long productId = cartItemDao.findProductIdById(cartId);
             final Product product = productDao.findProductById(productId);
-            carts.add(new Cart(cartId, product));
+            final int quantity = cartItemDao.findProductQuantityById(cartId);
+            final int stock = product.getStock();
+            optimizeQuantity(email, carts, cartId, productId, product, quantity, stock);
         }
         return carts;
     }
 
-    private List<Long> findCartIdsByCustomerName(final String customerName) {
-        final Long customerId = customerDao.findIdByEmail(new Email(customerName));
+    private List<Long> findCartIdsByEmail(final String email) {
+        final Long customerId = customerDao.findIdByEmail(new Email(email));
         return cartItemDao.findIdsByCustomerId(customerId);
     }
 
-    public Long addCart(final Long productId, final String customerName) {
-        final Long customerId = customerDao.findIdByEmail(new Email(customerName));
-        try {
-            return cartItemDao.addCartItem(customerId, productId);
-        } catch (Exception e) {
-            throw new InvalidProductException();
+    private void optimizeQuantity(String email, List<Cart> carts, Long cartId, Long productId, Product product,
+                                  int quantity,
+                                  int stock) {
+        if (stock < quantity) {
+            modifyCartQuantity(new CartRequest(productId, stock), email);
+            carts.add(new Cart(cartId, product, stock));
+            return;
         }
+        carts.add(new Cart(cartId, product, quantity));
     }
 
-    public void deleteCart(final String customerName, final Long cartId) {
-        validateCustomerCart(cartId, customerName);
+    public Long addCart(final CartRequest cartRequest, final String email) {
+        final Long customerId = customerDao.findIdByEmail(new Email(email));
+        Product product = productDao.findProductById(cartRequest.getProductId());
+        if (product.getStock() < cartRequest.getQuantity()) {
+            throw new InvalidCartItemException("상품의 수량이 부족합니다. 현재 재고 = " + product.getStock());
+        }
+        return cartItemDao.addCartItem(customerId, cartRequest.getProductId(), cartRequest.getQuantity());
+    }
+
+    public List<Cart> modifyCartQuantity(CartRequest cartRequest, String email) {
+        final Long customerId = customerDao.findIdByEmail(new Email(email));
+        cartItemDao.modifyQuantity(customerId, cartRequest.getProductId(), cartRequest.getQuantity());
+        return findCartsByEmail(email);
+    }
+
+    public List<Cart> deleteCart(final String email, final Long cartId) {
+        validateCustomerCart(cartId, email);
         cartItemDao.deleteCartItem(cartId);
+        return findCartsByEmail(email);
     }
 
-    private void validateCustomerCart(final Long cartId, final String customerName) {
-        final List<Long> cartIds = findCartIdsByCustomerName(customerName);
+    private void validateCustomerCart(final Long cartId, final String email) {
+        final List<Long> cartIds = findCartIdsByEmail(email);
         if (cartIds.contains(cartId)) {
             return;
         }
