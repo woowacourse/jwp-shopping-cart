8e53eb0983b8fa58bd46cb5bc04675deea86ee0c
Seongwon97 <osw970325@naver.com>
2022-06-08 01:42:48 +0900

    refactor(GlobalControllerAdvice): 예외 처리를 한 곳에서 하도록 통합

--- src/main/java/woowacourse/globalException/GlobalControllerAdvice.java
+++ src/main/java/woowacourse/globalException/GlobalControllerAdvice.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import woowacourse.auth.exception.InvalidTokenException;
 import woowacourse.globalException.dto.ErrorResponse;
 import woowacourse.member.exception.MemberException;
+import woowacourse.shoppingcart.exception.CartException;
+import woowacourse.shoppingcart.exception.OrderException;
+import woowacourse.shoppingcart.exception.ProductException;
 
 @RestControllerAdvice
 public class GlobalControllerAdvice {
@@ -24,8 +27,13 @@ public class GlobalControllerAdvice {
         return ResponseEntity.badRequest().body(new ErrorResponse(message.toString()));
     }
 
-    @ExceptionHandler(MemberException.class)
-    public ResponseEntity<ErrorResponse> handleMemberException(MemberException e) {
+    @ExceptionHandler({
+            MemberException.class,
+            ProductException.class,
+            CartException.class,
+            OrderException.class
+    })
+    public ResponseEntity<ErrorResponse> handleMemberException(RuntimeException e) {
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
     }
 
