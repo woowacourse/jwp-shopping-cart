aae1f2373e33173112db29dc16093db24f4fcd00
shindong96 <2006peter@naver.com>
2022-06-07 14:56:25 +0900

    fix: Product equals, hashCode 정의

--- src/main/java/woowacourse/shoppingcart/domain/product/Product.java
+++ src/main/java/woowacourse/shoppingcart/domain/product/Product.java
@@ -1,5 +1,7 @@
 package woowacourse.shoppingcart.domain.product;
 
+import java.util.Objects;
+
 public class Product {
     private Long id;
     private String name;
@@ -39,5 +41,20 @@ public class Product {
 
     public Long getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Product))
+            return false;
+        Product product = (Product)o;
+        return getId().equals(product.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }
