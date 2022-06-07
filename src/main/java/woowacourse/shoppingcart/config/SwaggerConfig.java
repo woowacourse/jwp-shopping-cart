983faed8af0be92da76edf06d7515fa4eb4ee233
kth990303 <kth990303@naver.com>
2022-06-07 11:13:49 +0900

    feat: swagger-ui 자동 api 문서화 기능 구현

---
+++ src/main/java/woowacourse/shoppingcart/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package woowacourse.shoppingcart.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.HttpAuthenticationScheme;
+import springfox.documentation.service.SecurityReference;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String REFERENCE = "Bearer 토큰 값";
+
+    @Bean
+    public Docket api() {
+        return new Docket(
+                DocumentationType.OAS_30) // OpenAPI 3.0
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+                .build().securityContexts(List.of(securityContext()))
+                .securitySchemes(List.of(bearerAuthSecurityScheme()));
+    }
+
+    private SecurityContext securityContext() {
+        return springfox.documentation.spi.service.contexts.SecurityContext.builder().securityReferences(defaultAuth())
+                .operationSelector(operationContext -> true).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = new AuthorizationScope("global", "accessEverything");
+        return List.of(new SecurityReference(REFERENCE, authorizationScopes));
+    }
+
+    private HttpAuthenticationScheme bearerAuthSecurityScheme() {
+        return HttpAuthenticationScheme.JWT_BEARER_BUILDER.name(REFERENCE).build();
+    }
+}
