86e4f7692dd8462c910184408a370c9ee7847d88
ulimy <ulim130@gmail.com>
2022-06-07 15:13:43 +0900

    test(ProductAcceptanceTest): 인수테스트에 RestAssuredConvenienceMethod를 이용하도록 수정

--- src/test/java/woowacourse/acceptance/RestAssuredConvenienceMethod.java
+++ src/test/java/woowacourse/acceptance/RestAssuredConvenienceMethod.java
@@ -45,6 +45,15 @@ public class RestAssuredConvenienceMethod {
                 .then().log().all();
     }
 
+    public static ValidatableResponse deleteRequestWithoutToken(String path) {
+        return RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .delete(path)
+                .then().log().all();
+    }
+
     public static ValidatableResponse putRequestWithToken(String accessToken, Object body, String path) {
         return RestAssured.given().log().all()
                 .auth().oauth2(accessToken)
