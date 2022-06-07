be27551d731393d1352bf39878a283fc6b4253ec
progress0407 <progress0407@gmail.com>
2022-06-07 16:50:28 +0900

    feat(LogTraceAspect): db 연결이 되지 않았을 시에 Application을 실행하지 않게 변경

--- src/main/java/woowacourse/Application.java
+++ src/main/java/woowacourse/Application.java
@@ -2,6 +2,7 @@ package woowacourse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Application {
