3f63e4e32ff2e81003c73565e22aac2acf2fcc79
Juhyung990122 <harukawa99@naver.com>
2022-06-08 19:47:56 +0900

    fix: DB cascade 조건 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,3 @@
-drop table if exists orders_detail;
-
-drop table if exists orders;
-
 drop table if exists cart_item;
 
 drop table if exists product;
@@ -41,8 +37,8 @@ create table cart_item
 
 alter table cart_item
     add constraint fk_cart_item_to_customer
-        foreign key (customer_id) references customer (id);
+        foreign key (customer_id) references customer (id) on delete cascade ;
 
 alter table cart_item
     add constraint fk_cart_item_to_product
-        foreign key (product_id) references product (id);
+        foreign key (product_id) references product (id)  on delete cascade ;;
