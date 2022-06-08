617d8454a1f004fa2013a8e48ad5edc3ffeee740
injoon2019 <ison@sfu.ca>
2022-06-08 14:40:46 +0900

    refactor: 스키마에서 테이블 비우던 것 제거

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,3 @@
-set FOREIGN_KEY_CHECKS = 0;
-truncate table customer;
-truncate table product;
-truncate table cart;
-truncate table orders;
-truncate table orders_detail;
-set FOREIGN_KEY_CHECKS = 1;
-
 create table IF NOT EXISTS customer
 (
     id bigint auto_increment not null,
