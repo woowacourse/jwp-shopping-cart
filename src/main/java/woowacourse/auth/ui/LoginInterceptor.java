9a12ddf585b60234a8b502f7f2e05204171634cd
brorae <jhr3239@gmail.com>
2022-06-08 22:06:54 +0900

    refactor: WebConfig 설정

--- src/main/java/woowacourse/auth/ui/LoginInterceptor.java
+++ src/main/java/woowacourse/auth/ui/LoginInterceptor.java
@@ -2,6 +2,7 @@ package woowacourse.auth.ui;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import woowacourse.auth.application.AuthService;
 import woowacourse.auth.support.AuthorizationExtractor;
@@ -21,6 +22,10 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
+
         String token = AuthorizationExtractor.extract(request);
         authService.validateToken(token);
         authService.validateExistUser(jwtTokenProvider.getPayload(token));
