4cb9ad32bd2ca2f974a6534d30fe8d2f76fa9a00
wilgur513 <wilgur513@naver.com>
2022-06-08 15:05:09 +0900

    refactor: json 반환 형식 변경

--- src/main/java/woowacourse/shoppingcart/dto/ProductsResponse.java
+++ src/main/java/woowacourse/shoppingcart/dto/ProductsResponse.java
@@ -6,15 +6,15 @@ import woowacourse.shoppingcart.domain.Product;
 
 public class ProductsResponse {
 
-    private final List<ProductResponse> products;
+    private final List<ProductResponse> productList;
 
-    public ProductsResponse(List<Product> products) {
-        this.products = products.stream()
+    public ProductsResponse(List<Product> productList) {
+        this.productList = productList.stream()
             .map(ProductResponse::new)
             .collect(Collectors.toUnmodifiableList());
     }
 
-    public List<ProductResponse> getProducts() {
-        return products;
+    public List<ProductResponse> getProductList() {
+        return productList;
     }
 }
