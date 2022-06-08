6f8129e59135a1baf225e2e43fe8619ea53ce61f
bcc0830 <bcc0830@naver.com>
2022-06-08 17:11:29 +0900

    fix : 협업 컨벤션에 맞게 Response의 필드명 수정

--- src/main/java/woowacourse/shoppingcart/dto/ProductResponse.java
+++ src/main/java/woowacourse/shoppingcart/dto/ProductResponse.java
@@ -5,7 +5,7 @@ public class ProductResponse {
     private Long id;
     private String name;
     private int price;
-    private String image_url;
+    private String imageUrl;
 
     public ProductResponse() {
     }
@@ -14,7 +14,7 @@ public class ProductResponse {
         this.id = id;
         this.name = name;
         this.price = price;
-        this.image_url = image_url;
+        this.imageUrl = image_url;
     }
 
     public Long getId() {
@@ -30,6 +30,6 @@ public class ProductResponse {
     }
 
     public String getImage_url() {
-        return image_url;
+        return imageUrl;
     }
 }
