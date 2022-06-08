0fbeb9fc20a33d484b4d61d36326382ded0b06bf
Juhyung990122 <harukawa99@naver.com>
2022-06-08 19:56:14 +0900

    test: ProductResponseDto 도입에 따른 테스트코드 수정

--- src/test/java/woowacourse/shoppingcart/acceptance/ProductAcceptanceTest.java
+++ src/test/java/woowacourse/shoppingcart/acceptance/ProductAcceptanceTest.java
@@ -72,8 +72,8 @@ public class ProductAcceptanceTest extends AcceptanceTest {
     }
 
     public static void 상품_목록_포함됨(Long productId1, Long productId2, ExtractableResponse<Response> response) {
-        final List<Long> resultProductIds = response.jsonPath().getList(".", Product.class).stream()
-                .map(Product::getId)
+        final List<Long> resultProductIds = response.jsonPath().getList(".", ProductResponseDto.class).stream()
+                .map(ProductResponseDto::getProductId)
                 .collect(Collectors.toList());
         assertThat(resultProductIds).contains(productId1, productId2);
     }
