b563908bfb60766c8a46c896953827a612aa8da6
cjlee38 <cjlee38@github.com>
2022-06-08 13:59:37 +0900

    test: 테스트 환경 세팅 및 어노테이션 정리

--- src/test/java/woowacourse/support/test/ExtendedJdbcTest.java
+++ src/test/java/woowacourse/support/test/ExtendedJdbcTest.java
@@ -8,13 +8,13 @@ import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.test.context.TestConstructor;
-import org.springframework.test.context.jdbc.Sql;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
+@ExtendedTest
 @JdbcTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Sql(scripts = {"classpath:schema-test.sql", "classpath:data-test.sql"})
 @TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
 public @interface ExtendedJdbcTest {
 }
+
--- src/test/java/woowacourse/support/test/ExtendedTest.java
+++ src/test/java/woowacourse/support/test/ExtendedTest.java
@@ -5,13 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
-@SpringBootTest
-@Transactional
-@ExtendedTest
-public @interface ExtendedApplicationTest {
+@ActiveProfiles("test")
+@Sql(scripts = {"classpath:schema-test.sql", "classpath:data-test.sql"})
+public @interface ExtendedTest {
 }
