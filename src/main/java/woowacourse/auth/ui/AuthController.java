39a345c4f812605b77cf96afd874846bf8c057b3
Hyunta <mohai2618@gmail.com>
2022-06-08 15:32:44 +0900

    refactor: CORS url 수정

--- src/main/java/woowacourse/auth/ui/AuthController.java
+++ src/main/java/woowacourse/auth/ui/AuthController.java
@@ -2,6 +2,7 @@ package woowacourse.auth.ui;
 
 import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import woowacourse.auth.service.AuthService;
 import woowacourse.auth.dto.TokenRequest;
 import woowacourse.auth.dto.TokenResponse;
 
+@CrossOrigin(origins = "http://13.124.173.149:8080")
 @RestController
 public class AuthController {
 
