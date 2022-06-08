b835c77e44a9abd5ed1d14c63c68980f911ce59c
devHudi <devhudi@gmail.com>
2022-06-08 23:22:59 +0900

    fix: preflight 대응 위해 AuthInterceptor preHandle 메소드 수정

--- src/main/java/woowacourse/shoppingcart/controller/AuthInterceptor.java
+++ src/main/java/woowacourse/shoppingcart/controller/AuthInterceptor.java
@@ -2,6 +2,7 @@ package woowacourse.shoppingcart.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import woowacourse.auth.service.AuthService;
@@ -18,6 +19,10 @@ public class AuthInterceptor implements HandlerInterceptor {
     // TODO: PathVariable로 customerId가 제공되지 않을 경우 NPE가 발생함
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
+
         String accessToken = AuthorizationExtractor.extract(request);
         authService.validateToken(accessToken);
         return true;
