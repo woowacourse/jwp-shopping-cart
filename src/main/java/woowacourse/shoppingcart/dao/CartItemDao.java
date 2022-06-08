0774fa35c80271668f9e9505063985b42fe9cf2e
이한울 <dkfghg3@naver.com>
2022-06-08 16:37:10 +0900

    refactor: cart 패키지 분리

--- src/main/java/woowacourse/shoppingcart/dao/CartItemDao.java
+++ src/main/java/woowacourse/shoppingcart/dao/CartItemDao.java
@@ -7,8 +7,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
-import woowacourse.shoppingcart.domain.CartItem;
-import woowacourse.shoppingcart.domain.Quantity;
+import woowacourse.shoppingcart.domain.cart.CartItem;
+import woowacourse.shoppingcart.domain.cart.Quantity;
 import woowacourse.shoppingcart.exception.InvalidCartItemException;
 
 @Repository
--- src/test/java/woowacourse/shoppingcart/domain/cart/QuantityTest.java
+++ src/test/java/woowacourse/shoppingcart/domain/cart/QuantityTest.java
@@ -1,4 +1,4 @@
-package woowacourse.shoppingcart.domain;
+package woowacourse.shoppingcart.domain.cart;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
