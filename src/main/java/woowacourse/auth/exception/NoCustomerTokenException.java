4f77ba1faa0111722d3852a07df6735ffb4bdd51
BeomsooKim <kbsat@naver.com>
2022-06-08 20:38:55 +0900

    refactor: 예외 메세지 변경

--- src/main/java/woowacourse/auth/exception/NoCustomerTokenException.java
+++ src/main/java/woowacourse/auth/exception/NoCustomerTokenException.java
@@ -3,7 +3,7 @@ package woowacourse.auth.exception;
 public class NoCustomerTokenException extends RuntimeException {
 
     public NoCustomerTokenException() {
-        this("이미 존재하지 않는 유저의 토큰입니다.");
+        this("존재하지 않는 유저의 토큰입니다.");
     }
 
     public NoCustomerTokenException(final String msg) {
