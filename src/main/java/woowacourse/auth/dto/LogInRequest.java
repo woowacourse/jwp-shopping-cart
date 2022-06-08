e5014b57ecd5c4d532469aa4087b1c651e907f7f
jaejae-yoo <wotj102@gmail.com>
2022-06-08 16:18:34 +0900

    fix: 비밀번호 정규 표현

--- src/main/java/woowacourse/auth/dto/LogInRequest.java
+++ src/main/java/woowacourse/auth/dto/LogInRequest.java
@@ -12,7 +12,6 @@ public class LogInRequest {
     @Size(max = 64)
     private String email;
     @NotBlank
-    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     @Size(min = 6)
     private String password;
 
