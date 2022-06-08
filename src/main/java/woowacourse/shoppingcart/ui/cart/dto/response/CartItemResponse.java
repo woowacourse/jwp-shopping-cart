183bff55c4e5f1409218b70a0edc6dfd77193fb7
sure-why-not <pkse1213@gmail.com>
2022-06-08 17:54:49 +0900

    refactor: CartItemResponse 에 productId 를 갖도록 변경

--- src/main/java/woowacourse/shoppingcart/ui/cart/dto/response/CartItemResponse.java
+++ src/main/java/woowacourse/shoppingcart/ui/cart/dto/response/CartItemResponse.java
@@ -5,14 +5,16 @@ import woowacourse.shoppingcart.domain.Cart;
 public class CartItemResponse {
 
     private final Long id;
+    private final Long productId;
     private final String name;
     private final int price;
     private final int quantity;
     private final String imageUrl;
 
-    public CartItemResponse(final Long id, final String name, final int price, final int quantity,
+    public CartItemResponse(final Long id, final Long productId, final String name, final int price, final int quantity,
                             final String imageUrl) {
         this.id = id;
+        this.productId = productId;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
@@ -20,12 +22,16 @@ public class CartItemResponse {
     }
 
     public static CartItemResponse from(final Cart cart) {
-        return new CartItemResponse(cart.getId(), cart.getName(), cart.getPrice(), cart.getQuantity(),
-                cart.getImageUrl());
+        return new CartItemResponse(cart.getId(), cart.getProductId(), cart.getName(), cart.getPrice(),
+                cart.getQuantity(), cart.getImageUrl());
     }
 
     public Long getId() {
         return id;
+    }
+
+    public Long getProductId() {
+        return productId;
     }
 
     public String getName() {
