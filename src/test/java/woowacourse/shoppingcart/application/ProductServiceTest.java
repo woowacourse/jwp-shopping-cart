d49026ae7377f4c6eb2d551eec2e2bbf8401b8c1
summerlunaa <summerlunaa9@gmail.com>
2022-06-09 04:20:36 +0900

    test: 잘못된 페이지를 입력한 경우 테스트 추가

--- src/test/java/woowacourse/shoppingcart/application/ProductServiceTest.java
+++ src/test/java/woowacourse/shoppingcart/application/ProductServiceTest.java
@@ -1,11 +1,13 @@
 package woowacourse.shoppingcart.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.jdbc.Sql;
 import woowacourse.shoppingcart.domain.Product;
 import woowacourse.shoppingcart.dto.product.PageRequest;
@@ -21,14 +23,24 @@ class ProductServiceTest {
         this.productService = productService;
     }
 
-    @DisplayName("상품 목록을 조회한다.")
+    @DisplayName("페이지별 상품 목록을 조회한다.")
     @Test
-    void products() {
+    void findByPage() {
         final PageRequest pageRequest = new PageRequest(3, 2);
 
         assertThat(productService.findByPage(pageRequest))
                 .hasSize(2)
                 .extracting(Product::getId)
                 .contains(5L, 6L);
+    }
+
+    @DisplayName("페이지별 상품 목록을 조회할때 잘못된 페이지를 입력한 경우 예외가 발생한다.")
+    @Test
+    void findByInvalidPage() {
+        final PageRequest pageRequest = new PageRequest(10, 10);
+
+        assertThatThrownBy(() -> productService.findByPage(pageRequest))
+                .isInstanceOf(EmptyResultDataAccessException.class)
+                .hasMessage("잘못된 페이지입니다.");
     }
 }
