90fe44e7a02499eec633fe35ab971a7e40143ab1
meatsby <meatsby@gmail.com>
2022-06-08 16:36:58 +0900

    refactor: CORS 헤더 접근 권한 부여

--- src/main/java/woowacourse/shoppingcart/config/WebConfig.java
+++ src/main/java/woowacourse/shoppingcart/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
-                .exposedHeaders(HttpHeaders.LOCATION);
+                .exposedHeaders(HttpHeaders.LOCATION, "x-total-count");
     }
 }
