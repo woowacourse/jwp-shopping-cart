9325399a719aaaa51dfc99055b25733075048d19
BeomsooKim <kbsat@naver.com>
2022-06-09 01:16:36 +0900

    feat: Amount 도메인 생성 및 테스트

---
+++ src/main/java/woowacourse/shoppingcart/domain/Amount.java
@@ -0,0 +1,24 @@
+package woowacourse.shoppingcart.domain;
+
+public class Amount {
+
+    private static final int MIN_VALUE = 0;
+
+    private final int value;
+
+    public Amount(int value) {
+        validateAmount(value);
+
+        this.value = value;
+    }
+
+    private void validateAmount(int value) {
+        if (value < MIN_VALUE) {
+            throw new IllegalArgumentException("개수는 0 미만일 수 없습니다.");
+        }
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
