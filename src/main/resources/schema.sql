d9ba4fcd5d9014794fac32646c8db97c4a69bbf5
awesomeo184 <awesomeo184@gmail.com>
2022-06-08 19:12:18 +0900

    feat: schema에 foreign key cascade 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -43,11 +43,13 @@ create table cart_item
 
 alter table cart_item
     add constraint fk_cart_item_to_customer
-        foreign key (customer_id) references customer (id);
+        foreign key (customer_id) references customer (id)
+            on delete cascade on delete cascade;
 
 alter table cart_item
     add constraint fk_cart_item_to_product
-        foreign key (product_id) references product (id);
+        foreign key (product_id) references product (id)
+            on delete cascade on delete cascade;
 
 create table orders
 (
