b9eff0848440f5a77d8fd2c82c8d74d1ef9ac2b9
yoon <duddbs774@gmail.com>
2022-06-09 10:31:07 +0900

    refactor: dto 생성자 수정

--- src/main/java/woowacourse/shoppingcart/dto/ChangePasswordRequest.java
+++ src/main/java/woowacourse/shoppingcart/dto/ChangePasswordRequest.java
@@ -6,10 +6,13 @@ public class ChangePasswordRequest {
     private static final String INVALID_PASSWORD = "[ERROR] 비밀번호는 공백 또는 빈 값일 수 없습니다.";
 
     @NotBlank(message = INVALID_PASSWORD)
-    private final String password;
+    private String password;
 
     @NotBlank(message = INVALID_PASSWORD)
-    private final String newPassword;
+    private String newPassword;
+
+    public ChangePasswordRequest() {
+    }
 
     public ChangePasswordRequest(String password, String newPassword) {
         this.password = password;
