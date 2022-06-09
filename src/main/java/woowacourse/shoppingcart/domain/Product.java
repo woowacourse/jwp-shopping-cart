eadca037f466f46457bc4ed908977e3eea8e0bfd
yukong <kyb062666@gmail.com>
2022-06-09 16:16:44 +0900

    feat: Product 도메인 객체 생성 시 가격 예외처리 추가

--- src/main/java/woowacourse/shoppingcart/domain/Product.java
+++ src/main/java/woowacourse/shoppingcart/domain/Product.java
@@ -1,15 +1,19 @@
 package woowacourse.shoppingcart.domain;
 
-public class Product {
-    private Long id;
-    private String name;
-    private Integer price;
-    private String imageUrl;
+import woowacourse.shoppingcart.exception.dataformat.ProductDataFormatException;
 
-    public Product() {
-    }
+public class Product {
+
+    private static final int MINIMUM_PRICE = 1;
+    private static final int MONEY_UNIT = 10;
+
+    private final Long id;
+    private final String name;
+    private final Integer price;
+    private final String imageUrl;
 
     public Product(final Long id, final String name, final int price, final String imageUrl) {
+        validatePrice(price);
         this.id = id;
         this.name = name;
         this.price = price;
@@ -18,6 +22,15 @@ public class Product {
 
     public Product(final String name, final int price, final String imageUrl) {
         this(null, name, price, imageUrl);
+    }
+
+    private void validatePrice(final int price) {
+        if (price < MINIMUM_PRICE) {
+            throw new ProductDataFormatException("상품의 가격은 0 원 이상이어야 합니다.");
+        }
+        if (price % MONEY_UNIT != 0) {
+            throw new ProductDataFormatException("상품의 가격은 10원 단위로 입력해주세요.");
+        }
     }
 
     public String getName() {
