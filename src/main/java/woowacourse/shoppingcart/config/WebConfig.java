08e9244afa412ded4d21e8e08daaa3128c6a5d62
cjlee38 <cjlee38@github.com>
2022-06-09 09:09:19 +0900

    refactor: allow origin 를 localhost:3000

--- src/main/java/woowacourse/shoppingcart/config/WebConfig.java
+++ src/main/java/woowacourse/shoppingcart/config/WebConfig.java
@@ -8,13 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    public static final String ALLOW_ORIGINS = "http://localhost:3000";
     public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
             .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
-            .allowedOrigins("*")
+            .allowedOrigins(ALLOW_ORIGINS)
             .exposedHeaders(HttpHeaders.LOCATION)
             .maxAge(60L * 60L * 12L);
     }
