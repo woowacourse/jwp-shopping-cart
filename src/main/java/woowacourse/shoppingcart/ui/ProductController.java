03242cb49f2d951f870cb28bfe20b81f09c45178
yukong <kyb062666@gmail.com>
2022-06-09 16:17:20 +0900

    refactor: 사용하지 않는 API 삭제

--- src/main/java/woowacourse/shoppingcart/ui/ProductController.java
+++ src/main/java/woowacourse/shoppingcart/ui/ProductController.java
@@ -1,15 +1,10 @@
 package woowacourse.shoppingcart.ui;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import woowacourse.shoppingcart.application.dto.response.ProductResponse;
-import woowacourse.shoppingcart.domain.Product;
-import woowacourse.shoppingcart.application.dto.request.Request;
 import woowacourse.shoppingcart.application.ProductService;
 
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -21,16 +16,6 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @PostMapping("/api/products")
-    public ResponseEntity<Void> add(@Validated(Request.allProperties.class) @RequestBody final Product product) {
-        final Long productId = productService.addProduct(product);
-        final URI uri = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/" + productId)
-                .build().toUri();
-        return ResponseEntity.created(uri).build();
-    }
-
     @GetMapping("/products/{productId}")
     public ResponseEntity<ProductResponse> findById(@PathVariable final Long productId) {
         return ResponseEntity.ok(productService.findById(productId));
@@ -39,11 +24,5 @@ public class ProductController {
     @GetMapping(value = "/products", params = {"page", "limit"})
     public ResponseEntity<List<ProductResponse>> findProductsInPage(@RequestParam final Long page, @RequestParam final Long limit) {
         return ResponseEntity.ok(productService.findProductsInPage(page, limit));
-    }
-
-    @DeleteMapping("/api/products/{productId}")
-    public ResponseEntity<Void> delete(@PathVariable final Long productId) {
-        productService.deleteProductById(productId);
-        return ResponseEntity.noContent().build();
     }
 }
