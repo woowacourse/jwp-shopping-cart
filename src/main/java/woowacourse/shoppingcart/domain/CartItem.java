e506039a53cdc9ed96f75960bd5557dfe91aaf9d
awesomeo184 <awesomeo184@gmail.com>
2022-06-09 16:43:56 +0900

    refactor(CartItem): 사용하지 않는 생성자 제거

--- src/main/java/woowacourse/shoppingcart/domain/CartItem.java
+++ src/main/java/woowacourse/shoppingcart/domain/CartItem.java
@@ -9,21 +9,6 @@ public class CartItem {
     private int quantity;
     private String imageUrl;
 
-    public CartItem() {
-    }
-
-    public CartItem(final Long id, final Product product) {
-        this(id, product.getId(), product.getName(), product.getPrice(), product.getImageUrl());
-    }
-
-    public CartItem(final Long id, final Long productId, final String name, final int price, final String imageUrl) {
-        this.id = id;
-        this.productId = productId;
-        this.name = name;
-        this.price = price;
-        this.imageUrl = imageUrl;
-    }
-
     public CartItem(Long id, Long productId, String name, int price, int quantity, String imageUrl) {
         this.id = id;
         this.productId = productId;
