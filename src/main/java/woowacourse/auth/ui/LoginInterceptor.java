dca102870c9ba65e0c4cfef38ec7e7412ecca5a7
ozragwort <ozragwort@gmail.com>
2022-06-09 09:47:29 +0900

    refactor: Preflight 옵션 체크 추가

--- src/main/java/woowacourse/auth/ui/LoginInterceptor.java
+++ src/main/java/woowacourse/auth/ui/LoginInterceptor.java
@@ -2,6 +2,7 @@ package woowacourse.auth.ui;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import woowacourse.auth.support.AuthorizationExtractor;
 import woowacourse.auth.support.JwtTokenProvider;
@@ -19,11 +20,19 @@ public class LoginInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
             throws Exception {
+        if (isPreflight(request)) {
+            return true;
+        }
+
         String token = AuthorizationExtractor.extract(request);
         validateToken(token);
         request.setAttribute(ACCESS_TOKEN, token);
 
         return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+
+    private boolean isPreflight(HttpServletRequest request) {
+        return HttpMethod.OPTIONS.matches(request.getMethod());
     }
 
     private void validateToken(String token) {
