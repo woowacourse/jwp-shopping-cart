2a883249851916f6453a36c53ecd68deed6e08ad
yukong <kyb062666@gmail.com>
2022-06-09 16:30:40 +0900

    fix: 삭제한 API 중에서 다른 파일에서 호출하는 경우 원복

--- src/main/java/woowacourse/shoppingcart/ui/ProductController.java
+++ src/main/java/woowacourse/shoppingcart/ui/ProductController.java
@@ -1,10 +1,16 @@
 package woowacourse.shoppingcart.ui;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import woowacourse.shoppingcart.application.dto.request.ProductRequest;
+import woowacourse.shoppingcart.application.dto.request.Request;
 import woowacourse.shoppingcart.application.dto.response.ProductResponse;
 import woowacourse.shoppingcart.application.ProductService;
+import woowacourse.shoppingcart.domain.Product;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,6 +20,16 @@ public class ProductController {
 
     public ProductController(final ProductService productService) {
         this.productService = productService;
+    }
+
+    @PostMapping("/api/products")
+    public ResponseEntity<Void> add(@Validated(Request.allProperties.class) @RequestBody final ProductRequest productRequest) {
+        final Long productId = productService.addProduct(productRequest);
+        final URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/" + productId)
+                .build().toUri();
+        return ResponseEntity.created(uri).build();
     }
 
     @GetMapping("/products/{productId}")
