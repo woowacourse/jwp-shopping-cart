7bd04ac7442f25bd3667e9bad5bfba8ff47a6079
Chris <bjuuuu98@gmail.com>
2022-06-09 17:08:08 +0900

    refactor: 동일한 테스트 환경 @beforeEach로 분리

--- src/test/java/woowacourse/member/dao/MemberDaoTest.java
+++ src/test/java/woowacourse/member/dao/MemberDaoTest.java
@@ -1,7 +1,6 @@
 package woowacourse.member.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
 
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -22,6 +21,8 @@ import woowacourse.member.domain.Member;
 @AutoConfigureTestDatabase(replace = Replace.NONE)
 class MemberDaoTest {
 
+    private static final Member MEMBER = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
+
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
@@ -30,24 +31,13 @@ class MemberDaoTest {
     @BeforeEach
     void beforeEach() {
         memberDao = new MemberDao(jdbcTemplate);
-    }
-
-    @DisplayName("회원 정보를 저장한다.")
-    @Test
-    void save() {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-
-        assertThatCode(() -> memberDao.save(member))
-                .doesNotThrowAnyException();
+        memberDao.save(MEMBER);
     }
 
     @DisplayName("이메일이 존재하는지 반환한다.")
     @ParameterizedTest
     @CsvSource({"abc@woowahan.com, true", "abc@naver.com, false"})
     void existsEmail(String email, boolean expected) {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         boolean actual = memberDao.existsEmail(email);
 
         assertThat(actual).isEqualTo(expected);
@@ -57,9 +47,6 @@ class MemberDaoTest {
     @ParameterizedTest
     @CsvSource({"1, true", "2, false"})
     void checkIdExistence(long id, boolean expected) {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         boolean actual = memberDao.checkIdExistence(id);
 
         assertThat(actual).isEqualTo(expected);
@@ -68,21 +55,15 @@ class MemberDaoTest {
     @DisplayName("회원 id로 회원정보를 조회한다.")
     @Test
     void findByEmail() {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         Member foundMember = memberDao.findById(1L);
 
-        assertThat(foundMember).isEqualTo(member);
+        assertThat(foundMember).isEqualTo(MEMBER);
     }
 
     @DisplayName("이메일과 비밀번호에 해당하는 회원정보가 없을 경우 empty인 Optional을 반환한다.")
     @ParameterizedTest
     @CsvSource({"abc@woowahan.com, 1q2w3e4r@", "abc@naver.com, 1q2w3e4r!", "abc@naver.com, 1q2w3e4r@"})
     void findByEmailAndPassword_Null(String email, String password) {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         Optional<Member> foundMember = memberDao.findByEmailAndPassword(email, password);
         boolean actual = foundMember.isEmpty();
 
@@ -93,9 +74,6 @@ class MemberDaoTest {
     @ParameterizedTest
     @CsvSource({"1q2w3e4r!, true", "1q2w3e4r@, false"})
     void checkPassword(String password, boolean expected) {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         boolean actual = memberDao.checkPassword(1L, password);
 
         assertThat(actual).isEqualTo(expected);
@@ -104,9 +82,6 @@ class MemberDaoTest {
     @DisplayName("회원 id에 해당하는 회원의 닉네임을 변경한다.")
     @Test
     void updateNicknameById() {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         memberDao.updateNicknameById(1L, "바꾼닉네임");
         Member foundMember = memberDao.findById(1L);
 
@@ -116,9 +91,6 @@ class MemberDaoTest {
     @DisplayName("회원 id에 해당하는 회원의 비밀번호를 변경한다.")
     @Test
     void updatePasswordById() {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         memberDao.updatePasswordById(1L, "1q2w3e4r@");
         Member foundMember = memberDao.findById(1L);
 
@@ -128,11 +100,8 @@ class MemberDaoTest {
     @DisplayName("회원 id에 해당하는 회원을 삭제한다.")
     @Test
     void deleteById() {
-        Member member = new Member("abc@woowahan.com", "1q2w3e4r!", "닉네임");
-        memberDao.save(member);
-
         memberDao.deleteById(1L);
-        boolean actual = memberDao.existsEmail(member.getEmail());
+        boolean actual = memberDao.existsEmail(MEMBER.getEmail());
 
         assertThat(actual).isFalse();
     }
