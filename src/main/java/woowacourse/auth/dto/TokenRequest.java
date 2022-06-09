6247c4bcdcd295bfa17c55a6fb007c0db6200626
Hongdonggeon <ghd700@naver.com>
2022-06-09 14:41:12 +0900

    refactor: 메서드 체이닝 수정

--- src/main/java/woowacourse/auth/dto/TokenRequest.java
+++ src/main/java/woowacourse/auth/dto/TokenRequest.java
@@ -12,7 +12,7 @@ public class TokenRequest {
     @Pattern(regexp = "^(?=.*\\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,20}", message = "1000:비밀번호 양식이 잘못 되었습니다.")
     private String password;
 
-    public TokenRequest() {
+    private TokenRequest() {
     }
 
     public TokenRequest(String email, String password) {
