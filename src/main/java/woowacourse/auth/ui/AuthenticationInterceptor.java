d219af56315742cf05123387cf843181b2c33c6b
이한울 <dkfghg3@naver.com>
2022-06-09 14:27:33 +0900

    fix: preflight 요청으로 발생하는 cors 이슈 해결

--- src/main/java/woowacourse/auth/ui/AuthenticationInterceptor.java
+++ src/main/java/woowacourse/auth/ui/AuthenticationInterceptor.java
@@ -2,6 +2,7 @@ package woowacourse.auth.ui;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import woowacourse.auth.exception.InvalidTokenException;
 import woowacourse.auth.support.AuthorizationExtractor;
@@ -17,6 +18,9 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        if (HttpMethod.OPTIONS.matches(request.getMethod())) {
+            return true;
+        }
         String token = AuthorizationExtractor.extract(request);
         if (jwtTokenProvider.isValidToken(token)) {
             return true;
