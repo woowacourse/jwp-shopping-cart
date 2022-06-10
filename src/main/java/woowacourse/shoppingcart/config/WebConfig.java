fa28a9884326009d5d01ec724ffe58ba88c812d5
cjlee38 <cjlee38@github.com>
2022-06-10 13:59:37 +0900

    refactor: HttpMethod를 활용하여 허용 가능 메소드 설정

--- src/main/java/woowacourse/shoppingcart/config/WebConfig.java
+++ src/main/java/woowacourse/shoppingcart/config/WebConfig.java
@@ -1,7 +1,10 @@
 package woowacourse.shoppingcart.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,14 +12,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     public static final String ALLOW_ORIGINS = "http://localhost:3000";
-    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
 
     @Override
     public void addCorsMappings(final CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+            .allowedMethods(getAllowedMethods())
             .allowedOrigins(ALLOW_ORIGINS)
             .exposedHeaders(HttpHeaders.LOCATION)
             .maxAge(60L * 60L * 12L);
+    }
+
+    private String[] getAllowedMethods() {
+        return Arrays.stream(HttpMethod.values())
+            .map(Enum::name)
+            .toArray(String[]::new);
     }
 }
--- src/test/java/woowacourse/shoppingcart/config/WebConfigTest.java
+++ src/test/java/woowacourse/shoppingcart/config/WebConfigTest.java
@@ -5,11 +5,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static woowacourse.shoppingcart.config.WebConfig.*;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -28,8 +32,14 @@ public class WebConfigTest {
             )
             .andExpect(status().isOk())
             .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, ALLOW_ORIGINS))
-            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, ALLOWED_METHOD_NAMES))
+            .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, getAllowedMethodsString()))
             .andExpect(header().string(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, HttpHeaders.LOCATION))
             .andDo(print());
+    }
+
+    private String getAllowedMethodsString() {
+        return Arrays.stream(HttpMethod.values())
+            .map(Enum::name)
+            .collect(Collectors.joining(","));
     }
 }
