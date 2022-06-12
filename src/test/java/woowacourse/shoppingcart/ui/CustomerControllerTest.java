1a63495f018ee053f75bfc20555895934493f8cf
2yujeong <yjfflq4872@gmail.com>
2022-06-13 03:11:27 +0900

    feat: Product API Spring REST Docs 적용

--- src/test/java/woowacourse/shoppingcart/ui/CustomerControllerTest.java
+++ src/test/java/woowacourse/shoppingcart/ui/CustomerControllerTest.java
@@ -3,7 +3,6 @@ package woowacourse.shoppingcart.ui;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
