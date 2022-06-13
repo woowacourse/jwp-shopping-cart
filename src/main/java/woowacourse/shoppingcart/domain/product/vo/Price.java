6f245543c2fb044f9c6e6021000168bf9e54084a
yeon-06 <rnjstldus2@gmail.com>
2022-06-13 21:25:15 +0900

    refactor: 하드코딩 제거

--- src/main/java/woowacourse/shoppingcart/domain/product/vo/Price.java
+++ src/main/java/woowacourse/shoppingcart/domain/product/vo/Price.java
@@ -14,7 +14,7 @@ public class Price {
     }
 
     private void validate(int value) {
-        if (value <= 0) {
+        if (value < MIN_VALUE) {
             throw new IllegalArgumentException(
                     String.format("가격은 %d원 이상이어야 합니다. 입력값: %d", MIN_VALUE, value));
         }
