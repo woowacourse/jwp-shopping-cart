f03fee2c4958953b5e6c9d11ea945e0bc4febd85
ulimy <ulim130@gmail.com>
2022-06-14 16:08:08 +0900

    test(CartServiceTest) : 정상작동 테스트 추가

--- src/test/java/woowacourse/shoppingcart/application/CartServiceTest.java
+++ src/test/java/woowacourse/shoppingcart/application/CartServiceTest.java
@@ -7,10 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.jdbc.Sql;
 import woowacourse.member.exception.MemberNotFoundException;
+import woowacourse.shoppingcart.domain.Cart;
 import woowacourse.shoppingcart.dto.AddCartItemRequest;
 import woowacourse.shoppingcart.dto.PutCartItemRequest;
 import woowacourse.shoppingcart.exception.cart.NotInMemberCartException;
 import woowacourse.shoppingcart.exception.product.ProductNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -30,6 +34,15 @@ class CartServiceTest {
     @DisplayName("findCartsByMemberId메서드는")
     @Nested
     class findCartsByMemberId {
+
+        @DisplayName("id로 해당 사용자의 장바구니를 찾아 반환환다.")
+        @Test
+        void findCartsByMemberId() {
+            List<Cart> carts = cartService.findCartsByMemberId(1L);
+            assertThat(carts).isNotEmpty();
+        }
+
+
         @DisplayName("유효하지 않은 사용자인 경우 예외가 발생한다.")
         @Test
         void notExistMemberId() {
@@ -43,6 +56,35 @@ class CartServiceTest {
     @DisplayName("addCartItem메서드는")
     @Nested
     class addCartItem {
+
+        @DisplayName("장바구니에 해당상품이 존재하지 않는다면 새롭게 추가한다.")
+        @Test
+        void addCartItem() {
+            cartService.addCartItem(5L, new AddCartItemRequest(10L));
+
+            Optional<Cart> cart = cartService.findCartsByMemberId(5L)
+                    .stream()
+                    .filter(c -> c.getProductId() == 10L)
+                    .findFirst();
+
+            assertThat(cart).isNotEmpty();
+            assertThat(cart.get().getQuantity()).isEqualTo(1);
+        }
+
+        @DisplayName("장바구니에 해당 상품이 존재한다면 수량만 추가한다.")
+        @Test
+        void alreadyExistProduct() {
+            // given
+            cartService.addCartItem(2L, new AddCartItemRequest(1L));
+            int beforeQuantity = cartService.findCartsByMemberId(2L).get(0).getQuantity();
+
+            // when
+            cartService.addCartItem(2L, new AddCartItemRequest(1L));
+            int afterQuantity = cartService.findCartsByMemberId(2L).get(0).getQuantity();
+
+            // then
+            assertThat(afterQuantity).isEqualTo(beforeQuantity + 1);
+        }
 
         @DisplayName("유효하지 않은 사용자인 경우 예외가 발생한다.")
         @Test
@@ -61,27 +103,26 @@ class CartServiceTest {
             ).isInstanceOf(ProductNotFoundException.class)
                     .hasMessageContaining("존재하지 않는 상품입니다.");
         }
-
-        @DisplayName("이미 장바구니에 해당 상품이 존재한다면 수량만 추가한다.")
-        @Test
-        void alreadyExistProduct() {
-            // given
-            cartService.addCartItem(2L, new AddCartItemRequest(1L));
-            int beforeQuantity = cartService.findCartsByMemberId(2L).get(0).getQuantity();
-
-            // when
-            cartService.addCartItem(2L, new AddCartItemRequest(1L));
-            int afterQuantity = cartService.findCartsByMemberId(2L).get(0).getQuantity();
-
-            // then
-            assertThat(afterQuantity).isEqualTo(beforeQuantity + 1);
-        }
     }
 
 
     @DisplayName("updateCartItem메서드는")
     @Nested
     class updateCartItem {
+
+        @DisplayName("수량을 수정한다.")
+        @Test
+        void updateCartItem() {
+            cartService.updateCartItem(1L, 1L, new PutCartItemRequest(50));
+
+            Optional<Cart> cart = cartService.findCartsByMemberId(1L)
+                    .stream()
+                    .filter(c -> c.getId() == 1L)
+                    .findFirst();
+
+            assertThat(cart).isNotEmpty();
+            assertThat(cart.get().getQuantity()).isEqualTo(50);
+        }
 
         @DisplayName("유효하지 않은 사용자인 경우 예외가 발생한다.")
         @Test
@@ -105,6 +146,19 @@ class CartServiceTest {
     @DisplayName("deleteCart메서드는")
     @Nested
     class deleteCart {
+
+        @DisplayName("장바구니를 삭제한다.")
+        @Test
+        void deleteCart() {
+            cartService.deleteCart(1L, 1L);
+
+            Optional<Cart> cart = cartService.findCartsByMemberId(1L)
+                    .stream()
+                    .filter(c -> c.getId() == 1L)
+                    .findFirst();
+
+            assertThat(cart).isEmpty();
+        }
 
         @DisplayName("유효하지 않은 사용자인 경우 예외가 발생한다.")
         @Test
