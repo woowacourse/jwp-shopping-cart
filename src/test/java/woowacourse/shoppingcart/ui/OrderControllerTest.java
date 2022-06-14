838dc701736f2e4c7b4979e790663ec73d87ae81
ulimy <ulim130@gmail.com>
2022-06-14 17:05:41 +0900

    test : 엣지케이스 추가

--- src/test/java/woowacourse/shoppingcart/ui/OrderControllerTest.java
+++ src/test/java/woowacourse/shoppingcart/ui/OrderControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -41,17 +42,42 @@ class OrderControllerTest {
         @DisplayName("장바구니 번호에 빈값은 허용하지 않는다.")
         @ParameterizedTest
         @NullSource
-        void addWithNullName(Long cartId) throws Exception {
+        void addWithNullCartId(Long cartId) throws Exception {
             List<OrderRequest> request = List.of(new OrderRequest(cartId, 3));
+            System.out.println(objectMapper.writeValueAsString(request));
             assertBadRequestFromPost("/api/members/me/orders", request, "장바구니 번호는 빈값일 수 없습니다.");
         }
 
-        @DisplayName("수량에 1이상의 정수만 허용한다.")
+        @DisplayName("장바구니 번호가 long의 범위를 넘어가는 경우는 허용하지 않는다.")
         @Test
-        void negativeProductId() throws Exception {
-            List<OrderRequest> request = List.of(new OrderRequest(1L, 0));
+        void maxCartId() throws Exception {
+            assertBadRequestFromPost("/api/members/me/orders", "[{\"cartId\":9223372036854775808,\"quantity\":1}]", "out of range of long");
+        }
+
+        @DisplayName("수량에 1이상의 정수만 허용한다.")
+        @ParameterizedTest
+        @ValueSource(ints = {-1, 0})
+        void negativeQuantity(int quantity) throws Exception {
+            List<OrderRequest> request = List.of(new OrderRequest(1L, quantity));
             assertBadRequestFromPost("/api/members/me/orders", request, "수량은 1이상이어야 합니다.");
         }
+
+        @DisplayName("수량이 int의 범위를 넘어가는 경우는 허용하지 않는다.")
+        @Test
+        void maxQuantity() throws Exception {
+            assertBadRequestFromPost("/api/members/me/orders", "[{\"cartId\":1,\"quantity\":2147483648}]", "out of range of int");
+        }
+    }
+
+    void assertBadRequestFromPost(String uri, String request, String errorMessage) throws Exception {
+        String token = jwtTokenProvider.createToken("1");
+
+        mockMvc.perform(post(uri)
+                        .header("Authorization", "Bearer " + token)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString(errorMessage)));
     }
 
     void assertBadRequestFromPost(String uri, Object request, String errorMessage) throws Exception {
--- src/test/java/woowacourse/shoppingcart/ui/ProductControllerTest.java
+++ src/test/java/woowacourse/shoppingcart/ui/ProductControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,10 +37,18 @@ public class ProductControllerTest {
     }
 
     @DisplayName("상품추가 - 금액은 양의 정수만 허용한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1000, 0})
+    void addWithNegative(int price) throws Exception {
+        AddProductRequest request = new AddProductRequest("이름", price, "이미지주소");
+        System.out.println(objectMapper.writeValueAsString(request));
+        assertBadRequestFromPost(request, "금액은 양의 정수만 허용합니다.");
+    }
+
+    @DisplayName("상품추가 - 금액이 int의 범위를 넘어가는 경우는 허용하지 않는다.")
     @Test
-    void addWithNegative() throws Exception{
-        AddProductRequest request = new AddProductRequest("이름",-1000,"이미지주소");
-        assertBadRequestFromPost(request,"금액은 양의 정수만 허용합니다.");
+    void maxPrice() throws Exception {
+        assertBadRequestFromPost("{\"name\":\"이름\",\"price\":2147483648,\"imageUrl\":\"이미지주소\"}", "out of range of int");
     }
 
     @DisplayName("상품추가 - 이미지 주소에 빈값은 허용하지 않는다.")
@@ -48,6 +57,14 @@ public class ProductControllerTest {
     void addWithNullImageUrl(String imageUrl) throws Exception {
         AddProductRequest request = new AddProductRequest("이름", 1000, imageUrl);
         assertBadRequestFromPost(request, "이미지 주소는 빈 값일 수 없습니다.");
+    }
+
+    void assertBadRequestFromPost(String request, String errorMessage) throws Exception {
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(request)
+                ).andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message", containsString(errorMessage)));
     }
 
     void assertBadRequestFromPost(Object request, String errorMessage) throws Exception {
