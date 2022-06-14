4d1fe44db8ed33c3c3b09e8aa24f955c47b1d698
ulimy <ulim130@gmail.com>
2022-06-14 13:55:56 +0900

    refactor(ControllerAdvice): 예외처리 로직 통일화

--- src/main/java/woowacourse/globalException/ControllerAdvice.java
+++ src/main/java/woowacourse/globalException/ControllerAdvice.java
@@ -2,12 +2,12 @@ package woowacourse.globalException;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import woowacourse.auth.exception.InvalidTokenException;
 import woowacourse.globalException.dto.ErrorResponse;
@@ -23,18 +23,21 @@ import javax.validation.ConstraintViolationException;
 public class ControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> parameterException(MethodArgumentNotValidException e) {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleParameter(final MethodArgumentNotValidException e) {
         StringBuilder message = new StringBuilder();
 
         for (FieldError error : e.getFieldErrors()) {
             message.append(error.getDefaultMessage()).append(" ");
         }
-        return ResponseEntity.badRequest().body(new ErrorResponse(message.toString()));
+
+        return new ErrorResponse(message.toString());
     }
 
     @ExceptionHandler(BindException.class)
-    public ResponseEntity<ErrorResponse> bindException(BindException e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getFieldError().getDefaultMessage()));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBind() {
+        return new ErrorResponse("바인딩에 실패했습니다.");
     }
 
     @ExceptionHandler({
@@ -43,42 +46,41 @@ public class ControllerAdvice {
             OrderException.class,
             ProductException.class
     })
-    public ResponseEntity<ErrorResponse> handleMemberException(Exception e) {
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleBusiness(final Exception e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(InvalidTokenException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidMemberException(InvalidTokenException e) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(e.getMessage()));
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public ErrorResponse handleInvalidToken(final InvalidTokenException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity handle() {
-        return ResponseEntity.badRequest().body("존재하지 않는 데이터 요청입니다.");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleEmptyData() {
+        return new ErrorResponse("존재하지 않는 데이터 요청입니다.");
     }
 
     @ExceptionHandler({
             HttpMessageNotReadableException.class,
             ConstraintViolationException.class,
     })
-    public ResponseEntity handleInvalidRequest(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidRequest(final RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(InvalidOrderException.class)
-    public ResponseEntity handleInvalidAccess(final RuntimeException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
-    }
-
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity handleUnhandledException(final RuntimeException e) {
-        e.printStackTrace();
-        return ResponseEntity.badRequest().body("Unhandled Exception");
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleInvalidAccess(final RuntimeException e) {
+        return new ErrorResponse(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorResponse> handlerException(Exception e) {
-        e.printStackTrace();
-        return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleUnhandledException() {
+        return new ErrorResponse("Unhandled Exception");
     }
 }
