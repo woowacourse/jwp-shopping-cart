8f3ae861da84fe24e7f6d2822795d869797095aa
hyunrrr <whgusrms96@gmail.com>
2022-06-14 17:21:52 +0900

    refactor(CartItem): delete setter method

--- src/main/java/woowacourse/shoppingcart/domain/CartItem.java
+++ src/main/java/woowacourse/shoppingcart/domain/CartItem.java
@@ -17,15 +17,7 @@ public class CartItem {
         return product;
     }
 
-    public void setProduct(Product product) {
-        this.product = product;
-    }
-
     public int getQuantity() {
         return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
     }
 }
