84591eff5e4ff0e0bf9ed4705e839d2860a494e2
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-25 20:05:58 +0900

    feat: 예외 공통 처리 advice 추가

---
+++ src/main/java/cart/controller/ItemControllerAdvice.java
@@ -0,0 +1,39 @@
+package cart.controller;
+
+import cart.controller.dto.ExceptionResponse;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ItemControllerAdvice extends ResponseEntityExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ItemControllerAdvice.class);
+    private static final String INTERNAL_ERROR_MESSAGE = "예상치 못한 예외가 발생했습니다.";
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> fieldErrors = ex.getBindingResult().getFieldErrors()
+                .stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        return ResponseEntity.badRequest().body(new ExceptionResponse<>(fieldErrors));
+    }
+
+    @ExceptionHandler(Exception.class)
+    private ResponseEntity<ExceptionResponse> handleException(Exception e) {
+        LOGGER.error(e.getMessage());
+        return ResponseEntity.internalServerError().body(new ExceptionResponse(INTERNAL_ERROR_MESSAGE));
+    }
+}
