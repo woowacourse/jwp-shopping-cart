30f3af8e3e0494127bb60107b0722af7503de2bf
ReO <yt930310@gmail.com>
2023-04-25 16:31:58 +0900

    refactor: entity 설계 변경

--- src/main/java/domain/Product.java
+++ src/main/java/domain/Product.java
@@ -1,13 +1,40 @@
 package domain;
 public class Product {
+    private final Long id;
     private final String name;
     private final String imageURL;
     private final int price;
 
-    public Product(String name, String imageURL, int price) {
-        this.name = name;
-        this.imageURL = imageURL;
-        this.price = price;
+    private Product(Long id, String name, String imageURL, int price) {
+        this.id = id;
+        this.name = validateName(name);
+        this.imageURL = validateImageURL(imageURL);
+        this.price = validatePrice(price);
+    }
+
+    public static Product createWithId(Long id, String name, String imageURL, int price) {
+        return new Product(id, name, imageURL, price);
+    }
+
+    public static Product createWithoutId(String name, String imageURL, int price) {
+        return new Product(null, name, imageURL, price);
+    }
+
+
+    private int validatePrice(int price) {
+        return 0;
+    }
+
+    private String validateImageURL(String imageURL) {
+        return null;
+    }
+
+    private String validateName(String name) {
+        return null;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
