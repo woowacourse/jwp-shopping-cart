6fd8e7fe18b66214b8a791e1dc7433d2268ad869
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-25 20:39:15 +0900

    feat: ItemController에 GET /items 요청 매핑
    - item 목록을 응답한다.
    
    Co-authored-by: dooboocookie <bkh3030@gmail.com>

--- src/main/java/cart/controller/ItemController.java
+++ src/main/java/cart/controller/ItemController.java
@@ -1,15 +1,22 @@
 package cart.controller;
 
 import cart.controller.dto.ItemRequest;
+import cart.dao.entity.Item;
 import cart.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("items")
 public class ItemController {
 
     private final ItemService itemService;
@@ -19,9 +26,18 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    @PostMapping("/items")
+    @PostMapping
     public ResponseEntity addItem(@RequestBody final ItemRequest itemRequest) {
         itemService.saveItem(itemRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> loadItem() {
+        List<Item> items = itemService.loadAllItem();
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body(items);
+    }
+
 }
