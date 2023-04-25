4baf89fef24cd4e4cfacd27a0298509735aad949
ingpyo <juninpyo@naver.com>
2023-04-25 17:18:54 +0900

    feat: 서비스 레이어 추가

--- src/main/java/cart/controller/WebPageController.java
+++ src/main/java/cart/controller/WebPageController.java
@@ -1,22 +1,22 @@
 package cart.controller;
 
-import cart.domain.Product;
+import cart.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
 public class WebPageController {
 
+    private final ProductService productService;
+
+    public WebPageController(final ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/")
     public String renderStartPage(Model model) {
-        List<Product> products = List.of(
-                new Product("비버", "https://gmlwjd9405.github.io/images/network/rest.png", 10000L),
-                new Product("땡칠", "https://gmlwjd9405.github.io/images/network/rest.png", 5000L)
-        );
-        model.addAttribute("products", products);
+        model.addAttribute("products", productService.getAll());
         return "index";
     }
 }
---
+++ src/main/java/cart/service/ProductService.java
@@ -0,0 +1,34 @@
+package cart.service;
+
+import cart.domain.Product;
+import cart.domain.Products;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class ProductService {
+    private final Products products;
+
+    public ProductService(final Products products) {
+        this.products = products;
+    }
+
+    public void add(final String name, final String image, final Long price) {
+        final Product product = new Product(name, image, price);
+        products.save(product);
+    }
+
+    public void delete(final Integer id) {
+        products.delete(id);
+    }
+
+    public void modify(final Integer id, final String name, final String image, final Long price) {
+        final Product product = new Product(id, name, image, price);
+        products.modify(product);
+    }
+
+    public Set<Product> getAll() {
+        return products.findAll();
+    }
+}
