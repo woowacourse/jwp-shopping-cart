7baffd856345fc74b9ddf1048d55eff8be037378
LJW25 <dbjwlee@gmail.com>
2023-04-25 17:47:58 +0900

    feat(AdminController): 관리자페이지 상품 조회 기능 구현
    Co-authored-by: xxeol2 <jhj01177@naver.com>

--- src/main/java/cart/service/ProductService.java
+++ src/main/java/cart/service/ProductService.java
@@ -4,6 +4,8 @@ import cart.dao.ProductDao;
 import cart.domain.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -16,5 +18,9 @@ public class ProductService {
     public void save(final String name, final int price, final String image) {
         Product product = new Product(name, price, image);
         productDao.insert(product);
+    }
+
+    public List<Product> findAll() {
+        return productDao.findAll();
     }
 }
