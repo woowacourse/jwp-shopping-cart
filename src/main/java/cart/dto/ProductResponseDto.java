d4f7bc979ecfb8182e61d79c7e2bfc3e25f861e3
Miseong Kim <miseongk@naver.com>
2023-04-25 16:54:44 +0900

    feat: ProductResponseDto 구현

---
+++ src/main/java/cart/dto/ProductResponseDto.java
@@ -0,0 +1,35 @@
+package cart.dto;
+
+import cart.entity.Product;
+
+public class ProductResponseDto {
+
+    private final int id;
+    private final String name;
+    private final int price;
+    private final String imageUrl;
+
+    public ProductResponseDto(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.price = product.getPrice();
+        this.imageUrl = product.getImageUrl();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+}
