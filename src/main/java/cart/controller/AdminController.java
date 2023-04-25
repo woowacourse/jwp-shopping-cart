d6f3d0cdf37a9840f32830ee43b0a961074860ab
Miseong Kim <miseongk@naver.com>
2023-04-25 17:24:10 +0900

    feat: 관리자 화면 반환 구현

---
+++ src/main/java/cart/controller/AdminController.java
@@ -0,0 +1,31 @@
+package cart.controller;
+
+import cart.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+
+    private final ProductService productService;
+
+    public AdminController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("")
+    public String admin() {
+        return "redirect:/admin/product";
+    }
+
+    @GetMapping("/product")
+    public String getProduct(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "admin";
+    }
+
+}
