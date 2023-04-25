d378e5e2ee96301e84d15b606443d28e8d1d9645
parkmuhyeun <pjhg410@gmail.com>
2023-04-25 16:40:11 +0900

    feat: 상품 목록 페이지 연동 기능 추가
    Co-authored-by: bjk1649 <kbjn1649@gmail.com>

---
+++ src/main/java/cart/controller/MainController.java
@@ -0,0 +1,22 @@
+package cart.controller;
+
+import cart.dao.ProductDao;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    private final ProductDao productDao;
+
+    public MainController(final ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @GetMapping("/")
+    public String showHome(Model model) {
+        model.addAttribute("products", productDao.findAll());
+        return "index";
+    }
+}
