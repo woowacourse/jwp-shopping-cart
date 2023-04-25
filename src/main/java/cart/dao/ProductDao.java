58fe4538cfe80d77e84fd1d31dfd4b1199c96e81
yoondgu <doyou2ing@naver.com>
2023-04-25 17:21:04 +0900

    feat: DAO 구현 완료

---
+++ src/main/java/cart/dao/ProductDao.java
@@ -0,0 +1,18 @@
+package cart.dao;
+
+import cart.domain.Product;
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductDao {
+
+    Product insert(Product product);
+
+    void update(Product product);
+
+    Optional<Product> findById(Long id);
+
+    List<Product> findAll();
+
+    void deleteById(Long id);
+}
--- src/main/java/cart/domain/Product.java
+++ src/main/java/cart/domain/Product.java
@@ -1,8 +1,10 @@
 package cart.domain;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
+@EqualsAndHashCode
 public class Product {
 
     private Long id;
@@ -10,10 +12,14 @@ public class Product {
     private final String image;
     private final int price;
 
-
-    public Product(final String name, final String image, final int price) {
+    public Product(final Long id, final String name, final String image, final int price) {
+        this.id = id;
         this.name = name;
         this.image = image;
         this.price = price;
+    }
+
+    public Product(final String name, final String image, final int price) {
+        this(null, name, image, price);
     }
 }
