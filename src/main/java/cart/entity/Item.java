41aa398554f210fa6bb8bcea7e9bfb57ba309193
thdwoqor <celebarte01@gmail.com>
2023-04-25 16:41:06 +0900

    feat(entity) : entity 안에 id 필드 추가

--- src/main/java/cart/entity/Item.java
+++ src/main/java/cart/entity/Item.java
@@ -1,15 +1,29 @@
-package cart.domain;
+package cart.entity;
 
 public class Item {
 
+    public static final Long INSERT_ID = null;
+    private final Long id;
     private final String name;
     private final String imageUrl;
     private final int price;
 
-    public Item(final String name, final String imageUrl, final int price) {
+    public Item(final Long id, final String name, final String imageUrl, final int price) {
+        this.id = id;
         this.name = name;
         this.imageUrl = imageUrl;
         this.price = price;
+    }
+
+    public Item(final String name, final String imageUrl, final int price) {
+        this.id = INSERT_ID;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {
