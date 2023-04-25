874f1b6896e9f2842598421a48b334a0096d6b7c
ingpyo <juninpyo@naver.com>
2023-04-25 20:37:24 +0900

    feat: 반환 객체 생성

--- src/main/java/cart/controller/WebPageController.java
+++ src/main/java/cart/controller/WebPageController.java
@@ -1,9 +1,14 @@
 package cart.controller;
 
+import cart.domain.Product;
+import cart.dto.ProductResponse;
 import cart.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class WebPageController {
@@ -16,13 +21,23 @@ public class WebPageController {
 
     @GetMapping("/")
     public String renderStartPage(final Model model) {
-        model.addAttribute("products", productService.getAll());
+        model.addAttribute("products", mapProducts(productService.getAll()));
         return "index";
     }
 
     @GetMapping("/admin")
     public String renderAdminPage(final Model model) {
-        model.addAttribute("products", productService.getAll());
+        model.addAttribute("products", mapProducts(productService.getAll()));
         return "admin";
+    }
+
+    private List<ProductResponse> mapProducts(List<Product> products) {
+        return products.stream()
+                .map(product -> new ProductResponse(
+                        product.getId(),
+                        product.getName(),
+                        product.getImage(),
+                        product.getPrice())
+                ).collect(Collectors.toList());
     }
 }
