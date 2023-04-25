c3b22d67dd6bf8e692ffd05b0fc0be460a8171ec
splitube <voicesplit@daum.net>
2023-04-25 17:36:15 +0900

    feat: CategoryDao 구현

---
+++ src/main/java/cart/dao/CategoryDao.java
@@ -0,0 +1,28 @@
+package cart.dao;
+
+import cart.entity.CategoryEntity;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CategoryDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public CategoryDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<CategoryEntity> findAllInId(final List<Long> categoryIds) {
+        final String inSql = String.join(",", Collections.nCopies(categoryIds.size(), "?"));
+        final String sql = String.format("SELECT id, name FROM category WHERE id IN (%s)", inSql);
+
+        return jdbcTemplate.query(
+            sql,
+            (rs, rowNum) -> new CategoryEntity(rs.getLong("id"), rs.getString("name")),
+            categoryIds.toArray()
+        );
+    }
+}
---
+++ src/test/java/cart/dao/CategoryDaoTest.java
@@ -0,0 +1,40 @@
+package cart.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import cart.entity.CategoryEntity;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@JdbcTest
+class CategoryDaoTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    private CategoryDao categoryDao;
+
+    @BeforeEach
+    void setUp() {
+        categoryDao = new CategoryDao(jdbcTemplate);
+    }
+
+    @Test
+    @DisplayName("카테고리 ID 목록으로 저장되어 있는 카테고리를 모두 조회한다.")
+    void getCategories() {
+        //given
+        final List<Long> ids = List.of(1L, 4L, 6L);
+
+        //when
+        final List<CategoryEntity> categoryEntities = categoryDao.findAllInId(ids);
+
+        //then
+        assertThat(categoryEntities).map(CategoryEntity::getName)
+            .containsExactly("카페", "일식", "치킨");
+    }
+}
