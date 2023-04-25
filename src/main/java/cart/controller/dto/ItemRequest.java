bce2003556720818710cb05799e647d27ce295ea
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-25 20:24:00 +0900

    feat: ItemService 아이템 저장 기능 추가
    Co-authored-by: dooboocookie <bkh3030@gmail.com>

---
+++ src/main/java/cart/controller/dto/ItemRequest.java
@@ -0,0 +1,26 @@
+package cart.controller.dto;
+
+public class ItemRequest {
+
+    private final String name;
+    private final Integer price;
+    private final String imageUrl;
+
+    public ItemRequest(String name, Integer price, String imageUrl) {
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
--- src/main/java/cart/service/ItemService.java
+++ src/main/java/cart/service/ItemService.java
@@ -1,5 +1,6 @@
 package cart.service;
 
+import cart.controller.dto.ItemRequest;
 import cart.dao.ItemDao;
 import cart.dao.entity.Item;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,5 +20,14 @@ public class ItemService {
 
     public List<Item> loadAllItem() {
         return itemDao.findAll();
+    }
+
+    public void saveItem(final ItemRequest itemRequest) {
+        Item item = new Item.Builder()
+                .name(itemRequest.getName())
+                .imageUrl(itemRequest.getImageUrl())
+                .price(itemRequest.getPrice())
+                .build();
+        itemDao.save(item);
     }
 }
