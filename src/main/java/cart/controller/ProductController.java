9365589d42ecd526eeb674c07b5acf9e167a742f
Miseong Kim <miseongk@naver.com>
2023-04-25 17:02:42 +0900

    feat: 상품 목록 조회 컨트롤러 구현

---
+++ src/main/java/cart/controller/ProductController.java
@@ -0,0 +1,23 @@
+package cart.controller;
+
+import cart.service.ProductService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/")
+    public String getProduct(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "index";
+    }
+
+}
