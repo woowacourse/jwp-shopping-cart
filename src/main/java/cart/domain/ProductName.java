15a825bc63ac0e49ab6842dd3f205f06a51c814d
eunbii0213 <eunbii0213@naver.com>
2023-04-25 17:28:32 +0900

    feat: 상품 이름은 Blank일 수 없다

--- src/main/java/cart/domain/ProductName.java
+++ src/main/java/cart/domain/ProductName.java
@@ -12,11 +12,18 @@ public class ProductName {
 
     private void validate(String name) {
         validateLength(name);
+        validateBlank(name);
     }
 
     private void validateLength(String name) {
         if (name.length() >= MAX_LENGTH) {
             throw new IllegalArgumentException(String.format("이름은 %d글자 이상 넘길 수 없습니다.", MAX_LENGTH));
+        }
+    }
+
+    private void validateBlank(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("이름은 공백일 수 없습니다.");
         }
     }
 }
--- src/test/java/cart/domain/ProductNameTest.java
+++ src/test/java/cart/domain/ProductNameTest.java
@@ -2,6 +2,11 @@ package cart.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EmptySource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -22,5 +27,14 @@ class ProductNameTest {
         //given, when, then
         assertThatCode(() -> new ProductName("judyjudyj"))
                 .doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    @DisplayName("상품 이름이 빈 경우에는 예외를 발생시킨다")
+    void test_productName_blank_exception(String name){
+        //given, when, then
+        assertThatThrownBy(() -> new ProductName(name))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
