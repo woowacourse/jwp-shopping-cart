0d58b31823967f23bcdfda74b8c8710cda21a7ae
echo <echo.backend@gmail.com>
2023-04-25 15:00:26 +0900

    refactor: Product 필드 원시값 포장
    Co-authored-by: go-jaecheol <gojaech@naver.com>

--- src/main/java/cart/product/domain/Product.java
+++ src/main/java/cart/product/domain/Product.java
@@ -2,33 +2,23 @@ package cart.product.domain;
 
 public class Product {
     
-    public static final String EMPTY_PRODUCT_NAME_ERROR = "상품 이름이 없습니다.";
-    private long id;
-    private String name;
-    private String image;
-    private int price;
+    private final long id;
+    private final Name name;
+    private final String image;
+    private final Price price;
     
-    public Product(final long id, final String name, final String image, final int price) {
-        validateName(name);
-        
+    public Product(final long id, final Name name, final String image, final Price price) {
         this.id = id;
         this.name = name;
         this.image = image;
         this.price = price;
     }
     
-    private void validateName(final String name) {
-        if(name.isEmpty()) {
-            throw new IllegalArgumentException(EMPTY_PRODUCT_NAME_ERROR);
-        }
-    }
-    
-    
     public long getId() {
         return this.id;
     }
     
-    public String getName() {
+    public Name getName() {
         return this.name;
     }
     
@@ -36,7 +26,7 @@ public class Product {
         return this.image;
     }
     
-    public int getPrice() {
+    public Price getPrice() {
         return this.price;
     }
 }
