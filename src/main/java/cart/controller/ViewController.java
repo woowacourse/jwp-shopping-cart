1a6f2eca32f5c7bf589cbdc02758d92e058bc42d
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-25 20:06:26 +0900

    feat: 뷰 렌더링 구현

---
+++ src/main/java/cart/controller/ViewController.java
@@ -0,0 +1,32 @@
+package cart.controller;
+
+import cart.controller.dto.ItemResponse;
+import cart.service.ItemService;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    private final ItemService itemService;
+
+    public ViewController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public String home(Model model) {
+        List<ItemResponse> itemResponses = itemService.findAll();
+        model.addAttribute("products", itemResponses);
+        return "index";
+    }
+
+    @GetMapping("/admin")
+    public String admin(Model model) {
+        List<ItemResponse> itemResponses = itemService.findAll();
+        model.addAttribute("products", itemResponses);
+        return "admin";
+    }
+}
--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -44,10 +44,11 @@ form.addEventListener('submit', (event) => {
     createProduct(product);
 });
 
-// TODO: [1단계] 상품 관리 CRUD API에 맞게 변경
 const createProduct = (product) => {
-    axios.request({
-        url: '',
+    axios.post('/items', {
+        name : product.name,
+        price: product.price,
+        imageUrl: product.imageUrl
     }).then((response) => {
         window.location.reload();
     }).catch((error) => {
@@ -55,12 +56,13 @@ const createProduct = (product) => {
     });
 };
 
-// TODO: [1단계] 상품 관리 CRUD API에 맞게 변경
 const updateProduct = (product) => {
     const { id } = product;
 
-    axios.request({
-        url: '',
+    axios.put('/items/' + id, {
+        name : product.name,
+        price: product.price,
+        imageUrl: product.imageUrl
     }).then((response) => {
         window.location.reload();
     }).catch((error) => {
@@ -68,11 +70,9 @@ const updateProduct = (product) => {
     });
 };
 
-// TODO: [1단계] 상품 관리 CRUD API에 맞게 변경
 const deleteProduct = (id) => {
-    axios.request({
-        url: '',
-    }).then((response) => {
+    axios.delete('/items/' + id)
+    .then((response) => {
         window.location.reload();
     }).catch((error) => {
         console.error(error);
