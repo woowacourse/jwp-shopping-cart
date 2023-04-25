60d62477110191913678574edc55c2b87f79b85e
sosow0212 <sosow0212@naver.com>
2023-04-25 20:39:42 +0900

    feat: MOCK 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,4 +7,7 @@ CREATE TABLE product
     price  INT         NOT NULL,
     img_url CLOB(10K)        NOT NULL,
     PRIMARY KEY (id)
-)
+);
+
+INSERT INTO product(id, name, price, img_url) VALUES(1, '상품명 테스트1', 100, 'https://cdn.pixabay.com/photo/2014/06/03/19/38/test-361512_960_720.jpg');
+INSERT INTO product(id, name, price, img_url) VALUES(2, '상품명 테스트2', 101, 'https://t1.daumcdn.net/cartoon/7520d517fbe0271cdceb64e57ff1b26aa2abb680');
