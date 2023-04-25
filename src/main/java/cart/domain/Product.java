7e7aa86da229df6f630df3d956f8e7d13658ee19
jiwon <ljwon77@daum.net>
2023-04-25 15:27:08 +0900

    feat: 상품 도메인 객체를 설계한다
    Co-authored-by: HubCreator <chocomilk8604@gmail.com>

---
+++ src/main/java/cart/domain/Product.java
@@ -0,0 +1,36 @@
+package cart.domain;
+
+public class Product {
+    private Long id;
+    private final String name;
+    private final String imageUrl;
+    private final int price;
+    private final ProductCategory category;
+
+    public Product(final String name, final String imageUrl, final int price, final ProductCategory category) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+}
---
+++ src/main/java/cart/domain/ProductCategory.java
@@ -0,0 +1,5 @@
+package cart.domain;
+
+public enum ProductCategory {
+    KOREAN, JAPANESE, CHINESE, WESTERN, SNACK, DESSERT
+}
