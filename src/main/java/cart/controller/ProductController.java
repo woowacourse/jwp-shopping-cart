11f9db70a44f932e6146122071a2e38cf234006e
gitchan <eunkeeeee@gmail.com>
2023-04-25 22:28:23 +0900

    feat(ProductIntegrationTest): 요청과 응답에 대한 테스트 코드 작성
    Co-authored-by: drunkenhw <drunkenhw@gmail.com>

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -42,7 +42,7 @@ public class ProductController {
         return "admin";
     }
 
-    @PostMapping("/admin/products")
+    @PostMapping("/products")
     public ResponseEntity<Product> createProduct(@Valid @RequestBody ProductRequest productRequest) {
         Product product = new Product(productRequest.getName(), productRequest.getImgUrl(), productRequest.getPrice());
         Product createdProduct = productService.createProduct(product);
@@ -53,14 +53,14 @@ public class ProductController {
         return ResponseEntity.created(location).body(createdProduct);
     }
 
-    @PatchMapping("/admin/products/{id}")
+    @PatchMapping("/products/{id}")
     public ResponseEntity<Void> updateProduct(@PathVariable long id, @Valid @RequestBody ProductRequest productRequest) {
         Product product = new Product(id, productRequest.getName(), productRequest.getImgUrl(), productRequest.getPrice());
         productService.updateProduct(product);
         return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/admin/products/{id}")
+    @DeleteMapping("/products/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
         productService.deleteProductBy(id);
         return ResponseEntity.ok().build();
--- src/main/java/cart/dto/ProductRequest.java
+++ src/main/java/cart/dto/ProductRequest.java
@@ -1,16 +1,16 @@
 package cart.dto;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
 public class ProductRequest {
-    @Max(value = 20, message = "상품 이름은 20자 이내로 입력해야 합니다.")
+    @Size(max = 20, message = "상품 이름은 20자 이내로 입력해야 합니다.")
     private final String name;
     private final String imgUrl;
     @Min(value = 1000, message = "상품 가격은 최소 1000원 이상이어야 합니다.")
     private final int price;
 
-    private ProductRequest(String name, String imgUrl, int price) {
+    public ProductRequest(String name, String imgUrl, int price) {
         this.name = name;
         this.imgUrl = imgUrl;
         this.price = price;
