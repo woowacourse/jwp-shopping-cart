6d798011ba19aa194befacb4c49d1b4bc9cc1726
gitchan <eunkeeeee@gmail.com>
2023-04-25 17:26:42 +0900

    refactor(DbProductDao): ParameterSource 간결화
    Co-authored-by: drunkenhw <drunkenhw@gmail.com>

--- src/main/java/cart/dao/DbProductDao.java
+++ src/main/java/cart/dao/DbProductDao.java
@@ -2,13 +2,14 @@ package cart.dao;
 
 import cart.entity.Product;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -25,25 +26,15 @@ public class DbProductDao implements ProductDao {
 
     @Override
     public Product save(Product product) {
-        String sql = "INSERT INTO product (name, img_url, price) VALUES (:name, :img_url, :price)";
+        String sql = "INSERT INTO product (name, img_url, price) VALUES (:name, :imgUrl, :price)";
 
-        String name = product.getName();
-        String imgUrl = product.getImgUrl();
-        Integer price = product.getPrice();
-
-        SqlParameterSource parameters = new MapSqlParameterSource(
-                Map.of(
-                        "name", name,
-                        "img_url", imgUrl,
-                        "price", price
-                )
-        );
+        SqlParameterSource parameters = new BeanPropertySqlParameterSource(product);
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
         namedParameterJdbcTemplate.update(sql, parameters, keyHolder);
 
         long id = Objects.requireNonNull(keyHolder.getKey()).longValue();
-        return new Product(id, name, imgUrl, price);
+        return new Product(id, product.getName(), product.getImgUrl(), product.getPrice());
     }
 
     @Override
@@ -59,15 +50,9 @@ public class DbProductDao implements ProductDao {
 
     @Override
     public Product update(Product product) {
-        String sql = "UPDATE product SET name = :name, img_url = :img_url, price = :price WHERE id = :id";
+        String sql = "UPDATE product SET name = :name, img_url = :imgUrl, price = :price WHERE id = :id";
 
-        SqlParameterSource parameters = new MapSqlParameterSource(Map.of(
-                "name", product.getName(),
-                "img_url", product.getImgUrl(),
-                "price", product.getPrice(),
-                "id", product.getId()
-        ));
-
+        SqlParameterSource parameters = new BeanPropertySqlParameterSource(product);
         namedParameterJdbcTemplate.update(sql, parameters);
 
         return product;
@@ -76,9 +61,7 @@ public class DbProductDao implements ProductDao {
     @Override
     public void deleteById(long id) {
         String sql = "DELETE FROM product WHERE id = :id";
-        SqlParameterSource parameters = new MapSqlParameterSource(Map.of(
-                "id", id
-        ));
+        Map<String, Long> parameters = Collections.singletonMap("id", id);
 
         namedParameterJdbcTemplate.update(sql, parameters);
     }
