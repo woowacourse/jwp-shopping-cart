650e7f49e12df77dc563e623cfb3c80233fa0085
ReO <yt930310@gmail.com>
2023-04-25 15:03:23 +0900

    feat: db 테이블 생성 쿼리 작성

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS products
+(
+    id      LONG            NOT NULL    AUTO_INCREMENT
+    name    VARCHAR(255)    NOT NULL
+    imgURL  VARCHAR(255)    NOT NULL
+    price   INT             NOT NULL
+)
