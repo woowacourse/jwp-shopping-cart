b174b388af371ee2ab91a30103e078cc3a03d622
wugawuga <dnr3655@naver.com>
2023-04-25 17:02:28 +0900

    feat: admin.html 상품 조회 렌더링 구현
    Co-authored-by: the9kim <the9rla@gmail.com>

--- src/main/java/cart/controller/AdminController.java
+++ src/main/java/cart/controller/AdminController.java
@@ -1,13 +1,25 @@
 package cart.controller;
 
+import cart.dao.ProductEntity;
+import cart.service.ProductService;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
 
+    private final ProductService productService;
+
+    public AdminController(final ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/admin")
-    public String adminHome() {
+    public String adminHome(Model model) {
+        List<ProductEntity> products = productService.findAll();
+        model.addAttribute("products", products);
         return "admin";
     }
 }
