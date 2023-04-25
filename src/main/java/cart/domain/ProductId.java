1c5bfe65945ffbca671a04cd9fc116027439ee81
odo27 <odo27@naver.com>
2023-04-25 16:33:26 +0900

    feat: ProductId 구현

---
+++ src/main/java/cart/domain/ProductId.java
@@ -0,0 +1,33 @@
+package cart.domain;
+
+import java.util.Objects;
+
+public class ProductId {
+
+    private final Long value;
+
+    public ProductId(final Long value) {
+        this.value = value;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final ProductId productId = (ProductId) o;
+        return Objects.equals(value, productId.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
