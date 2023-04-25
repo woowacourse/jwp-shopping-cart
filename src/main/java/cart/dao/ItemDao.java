94f1efb0fe31dd80c5f60daea41a0de7ab83ba4d
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-04-25 19:25:52 +0900

    feat: ItemDAO 저장하는 기능 추가
    Co-authored-by: dooboocookie <bkh3030@gmail.com>

--- src/main/java/cart/dao/ItemDao.java
+++ src/main/java/cart/dao/ItemDao.java
@@ -4,8 +4,11 @@ import cart.dao.entity.Item;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 
 @Repository
@@ -19,17 +22,29 @@ public class ItemDao {
     }
 
     private final RowMapper<Item> actorRowMapper =  (resultSet, rowNumber) -> {
-        Item item = new Item(
+        return new Item(
                 resultSet.getLong("id"),
                 resultSet.getString("name"),
                 resultSet.getString("image_url"),
                 resultSet.getInt("price")
         );
-        return item;
     };
 
     public List<Item> findAll() {
         final String sql = "SELECT id, name, image_url, price FROM items ";
         return jdbcTemplate.query(sql, actorRowMapper);
+    }
+
+    public Long save(final Item item) {
+        final String sql = "INSERT INTO items(name, image_url, price) VALUES(?, ?, ?)";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, new String[]{"id"});
+            preparedStatement.setString(1, item.getName());
+            preparedStatement.setString(2, item.getImageUrl());
+            preparedStatement.setInt(3, item.getPrice());
+            return preparedStatement;
+        }, keyHolder);
+        return (Long) keyHolder.getKey();
     }
 }
