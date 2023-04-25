919dab71386a6f6a200e429417cbdf2c54e63a1f
LJW25 <dbjwlee@gmail.com>
2023-04-25 16:58:18 +0900

    feat(ProductService): 서비스 저장 기능 구현
    Co-authored-by: xxeol2 <jhj01177@naver.com>

--- src/main/java/cart/domain/Product.java
+++ src/main/java/cart/domain/Product.java
@@ -2,29 +2,60 @@ package cart.domain;
 
 public class Product {
 
-    private final Integer id;
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MAX_NAME_LENGTH = 64;
+    public static final String NAME_LENGTH_ERROR_MESSAGE = "이름의 길이는 " + MIN_NAME_LENGTH + "자 이상 " + MAX_NAME_LENGTH + "자 이하입니다.";
+    public static final int MAX_PRICE = 10_000_000;
+    public static final String PRICE_ERROR_MESSAGE = "상품의 가격은 " + MAX_PRICE + "를 초과할 수 없습니다.";
+    public static final int MAX_IMAGE_URL_LENGTH = 256;
+    public static final String IMAGE_URL_LENGTH_ERROR_MESSAGE = "이미지 주소 길이는 " + MAX_IMAGE_URL_LENGTH + "자를 초과할 수 없습니다.";
+
+    private final Long id;
     private final String name;
     private final int price;
     private final String image;
 
-    public Product(Integer id, String name, int price, String image) {
+    public Product(Long id, String name, int price, String image) {
+        validate(name, price, image);
         this.id = id;
         this.name = name;
         this.price = price;
         this.image = image;
     }
 
+    public void validate(String name, int price, String image) {
+        if (name.length() < MIN_NAME_LENGTH || MAX_NAME_LENGTH < name.length()) {
+            throw new IllegalArgumentException(NAME_LENGTH_ERROR_MESSAGE);
+        }
+        if (price > MAX_PRICE) {
+            throw new IllegalArgumentException(PRICE_ERROR_MESSAGE);
+        }
+        if (image != null && image.length() > MAX_IMAGE_URL_LENGTH) {
+            throw new IllegalArgumentException(IMAGE_URL_LENGTH_ERROR_MESSAGE);
+        }
+    }
+
     public Product(String name, int price, String image) {
+        validate(name, price, image);
         this.id = null;
         this.name = name;
         this.price = price;
         this.image = image;
     }
 
-    public Product(String name, int price) {
-        this.id = null;
-        this.name = name;
-        this.price = price;
-        this.image = null;
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getImage() {
+        return image;
     }
 }
---
+++ src/main/java/cart/service/ProductService.java
@@ -0,0 +1,20 @@
+package cart.service;
+
+import cart.dao.ProductDao;
+import cart.domain.Product;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    private final ProductDao productDao;
+
+    public ProductService(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    public void save(final String name, final int price, final String image) {
+        Product product = new Product(name, price, image);
+        productDao.insert(product);
+    }
+}
