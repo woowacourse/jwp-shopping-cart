b86c52de390f03b9ea805ea1d929dc4fbd066278
ingpyo <juninpyo@naver.com>
2023-04-25 17:17:44 +0900

    refactor: 사용하지 않는 생성자 삭제

--- src/main/java/cart/domain/Product.java
+++ src/main/java/cart/domain/Product.java
@@ -1,20 +1,18 @@
 package cart.domain;
 
+import java.util.Objects;
+
 public class Product {
     private final Integer id;
     private final String name;
     private final String image;
     private final Long price;
 
-    public Product(final Integer id, Product product) {
-        this(id, product.name, product.image, product.price);
-    }
-
     public Product(final String name, final String image, final Long price) {
         this(null, name, image, price);
     }
 
-    private Product(final Integer id, final String name, final String image, final Long price) {
+    public Product(final Integer id, final String name, final String image, final Long price) {
         this.id = id;
         this.name = name;
         this.image = image;
