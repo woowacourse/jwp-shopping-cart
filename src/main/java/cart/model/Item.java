e9a34b27d176e9a5174b14b687b4b19ba1912635
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-25 20:03:08 +0900

    feat: 상품 DAO 기능 구현

---
+++ src/main/java/cart/model/Item.java
@@ -0,0 +1,26 @@
+package cart.model;
+
+public class Item {
+
+    private final String name;
+    private final String imageUrl;
+    private final int price;
+
+    public Item(String name, String imageUrl, int price) {
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
---
+++ src/main/java/cart/repository/ItemDao.java
@@ -0,0 +1,70 @@
+package cart.repository;
+
+import cart.model.Item;
+import cart.repository.dto.ItemDto;
+import java.sql.PreparedStatement;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ItemDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ItemDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public Long insert(Item item) {
+        String sql = "INSERT INTO ITEM(name, image_url, price) VALUES (?, ?, ?)";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"});
+            ps.setString(1, item.getName());
+            ps.setString(2, item.getImageUrl());
+            ps.setInt(3, item.getPrice());
+            return ps;
+        }, keyHolder);
+
+        return keyHolder.getKey().longValue();
+    }
+
+    public ItemDto findById(Long id) {
+        String sql = "SELECT item_id, name, image_url, price FROM ITEM WHERE item_id = ?";
+
+        return jdbcTemplate.queryForObject(sql, actorRowMapper, id);
+    }
+
+    public List<ItemDto> findAll() {
+        String sql = "SELECT item_id, name, image_url, price FROM ITEM";
+
+        return jdbcTemplate.query(sql, actorRowMapper);
+    }
+
+    public void update(Long id, Item item) {
+        String sql = "UPDATE ITEM SET name = ?, image_url = ?, price = ? WHERE item_id = ?";
+
+        jdbcTemplate.update(sql, item.getName(), item.getImageUrl(), item.getPrice(), id);
+    }
+
+    public void delete(Long id) {
+        String sql = "DELETE FROM ITEM WHERE item_id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    private final RowMapper<ItemDto> actorRowMapper = (resultSet, rowNum) -> {
+        ItemDto itemDto = new ItemDto(
+                resultSet.getLong("item_id"),
+                resultSet.getString("name"),
+                resultSet.getString("image_url"),
+                resultSet.getInt("price")
+        );
+        return itemDto;
+    };
+}
