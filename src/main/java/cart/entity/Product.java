bfb0c3163f8e8fbd7ef5183cf17fe8e1e9570ebb
hong-sile <gurwns9325@naver.com>
2023-04-25 14:44:28 +0900

    feat: product Entity 객체 생성

---
+++ src/main/java/cart/entity/Product.java
@@ -0,0 +1,32 @@
+package cart.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String image;
+    private int price;
+
+    public Product() {
+    }
+
+    // TDOO : 완성 후 이 생성자가 꼭 필요한지 확인해보기
+    public Product(final Long id, final String name, final String image, final int price) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+    }
+
+    public Product(final String name, final String image, final int price) {
+        this(null, name, image, price);
+    }
+}
