7d4f649f0d670707815a362613a1ab4b07794615
hectick <ch.yeondev@gmail.com>
2023-04-25 16:00:02 +0900

    test: 테스트 이름 추가, 테스트 목적에 부합하도록 데이터 강화
    Co-authored-by: 70825 <hello70825@gmail.com>

--- src/test/java/cart/dao/JdbcProductDaoTest.java
+++ src/test/java/cart/dao/JdbcProductDaoTest.java
@@ -1,7 +1,7 @@
 package cart.dao;
 
 import cart.entity.ProductEntity;
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,7 +12,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -24,15 +24,27 @@ class JdbcProductDaoTest {
     @Mock
     private JdbcTemplate jdbcTemplate;
 
+    @DisplayName("모든 상품 데이터를 제대로 반환하는지 확인한다")
     @Test
-    void test() {
-        when(productDao.selectAll()).thenReturn(List.of(ProductEntity.of(1L, "chicken", "image", 10000)));
+    void selectAllTest() {
+        final List<ProductEntity> data = List.of(
+                ProductEntity.of(1L, "chicken", "image", 10000),
+                ProductEntity.of(2L, "pizza", "image2", 20000)
+        );
+        when(productDao.selectAll()).thenReturn(data);
+
+        final List<ProductEntity> productEntities = productDao.selectAll();
 
         assertAll(
-                () -> assertThat(productDao.selectAll().get(0).getId()).isEqualTo(1L),
-                () -> assertThat(productDao.selectAll().get(0).getName()).isEqualTo("chicken"),
-                () -> assertThat(productDao.selectAll().get(0).getImage()).isEqualTo("image"),
-                () -> assertThat(productDao.selectAll().get(0).getPrice()).isEqualTo(10000)
+                () -> assertThat(productEntities.size()).isEqualTo(data.size()),
+                () -> assertThat(productEntities.get(0).getId()).isEqualTo(1L),
+                () -> assertThat(productEntities.get(0).getName()).isEqualTo("chicken"),
+                () -> assertThat(productEntities.get(0).getImage()).isEqualTo("image"),
+                () -> assertThat(productEntities.get(0).getPrice()).isEqualTo(10000),
+                () -> assertThat(productEntities.get(1).getId()).isEqualTo(2L),
+                () -> assertThat(productEntities.get(1).getName()).isEqualTo("pizza"),
+                () -> assertThat(productEntities.get(1).getImage()).isEqualTo("image2"),
+                () -> assertThat(productEntities.get(1).getPrice()).isEqualTo(20000)
         );
     }
 }
