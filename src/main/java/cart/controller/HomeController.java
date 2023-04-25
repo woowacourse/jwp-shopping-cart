14ad34c43ade9819a88379b562c7fcdaed98c2ee
amaran-th <songsy405@gmail.com>
2023-04-25 15:52:06 +0900

    chore: 컨벤션 적용 및 주석 제거

--- src/main/java/cart/controller/HomeController.java
+++ src/main/java/cart/controller/HomeController.java
@@ -9,12 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/")
-    public String loadHome(Model model){
+    @GetMapping
+    public String loadHome(Model model) {
 
         List<ProductResponse> products = new ArrayList<>();
 
-        ProductResponse product = new ProductResponse(3L, "onigiri", 3800, "https://ssl.pstatic.net/melona/libs/1442/1442607/3cbc04bb1da9138cb6e2_20230424140340572.jpg");
+        ProductResponse product = new ProductResponse(3L, "onigiri", 3800,
+            "https://ssl.pstatic.net/melona/libs/1442/1442607/3cbc04bb1da9138cb6e2_20230424140340572.jpg");
         products.add(product);
 
         model.addAttribute("products", products);
