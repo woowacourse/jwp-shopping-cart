9462c43fc17e4a19d314a2fab4fb11cb2a871413
Miseong Kim <miseongk@naver.com>
2023-04-25 17:38:43 +0900

    feat: 상품 등록 구현

--- src/main/java/cart/controller/AdminController.java
+++ src/main/java/cart/controller/AdminController.java
@@ -1,10 +1,13 @@
 package cart.controller;
 
+import cart.dto.ProductRequestDto;
 import cart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,6 +29,12 @@ public class AdminController {
     public String getProduct(Model model) {
         model.addAttribute("products", productService.findAll());
         return "admin";
+    }
+
+    @PostMapping("/product")
+    public String saveProduct(@RequestBody ProductRequestDto productRequestDto) {
+        productService.save(productRequestDto);
+        return "redirect:/admin/product";
     }
 
 }
