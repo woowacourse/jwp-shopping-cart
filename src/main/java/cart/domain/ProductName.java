7ba4124b534f3d77bec3072ba9066a3308593ea4
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-04-25 15:26:05 +0900

    feat(domain): 상품 이름 구현

---
+++ src/main/java/cart/domain/ProductName.java
@@ -0,0 +1,21 @@
+package cart.domain;
+
+public class ProductName {
+    private static final int MAX_LENGTH = 20;
+    private final String name;
+
+    public ProductName(String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+
+        if (name.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
