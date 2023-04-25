09630f331c2041ab446d02a10fc55637898468a7
splitube <voicesplit@daum.net>
2023-04-25 16:20:29 +0900

    feat: 이미지 경로를 검증하는 ImageUrl 객체 구현

---
+++ src/main/java/cart/entity/product/ImageUrl.java
@@ -0,0 +1,26 @@
+package cart.entity.product;
+
+public class ImageUrl {
+
+    private static final int MAX_LENGTH = 255;
+
+    private final String value;
+
+    public ImageUrl(final String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final String value) {
+        if (value == null || value.isBlank()) {
+            throw new IllegalArgumentException("이미지 경로가 존재하지 않습니다.");
+        }
+        if (value.length() > MAX_LENGTH) {
+            throw new IllegalArgumentException(String.format("이미지 경로 길이는 %d를 넘을 수 없습니다.", MAX_LENGTH));
+        }
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/cart/entity/product/ImageUrlTest.java
@@ -0,0 +1,44 @@
+package cart.entity.product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class ImageUrlTest {
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("이미지 경로가 존재하지 않을 경우 오류를 던진다.")
+    void nameNotExist(final String value) {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new ImageUrl(value))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이미지 경로가 50자 초과일 경우 오류를 던진다")
+    void nameOverLength() {
+        //given
+        //when
+        //then
+        assertThatThrownBy(() -> new ImageUrl("1".repeat(256)))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("이미지 경로를 조회한다.")
+    void getValue() {
+        //given
+        final ImageUrl imageUrl = new ImageUrl("validName");
+
+        //when
+        //then
+        assertThat(imageUrl.getValue()).isEqualTo("validName");
+    }
+}
