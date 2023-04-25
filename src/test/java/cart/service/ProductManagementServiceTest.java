26b68e46884d7dfa3eeba570688503c2e1fd876f
hectick <ch.yeondev@gmail.com>
2023-04-25 15:58:53 +0900

    feat: 상품 전체를 조회하는 기능 테스트
    Co-authored-by: 70825 <hello70825@gmail.com>

---
+++ src/test/java/cart/service/ProductManagementServiceTest.java
@@ -0,0 +1,49 @@
+package cart.service;
+
+import cart.dao.JdbcProductDao;
+import cart.dto.ProductDto;
+import cart.entity.ProductEntity;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductManagementServiceTest {
+
+    @InjectMocks
+    ProductManagementService managementService;
+
+    @Mock
+    JdbcProductDao productDao;
+
+    @DisplayName("모든 상품 데이터를 제대로 가져와서 반환하는지 확인한다")
+    @Test
+    void findAllTest() {
+        final List<ProductEntity> data = List.of(
+                ProductEntity.of(1L, "chicken", "image", 10000),
+                ProductEntity.of(2L, "pizza", "image2", 20000)
+        );
+        when(productDao.selectAll()).thenReturn(data);
+
+        final List<ProductDto> productDtos = managementService.findAll();
+
+        assertAll(
+                () -> assertThat(productDtos.size()).isEqualTo(data.size()),
+                () -> assertThat(productDtos.get(0).getName()).isEqualTo("chicken"),
+                () -> assertThat(productDtos.get(0).getImage()).isEqualTo("image"),
+                () -> assertThat(productDtos.get(0).getPrice()).isEqualTo(10000),
+                () -> assertThat(productDtos.get(1).getName()).isEqualTo("pizza"),
+                () -> assertThat(productDtos.get(1).getImage()).isEqualTo("image2"),
+                () -> assertThat(productDtos.get(1).getPrice()).isEqualTo(20000)
+        );
+    }
+}
