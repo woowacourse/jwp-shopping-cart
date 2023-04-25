d29af75cf18b49fb7aa5358c1677c11939fc6514
splitube <voicesplit@daum.net>
2023-04-25 16:38:25 +0900

    feat: 상품 가격을 검증하는 Price 객체 구현

---
+++ src/main/java/cart/entity/product/Price.java
@@ -0,0 +1,26 @@
+package cart.entity.product;
+
+class Price {
+
+    private static final int MIN_PRICE = 0;
+    
+    private final Integer value;
+
+    public Price(final Integer value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final Integer value) {
+        if (value == null) {
+            throw new IllegalArgumentException("상품의 가격이 존재하지 않습니다.");
+        }
+        if (value < MIN_PRICE) {
+            throw new IllegalArgumentException(String.format("상품의 가격은 %d보다 높아야합니다.", MIN_PRICE));
+        }
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+}
