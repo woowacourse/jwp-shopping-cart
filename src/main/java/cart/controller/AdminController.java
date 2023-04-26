9990249e6d73de16939910b900d9666bc9bd00c8
kokodak <kokodakadokok@gmail.com>
2023-04-26 14:51:34 +0900

    feat: 응답 상태코드 추가
    Co-authored-by: jundonghyuk <ehdgur4814@naver.com>

--- src/main/java/cart/controller/AdminController.java
+++ src/main/java/cart/controller/AdminController.java
@@ -3,6 +3,7 @@ package cart.controller;
 import cart.dto.ProductDto;
 import cart.service.ProductManagementService;
 import java.util.List;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class AdminController {
@@ -22,25 +24,29 @@ public class AdminController {
     }
 
     @GetMapping(value = {"/admin", "/admin/products"})
-    public void readProducts(final Model model) {
+    @ResponseStatus(HttpStatus.OK)
+    public String readProducts(final Model model) {
         final List<ProductDto> allProduct = productManagementService.findAllProduct();
         model.addAttribute("products", allProduct);
+        return "admin";
     }
 
     @PostMapping("/admin/products")
+    @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody final ProductDto productDto) {
         productManagementService.addProduct(productDto);
     }
 
     @PatchMapping("/admin/products/{product_id}")
+    @ResponseStatus(HttpStatus.CREATED)
     public void updateProduct(@PathVariable("product_id") final Long id, @RequestBody final ProductDto productDto) {
-        System.out.println(productDto);
         final ProductDto updatedProductDto
                 = new ProductDto(id, productDto.getName(), productDto.getImageUrl(), productDto.getPrice());
         productManagementService.updateProduct(updatedProductDto);
     }
 
     @DeleteMapping("/admin/products/{product_id}")
+    @ResponseStatus(HttpStatus.OK)
     public void deleteProduct(@PathVariable("product_id") final Long id) {
         productManagementService.deleteProduct(id);
     }
