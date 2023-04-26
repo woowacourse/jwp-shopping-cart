760c9fc919541bfc346cd1be23ca8035abcc765f
BGuga <tmdwns9913@gmail.com>
2023-04-26 15:23:08 +0900

    refactor: DB option 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS product (
     id       INT           AUTO_INCREMENT,
     name     VARCHAR(20)   NOT NULL,
-    image    VARCHAR(255)  NOT NULL,
+    image    VARCHAR       NOT NULL,
     price    INT           NOT NULL,
     PRIMARY KEY (id)
 );
