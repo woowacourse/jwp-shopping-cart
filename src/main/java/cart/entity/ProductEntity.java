8d9e61a1ce635fe74469870067925273329d60df
ingpyo <juninpyo@naver.com>
2023-04-26 15:52:56 +0900

    fix: ProductInMemoryDao update()

--- src/main/java/cart/entity/ProductEntity.java
+++ src/main/java/cart/entity/ProductEntity.java
@@ -1,5 +1,7 @@
 package cart.entity;
 
+import java.util.Objects;
+
 public class ProductEntity {
     private final Integer id;
     private final String name;
@@ -27,5 +29,20 @@ public class ProductEntity {
 
     public Long getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductEntity that = (ProductEntity) o;
+
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
