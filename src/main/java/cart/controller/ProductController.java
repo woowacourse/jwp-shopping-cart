63cffe96bc8190b7c6638867e6fedd6cdbae9e5d
yoondgu <doyou2ing@naver.com>
2023-04-26 12:59:53 +0900

    feat: Request 예외 처리 기능 구현

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -4,12 +4,14 @@ import cart.dao.ProductDao;
 import cart.domain.Product;
 import cart.dto.ErrorDto;
 import cart.dto.ProductDto;
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -22,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/product")
 public class ProductController {
@@ -50,18 +53,26 @@ public class ProductController {
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> delete(@Positive @PathVariable Long id) {
+    public ResponseEntity<Void> delete(@PathVariable("id") @Positive Long id) {
         productDao.deleteById(id);
         return ResponseEntity.ok().build();
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorDto handle(MethodArgumentNotValidException exception) {
+    public ErrorDto handleException(MethodArgumentNotValidException exception) {
         FieldError foundError = (FieldError) exception.getBindingResult()
                 .getAllErrors().stream()
                 .findFirst()
                 .orElseThrow(() -> new IllegalStateException("예외 메시지 정보가 존재하지 않습니다."));
-        return new ErrorDto(foundError.getField(), foundError.getDefaultMessage());
+        log.error("MethodArgumentNotValid message={}", foundError.getDefaultMessage());
+        return new ErrorDto(foundError.getDefaultMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ErrorDto handleException(ConstraintViolationException exception) {
+        log.error("ConstraintViolationException message={}", exception.getMessage());
+        return new ErrorDto(exception.getMessage());
     }
 }
--- src/main/java/cart/dto/ErrorDto.java
+++ src/main/java/cart/dto/ErrorDto.java
@@ -7,6 +7,5 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ErrorDto {
 
-    private final String field;
     private final String message;
 }
