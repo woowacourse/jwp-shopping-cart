4de594adb0bb5a55282ce5eb321e1ac4ec1be12a
hanueleee <ezzeezz@naver.com>
2023-04-26 11:11:55 +0900

    feat: 상품 수정 API 연동 기능 구현
    Co-authored-by: ezzanzzan <eunzzann@gmail.com>

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -56,13 +56,15 @@ const createProduct = (product) => {
     });
 };
 
-// TODO: [1단계] 상품 관리 CRUD API에 맞게 변경
 const updateProduct = (product) => {
     const { id } = product;
 
-    axios.request({
-        url: '',
-    }).then((response) => {
+    axios.post(`/admin/${id}`,{
+            name : product.name,
+            imgUrl : product.imgUrl,
+            price : product.price
+        }
+    ).then((response) => {
         window.location.reload();
     }).catch((error) => {
         console.error(error);
