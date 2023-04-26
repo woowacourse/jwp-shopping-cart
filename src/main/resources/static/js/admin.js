8386aad5642af8bcf5f3ec47aafef8a0ec55c14a
be-student <presentsong@naver.com>
2023-04-26 15:09:14 +0900

    feat: admin 정적 리소스 작성

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,80 +1,86 @@
 const modal = document.getElementById('modal');
 
 const showAddModal = () => {
-    modal.dataset.formType = 'add';
-    modal.style.display = 'block';
+  modal.dataset.formType = 'add';
+  modal.style.display = 'block';
 };
 
 const showEditModal = (product) => {
-    const elements = modal.getElementsByTagName('input');
-    for (const element of elements) {
-        element.value = product[element.getAttribute('name')];
-    }
-    modal.dataset.formType = 'edit';
-    modal.dataset.productId = product.id;
-    modal.style.display = 'block';
+  const elements = modal.getElementsByTagName('input');
+  for (const element of elements) {
+    element.value = product[element.getAttribute('name')];
+  }
+  modal.dataset.formType = 'edit';
+  modal.dataset.productId = product.id;
+  modal.style.display = 'block';
 };
 
 const hideAddModal = () => {
-    modal.style.display = 'none';
-    const elements = modal.getElementsByTagName('input');
-    for (const element of elements) {
-        element.value = '';
-    }
+  modal.style.display = 'none';
+  const elements = modal.getElementsByTagName('input');
+  for (const element of elements) {
+    element.value = '';
+  }
 }
 
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
+  event.preventDefault();
+  
+  const formData = new FormData(event.target);
+  let product = {};
+  for (const entry of formData.entries()) {
+    const [key, value] = entry;
+    product[key] = value;
+  }
 
-    const formData = new FormData(event.target);
-    let product = {};
-    for (const entry of formData.entries()) {
-        const [key, value] = entry;
-        product[key] = value;
-    }
+  if (modal.dataset.formType === 'edit') {
+    product['id'] = modal.dataset.productId;
+    updateProduct(product);
+    return;
+  }
 
-    if (modal.dataset.formType === 'edit') {
-        product['id'] = modal.dataset.productId;
-        updateProduct(product);
-        return;
-    }
-
-    createProduct(product);
+  createProduct(product);
 });
 
 // TODO: [1단계] 상품 관리 CRUD API에 맞게 변경
 const createProduct = (product) => {
-    axios.request({
-        url: '',
-    }).then((response) => {
-        window.location.reload();
-    }).catch((error) => {
-        console.error(error);
-    });
+  console.log("send request!!")
+  axios.request({
+    url: '/products',
+    method: 'POST',
+    data: product
+  }).then((response) => {
+    window.location.reload();
+  }).catch((error) => {
+    console.error(error);
+  });
 };
 
 // TODO: [1단계] 상품 관리 CRUD API에 맞게 변경
 const updateProduct = (product) => {
-    const { id } = product;
+  const {id} = product;
 
-    axios.request({
-        url: '',
-    }).then((response) => {
-        window.location.reload();
-    }).catch((error) => {
-        console.error(error);
-    });
+  axios.request({
+    url: `/products/${id}`,
+    method: 'PUT',
+    data: product
+  }).then((response) => {
+    window.location.reload();
+  }).catch((error) => {
+    console.error(error);
+  });
 };
 
 // TODO: [1단계] 상품 관리 CRUD API에 맞게 변경
 const deleteProduct = (id) => {
-    axios.request({
-        url: '',
-    }).then((response) => {
-        window.location.reload();
-    }).catch((error) => {
-        console.error(error);
-    });
+  axios.request({
+    url: `/products/${id}`,
+    method: 'DELETE'
+  }).then((response) => {
+    window.location.reload();
+  }).catch((error) => {
+    console.error(error);
+  });
 };
