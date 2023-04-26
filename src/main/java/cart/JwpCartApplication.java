4810b46545d6092b3c4be2e400ac1bc13070d926
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-04-26 19:14:36 +0900

    refactor : 쓰이지 않는 클래스, 주석 제거

--- src/main/java/cart/JwpCartApplication.java
+++ src/main/java/cart/JwpCartApplication.java
@@ -8,8 +8,6 @@ public class JwpCartApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JwpCartApplication.class, args);
-        Integer a = 10;
-        Long b = 10L;
     }
 
 }
