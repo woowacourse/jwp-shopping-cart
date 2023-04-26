aaa69529dabcbc06d0981ee825d904d64b8d73cb
be-student <presentsong@naver.com>
2023-04-26 15:08:52 +0900

    fix: ProductViewControllerTest mockBean 추가

--- src/test/java/cart/controller/ProductViewControllerTest.java
+++ src/test/java/cart/controller/ProductViewControllerTest.java
@@ -1,6 +1,7 @@
 package cart.controller;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -8,6 +9,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.util.List;
 
+import cart.domain.Product;
 import cart.dto.ProductResponse;
 import cart.dto.ResponseFixture;
 import org.junit.jupiter.api.Test;
@@ -20,6 +22,11 @@ class ProductViewControllerTest extends AbstractProductControllerTest {
 
     @Test
     void 상품_조회_테스트() throws Exception {
+        final List<Product> products = List.of(
+                new Product(1L, "누누", "naver.com", 1),
+                new Product(2L, "오도", "naver.com", 1)
+        );
+        given(productSearchService.find()).willReturn(products);
         final List<ProductResponse> expected = List.of(
                 ResponseFixture.NUNU_RESPONSE,
                 ResponseFixture.ODO_RESPONSE
@@ -32,6 +39,11 @@ class ProductViewControllerTest extends AbstractProductControllerTest {
 
     @Test
     void 어드민_페이지_조회_테스트() throws Exception {
+        final List<Product> products = List.of(
+                new Product(1L, "누누", "naver.com", 1),
+                new Product(2L, "오도", "naver.com", 1)
+        );
+        given(productSearchService.find()).willReturn(products);
         final List<ProductResponse> expected = List.of(
                 ResponseFixture.NUNU_RESPONSE,
                 ResponseFixture.ODO_RESPONSE
