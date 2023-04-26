b6f0809c9c086c700c6c61723c01c18f10626512
yujamint <woawoa0513@gmail.com>
2023-04-26 15:14:18 +0900

    test: 실패하는 테스트 개선

--- src/test/java/cart/JwpCartApplicationTests.java
+++ src/test/java/cart/JwpCartApplicationTests.java
@@ -1,13 +1,27 @@
 package cart;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class JwpCartApplicationTests {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
 
     @Test
     void contextLoads() {
+        System.out.println(port);
+        System.out.println("JwpCarApplicationTests Thread: "+Thread.currentThread());
     }
 
 }
