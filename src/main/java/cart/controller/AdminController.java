5382c22c930145930d9998a514b551f5b66bc8c5
hectick <ch.yeondev@gmail.com>
2023-04-26 11:45:32 +0900

    feat: 관리자 페이지에서 상품을 등록에 대한 응답 기능 구현
    Co-authored-by: 70825 <hello70825@gmail.com>

--- src/main/java/cart/controller/AdminController.java
+++ src/main/java/cart/controller/AdminController.java
@@ -1,12 +1,17 @@
 package cart.controller;
 
+import cart.dto.ProductCreationRequest;
+import cart.dto.ProductDto;
 import cart.dto.ProductResponse;
 import cart.service.ProductManagementService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
     private final ProductManagementService managementService;
@@ -15,10 +20,17 @@ public class AdminController {
         this.managementService = managementService;
     }
 
-    @GetMapping("/admin")
+    @GetMapping
     public ModelAndView admin(ModelAndView modelAndView) {
         modelAndView.addObject("products", ProductResponse.from(managementService.findAll()));
         modelAndView.setViewName("admin");
         return modelAndView;
+    }
+
+    @PostMapping("/products")
+    @ResponseBody
+    public ResponseEntity<Void> postProducts(@RequestBody ProductCreationRequest request) {
+        managementService.save(ProductDto.from(request));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
