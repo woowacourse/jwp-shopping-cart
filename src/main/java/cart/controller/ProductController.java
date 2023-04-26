b0cd9b25435c62c3564324630607d2466ffd5cf8
zillionme <beatus96@naver.com>
2023-04-26 11:18:51 +0900

    feat: 뷰 컨트롤러와 api 컨트롤러 분리

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -1,13 +1,9 @@
 package cart.controller;
 
-import cart.domain.Product;
 import cart.service.ProductService;
-import java.util.List;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ProductController {
 
     private final ProductService productService;
@@ -15,12 +11,5 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
-
-    // todo : 뷰에 productDto로 감싸서 넘겨줘야 하는지 생각해보기
-    @GetMapping("/")
-     public String productListView(Model model) {
-        List<Product> products = productService.findAllProducts();
-        model.addAttribute("products", products);
-        return "index";
-    }
+    
 }
