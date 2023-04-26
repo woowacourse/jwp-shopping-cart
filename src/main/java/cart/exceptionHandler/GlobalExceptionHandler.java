14b7d00bef81b7e3ff60e7856079c965a26677fe
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-04-26 18:13:00 +0900

    refactor: 오류 메시지만 전달하도록 수정
    Co-authored-by: kdkdhoho <hkim4410@gmail.com>

--- src/main/java/cart/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/cart/exceptionHandler/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package cart.exceptionHandler;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +12,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> methodArgumentNotValidException(final MethodArgumentNotValidException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+        final BindingResult bindingResult = e.getBindingResult();
+        final FieldError fieldError = bindingResult.getFieldError();
+
+        return ResponseEntity.badRequest().body(fieldError.getDefaultMessage());
     }
 }
