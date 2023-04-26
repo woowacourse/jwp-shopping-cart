5601a94de11ac954cdfd9350d921151009873faf
hectick <ch.yeondev@gmail.com>
2023-04-26 16:36:07 +0900

    test: 클라이언트 요청에 대한 검증 기능 테스트
    Co-authored-by: 70825 <hello70825@gmail.com>

--- src/test/java/cart/controller/AdminControllerTest.java
+++ src/test/java/cart/controller/AdminControllerTest.java
@@ -6,6 +6,8 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -44,6 +46,48 @@ class AdminControllerTest {
                 .statusCode(HttpStatus.CREATED.value());
     }
 
+    @DisplayName("상품을 등록할 때 잘못된 형식의 이름이 들어오면 상태코드 400을 반환하는지 확인한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void throwExceptionWhenInvalidNamePostProductsTest(final String nameInput) {
+        ProductCreationRequest request = new ProductCreationRequest(nameInput, "image", 10_000_000);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/admin/products")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("상품을 등록할 때 잘못된 범위의 가격이 들어오면 상태코드 400을 반환하는지 확인한다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10_000_001})
+    void throwExceptionWhenInvalidPricePostProductsTest(final int priceInput) {
+        ProductCreationRequest request = new ProductCreationRequest("pobi", "image", priceInput);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/admin/products")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("상품을 등록할 때 잘못된 형식의 이미지 경로가 들어오면 상태코드 400을 반환하는지 확인한다")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", ""})
+    void throwExceptionWhenInvalidPricePostProductsTest(final String imageInput) {
+        ProductCreationRequest request = new ProductCreationRequest("pobi", imageInput, 10_000_000);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().post("/admin/products")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
     @DisplayName("상품을 수정하면 상태코드 204를 반환하는지 확인한다")
     @Test
     void putProductsTest() {
@@ -55,6 +99,48 @@ class AdminControllerTest {
                 .when().put("/admin/products/1")
                 .then().log().all()
                 .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @DisplayName("상품을 수정할 때 잘못된 형식의 이름이 들어오면 상태코드 400을 반환하는지 확인한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void throwExceptionWhenInvalidNamePutProductsTest(final String nameInput) {
+        ProductModificationRequest request = new ProductModificationRequest(1L, nameInput, "image", 10_000_000);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().put("/admin/products/" + request.getId())
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("상품을 수정할 때 잘못된 범위의 가격이 들어오면 상태코드 400을 반환하는지 확인한다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10_000_001})
+    void throwExceptionWhenInvalidPricePutProductsTest(final int priceInput) {
+        ProductModificationRequest request = new ProductModificationRequest(1L, "pobi", "image", priceInput);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().put("/admin/products/" + request.getId())
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @DisplayName("상품을 수정할 때 잘못된 형식의 이미지 경로가 들어오면 상태코드 400을 반환하는지 확인한다")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", ""})
+    void throwExceptionWhenInvalidPricePutProductsTest(final String imageInput) {
+        ProductModificationRequest request = new ProductModificationRequest(1L, "pobi", imageInput, 10_000_000);
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(request)
+                .when().put("/admin/products/" + request.getId())
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @DisplayName("상품을 삭제하면 상태코드 204를 반환하는지 확인한다")
