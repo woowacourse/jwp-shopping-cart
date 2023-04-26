d8d1c98fbe698da34b900ff088f96112acf9d8fa
LJW25 <dbjwlee@gmail.com>
2023-04-26 10:58:03 +0900

    test(AdminControllerTest): 관리자페이지 상품 조회 기능 테스트
    Co-authored-by: xxeol2 <jhj01177@naver.com>

---
+++ src/test/java/cart/controller/AdminControllerTest.java
@@ -0,0 +1,41 @@
+package cart.controller;
+
+import cart.dao.ProductDao;
+import cart.domain.Product;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.hamcrest.Matchers.stringContainsInOrder;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AdminControllerTest {
+
+    @Autowired
+    private ProductDao productDao;
+
+    @BeforeEach
+    void setUp(@LocalServerPort final int port) {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("GET /admin")
+    @Test
+    void getAdmin() {
+        productDao.insert(new Product("이오", 1000, null));
+        productDao.insert(new Product("애쉬", 2000, null));
+
+        RestAssured.given().log().all()
+                .accept(MediaType.TEXT_HTML_VALUE)
+                .when().get("/admin")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body(stringContainsInOrder("이오", "1000", "애쉬", "2000"));
+    }
+}
