e232831e0435673b3d4d827189e1016548ebbd30
somsom13 <somin1113@naver.com>
2023-04-26 21:35:00 +0900

    test: 통합 테스트 구현

---
+++ src/test/java/cart/controller/AdminControllerUnitTest.java
@@ -0,0 +1,174 @@
+package cart.controller;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import cart.service.CartService;
+import cart.service.dto.ProductRequest;
+import cart.service.dto.ProductResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@WebMvcTest
+public class AdminControllerUnitTest {
+
+    private ProductResponse cuteSeonghaDoll =
+            new ProductResponse(1, "https://avatars.githubusercontent.com/u/95729738?v=4",
+                    "CuteSeonghaDoll", 25000);
+
+    private ProductResponse cuteBaronDoll =
+            new ProductResponse(2, "https://avatars.githubusercontent.com/u/95729738?v=4",
+                    "CuteBaronDoll", 250000);
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+    @MockBean
+    private CartService cartService;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+
+    @DisplayName("전체 상품 조회 API 호출 시 전체 상품이 반환된다.")
+    @Test
+    void showAllProducts() throws Exception {
+        given(cartService.findAllProducts()).willReturn(List.of(cuteSeonghaDoll, cuteBaronDoll));
+        mockMvc.perform(get("/admin"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("admin"));
+    }
+
+    @DisplayName("상품 등록 API 호출 시 상품을 등록한다.")
+    @Test
+    void registerProduct() throws Exception {
+        // given
+        String requestString = objectMapper.writeValueAsString(cuteSeonghaDoll);
+        given(cartService.save(any(ProductRequest.class))).willReturn(1L);
+
+        // when then
+        mockMvc.perform(post("/admin/product")
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestString))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location", "/admin/product/1"));
+    }
+
+    @DisplayName("상품 정보 수정 API 호출 시 상품 정보가 수정된다.")
+    @Test
+    void modifyProduct() throws Exception {
+        // given
+        String requestString = objectMapper.writeValueAsString(cuteSeonghaDoll);
+
+        mockMvc.perform(put("/admin/product/1")
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(requestString))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("상품 삭제 API 호출 시 상품이 삭제된다.")
+    @Test
+    void deleteProduct() throws Exception {
+        // when, then
+        mockMvc.perform(delete("/admin/product/1"))
+                .andExpect(status().isNoContent());
+    }
+
+    @DisplayName("가격이 0이하의 값이면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void exceptionWhenPriceNotPositive(int price) throws Exception {
+        // given
+        ProductResponse wrongCuteSeonghaDoll =
+                new ProductResponse(1, "https://avatars.githubusercontent.com/u/95729738?v=4",
+                        "CuteSeonghaDoll", price);
+        String requestString = objectMapper.writeValueAsString(wrongCuteSeonghaDoll);
+        given(cartService.save(any(ProductRequest.class))).willReturn(1L);
+
+        // when then
+        mockMvc.perform(post("/admin/product")
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(requestString))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("가격은 0보다 커야합니다."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("이름이 1자 미만이거나 50자 초과면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"dskjgfdsvesvurevhjdsbvehsbvhjesbvhjesbvfhvsdhvhdsvhfdshv", ""})
+    void exceptionWhenNameWrongLength(String name) throws Exception {
+        // given
+        ProductResponse wrongCuteSeonghaDoll =
+                new ProductResponse(1, "https://avatars.githubusercontent.com/u/95729738?v=4",
+                        name, 24000);
+        String requestString = objectMapper.writeValueAsString(wrongCuteSeonghaDoll);
+        given(cartService.save(any(ProductRequest.class))).willReturn(1L);
+
+        // when then
+        mockMvc.perform(post("/admin/product")
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(requestString))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("이름은 1글자 이상 50글자 이하여야합니다."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+    @DisplayName("이미지 URL이 없으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void exceptionWhenBlankImgUrl(String imgUrl) throws Exception {
+        // given
+        ProductResponse wrongCuteSeonghaDoll =
+                new ProductResponse(1, imgUrl, "cuteSeonghaDoll", 24000);
+        String requestString = objectMapper.writeValueAsString(wrongCuteSeonghaDoll);
+        given(cartService.save(any(ProductRequest.class))).willReturn(1L);
+
+        // when then
+        mockMvc.perform(post("/admin/product")
+                        .accept(MediaType.APPLICATION_JSON_VALUE)
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .content(requestString))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string("이미지 URL은 필수입니다."))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+}
---
+++ src/test/java/cart/service/CartServiceTest.java
@@ -0,0 +1,83 @@
+package cart.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import cart.service.dto.ProductRequest;
+import cart.service.dto.ProductResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@AutoConfigureTestDatabase
+@SpringBootTest
+class CartServiceTest {
+
+    @Autowired
+    private CartService cartService;
+
+    private ProductRequest cuteSeonghaDoll =
+            new ProductRequest("https://avatars.githubusercontent.com/u/95729738?v=4",
+                    "CuteSeonghaDoll", 25000);
+
+    private ProductRequest cuteBaronDoll =
+            new ProductRequest("https://avatars.githubusercontent.com/u/95729738?v=4",
+                    "CuteBaronDoll", 250000);
+
+    @Test
+    @DisplayName("상품을 저장하고, 모든 상품을 조회할 수 있다.")
+    void findAll() {
+        // given
+        long savedId1 = cartService.save(cuteSeonghaDoll);
+        long savedId2 = cartService.save(cuteBaronDoll);
+
+        // when
+        List<ProductResponse> products = cartService.findAllProducts();
+        List<Long> foundIds = products.stream()
+                .map(ProductResponse::getId)
+                .collect(Collectors.toList());
+
+        // then
+        assertThat(foundIds).containsExactly(savedId1, savedId2);
+    }
+
+    @Test
+    @DisplayName("상품을 저장하고 수정할 수 있다.")
+    void modifyById() {
+        // given
+        long savedId = cartService.save(cuteSeonghaDoll);
+        ProductRequest productToModify = cuteBaronDoll;
+
+        // when
+        cartService.modifyById(productToModify, savedId);
+
+        // then
+        ProductResponse foundProduct = cartService.findAllProducts().get(0);
+
+        assertAll(
+                () -> assertThat(foundProduct.getId()).isEqualTo(savedId),
+                () -> assertThat(foundProduct.getName()).isEqualTo(productToModify.getName()),
+                () -> assertThat(foundProduct.getPrice()).isEqualTo(productToModify.getPrice()),
+                () -> assertThat(foundProduct.getImgUrl()).isEqualTo(productToModify.getImgUrl())
+        );
+    }
+
+    @Test
+    @DisplayName("상품을 저장하고 삭제할 수 있다.")
+    void deleteById() {
+        // given
+        long savedId = cartService.save(cuteSeonghaDoll);
+
+        // when
+        cartService.removeById(savedId);
+
+        // then
+        assertThat(cartService.findAllProducts()).isEmpty();
+    }
+}
