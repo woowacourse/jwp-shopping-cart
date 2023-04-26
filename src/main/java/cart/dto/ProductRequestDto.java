b1e953fb8726ab4ca11f69bf7273bda2a7db3d7e
seokhwan-an <asok369258@naver.com>
2023-04-26 21:20:30 +0900

    feat: 상품등록 및 수정 요청 예외처리 추가

--- src/main/java/cart/dto/ProductRequestDto.java
+++ src/main/java/cart/dto/ProductRequestDto.java
@@ -1,9 +1,16 @@
 package cart.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 public class ProductRequestDto {
 
+    @NotBlank(message = "상품 이름은 빈 값이 될 수 없습니다.")
     private final String name;
+    @NotBlank(message = "상품 이미지는 빈 이미지가 될 수 없습니다.")
     private final String imageUrl;
+    @Min(value = 0, message = "상품 가격은 음수가 될 수 없습니다.")
     private final int price;
 
     public ProductRequestDto(final String name, final String imageUrl, final int price) {
---
+++ src/main/java/cart/exception/ExceptionHandlerAdvice.java
@@ -0,0 +1,22 @@
+package cart.exception;
+
+import java.util.stream.Collectors;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerAdvice {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleInvalidValueException(MethodArgumentNotValidException exception) {
+        String errorMessage = exception.getFieldErrors().stream()
+            .map(DefaultMessageSourceResolvable::getDefaultMessage)
+            .collect(Collectors.joining(", "));
+        return ResponseEntity.badRequest().body(errorMessage);
+    }
+
+}
