beda2cf3d193cd24ba93d50bae0e8d099bd892bc
BGuga <tmdwns9913@gmail.com>
2023-04-26 15:22:13 +0900

    refactor: controller 분리

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -4,21 +4,23 @@ import cart.dto.ProductRequestDto;
 import cart.entity.ProductEntity;
 import cart.service.ProductService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
-
-@Controller
+@RestController
+@RequestMapping("/products")
 public class ProductController {
     private final ProductService productService;
 
@@ -26,20 +28,13 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @PostMapping("/admin")
+    @PostMapping
     public ResponseEntity<Void> create(@RequestBody @Valid ProductRequestDto productRequestDto) {
         productService.create(productRequestDto);
         return ResponseEntity.created(URI.create("/admin")).build();
     }
 
-    @GetMapping("/admin")
-    public String findAll(Model model) {
-        final List<ProductEntity> allProducts = productService.findAll();
-        model.addAttribute("products", allProducts);
-        return "admin";
-    }
-
-    @PatchMapping("/admin/{id}")
+    @PatchMapping("/{id}")
     public ResponseEntity<Void> update(@RequestBody final ProductRequestDto productRequestDto, @PathVariable final int id){
         productService.update(productRequestDto, id);
         return ResponseEntity.created(URI.create("/admin")).build();
