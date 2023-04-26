bdd90ae3caf00a48e98dc539061e558905cee8e8
junpakPark <junpak.park@gmail.com>
2023-04-26 15:36:05 +0900

    test : Controller 및 DAO 테스트 케이스 추가

--- src/test/java/cart/product/controller/ProductApiControllerTest.java
+++ src/test/java/cart/product/controller/ProductApiControllerTest.java
@@ -1,16 +1,24 @@
 package cart.product.controller;
 
 import static io.restassured.RestAssured.*;
-import static io.restassured.config.EncoderConfig.*;
+import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
+import cart.product.dto.ProductRequest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductApiControllerTest {
@@ -23,19 +31,104 @@ class ProductApiControllerTest {
 		RestAssured.port = port;
 	}
 
-	@Test
-	void createProductsTest() {
-		//given
-		given()
-				.config(config()
-					.encoderConfig(encoderConfig().encodeContentTypeAs("multipart/form-data", ContentType.TEXT)))
-				.param("name", "name")
-				.param("price", 1000)
-				.param("image", "image")
-			.when()
-				.post("/products")
-			.then()
-				.statusCode(HttpStatus.CREATED.value())
+	@Nested
+	class CrudTest {
+		@Test
+		void createProductsTest() {
+			ProductRequest productRequest = new ProductRequest("name", 1000, "image");
+
+			ExtractableResponse<Response> response = saveProducts(productRequest);
+
+			assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+		}
+
+		@Test
+		void updateProductsTest() {
+			ProductRequest productRequest = new ProductRequest("name", 1000, "image");
+			ProductRequest updatedRequest = new ProductRequest("name", 10, "image");
+
+			saveProducts(productRequest);
+
+			given()
+				.log().all()
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.body(updatedRequest)
+				.when()
+				.put("/products/1")
+				.then()
+				.log().all()
+				.statusCode(HttpStatus.OK.value())
 				.contentType(ContentType.JSON);
+
+		}
+
+		@Test
+		void deleteProductsTest() {
+			ProductRequest productRequest = new ProductRequest("name", 1000, "image");
+
+			saveProducts(productRequest);
+
+			when()
+				.delete("/products/1")
+				.then()
+				.log().all()
+				.statusCode(HttpStatus.NO_CONTENT.value());
+		}
+	}
+
+	@Nested
+	class ExceptionTest {
+
+		@ParameterizedTest
+		@ValueSource(strings = {"", " "})
+		@NullSource
+		void nameNullTest(String name) {
+			ProductRequest productRequest = new ProductRequest(name, 1000, "image");
+
+			ExtractableResponse<Response> response = saveProducts(productRequest);
+
+			assertThat(response.body().asPrettyString()).contains("상품명을 입력해주세요.");
+		}
+
+		@Test
+		void nameSizeTest() {
+			ProductRequest productRequest = new ProductRequest("fsd;kljgnad;ofgadfs;kgjadsfkjhsadflk", 1000, "image");
+
+			ExtractableResponse<Response> response = saveProducts(productRequest);
+
+			assertThat(response.body().asPrettyString()).contains("20 글자 이하만 입력 가능합니다.");
+		}
+
+		@Test
+		void priceNullTest() {
+			ProductRequest productRequest = new ProductRequest("name", null, "image");
+
+			ExtractableResponse<Response> response = saveProducts(productRequest);
+
+			assertThat(response.body().asPrettyString()).contains("상품가격을 입력해주세요.");
+		}
+
+		@ParameterizedTest
+		@ValueSource(ints = {-1, -100})
+		void nameRangeTest(int price) {
+			ProductRequest productRequest = new ProductRequest("name", price, "image");
+
+			ExtractableResponse<Response> response = saveProducts(productRequest);
+
+			assertThat(response.body().asPrettyString()).contains("상품 금액은 0원 이상의 정수만 입력가능 합니다.");
+		}
+
+	}
+
+	private ExtractableResponse<Response> saveProducts(ProductRequest productRequest) {
+
+		//given
+		return given()
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(productRequest)
+			.when()
+			.post("/products")
+			.then()
+			.extract();
 	}
 }
---
+++ src/test/java/cart/product/dao/ProductDaoTest.java
@@ -0,0 +1,95 @@
+package cart.product.dao;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import cart.product.entity.Product;
+
+@JdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class ProductDaoTest {
+
+	private ProductDao productDao;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	private Product product;
+	private final RowMapper<Product> productRowMapper = (rs, rowNum) -> new Product(
+		rs.getLong("id"),
+		rs.getString("name"),
+		rs.getString("image"),
+		rs.getInt("price"),
+		rs.getTimestamp("created_at").toLocalDateTime(),
+		rs.getTimestamp("updated_at").toLocalDateTime()
+	);
+
+	@BeforeEach
+	public void setProduct() {
+		productDao = new ProductDao(jdbcTemplate);
+		product = new Product(null, "name", "image", 10000, null, null);
+	}
+
+	@Test
+	void findAllTest() {
+		// when
+		List<Product> all = productDao.findAll();
+
+		// then
+		assertThat(all).isNotNull();
+		assertThat(all).hasSize(0);
+	}
+
+	@Test
+	void saveTest() {
+		// when
+		productDao.save(product);
+		List<Product> products = jdbcTemplate.query("SELECT * FROM PRODUCT", productRowMapper);
+		Product savedProduct = products.get(0);
+
+		// then
+		assertThat(savedProduct.getName()).isEqualTo(this.product.getName());
+		assertThat(savedProduct.getImage()).isEqualTo(this.product.getImage());
+		assertThat(savedProduct.getPrice()).isEqualTo(this.product.getPrice());
+	}
+
+	@Test
+	void updateByIdTest() {
+		// given
+		long id = productDao.save(product);
+
+		// when
+		Product newProduct = new Product(null, "newName", "newImage", 0, null, null);
+		productDao.updateById(id, newProduct);
+
+		// then
+		Product findProduct = jdbcTemplate.queryForObject("SELECT * FROM PRODUCT WHERE id = ?", productRowMapper, id);
+		assertThat(findProduct.getName()).isEqualTo(newProduct.getName());
+		assertThat(findProduct.getImage()).isEqualTo(newProduct.getImage());
+		assertThat(findProduct.getPrice()).isEqualTo(newProduct.getPrice());
+	}
+
+	@Test
+	void deleteById() {
+		//given
+		long id = productDao.save(product);
+
+		//when
+		productDao.deleteById(id);
+
+		//then
+		assertThatThrownBy(
+			() -> jdbcTemplate.queryForObject("SELECT * FROM PRODUCT WHERE id = ?", productRowMapper, id))
+			.isInstanceOf(EmptyResultDataAccessException.class);
+
+	}
+}
