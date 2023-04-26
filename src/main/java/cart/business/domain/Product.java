e89b1825aae214da0e57f55a94427d45be8aa54a
eunbii0213 <eunbii0213@naver.com>
2023-04-26 21:57:10 +0900

    refactor: Product id Wrapper 타입으로 변경

--- src/main/java/cart/business/domain/Product.java
+++ src/main/java/cart/business/domain/Product.java
@@ -2,19 +2,19 @@ package cart.business.domain;
 
 public class Product {
 
-    private final int id;
+    private final Integer id;
     private final ProductName name;
     private final ProductImage image;
     private final ProductPrice productPrice;
 
-    public Product(int id, ProductName name, ProductImage image, ProductPrice productPrice) {
+    public Product(Integer id, ProductName name, ProductImage image, ProductPrice productPrice) {
         this.id = id;
         this.name = name;
         this.image = image;
         this.productPrice = productPrice;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
@@ -22,7 +22,7 @@ public class Product {
         return name.getValue();
     }
 
-    public String getUrl() {
+    public String getImage() {
         return image.getValue();
     }
 
