98dd2c68133b28e08663ddd23744db2cfa62058f
hong-sile <gurwns9325@naver.com>
2023-04-26 20:55:51 +0900

    test: ApplicationContext가 한 번만 생성되도록 수정

--- src/test/java/cart/controller/IntegrationTest.java
+++ src/test/java/cart/controller/IntegrationTest.java
@@ -8,14 +8,12 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureTestDatabase
 class IntegrationTest {
 
     @LocalServerPort
