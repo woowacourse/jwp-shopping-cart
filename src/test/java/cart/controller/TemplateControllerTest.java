c5e7a81aecdeeb9aaf6b9583cd9277598b019453
odo27 <odo27@naver.com>
2023-04-26 13:53:00 +0900

    fix: TemplateControllerTest 수정

--- src/test/java/cart/controller/TemplateControllerTest.java
+++ src/test/java/cart/controller/TemplateControllerTest.java
@@ -11,16 +11,12 @@ import java.util.List;
 import cart.dto.ProductResponse;
 import cart.dto.ResponseFixture;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
 
 @SuppressWarnings("NonAsciiCharacters")
 @WebMvcTest
-class TemplateControllerTest {
-    @Autowired
-    private MockMvc mockMvc;
+class TemplateControllerTest extends AbstractProductControllerTest {
 
     @Test
     void 상품_조회_테스트() throws Exception {
