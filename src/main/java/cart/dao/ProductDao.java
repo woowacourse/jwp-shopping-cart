8ba0d1d44209aabd732c1b215856a001ffc91f01
Miseong Kim <miseongk@naver.com>
2023-04-26 11:33:54 +0900

    refactor: rowMapper 변수 제거

--- src/main/java/cart/dao/ProductDao.java
+++ src/main/java/cart/dao/ProductDao.java
@@ -1,23 +1,12 @@
 package cart.dao;
 
 import cart.entity.Product;
-import java.util.ArrayList;
 import java.util.List;
-import org.apache.catalina.webresources.AbstractArchiveResource;
-import org.springframework.boot.autoconfigure.batch.BatchProperties.Jdbc;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductDao {
-
-    private final RowMapper<Product> rowMapper = (resultSet, rowNum) ->
-        new Product.Builder().id(resultSet.getInt("id"))
-                .price(resultSet.getInt("price"))
-                .name(resultSet.getString("name"))
-                .imageUrl(resultSet.getString("image_url"))
-                .build();
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -27,7 +16,15 @@ public class ProductDao {
 
     public List<Product> findAll() {
         String sql = "SELECT * FROM Product";
-        return jdbcTemplate.query(sql, rowMapper);
+        return jdbcTemplate.query(
+                sql,
+                (resultSet, rowNum) ->
+                        new Product.Builder().id(resultSet.getInt("id"))
+                                .price(resultSet.getInt("price"))
+                                .name(resultSet.getString("name"))
+                                .imageUrl(resultSet.getString("image_url"))
+                                .build()
+        );
     }
 
     public void save(Product product) {
