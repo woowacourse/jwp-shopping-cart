0c94d84aff4a8b36b534f9fb2fcd4da528b46793
hectick <ch.yeondev@gmail.com>
2023-04-26 11:07:39 +0900

    test: DB 에서 상품을 등록하는 기능 테스트, 테스트 전략 변경 Mockito -> JdbcTest
    Co-authored-by: 70825 <hello70825@gmail.com>

--- src/test/java/cart/dao/JdbcProductDaoTest.java
+++ src/test/java/cart/dao/JdbcProductDaoTest.java
@@ -3,48 +3,35 @@ package cart.dao;
 import cart.entity.ProductEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.context.annotation.Import;
 
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-@ExtendWith(MockitoExtension.class)
+@Import(JdbcProductDao.class)
+@JdbcTest
 class JdbcProductDaoTest {
 
-    @InjectMocks
-    private JdbcProductDao productDao;
-
-    @Mock
-    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    JdbcProductDao productDao;
 
     @DisplayName("모든 상품 데이터를 제대로 반환하는지 확인한다")
     @Test
     void selectAllTest() {
-        final List<ProductEntity> data = List.of(
-                ProductEntity.of(1L, "chicken", "image", 10000),
-                ProductEntity.of(2L, "pizza", "image2", 20000)
-        );
-        when(productDao.selectAll()).thenReturn(data);
-
         final List<ProductEntity> productEntities = productDao.selectAll();
 
-        assertAll(
-                () -> assertThat(productEntities.size()).isEqualTo(data.size()),
-                () -> assertThat(productEntities.get(0).getId()).isEqualTo(1L),
-                () -> assertThat(productEntities.get(0).getName()).isEqualTo("chicken"),
-                () -> assertThat(productEntities.get(0).getImage()).isEqualTo("image"),
-                () -> assertThat(productEntities.get(0).getPrice()).isEqualTo(10000),
-                () -> assertThat(productEntities.get(1).getId()).isEqualTo(2L),
-                () -> assertThat(productEntities.get(1).getName()).isEqualTo("pizza"),
-                () -> assertThat(productEntities.get(1).getImage()).isEqualTo("image2"),
-                () -> assertThat(productEntities.get(1).getPrice()).isEqualTo(20000)
-        );
+        assertThat(productEntities.size()).isEqualTo(2);
+    }
+
+    @DisplayName("상품 등록이 제대로 되는지 확인한다")
+    @Test
+    void insertTest() {
+        ProductEntity productEntity = ProductEntity.of("chicken", "image", 10000);
+
+        assertDoesNotThrow(() -> productDao.insert(productEntity));
     }
 }
