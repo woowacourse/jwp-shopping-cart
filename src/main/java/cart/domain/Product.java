29560d511fbbbd3c66430bd55b9ce328bcab2888
yoondgu <doyou2ing@naver.com>
2023-04-26 16:17:27 +0900

    refactor: 도메인 검증로직 관련 상수화

--- src/main/java/cart/domain/Product.java
+++ src/main/java/cart/domain/Product.java
@@ -7,6 +7,13 @@ import lombok.Getter;
 @EqualsAndHashCode
 public class Product {
 
+    private static final int NAME_LENGTH_MINIMUM = 1;
+    private static final int NAME_LENGTH_MAXIMUM = 30;
+    private static final int IMAGE_URL_LENGTH_MINIMUM = 1;
+    private static final int IMAGE_URL_LENGTH_MAXIMUM = 1000;
+    private static final int PRICE_MINIMUM = 0;
+    private static final int PRICE_MAXIMUM = 1_000_000_000;
+
     private final Long id;
     private final String name;
     private final String imageUrl;
@@ -25,8 +32,7 @@ public class Product {
     public Product(final String name, final String imageUrl, final int price) {
         this(null, name, imageUrl, price);
     }
-
-
+    
     private void validate(final String name, final String imageUrl, final int price) {
         validateName(name);
         validateImageUrl(imageUrl);
@@ -37,7 +43,7 @@ public class Product {
         if (name.isBlank()) {
             throw new IllegalArgumentException("상품 이름은 빈 문자열일 수 없습니다");
         }
-        if (name.length() < 1 || name.length() > 30) {
+        if (name.length() < NAME_LENGTH_MINIMUM || name.length() > NAME_LENGTH_MAXIMUM) {
             throw new IllegalArgumentException("상품 이름은 최소 1, 최대 30 글자입니다");
         }
     }
@@ -46,13 +52,13 @@ public class Product {
         if (imageUrl.isBlank()) {
             throw new IllegalArgumentException("이미지 URL은 빈 문자열일 수 없습니다");
         }
-        if (imageUrl.length() < 1 || imageUrl.length() > 1000) {
+        if (imageUrl.length() < IMAGE_URL_LENGTH_MINIMUM || imageUrl.length() > IMAGE_URL_LENGTH_MAXIMUM) {
             throw new IllegalArgumentException("이미지 URL은 최소 1, 최대 1000 글자입니다");
         }
     }
 
     private void validatePrice(int price) {
-        if (price < 0 || price > 1_000_000_000) {
+        if (price < PRICE_MINIMUM || price > PRICE_MAXIMUM) {
             throw new IllegalArgumentException("유효한 상품 금액이 아닙니다.");
         }
     }
