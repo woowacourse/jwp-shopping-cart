74ba6634133b5b34afa4f8a516744d0e62d93702
iamjooon2 <iamjooon2@gmail.com>
2023-04-26 15:39:32 +0900

    feat: 예외 처리 코드 추가
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -1,13 +1,15 @@
 package cart.controller;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(IllegalArgumentException.class)
+    @ExceptionHandler({IllegalArgumentException.class, MethodArgumentNotValidException.class, DataAccessException.class})
     public ResponseEntity<String> handle(IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
