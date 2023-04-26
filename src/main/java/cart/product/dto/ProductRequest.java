1e61e89888cfe390a84a4a6171243df3e3c0bb43
junpakPark <junpak.park@gmail.com>
2023-04-26 15:33:44 +0900

    refactor : validation 예외 메시지 추가

--- src/main/java/cart/product/dto/ProductRequest.java
+++ src/main/java/cart/product/dto/ProductRequest.java
@@ -7,11 +7,11 @@ import javax.validation.constraints.Size;
 import org.hibernate.validator.constraints.Range;
 
 public class ProductRequest {
-	@NotBlank
-	@Size(max = 255, message = "{max} 글자 이하만 입력 가능합니다.")
+	@NotBlank(message = "상품명을 입력해주세요.")
+	@Size(max = 20, message = "{max} 글자 이하만 입력 가능합니다.")
 	private final String name;
 
-	@NotNull
+	@NotNull(message = "상품가격을 입력해주세요.")
 	@Range(min = 0, message = "상품 금액은 {min}원 이상의 정수만 입력가능 합니다.")
 	private final Integer price;
 	private final String image;
