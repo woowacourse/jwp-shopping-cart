e9ce80f4aa0b8d7af0defc6baeca702ca901ab9b
hectick <ch.yeondev@gmail.com>
2023-04-26 11:54:51 +0900

    test: 메인 페이지에서 상품 전체 목록 조회에 대한 응답 기능 테스트
    Co-authored-by: 70825 <hello70825@gmail.com>

---
+++ src/test/java/cart/controller/MainControllerTest.java
@@ -0,0 +1,31 @@
+package cart.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MainControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("상품 전체 목록을 조회하면 상태코드 200을 반환하는지 확인")
+    @Test
+    void getHomeTest() {
+        RestAssured.given().log().all()
+                .when().get("/")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+}
