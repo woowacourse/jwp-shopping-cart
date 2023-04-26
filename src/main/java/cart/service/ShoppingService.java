70766392b296f4eee042db3dad027efcdde64657
jiwon <ljwon77@daum.net>
2023-04-26 14:10:45 +0900

    test: 서비스 레이어의 테스트를 추가한다
    Co-authored-by: HubCreator <chocomilk8604@gmail.com>

--- src/main/java/cart/service/ShoppingService.java
+++ src/main/java/cart/service/ShoppingService.java
@@ -5,7 +5,6 @@ import cart.exception.ErrorCode;
 import cart.exception.GlobalException;
 import cart.persistence.dao.ProductDao;
 import cart.persistence.entity.Product;
-import cart.persistence.entity.ProductCategory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -24,17 +23,17 @@ public class ShoppingService {
         final List<Product> products = productDao.findAll();
         return products.stream()
                 .map(product -> new ProductDto(product.getId(), product.getName(), product.getImageUrl(),
-                        product.getPrice(), product.getCategory().name())).collect(Collectors.toList());
+                        product.getPrice(), product.getCategory())).collect(Collectors.toList());
     }
 
     public void save(final ProductDto productDto) {
         productDao.insert(new Product(productDto.getId(), productDto.getName(), productDto.getImageUrl(),
-                productDto.getPrice(), ProductCategory.from(productDto.getCategory())));
+                productDto.getPrice(), productDto.getCategory()));
     }
 
     public void update(final Long id, final ProductDto productDto) {
         int updatedCount = productDao.update(new Product(id, productDto.getName(), productDto.getImageUrl(),
-                productDto.getPrice(), ProductCategory.from(productDto.getCategory())));
+                productDto.getPrice(), productDto.getCategory()));
         if (updatedCount != 1) {
             throw new GlobalException(ErrorCode.PRODUCT_NOT_FOUND);
         }
