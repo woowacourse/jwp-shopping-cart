5c63eea4ccab2d0f6a49e07a0ccc8d8ed6157406
mcodnjs <codnjs8989@gmail.com>
2023-04-26 21:08:11 +0900

    feat: ProductDao findById 메서드 구현
    Co-authored-by: jeomxon <jeomxon@gmail.com>

--- src/main/java/cart/dao/ProductDao.java
+++ src/main/java/cart/dao/ProductDao.java
@@ -2,47 +2,61 @@ package cart.dao;
 
 import cart.controller.dto.ProductRequest;
 import cart.domain.Product;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Repository
 public class ProductDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    private final RowMapper<Product> productRowMapper = (resultSet, rowNum) -> new Product(
+            resultSet.getLong("id"),
+            resultSet.getString("name"),
+            resultSet.getString("image_url"),
+            resultSet.getInt("price")
+    );
 
     public ProductDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("product")
+                .usingGeneratedKeyColumns("id");
     }
 
-    public void save(final ProductRequest productRequest) {
-        String sql = "INSERT INTO product (name, price, image_url) VALUES (?, ?, ?)";
+    public Long save(final ProductRequest productRequest) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("name", productRequest.getName());
+        params.put("price", productRequest.getPrice());
+        params.put("image_url", productRequest.getImageUrl());
 
-        jdbcTemplate.update(sql, ps -> {
-            ps.setString(1, productRequest.getName());
-            ps.setInt(2, productRequest.getPrice());
-            ps.setString(3, productRequest.getImageUrl());
-        });
+        return simpleJdbcInsert.executeAndReturnKey(params).longValue();
     }
 
     public List<Product> findAll() {
         String sql = "SELECT * FROM product";
 
-        return jdbcTemplate.query(sql, resultSet -> {
-            List<Product> products = new ArrayList<>();
-            while (resultSet.next()) {
-                Product product = new Product(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getInt(4)
-                );
-                products.add(product);
-            }
-            return products;
-        });
+        return jdbcTemplate.query(sql, productRowMapper);
+    }
+
+    public Optional<Product> findById(final Long productId) {
+        String sql = "SELECT * from product where id = ?";
+
+        try {
+            Product product = jdbcTemplate.queryForObject(sql, productRowMapper, productId);
+            return Optional.ofNullable(product);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
     }
 
     public void updateById(final Long id, final ProductRequest productRequest) {
