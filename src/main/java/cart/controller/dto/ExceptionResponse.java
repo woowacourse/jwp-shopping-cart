31221eede32ea2017012d2fdfcdee69ea9b7814e
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-27 16:39:35 +0900

    refactor: ExceptionResponse 필드 명 변경

--- src/main/java/cart/controller/dto/ExceptionResponse.java
+++ src/main/java/cart/controller/dto/ExceptionResponse.java
@@ -2,13 +2,13 @@ package cart.controller.dto;
 
 public class ExceptionResponse<T> {
 
-    private final T message;
+    private final T response;
 
-    public ExceptionResponse(T message) {
-        this.message = message;
+    public ExceptionResponse(T response) {
+        this.response = response;
     }
 
-    public T getMessage() {
-        return message;
+    public T getResponse() {
+        return response;
     }
 }
