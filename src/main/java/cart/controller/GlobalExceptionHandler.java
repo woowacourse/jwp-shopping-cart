f270731cf955a6eec5728696159c28482b117cda
jiwon <ljwon77@daum.net>
2023-04-28 01:08:31 +0900

    refactor: controllerAdvice 클래스의 네이밍을 변경한다

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestControllerAdvice
-public class GlobalControllerAdvice {
+public class GlobalExceptionHandler {
 
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<ErrorResponse> globalException(final GlobalException e) {
