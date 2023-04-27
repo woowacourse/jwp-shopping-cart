bae372a12f5979586e82c41af28cd4f3c2baf006
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-04-27 17:07:10 +0900

    refactor: fieldsError -> 추상적인 메시지를 던지도록 변경

--- src/main/java/cart/controller/ControllerAdvice.java
+++ src/main/java/cart/controller/ControllerAdvice.java
@@ -2,13 +2,10 @@ package cart.controller;
 
 import cart.controller.dto.ExceptionResponse;
 import cart.exception.ItemException;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -20,21 +17,15 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ControllerAdvice extends ResponseEntityExceptionHandler {
 
     private static final String INTERNAL_ERROR_MESSAGE = "서버에서 예상치 못한 문제가 발생했습니다.";
-    private static final String NOT_READABLE_MESSAGE = "잘못된 입력입니다. 입력을 확인하세요.";
+    private static final String INPUT_NOT_READABLE_MESSAGE = "잘못된 입력입니다. 입력을 확인하세요.";
     private static final String PATH_VARIABLE_ERROR_MESSAGE = "요청 URI 형식이 잘못되었습니다.";
 
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status,
                                                                   WebRequest request) {
-        Map<String, String> fieldErrors = ex.getBindingResult().getFieldErrors()
-                .stream()
-                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
-
-        fieldErrors.values()
-                .forEach(logger::warn);
-
-        return ResponseEntity.badRequest().body(new ExceptionResponse<>(fieldErrors));
+        logger.warn(ex.getMessage());
+        return ResponseEntity.badRequest().body(new ExceptionResponse<>(INPUT_NOT_READABLE_MESSAGE));
     }
 
     @Override
@@ -42,7 +33,7 @@ public class ControllerAdvice extends ResponseEntityExceptionHandler {
                                                                   HttpHeaders headers, HttpStatus status,
                                                                   WebRequest request) {
         logger.warn(ex.getMessage());
-        return ResponseEntity.badRequest().body(new ExceptionResponse<>(NOT_READABLE_MESSAGE));
+        return ResponseEntity.badRequest().body(new ExceptionResponse<>(INPUT_NOT_READABLE_MESSAGE));
     }
 
     @Override
