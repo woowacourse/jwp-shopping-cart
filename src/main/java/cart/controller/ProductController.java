3ded68cfebbfd6c8f584fa5cce2d5f80e32b5eaa
Cyma-s <ssol0319@naver.com>
2023-04-27 15:00:31 +0900

    refactor: PutMapping으로 변경

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -7,9 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,7 +30,7 @@ public class ProductController {
         productService.registerProduct(productRegisterRequest);
     }
 
-    @PatchMapping("/{product-id}")
+    @PutMapping("/{product-id}")
     public String modifyProduct(
             @PathVariable("product-id") final Long productId,
             @Validated @RequestBody final ProductModifyRequest productModifyRequest
