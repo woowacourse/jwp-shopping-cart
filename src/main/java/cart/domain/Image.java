cefc59c9fe1881c058fa4355566cfbb5a5998330
Choi-JJunho <junho5336@gmail.com>
2023-04-27 15:27:51 +0900

    feat: image URL 검증 추가
    Co-authored-by: youngh0 <cyh6099@gmail.com>

--- src/main/java/cart/domain/Image.java
+++ src/main/java/cart/domain/Image.java
@@ -1,5 +1,8 @@
 package cart.domain;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Image {
     private final String url;
 
@@ -11,6 +14,11 @@ public class Image {
     private void validateUrl(String url) {
         if (url == null || url.isBlank()) {
             throw new IllegalArgumentException("이미지 URL은 비어있을 수 없습니다.");
+        }
+        try {
+            new URL(url);
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("적절한 형태의 url이 아닙니다.");
         }
     }
 
