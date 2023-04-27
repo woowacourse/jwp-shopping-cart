189c4c3e82e7d74b9cbf4dab3e6ece4726753526
BGuga <tmdwns9913@gmail.com>
2023-04-27 16:08:05 +0900

    test: dao 테스트 작성

--- src/test/java/cart/repository/ProductDaoTest.java
+++ src/test/java/cart/repository/ProductDaoTest.java
@@ -1,16 +1,21 @@
 package cart.repository;
 
+import cart.dto.ProductRequestDto;
 import cart.entity.ProductEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @JdbcTest
@@ -30,7 +35,7 @@ class ProductDaoTest {
     }
 
     @Test
-    @DisplayName("정상적인 request 는 정상 생성한다.")
+    @DisplayName("상품 생성 테스트")
     void create() {
         //given
         final ProductEntity request = new ProductEntity("test", "dully.jpg", 100);
@@ -56,4 +61,68 @@ class ProductDaoTest {
         );
     }
 
+    @Test
+    @DisplayName("상품 전체 조회 테스트")
+    @Sql(scripts = "/dummy_data.sql")
+    void findALl() {
+        //given,when
+        final List<ProductEntity> allProducts = productDao.findAll();
+
+        //then
+        assertAll(
+                () -> assertThat(allProducts).hasSize(3),
+                () -> assertThat(allProducts.get(0).getName()).isEqualTo("pooh"),
+                () -> assertThat(allProducts.get(0).getImage()).isEqualTo("pooh.jpg"),
+                () -> assertThat(allProducts.get(0).getPrice()).isEqualTo(1_000_000),
+                () -> assertThat(allProducts.get(2).getPrice()).isEqualTo(10)
+        );
+    }
+
+    @Test
+    @DisplayName("수정 테스트")
+    @Sql(scripts = "/dummy_data.sql")
+    void update() {
+        //given
+        final ProductRequestDto requestDto = new ProductRequestDto("푸우", "pooh.png", 1_000_001);
+        final Integer targetId = findPoohId();
+
+        //when
+        productDao.update(requestDto, targetId);
+        final String findUpdatedUserSql = "select * from product where id = ?";
+        final ProductEntity updatedProduct = jdbcTemplate.queryForObject(findUpdatedUserSql,
+                (rs, rowNum) -> new ProductEntity(
+                        rs.getString("name"),
+                        rs.getString("image"),
+                        rs.getInt("price")
+                ),
+                targetId);
+
+        //then
+        assertAll(
+                () -> assertThat(updatedProduct.getName()).isEqualTo("푸우"),
+                () -> assertThat(updatedProduct.getImage()).isEqualTo("pooh.png"),
+                () -> assertThat(updatedProduct.getPrice()).isEqualTo(1_000_001)
+        );
+    }
+
+    @Test
+    @DisplayName("유저의 정보를 삭제한다")
+    @Sql(scripts = "/dummy_data.sql")
+    void delete() {
+        //given
+        final Integer poohId = findPoohId();
+
+        //when
+        productDao.delete(poohId);
+
+        //then
+        String sql = "select * from product where id = ?";
+        assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, Integer.class, poohId))
+                .isInstanceOf(DataAccessException.class);
+    }
+
+    private Integer findPoohId() {
+        final String sql = "select id from product where name = 'pooh'";
+        return jdbcTemplate.queryForObject(sql, Integer.class);
+    }
 }
