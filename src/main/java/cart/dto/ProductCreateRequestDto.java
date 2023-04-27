9afc9faabdcc660f6896997d6d5cf47776ae047f
sosow0212 <sosow0212@naver.com>
2023-04-27 15:22:38 +0900

    refactor: 상품명 사이즈 검증 및 생성자의 기본 접근제어자 private로 변경

--- src/main/java/cart/dto/ProductCreateRequestDto.java
+++ src/main/java/cart/dto/ProductCreateRequestDto.java
@@ -1,12 +1,13 @@
 package cart.dto;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.PositiveOrZero;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.*;
 
 public class ProductCreateRequestDto {
 
     @NotBlank(message = "상품명은 공백일 수 없습니다.")
+    @Length(max = 10, message = "상품명은 최대 10자까지 가능합니다.")
     private String name;
 
     @NotNull(message = "가격은 공백일 수 없습니다.")
@@ -16,7 +17,7 @@ public class ProductCreateRequestDto {
     @NotNull(message = "상품 이미지 url을 넣어주세요.")
     private String imgUrl;
 
-    public ProductCreateRequestDto() {
+    private ProductCreateRequestDto() {
     }
 
     public ProductCreateRequestDto(final String name, final Integer price, final String imgUrl) {
--- src/main/java/cart/dto/ProductEditRequestDto.java
+++ src/main/java/cart/dto/ProductEditRequestDto.java
@@ -1,5 +1,7 @@
 package cart.dto;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
@@ -7,6 +9,7 @@ import javax.validation.constraints.PositiveOrZero;
 public class ProductEditRequestDto {
 
     @NotBlank(message = "상품명은 공백일 수 없습니다.")
+    @Length(max = 10, message = "상품명은 최대 10자까지 가능합니다.")
     private String name;
 
     @NotNull(message = "가격은 공백일 수 없습니다.")
@@ -16,7 +19,7 @@ public class ProductEditRequestDto {
     @NotNull(message = "상품 이미지 url을 넣어주세요.")
     private String imgUrl;
 
-    public ProductEditRequestDto() {
+    private ProductEditRequestDto() {
 
     }
 
