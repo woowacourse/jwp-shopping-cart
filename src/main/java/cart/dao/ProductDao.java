b429493e9016c6ab618a82f5e28e0e25880735ff
Junhee Lee <iamjooon2@gmail.com>
2023-04-27 23:48:08 +0900

    [1단계 - 상품 관리 기능] 가비(이준희) 미션 제출합니다. (#251)
    * docs: 기능 명세 작성
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * docs: 기능 명세 작성
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: DB 관련 설정 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 상품 목록 페이지 연동 구현
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 관리자 페이지 연동 구현
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * build: validation 의존성 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 상품 생성 및 저장 기능 구현
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 상품 수정 기능 구현
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 상품 삭제 기능 구현
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 상품 도메인 기능 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 예외 처리 기능 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 패키지 구조 변경
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 네이밍 수정 및 Dto 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * feat: 예외 처리 코드 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * test: dao 테스트 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * test: dao 테스트 추가 및 수정
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: 유효성 검사 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * test: 컨트롤러 테스트 구현
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: @Valid 예외 메시지 추가
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * test: Controller 테스트 Dao 의존성 제거
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * test: 전체 테스트 작성
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * style: 라인 포매팅 변경
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: Controller 예외 처리 로직 변경
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * style: 코드 형식 통일 및 정리
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * docs: 기능 요구 사항 정리
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>
    
    * refactor: response, request 패키지 분리
    
    * refactor: dto 내 request, response 패키지 분리
    
    * feat: @Transactional 어노테이션 추가
    
    * feat: @Transactional 어노테이션 제거
    
    ---------
    
    Co-authored-by: Combi153 <jcm04@naver.com>
    Co-authored-by: swonny <wonny921@gmail.com>

---
+++ src/main/java/cart/dao/ProductDao.java
@@ -0,0 +1,78 @@
+package cart.dao;
+
+import cart.dao.entity.ProductEntity;
+import cart.domain.Product;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public ProductDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<ProductEntity> selectAll() {
+        final String sql = "SELECT * FROM PRODUCT";
+        return jdbcTemplate.query(sql, getProductRowMapper());
+    }
+
+    private RowMapper<ProductEntity> getProductRowMapper() {
+        return (resultSet, rowNum) -> new ProductEntity(
+                resultSet.getLong("id"),
+                resultSet.getString("name"),
+                resultSet.getInt("price"),
+                resultSet.getString("image")
+        );
+    }
+
+    public Long insert(final Product product) {
+        final String sql = "INSERT INTO PRODUCT (name, price, image) VALUES (?, ?, ?)";
+        final KeyHolder keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(new PreparedStatementCreator() {
+            @Override
+            public PreparedStatement createPreparedStatement(final Connection con) throws SQLException {
+                final PreparedStatement preparedStatement = con.prepareStatement(
+                        sql, new String[]{"ID"}
+                );
+                preparedStatement.setString(1, product.getName());
+                preparedStatement.setInt(2, product.getPrice());
+                preparedStatement.setString(3, product.getImage());
+                return preparedStatement;
+            }
+        }, keyHolder);
+        return keyHolder.getKey().longValue();
+    }
+
+    public int update(final Product product, final Long id) {
+        final String sql = "UPDATE PRODUCT SET name = ?, price = ?, image = ? WHERE id = ?";
+        return jdbcTemplate.update(
+                sql,
+                product.getName(),
+                product.getPrice(),
+                product.getImage(),
+                id
+        );
+    }
+
+    public int delete(final Long id) {
+        final String sql = "DELETE FROM product WHERE id = ?";
+        return jdbcTemplate.update(sql, id);
+    }
+
+    public ProductEntity findById(final Long id) {
+        final String sql = "SELECT * from product where id = ?";
+        return jdbcTemplate.queryForObject(sql, getProductRowMapper(), id);
+    }
+}
