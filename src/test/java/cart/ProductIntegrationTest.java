6a240447db4be3f59ee8d0e2078625d39b068bfb
kokodak <kokodakadokok@gmail.com>
2023-04-27 16:01:20 +0900

    test: 통합 테스트 코드 작성
    Co-authored-by: jundonghyuk <ehdgur4814@naver.com>

--- src/test/java/cart/ProductIntegrationTest.java
+++ src/test/java/cart/ProductIntegrationTest.java
@@ -1,16 +1,20 @@
 package cart;
 
+import static io.restassured.RestAssured.given;
+
+import cart.dto.ProductDto;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-import static io.restassured.RestAssured.given;
-import static org.assertj.core.api.Assertions.assertThat;
-
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ProductIntegrationTest {
 
@@ -23,15 +27,64 @@ public class ProductIntegrationTest {
     }
 
     @Test
-    public void getProducts() {
-        var result = given()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .when()
-                .get("/products")
+    void 상품을_조회한다() {
+        given()
+                .when().get("/admin/products")
                 .then()
-                .extract();
-
-        assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
+                .statusCode(HttpStatus.OK.value());
     }
 
+    @Test
+    void 상품을_추가한다() {
+        final ProductDto productDto = new ProductDto("하디", "imageUrl", 10000);
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(productDto)
+                .when().post("/admin/products")
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 상품을_업데이트한다() {
+        final ProductDto productDto = new ProductDto("하디", "image", 100000);
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(productDto)
+                .when().post("/admin/products")
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        final Long id = 1L;
+        final ProductDto updatedProductDto = new ProductDto("코코닥", "imageUrl", 10000);
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(updatedProductDto)
+                .when().patch("/admin/products/{product_id}", id)
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 상품을_삭제한다() {
+        final ProductDto productDto = new ProductDto("하디", "image", 100000);
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(productDto)
+                .when().post("/admin/products")
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        final Long id = 1L;
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete("/admin/products/{product_id}", id)
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
 }
