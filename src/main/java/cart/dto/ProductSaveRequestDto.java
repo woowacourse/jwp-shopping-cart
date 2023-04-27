69b8fefaae00d68c488bd4bf2e37eb07193a1ba3
songusika <sws981202@gmail.com>
2023-04-27 15:58:55 +0900

    refactor: 예외메시지 수정
    Co-authored-by: greeng00se <thegreengoose7@gmail.com>

--- src/main/java/cart/dto/ProductSaveRequestDto.java
+++ src/main/java/cart/dto/ProductSaveRequestDto.java
@@ -7,13 +7,13 @@ import org.hibernate.validator.constraints.Range;
 public class ProductSaveRequestDto {
 
     @NotBlank(message = "이름은 공백일 수 없습니다.")
-    @Size(min = 1, max = 100, message = "이름은 최소 {min} 이상, {max} 이하여야 합니다.")
+    @Size(min = 1, max = 100, message = "이름은 최소 {min}자 이상, {max}자 이하여야 합니다.")
     private final String name;
 
     @NotBlank(message = "이미지는 공백일 수 없습니다.")
     private final String image;
 
-    @Range(message = "가격은 최소 {min} 이상, {max} 이하여야 합니다.")
+    @Range(message = "가격은 최소 {min}원 이상, {max}원 이하여야 합니다.")
     private final long price;
 
     public ProductSaveRequestDto(final String name, final String image, final long price) {
