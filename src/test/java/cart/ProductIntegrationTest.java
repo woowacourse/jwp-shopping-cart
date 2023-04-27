12c57e0d35c4b8a8494cc275cc2c23f6199c35d1
Woojin Lee <dhkep03@gmail.com>
2023-04-27 15:35:37 +0900

    test: 통합 테스트 수정

--- src/test/java/cart/ProductIntegrationTest.java
+++ src/test/java/cart/ProductIntegrationTest.java
@@ -8,10 +8,10 @@ import cart.controller.dto.ModifyRequest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
-import io.restassured.path.xml.element.Node;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.path.xml.XmlPath.CompatibilityMode;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBodyExtractionOptions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -40,8 +40,10 @@ public class ProductIntegrationTest {
         // 2개의 상품 추가
         ModifyRequest request1 = new ModifyRequest("사과", 100, "super.com");
         ModifyRequest request2 = new ModifyRequest("당근", 1000, "super.com");
+        ModifyRequest request3 = new ModifyRequest("포도", 2000, "super.com");
         String jsonRequest1 = objectMapper.writeValueAsString(request1);
         String jsonRequest2 = objectMapper.writeValueAsString(request2);
+        String jsonRequest3 = objectMapper.writeValueAsString(request3);
 
         given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -55,12 +57,18 @@ public class ProductIntegrationTest {
                 .when()
                 .post("/admin/product");
 
-        // 1개의 상품 수정
-        ModifyRequest request3 = new ModifyRequest("애플", 1000, "super.com");
-        String jsonRequest3 = objectMapper.writeValueAsString(request3);
         given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .body(jsonRequest3)
+                .when()
+                .post("/admin/product");
+
+        // 1개의 상품 수정
+        ModifyRequest request4 = new ModifyRequest("사과즙", 1000, "super.com");
+        String jsonRequest4 = objectMapper.writeValueAsString(request4);
+        given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(jsonRequest4)
                 .when()
                 .put("admin/product/1");
 
@@ -76,14 +84,19 @@ public class ProductIntegrationTest {
                 .then().log().all()
                 .extract();
 
-        // 검증
-        ResponseBodyExtractionOptions body = result.body();
-        Node strings = body.htmlPath().get();
+        // XmlPath로 HTML 내부 정보 Parsing
+        String resultString = result.asString();
+        XmlPath xmlPath = new XmlPath(CompatibilityMode.HTML, resultString);
         assertAll(
-                () -> assertThat(strings.toString()).contains("애플"),
-                () -> assertThat(strings.toString()).doesNotContain("사과"),
-                () -> assertThat(strings.toString()).doesNotContain("당근")
+                () -> assertThat(
+                        xmlPath.getString("html.body.div.table.tbody.tr[0].td[1]")
+                ).contains("사과즙"),
+                () -> assertThat(
+                        xmlPath.getString("html.body.div.table.tbody.tr[0].td[2]")
+                ).contains("1000"),
+                () -> assertThat(
+                        xmlPath.getString("html.body.div.table.tbody.tr[1].td[1]")
+                ).contains("포도")
         );
-
     }
 }
