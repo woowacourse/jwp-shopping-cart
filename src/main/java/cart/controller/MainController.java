808d6085bc0b9e3c3f2e82594ea203e5fa61b428
BGuga <tmdwns9913@gmail.com>
2023-04-27 13:42:33 +0900

    feat: root 경로애 대한 get api 제공

--- src/main/java/cart/controller/MainController.java
+++ src/main/java/cart/controller/MainController.java
@@ -6,7 +6,6 @@ import cart.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -20,15 +19,16 @@ public class MainController {
     }
 
     @GetMapping("/admin")
-    public String adminPage(final Model model){
-        final List<ProductEntity> all = productService.findAll();
-        model.addAttribute("products", all);
+    public String adminPage(final Model model) {
+        final List<ProductEntity> allProducts = productService.findAll();
+        model.addAttribute("products", allProducts);
         return "admin";
     }
 
-//    @GetMapping
-//    public String rootPage(Model model){
-//        final List<ProductEntity>
-//        return "index";
-//    }
+    @GetMapping
+    public String rootPage(Model model) {
+        final List<ProductEntity> allProducts = productService.findAll();
+        model.addAttribute("products", allProducts);
+        return "index";
+    }
 }
