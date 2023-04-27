000c8faa67f8cce0fa1bc10e398e0c4e30f421a7
sosow0212 <sosow0212@naver.com>
2023-04-27 16:22:57 +0900

    feat: member 테이블 생성 및 mock 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS member;
 
 CREATE TABLE product
 (
@@ -9,6 +10,18 @@ CREATE TABLE product
     PRIMARY KEY (id)
 );
 
+CREATE TABLE member
+(
+    id       BIGINT AUTO_INCREMENT,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 INSERT INTO product(id, name, price, img_url)
 VALUES (1, '상품명 테스트1', 100, 'https://cdn.pixabay.com/photo/2014/06/03/19/38/test-361512_960_720.jpg'),
        (2, '상품명 테스트2', 101, 'https://t1.daumcdn.net/cartoon/7520d517fbe0271cdceb64e57ff1b26aa2abb680');
+
+INSERT INTO member(id, email, password)
+VALUES (1, 'test1@test.com', '!!abc123'),
+       (2, 'test2@test.com', '!!abc123')
