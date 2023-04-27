7156aad8775e3c3ad25295c62d66ca9b2fe013d6
0chil <0@chll.it>
2023-04-27 14:55:00 +0900

    refactor: InvalidFormatException, 복수 예외 메시지 대응

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -1,30 +1,40 @@
 package cart.controller;
 
-import cart.controller.dto.ExceptionResponse;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import java.util.stream.Collectors;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+import cart.controller.dto.ExceptionResponse;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    private static final String DELIMITER = ", ";
+
+    private static final String INVALID_FORMAT_EXCEPTION_MESSAGE = "형이 잘못되었습니다";
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(final IllegalArgumentException exception) {
+    public ResponseEntity<ExceptionResponse> handleIllegalArgument(final IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(new ExceptionResponse(exception.getMessage()));
     }
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, MethodArgumentTypeMismatchException.class})
-    public ResponseEntity<ExceptionResponse> handleValidationException(final MethodArgumentNotValidException exception) {
-        final String exceptionMessage = exception.getFieldErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.joining(DELIMITER));
+    @ExceptionHandler(InvalidFormatException.class)
+    public ResponseEntity<ExceptionResponse> handle() {
+        return ResponseEntity.badRequest().body(new ExceptionResponse(INVALID_FORMAT_EXCEPTION_MESSAGE));
+    }
 
-        return ResponseEntity.badRequest().body(new ExceptionResponse(exceptionMessage));
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException exception
+    ) {
+        final List<String> exceptionMessages = exception.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+        return ResponseEntity.badRequest().body(new ExceptionResponse(exceptionMessages));
     }
 }
--- src/main/java/cart/controller/dto/ExceptionResponse.java
+++ src/main/java/cart/controller/dto/ExceptionResponse.java
@@ -1,14 +1,20 @@
 package cart.controller.dto;
 
+import java.util.List;
+
 public class ExceptionResponse {
 
-    private final String message;
+    private final List<String> messages;
 
     public ExceptionResponse(final String message) {
-        this.message = message;
+        this.messages = List.of(message);
     }
 
-    public String getMessage() {
-        return message;
+    public ExceptionResponse(final List<String> messages) {
+        this.messages = messages;
+    }
+
+    public List<String> getMessages() {
+        return messages;
     }
 }
