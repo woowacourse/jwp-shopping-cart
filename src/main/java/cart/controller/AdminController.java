caa94c43c83b0c0f0b0a7d1e8c65b8c165568ea9
Ethan <kim76744dev@gmail.com>
2023-04-28 08:40:35 +0900

    refactor: @ModelAttribute를 사용해서 자동으로 view로 model을 넘기도록 수정

--- src/main/java/cart/controller/AdminController.java
+++ src/main/java/cart/controller/AdminController.java
@@ -1,24 +1,21 @@
 package cart.controller;
 
+import cart.dto.ProductRequest;
+import cart.dto.ProductResponse;
+import cart.service.ProductService;
 import java.net.URI;
 import java.util.List;
-
 import javax.validation.Valid;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import cart.dto.ProductRequest;
-import cart.dto.ProductResponse;
-import cart.service.ProductService;
 
 @Controller
 @RequestMapping("/admin")
@@ -31,10 +28,9 @@ public class AdminController {
     }
 
     @GetMapping
-    public String admin(final Model model) {
-        final List<ProductResponse> products = productService.findAll();
-        model.addAttribute("products", products);
-        return "admin";
+    @ModelAttribute("products")
+    public List<ProductResponse> admin() {
+        return productService.findAll();
     }
 
     @PostMapping("/product")
