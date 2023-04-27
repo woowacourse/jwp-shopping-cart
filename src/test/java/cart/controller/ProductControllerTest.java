7e7d399c7dc76b154e7f50e14c5dfb9279e2facc
This2sho <zx00018@naver.com>
2023-04-27 20:12:19 +0900

    refactor(test): 필요없는 쿼리 삭제

--- src/test/java/cart/controller/ProductControllerTest.java
+++ src/test/java/cart/controller/ProductControllerTest.java
@@ -22,17 +22,6 @@ class ProductControllerTest {
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
-        String productDropQuery = "DROP TABLE product IF EXISTS";
-        String productCreateQuery = "CREATE TABLE product(id BIGINT NOT NULL AUTO_INCREMENT,name VARCHAR(20) NOT NULL,price INT NOT NULL,image_url TEXT NOT NULL, PRIMARY KEY(id))";
-        String sampleQuery1 = "INSERT INTO product(name, price, image_url) VALUES ('피자', 13000, 'https://searchad-phinf.pstatic.net/MjAyMjEyMjdfMTE1/MDAxNjcyMTAxNTI0Nzg4.WfiSlsy9fTUQJ6q2FTGOaaOVU0QpSB0U1LvplKZQXzIg.H4UgI0VbKUszP7mzC3qhwpSMe15DluJnxjxVGDq_QUgg.PNG/451708-1fa87663-02e3-4303-b8a9-d7eea3676018.png?type=f160_160')";
-        String sampleQuery2 = "INSERT INTO product(name, price, image_url) VALUES ('치킨', 27000, 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzAzMjdfMTI2%2FMDAxNjc5OTI1ODQ4NTgy.6RT9z-i5prsnwwc-6B9TaK6Q0Zcgsd3TeDGiUdqyDRIg.rW2kMtzBKNFhWWXyr_X2bZfR15AEPUOz-VJnqVaP0jEg.JPEG.koreasinju%2FIMG_3379.jpg&type=ff332_332')";
-        String sampleQuery3 = "INSERT INTO product(name, price, image_url) VALUES ('샐러드', 9500, 'https://searchad-phinf.pstatic.net/MjAyMzA0MThfMjk0/MDAxNjgxODAwNDY4NjU4.FJcjmoGsxyCq0nZqlcmoAL2mwX8mM9ny9DdliQcqGZ0g.9cGk2IQHfPIm2-ABelEOY1cc-_8NBQgPMgPpjFZkGFEg.JPEG/2814800-bb4236af-96dd-42e7-8256-32ffaa73de52.jpg?type=f160_160')";
-
-        jdbcTemplate.execute(productDropQuery);
-        jdbcTemplate.execute(productCreateQuery);
-        jdbcTemplate.update(sampleQuery1);
-        jdbcTemplate.update(sampleQuery2);
-        jdbcTemplate.update(sampleQuery3);
     }
 
     @Test
@@ -42,12 +31,12 @@ class ProductControllerTest {
         final int requestPrice = 4_000;
         final String requestUrl = "none";
         final ProductRequest createRequest = new ProductRequest(requestName, requestPrice,
-            requestUrl);
+                requestUrl);
 
         RestAssured
-            .given().contentType(ContentType.JSON).body(createRequest)
-            .when().post("/products")
-            .then().statusCode(200);
+                .given().contentType(ContentType.JSON).body(createRequest)
+                .when().post("/products")
+                .then().statusCode(200);
     }
 
     @Test
@@ -57,12 +46,12 @@ class ProductControllerTest {
         final int requestPrice = 4_000;
         final String requestUrl = "none";
         final ProductRequest createRequest = new ProductRequest(requestName, requestPrice,
-            requestUrl);
+                requestUrl);
 
         RestAssured
-            .given().contentType(ContentType.JSON).body(createRequest)
-            .when().post("/products")
-            .then().statusCode(400).body(Matchers.containsString("유효하지 않은 값입니다."));
+                .given().contentType(ContentType.JSON).body(createRequest)
+                .when().post("/products")
+                .then().statusCode(400).body(Matchers.containsString("유효하지 않은 값입니다."));
     }
 
     @Test
@@ -72,12 +61,12 @@ class ProductControllerTest {
         final int requestPrice = 4_000;
         final String requestUrl = "none";
         final ProductRequest createRequest = new ProductRequest(requestName, requestPrice,
-            requestUrl);
+                requestUrl);
 
         RestAssured
-            .given().contentType(ContentType.JSON).body(createRequest)
-            .when().put("/products/1")
-            .then().statusCode(200);
+                .given().contentType(ContentType.JSON).body(createRequest)
+                .when().put("/products/1")
+                .then().statusCode(200);
     }
 
     @Test
@@ -87,12 +76,12 @@ class ProductControllerTest {
         final int requestPrice = 4_000;
         final String requestUrl = "none";
         final ProductRequest createRequest = new ProductRequest(requestName, requestPrice,
-            requestUrl);
+                requestUrl);
 
         RestAssured
-            .given().contentType(ContentType.JSON).body(createRequest)
-            .when().put("/products/1")
-            .then().statusCode(400).body(Matchers.containsString("유효하지 않은 값입니다."));
+                .given().contentType(ContentType.JSON).body(createRequest)
+                .when().put("/products/1")
+                .then().statusCode(400).body(Matchers.containsString("유효하지 않은 값입니다."));
     }
 
     @Test
@@ -102,29 +91,29 @@ class ProductControllerTest {
         final int requestPrice = 4_000;
         final String requestUrl = "none";
         final ProductRequest createRequest = new ProductRequest(requestName, requestPrice,
-            requestUrl);
+                requestUrl);
 
         RestAssured
-            .given().contentType(ContentType.JSON).body(createRequest)
-            .when().put("/products/-1")
-            .then().statusCode(400).body(Matchers.containsString("존재하지 않는 id입니다."));
+                .given().contentType(ContentType.JSON).body(createRequest)
+                .when().put("/products/-1")
+                .then().statusCode(400).body(Matchers.containsString("존재하지 않는 id입니다."));
     }
 
     @Test
     @DisplayName("/products/{id}로 delete 요청을 보내면 상태코드 200(OK)을 응답한다")
     void deleteProduct() {
         RestAssured
-            .given().contentType(ContentType.JSON)
-            .when().delete("/products/1")
-            .then().statusCode(200);
+                .given().contentType(ContentType.JSON)
+                .when().delete("/products/1")
+                .then().statusCode(200);
     }
 
     @Test
     @DisplayName("/products/{id}로 존재하지 않는 상품에 대해 delete 요청을 보내면 상태코드 400(BadRequest)을 응답한다")
     void deleteProduct_fail() {
         RestAssured
-            .given().contentType(ContentType.JSON)
-            .when().delete("/products/-1")
-            .then().statusCode(400).body(Matchers.containsString("존재하지 않는 id입니다."));
+                .given().contentType(ContentType.JSON)
+                .when().delete("/products/-1")
+                .then().statusCode(400).body(Matchers.containsString("존재하지 않는 id입니다."));
     }
 }
--- src/test/java/cart/dao/ProductDaoTest.java
+++ src/test/java/cart/dao/ProductDaoTest.java
@@ -1,19 +1,16 @@
 package cart.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 import cart.controller.ProductRequest;
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Optional;
-import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @JdbcTest
@@ -29,17 +26,6 @@ public class ProductDaoTest {
     @BeforeEach
     void setUp() {
         productDao = new ProductDao(jdbcTemplate);
-        String productDropQuery = "DROP TABLE product IF EXISTS";
-        String productCreateQuery = "CREATE TABLE product(id BIGINT NOT NULL AUTO_INCREMENT,name VARCHAR(20) NOT NULL,price INT NOT NULL,image_url TEXT NOT NULL, PRIMARY KEY(id))";
-        String sampleQuery1 = "INSERT INTO product(name, price, image_url) VALUES ('피자', 13000, 'https://searchad-phinf.pstatic.net/MjAyMjEyMjdfMTE1/MDAxNjcyMTAxNTI0Nzg4.WfiSlsy9fTUQJ6q2FTGOaaOVU0QpSB0U1LvplKZQXzIg.H4UgI0VbKUszP7mzC3qhwpSMe15DluJnxjxVGDq_QUgg.PNG/451708-1fa87663-02e3-4303-b8a9-d7eea3676018.png?type=f160_160')";
-        String sampleQuery2 = "INSERT INTO product(name, price, image_url) VALUES ('치킨', 27000, 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzAzMjdfMTI2%2FMDAxNjc5OTI1ODQ4NTgy.6RT9z-i5prsnwwc-6B9TaK6Q0Zcgsd3TeDGiUdqyDRIg.rW2kMtzBKNFhWWXyr_X2bZfR15AEPUOz-VJnqVaP0jEg.JPEG.koreasinju%2FIMG_3379.jpg&type=ff332_332')";
-        String sampleQuery3 = "INSERT INTO product(name, price, image_url) VALUES ('샐러드', 9500, 'https://searchad-phinf.pstatic.net/MjAyMzA0MThfMjk0/MDAxNjgxODAwNDY4NjU4.FJcjmoGsxyCq0nZqlcmoAL2mwX8mM9ny9DdliQcqGZ0g.9cGk2IQHfPIm2-ABelEOY1cc-_8NBQgPMgPpjFZkGFEg.JPEG/2814800-bb4236af-96dd-42e7-8256-32ffaa73de52.jpg?type=f160_160')";
-
-        jdbcTemplate.execute(productDropQuery);
-        jdbcTemplate.execute(productCreateQuery);
-        jdbcTemplate.update(sampleQuery1);
-        jdbcTemplate.update(sampleQuery2);
-        jdbcTemplate.update(sampleQuery3);
 
         firstRecord = productDao.findAll().get(0);
         firstRecordId = firstRecord.getId();
@@ -83,7 +69,7 @@ public class ProductDaoTest {
         final int requestPrice = 4_000;
         final String requestUrl = "none";
         final ProductRequest updateRequest = new ProductRequest(requestName, requestPrice,
-            requestUrl);
+                requestUrl);
 
         // when
         final int updateCount = productDao.updateById(firstRecordId, updateRequest);
@@ -91,10 +77,10 @@ public class ProductDaoTest {
         // then
         final ProductEntity actual = productDao.findById(firstRecordId).get();
         assertAll(
-            () -> assertThat(updateCount).isEqualTo(1),
-            () -> assertThat(actual.getName()).isEqualTo(requestName),
-            () -> assertThat(actual.getPrice()).isEqualTo(requestPrice),
-            () -> assertThat(actual.getImageUrl()).isEqualTo(requestUrl)
+                () -> assertThat(updateCount).isEqualTo(1),
+                () -> assertThat(actual.getName()).isEqualTo(requestName),
+                () -> assertThat(actual.getPrice()).isEqualTo(requestPrice),
+                () -> assertThat(actual.getImageUrl()).isEqualTo(requestUrl)
         );
     }
 
@@ -109,8 +95,8 @@ public class ProductDaoTest {
 
         // then
         assertAll(
-            () -> assertThat(deletedRecord).isEmpty(),
-            () -> assertThat(deleteCount).isEqualTo(1)
+                () -> assertThat(deletedRecord).isEmpty(),
+                () -> assertThat(deleteCount).isEqualTo(1)
         );
     }
 }
