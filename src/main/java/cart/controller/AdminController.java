10e004d91f5a79bd5ed337249841c7c38fb898bd
kokodak <kokodakadokok@gmail.com>
2023-04-27 15:18:10 +0900

    refactor: 공통된 경로 중복 제거
    Co-authored-by: jundonghyuk <ehdgur4814@naver.com>

--- src/main/java/cart/controller/AdminController.java
+++ src/main/java/cart/controller/AdminController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
     private final ProductManagementService productManagementService;
@@ -24,7 +26,7 @@ public class AdminController {
         this.productManagementService = productManagementService;
     }
 
-    @GetMapping(value = {"/admin", "/admin/products"})
+    @GetMapping(value = {"", "/products"})
     @ResponseStatus(HttpStatus.OK)
     public String readProducts(final Model model) {
         final List<ProductDto> allProduct = productManagementService.findAllProduct();
@@ -32,21 +34,22 @@ public class AdminController {
         return "admin";
     }
 
-    @PostMapping("/admin/products")
+    @PostMapping("/products")
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody @Valid final ProductDto productDto) {
         productManagementService.addProduct(productDto);
     }
 
-    @PatchMapping("/admin/products/{product_id}")
+    @PatchMapping("/products/{product_id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public void updateProduct(@PathVariable("product_id") final Long id, @RequestBody @Valid final ProductDto productDto) {
+    public void updateProduct(@PathVariable("product_id") final Long id,
+                              @RequestBody @Valid final ProductDto productDto) {
         final ProductDto updatedProductDto
                 = new ProductDto(id, productDto.getName(), productDto.getImageUrl(), productDto.getPrice());
         productManagementService.updateProduct(updatedProductDto);
     }
 
-    @DeleteMapping("/admin/products/{product_id}")
+    @DeleteMapping("/products/{product_id}")
     @ResponseStatus(HttpStatus.OK)
     public void deleteProduct(@PathVariable("product_id") final Long id) {
         productManagementService.deleteProduct(id);
