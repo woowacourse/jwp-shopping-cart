f76f60cb65a006fb09f0746aadb5b202cc007508
parkmuhyeun <pjhg410@gmail.com>
2023-04-27 10:49:54 +0900

    feat: 상품을 조회하는 기능 추가
    Co-authored-by: bjk1649 <kbjn1649@gmail.com>

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -1,12 +1,14 @@
 package cart.controller;
 
 import cart.dto.ProductRequest;
+import cart.dto.ProductResponse;
 import cart.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/products")
@@ -17,20 +19,25 @@ public class ProductController {
         this.productService = productService;
     }
 
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> findProduct() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+
     @PostMapping
-    public ResponseEntity<?> addProduct(@Valid @RequestBody ProductRequest productRequest) {
+    public ResponseEntity<String> addProduct(@Valid @RequestBody ProductRequest productRequest) {
         productService.addProduct(productRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> updateProduct(@PathVariable long id, @Valid @RequestBody ProductRequest productRequest) {
+    public ResponseEntity<String> updateProduct(@PathVariable long id, @Valid @RequestBody ProductRequest productRequest) {
         productService.updateProduct(id, productRequest);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deleteProduct(@PathVariable long id) {
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
         productService.deleteProduct(id);
         return ResponseEntity.ok().build();
     }
