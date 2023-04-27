7f67b8718256a53ad4c2298b20ea15e47c24bf0e
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-04-27 11:32:25 +0900

    feat(DB): 샘플 데이터 추가

---
+++ src/main/resources/data-initial.sql
@@ -0,0 +1,8 @@
+INSERT INTO product (name, price, category, image_url)
+values ('치킨', 20000, 'FOOD',
+        'https://i.namu.wiki/i/8yy6XiTsE0gUGm7L1vYk9hcdUqgAqmToXyUoqnTycsiH1ibSyX7odxQVBGzdThqFH6NIi_-ehK5AoYgHL7fa2qeBEAmM-2nd_lCY656m6Go9aEz94ZDbZCzM1Qkp0bJm29IOkRysn7LqHX3y8mduXQ.webp')
+     , ('아이폰14 PRO', 1200000, 'ELECTRONIC',
+        'https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone14pro-digitalmat-gallery-3-202209?wid=728&hei=666&fmt=png-alpha&.v=1663346233180')
+     , ('고퍼우드 g110', 170000, 'INSTRUMENT',
+        'https://m.gopherwood.co.kr/web/product/big/201910/48430668d3e313d9446d19a9b32afb83.jpg');
+
--- src/test/java/cart/domain/product/CartServiceTest.java
+++ src/test/java/cart/domain/product/CartServiceTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 class CartServiceTest {
 
