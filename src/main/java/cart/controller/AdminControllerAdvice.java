c7c2e678e5e1680664c58c82f80083bb45029f82
Ethan <kim76744dev@gmail.com>
2023-04-27 13:33:43 +0900

    refactor: 에러 메시지가 모두 반환되도록 수정

--- src/main/java/cart/controller/AdminControllerAdvice.java
+++ src/main/java/cart/controller/AdminControllerAdvice.java
@@ -1,5 +1,7 @@
 package cart.controller;
 
+import cart.dto.ErrorResponse;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -7,25 +9,26 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import cart.dto.ErrorResponse;
-
 @ControllerAdvice(assignableTypes = AdminController.class)
 public class AdminControllerAdvice {
 
+    private static final String UNEXPECTED_ERROR_MESSAGE = "네트워크 연결 상태가 좋지 않습니다 잠시 후 다시 시도해 주세요.";
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleBindException(MethodArgumentNotValidException exception) {
-        final String errorMessage = exception.getBindingResult()
-                .getFieldError()
-                .getDefaultMessage();
+    public ResponseEntity<ErrorResponse> handleBindException(final MethodArgumentNotValidException exception) {
+        final String errorMessage = exception.getBindingResult().getFieldErrors().stream()
+                .map(fieldError -> String.format("%s : %s", fieldError.getField(), fieldError.getDefaultMessage()))
+                .collect(Collectors.joining(System.lineSeparator()));
+
         return ResponseEntity.badRequest().body(new ErrorResponse(errorMessage));
     }
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleUnexpectedException(Exception exception) {
+    public ResponseEntity<ErrorResponse> handleUnexpectedException(final Exception exception) {
         final String errorMessage = exception.getMessage();
         logger.error("error : {}", errorMessage);
-        return ResponseEntity.internalServerError().body(new ErrorResponse(errorMessage));
+
+        return ResponseEntity.internalServerError().body(new ErrorResponse(UNEXPECTED_ERROR_MESSAGE));
     }
 }
