2f14770e541ced1f55b05632e432534e40e2367a
nuyh <hjaehyun25@gmail.com>
2023-04-27 10:58:59 +0900

    feat: 예상 못한 서버 에러 로깅

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package cart.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @ExceptionHandler({MethodArgumentNotValidException.class, IllegalArgumentException.class})
     public ResponseEntity<String> handle(Exception e) {
@@ -15,6 +18,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     public ResponseEntity<String> handleAnyException(Exception e) {
+        logger.info("info log = {}", e.getMessage());
         return ResponseEntity.internalServerError().body("예기치 못한 에러가 발생했습니다.");
     }
 }
