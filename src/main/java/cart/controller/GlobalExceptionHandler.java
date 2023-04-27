f41f697628572f9f1fe84d04108f8a032f91e454
ingpyo <juninpyo@naver.com>
2023-04-27 22:14:34 +0900

    fix: logger 추가

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package cart.controller;
 
 import cart.dto.ExceptionResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,10 +14,12 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private static final String DELIMITER = ", ";
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
+        logger.error("[internal server error] ", exception);
 
         return ResponseEntity.internalServerError().body(new ExceptionResponse("처리 중 예외가 발생했습니다"));
     }
@@ -23,11 +27,15 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(final IllegalArgumentException exception) {
+        logger.info("[IllegalArgumentException] ", exception);
+
         return ResponseEntity.badRequest().body(new ExceptionResponse(exception.getMessage()));
     }
 
     @ExceptionHandler({MethodArgumentNotValidException.class, MethodArgumentTypeMismatchException.class})
     public ResponseEntity<ExceptionResponse> handleValidationException(final MethodArgumentNotValidException exception) {
+        logger.info("[MethodArgumentException] ", exception);
+
         final String exceptionMessage = exception.getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(DELIMITER));
