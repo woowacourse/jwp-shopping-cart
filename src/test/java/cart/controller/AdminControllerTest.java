c7773ad21d586c9dedd7318adf808b61772f2796
70825 <hello70825@gmail.com>
2023-04-28 02:11:52 +0900

    refactor: get 호출을 하는 컨트롤러는 WebMvcTest로 테스트하도록 변경

--- src/test/java/cart/controller/AdminControllerTest.java
+++ src/test/java/cart/controller/AdminControllerTest.java
@@ -1,30 +1,41 @@
 package cart.controller;
 
-import io.restassured.RestAssured;
-import org.junit.jupiter.api.BeforeEach;
+import cart.dto.ProductDto;
+import cart.service.ProductManagementService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AdminController.class)
 class AdminControllerTest {
 
-    @LocalServerPort
-    int port;
+    private static final String path = "/admin";
 
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ProductManagementService managementService;
 
     @DisplayName("상품 전체 목록을 조회하면 상태코드 200을 반환하는지 확인한다")
     @Test
-    void getAdminTest() {
-        RestAssured.given().log().all()
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value());
+    void getAdminTest() throws Exception {
+        final ProductDto productDto = ProductDto.of(1L, "test", "http:", 10_000);
+        final List<ProductDto> productDtos = List.of(productDto);
+
+        when(managementService.findAll())
+                .thenReturn(productDtos);
+
+        mockMvc.perform(get(path))
+                .andExpect(status().isOk());
     }
 }
--- src/test/java/cart/controller/HomeControllerTest.java
+++ src/test/java/cart/controller/HomeControllerTest.java
@@ -1,30 +1,46 @@
 package cart.controller;
 
+import cart.dto.ProductDto;
+import cart.service.ProductManagementService;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HomeController.class)
 class HomeControllerTest {
 
-    @LocalServerPort
-    int port;
+    private static final String path = "/";
 
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    ProductManagementService managementService;
 
     @DisplayName("상품 전체 목록을 조회하면 상태코드 200을 반환하는지 확인한다")
     @Test
-    void getHomeTest() {
-        RestAssured.given().log().all()
-                .when().get("/")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value());
+    void getHomeTest() throws Exception {
+        final ProductDto productDto = ProductDto.of(1L, "test", "http:", 10_000);
+        final List<ProductDto> productDtos = List.of(productDto);
+
+        when(managementService.findAll())
+                .thenReturn(productDtos);
+
+        mockMvc.perform(get(path))
+                .andExpect(status().isOk());
     }
 }
