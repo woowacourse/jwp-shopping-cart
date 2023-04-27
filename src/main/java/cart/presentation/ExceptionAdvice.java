e2c1c5da032960407659d7ac83fbcb680bc31b67
eunbii0213 <eunbii0213@naver.com>
2023-04-27 14:58:26 +0900

    feat: 예외처리 메커니즘 생성

---
+++ src/main/java/cart/presentation/ExceptionAdvice.java
@@ -0,0 +1,24 @@
+package cart.presentation;
+
+import cart.presentation.dto.ExceptionResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @ExceptionHandler({
+            IllegalArgumentException.class
+    })
+    public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        ExceptionResponse exception = new ExceptionResponse(e.getMessage());
+        logger.info(e.getMessage());
+
+        return ResponseEntity.badRequest().body(exception);
+    }
+}
