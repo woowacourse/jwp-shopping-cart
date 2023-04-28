6bda9458a79bc05f3bbc149b65badbf38487d46b
odo27 <odo27@naver.com>
2023-04-28 14:54:50 +0900

    fix: ProductRequest 기본 생성자 추가

--- src/main/java/cart/dto/ProductRequest.java
+++ src/main/java/cart/dto/ProductRequest.java
@@ -6,16 +6,19 @@ import javax.validation.constraints.Size;
 
 public class ProductRequest {
 
-    @Size(min = 1, max = 255,message = "상품명은 1자 이상 255자 이하입니다. 입력값 : ${validatedValue}")
+    @Size(min = 1, max = 255, message = "상품명은 1자 이상 255자 이하입니다. 입력값 : ${validatedValue}")
     @NotBlank(message = "상품명은 필수 입력값입니다.")
-    private final String name;
+    private String name;
 
-    @Size(min=1,max=2048,message = "이미지 url은 1자 이상 2048자 이하입니다. 입력값 : ${validatedValue}")
+    @Size(min = 1, max = 2048, message = "이미지 url은 1자 이상 2048자 이하입니다. 입력값 : ${validatedValue}")
     @NotBlank(message = "이미지 url은 필수 입력값입니다.")
-    private final String image;
+    private String image;
 
     @PositiveOrZero(message = "상품 가격은 0원 이상이어야 합니다. 입력값 : ${validatedValue}")
-    private final int price;
+    private int price;
+
+    private ProductRequest() {
+    }
 
     public ProductRequest(final String name, final String image, final int price) {
         this.name = name;
