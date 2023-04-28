59d3841af386586b0f30e945f30a58f67cb5bc07
jiwon <ljwon77@daum.net>
2023-04-29 00:43:44 +0900

    refactor: 테스트 코드에 대한 리팩터링을 진행한다

--- src/test/java/cart/service/ProductServiceTest.java
+++ src/test/java/cart/service/ProductServiceTest.java
@@ -5,7 +5,6 @@ import cart.exception.GlobalException;
 import cart.persistence.dao.ProductDao;
 import cart.persistence.entity.Product;
 import cart.persistence.entity.ProductCategory;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +16,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -25,27 +24,20 @@ import static org.mockito.Mockito.when;
 @WebMvcTest(ProductService.class)
 class ProductServiceTest {
 
-    private ProductDto productDto;
-
     @MockBean
     private ProductDao productDao;
 
     @Autowired
     private ProductService productService;
 
-    @BeforeEach
-    void setUp() {
-        productDto = new ProductDto(1L, "스테이크", "steakUrl", 40000, ProductCategory.WESTERN);
-    }
-
     @DisplayName("전체 상품을 조회한다")
     @Test
     void getProducts() {
         // given
         final List<Product> products = List.of(
-                new Product("치킨", "chickenUrl", 20000, ProductCategory.KOREAN),
-                new Product("초밥", "chobobUrl", 30000, ProductCategory.JAPANESE),
-                new Product("스테이크", "steakUrl", 40000, ProductCategory.WESTERN)
+                new Product("치킨", "chicken_image_url", 20000, ProductCategory.KOREAN),
+                new Product("초밥", "chobab_image_url", 30000, ProductCategory.JAPANESE),
+                new Product("스테이크", "steak_image_url", 40000, ProductCategory.WESTERN)
         );
         when(productDao.findAll()).thenReturn(products);
 
@@ -53,19 +45,20 @@ class ProductServiceTest {
         final List<ProductDto> resultProducts = productService.getProducts();
 
         // then
-        assertAll(
-                () -> assertThat(resultProducts).hasSize(3),
-                () -> assertThat(resultProducts.get(0).getName()).isEqualTo("치킨"),
-                () -> assertThat(resultProducts.get(1).getName()).isEqualTo("초밥"),
-                () -> assertThat(resultProducts.get(2).getName()).isEqualTo("스테이크")
-        );
+        assertThat(resultProducts).hasSize(3);
+        assertThat(resultProducts)
+                .extracting("name", "imageUrl", "price", "category")
+                .contains(tuple("치킨", "chicken_image_url", 20000, ProductCategory.KOREAN),
+                        tuple("초밥", "chobab_image_url", 30000, ProductCategory.JAPANESE),
+                        tuple("스테이크", "steak_image_url", 40000, ProductCategory.WESTERN));
     }
 
     @DisplayName("상품을 저장한다")
     @Test
     void save() {
         // given
-        final List<Product> products = List.of(new Product("스테이크", "steakUrl", 40000, ProductCategory.WESTERN));
+        final List<Product> products = List.of(new Product("스테이크", "steak_image_url", 40000, ProductCategory.WESTERN));
+        final ProductDto productDto = new ProductDto(1L, "스테이크", "steak_image_url", 40000, ProductCategory.WESTERN);
         when(productDao.insert(any())).thenReturn(1L);
         when(productDao.findAll()).thenReturn(products);
 
@@ -74,17 +67,19 @@ class ProductServiceTest {
 
         // then
         final List<ProductDto> resultProducts = productService.getProducts();
-        assertAll(
-                () -> assertThat(resultProducts).hasSize(1),
-                () -> assertThat(resultProducts.get(0).getName()).isEqualTo("스테이크")
-        );
+
+        assertThat(resultProducts).hasSize(1);
+        assertThat(resultProducts)
+                .extracting("name", "price", "imageUrl", "category")
+                .contains(tuple("스테이크", "steak_image_url", 40000, ProductCategory.WESTERN));
     }
 
     @DisplayName("상품 수정을 정상적으로 진행한다")
     @Test
     void update_success() {
         // given
-        when(productDao.update(any(), any())).thenReturn(1);
+        final ProductDto productDto = new ProductDto(1L, "스테이크", "steak_image_url", 40000, ProductCategory.WESTERN);
+        when(productDao.updateById(any(), any())).thenReturn(1);
 
         // when, then
         assertDoesNotThrow(() -> productService.update(1L, productDto));
@@ -94,7 +89,8 @@ class ProductServiceTest {
     @Test
     void update_fail() {
         // given
-        when(productDao.update(any(), any())).thenReturn(0);
+        final ProductDto productDto = new ProductDto(1L, "스테이크", "steak_image_url", 40000, ProductCategory.WESTERN);
+        when(productDao.updateById(any(), any())).thenReturn(0);
 
         // when, then
         assertThatThrownBy(() -> productService.update(1L, productDto))
@@ -126,19 +122,16 @@ class ProductServiceTest {
     @Test
     void getById_success() {
         // given
-        final Product product = new Product("스테이크", "steakUrl", 40000, ProductCategory.WESTERN);
+        final Product product = new Product("스테이크", "steak_image_url", 40000, ProductCategory.WESTERN);
         when(productDao.findById(any())).thenReturn(Optional.of(product));
 
         // when
         final ProductDto productDto = productService.getById(1L);
 
         // then
-        assertAll(
-                () -> assertThat(productDto.getName()).isEqualTo("스테이크"),
-                () -> assertThat(productDto.getImageUrl()).isEqualTo("steakUrl"),
-                () -> assertThat(productDto.getPrice()).isEqualTo(40000),
-                () -> assertThat(productDto.getCategory()).isEqualTo(ProductCategory.WESTERN)
-        );
+        assertThat(productDto)
+                .extracting("name", "price", "imageUrl", "category")
+                .contains("스테이크", 40000, "steak_image_url", ProductCategory.WESTERN);
     }
 
     @DisplayName("존재하지 않는 상품을 조회하면 예외가 발생한다.")
