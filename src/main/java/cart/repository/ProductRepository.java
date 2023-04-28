520d1c2652f1eb9aecf4f632a12cacd8e0c4aff1
kdkdhoho <hkim4410@naver.com>
2023-04-28 15:26:01 +0900

    feat(ProductRepository): ProductRepository 구현

---
+++ src/main/java/cart/repository/ProductRepository.java
@@ -0,0 +1,53 @@
+package cart.repository;
+
+import static java.util.stream.Collectors.toList;
+
+import cart.dao.ProductDao;
+import cart.domain.Product;
+import cart.entity.ProductEntity;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    private final ProductDao productDao;
+
+    public ProductRepository(final ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    public long insert(final Product product) {
+        ProductEntity productEntity = ProductEntity.from(product);
+
+        return productDao.insert(productEntity);
+    }
+
+    public Product findById(final long id) {
+        ProductEntity result = productDao.findById(id);
+
+        return new Product(result.getName(), result.getPrice(), result.getImageUrl());
+    }
+
+    public List<Product> findAll() {
+        List<ProductEntity> result = productDao.findAll();
+
+        return result.stream()
+                .map(productEntity -> new Product(productEntity.getName(), productEntity.getPrice(), productEntity.getImageUrl()))
+                .collect(toList());
+    }
+
+    public void update(final long id, final Product newProduct) {
+        ProductEntity newProductEntity = new ProductEntity(id, newProduct.getName(), newProduct.getPrice(), newProduct.getImageUrl());
+
+        productDao.update(newProductEntity);
+    }
+
+    public void delete(final long id) {
+        productDao.delete(id);
+    }
+
+    public boolean isExist(final long id) {
+        return productDao.isExist(id);
+    }
+}
---
+++ src/test/java/cart/repository/ProductRepositoryTest.java
@@ -0,0 +1,80 @@
+package cart.repository;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import cart.dao.ProductDao;
+import cart.domain.Product;
+import cart.entity.ProductEntity;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql("classpath:schema.sql")
+class ProductRepositoryTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private cart.repository.ProductRepository productRepository;
+
+    private final Product product = new Product("item1", 1000, "https://");
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+
+        productDao.insert(new ProductEntity("pizza", 1000,
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg"));
+        productDao.insert(new ProductEntity("salad", 2000,
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Salad_platter.jpg/1200px-Salad_platter.jpg"));
+    }
+
+    @Test
+    void 상품_데이터_삽입() {
+        long id = productRepository.insert(product);
+
+        assertThat(id).isEqualTo(3L);
+    }
+
+    @Test
+    void 단일_상품_데이터_조회() {
+        Product product = productRepository.findById(1L);
+
+        assertThat(product.getName()).isEqualTo("pizza");
+    }
+
+    @Test
+    void 모든_상품_데이터_조회() {
+        List<Product> products = productRepository.findAll();
+
+        assertThat(products.size()).isEqualTo(2);
+    }
+
+    @Test
+    void 상품_데이터_수정() {
+        long id = 1L;
+        Product newProduct = new Product("apple pizza", 10000, "https://");
+
+        productRepository.update(id, newProduct);
+
+        Product updatedProduct = productRepository.findById(id);
+        assertThat(updatedProduct.getName()).isEqualTo("apple pizza");
+    }
+
+    @Test
+    void 상품_데이터_삭제() {
+        productRepository.delete(2L);
+
+        assertThat(productRepository.findAll().size()).isEqualTo(1);
+    }
+}
