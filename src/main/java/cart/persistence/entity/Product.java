d37dcf7bc4a9a7db9d7491eb7310597564f5ba83
HubCreator <chocomilk8604@gmail.com>
2023-04-28 14:00:25 +0900

    refactor: Product 엔티티에 검증 로직 추가

--- src/main/java/cart/persistence/entity/Product.java
+++ src/main/java/cart/persistence/entity/Product.java
@@ -1,5 +1,8 @@
 package cart.persistence.entity;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Product {
     private Long id;
     private final String name;
@@ -7,7 +10,11 @@ public class Product {
     private final int price;
     private final ProductCategory category;
 
+    public final Logger log = LoggerFactory.getLogger(getClass());
+
+
     public Product(final Long id, final String name, final String imageUrl, final int price, final ProductCategory category) {
+        validate(name, price, category);
         this.id = id;
         this.name = name;
         this.imageUrl = imageUrl;
@@ -16,10 +23,22 @@ public class Product {
     }
 
     public Product(final String name, final String imageUrl, final int price, final ProductCategory category) {
-        this.name = name;
-        this.imageUrl = imageUrl;
-        this.price = price;
-        this.category = category;
+        this(null, name, imageUrl, price, category);
+    }
+
+    private void validate(final String name, final int price, final ProductCategory category) {
+        if (name.length() < 1 || name.length() > 25) {
+            log.error("error = {}", "Invalid name length");
+            throw new IllegalArgumentException();
+        }
+        if (price < 0 || price > 10_000_000) {
+            log.error("error = {}", "Invalid price range");
+            throw new IllegalArgumentException();
+        }
+        if (category == null) {
+            log.error("error = {}", "ProductCategory can't be null");
+            throw new IllegalArgumentException();
+        }
     }
 
     public Long getId() {
