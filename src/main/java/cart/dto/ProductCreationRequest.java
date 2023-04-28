3417005aeeea9574d77a170015d9588ab8b91d6b
hectick <ch.yeondev@gmail.com>
2023-04-28 17:20:22 +0900

    refactor: url 검증 어노테이션 추가

--- src/main/java/cart/dto/ProductCreationRequest.java
+++ src/main/java/cart/dto/ProductCreationRequest.java
@@ -2,9 +2,10 @@ package cart.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.URL;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
 public class ProductCreationRequest {
@@ -12,7 +13,8 @@ public class ProductCreationRequest {
     @NotBlank
     private final String name;
 
-    @NotBlank
+    @NotNull
+    @URL
     private final String image;
 
     @Positive
--- src/main/java/cart/dto/ProductModificationRequest.java
+++ src/main/java/cart/dto/ProductModificationRequest.java
@@ -2,8 +2,8 @@ package cart.dto;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.URL;
 
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
@@ -16,10 +16,10 @@ public class ProductModificationRequest {
     @NotBlank
     private final String name;
 
-    @NotBlank
+    @NotNull
+    @URL
     private final String image;
 
-    @Max(value = 10_000_000, message = "상품 등록은 최대 천만원까지 가능합니다.")
     @Positive
     private final Integer price;
 
