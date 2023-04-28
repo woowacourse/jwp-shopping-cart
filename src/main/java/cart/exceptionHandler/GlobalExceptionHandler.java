ddd0e2fcd6c135329f5b83db0dd45018819e4f7e
kdkdhoho <hkim4410@naver.com>
2023-04-28 15:21:25 +0900

    feat: IllegalArgumentException 예외 처리 기능 구현

--- src/main/java/cart/exceptionHandler/GlobalExceptionHandler.java
+++ src/main/java/cart/exceptionHandler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package cart.exceptionHandler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -16,5 +17,10 @@ public class GlobalExceptionHandler {
         final FieldError fieldError = bindingResult.getFieldError();
 
         return ResponseEntity.badRequest().body(fieldError.getDefaultMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> illegalArgumentException() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("처리할 수 없습니다. 잠시 후 다시 요청해주세요.");
     }
 }
