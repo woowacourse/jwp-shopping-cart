14b4dd024521a77a82d8e3b477537ff87612b52d
jeonghoon <shb03323@naver.com>
2023-04-28 16:00:39 +0900

    test: MainController 테스트 코드 작성

--- src/test/java/cart/controller/MainControllerTest.java
+++ src/test/java/cart/controller/MainControllerTest.java
@@ -1,45 +1,66 @@
 package cart.controller;
 
+import cart.dto.ProductRequestDto;
+import cart.entity.ProductEntity;
+import cart.service.ProductService;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(MainController.class)
 class MainControllerTest {
 
-    @LocalServerPort
-    int port;
+    @Autowired
+    private MockMvc mockMvc;
 
-    @BeforeEach
-    void setUp() {
-        RestAssured.port = port;
-    }
+    @MockBean
+    private ProductService productService;
 
     @Test
     @DisplayName("root directory 로 요청을 보내면 홈 html 화면을 보내준다")
-    void rootPage() {
-        //expect
-        RestAssured.given().log().headers()
-                .when().get("/")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .contentType(ContentType.HTML);
+    void rootPage() throws Exception {
+        // given
+        List<ProductEntity> allProducts = new ArrayList<>();
+        given(productService.findAll())
+                .willReturn(allProducts);
+
+        // expect
+        mockMvc.perform(get("/"))
+                .andExpect(model().attributeExists("products"))
+                .andExpect(model().attribute("products", allProducts))
+                .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName(" /admin driectory 로 요청을 보내면 admin html 화면을 보내준다")
-    void admin() {
-        //expect
-        RestAssured.given().log().headers()
-                .when().get("/admin")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value())
-                .contentType(ContentType.HTML);
+    @DisplayName("'/admin' directory 로 요청을 보내면 admin html 화면을 보내준다")
+    void admin() throws Exception {
+        // expect
+        List<ProductEntity> allProducts = new ArrayList<>();
+        given(productService.findAll())
+                .willReturn(allProducts);
+
+        // expect
+        mockMvc.perform(get("/admin"))
+                .andExpect(model().attributeExists("products"))
+                .andExpect(model().attribute("products", allProducts))
+                .andExpect(status().isOk());
     }
 
 }
