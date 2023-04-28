0ff9f70bb8ee2f1f374f1bbdc231fc458ab3f9d7
kdkdhoho <hkim4410@naver.com>
2023-04-28 18:38:35 +0900

    test(ProductServiceTest): Mock 방식 적용하여 단위 테스트로 변경

--- src/test/java/cart/service/ProductServiceTest.java
+++ src/test/java/cart/service/ProductServiceTest.java
@@ -1,80 +1,93 @@
 package cart.service;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 import cart.domain.Product;
 import cart.dto.ProductDto;
 import cart.repository.ProductRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
+import java.util.List;
 
 @SpringBootTest
 @Sql("classpath:schema.sql")
+@MockitoSettings
 class ProductServiceTest {
 
     @Autowired
     private ProductService productService;
 
-    @Autowired
+    @MockBean
     private ProductRepository productRepository;
-
-    @BeforeEach
-    void setUp() {
-        productRepository.insert(new Product("pizza", 1000,
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg"));
-        productRepository.insert(new Product("salad", 2000,
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Salad_platter.jpg/1200px-Salad_platter.jpg"));
-    }
 
     @Test
     void 모든_상품_목록_조회() {
+        Mockito.when(productRepository.findAll())
+                .thenReturn(List.of(
+                        new Product("item1", 1000, "https://"),
+                        new Product("item1", 1000, "https://"),
+                        new Product("item1", 1000, "https://")
+                ));
+
         final var products = productService.findAll();
 
-        assertThat(products.size()).isEqualTo(2);
+        assertThat(products.size()).isEqualTo(3);
     }
 
     @Test
     void 상품_등록() {
-        final var expectedId = 3L;
-        final var savedId = productService.register(new ProductDto("item1", 1000, "item1 image"));
+        Mockito.when(productRepository.insert(any(Product.class)))
+                .thenReturn(4L);
 
-        assertThat(savedId).isEqualTo(expectedId);
+        final var savedId = productService.register(new ProductDto("item1", 1000, "https://"));
+
+        assertThat(savedId).isEqualTo(4L);
     }
 
     @Test
-    void 상품_수정() {
-        productService.updateProduct(1L, new ProductDto("new Name", 10, "new Image Url"));
+    void 상품_수정_성공() {
+        Mockito.when(productRepository.isExist(anyLong()))
+                .thenReturn(true);
 
-        final var updatedProduct = productRepository.findById(1L);
-
-        assertAll(
-                () -> assertThat(updatedProduct.getName()).isEqualTo("new Name"),
-                () -> assertThat(updatedProduct.getPrice()).isEqualTo(10),
-                () -> assertThat(updatedProduct.getImageUrl()).isEqualTo("new Image Url")
+        assertThatNoException().isThrownBy(
+                () -> productService.updateProduct(1L, new ProductDto("new Name", 10, "new Image Url"))
         );
     }
 
     @Test
-    void 존재하지_않는_상품_수정시_예외_발생() {
+    void 존재하지_않는_ID의_상품을_수정시_예외_발생() {
+        Mockito.when(productRepository.isExist(anyLong()))
+                .thenReturn(false);
+
         assertThatIllegalArgumentException().isThrownBy(
-                () -> productService.updateProduct(10L, new ProductDto("name", 1234, "imageUrl"))
-        ).withMessage("존재하지 않는 id 입니다.");
+                () -> productService.updateProduct(1L, new ProductDto("new Name", 10, "new Image Url"))
+        );
     }
 
     @Test
-    void 상품_삭제() {
-        productService.deleteProduct(1L);
+    void 상품_삭제_성공() {
+        Mockito.when(productRepository.isExist(anyLong()))
+                .thenReturn(true);
 
-        assertThat(productService.findAll().size()).isEqualTo(1);
+        assertThatNoException().isThrownBy(
+                () -> productService.deleteProduct(1L)
+        );
     }
 
     @Test
     void 존재하지_않는_상품_삭제시_예외_발생() {
+        Mockito.when(productRepository.isExist(anyLong()))
+                .thenReturn(false);
+
         assertThatIllegalArgumentException().isThrownBy(
                 () -> productService.deleteProduct(3L)
         ).withMessage("존재하지 않는 id 입니다.");
