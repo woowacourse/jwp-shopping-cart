8a51a6e29c57e05c92d4bc2ed3ffac1b2d276453
hectick <ch.yeondev@gmail.com>
2023-04-28 17:16:50 +0900

    refactor: 더미데이터 생성 방법 수정

---
+++ src/main/java/cart/dao/ProductInitializer.java
@@ -0,0 +1,27 @@
+package cart.dao;
+
+import cart.entity.ProductEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductInitializer implements ApplicationRunner {
+
+    private final ProductDao productDao;
+
+    @Autowired
+    public ProductInitializer(final ProductDao productDao){
+        this.productDao = productDao;
+    }
+    @Override
+    public void run(final ApplicationArguments args) throws Exception {
+        final ProductEntity productEntity1 = ProductEntity.of("mouse", "https://cdn.polinews.co.kr/news/photo/201910/427334_3.jpg", 100000);
+        final ProductEntity productEntity2 = ProductEntity.of("keyboard", "https://i1.wp.com/blog.peoplefund.co.kr/wp-content/uploads/2020/01/진혁.jpg?fit=770%2C418&ssl=1", 250000);
+
+        productDao.insert(productEntity1);
+        productDao.insert(productEntity2);
+    }
+
+}
