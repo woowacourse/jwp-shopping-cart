2952998ce1005ee94761e4c55170298c473dc642
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-04-28 17:42:07 +0900

    chore: ACCOUNT 및 CART 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS CART;
+DROP TABLE IF EXISTS ACCOUNT;
 DROP TABLE IF EXISTS PRODUCT;
 
 CREATE TABLE PRODUCT
@@ -7,4 +9,23 @@ CREATE TABLE PRODUCT
     price     INT           NOT NULL,
     image_url VARCHAR(2083) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE ACCOUNT
+(
+    id       INT          NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    password VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (username)
+);
+
+CREATE TABLE CART
+(
+    id         INT NOT NULL AUTO_INCREMENT,
+    user_id    INT NOT NULL,
+    product_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES ACCOUNT (id),
+    FOREIGN KEY (product_id) REFERENCES PRODUCT (id)
 );
