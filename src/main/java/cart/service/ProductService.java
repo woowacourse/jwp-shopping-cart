9ba7c4fe2edb1201fa1650c9bcd1e037f4f011f2
jiwon <ljwon77@daum.net>
2023-04-29 00:43:32 +0900

    feat: @Transactional 어노테이션을 추가한다

--- src/main/java/cart/service/ProductService.java
+++ src/main/java/cart/service/ProductService.java
@@ -6,9 +6,9 @@ import cart.exception.GlobalException;
 import cart.persistence.dao.ProductDao;
 import cart.persistence.entity.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -30,13 +30,15 @@ public class ProductService {
         return productDao.insert(productDto.toEntity());
     }
 
+    @Transactional
     public void update(final Long id, final ProductDto productDto) {
-        int updatedCount = productDao.update(productDto.toEntity(), id);
+        int updatedCount = productDao.updateById(productDto.toEntity(), id);
         if (updatedCount != 1) {
             throw new GlobalException(ErrorCode.PRODUCT_INVALID_UPDATE);
         }
     }
 
+    @Transactional
     public void delete(final Long id) {
         int deletedCount = productDao.deleteById(id);
         if (deletedCount != 1) {
@@ -45,11 +47,8 @@ public class ProductService {
     }
 
     public ProductDto getById(final Long id) {
-        final Optional<Product> product = productDao.findById(id);
-        if (product.isEmpty()) {
-            throw new GlobalException(ErrorCode.PRODUCT_NOT_FOUND);
-        }
-        final Product findProduct = product.get();
-        return ProductDto.fromEntity(findProduct);
+        return productDao.findById(id)
+                .map(ProductDto::fromEntity)
+                .orElseThrow(() -> new GlobalException(ErrorCode.PRODUCT_NOT_FOUND));
     }
 }
