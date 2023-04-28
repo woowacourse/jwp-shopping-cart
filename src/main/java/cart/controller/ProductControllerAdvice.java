ac01ec8bf37132ceeb954a0bab74ff188f6e74f9
gitchan <eunkeeeee@gmail.com>
2023-04-28 17:15:59 +0900

    refactor(ProductControllerAdvice): 반환 상태 코드 변경

--- src/main/java/cart/controller/ProductControllerAdvice.java
+++ src/main/java/cart/controller/ProductControllerAdvice.java
@@ -14,6 +14,6 @@ public class ProductControllerAdvice {
 
     @ExceptionHandler
     public ResponseEntity<String> handleException(Exception e) {
-        return ResponseEntity.badRequest().body("알 수 없는 에러가 발생했습니다.");
+        return ResponseEntity.internalServerError().body("알 수 없는 에러가 발생했습니다. 잠시 후 다시 시도해주세요.");
     }
 }
