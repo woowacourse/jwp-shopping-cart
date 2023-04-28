ca78a5352f215a286cafa80c35166c4a9f8c06a5
yoondgu <doyou2ing@naver.com>
2023-04-28 14:07:11 +0900

    refactor: 테스트 전용 DB 초기화 자체 설정, 어노테이션 삭제

--- src/test/java/cart/ProductIntegrationTest.java
+++ src/test/java/cart/ProductIntegrationTest.java
@@ -21,10 +21,8 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@Sql("/schema.sql")
 public class ProductIntegrationTest {
 
     @LocalServerPort
