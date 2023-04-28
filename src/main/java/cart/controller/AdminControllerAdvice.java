a0ac062f09eb27bade99b2bde941c8fbe6520c9e
Ethan <kim76744dev@gmail.com>
2023-04-28 13:07:19 +0900

    refactor: NoSuchElement를 handler 처리

--- src/main/java/cart/controller/AdminControllerAdvice.java
+++ src/main/java/cart/controller/AdminControllerAdvice.java
@@ -1,21 +1,25 @@
 package cart.controller;
 
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+
 import cart.dto.ErrorResponse;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice(assignableTypes = AdminController.class)
+@RestControllerAdvice(assignableTypes = AdminController.class)
 public class AdminControllerAdvice {
 
     private static final String UNEXPECTED_ERROR_MESSAGE = "네트워크 연결 상태가 좋지 않습니다 잠시 후 다시 시도해 주세요.";
+
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    @ExceptionHandler
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleBindException(final MethodArgumentNotValidException exception) {
         final String errorMessage = exception.getBindingResult().getFieldErrors().stream()
                 .map(fieldError -> String.format("%s : %s", fieldError.getField(), fieldError.getDefaultMessage()))
@@ -30,5 +34,13 @@ public class AdminControllerAdvice {
         logger.error("error : {}", errorMessage);
 
         return ResponseEntity.internalServerError().body(new ErrorResponse(UNEXPECTED_ERROR_MESSAGE));
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ErrorResponse> handleNoSuchElementException(final NoSuchElementException exception) {
+        final String errorMessage = exception.getMessage();
+        logger.error("error : {}", errorMessage);
+
+        return ResponseEntity.status(NOT_FOUND).body(new ErrorResponse(errorMessage));
     }
 }
