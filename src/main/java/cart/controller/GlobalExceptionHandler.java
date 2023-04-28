3096fd61517ba1a4aff633c825b27b0c03231c06
MoonJeWoong <munjin0201@naver.com>
2023-04-28 17:32:45 +0900

    refactor: 전역 예외처리 클래스 어노테이션 @RestControllerAdvice로 변경

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -4,18 +4,20 @@ import cart.dto.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<List<ErrorResponse>> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<List<ErrorResponse>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException exception
+    ) {
         List<ErrorResponse> responses = exception.getBindingResult().getAllErrors()
                 .stream()
                 .map(error -> new ErrorResponse(
