7abf9b56f56ee7cf428fe64ee633961306d6681f
hanueleee <ezzeezz@naver.com>
2023-04-28 22:30:15 +0900

    refactor: product 테이블의 imgUrl 컬럼의 자료형을 VARCHAR에서 TEXT로 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS product
     id     INT          NOT NULL AUTO_INCREMENT,
     name   VARCHAR(255) NOT NULL,
     price  INT          NOT NULL,
-    imgUrl VARCHAR(255) NOT NULL,
+    imgUrl TEXT NOT NULL,
     PRIMARY KEY (id)
 );
