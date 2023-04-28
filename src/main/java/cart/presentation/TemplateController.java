431f16af4552d44aa8f7a59d3e2db2da9701d846
woosung1223 <dntjd991223@naver.com>
2023-04-28 15:31:38 +0900

    chore: 클래스명 변경, 메소드 분리

--- src/main/java/cart/presentation/TemplateController.java
+++ src/main/java/cart/presentation/TemplateController.java
@@ -10,35 +10,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-public class WebController {
+public class TemplateController {
 
     private final ReadProductService readProductService;
 
-    public WebController(ReadProductService readProductService) {
+    public TemplateController(ReadProductService readProductService) {
         this.readProductService = readProductService;
     }
 
     @GetMapping("/")
     public String home(Model model) {
-        List<ProductDto> products = readProductService.perform().stream()
-                .map(product -> new ProductDto(product.getId(),
-                        product.getName(),
-                        product.getUrl(),
-                        product.getPrice()))
-                .collect(Collectors.toList());
+        List<ProductDto> products = readAllProducts();
         model.addAttribute("products", products);
         return "index";
     }
 
     @GetMapping("/admin")
     public String admin(Model model) {
-        List<ProductDto> products = readProductService.perform().stream()
+        List<ProductDto> products = readAllProducts();
+        model.addAttribute("products", products);
+        return "admin";
+    }
+
+    private List<ProductDto> readAllProducts() {
+        return readProductService.perform().stream()
                 .map(product -> new ProductDto(product.getId(),
                         product.getName(),
                         product.getUrl(),
                         product.getPrice()))
                 .collect(Collectors.toList());
-        model.addAttribute("products", products);
-        return "admin";
     }
 }
