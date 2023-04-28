59883de407f5b3552b2da039a0e451f6fa928125
jeonghoon <shb03323@naver.com>
2023-04-28 16:59:28 +0900

    feat: validation 예외 추가

--- src/main/java/cart/controller/CartControllerAdvice.java
+++ src/main/java/cart/controller/CartControllerAdvice.java
@@ -1,9 +1,13 @@
 package cart.controller;
 
 import cart.exception.ProductNotFoundException;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class CartControllerAdvice {
@@ -11,6 +15,14 @@ public class CartControllerAdvice {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handle(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handle(MethodArgumentNotValidException e) {
+        final String errorMessage = e.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining("\n"));
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
