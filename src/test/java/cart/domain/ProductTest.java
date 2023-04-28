0eff2f4c08be71fb32fb4a8b21ffc3fd2e16a477
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-04-29 08:41:06 +0900

    test: 상품 생성 및 예외 테스트, 중첩 테스트로 수정

--- src/test/java/cart/domain/ProductTest.java
+++ src/test/java/cart/domain/ProductTest.java
@@ -1,6 +1,7 @@
 package cart.domain;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -9,35 +10,63 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class ProductTest {
-	@DisplayName("상품 생성 성공 테스트")
-	@Test
-	void constructor() {
-		assertDoesNotThrow(() ->
-			new Product(3L, "name", 300, "img")
-		);
-	}
 
-	@DisplayName("상품명 유효성 검증")
-	@ParameterizedTest
-	@ValueSource(strings = {"", " ", "qwertyuioplkjhgfdsazx"})
-	void validateName(String name) {
-		assertThatThrownBy(() -> new Product(3L, name, 300, "https://avatars.githubusercontent.com/u/101039161?v=4"))
-			.isInstanceOf(IllegalArgumentException.class);
-	}
+    @DisplayName("상품을 생성할 때")
+    @Nested
+    class Validate {
 
-	@DisplayName("상품 가격 유효성 검증")
-	@ParameterizedTest
-	@ValueSource(doubles = {-2000, -1, -2})
-	void validatePrice(double price) {
-		assertThatThrownBy(
-			() -> new Product(3L, "hyena", price, "https://avatars.githubusercontent.com/u/101039161?v=4"))
-			.isInstanceOf(IllegalArgumentException.class);
-	}
+        @DisplayName("상품명 길이가 1이상 20이하가")
+        @Nested
+        class NameLength {
+            @ValueSource(strings = {"", " ", "012345678901234567890"})
+            @ParameterizedTest(name = "아닐 경우 예외가 발생한다. [상품명 : {0}]")
+            void greater(final String name) {
+                assertThatThrownBy(() -> new Product(1L, name, 300, "이미지-url"))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
 
-	@DisplayName("상품 사진 유효성 검증")
-	@Test
-	void validateImage() {
-		assertThatThrownBy(() -> new Product(3L, "hyena", 300, null))
-			.isInstanceOf(IllegalArgumentException.class);
-	}
+            @ValueSource(strings = {
+                    "0", "01", "012", "0123", "01234",
+                    "0123456789012345", "01234567890123456", "012345678901234567", "0123456789012345678", "01234567890123456789"
+            })
+            @ParameterizedTest(name = "맞을 경우 생성 성공한다. [상품명 : {0}]")
+            void ok(final String name) {
+                assertDoesNotThrow(() -> new Product(1L, name, 300, "이미지-url"));
+            }
+        }
+
+        @DisplayName("상품 가격이 0원 미만이")
+        @Nested
+        class PriceRange {
+            @ValueSource(ints = {-1, -2, -3, -1000000000})
+            @ParameterizedTest(name = "맞을 경우에 예외가 발생한다. [상품 가격 : {0}]")
+            void negative(final int price) {
+                assertThatThrownBy(() -> new Product(1L, "헤나", price, "이미지-url"))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @ValueSource(ints = {0, 1, 2, 1000000000})
+            @ParameterizedTest(name = "아닐 경우에 생성 성공한다. [상품 가격 : {0}]")
+            void positiveOrZero(final int price) {
+                assertDoesNotThrow(() -> new Product(1L, "헤나", price, "이미지-url"));
+            }
+        }
+
+        @DisplayName("상품 사진의 url이")
+        @Nested
+        class Image {
+            @DisplayName("null일 경우 예외가 발생한다.")
+            @Test
+            void isNull() {
+                assertThatThrownBy(() -> new Product(1L, "헤나", 10000, null))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @ValueSource(strings = {"0", "01", "url", "http://"})
+            @ParameterizedTest(name = "문자열일 경우 생성 성공한다. [url : {0}]")
+            void isString(final String imageUrl) {
+                assertDoesNotThrow(() -> new Product(1L, "헤나", 10000, imageUrl));
+            }
+        }
+    }
 }
