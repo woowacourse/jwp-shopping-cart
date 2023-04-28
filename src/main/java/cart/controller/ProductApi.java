2b02600e183fbf7c1987b77e64ba4ce641f8f7e7
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-04-28 23:54:29 +0900

    refactor: 중복되는 uri 해결

--- src/main/java/cart/controller/ProductApi.java
+++ src/main/java/cart/controller/ProductApi.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
 
+@RequestMapping("/products")
 @RestController
 public class ProductApi {
 	private final ProductService productService;
@@ -19,26 +20,26 @@ public class ProductApi {
 		this.productService = productService;
 	}
 
-	@GetMapping("/products")
+	@GetMapping
 	public ResponseEntity<List<ProductResponse>> getProducts() {
 		final List<ProductResponse> products = productService.findAll();
 		return ResponseEntity.ok(products);
 	}
 
-	@PostMapping("/products")
+	@PostMapping
 	public ResponseEntity<Void> createProduct(@RequestBody @Valid ProductCreateRequest request) {
 		final long save = productService.save(request);
 		final URI uri = URI.create("/products/" + save);
 		return ResponseEntity.created(uri).build();
 	}
 
-	@DeleteMapping("/products/{id}")
+	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deleteProduct(@PathVariable(name = "id") long productId) {
 		productService.deleteByProductId(productId);
 		return ResponseEntity.noContent().build();
 	}
 
-	@PatchMapping("/products/{id}")
+	@PatchMapping("/{id}")
 	public ResponseEntity<ProductResponse> updateProduct(
 			@PathVariable(value = "id") long productId,
 			@RequestBody @Valid ProductUpdateRequest request
