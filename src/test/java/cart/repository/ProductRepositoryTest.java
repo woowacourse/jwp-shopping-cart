3d4caa40c31379fd95892f8ce655149b6dc89024
kdkdhoho <hkim4410@naver.com>
2023-04-28 19:09:26 +0900

    test(ProductRepositoryTest): Mock 방식 적용하여 단위 테스트로 변경

--- src/test/java/cart/repository/ProductRepositoryTest.java
+++ src/test/java/cart/repository/ProductRepositoryTest.java
@@ -1,72 +1,92 @@
 package cart.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatNoException;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 
 import cart.dao.ProductDao;
 import cart.domain.Product;
 import cart.entity.ProductEntity;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.jdbc.Sql;
+import java.util.List;
 
 @SpringBootTest
+@MockitoSettings
 @Sql("classpath:schema.sql")
 class ProductRepositoryTest {
 
-    @Autowired
+    @MockBean
     private ProductDao productDao;
 
     @Autowired
-    private cart.repository.ProductRepository productRepository;
+    private ProductRepository productRepository;
 
     private final Product product = new Product("item1", 1000, "https://");
 
-    @BeforeEach
-    void setUp() {
-        productDao.insert(new ProductEntity("pizza", 1000,
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg/800px-Eq_it-na_pizza-margherita_sep2005_sml.jpg"));
-        productDao.insert(new ProductEntity("salad", 2000,
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/Salad_platter.jpg/1200px-Salad_platter.jpg"));
-    }
-
     @Test
     void 상품_데이터_삽입() {
-        var id = productRepository.insert(product);
+        final var expectId = 4L;
 
-        assertThat(id).isEqualTo(3L);
+        Mockito.when(productDao.insert(any(ProductEntity.class)))
+                .thenReturn(expectId);
+
+        final var id = productRepository.insert(product);
+
+        assertThat(id).isEqualTo(expectId);
     }
 
     @Test
     void 단일_상품_데이터_조회() {
+        final var expectName = "expectName";
+        final var expectPrice = 1_000;
+        final var expectImageUrl = "imageUrl";
+
+        Mockito.when(productDao.findById(anyLong()))
+                .thenReturn(new ProductEntity(expectName, expectPrice, expectImageUrl));
+
         var product = productRepository.findById(1L);
 
-        assertThat(product.getName()).isEqualTo("pizza");
+        assertAll(
+                () -> assertThat(product.getName()).isEqualTo(expectName),
+                () -> assertThat(product.getPrice()).isEqualTo(expectPrice),
+                () -> assertThat(product.getImageUrl()).isEqualTo(expectImageUrl)
+        );
     }
 
     @Test
     void 모든_상품_데이터_조회() {
+        final var expectSize = 2;
+
+        Mockito.when(productDao.findAll())
+                .thenReturn(List.of(
+                        new ProductEntity("item1", 1000, "imageUrl1"),
+                        new ProductEntity("item2", 2000, "imageUrl2")
+                ));
+
         var products = productRepository.findAll();
 
-        assertThat(products.size()).isEqualTo(2);
+        assertThat(products.size()).isEqualTo(expectSize);
     }
 
     @Test
-    void 상품_데이터_수정() {
-        var id = 1L;
-        var newProduct = new Product("apple pizza", 10000, "https://");
-
-        productRepository.update(id, newProduct);
-
-        var updatedProduct = productRepository.findById(id);
-        assertThat(updatedProduct.getName()).isEqualTo("apple pizza");
+    void 상품_데이터_수정_성공() {
+        assertThatNoException().isThrownBy(
+                () -> productRepository.update(1L, new Product("name", 10, "imageUrl"))
+        );
     }
 
     @Test
     void 상품_데이터_삭제() {
-        productRepository.delete(2L);
-
-        assertThat(productRepository.findAll().size()).isEqualTo(1);
+        assertThatNoException().isThrownBy(
+                () -> productRepository.delete(1L)
+        );
     }
 }
