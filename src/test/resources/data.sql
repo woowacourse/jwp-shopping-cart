b7328c684dd1b1ffaba08bb081ada334480f363c
sosow0212 <sosow0212@naver.com>
2023-04-28 22:41:03 +0900

    fix: sql문의 외래키 때문에 테스트 실패하는 문제 해결

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,4 +1,6 @@
+DROP TABLE IF EXISTS cart;
 DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS member;
 
 CREATE TABLE product
 (
@@ -7,4 +9,22 @@ CREATE TABLE product
     price   INT         NOT NULL,
     img_url CLOB(10K) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE member
+(
+    id       BIGINT AUTO_INCREMENT,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE cart
+(
+    id         BIGINT AUTO_INCREMENT,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
 );
