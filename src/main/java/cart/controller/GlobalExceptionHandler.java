48d6f551526df1775f8850333a10e9ba500249ca
BGuga <tmdwns9913@gmail.com>
2023-04-29 11:44:39 +0900

    refactor: Generic Raw Type 제거

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity handle(IllegalArgumentException e) {
+    public ResponseEntity<String> handle(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity handle(EmptyResultDataAccessException e) {
+    public ResponseEntity<String> handle(EmptyResultDataAccessException e) {
         return ResponseEntity.notFound().build();
     }
 }
