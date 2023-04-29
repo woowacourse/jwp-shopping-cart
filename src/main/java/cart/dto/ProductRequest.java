e648bf493b04dda8b85644bbbe44da429d1259a6
drunkenhw <drunkenhw@gmail.com>
2023-04-29 20:15:23 +0900

    refactor(ProductRequest): 요청에 대한 검증 추가

--- src/main/java/cart/dto/ProductRequest.java
+++ src/main/java/cart/dto/ProductRequest.java
@@ -1,16 +1,21 @@
 package cart.dto;
 
+import cart.entity.Product;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import org.hibernate.validator.constraints.URL;
 
 public class ProductRequest {
     @Size(max = 20, message = "상품 이름은 20자 이내로 입력해야 합니다.")
     private final String name;
+    @URL(message = "이미지는 url 형식으로 입력해주세요")
     private final String imgUrl;
+    @NotNull(message = "가격을 입력해주세요")
     @Min(value = 1000, message = "상품 가격은 최소 1000원 이상이어야 합니다.")
-    private final int price;
+    private final Integer price;
 
-    public ProductRequest(String name, String imgUrl, int price) {
+    public ProductRequest(String name, String imgUrl, Integer price) {
         this.name = name;
         this.imgUrl = imgUrl;
         this.price = price;
@@ -24,8 +29,12 @@ public class ProductRequest {
         return imgUrl;
     }
 
-    public int getPrice() {
+    public Integer getPrice() {
         return price;
+    }
+
+    public Product toEntity() {
+        return new Product(name, imgUrl, price);
     }
 
     @Override
