e5511f79b0cf5f5e2191c6a413f9166be99ba087
seokjin8678 <seokjin8678@gmail.com>
2023-04-29 17:16:59 +0900

    feat: Member 도메인 추가

---
+++ src/main/java/cart/repository/MemberDao.java
@@ -0,0 +1,16 @@
+package cart.repository;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberDao {
+    private final JdbcTemplate jdbcTemplate;
+    private final SimpleJdbcInsert simpleJdbcInsert;
+
+    public MemberDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+    }
+}
---
+++ src/main/java/cart/service/MemberService.java
@@ -0,0 +1,13 @@
+package cart.service;
+
+import cart.repository.MemberDao;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    private final MemberDao memberDao;
+
+    public MemberService(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+}
