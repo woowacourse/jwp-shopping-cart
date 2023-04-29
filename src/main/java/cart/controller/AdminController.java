88c0c3bf5aca876a13751466176107b52f2c79a2
JunDonghyuk <ehdgur4814@naver.com>
2023-04-29 19:20:02 +0900

    refactor: 컨트롤러의 분리

--- src/main/java/cart/controller/AdminController.java
+++ src/main/java/cart/controller/AdminController.java
@@ -2,21 +2,13 @@ package cart.controller;
 
 import cart.dto.ProductDto;
 import cart.service.ProductManagementService;
-import java.util.List;
-import javax.validation.Valid;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
 @RequestMapping("/admin")
 public class AdminController {
 
@@ -26,32 +18,24 @@ public class AdminController {
         this.productManagementService = productManagementService;
     }
 
-    @GetMapping(value = {"", "/products"})
-    @ResponseStatus(HttpStatus.OK)
-    public String readProducts(final Model model) {
-        final List<ProductDto> allProduct = productManagementService.findAllProduct();
-        model.addAttribute("products", allProduct);
-        return "admin";
-    }
-
     @PostMapping("/products")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createProduct(@RequestBody @Valid final ProductDto productDto) {
-        productManagementService.addProduct(productDto);
+    public ResponseEntity<Void> createProduct(@RequestBody @Valid final ProductDto productDto) {
+        Long id = productManagementService.addProduct(productDto);
+        return ResponseEntity.created(URI.create("/admin/products/" + id)).build();
     }
 
-    @PatchMapping("/products/{product_id}")
-    @ResponseStatus(HttpStatus.CREATED)
-    public void updateProduct(@PathVariable("product_id") final Long id,
-                              @RequestBody @Valid final ProductDto productDto) {
+    @PutMapping("/products/{product_id}")
+    public ResponseEntity<Void> updateProduct(@PathVariable("product_id") final Long id,
+                                              @RequestBody @Valid final ProductDto productDto) {
         final ProductDto updatedProductDto
                 = new ProductDto(id, productDto.getName(), productDto.getImageUrl(), productDto.getPrice());
         productManagementService.updateProduct(updatedProductDto);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/products/{product_id}")
-    @ResponseStatus(HttpStatus.OK)
-    public void deleteProduct(@PathVariable("product_id") final Long id) {
+    public ResponseEntity<Void> deleteProduct(@PathVariable("product_id") final Long id) {
         productManagementService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/cart/controller/HomePageController.java
@@ -0,0 +1,26 @@
+package cart.controller;
+
+import cart.dto.ProductDto;
+import cart.service.ProductManagementService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomePageController {
+
+    private final ProductManagementService productManagementService;
+
+    public HomePageController(final ProductManagementService productManagementService) {
+        this.productManagementService = productManagementService;
+    }
+
+    @GetMapping("/")
+    public String readHomePage(final Model model) {
+        final List<ProductDto> products = productManagementService.findAllProduct();
+        model.addAttribute("products", products);
+        return "index";
+    }
+}
