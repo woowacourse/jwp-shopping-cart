8a57d14effac8a338439b5632ee1b25b7cd2acc9
BackFoxx <backfoxxx@gmail.com>
2023-04-30 01:02:58 +0900

    test : AdminController 유닛 테스트

--- src/main/java/cart/entity/Product.java
+++ src/main/java/cart/entity/Product.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
@@ -48,6 +49,19 @@ public class Product {
 
     public BigDecimal getPrice() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) && Objects.equals(name, product.name) && Objects.equals(imageUrl, product.imageUrl) && Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, imageUrl, price);
     }
 
     @Override
---
+++ src/test/java/cart/controller/AdminControllerTest.java
@@ -0,0 +1,92 @@
+package cart.controller;
+
+import cart.entity.Product;
+import cart.repository.JdbcProductRepository;
+import cart.repository.ProductRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Locale;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest
+class AdminControllerTest {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockBean
+    private ProductRepository productRepository;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .build();
+    }
+
+    @Test
+    @DisplayName("상품 전체 조회 테스트")
+    public void findAll() throws Exception {
+        List<Product> products = List.of(
+                new Product(1L, "Test1", "test1.png", new BigDecimal(1000)),
+                new Product(2L, "Test2", "test2.png", new BigDecimal(2000)),
+                new Product(3L, "Test3", "test3.png", new BigDecimal(3000)),
+                new Product(4L, "Test4", "test4.png", new BigDecimal(4000))
+        );
+
+        Mockito.when(productRepository.findAll())
+                .thenReturn(products);
+
+        mockMvc.perform(get("/admin")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andExpect(model().attribute("products", products))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("상품 등록 테스트")
+    public void createProduct() throws Exception {
+        Product product = new Product("테스트", "image.png", new BigDecimal(5000));
+        Mockito.when(productRepository.save(product))
+                .thenReturn(new Product(1L, product.getName(), product.getImageUrl(), product.getPrice()));
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        mockMvc.perform(post("/admin/create")
+                        .content(objectMapper.writeValueAsString(product))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                )
+                .andExpect(status().isCreated())
+                .andExpect(header().exists("location"))
+                .andExpect(header().string("location", "/admin/1"));
+    }
+}
