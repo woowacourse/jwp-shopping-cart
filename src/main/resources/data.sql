652cfc4100cc7927261875068b2d0e250cb53a2a
Combi153 <jcm04@naver.com>
2023-04-29 18:11:43 +0900

    feat: DDL 수정 및 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,26 @@
-CREATE TABLE PRODUCT (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
-    price INT NOT NULL,
+CREATE TABLE product
+(
+    id    INT           NOT NULL AUTO_INCREMENT,
+    name  VARCHAR(50)   NOT NULL,
+    price INT           NOT NULL,
     image VARCHAR(2000) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE user
+(
+    id       INT          NOT NULL AUTO_INCREMENT,
+    email    VARCHAR(320) NOT NULL,
+    password VARCHAR(20)  NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE cart
+(
+    id         INT NOT NULL AUTO_INCREMENT,
+    user_id    INT NOT NULL,
+    product_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE
 );
