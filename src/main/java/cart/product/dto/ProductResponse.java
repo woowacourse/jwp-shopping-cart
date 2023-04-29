0d82ccef93fbbd314e077329b94be5cefca874cd
rawfishthelgh <rg970604@naver.com>
2023-04-29 20:46:30 +0900

    refactor : 생성자 체이닝 활용 중복 제거

--- src/main/java/cart/product/dto/ProductResponse.java
+++ src/main/java/cart/product/dto/ProductResponse.java
@@ -4,38 +4,39 @@ import cart.product.entity.Product;
 
 public class ProductResponse {
 
-	private final Long id;
-	private final String name;
-	private final String image;
-	private final Long price;
+    private final Long id;
+    private final String name;
+    private final String image;
+    private final Long price;
 
-	public ProductResponse(Product product) {
-		this.id = product.getId();
-		this.name = product.getName();
-		this.image = product.getImage();
-		this.price = product.getPrice();
-	}
+    public ProductResponse(Long id, String name, String image, Long price) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+    }
 
-	public ProductResponse(Long id, Product product) {
-		this.id = id;
-		this.name = product.getName();
-		this.image = product.getImage();
-		this.price = product.getPrice();
-	}
+    public ProductResponse(Product product) {
+        this(product.getId(), product.getName(), product.getImage(), product.getPrice());
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public ProductResponse(Long id, Product product) {
+        this(id, product.getName(), product.getImage(), product.getPrice());
+    }
 
-	public String getName() {
-		return name;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getImage() {
-		return image;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public Long getPrice() {
-		return price;
-	}
+    public String getImage() {
+        return image;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
 }
