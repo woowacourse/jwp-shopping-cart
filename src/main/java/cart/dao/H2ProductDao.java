880929e7660c083d8521f6a1da9dab94500c1ef9
dhkim <akdlzmf1123@gmail.com>
2023-04-30 03:29:31 +0900

    refactor: dao SimpleJdbcInsert 사용하도록 변경

--- src/main/java/cart/dao/H2ProductDao.java
+++ src/main/java/cart/dao/H2ProductDao.java
@@ -6,7 +6,7 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,12 +15,11 @@ import java.util.Optional;
 @Repository
 public class H2ProductDao implements ProductDao {
 
-    private static final RowMapper<Product> PRODUCT_ROW_MAPPER = (rs, rowNum) -> new Product(
-            rs.getLong("id"),
-            rs.getString("name"),
-            rs.getString("image_url"),
-            rs.getInt("price")
-    );
+    private static final RowMapper<Product> productRowMapper = (rs, rowNum) -> new Product(rs.getLong("id"),
+                                                                                           rs.getString("name"),
+                                                                                           rs.getString("image_url"),
+                                                                                           rs.getInt("price"));
+
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     public H2ProductDao(final JdbcTemplate jdbcTemplate) {
@@ -29,14 +28,10 @@ public class H2ProductDao implements ProductDao {
 
     @Override
     public Product insert(final Product product) {
-        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
-        namedParameterJdbcTemplate.update(
-                "INSERT INTO products (name, image_url, price) VALUES (:name, :imageUrl, :price)",
-                new BeanPropertySqlParameterSource(product),
-                keyHolder,
-                new String[]{"id"}
-        );
-        long id = keyHolder.getKey().longValue();
+        final SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(namedParameterJdbcTemplate.getJdbcTemplate()).withTableName("products")
+                                                                                                                    .usingGeneratedKeyColumns("id");
+        final Number key = simpleJdbcInsert.executeAndReturnKey(new BeanPropertySqlParameterSource(product));
+        long id = key.longValue();
         return new Product(id, product.getName(), product.getImageUrl(), product.getPrice());
     }
 
@@ -47,20 +42,14 @@ public class H2ProductDao implements ProductDao {
 
     @Override
     public Optional<Product> findById(final Long id) {
-        Product product = namedParameterJdbcTemplate.queryForObject(
-                "SELECT id, name, image_url, price FROM products WHERE id = :id",
-                new MapSqlParameterSource("id", id),
-                PRODUCT_ROW_MAPPER
-        );
+        Product product = namedParameterJdbcTemplate.queryForObject("SELECT id, name, image_url, price FROM products WHERE id = :id",
+                                                                    new MapSqlParameterSource("id", id), productRowMapper);
         return Optional.ofNullable(product);
     }
 
     @Override
     public List<Product> findAll() {
-        return namedParameterJdbcTemplate.query(
-                "SELECT id, name, image_url, price FROM products",
-                PRODUCT_ROW_MAPPER
-        );
+        return namedParameterJdbcTemplate.query("SELECT id, name, image_url, price FROM products", productRowMapper);
     }
 
     @Override
