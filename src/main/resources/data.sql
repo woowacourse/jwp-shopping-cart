0a80d2e1d36796d4f189361eef35b55f3ed8f28a
seokjin8678 <seokjin8678@gmail.com>
2023-04-29 22:21:11 +0900

    feat: CART 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -18,3 +18,12 @@ CREATE TABLE MEMBER
     PRIMARY KEY (id)
 );
 
+CREATE TABLE CART
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES MEMBER (id),
+    FOREIGN KEY (product_id) REFERENCES PRODUCT (id)
+);
