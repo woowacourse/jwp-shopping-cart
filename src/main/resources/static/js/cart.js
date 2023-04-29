e00335abdef65a373f3fcc141379d8e686e149cf
seokjin8678 <seokjin8678@gmail.com>
2023-04-30 01:04:59 +0900

    feat: 장바구니 페이지에 API 연동

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -6,11 +6,13 @@ const addCartItem = (productId) => {
         return;
     }
 
-    // TODO: [2단계] 장바구니 CRUD API에 맞게 변경
     axios.request({
-        url: '',
+        method: 'post',
+        url: '/api/cart',
+        data: productId,
         headers: {
-            'Authorization': `Basic ${credentials}`
+            'Authorization': `Basic ${credentials}`,
+            'Content-Type': 'application/json'
         }
     }).then((response) => {
         alert('장바구니에 담았습니다.');
@@ -19,7 +21,7 @@ const addCartItem = (productId) => {
     });
 }
 
-const removeCartItem = (id) => {
+const removeCartItem = (productId) => {
     const credentials = localStorage.getItem('credentials');
     if (!credentials) {
         alert('사용자 정보가 없습니다.');
@@ -27,11 +29,13 @@ const removeCartItem = (id) => {
         return;
     }
 
-    // TODO: [2단계] 장바구니 CRUD API에 맞게 변경
     axios.request({
-        url: '',
+        method: 'delete',
+        url: '/api/cart',
+        data: productId,
         headers: {
-            'Authorization': `Basic ${credentials}`
+            'Authorization': `Basic ${credentials}`,
+            'Content-Type': 'application/json'
         }
     }).then((response) => {
         window.location.reload();
