17892f73ae2c8b46595505dd8427f56b14890a68
seokjin8678 <seokjin8678@gmail.com>
2023-04-30 01:05:32 +0900

    feat: 테스트용 데이터 추가

--- src/main/java/cart/TestDataInit.java
+++ src/main/java/cart/TestDataInit.java
@@ -2,6 +2,9 @@ package cart;
 
 import cart.domain.Member;
 import cart.domain.Product;
+import cart.entity.MemberEntity;
+import cart.entity.ProductEntity;
+import cart.repository.CartDao;
 import cart.repository.MemberDao;
 import cart.repository.ProductDao;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,15 +18,27 @@ import org.springframework.context.event.EventListener;
 public class TestDataInit {
     @Autowired
     MemberDao memberDao;
+
     @Autowired
     ProductDao productDao;
 
+    @Autowired
+    CartDao cartDao;
+
     @EventListener(ApplicationReadyEvent.class)
     public void initData() {
-        memberDao.save(new Member("glenfiddich@naver.com", "123456"));
-        memberDao.save(new Member("glenlivet@naver.com", "123456"));
-        productDao.save(new Product("글렌피딕 12년", 10_000, "https://static.thcdn.com/images/large/original//productimg/1600/1600/12957094-4064940467862805.jpg"));
-        productDao.save(new Product("글렌리벳 12년", 20_000, "https://www.willowpark.net/cdn/shop/products/21097-GlenLivet12YearOldSingleMaltScotchWhisky.png?v=1619049207"));
-        productDao.save(new Product("달모어 62년", 9_999_999, "https://www.thewhiskyexchange.com/media/rtwe/uploads/product/large/5480.jpg"));
+        MemberEntity member1 = memberDao.save(new Member("glenfiddich@naver.com", "123456"));
+        MemberEntity member2 = memberDao.save(new Member("glenlivet@naver.com", "123456"));
+        ProductEntity product1 = productDao.save(new Product("글렌피딕 12년", 10_000,
+                "https://static.thcdn.com/images/large/original//productimg/1600/1600/12957094-4064940467862805.jpg"));
+        ProductEntity product2 = productDao.save(new Product("글렌리벳 12년", 20_000,
+                "https://www.willowpark.net/cdn/shop/products/21097-GlenLivet12YearOldSingleMaltScotchWhisky.png?v=1619049207"));
+        ProductEntity product3 = productDao.save(new Product("달모어 62년", 9_999_999,
+                "https://www.thewhiskyexchange.com/media/rtwe/uploads/product/large/5480.jpg"));
+        cartDao.save(member1.getId(), product1.getId());
+        cartDao.save(member1.getId(), product2.getId());
+        cartDao.save(member2.getId(), product1.getId());
+        cartDao.save(member2.getId(), product2.getId());
+        cartDao.save(member2.getId(), product3.getId());
     }
 }
