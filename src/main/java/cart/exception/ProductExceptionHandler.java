7efcd404226fb30fddfdea0b92a03a44743e980d
kokodak <kokodakadokok@gmail.com>
2023-04-29 13:54:15 +0900

    refactor: NOT FOUND 에러를 처리하는 예외 처리 메서드 추가

--- src/main/java/cart/exception/ProductExceptionHandler.java
+++ src/main/java/cart/exception/ProductExceptionHandler.java
@@ -2,6 +2,7 @@ package cart.exception;
 
 import java.util.stream.Collectors;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,18 +13,27 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class ProductExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionResponse> handleInvalidException(final MethodArgumentNotValidException e) {
+    public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         final String errorMessage = e.getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.joining(", "));
-        return ResponseEntity.badRequest().body(
-                new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), errorMessage));
+
+        return ResponseEntity
+                .badRequest()
+                .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.value(), errorMessage));
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<ExceptionResponse> handleEmptyResultDataAccessException(final EmptyResultDataAccessException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionResponse(HttpStatus.NOT_FOUND.value(), e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleUnexpectedException(final Exception e) {
-        System.out.println(e.getClass());
-        return ResponseEntity.internalServerError().body(
-                new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
+        return ResponseEntity
+                .internalServerError()
+                .body(new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()));
     }
 }
