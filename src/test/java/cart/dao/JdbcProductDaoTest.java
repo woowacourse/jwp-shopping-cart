4121e25882db036cf6c2bcddc64c4fdb4fc5ec6f
hectick <ch.yeondev@gmail.com>
2023-04-29 16:02:11 +0900

    test: 존재하지 않는 상품을 수정, 삭제하려고 할 때 0을 반환하는 기능 테스트

--- src/test/java/cart/dao/JdbcProductDaoTest.java
+++ src/test/java/cart/dao/JdbcProductDaoTest.java
@@ -35,19 +35,39 @@ class JdbcProductDaoTest {
         assertDoesNotThrow(() -> productDao.insert(productEntity));
     }
 
-    @DisplayName("상품 수정이 되는지 확인한다")
+    @DisplayName("상품 수정이 되면 1을 반환하는지 확인한다")
     @Test
     void updateTest() {
         final ProductEntity productEntity = ProductEntity.of(1L, "chicken", "https://cdn.polinews.co.kr/news/photo/201910/427334_3.jpg", 10000);
+        int updatedRow = productDao.update(productEntity);
 
-        assertDoesNotThrow(() -> productDao.update(productEntity));
+        assertThat(updatedRow).isEqualTo(1);
     }
 
-    @DisplayName("상품 삭제가 되는지 확인한다")
+    @DisplayName("상품 수정이 되지 않으면 0을 반환하는지 확인한다")
+    @Test
+    void updateTest2() {
+        final ProductEntity productEntity = ProductEntity.of(3L, "chicken", "https://cdn.polinews.co.kr/news/photo/201910/427334_3.jpg", 10000);
+        int updatedRow = productDao.update(productEntity);
+
+        assertThat(updatedRow).isEqualTo(0);
+    }
+
+    @DisplayName("상품 삭제가 되면 1을 반환하는지 확인한다")
     @Test
     void deleteTest() {
         final ProductEntity productEntity = ProductEntity.of(1L, null, null, null);
+        int deletedRow = productDao.delete(productEntity);
 
-        assertDoesNotThrow(() -> productDao.delete(productEntity));
+        assertThat(deletedRow).isEqualTo(1);
+    }
+
+    @DisplayName("상품 삭제가 되지 않으면 0을 반환하는지 확인한다")
+    @Test
+    void deleteTest2() {
+        final ProductEntity productEntity = ProductEntity.of(3L, null, null, null);
+        int deletedRow = productDao.delete(productEntity);
+
+        assertThat(deletedRow).isEqualTo(0);
     }
 }
