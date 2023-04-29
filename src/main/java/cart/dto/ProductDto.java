bb0568d6ee97613e76506e81ab0c61f927039a21
kokodak <kokodakadokok@gmail.com>
2023-04-29 13:33:55 +0900

    refactor: 값의 범위 검증을 @Range 어노테이션을 사용하도록 변경

--- src/main/java/cart/dto/ProductDto.java
+++ src/main/java/cart/dto/ProductDto.java
@@ -1,9 +1,8 @@
 package cart.dto;
 
 import cart.repository.entity.ProductEntity;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.PositiveOrZero;
+import org.hibernate.validator.constraints.Range;
 
 public class ProductDto {
 
@@ -15,8 +14,7 @@ public class ProductDto {
     @NotBlank(message = "이미지 경로는 공백일 수 없습니다.")
     private String imageUrl;
 
-    @PositiveOrZero(message = "가격은 10억 이하의 음이 아닌 정수여야 합니다.")
-    @Max(value = 1_000_000_000, message = "가격은 10억 이하의 음이 아닌 정수여야 합니다.")
+    @Range(min = 0, max = 1_000_000_000 ,message = "가격은 10억 이하의 음이 아닌 정수여야 합니다.")
     private int price;
 
     public ProductDto() {
