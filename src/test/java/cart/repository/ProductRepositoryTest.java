bb5aa8b48c7fb23f020f0175fbe4d09b006519cf
hong-sile <gurwns9325@naver.com>
2023-04-29 16:53:31 +0900

    feat: 테스트와 프로덕션 데이터베이스 환경 분리하기

--- src/test/java/cart/repository/ProductRepositoryTest.java
+++ src/test/java/cart/repository/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package cart.repository;
 
+import static cart.ProductFixture.PRODUCT_ENTITY1;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
@@ -11,81 +12,81 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
 
+
+@TestPropertySource(locations = "/application-data.properties")
 @JdbcTest
 @Import(JdbcProductRepository.class)
 class ProductRepositoryTest {
 
-    private static final Product PRODUCT_ENTITY = new Product("테스트", "테스트URL.png", 4000);
     @Autowired
     private ProductRepository productRepository;
 
+    @Sql("/test-fixture.sql")
     @Test
     @DisplayName("모든 상품을 조회하는 기능 테스트")
     public void findAll() {
-        productRepository.save(PRODUCT_ENTITY);
-        productRepository.save(PRODUCT_ENTITY);
-        productRepository.save(PRODUCT_ENTITY);
-        productRepository.save(PRODUCT_ENTITY);
-
         final List<Product> all = productRepository.findAll();
         assertThat(all).hasSize(4);
     }
 
+    @Sql("/test-fixture.sql")
     @Test
     @DisplayName("ID로 상품을 조회하는 기능 테스트")
     public void findById() {
-        final Product savedProduct = productRepository.save(PRODUCT_ENTITY);
-
-        final Optional<Product> product = productRepository.findById(savedProduct.getId());
+        final Optional<Product> product = productRepository.findById(PRODUCT_ENTITY1.getId());
 
         assertAll(
                 () -> assertThat(product).isNotEmpty(),
-                () -> assertThat(product.get().getName()).isEqualTo(PRODUCT_ENTITY.getName())
+                () -> assertThat(product.get().getName()).isEqualTo(PRODUCT_ENTITY1.getName())
         );
     }
 
+    @Sql("/clear.sql")
     @Test
     @DisplayName("상품을 저장하는 기능 테스트")
     public void createProduct() {
-        final Product savedProduct = productRepository.save(PRODUCT_ENTITY);
+        final Product product = new Product("TestProduct", "a.png", 2000);
+        final Product savedProduct = productRepository.save(product);
 
         assertAll(
                 () -> assertThat(savedProduct.getId()).isNotNull(),
-                () -> assertThat(savedProduct.getName()).isEqualTo(PRODUCT_ENTITY.getName()),
-                () -> assertThat(savedProduct.getImageUrl()).isEqualTo(PRODUCT_ENTITY.getImageUrl()),
-                () -> assertThat(savedProduct.getPrice()).isEqualTo(PRODUCT_ENTITY.getPrice())
+                () -> assertThat(savedProduct.getName()).isEqualTo("TestProduct"),
+                () -> assertThat(savedProduct.getImageUrl()).isEqualTo("a.png"),
+                () -> assertThat(savedProduct.getPrice()).isEqualTo(2000)
         );
     }
 
+    @Sql("/test-fixture.sql")
     @Test
     @DisplayName("상품을 업데이트하는 기능 테스트")
     public void updateProduct() {
-        final Product savedProduct = productRepository.save(PRODUCT_ENTITY);
         //given
-        final Product product = productRepository.findById(savedProduct.getId())
+        final Product product = productRepository.findById(PRODUCT_ENTITY1.getId())
                 .orElseThrow(IllegalArgumentException::new);
-        assertThat(product.getName()).isEqualTo(PRODUCT_ENTITY.getName());
+        assertThat(product.getName()).isEqualTo(PRODUCT_ENTITY1.getName());
 
         //when
-        final Product changedProduct = new Product(savedProduct.getId(), "CHANGED", product.getImageUrl(),
+        final Product changedProduct = new Product(PRODUCT_ENTITY1.getId(), "CHANGED", product.getImageUrl(),
                 product.getPrice());
         productRepository.update(changedProduct);
 
         //then
-        final Product updatedProduct = productRepository.findById(savedProduct.getId())
+        final Product updatedProduct = productRepository.findById(PRODUCT_ENTITY1.getId())
                 .orElseThrow(IllegalArgumentException::new);
         assertThat(updatedProduct.getName()).isEqualTo("CHANGED");
     }
 
+    @Sql("/test-fixture.sql")
     @Test
     @DisplayName("상품을 삭제하는 기능 테스트")
     public void deleteById() {
-        final Product savedProduct = productRepository.save(PRODUCT_ENTITY);
-        assertThat(productRepository.findAll()).hasSize(1);
+        assertThat(productRepository.findAll()).hasSize(4);
 
-        productRepository.deleteById(savedProduct.getId());
+        productRepository.deleteById(PRODUCT_ENTITY1.getId());
 
-        assertThat(productRepository.findAll()).hasSize(0);
+        assertThat(productRepository.findAll()).hasSize(3);
     }
 }
