553f0b4b7b4005c5d771a22e2ff67b74b702f8fd
drunkenhw <drunkenhw@gmail.com>
2023-04-29 20:17:10 +0900

    refactor(ProductController): http 상태코드 변경

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Controller
 public class ProductController {
@@ -48,13 +47,8 @@ public class ProductController {
 
     @PostMapping("/products")
     public ResponseEntity<Product> createProduct(@Valid @RequestBody ProductRequest productRequest) {
-        Product product = new Product(productRequest.getName(), productRequest.getImgUrl(), productRequest.getPrice());
-        Product createdProduct = productService.createProduct(product);
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/")
-                .build()
-                .toUri();
-        return ResponseEntity.created(location).body(createdProduct);
+        Product createdProduct = productService.createProduct(productRequest.toEntity());
+        return ResponseEntity.created(URI.create("/products/" + createdProduct.getId())).body(createdProduct);
     }
 
     @PatchMapping("/products/{id}")
@@ -63,12 +57,12 @@ public class ProductController {
         Product product = new Product(id, productRequest.getName(), productRequest.getImgUrl(),
                 productRequest.getPrice());
         productService.updateProduct(product);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/products/{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable long id) {
         productService.deleteProductBy(id);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
