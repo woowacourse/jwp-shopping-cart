533345f64dafdd7a296b46bcf4d2199ffa9759a2
hectick <ch.yeondev@gmail.com>
2023-04-29 18:18:48 +0900

    test: 매 테스트가 끝나고 데이터베이스를 롤백하는 기능 추가 및 존재하지 않는 상품에 대한 요청 테스트 작성

--- src/main/java/cart/dao/ProductInitializer.java
+++ src/main/java/cart/dao/ProductInitializer.java
@@ -4,8 +4,10 @@ import cart.entity.ProductEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Profile("!test")
 @Component
 public class ProductInitializer implements ApplicationRunner {
 
@@ -18,6 +20,7 @@ public class ProductInitializer implements ApplicationRunner {
 
     @Override
     public void run(final ApplicationArguments args) throws Exception {
+        System.out.println("헤ㅔ헤헤헤헤헤ㅔㅖ");
         final ProductEntity productEntity1 = ProductEntity.of("mouse", "https://cdn.polinews.co.kr/news/photo/201910/427334_3.jpg", 100000);
         final ProductEntity productEntity2 = ProductEntity.of("keyboard", "https://i1.wp.com/blog.peoplefund.co.kr/wp-content/uploads/2020/01/진혁.jpg?fit=770%2C418&ssl=1", 250000);
 
---
+++ src/test/resources/testData.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS product;
+
+CREATE TABLE product
+(
+    id bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    image text NOT NULL,
+    price int NOT NULL
+);
+
+INSERT INTO product(name, image, price) VALUES ('mouse', 'https://cdn.polinews.co.kr/news/photo/201910/427334_3.jpg', 100000);
+INSERT INTO product(name, image, price) VALUES ('keyboard', 'https://i1.wp.com/blog.peoplefund.co.kr/wp-content/uploads/2020/01/진혁.jpg?fit=770%2C418&ssl=1', 250000);
