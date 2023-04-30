8fc6f57b4ef53d8c9538e9765333d609e0b846d7
Gyuseong Lee <35948985+aak2075@users.noreply.github.com>
2023-04-30 18:11:14 +0900

    style : 테스트 파일 이름 변경

--- src/test/java/cart/controller/rest/ProductsControllerTest.java
+++ src/test/java/cart/controller/rest/ProductsControllerTest.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
-class ProductsControllerMockTest {
+class ProductsControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
