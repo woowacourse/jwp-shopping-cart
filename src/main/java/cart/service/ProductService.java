797de0adef9f45eff046061fd007711035bec981
Hyeonjae-K <kwonhyeonjae01@gmail.com>
2023-04-30 21:02:09 +0900

    refactor: 실패시 롤백을 위해 Transactional 어노테이션 추가

--- src/main/java/cart/service/ProductService.java
+++ src/main/java/cart/service/ProductService.java
@@ -5,8 +5,10 @@ import cart.domain.Product;
 import cart.dto.ProductDto;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class ProductService {
 
     private final ProductDao productDao;
