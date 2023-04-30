027a543031af41986f9b2b30aa862698dbdabf5c
amaran-th <songsy405@gmail.com>
2023-05-01 00:42:01 +0900

    docs: member, cart 테이블 정의 sql 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,6 @@
 DROP TABLE product IF EXISTS;
+DROP TABLE member IF EXISTS;
+DROP TABLE cart IF EXISTS;
 
 CREATE TABLE product(
     `id` BIGINT NOT NULL AUTO_INCREMENT,
@@ -8,6 +10,25 @@ CREATE TABLE product(
     PRIMARY KEY(id)
 );
 
+CREATE TABLE member(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(20) NOT NULL,
+    `password` VARCHAR(20) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE cart(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `member_id` BIGINT NOT NULL,
+    `product_id` BIGINT NOT NULL,
+    FOREIGN KEY(member_id) REFERENCES `member`(id),
+    FOREIGN KEY(product_id) REFERENCES `product`(id),
+    PRIMARY KEY(id)
+);
+
 INSERT INTO product(name, price, image_url) VALUES ('피자', 13000, 'https://searchad-phinf.pstatic.net/MjAyMjEyMjdfMTE1/MDAxNjcyMTAxNTI0Nzg4.WfiSlsy9fTUQJ6q2FTGOaaOVU0QpSB0U1LvplKZQXzIg.H4UgI0VbKUszP7mzC3qhwpSMe15DluJnxjxVGDq_QUgg.PNG/451708-1fa87663-02e3-4303-b8a9-d7eea3676018.png?type=f160_160');
 INSERT INTO product(name, price, image_url) VALUES ('치킨', 27000, 'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMzAzMjdfMTI2%2FMDAxNjc5OTI1ODQ4NTgy.6RT9z-i5prsnwwc-6B9TaK6Q0Zcgsd3TeDGiUdqyDRIg.rW2kMtzBKNFhWWXyr_X2bZfR15AEPUOz-VJnqVaP0jEg.JPEG.koreasinju%2FIMG_3379.jpg&type=ff332_332');
 INSERT INTO product(name, price, image_url) VALUES ('샐러드', 9500, 'https://searchad-phinf.pstatic.net/MjAyMzA0MThfMjk0/MDAxNjgxODAwNDY4NjU4.FJcjmoGsxyCq0nZqlcmoAL2mwX8mM9ny9DdliQcqGZ0g.9cGk2IQHfPIm2-ABelEOY1cc-_8NBQgPMgPpjFZkGFEg.JPEG/2814800-bb4236af-96dd-42e7-8256-32ffaa73de52.jpg?type=f160_160');
+
+INSERT INTO member(email, password) VALUES('songsy405@naver.com', 'abcd')
+INSERT INTO member(email, password) VALUES('songsy405@pusan.ac.kr', '1234')
