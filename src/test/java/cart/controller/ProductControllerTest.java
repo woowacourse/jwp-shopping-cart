9de4338090c6ce0b9cd8522d96657634071100b4
Miseong Kim <miseongk@naver.com>
2023-05-01 01:07:01 +0900

    test: 존재하지 않는 상품 수정, 삭제 시 예외 발생 테스트

--- src/test/java/cart/controller/ProductControllerTest.java
+++ src/test/java/cart/controller/ProductControllerTest.java
@@ -1,5 +1,7 @@
 package cart.controller;
 
+import static org.hamcrest.Matchers.is;
+
 import cart.dto.ProductAddRequestDto;
 import cart.dto.ProductModifyRequestDto;
 import io.restassured.RestAssured;
@@ -60,6 +62,21 @@ public class ProductControllerTest {
     }
 
     @Test
+    @DisplayName("존재하지 않는 상품을 수정하면 예외가 발생한다.")
+    void validateModifyNonExistProduct() {
+        ProductModifyRequestDto productModifyRequestDto = new ProductModifyRequestDto("샐러드", 10000, "밋밋엉");
+
+        RestAssured.given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(productModifyRequestDto)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().put("/products/1")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("message", is("존재하지 않는 id 입니다."));
+    }
+
+    @Test
     @DisplayName("상품을 삭제한다.")
     void removeProduct() {
         RestAssured.given().log().all()
@@ -67,6 +84,17 @@ public class ProductControllerTest {
                 .when().delete("/products/2")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 상품을 삭제하면 예외가 발생한다.")
+    void validateRemoveNonExistProduct() {
+        RestAssured.given().log().all()
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().delete("/products/1")
+                .then().log().all()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("message", is("존재하지 않는 id 입니다."));
     }
 
     @AfterEach
