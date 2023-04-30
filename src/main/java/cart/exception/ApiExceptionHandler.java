bf37440b3e8509a8866023b330e7a9839a329d4f
yujamint <woawoa0513@gmail.com>
2023-05-01 00:13:47 +0900

    feat: 뷰 입력값 검증 과정에서 발생할 수 있는 예외에 대한 핸들러 구현

--- src/main/java/cart/exception/ApiExceptionHandler.java
+++ src/main/java/cart/exception/ApiExceptionHandler.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,6 +28,11 @@ public class ApiExceptionHandler {
     public ResponseEntity<String> handleDataAccessException(DataAccessException e) {
         logger.error("예기치 못한 DataAccessException 발생: " + e);
         return ResponseEntity.badRequest().body("데이터를 찾아오는 과정에서 문제가 발생했습니다.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body("잘못된 형식의 값이 입력됐습니다.");
     }
 
     @ExceptionHandler(Exception.class)
