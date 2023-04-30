f91852c3ccf126bd8393e2c41d499b1e999e4ea5
cpot5620 <=cpot5620@naver.com>
2023-04-30 14:39:22 +0900

    refactor(Exception): 예외 클래스 명시 방법 통일

--- src/main/java/cart/web/handler/CartExceptionHandler.java
+++ src/main/java/cart/web/handler/CartExceptionHandler.java
@@ -1,36 +1,37 @@
 package cart.web.handler;
 
-import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @RestControllerAdvice
 public class CartExceptionHandler {
-    @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<String> handleIllegalResourceAccess() {
+    @ExceptionHandler
+    public ResponseEntity<String> handleIllegalResourceAccess(NoSuchElementException e) {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body("존재하지 않는 자원입니다.");
     }
 
-    @ExceptionHandler(NumberFormatException.class)
-    public ResponseEntity<String> handleIllegalNumberFormatRequest() {
+    @ExceptionHandler
+    public ResponseEntity<String> handleIllegalNumberFormatRequest(NumberFormatException e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body("옳바르지 않은 숫자 형식입니다.");
     }
 
     @ExceptionHandler
-    public ResponseEntity<String> handleIllegalArgumentRequest(IllegalArgumentException exception) {
+    public ResponseEntity<String> handleIllegalArgumentRequest(IllegalArgumentException e) {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
-                .body(exception.getMessage());
+                .body(e.getMessage());
     }
 
-    @ExceptionHandler(RuntimeException.class)
-    public ResponseEntity<String> handleRuntimeException() {
+    @ExceptionHandler
+    public ResponseEntity<String> handleRuntimeException(RuntimeException e) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("예기치 못한 오류가 발생했습니다.");
