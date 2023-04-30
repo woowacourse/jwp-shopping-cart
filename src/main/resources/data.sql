82c183a22d14853650881676553c40fce100089a
thdwoqor <celebarte01@gmail.com>
2023-04-30 13:53:25 +0900

    chore : member 테이블 설계 및 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,16 @@ CREATE TABLE IF NOT EXISTS item
     item_url text                              NOT NULL,
     price    int                               NOT NULL
 );
+
+insert into item(name, item_url, price)
+values ('치킨', 'https://image.homeplus.kr/td/f42afe4b-e0a8-4c79-a07c-aaee40c93a57', 10000);
+
+CREATE TABLE IF NOT EXISTS member
+(
+    email        varchar(30) PRIMARY KEY NOT NULL,
+    name         varchar(30)             NOT NULL,
+    phone_number varchar(30)             NOT NULL,
+    password     varchar(30)             NOT NULL
+);
+
+insert into member(email, name, phone_number, password) values ('test@gmail.com', 'test', '01098765432', 'qwer1234')
