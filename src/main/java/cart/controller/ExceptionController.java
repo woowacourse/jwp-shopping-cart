984a23b13f54b8cf096fecf540f26f6c539b2f6d
kevstevie <jianna442@gmail.com>
2023-04-30 14:17:06 +0900

    refactor: ResponseEntityExceptionHandler 적용

--- src/main/java/cart/controller/ExceptionController.java
+++ src/main/java/cart/controller/ExceptionController.java
@@ -1,20 +1,9 @@
 package cart.controller;
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class ExceptionController {
+public class ExceptionController extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentException(final MethodArgumentNotValidException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException exception) {
-        return ResponseEntity.badRequest().body(exception.getMessage());
-    }
 }
