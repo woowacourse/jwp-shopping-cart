cdddba83b628bde5251fbf15a119b7b77a2308f4
woo-chang <clllickme@naver.com>
2023-04-30 16:14:38 +0900

    refactor(MemberDao): 쿼리 결과가 존재하지 않았을 때 예외 처리

--- src/test/java/cart/dao/MemberDaoTest.java
+++ src/test/java/cart/dao/MemberDaoTest.java
@@ -3,12 +3,16 @@ package cart.dao;
 import cart.entity.MemberEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
@@ -38,5 +42,33 @@ class MemberDaoTest {
                 () -> assertThat(result.get(1).getEmail()).isEqualTo("b@b.com"),
                 () -> assertThat(result.get(1).getPassword()).isEqualTo("password2")
         );
+    }
+
+    @Nested
+    @DisplayName("이메일, 비밀번호를 통한 멤버 조회 시")
+    class FindByEmailAndPassword {
+
+        @Test
+        @DisplayName("일치하는 멤버가 존재한다면 멤버를 조회한다.")
+        void findByEmailAndPassword() {
+            final String email = "a@a.com";
+            final String password = "password1";
+
+            final Optional<MemberEntity> result = memberDao.findByEmailAndPassword(email, password);
+
+            assertThat(result).isNotEmpty();
+        }
+
+        @ParameterizedTest
+        @CsvSource(value = {
+                "a@a.com,password2",
+                "a@b.com,password1",
+        })
+        @DisplayName("일치하는 멤버가 존재하지 않으면 멤버를 조회하지 못한다.")
+        void findByEmailAndPasswordWithNull(final String email, final String password) {
+            final Optional<MemberEntity> result = memberDao.findByEmailAndPassword(email, password);
+
+            assertThat(result).isEmpty();
+        }
     }
 }
