02d12d94df9ef66962457e1821e2ad167623563f
dhkim <akdlzmf1123@gmail.com>
2023-04-30 20:16:29 +0900

    feat: Members table 설계

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,12 @@ CREATE TABLE IF NOT EXISTS products
     price     INT           NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS members
+(
+    id           BIGINT       NOT NULL AUTO_INCREMENT,
+    email        VARCHAR(100) NOT NULL UNIQUE,
+    password     VARCHAR(40)  NOT NULL,
+    phone_number VARCHAR(20)  NOT NULL UNIQUE,
+    PRIMARY KEY (id)
+);
