0b711cdefbced68c466b7c99329e0160bc578d02
BackFoxx <backfoxxx@gmail.com>
2023-04-30 14:16:22 +0900

    refactor : 컨트롤러 유닛 테스트에서는 테스트 대상인 컨트롤러만 빈으로 등록하도록 변경

--- src/test/java/cart/controller/AdminControllerTest.java
+++ src/test/java/cart/controller/AdminControllerTest.java
@@ -1,42 +1,28 @@
 package cart.controller;
 
 import cart.entity.Product;
-import cart.repository.JdbcProductRepository;
 import cart.repository.ProductRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
-import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Locale;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@WebMvcTest(controllers = AdminController.class)
 class AdminControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
