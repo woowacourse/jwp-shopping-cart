6e214a8d5e7a349245d85b128d45739dcfdd1c7c
0chil <0@chll.it>
2023-04-30 18:59:56 +0900

    refactor: 예외 핸들러 메시지 개선, 불필요한 핸들러 제거

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -5,26 +5,30 @@ import java.util.stream.Collectors;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import cart.controller.dto.ExceptionResponse;
 
-// @RestControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    private static final String INVALID_FORMAT_EXCEPTION_MESSAGE = "형이 잘못되었습니다";
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleUnhandled(Exception exception) {
+        exception.printStackTrace();
+        return ResponseEntity.internalServerError().build();
+    }
 
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgument(final IllegalArgumentException exception) {
         return ResponseEntity.badRequest().body(new ExceptionResponse(exception.getMessage()));
     }
 
-    @ExceptionHandler(InvalidFormatException.class)
-    public ResponseEntity<ExceptionResponse> handle() {
-        return ResponseEntity.badRequest().body(new ExceptionResponse(INVALID_FORMAT_EXCEPTION_MESSAGE));
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadable() {
+        return ResponseEntity.badRequest().body(new ExceptionResponse("요청 페이로드를 읽지 못했습니다"));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
