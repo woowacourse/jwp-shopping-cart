c15ea9a789350d159c5f780309edf62a1f740fbc
jiwon <ljwon77@daum.net>
2023-05-01 01:56:03 +0900

    feat: 사용자 페이지 뷰를 구현한다

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,7 +1,51 @@
+const modal = document.getElementById('modal');
+
 const selectMember = (member) => {
-    <!-- TODO: [2단계] 사용자 정보에 맞게 변경 -->
     const {email, password} = member;
-    const string = `${email}:${password}`;
-    localStorage.setItem('credentials', btoa(string));
+    const token = `${email}:${password}`;
+    localStorage.setItem('credentials', btoa(token));
     alert(`${email} 사용자로 설정 했습니다.`);
+    window.location.href = '/';
 }
+
+const showMemberInfo = (memberId) => {
+    window.location.href = `/member/${memberId}`
+}
+
+const showUserAddModal = () => {
+    modal.dataset.formType = 'add';
+    modal.style.display = 'block';
+};
+
+const hideUserAddModal = () => {
+    modal.style.display = 'none';
+    const elements = modal.getElementsByTagName('input');
+    for (const element of elements) {
+        element.value = '';
+    }
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    let user = {};
+
+    for (const entry of formData.entries()) {
+        const [key, value] = entry;
+        user[key] = value;
+    }
+
+    createUser(user);
+});
+
+
+const createUser = (user) => {
+    axios.post('/member', user)
+        .then((response) => {
+            window.location.reload();
+        }).catch((error) => {
+        const {data} = error.response;
+        window.alert(data.errorMessage)
+    });
+};
