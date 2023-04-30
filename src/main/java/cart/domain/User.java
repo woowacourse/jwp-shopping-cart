855f652ef5361ef9bd19be6fbb24c3e345212c3d
jiwon <ljwon77@daum.net>
2023-04-30 14:05:04 +0900

    feat: 사용자 도메인을 추가한다

---
+++ src/main/java/cart/domain/User.java
@@ -0,0 +1,67 @@
+package cart.domain;
+
+import cart.exception.GlobalException;
+
+import static cart.exception.ErrorCode.USER_EMAIL_LENGTH;
+import static cart.exception.ErrorCode.USER_NICKNAME_LENGTH;
+import static cart.exception.ErrorCode.USER_PASSWORD_LENGTH;
+
+public class User {
+
+    private static final int EMAIl_MIN_LENGTH = 5, EMAIL_MAX_LENGTH = 50;
+    private static final int PASSWORD_MIN_LENGTH = 8, PASSWORD_MAX_LENGTH = 50;
+    private static final int NICKNAME_MIN_LENGTH = 1, NICKNAME_MAX_LENGTH = 30;
+
+    private final String email;
+    private final String password;
+    private final String nickname;
+    private final String telephone;
+
+    private User(final String email, final String password, final String nickname, final String telephone) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+        this.telephone = telephone;
+    }
+
+    public static User create(final String email, final String password, final String nickname, final String telephone) {
+        validateEmail(email);
+        validatePassword(password);
+        validateNickname(nickname);
+        return new User(email, password, nickname, telephone);
+    }
+
+    private static void validateEmail(final String email) {
+        if (email.length() < EMAIl_MIN_LENGTH || email.length() > EMAIL_MAX_LENGTH) {
+            throw new GlobalException(USER_EMAIL_LENGTH);
+        }
+    }
+
+    private static void validatePassword(final String password) {
+        if (password.length() < PASSWORD_MIN_LENGTH || password.length() > PASSWORD_MAX_LENGTH) {
+            throw new GlobalException(USER_PASSWORD_LENGTH);
+        }
+    }
+
+    private static void validateNickname(final String nickname) {
+        if (nickname.length() < NICKNAME_MIN_LENGTH || nickname.length() > NICKNAME_MAX_LENGTH) {
+            throw new GlobalException(USER_NICKNAME_LENGTH);
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+}
---
+++ src/test/java/cart/domain/UserTest.java
@@ -0,0 +1,55 @@
+package cart.domain;
+
+import cart.exception.ErrorCode;
+import cart.exception.GlobalException;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class UserTest {
+
+    @ParameterizedTest(name = "정상적인 사용자 정보가 들어올 경우 예외가 발생하지 않는다.")
+    @CsvSource(value = {"a@b.c:12345678:오",
+            "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde@fghj.com:" +
+                    "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde:" +
+                    "abcdeabcdeabcdeabcdeabcdeabcde"}, delimiter = ':')
+    void create_success(final String validEmail, final String validPassword, final String validNickname) {
+        final User createdUser = assertDoesNotThrow(() ->
+                User.create(validEmail, validPassword, validNickname, "010-1234-5678"));
+
+        assertThat(createdUser)
+                .extracting("email", "password", "nickname", "telephone")
+                .containsExactly(validEmail, validPassword, validNickname, "010-1234-5678");
+    }
+
+    @ParameterizedTest(name = "이메일 길이가 5글자 미만, 50글자 초과면 예외가 발생한다.")
+    @ValueSource(strings = {"abcd", "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdea"})
+    void create_withEmail_fail(final String invalidEmail) {
+        assertThatThrownBy(() -> User.create(invalidEmail, "password", "져니", "010-1234-5678"))
+                .isInstanceOf(GlobalException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.USER_EMAIL_LENGTH);
+    }
+
+    @ParameterizedTest(name = "비밀번호가 8글자 미만, 50글자 초과면 예외가 발생한다.")
+    @ValueSource(strings = {"abcdefg", "abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdea"})
+    void create_withPassword_fail(final String invalidPassword) {
+        assertThatThrownBy(() -> User.create("journey@gmail.com", invalidPassword, "져니", "010-1234-5678"))
+                .isInstanceOf(GlobalException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.USER_PASSWORD_LENGTH);
+    }
+
+    @ParameterizedTest(name = "닉네임이 1글자 미만, 30글자 초과면 예외가 발생한다.")
+    @ValueSource(strings = {"", "abcdeabcdeabcdeabcdeabcdeabcdea"})
+    void create_withNickname_fail(final String invalidNickname) {
+        assertThatThrownBy(() -> User.create("journey@gmail.com", "password", invalidNickname, "010-1234-5678"))
+                .isInstanceOf(GlobalException.class)
+                .extracting("errorCode")
+                .isEqualTo(ErrorCode.USER_NICKNAME_LENGTH);
+    }
+}
