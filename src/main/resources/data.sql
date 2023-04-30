07c95e58264782345271e87278111115fb867530
jeomxon <jeomxon@gmail.com>
2023-04-30 16:27:37 +0900

    refactor: data.sql의 테이블 컬럼 타입 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
 CREATE TABLE product
 (
     id        BIGINT       NOT NULL AUTO_INCREMENT,
-    name      VARCHAR(255) NOT NULL,
-    image_url VARCHAR(255) NOT NULL,
+    name      VARCHAR(100) NOT NULL,
+    image_url TEXT         NOT NULL,
     price     INT          NOT NULL,
     PRIMARY KEY (id)
 );
