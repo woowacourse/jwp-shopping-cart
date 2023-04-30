682bb49bb0e7d3115fb8cd9c1e8b0775d93e8654
apptie <appti@naver.com>
2023-04-30 12:05:54 +0900

    chore: 페이지에 javascript 기능 연동

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -4,10 +4,10 @@ const selectMember = (member) => {
         password: member.password
     }).then((response) => {
         console.log(response.data.basic);
+        localStorage.clear();
         localStorage.setItem('credentials', response.data.basic);
         alert(`${member.email} 사용자로 설정 했습니다.`);
     }).catch((error) => {
-        console.log(error);
         alert(error.response.data.message);
         console.error(error);
     });
