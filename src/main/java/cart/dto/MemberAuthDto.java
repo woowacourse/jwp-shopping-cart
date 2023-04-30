d6b0c550adf382bfe6a7ff0fcfc1cb7a9232cd22
woo-chang <clllickme@naver.com>
2023-04-30 16:41:11 +0900

    feat(MemberArgumentResolver): 인증 정보 파싱하는 기능 구현

---
+++ src/main/java/cart/dto/MemberAuthDto.java
@@ -0,0 +1,20 @@
+package cart.dto;
+
+public class MemberAuthDto {
+
+    private String email;
+    private String password;
+
+    public MemberAuthDto(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
