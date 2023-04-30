0eb7ef6ebf725d4021c27e9c4d04c166af908628
Miseong Kim <miseongk@naver.com>
2023-05-01 01:06:40 +0900

    feat: NoSuchElementException 예외 처리 추가

--- src/main/java/cart/exception/GlobalExceptionHandler.java
+++ src/main/java/cart/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package cart.exception;
 
+import java.util.NoSuchElementException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +13,12 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleArgumentNotValid(final MethodArgumentNotValidException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleIllegalArgument(final NoSuchElementException e) {
         return new ErrorResponse(e.getMessage());
     }
 
