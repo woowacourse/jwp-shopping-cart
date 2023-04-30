3fd65be434c22b20f8e070b82b4d48c70fd9502b
sh111-coder <ohk9134@naver.com>
2023-04-30 22:08:13 +0900

    refactor: DB 테이블 DDL 구조 변경 리팩토링

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,31 +1,25 @@
-CREATE TABLE CART (
-    cart_id	BIGINT NOT NULL AUTO_INCREMENT
-);
-
 CREATE TABLE MEMBER (
     member_id BIGINT NOT NULL AUTO_INCREMENT,
     nickname VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    cart_id BIGINT NOT NULL,
-    PRIMARY KEY (member_id),
-    FOREIGN KEY (cart_id) REFERENCES CART (cart_id)
+    cart_id BIGINT UNIQUE NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (member_id)
 );
 
 CREATE TABLE PRODUCT (
-   product_id BIGINT NOT NULL AUTO_INCREMENT,
-   name VARCHAR(50) NOT NULL,
-   img_url VARCHAR(255) NOT NULL,
-   price INT NOT NULL,
-   PRIMARY KEY (product_id)
+     product_id BIGINT NOT NULL AUTO_INCREMENT,
+     name VARCHAR(50) NOT NULL,
+     img_url VARCHAR(255) NOT NULL,
+     price INT NOT NULL,
+     PRIMARY KEY (product_id)
 );
 
-CREATE TABLE CART_PRODUCT (
-     cart_product_id BIGINT	NOT NULL AUTO_INCREMENT,
-     cart_id	BIGINT	NOT NULL,
-     product_id	BIGINT	NOT NULL,
-     PRIMARY KEY (cart_product_id),
-     FOREIGN KEY (cart_id) REFERENCES CART (cart_id),
-     FOREIGN KEY (product_id) REFERENCES PRODUCT (product_id)
+CREATE TABLE MEMBER_PRODUCT (
+      cart_product_id BIGINT	NOT NULL AUTO_INCREMENT,
+      cart_id	BIGINT	NOT NULL,
+      product_id	BIGINT	NOT NULL,
+      PRIMARY KEY (cart_product_id),
+      FOREIGN KEY (cart_id) REFERENCES MEMBER (cart_id),
+      FOREIGN KEY (product_id) REFERENCES PRODUCT (product_id)
 );
-
