d535b41e25e6a6f758d609edcd4d218e837d403c
yujamint <woawoa0513@gmail.com>
2023-04-30 14:39:21 +0900

    feat: DataAccess 관련 예외 핸들러 구현

--- src/main/java/cart/exception/ApiExceptionHandler.java
+++ src/main/java/cart/exception/ApiExceptionHandler.java
@@ -2,6 +2,8 @@ package cart.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,6 +16,17 @@ public class ApiExceptionHandler {
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleEmptyResultDataAccess(DataAccessException e) {
+        return ResponseEntity.badRequest().body("존재하지 않는 데이터입니다.");
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<String> handleDataAccessException(DataAccessException e) {
+        logger.error("예기치 못한 DataAccessException 발생: " + e);
+        return ResponseEntity.badRequest().body("데이터를 찾아오는 과정에서 문제가 발생했습니다.");
     }
 
     @ExceptionHandler(Exception.class)
