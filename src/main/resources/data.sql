078fed9fb31b6272b4a2a4802531d06aa81a596d
kiarakim <kiarakim413@gmail.com>
2023-04-30 23:48:21 +0900

    refactor: created_at, updated_at 컬럼 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,5 +4,8 @@ id      BIGINT      NOT NULL AUTO_INCREMENT,
 name    VARCHAR(10) NOT NULL,
 price   DOUBLE      NOT NULL,
 image   TEXT        NOT NULL,
+created_at timestamp not null default current_timestamp,
+updated_at timestamp not null default current_timestamp on update current_timestamp,
+
 PRIMARY KEY(id)
 );
