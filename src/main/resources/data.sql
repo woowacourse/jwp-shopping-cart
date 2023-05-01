5b0309419bf765ec29e898d3d00ffd220b9cc9b4
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-01 14:00:50 +0900

    chore : 회원, 장바구니, 장바구니_상품 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,35 @@
 CREATE TABLE IF NOT EXISTS products
 (
-id      BIGINT      NOT NULL AUTO_INCREMENT,
-name    VARCHAR(10) NOT NULL,
-price   DOUBLE      NOT NULL,
-image   TEXT        NOT NULL,
-PRIMARY KEY(id)
+    id    BIGINT      NOT NULL AUTO_INCREMENT,
+    name  VARCHAR(10) NOT NULL,
+    price DOUBLE      NOT NULL,
+    image TEXT        NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS members
+(
+    id       BIGINT      NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(30) NOT NULL,
+    email    VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS carts
+(
+    id        BIGINT NOT NULL AUTO_INCREMENT,
+    member_id BIGINT NOT NULL UNIQUE,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES members (id)
+);
+
+CREATE TABLE  IF NOT EXISTS cart_product
+(
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    cart_id BIGINT NOT NULL ,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (cart_id) REFERENCES carts(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 );
