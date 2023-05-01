66642dde0d9e7dda5b039f11c36eac0cce49df68
splitube <voicesplit@daum.net>
2023-05-01 21:30:52 +0900

    refactor: CustomerEntity equals&hashCode 재정의

--- src/main/java/cart/entity/customer/CustomerEntity.java
+++ src/main/java/cart/entity/customer/CustomerEntity.java
@@ -1,5 +1,7 @@
 package cart.entity.customer;
 
+import java.util.Objects;
+
 public final class CustomerEntity {
 
     private Long id;
@@ -26,5 +28,22 @@ public final class CustomerEntity {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final CustomerEntity that = (CustomerEntity) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
