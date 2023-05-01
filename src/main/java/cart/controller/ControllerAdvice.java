da5add5f190d068a0fc092d29bbe1a4758ef1284
gitchan <eunkeeeee@gmail.com>
2023-05-01 10:44:23 +0900

    style(ControllerAdvice): 클래스 이름 변경

--- src/main/java/cart/controller/ControllerAdvice.java
+++ src/main/java/cart/controller/ControllerAdvice.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class ProductControllerAdvice {
+public class ControllerAdvice {
     @ExceptionHandler
     public ResponseEntity<String> handleException(MethodArgumentNotValidException e) {
         return ResponseEntity.badRequest().body(e.getBindingResult().getFieldError().getDefaultMessage());
