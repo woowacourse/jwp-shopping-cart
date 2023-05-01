d55004f6d5744480faf004d6ce2346e31ce9426f
JunDonghyuk <ehdgur4814@naver.com>
2023-05-02 01:01:12 +0900

    feat: 사용자 엔티티 및 디티오 구현

---
+++ src/main/java/cart/dto/MemberDto.java
@@ -0,0 +1,48 @@
+package cart.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class MemberDto {
+
+    private Long id;
+
+    @Email(message = "email 형식으로 입력해주세요.")
+    private String email;
+
+    @NotBlank(message = "이름은 공백일 수 없습니다.")
+    private String name;
+
+    @NotBlank(message = "비밀번호는 공백일 수 없습니다.")
+    private String password;
+
+    public MemberDto() {
+    }
+
+    public MemberDto(final Long id, final String email, final String name, final String password) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+
+    public MemberDto(final String email, final String name, final String password) {
+        this(null, email, name, password);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
