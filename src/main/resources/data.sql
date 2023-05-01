040cebfc2a3e237ecb40bedd1ce1d369ebe07e95
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-01 23:22:46 +0900

    refactor: imageUrl의 타입으로 text로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS ITEMS
 (
     id        BIGINT      NOT NULL AUTO_INCREMENT,
     name      VARCHAR(50) NOT NULL,
-    image_url VARCHAR(5000),
+    image_url TEXT,
     price     INT         NOT NULL,
     PRIMARY KEY (id)
 );
