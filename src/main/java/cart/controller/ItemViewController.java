abc97e11f7de4b5ae0316b92a24379f98012311d
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-01 18:43:34 +0900

    refactor: view를 반환하는 api를 한 컨트롤러로 통합

--- src/main/java/cart/controller/ItemViewController.java
+++ src/main/java/cart/controller/ItemViewController.java
@@ -10,13 +10,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.util.List;
 
 @Controller
-public class AdminController {
+public class ItemViewController {
 
     private final ItemService itemService;
 
     @Autowired
-    public AdminController(ItemService itemService) {
+    public ItemViewController(ItemService itemService) {
         this.itemService = itemService;
+    }
+
+    @GetMapping("/")
+    public String showItemList(final Model model) {
+        List<ItemResponse> items = itemService.loadAllItem();
+        model.addAttribute("products", items);
+        return "index";
     }
 
     @GetMapping("/admin")
