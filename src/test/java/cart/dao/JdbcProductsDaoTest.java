fbd7f5e00c33109ee44ed0c388a11121f8b6b6e6
pilyang <30036534+pilyang@users.noreply.github.com>
2023-05-01 19:31:51 +0900

    test: jdbc dao테스트용 db 초기화 방식 변경

--- src/test/java/cart/dao/JdbcProductsDaoTest.java
+++ src/test/java/cart/dao/JdbcProductsDaoTest.java
@@ -3,15 +3,13 @@ package cart.dao;
 import cart.entity.Product;
 import cart.exception.NoSuchProductException;
 import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Map;
 
@@ -21,22 +19,28 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @JdbcTest
 class JdbcProductsDaoTest {
 
-    @TestConfiguration
-    static class testConfig {
-        @Autowired
-        JdbcTemplate jdbcTemplate;
-
-        @Bean
-        public ProductsDao productsDao() {
-            return new JdbcProductsDao(jdbcTemplate);
-        }
-    }
+    private static final String TEST_NAME_1 = "test1";
+    private static final String TEST_NAME_2 = "test2";
+    private static final String TEST_NAME_3 = "test3";
+    private static final int TEST_PRICE_1 = 1000;
+    private static final int TEST_PRICE_2 = 2000;
+    private static final int TEST_PRICE_3 = 3000;
+    private static final String TEST_IMAGE_URL_1 = "https://test.com/image1";
+    private static final String TEST_IMAGE_URL_2 = "https://test.com/image2";
+    private static final String TEST_IMAGE_URL_3 = "https://test.com/image3";
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    @Autowired
     private ProductsDao productsDao;
+
+    @BeforeEach
+    void setup() {
+        productsDao = new JdbcProductsDao(jdbcTemplate);
+
+        jdbcTemplate.execute("truncate table products;");
+        jdbcTemplate.execute("alter table products alter id restart with 1;");
+    }
 
     @Test
     @DisplayName("상품 추가 테스트")
@@ -60,27 +64,38 @@ class JdbcProductsDaoTest {
 
     @Test
     @DisplayName("id로 검색 성공 테스트")
-    @Sql("classpath:testData.sql")
     void findById_success_test() {
         // given
         final long id = 1L;
+        insertInitialData();
 
         // when
         final Product product = productsDao.findById(id);
 
         // then
         SoftAssertions.assertSoftly(softly -> {
-            softly.assertThat(product.getName()).isEqualTo("test1");
-            softly.assertThat(product.getPrice()).isEqualTo(1000);
-            softly.assertThat(product.getImageUrl()).isEqualTo("https://test.com/image1");
+            softly.assertThat(product.getName()).isEqualTo(TEST_NAME_1);
+            softly.assertThat(product.getPrice()).isEqualTo(TEST_PRICE_1);
+            softly.assertThat(product.getImageUrl()).isEqualTo(TEST_IMAGE_URL_1);
         });
+    }
+
+    private void insertInitialData() {
+        jdbcTemplate.update("insert into products(product_name, product_price, product_image) values (?, ?, ?);",
+                TEST_NAME_1, TEST_PRICE_1, TEST_IMAGE_URL_1);
+        jdbcTemplate.update("insert into products(product_name, product_price, product_image) values (?, ?, ?);",
+                TEST_NAME_2, TEST_PRICE_2, TEST_IMAGE_URL_2);
+        jdbcTemplate.update("insert into products(product_name, product_price, product_image) values (?, ?, ?);",
+                TEST_NAME_3, TEST_PRICE_3, TEST_IMAGE_URL_3);
     }
 
     @Test
     @DisplayName("id로 검색 실패 테스트")
     void findById_fail_test() {
+        // given
         final long id = 1L;
 
+        // when & then
         assertThatThrownBy(() -> productsDao.findById(id))
                 .isInstanceOf(NoSuchProductException.class)
                 .hasMessage("해당 상품이 없습니다. 입략된 상품 id : " + id);
@@ -88,16 +103,19 @@ class JdbcProductsDaoTest {
 
     @Test
     @DisplayName("추가된 상품 조회 테스트")
-    @Sql("classpath:testData.sql")
     void readAll_test() {
+        // given
+        insertInitialData();
+
+        // when & then
         assertThat(productsDao.readAll()).hasSize(3);
     }
 
     @Test
     @DisplayName("업데이트 테스트")
-    @Sql("classpath:testData.sql")
     void update_test() {
         //given
+        insertInitialData();
         final long id = 1L;
         final String newName = "newName";
         final int newPrice = 10000;
@@ -121,11 +139,15 @@ class JdbcProductsDaoTest {
 
     @Test
     @DisplayName("데이터 제거 테스트")
-    @Sql("classpath:testData.sql")
     void delete_test() {
+        // given
+        insertInitialData();
         final long id = 1L;
+
+        // when
         productsDao.delete(id);
 
+        // then
         assertThatThrownBy(() -> jdbcTemplate.queryForObject("SELECT product_name FROM products where id = ?", String.class, id))
                 .isInstanceOf(EmptyResultDataAccessException.class);
     }
--- src/test/resources/testData.sql
+++ src/test/resources/testData.sql
@@ -1,6 +1,3 @@
-truncate table products;
-alter table products alter id restart with 1;
-
 insert into products(product_name, product_price, product_image) values ('test1', 1000, 'https://test.com/image1');
 insert into products(product_name, product_price, product_image) values ('test2', 2000, 'https://test.com/image2');
 insert into products(product_name, product_price, product_image) values ('test3', 3000, 'https://test.com/image3');
