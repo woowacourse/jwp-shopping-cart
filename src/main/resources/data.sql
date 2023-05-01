acc6787653d9a813dc7645fb18ef4b5459c7e0bf
yenawee <yaena0319@naver.com>
2023-05-01 14:35:18 +0900

    feat : production DB url 변경, 실행 시 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,3 @@
-CREATE TABLE product
-(
-    product_id BIGINT       NOT NULL AUTO_INCREMENT,
-    name       VARCHAR(50)  NOT NULL,
-    price      INT          NOT NULL,
-    image_url  VARCHAR(255) NOT NULL,
-
-    PRIMARY KEY (product_id)
-);
+INSERT INTO product(name, price, image_url)
+VALUES ('스프링', 1000, 'https://previews.123rf.com/images/marucyan/marucyan1211/marucyan121100106/16114832-새싹.jpg'),
+       ('빙봉', 10000, 'https://mblogthumb-phinf.pstatic.net/MjAxNjEyMTNfMTk4/MDAxNDgxNjAwMDE1ODM1.mxPz7FBOV9DF5ryoeYEAcfZfBwMeFOYp-p03N7RAO68g.ICYGpmoaW0Q0wSenF5JQzQ4LZ3aqRWazx66DTqYgJFcg.JPEG.jeenyjin/Screenshot_2016-12-12-13-48-12-1.jpg?type=w800');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS product
+(
+    product_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(50)  NOT NULL,
+    price      INT          NOT NULL,
+    image_url  VARCHAR(2048) NOT NULL,
+
+    PRIMARY KEY (product_id)
+);
