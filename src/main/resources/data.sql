ab6da6d7106c0ca246ebaf2edfb0a38777bb4b6d
Miseong Kim <miseongk@naver.com>
2023-05-02 00:47:23 +0900

    chore: 장바구니, 사용자 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,17 @@ CREATE TABLE Product (
      image_url LONGTEXT NOT NULL,
      PRIMARY KEY (id)
 );
+
+CREATE TABLE Member (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE Cart (
+    member_id INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN KEY(member_id) REFERENCES Member(id),
+    FOREIGN KEY(product_id) REFERENCES Product(id)
+);
