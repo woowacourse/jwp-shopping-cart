e42043cbfbd75d9be15b0ab6d75836d73916d6e7
sh111-coder <ohk9134@naver.com>
2023-05-01 17:06:07 +0900

    fix: 전체 테스트 한 번에 돌릴 시 실패하던 오류 fix

--- src/test/java/cart/controller/HomeControllerTest.java
+++ src/test/java/cart/controller/HomeControllerTest.java
@@ -1,17 +1,18 @@
 package cart.controller;
 
 import static io.restassured.RestAssured.given;
-import static org.junit.jupiter.api.Assertions.*;
 
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
+@AutoConfigureTestDatabase
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class HomeControllerTest {
 
