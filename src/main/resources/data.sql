6b63dda0f5e578a50d9ed95d873096494e9b1398
kdkdhoho <hkim4410@naver.com>
2023-05-01 16:58:12 +0900

    rename(data): test 패키지에서 main 패키지로 이동

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO MEMBER
+VALUES (1L, 'userA@woowahan.com', 'passwordB', 'userA', 'address1', 1);
+INSERT INTO MEMBER
+VALUES (2L, 'userB@woowahan.com', 'passwordB', 'userB', 'address2', 2);
--- src/test/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO MEMBER
-VALUES (1L, 'email1', 'password1', 'name1', 'address1', 1);
-INSERT INTO MEMBER
-VALUES (2L, 'email2', 'password2', 'name2', 'address2', 2);
