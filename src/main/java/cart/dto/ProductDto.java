5fc0befb4075c7783ffca92a6f63ed9658c30756
kokodak <kokodakadokok@gmail.com>
2023-05-01 21:13:55 +0900

    feat: 요청 종류에 따른 DTO 분리

--- src/main/java/cart/dto/ProductDto.java
+++ src/main/java/cart/dto/ProductDto.java
@@ -1,34 +1,19 @@
 package cart.dto;
 
 import cart.repository.entity.ProductEntity;
-import javax.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.Range;
 
 public class ProductDto {
 
     private Long id;
-
-    @NotBlank(message = "이름은 공백일 수 없습니다.")
     private String name;
-
-    @NotBlank(message = "이미지 경로는 공백일 수 없습니다.")
     private String imageUrl;
-
-    @Range(min = 0, max = 1_000_000_000 ,message = "가격은 10억 이하의 음이 아닌 정수여야 합니다.")
     private int price;
 
-    public ProductDto() {
-    }
-
-    public ProductDto(final Long id, final String name, final String imageUrl, final int price) {
+    private ProductDto(final Long id, final String name, final String imageUrl, final int price) {
         this.id = id;
         this.name = name;
         this.imageUrl = imageUrl;
         this.price = price;
-    }
-
-    public ProductDto(final String name, final String imageUrl, final int price) {
-        this(null, name, imageUrl, price);
     }
 
     public static ProductDto from(final ProductEntity productEntity) {
