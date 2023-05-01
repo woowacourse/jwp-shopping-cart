e7e9c3f53a937b6f4ae2fa8643b3fffb523bd8c6
dev_kong <ahdjd5@gmail.com>
2023-05-01 12:00:51 +0900

    feat: index 페이지 등록한 상품 노출하는 기능 구현

--- src/main/java/cart/controller/CartController.java
+++ src/main/java/cart/controller/CartController.java
@@ -1,14 +1,27 @@
 package cart.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import cart.dto.ProductResponse;
+import cart.service.ProductService;
 
 @Controller
 public class CartController {
-    @GetMapping("/")
-    public String home() {
-        return "index";
+
+    private final ProductService productService;
+
+    public CartController(ProductService productService) {
+        this.productService = productService;
     }
 
-
+    @GetMapping("/")
+    public String home(final Model model) {
+        final List<ProductResponse> products = productService.findAll();
+        model.addAttribute("products", products);
+        return "index";
+    }
 }
