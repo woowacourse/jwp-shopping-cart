484651733c63c500b576e2ab881599fec4038ef2
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-01 22:43:42 +0900

    test: 상품 인수 테스트 중복 정리

--- src/test/java/cart/integration/ProductIntegrationTest.java
+++ src/test/java/cart/integration/ProductIntegrationTest.java
@@ -1,55 +1,29 @@
-package cart;
+package cart.integration;
 
-import cart.repository.ProductRepository;
+import cart.config.IntegrationTestConfig;
+import cart.domain.product.Product;
+import cart.domain.product.ProductId;
+import cart.repository.product.ProductRepository;
 import cart.service.request.ProductCreateRequest;
 import cart.service.request.ProductUpdateRequest;
-import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.restdocs.RestDocumentationContextProvider;
-import org.springframework.restdocs.RestDocumentationExtension;
 
 import static io.restassured.RestAssured.given;
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.document;
-import static org.springframework.restdocs.restassured3.RestAssuredRestDocumentation.documentationConfiguration;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ExtendWith(RestDocumentationExtension.class)
-class ProductIntegrationTest {
-    static final String METHOD_NAME = "{method-name}";
+class ProductIntegrationTest extends IntegrationTestConfig {
+    private static final Product CHICKEN = new Product("치킨", 10000, "이미지-url");
 
     @Autowired
     ProductRepository repository;
 
-    @LocalServerPort
-    int port;
-
-    RequestSpecification spec;
-
-    @BeforeEach
-    void setUp(RestDocumentationContextProvider restDocs) {
-        RestAssured.port = this.port;
-        this.spec = new RequestSpecBuilder()
-                .addFilter(document("{method-name}",
-                        preprocessRequest(prettyPrint()),
-                        preprocessResponse(prettyPrint())))
-                .addFilter(documentationConfiguration(restDocs))
-                .build();
-    }
-
     @Test
     void getProducts() {
-        repository.save(new ProductCreateRequest("HYENA", 10000, "이미지-url"));
+        repository.save(CHICKEN);
 
         given(this.spec)
                 .log().all()
@@ -93,13 +67,13 @@ class ProductIntegrationTest {
 
     @Test
     void deleteProduct() {
-        final long saveId = repository.save(new ProductCreateRequest("KIARA", 10000, "이미지-url"));
+        final ProductId saveId = repository.save(CHICKEN);
 
         given(this.spec)
                 .log().all()
                 .filter(document(METHOD_NAME))
                 .contentType(APPLICATION_JSON_VALUE)
-                .pathParam("id", saveId)
+                .pathParam("id", saveId.getId())
 
         .when()
                 .delete("/products/{id}")
@@ -109,7 +83,7 @@ class ProductIntegrationTest {
 
     @Test
     void updateProduct() {
-        final long saveId = repository.save(new ProductCreateRequest("KIARA", 10000, "이미지-url"));
+        final ProductId saveId = repository.save(CHICKEN);
         final ProductUpdateRequest request = new ProductUpdateRequest("HYENA", 3000, "이미지2-url");
 
         given(this.spec)
@@ -128,7 +102,7 @@ class ProductIntegrationTest {
                         )
                 ))
                 .contentType(APPLICATION_JSON_VALUE)
-                .pathParam("id", saveId)
+                .pathParam("id", saveId.getId())
                 .body(request)
 
         .when()
