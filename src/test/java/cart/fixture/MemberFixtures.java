5d6878f8adeb46530b791271391e18bd37b05152
sh111-coder <ohk9134@naver.com>
2023-05-01 16:01:18 +0900

    test: 테스트에 사용할 더미 데이터 Fixture 생성

---
+++ src/test/java/cart/fixture/MemberFixtures.java
@@ -0,0 +1,32 @@
+package cart.fixture;
+
+import cart.dto.MemberRegisterRequest;
+import cart.entity.MemberEntity;
+
+public class MemberFixtures {
+
+    /**
+     * DUMMY_DATA
+     */
+    public static final String DUMMY_NICKNAME = "SeongHa";
+    public static final String DUMMY_EMAIL = "seongha@gmail.com";
+    public static final String DUMMY_PASSWORD = "1234";
+    public static final Long DUMMY_MEMBER_ID = 1L;
+    public static final Long DUMMY_CART_ID = 1L;
+
+    /**
+     * DUMMY_ENTITY
+     */
+    public static final MemberEntity INSERT_MEMBER_ENTITY =
+            new MemberEntity.Builder()
+                    .nickname(DUMMY_NICKNAME)
+                    .email(DUMMY_EMAIL)
+                    .password(DUMMY_PASSWORD)
+                    .build();
+
+    /**
+     * DUMMY_MEMBER_REGISTER_REQUEST
+     */
+    public static final MemberRegisterRequest MEMBER_REGISTER_REQUEST =
+            new MemberRegisterRequest(DUMMY_NICKNAME, DUMMY_EMAIL, DUMMY_PASSWORD);
+}
