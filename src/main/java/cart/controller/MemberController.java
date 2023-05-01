b9ad176cfd172abacabcba8af2b9f2d5ecf9c9ad
JunDonghyuk <ehdgur4814@naver.com>
2023-05-02 01:02:33 +0900

    feat: 사용자 api 컨트롤러 구현

---
+++ src/main/java/cart/controller/MemberController.java
@@ -0,0 +1,35 @@
+package cart.controller;
+
+import cart.dto.MemberDto;
+import cart.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.net.URI;
+
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(final MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/register")
+    ResponseEntity<Void> register(@Valid @RequestBody final MemberDto memberDto) {
+        final Long id = memberService.registerMember(memberDto);
+        return ResponseEntity.created(URI.create("/register/" + id)).build();
+    }
+
+    @PostMapping("/settings")
+    ResponseEntity<MemberDto> login(@Valid @RequestBody final MemberDto memberDto) {
+        final String email = memberDto.getEmail();
+        final String password = memberDto.getPassword();
+        MemberDto member = memberService.loginMember(email, password);
+        return ResponseEntity.ok().body(member);
+    }
+}
