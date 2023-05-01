3bccb2c3c6f9eeeeec3164b76ee3a2cb11b4682b
JunDonghyuk <ehdgur4814@naver.com>
2023-05-02 01:02:57 +0900

    feat: 간단한 시저 암호화, 복호화 기능구현

---
+++ src/main/java/cart/utils/CaesarCipher.java
@@ -0,0 +1,35 @@
+package cart.utils;
+
+public class CaesarCipher {
+
+    private static int KEY = 3;
+    private static int LENGTH = 26;
+
+    public static String encrypt(final String password) {
+        final char[] letters = password.toCharArray();
+        for (int position = 0; position < letters.length; position++) {
+            final char letter = letters[position];
+            if (letter >= 'a' && letter <= 'z') {
+                letters[position] = (char) ((letter - 'a' + KEY) % LENGTH + 'a');
+            }
+            if (letter >= 'A' && letter <= 'Z') {
+                letters[position] = (char) ((letter - 'A' + KEY) % LENGTH + 'A');
+            }
+        }
+        return String.valueOf(letters);
+    }
+
+    public static String decrypt(final String encryptedPassword) {
+        final char[] letters = encryptedPassword.toCharArray();
+        for (int position = 0; position < letters.length; position++) {
+            final char letter = letters[position];
+            if (letter >= 'a' && letter <= 'z') {
+                letters[position] = (char) ((letter - 'a' - KEY + LENGTH) % LENGTH + 'a');
+            }
+            if (letter >= 'A' && letter <= 'Z') {
+                letters[position] = (char) ((letter - 'A' - KEY + LENGTH) % LENGTH + 'A');
+            }
+        }
+        return String.valueOf(letters);
+    }
+}
