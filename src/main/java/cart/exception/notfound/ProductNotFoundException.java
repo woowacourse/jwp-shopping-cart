c21a4d3f6bf5562ae24ed140fe86a1fa5dcc04c5
seokhwan-an <asok369258@naver.com>
2023-05-01 15:25:47 +0900

    style: 사용하지 않은 import문 삭제

--- src/main/java/cart/exception/notfound/ProductNotFoundException.java
+++ src/main/java/cart/exception/notfound/ProductNotFoundException.java
@@ -1,7 +1,5 @@
 package cart.exception.notfound;
 
-import cart.exception.product.ProductException;
-
 public class ProductNotFoundException extends NotFoundException {
 
     private static final String NOT_FOUND_ERROR_MESSAGE = "존재하지 않는 상품입니다.";
