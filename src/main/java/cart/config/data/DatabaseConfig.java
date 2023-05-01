8c366b030b4705c177b1be9ab1942f0b6f53800a
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-01 22:44:33 +0900

    feat: 애플리케이션 실행시 기본 데이터 추가 기능

---
+++ src/main/java/cart/config/data/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package cart.config.data;
+
+import cart.domain.member.Member;
+import cart.domain.product.Product;
+import cart.repository.member.MemberRepository;
+import cart.repository.product.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig implements CommandLineRunner {
+    private final MemberRepository memberRepository;
+    private final ProductRepository productRepository;
+
+    public DatabaseConfig(final MemberRepository memberRepository, final ProductRepository productRepository) {
+        this.memberRepository = memberRepository;
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        memberRepository.save(new Member("헤나", "hyena@hyena.com", "hyena"));
+        memberRepository.save(new Member("토니", "tony@tony.com", "tony"));
+
+        productRepository.save(new Product("치킨", 20000, "https://pelicana.co.kr/resources/images/menu/original_menu10_210705.png"));
+    }
+}
