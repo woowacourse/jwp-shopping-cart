69438942096a77f93222d222961df07bff1bd6d7
dev_kong <ahdjd5@gmail.com>
2023-05-01 12:30:37 +0900

    fix: user -> users table명 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,12 +11,12 @@ INSERT INTO `product` (name, price, img_url)
 VALUES ('치킨', 10000,
         'https://i.namu.wiki/i/NH4o6m-2L377GTn-Nte1w5TX7h7vs9XdeIKyoKh9y72CNad59DFCCXN-sZkyRllbuM1Ahy74zj5Hayzecd59P8VwkO3cIwnKMAqPXYwKD3bq55Al09UwQ8d6MftSzV-CPgUuxBmxPLx95DRQJnQEZw.webp');
 
-CREATE TABLE IF NOT EXISTS `user`
+CREATE TABLE IF NOT EXISTS `users`
 (
     `id`       BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `email`    VARCHAR(20) NOT NULL,
     `password` VARCHAR(20) NOT NULL
 );
 
-INSERT INTO `user` (email, password)
+INSERT INTO `users` (email, password)
 VALUES ('ahdjd5@gmail.com', 'qwer1234');
