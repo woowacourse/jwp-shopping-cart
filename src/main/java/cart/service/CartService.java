605f6a3a588ddca8f027ceab586c7c96ad35ecab
Combi153 <jcm04@naver.com>
2023-05-01 21:21:47 +0900

    feat: CartService 기능 추가

---
+++ src/main/java/cart/service/CartService.java
@@ -0,0 +1,25 @@
+package cart.service;
+
+import cart.dao.CartDao;
+import cart.dao.entity.CartEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CartService {
+
+    private final CartDao cartDao;
+
+    public CartService(final CartDao cartDao) {
+        this.cartDao = cartDao;
+    }
+
+    public List<Long> findProductIdsByUserId(final Long userId) {
+        final List<CartEntity> cartEntities = cartDao.findAllByUserId(userId);
+        return cartEntities.stream()
+                .map(CartEntity::getProductId)
+                .collect(Collectors.toUnmodifiableList());
+    }
+}
