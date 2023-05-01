11f2259e11136e5b90489347a95026efa3b61972
hong-sile <gurwns9325@naver.com>
2023-05-01 17:54:37 +0900

    refactor: patchMapping -> putMapping으로 리팩터링

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -6,9 +6,9 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +29,7 @@ public class ProductController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @PatchMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<Void> editProduct(@RequestBody @Valid final ProductHttpRequest productHttpRequest) {
         productService.updateProduct(productHttpRequest);
         return ResponseEntity.ok().build();
