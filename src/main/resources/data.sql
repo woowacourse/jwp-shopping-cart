bc53942d4d10b703ab5d7e3910373abc82f8cb85
kdkdhoho <hkim4410@naver.com>
2023-05-01 17:08:42 +0900

    chore: 초기 상품 데이터 삽입문 및 DB 연결 정보 작성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 INSERT INTO MEMBER
-VALUES (1L, 'userA@woowahan.com', 'passwordB', 'userA', 'address1', 1);
+VALUES (1, 'userA@woowahan.com', 'passwordB', 'userA', 'address1', 1);
 INSERT INTO MEMBER
-VALUES (2L, 'userB@woowahan.com', 'passwordB', 'userB', 'address2', 2);
+VALUES (2, 'userB@woowahan.com', 'passwordB', 'userB', 'address2', 2);
+
+INSERT INTO PRODUCT VALUES (1, 'item1', 1000, 'https://picsum.photos/id/1/565/300');
+INSERT INTO PRODUCT VALUES (2, 'item2', 2000, 'https://picsum.photos/id/2/565/300');
