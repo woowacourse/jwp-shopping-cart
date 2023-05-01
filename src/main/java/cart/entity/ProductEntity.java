c85d06b8abeefa3c50478ea78ac6cdcec53d3ffe
yeonkkk <tjddus0302@gmail.com>
2023-05-01 12:58:06 +0900

    refactor: equals & hashCode 재정의 방법 변경

--- src/main/java/cart/entity/ProductEntity.java
+++ src/main/java/cart/entity/ProductEntity.java
@@ -1,32 +1,15 @@
 package cart.entity;
 
-import java.util.Objects;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 @Getter
 @Builder
+@EqualsAndHashCode
 public class ProductEntity {
     private final Long id;
     private final String name;
     private final int price;
     private final String imageUrl;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (!(o instanceof ProductEntity)) {
-            return false;
-        }
-        ProductEntity that = (ProductEntity) o;
-        return price == that.price && Objects.equals(id, that.id) && Objects.equals(name, that.name)
-                && Objects.equals(imageUrl, that.imageUrl);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, price, imageUrl);
-    }
 }
