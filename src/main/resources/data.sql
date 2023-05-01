6c1bc83919d477068566660c78dbb0532275f33b
dev_kong <ahdjd5@gmail.com>
2023-05-01 12:12:53 +0900

    feat: user table 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,5 +8,15 @@ CREATE TABLE IF NOT EXISTS `product`
 );
 
 INSERT INTO `product` (name, price, img_url)
-    VALUES ('치킨', 10000,
-           'https://i.namu.wiki/i/NH4o6m-2L377GTn-Nte1w5TX7h7vs9XdeIKyoKh9y72CNad59DFCCXN-sZkyRllbuM1Ahy74zj5Hayzecd59P8VwkO3cIwnKMAqPXYwKD3bq55Al09UwQ8d6MftSzV-CPgUuxBmxPLx95DRQJnQEZw.webp');
+VALUES ('치킨', 10000,
+        'https://i.namu.wiki/i/NH4o6m-2L377GTn-Nte1w5TX7h7vs9XdeIKyoKh9y72CNad59DFCCXN-sZkyRllbuM1Ahy74zj5Hayzecd59P8VwkO3cIwnKMAqPXYwKD3bq55Al09UwQ8d6MftSzV-CPgUuxBmxPLx95DRQJnQEZw.webp');
+
+CREATE TABLE IF NOT EXISTS `user`
+(
+    `id`       BIGINT      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email`    VARCHAR(20) NOT NULL,
+    `password` VARCHAR(20) NOT NULL
+);
+
+INSERT INTO `user` (email, password)
+VALUES ('ahdjd5@gmail.com', 'qwer1234');
