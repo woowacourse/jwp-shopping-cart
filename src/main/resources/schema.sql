9003bdeb6ce0286000f31a2d3f09942f367d886f
JunDonghyuk <ehdgur4814@naver.com>
2023-05-02 00:55:04 +0900

    feat: 데이터베이스 테이블 설계

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS PRODUCT (
+    id          BIGINT                            NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(50)                       NOT NULL,
+    image_url   VARCHAR(200)                      NOT NULL,
+    price       INT                               NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS MEMBER (
+    id           BIGINT                           NOT NULL AUTO_INCREMENT,
+    email        VARCHAR(50)                      NOT NULL,
+    name         VARCHAR(50)                      NOT NULL,
+    password     VARCHAR(100)                     NOT NULL,
+    PRIMARY KEY  (id),
+    UNIQUE KEY   member_email(email)
+);
+
+CREATE TABLE IF NOT EXISTS CART (
+    id           BIGINT                           NOT NULL AUTO_INCREMENT,
+    email        VARCHAR(50)                      NOT NULL,
+    product_id   BIGINT                           NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (product_id) REFERENCES PRODUCT (id)      ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (email) REFERENCES MEMBER (email)         ON DELETE CASCADE ON UPDATE CASCADE
+);
