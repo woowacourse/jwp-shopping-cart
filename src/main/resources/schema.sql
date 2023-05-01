8f3a96aeb2f7f3a26523deb9ba46bae9d3fd35cc
kdkdhoho <hkim4410@naver.com>
2023-05-01 15:15:01 +0900

    chore(schema): 2단계를 위한 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS CART;
+DROP TABLE IF EXISTS MEMBER;
 DROP TABLE IF EXISTS PRODUCT;
 
 CREATE TABLE PRODUCT
@@ -7,4 +9,24 @@ CREATE TABLE PRODUCT
     price     INT           NOT NULL,
     image_url VARCHAR(2083) NOT NULL,
     PRIMARY KEY (id)
+);
+
+CREATE TABLE MEMBER
+(
+    id       INT         NOT NULL AUTO_INCREMENT,
+    email    varchar(50) NOT NULL,
+    password varchar(30) NOT NULL,
+    name     varchar(10) NOT NULL,
+    address  varchar(50) NOT NULL,
+    age      int         NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE CART
+(
+    member_id  INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES MEMBER ON UPDATE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES PRODUCT ON UPDATE CASCADE,
+    PRIMARY KEY (member_id, product_id)
 );
