db860867138002109c32e59bac13c35987c80bca
JunDonghyuk <ehdgur4814@naver.com>
2023-05-02 01:02:21 +0900

    feat: 사용자 뷰 컨트롤러 구현

---
+++ src/main/java/cart/controller/MemberPageController.java
@@ -0,0 +1,31 @@
+package cart.controller;
+
+import cart.dto.MemberDto;
+import cart.service.MemberService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberPageController {
+
+    private final MemberService memberService;
+
+    public MemberPageController(final MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/register")
+    public String readRegisterPage() {
+        return "register";
+    }
+
+    @GetMapping("/settings")
+    public String readLoginPage(final Model model) {
+        List<MemberDto> members = memberService.findAllMember();
+        model.addAttribute("members", members);
+        return "settings";
+    }
+}
