43264d5f77ba4d669d6f5d85f8672c095fab9b42
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-01 22:37:18 +0900

    chore: 장바구니 테이블 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,23 +13,17 @@ CREATE TABLE IF NOT EXISTS members
     name     VARCHAR(30) NOT NULL,
     email    VARCHAR(30) NOT NULL,
     password VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    UNIQUE (email)
 );
 
 CREATE TABLE IF NOT EXISTS carts
 (
-    id        BIGINT NOT NULL AUTO_INCREMENT,
-    member_id BIGINT NOT NULL UNIQUE,
-    PRIMARY KEY (id),
-    FOREIGN KEY (member_id) REFERENCES members (id)
-);
-
-CREATE TABLE  IF NOT EXISTS cart_product
-(
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    cart_id BIGINT NOT NULL ,
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    member_id  BIGINT NOT NULL,
     product_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (cart_id) REFERENCES carts(id),
-    FOREIGN KEY (product_id) REFERENCES products(id)
+    FOREIGN KEY (member_id) REFERENCES members (id),
+    FOREIGN KEY (product_id) REFERENCES products (id),
+    UNIQUE (member_id, product_id)
 );
