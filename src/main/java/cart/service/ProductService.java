9ba8fbeab3867425e52b8f1c705cf257ce4d828b
songusika <sws981202@gmail.com>
2023-05-02 03:28:01 +0900

    refactor: 서비스 객체에 @Transactional 어노테이션 추가

--- src/main/java/cart/service/ProductService.java
+++ src/main/java/cart/service/ProductService.java
@@ -10,7 +10,9 @@ import cart.dto.ProductUpdateRequestDto;
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class ProductService {
 
