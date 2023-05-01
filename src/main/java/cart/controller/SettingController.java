7e97c009c606c7d1df0d1ded3974d90633edda22
dev_kong <ahdjd5@gmail.com>
2023-05-02 00:14:42 +0900

    feat: 등록된 회원을 화면에 보여주는 기능 구현

--- src/main/java/cart/controller/SettingController.java
+++ src/main/java/cart/controller/SettingController.java
@@ -1,15 +1,29 @@
 package cart.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import cart.dto.UserResponse;
+import cart.service.UserService;
 
 @Controller
 @RequestMapping("/settings")
 public class SettingController {
 
+    private final UserService userService;
+
+    public SettingController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping
-    public String setting() {
+    public String setting(final Model model) {
+        final List<UserResponse> users = userService.findAll();
+        model.addAttribute("members", users);
         return "settings";
     }
 }
