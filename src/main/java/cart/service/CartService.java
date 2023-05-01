bf328269655b38d12174643d6d29c28ac6d561cf
splitube <voicesplit@daum.net>
2023-05-02 01:15:19 +0900

    feat: CartService 객체 구현

---
+++ src/main/java/cart/service/CartService.java
@@ -0,0 +1,46 @@
+package cart.service;
+
+import cart.dao.CartDao;
+import cart.dao.ProductDao;
+import cart.entity.CartEntity;
+import cart.entity.product.ProductEntity;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class CartService {
+
+    private final CartDao cartDao;
+    private final ProductDao productDao;
+
+    public CartService(final CartDao cartDao, final ProductDao productDao) {
+        this.cartDao = cartDao;
+        this.productDao = productDao;
+    }
+
+    @Transactional
+    public Long add(final CartEntity cartEntity) {
+        return cartDao.save(cartEntity);
+    }
+
+    @Transactional
+    public void delete(final Long id) {
+        cartDao.delete(id);
+    }
+
+    public List<ProductEntity> findAllById(final Long customerId) {
+        final List<Long> productIds = cartDao.findAllProductIdsBy(customerId);
+        final List<Optional<ProductEntity>> products = productIds.stream()
+            .map(productDao::findById)
+            .collect(Collectors.toList());
+
+        return products.stream()
+            .map(product -> product.orElseThrow(
+                () -> new IllegalArgumentException("장바구니에 담긴 상품의 Id에 해당하는 상품이 존재하지 않습니다."))
+            ).collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/cart/service/CartServiceTest.java
@@ -0,0 +1,82 @@
+package cart.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import cart.dto.request.ProductRequestDto;
+import cart.entity.CartEntity;
+import cart.entity.customer.CustomerEntity;
+import cart.entity.product.ProductEntity;
+import java.util.List;
+import java.util.Objects;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class CartServiceTest {
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private ProductService productService;
+
+    @DisplayName("상품과 고객의 Id를 통해 카트에 상품을 추가한다.")
+    @Test
+    void add() {
+        //given
+        final Long registeredProductID = productService.register(
+            new ProductRequestDto(
+                "name",
+                "imageUrl",
+                1000,
+                "description",
+                List.of(1L)
+            )
+        );
+        final List<CustomerEntity> customers = customerService.findAll();
+        final Long firstCustomerId = customers.get(0).getId();
+
+        //when
+        cartService.add(new CartEntity(firstCustomerId, registeredProductID));
+
+        //then
+        final List<ProductEntity> products = cartService.findAllById(firstCustomerId);
+        assertAll(
+            () -> assertThat(products).hasSize(1),
+            () -> assertThat(Objects.requireNonNull(products).get(0).getId()).isEqualTo(registeredProductID)
+        );
+    }
+
+    @DisplayName("카트의 Id로 카트 데이터를 삭제한다.")
+    @Test
+    void delete() {
+        //given
+        final Long registeredProductID = productService.register(
+            new ProductRequestDto(
+                "name",
+                "imageUrl",
+                1000,
+                "description",
+                List.of(1L)
+            )
+        );
+        final List<CustomerEntity> customers = customerService.findAll();
+        final Long firstCustomerId = customers.get(0).getId();
+        final Long cartId = cartService.add(new CartEntity(firstCustomerId, registeredProductID));
+
+        //when
+        cartService.delete(cartId);
+
+        //then
+        final List<ProductEntity> products = cartService.findAllById(firstCustomerId);
+        assertThat(products).hasSize(0);
+    }
+}
