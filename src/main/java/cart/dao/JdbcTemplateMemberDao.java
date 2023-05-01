5e8ae65c152feebc6f41a559887f712c3230bc15
yujamint <woawoa0513@gmail.com>
2023-05-02 02:59:59 +0900

    feat: Member Dao 레벨 구현
    - MemberDao 인터페이스와 JdbcTemplate 구현체 구현
    - 전체 조회 기능 구현
    - MemberEntity 클래스 구현

---
+++ src/main/java/cart/dao/JdbcTemplateMemberDao.java
@@ -0,0 +1,34 @@
+package cart.dao;
+
+import cart.entity.MemberEntity;
+import java.util.List;
+import javax.sql.DataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JdbcTemplateMemberDao implements MemberDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplateMemberDao(final DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Override
+    public List<MemberEntity> selectAll() {
+        String sql = "select * from members";
+
+        return jdbcTemplate.query(sql, memberEntityRowMapper);
+    }
+
+    private final RowMapper<MemberEntity> memberEntityRowMapper = (resultSet, rowNumber) -> {
+        MemberEntity memberEntity = new MemberEntity(
+                resultSet.getInt("id"),
+                resultSet.getString("email"),
+                resultSet.getString("password")
+        );
+        return memberEntity;
+    };
+}
---
+++ src/main/java/cart/entity/MemberEntity.java
@@ -0,0 +1,26 @@
+package cart.entity;
+
+public class MemberEntity {
+
+    private final int id;
+    private final String email;
+    private final String password;
+
+    public MemberEntity(final int id, final String email, final String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
