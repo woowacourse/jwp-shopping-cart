4547b84e6c608d1ae0cad8e08ec621b0a13d10e0
0chil <0@chll.it>
2023-05-01 20:42:45 +0900

    refactor(ProductTestController): Id 가져오기, TestFixture 적용

--- src/test/java/cart/controller/ProductControllerTest.java
+++ src/test/java/cart/controller/ProductControllerTest.java
@@ -3,11 +3,14 @@ package cart.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import cart.TestFixture;
 import cart.controller.dto.ProductRequest;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -18,13 +21,16 @@ class ProductControllerTest {
     @LocalServerPort
     int port;
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
         RestAssured
                 .given()
                 .contentType(ContentType.JSON)
-                .body(new ProductRequest("비버", "a", 100L))
+                .body(new ProductRequest("비버", TestFixture.IMAGE_BEAVER, 100L))
                 .post("/products");
     }
 
@@ -34,7 +40,7 @@ class ProductControllerTest {
         RestAssured.given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(ContentType.JSON)
-                .body(new ProductRequest("비버", "a", 100L))
+                .body(new ProductRequest("비버", TestFixture.IMAGE_BEAVER, 100L))
                 .when().post("/products")
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
@@ -46,8 +52,8 @@ class ProductControllerTest {
         RestAssured.given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(ContentType.JSON)
-                .body(new ProductRequest("비버", "a", 100L))
-                .when().put("/products/1")
+                .body(new ProductRequest("비버", TestFixture.IMAGE_BEAVER, 100L))
+                .when().put("/products/" + getGreatestId())
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
     }
@@ -58,8 +64,12 @@ class ProductControllerTest {
         RestAssured.given().log().all()
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .contentType(ContentType.JSON)
-                .when().delete("/products/1")
+                .when().delete("/products/" + getGreatestId())
                 .then().log().all()
                 .statusCode(HttpStatus.OK.value());
+    }
+
+    private Integer getGreatestId() {
+        return jdbcTemplate.queryForObject("SELECT MAX(id) FROM product", Integer.class);
     }
 }
