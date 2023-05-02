b3d7e90673132430f435b520b41daa78327bb290
70825 <hello70825@gmail.com>
2023-05-02 15:28:10 +0900

    sql: 2단계 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,24 @@
+CREATE TABLE member
+(
+    id       BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email    VARCHAR(50) NOT NULL,
+    password VARCHAR(30) NOT NULL
+)ENGINE=InnoDB;
+
 CREATE TABLE product
 (
-    id        bigint       NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name      varchar(255) NOT NULL,
-    image_url text         NOT NULL,
-    price     int          NOT NULL
-);
+    id        BIGINT       NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `name`    VARCHAR(255) NOT NULL,
+    image_url TEXT         NOT NULL,
+    price     INT          NOT NULL
+)ENGINE=InnoDB;
+
+CREATE TABLE cart_product
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id),
+    UNIQUE (member_id, product_id);
+)ENGINE=InnoDB;
