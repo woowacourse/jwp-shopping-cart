d74a2bc4fb90d7852b61f72db3a4d8c6d29c67e4
JunDonghyuk <ehdgur4814@naver.com>
2023-05-02 16:21:25 +0900

    refactor: 모달 스타일 변경

--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,11 +1,3 @@
-const selectMember = (member) => {
-    <!-- TODO: [2단계] 사용자 정보에 맞게 변경 -->
-    const { email, password } = member;
-    const string = `${email}:${password}`;
-    localStorage.setItem('credentials', btoa(string));
-    alert(`${email} 사용자로 설정 했습니다.`);
-}
-
 const modal = document.getElementById('modal');
 
 const showAddModal = () => {
@@ -69,6 +61,7 @@ const createMember = (member) => {
         hideAddModal();
         localStorage.setItem('credentials', btoa(string));
         window.location.replace("http://localhost:8080/");
+        axios.headers.Authorization
     }).catch((error) => {
         alert(error.response.data.message);
         console.error(error);
