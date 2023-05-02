cf5f39b52d930f5ba6f762371c119e383f989ed8
jjongwa <troas96@naver.com>
2023-05-02 20:20:47 +0900

    refactor: 사용자 전체 조회 테스트 로직 변경

--- src/test/java/cart/dao/JdbcMemberDaoTest.java
+++ src/test/java/cart/dao/JdbcMemberDaoTest.java
@@ -1,13 +1,17 @@
 package cart.dao;
 
+import cart.entity.MemberEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.groups.Tuple.tuple;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
@@ -17,10 +21,19 @@ class JdbcMemberDaoTest {
     @Autowired
     private JdbcMemberDao jdbcCustomerDao;
 
-    @DisplayName("")
+    @DisplayName("selectAllUsers 성공 테스트")
     @Test
     void selectAllUsers() {
-        assertDoesNotThrow(() -> jdbcCustomerDao.selectAllMembers());
+        List<MemberEntity> memberEntities = jdbcCustomerDao.selectAllMembers();
+
+        assertAll(
+                () -> assertThat(memberEntities).hasSize(2),
+                () -> assertThat(memberEntities).extracting("email", "password")
+                        .contains(
+                                tuple("dino96@naver.com", "jjongwa96"),
+                                tuple("jeomxon@gmail.com", "jeomxon00")
+                        )
+        );
     }
 
 }
