cc02e63b40c3df9ce4cad790e256b6a17271d10b
dooboocookie <bkh3030@gmail.com>
2023-05-02 17:27:43 +0900

    feat: user 관련 도메인(엔티티) 추가

---
+++ src/main/java/cart/domain/User.java
@@ -0,0 +1,50 @@
+package cart.domain;
+
+import java.util.Objects;
+
+public class User {
+
+    private final Long id;
+    private final Email email;
+    private final Password password;
+
+    public User(final Long id, final Email email, final Password password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email.getValue();
+    }
+
+    public String getPassword() {
+        return password.getValue();
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email=" + email +
+                ", password=" + password +
+                '}';
+    }
+}
