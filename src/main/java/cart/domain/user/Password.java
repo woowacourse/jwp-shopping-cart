d549c7d0d745164fd6210985c7945312bef6dbb2
70825 <hello70825@gmail.com>
2023-05-02 15:22:15 +0900

    feat: 비밀번호 객체 추가

---
+++ src/main/java/cart/domain/user/Password.java
@@ -0,0 +1,24 @@
+package cart.domain.user;
+
+import org.springframework.util.StringUtils;
+
+public class Password {
+
+    private final String password;
+
+    public Password(final String password) {
+        validate(password);
+
+        this.password = password;
+    }
+
+    private void validate(final String password) {
+        if (!StringUtils.hasText(password)) {
+            throw new IllegalArgumentException("비밀번호가 존재하지 않습니다");
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/test/java/cart/domain/user/PasswordTest.java
@@ -0,0 +1,29 @@
+package cart.domain.user;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class PasswordTest {
+
+    @Test
+    @DisplayName("비밀번호를 정상적으로 등록할 수 있다")
+    void passwordTest() {
+        final String input = "test";
+
+        assertDoesNotThrow(() -> new Password(input));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("비밀번호가 존재하지 않으면 예외가 발생한다")
+    void throwExceptionWhenPasswordNotExist(final String input) {
+        assertThatThrownBy(() -> new Password(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("비밀번호가 존재하지 않습니다");
+    }
+}
