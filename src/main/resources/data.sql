da04548e059e83eb18bfcbc48975d26397f7f7a6
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-05-02 16:10:49 +0900

    chore: 데이터베이스 설계

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,21 @@
-CREATE TABLE IF NOT EXISTS item (
-    id       bigint      PRIMARY KEY    AUTO_INCREMENT    NOT NULL,
-    name     varchar(30) NOT NULL,
-    item_url text        NOT NULL,
-    price    int         NOT NULL
+CREATE TABLE IF NOT EXISTS item
+(
+    id       bigint PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name     varchar(30)        NOT NULL,
+    item_url text               NOT NULL,
+    price    int                NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS member
+(
+    id       BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    email    VARCHAR(320)       NOT NULL,
+    password VARCHAR(20)        NOT NULL
+    );
+
+CREATE TABLE IF NOT EXISTS basket
+(
+    id      BIGINT NOT NULL,
+    user_id BIGINT NOT NULL,
+    item_id BIGINT NOT NULL
 );
