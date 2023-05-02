e55e052346d0e82f5fbbf28758eeab726939bff3
2014012606 <2014012606@hanyang.ac.kr>
2023-05-02 17:26:10 +0900

    refactor: 기본 예외의 상태 코드와 메시지를 활용

--- src/main/java/cart/exception/ApiExceptionHandler.java
+++ src/main/java/cart/exception/ApiExceptionHandler.java
@@ -2,14 +2,10 @@ package cart.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -21,25 +17,9 @@ public class ApiExceptionHandler {
         return ResponseEntity.badRequest().body(e.getMessage());
     }
 
-    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<Map<String,String>> handleHttpRequestNotSupported(HttpRequestMethodNotSupportedException e) {
-        Map<String, String> message = new HashMap<>();
-        message.put("method", e.getMethod());
-        message.put("message", e.getMessage());
-        return ResponseEntity.status(405).body(message);
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public ResponseEntity<Map<String, String>> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
-        Map<String, String> message = new HashMap<>();
-        message.put("method", e.getErrorCode());
-        message.put("message", e.getMessage());
-        return ResponseEntity.badRequest().body(message);
-    }
-
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception e) {
-        logger.error("예기치 못한 오류 발생: " + e);
-        return ResponseEntity.badRequest().body(e.getMessage());
+    public ResponseEntity<String> handleException(Exception e, HttpStatus httpStatus) {
+        logger.error("예기치 못한 오류: " + e);
+        return ResponseEntity.status(httpStatus).body(e.getMessage());
     }
 }
