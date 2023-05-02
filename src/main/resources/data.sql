e988fd5346cec1e6ecc117853262642209429645
70825 <hello70825@gmail.com>
2023-05-02 19:02:53 +0900

    feat: 기본 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -22,3 +22,8 @@ CREATE TABLE cart_product
     FOREIGN KEY (product_id) REFERENCES product (id),
     UNIQUE (member_id, product_id)
 );
+
+INSERT INTO product(name, image_url, price) VALUES ('mouse', 'https://cdn.polinews.co.kr/news/photo/201910/427334_3.jpg', 100000);
+INSERT INTO product(name, image_url, price) VALUES ('keyboard', 'https://i1.wp.com/blog.peoplefund.co.kr/wp-content/uploads/2020/01/진혁.jpg?fit=770%2C418&ssl=1', 250000);
+INSERT INTO member(email, password) VALUES ('test@test.com', 'test');
+INSERT INTO member(email, password) VALUES ('woowacourse@woowa.com', 'pobi');
