449bc506abc474e2836a7b05729991d13131c65e
hectick <ch.yeondev@gmail.com>
2023-05-02 15:54:13 +0900

    feat: DB 에 더미 데이터를 넣는 기능 구현

---
+++ src/main/java/cart/dao/MemberInitializer.java
@@ -0,0 +1,28 @@
+package cart.dao;
+
+import cart.entity.MemberEntity;
+import cart.entity.ProductEntity;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Profile("!test")
+@Component
+public class MemberInitializer implements ApplicationRunner {
+
+    private final MemberDao memberDao;
+
+    public MemberInitializer(final MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Override
+    public void run(final ApplicationArguments args) throws Exception {
+        final MemberEntity memberEntity1 = MemberEntity.of("irene@email.com", "password1");
+        final MemberEntity memberEntity2 = MemberEntity.of("abcd@email.com", "password2");
+
+        memberDao.insert(memberEntity1);
+        memberDao.insert(memberEntity2);
+    }
+}
