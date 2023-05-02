297d97cabee41a46c2d89f7dbf5cdb3c8716522c
parkmuhyeun <pjhg410@gmail.com>
2023-05-03 00:56:40 +0900

    feat: 사용자 인증 기능 추가

---
+++ src/main/java/cart/argumnetresolver/LoginArgumentResolver.java
@@ -0,0 +1,43 @@
+package cart.argumnetresolver;
+
+import cart.dao.MemberDao;
+import cart.entity.MemberEntity;
+import cart.exception.AuthenticationException;
+import java.util.Base64;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
+
+  private final MemberDao memberDao;
+
+  public LoginArgumentResolver(MemberDao memberDao) {
+    this.memberDao = memberDao;
+  }
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.hasParameterAnnotation(Login.class);
+  }
+
+  @Override
+  public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+      NativeWebRequest webRequest, WebDataBinderFactory binderFactory)  {
+    final HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+    final String header = request.getHeader("Authorization");
+    final String authorization = header.split(" ")[1];
+    final String emailAndPassword = new String(Base64.getDecoder().decode(authorization));
+    final String[] splitEmailAndPassword = emailAndPassword.split(":");
+    final MemberEntity memberEntity = new MemberEntity(splitEmailAndPassword[0], splitEmailAndPassword[1]);
+    final MemberEntity findEntity = memberDao.findByMemberEntity(memberEntity)
+        .orElseThrow(() -> new AuthenticationException("올바른 인증정보를 입력해주세요."));
+
+    return findEntity.getId();
+  }
+}
---
+++ src/main/java/cart/config/WebConfig.java
@@ -0,0 +1,22 @@
+package cart.config;
+
+import cart.argumnetresolver.LoginArgumentResolver;
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  private final LoginArgumentResolver loginArgumentResolver;
+
+  public WebConfig(LoginArgumentResolver loginArgumentResolver) {
+    this.loginArgumentResolver = loginArgumentResolver;
+  }
+
+  @Override
+  public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+    resolvers.add(loginArgumentResolver);
+  }
+}
