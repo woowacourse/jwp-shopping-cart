509c06ff0b5fd53659a1173d6921749b568dcce6
the9kim <the9rla@gmail.com>
2023-05-02 19:43:09 +0900

    test: 등록 제품 조회 관련 통합 테스트

--- src/test/java/cart/ProductIntegrationTest.java
+++ src/test/java/cart/ProductIntegrationTest.java
@@ -1,12 +1,18 @@
 package cart;
 
+import cart.dao.ProductEntity;
+import cart.dto.ProductDto;
+import cart.service.ProductService;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,12 +28,22 @@ public class ProductIntegrationTest {
         RestAssured.port = port;
     }
 
+    @Autowired
+    ProductService productService;
+
     @Test
     public void getProducts() {
+
+        productService.insert(new ProductDto("pizza", "https://www.hmj2k.com/data/photos/20210936/art_16311398425635_31fd17.jpg", 1000));
+        productService.insert(new ProductDto("chicken", "https://www.hmj2k.com/data/photos/20210936/art_16311398425635_31fd17.jpg", 2000));
+
+        List<ProductEntity> products = productService.findAll();
+
         var result = given()
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(products)
                 .when()
-                .get("/products")
+                .get("/")
                 .then()
                 .extract();
 
