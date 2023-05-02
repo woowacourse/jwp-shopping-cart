5368ce052cc1adad5ea0f27d895b13e3918d64ee
hectick <ch.yeondev@gmail.com>
2023-05-02 16:24:24 +0900

    feat: 설정 페이지에서 사용자 전체를 조회하는 기능 구현

--- src/main/java/cart/controller/HomeController.java
+++ src/main/java/cart/controller/HomeController.java
@@ -1,6 +1,8 @@
 package cart.controller;
 
+import cart.dto.MemberResponse;
 import cart.mapper.ProductResponseMapper;
+import cart.service.MemberManagementService;
 import cart.service.ProductManagementService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +11,25 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    private final ProductManagementService managementService;
+    private final ProductManagementService productManagementService;
+    private final MemberManagementService memberManagementService;
 
-    public HomeController(final ProductManagementService managementService) {
-        this.managementService = managementService;
+    public HomeController(final ProductManagementService productManagementService, final MemberManagementService memberManagementService) {
+        this.productManagementService = productManagementService;
+        this.memberManagementService = memberManagementService;
     }
 
     @GetMapping("/")
     public ModelAndView home(ModelAndView modelAndView) {
-        modelAndView.addObject("products", ProductResponseMapper.from(managementService.findAll()));
+        modelAndView.addObject("products", ProductResponseMapper.from(productManagementService.findAll()));
         modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+    @GetMapping("/settings")
+    public ModelAndView settings(ModelAndView modelAndView) {
+        modelAndView.addObject("members", MemberResponse.from(memberManagementService.findAll()));
+        modelAndView.setViewName("settings");
         return modelAndView;
     }
 }
--- src/main/java/cart/dto/MemberResponse.java
+++ src/main/java/cart/dto/MemberResponse.java
@@ -1,25 +1,27 @@
 package cart.dto;
 
-import cart.entity.MemberEntity;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class MemberDto {
+public class MemberResponse {
 
     private final Long id;
     private final String email;
     private final String password;
 
-    private MemberDto(final Long id, final String email, final String password) {
+    public MemberResponse(final Long id, final String email, final String password) {
         this.id = id;
         this.email = email;
         this.password = password;
     }
 
-    public static List<MemberDto> from(final List<MemberEntity> memberEntities){
-        return memberEntities.stream()
-                .map(memberEntity-> new MemberDto(memberEntity.getId(), memberEntity.getEmail(), memberEntity.getPassword()))
+    public static MemberResponse from(final MemberDto memberDto) {
+        return new MemberResponse(memberDto.getId(), memberDto.getEmail(), memberDto.getPassword());
+    }
+
+    public static List<MemberResponse> from(final List<MemberDto> memberDtos) {
+        return memberDtos.stream()
+                .map(MemberResponse::from)
                 .collect(Collectors.toUnmodifiableList());
     }
 
