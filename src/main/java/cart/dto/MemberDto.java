db7e2c496c5e51593ba1d450f4d6927c6dc2b3db
hectick <ch.yeondev@gmail.com>
2023-05-02 16:08:36 +0900

    feat: 사용자 전체를 조회하는 기능 구현

---
+++ src/main/java/cart/dto/MemberDto.java
@@ -0,0 +1,37 @@
+package cart.dto;
+
+import cart.entity.MemberEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MemberDto {
+
+    private final Long id;
+    private final String email;
+    private final String password;
+
+    private MemberDto(final Long id, final String email, final String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public static List<MemberDto> from(List<MemberEntity> memberEntities){
+        return memberEntities.stream()
+                .map(memberEntity-> new MemberDto(memberEntity.getId(), memberEntity.getEmail(), memberEntity.getEmail()))
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/main/java/cart/service/MemberManagementService.java
@@ -0,0 +1,24 @@
+package cart.service;
+
+import cart.dao.MemberDao;
+import cart.dto.MemberDto;
+import cart.entity.MemberEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MemberManagementService {
+
+    private final MemberDao memberDao;
+
+    public MemberManagementService(final MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    public List<MemberDto> findAll() {
+        final List<MemberEntity> memberEntities = memberDao.selectAll();
+        return MemberDto.from(memberEntities);
+    }
+
+}
