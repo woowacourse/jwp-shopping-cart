0c9fe79e4e85b453d20dbaee16b629026fb9afe5
jeonghoon <shb03323@naver.com>
2023-05-02 21:42:18 +0900

    refactor: data.sql에 유저 추가 쿼리 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,3 +20,8 @@ CREATE TABLE IF NOT EXISTS cart (
     product_id  INT       NOT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO users(email, password, name) VALUES ('ditoo@wooteco.com', 'ditoo1234', '디투'),
+                         ('barrel@wooteco.com', 'barrel1234', '배럴'),
+                         ('brown@wooteco.com', 'brown1234', '브라운');
+INSERT INTO users(email, password) VALUES ('anonymous@wooteco.com', '12345678');
