4e8535973d44018feb589f61cc181ae4409e2e20
woosung1223 <dntjd991223@naver.com>
2023-05-02 16:02:04 +0900

    feat: MemberRepository 구현체 생성

---
+++ src/main/java/cart/persistence/H2MemberRepository.java
@@ -0,0 +1,35 @@
+package cart.persistence;
+
+import cart.business.MemberRepository;
+import cart.business.domain.member.Member;
+import cart.business.domain.member.MemberEmail;
+import cart.business.domain.member.MemberPassword;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class H2MemberRepository implements MemberRepository {
+
+    private final RowMapper<Member> memberRowMapper = (resultSet, rowNum) -> {
+        Integer id = resultSet.getInt("id");
+        String email = resultSet.getString("email");
+        String password = resultSet.getString("password");
+
+        return new Member(id, new MemberEmail(email), new MemberPassword(password));
+    };
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public H2MemberRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Member> findAll() {
+        final String sql = "SELECT * FROM MEMBER";
+        return jdbcTemplate.query(sql, memberRowMapper);
+    }
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,17 @@
 CREATE TABLE PRODUCT (
-    id      INT             NOT NULL AUTO_INCREMENT,
-    name    VARCHAR(10)     NOT NULL,
-    url     VARCHAR(255)    NOT NULL,
-    price   INT             NOT NULL,
+    id       INT             NOT NULL AUTO_INCREMENT,
+    name     VARCHAR(10)     NOT NULL,
+    url      VARCHAR(255)    NOT NULL,
+    price    INT             NOT NULL,
+    PRIMARY  KEY (id)
+);
+
+CREATE TABLE MEMBER (
+    id       INT             NOT NULL AUTO_INCREMENT,
+    email    VARCHAR(100)    NOT NULL,
+    password VARCHAR(10)     NOT NULL,
     PRIMARY KEY (id)
 );
+
+INSERT INTO MEMBER VALUES (1, 'a@a.com', 'password1');
+INSERT INTO MEMBER VALUES (2, 'b@b.com', 'password2');
