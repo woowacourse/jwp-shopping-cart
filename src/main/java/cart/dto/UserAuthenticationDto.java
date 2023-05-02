6b6432df2201471ae8d4067ed2d34d530a6ac09f
jeonghoon <shb03323@naver.com>
2023-05-02 21:41:51 +0900

    refactor: user 인증 dto validation 제거

--- src/main/java/cart/dto/UserAuthenticationDto.java
+++ src/main/java/cart/dto/UserAuthenticationDto.java
@@ -1,16 +1,8 @@
 package cart.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
-
 public class UserAuthenticationDto {
 
-    @NotNull(message = "email이 null이면 안됩니다.")
-    @Email(message = "이메일 형식이 아닙니다.")
     private final String email;
-    @NotNull(message = "password가 null이면 안됩니다.")
-    @Min(value = 8, message = "비밀번호는 8자 이상이어야 합니다.")
     private final String password;
 
     public UserAuthenticationDto(final String email, final String password) {
