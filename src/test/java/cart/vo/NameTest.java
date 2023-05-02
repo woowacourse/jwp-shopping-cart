5c4b943da6d05afd833f6fd14cc356e8105a8d89
jaeyeon kim <jakind@naver.com>
2023-05-02 21:09:18 +0900

    feat : 원시값 포장 Name 객체 구현

---
+++ src/test/java/cart/vo/NameTest.java
@@ -0,0 +1,39 @@
+package cart.vo;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static cart.vo.Name.of;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NameTest {
+
+    @Test
+    @DisplayName("올바르지 않은 길이의 이름이 들어갔을 때 예외 발생")
+    void createNameFail() {
+        String length0 = "";
+        String length101 = "a".repeat(101);
+
+        assertThatThrownBy(() -> of(length0))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("올바르지 않은 이름입니다.");
+        assertThatThrownBy(() -> of(length101))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("올바르지 않은 이름입니다.");
+    }
+
+    @Test
+    @DisplayName("올바른 길이의 이름이 들어갔을 때에는 예외가 발생하지 않는다.")
+    void createNameSuccess() {
+        String length1 = "a";
+        String length100 = "a".repeat(100);
+
+        Name nameOfLength1 = Name.of(length1);
+        Name nameOfLength100 = of(length100);
+
+        assertThat(nameOfLength1.getValue()).isEqualTo(length1);
+        assertThat(nameOfLength100.getValue()).isEqualTo(length100);
+    }
+}
