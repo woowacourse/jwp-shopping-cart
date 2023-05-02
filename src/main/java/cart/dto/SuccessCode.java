2c6f9c849eeb5c9c2a3826666bdfeb98de9d8a25
thdwoqor <celebarte01@gmail.com>
2023-05-02 17:38:04 +0900

    refactor : 상품 삭제 문구 변경

--- src/main/java/cart/dto/SuccessCode.java
+++ src/main/java/cart/dto/SuccessCode.java
@@ -4,12 +4,12 @@ public enum SuccessCode {
 
     CREATE_ITEM(201, "상품 추가 성공"),
     UPDATE_ITEM(200, "상품 업데이트 성공"),
-    DELETE_ITEM(200, "상품 업데이트 성공"),
+    DELETE_ITEM(200, "상품 삭제 성공"),
     CREATE_MEMBER(201, "유저 추가 성공"),
     UPDATE_MEMBER(200, "유저 업데이트 성공"),
-    DELETE_MEMBER(200, "유저 업데이트 성공"),
+    DELETE_MEMBER(200, "유저 삭제 성공"),
     CREATE_CART(201, "장바구니 추가 성공"),
-    DELETE_CART(200, "장바구니 업데이트 성공");
+    DELETE_CART(200, "장바구니 삭제 성공");
 
     private final int status;
     private final String message;
