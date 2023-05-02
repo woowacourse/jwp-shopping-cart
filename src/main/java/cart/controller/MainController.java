5e3e3e05ae8dd2256b849f7c9f05e2413cb5a300
parkmuhyeun <pjhg410@gmail.com>
2023-05-02 21:29:24 +0900

    feat: 사용자 설정 페이지 연동 기능 구현

--- src/main/java/cart/controller/MainController.java
+++ src/main/java/cart/controller/MainController.java
@@ -1,5 +1,6 @@
 package cart.controller;
 
+import cart.service.MemberService;
 import cart.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
     private final ProductService productService;
+    private final MemberService memberService;
 
-    public MainController(final ProductService productService) {
+    public MainController(final ProductService productService, MemberService memberService) {
         this.productService = productService;
+        this.memberService = memberService;
     }
 
     @GetMapping("/")
@@ -24,5 +27,11 @@ public class MainController {
     public String showAdmin(final Model model) {
         model.addAttribute("products", productService.findAll());
         return "admin";
+    }
+
+    @GetMapping("/settings")
+    public String showSettings(final Model model) {
+        model.addAttribute("members", memberService.findAll());
+        return "settings";
     }
 }
