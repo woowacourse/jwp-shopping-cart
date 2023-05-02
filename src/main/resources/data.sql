8fa604026a0b26ab24324f1539a3aecf3086ba51
mcodnjs <codnjs8989@gmail.com>
2023-05-02 22:19:34 +0900

    feat: user table 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,11 @@ CREATE TABLE product
     price     INT          NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE users
+(
+    id       BIGINT       NOT NULL AUTO_INCREMENT,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
