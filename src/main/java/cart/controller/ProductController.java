04225709926a73aa716231aa1d5d3893a492b7dd
parkmuhyeun <pjhg410@gmail.com>
2023-05-02 14:16:40 +0900

    refactor: 컨트롤러 반환값 수정

--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -31,13 +31,13 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<String> updateProduct(@PathVariable final long id, @Valid @RequestBody final ProductRequest productRequest) {
+    public ResponseEntity<Void> updateProduct(@PathVariable final long id, @Valid @RequestBody final ProductRequest productRequest) {
         productService.updateProduct(id, productRequest);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteProduct(@PathVariable final long id) {
+    public ResponseEntity<Void> deleteProduct(@PathVariable final long id) {
         productService.deleteProduct(id);
         return ResponseEntity.ok().build();
     }
