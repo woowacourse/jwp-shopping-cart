8756a14313c594dee444b09d6828f786e31e2ea3
parkmuhyeun <pjhg410@gmail.com>
2023-05-02 21:57:02 +0900

    feat: Member id 추가

--- src/main/java/cart/entity/MemberEntity.java
+++ src/main/java/cart/entity/MemberEntity.java
@@ -1,13 +1,22 @@
 package cart.entity;
 
+import java.util.Objects;
+
 public class MemberEntity {
 
+  private final Long id;
   private final String email;
   private final String password;
 
-  public MemberEntity(String email, String password) {
+  public MemberEntity(Long id, String email, String password) {
+    this.id = id;
     this.email = email;
     this.password = password;
+  }
+
+  public Long getId() {
+    Objects.requireNonNull(id, "id가 null입니다.");
+    return id;
   }
 
   public String getEmail() {
