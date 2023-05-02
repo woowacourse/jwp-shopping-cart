47c87ee9a56b226cea3685a5c6402c685fb47b0d
odo27 <odo27@naver.com>
2023-05-02 17:23:56 +0900

    feat: CartSearchController 구현

--- src/main/java/cart/controller/CartSearchController.java
+++ src/main/java/cart/controller/CartSearchController.java
@@ -1,8 +1,15 @@
 package cart.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import cart.domain.product.Product;
 import cart.dto.ProductResponse;
+import cart.dto.UserResponse;
+import cart.infrastructure.BasicAuthorizationExtractor;
+import cart.service.AuthService;
+import cart.service.CartSearchService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +17,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CartSearchController {
 
+    private final BasicAuthorizationExtractor basicAuthorizationExtractor = new BasicAuthorizationExtractor();
+    private final AuthService authService;
+    private final CartSearchService cartSearchService;
+
+    public CartSearchController(final AuthService authService, final CartSearchService cartSearchService) {
+        this.authService = authService;
+        this.cartSearchService = cartSearchService;
+    }
+
     @GetMapping("/carts")
-    public ResponseEntity<List<ProductResponse>> getCarts() {
-        final List<ProductResponse> productResponses = List.of(
-                new ProductResponse(1L, "odo", "url", 1),
-                new ProductResponse(2L, "nunu", "url", 2)
-        );
-        return ResponseEntity.ok(productResponses);
+    public ResponseEntity<List<ProductResponse>> getCarts(final HttpServletRequest request) {
+        final UserResponse extract = basicAuthorizationExtractor.extract(request);
+        final String email = extract.getEmail();
+        final String password = extract.getPassword();
+        if (authService.isValidLogin(email, password)) {
+            final List<Product> products = cartSearchService.findByEmail(email);
+            final List<ProductResponse> productResponses = products.stream()
+                    .map(ProductResponse::from)
+                    .collect(Collectors.toList());
+            return ResponseEntity.ok(productResponses);
+        }
+        throw new LoginFailException();
     }
 }
---
+++ src/main/java/cart/service/AuthService.java
@@ -0,0 +1,16 @@
+package cart.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public AuthService() {
+    }
+
+    public boolean isValidLogin(final String email, final String password) {
+        final String validEmail = "a@a.com";
+        final String validPassword = "password1";
+        return validEmail.equals(email) && validPassword.equals(password);
+    }
+}
