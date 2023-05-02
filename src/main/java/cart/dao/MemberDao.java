5d44dfa5397369eeeb7f033ec178de663d12db97
kdkdhoho <hkim4410@naver.com>
2023-05-02 16:03:42 +0900

    feat(MemberDao): 인증 정보로 회원을 찾는 기능 구현

--- src/main/java/cart/dao/MemberDao.java
+++ src/main/java/cart/dao/MemberDao.java
@@ -1,15 +1,20 @@
 package cart.dao;
 
+import cart.dto.AuthInfo;
 import cart.entity.MemberEntity;
 import lombok.AllArgsConstructor;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @AllArgsConstructor
 public class MemberDao {
+
+    private final static String ALL_COLUMNS = "id, email, password, name, address, age";
 
     private static final RowMapper<MemberEntity> memberRowMapper = (resultSet, rowNum) -> new MemberEntity(
             resultSet.getInt("id"),
@@ -23,8 +28,22 @@ public class MemberDao {
     private final JdbcTemplate jdbcTemplate;
 
     public List<MemberEntity> findAll() {
-        String sql = "select id, email, password, name, address, age from Member";
+        String sql = "select " + ALL_COLUMNS + " from Member";
 
         return jdbcTemplate.query(sql, memberRowMapper);
+    }
+
+    public Optional<MemberEntity> findByAuthInfo(final AuthInfo authInfo) {
+        final String email = authInfo.getEmail();
+        final String password = authInfo.getPassword();
+
+        String sql = "select " + ALL_COLUMNS + " from Member where email = ? and password = ?";
+
+        try {
+            final MemberEntity foundMember = jdbcTemplate.queryForObject(sql, memberRowMapper, email, password);
+            return Optional.ofNullable(foundMember);
+        } catch (final DataAccessException e) {
+            return Optional.empty();
+        }
     }
 }
--- src/test/java/cart/dao/MemberDaoTest.java
+++ src/test/java/cart/dao/MemberDaoTest.java
@@ -3,6 +3,7 @@ package cart.dao;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import cart.dto.AuthInfo;
 import cart.entity.MemberEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.jdbc.Sql;
+import java.util.Optional;
 
 @JdbcTest
 @Sql({"/schema.sql", "/data.sql"})
@@ -32,6 +34,29 @@ class MemberDaoTest {
         assertAll(
                 () -> assertThat(members.size()).isEqualTo(2),
                 () -> assertThat(members.get(0)).isInstanceOf(MemberEntity.class)
+        );
+    }
+
+    @Test
+    void AuthInfo로_존재하지_않는_회원을_찾는_경우_Optional_empty가_반환된다() {
+        final AuthInfo emptyAuthInfo = new AuthInfo("emptyEmail@email.com", "emptyPassword");
+
+        final Optional<MemberEntity> result = memberDao.findByAuthInfo(emptyAuthInfo);
+
+        assertThat(result.isEmpty()).isTrue();
+    }
+
+    @Test
+    void AuthInfo로_존재하지_않는_회원을_찾는_경우_Optional에_감싸져서_반환된다() {
+        final AuthInfo userAInfo = new AuthInfo("userA@woowahan.com", "passwordA");
+
+        final Optional<MemberEntity> result = memberDao.findByAuthInfo(userAInfo);
+        assertThat(result.isPresent()).isTrue();
+
+        final MemberEntity member = result.get();
+        assertAll(
+                () -> assertThat(member.getId()).isEqualTo(1),
+                () -> assertThat(member.getClass()).isEqualTo(MemberEntity.class)
         );
     }
 }
