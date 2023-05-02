010b8d8c8495e4b066d8ba2239d64cb55b7d6a7d
Miseong Kim <miseongk@naver.com>
2023-05-02 20:04:04 +0900

    feat: Basic 인증 추가

---
+++ src/main/java/cart/dto/AuthInfo.java
@@ -0,0 +1,21 @@
+package cart.dto;
+
+public class AuthInfo {
+
+    private final String email;
+    private final String password;
+
+    public AuthInfo(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
---
+++ src/main/java/cart/infrastructure/AuthorizationExtractor.java
@@ -0,0 +1,10 @@
+package cart.infrastructure;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface AuthorizationExtractor<T> {
+
+    String AUTHORIZATION = "Authorization";
+
+    T extract(HttpServletRequest request);
+}
