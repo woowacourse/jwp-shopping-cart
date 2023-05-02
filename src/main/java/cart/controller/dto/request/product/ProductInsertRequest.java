4215448b02998ed5516dba4084ddbd78c4cd5306
wugawuga <dnr3655@naver.com>
2023-05-02 15:24:37 +0900

    feat: @URL 추가 url 아닐 시 예외발생

--- src/main/java/cart/controller/dto/request/product/ProductInsertRequest.java
+++ src/main/java/cart/controller/dto/request/product/ProductInsertRequest.java
@@ -5,13 +5,14 @@ import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.URL;
 
 public class ProductInsertRequest {
 
     @NotBlank(message = "이름을 다시 입력해주세요.")
     private final String name;
 
-    @NotBlank(message = "이미지 주소를 다시 입력해주세요.")
+    @URL(message = "이미지 주소를 다시 입력해주세요.")
     private final String image;
 
     @NotNull(message = "가격은 최소 100원, 최대 10000000원입니다.")
