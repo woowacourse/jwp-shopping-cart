8d688f636dd4cb5e8d620fe88974af550176ac68
jaeyeon kim <jakind@naver.com>
2023-05-02 17:17:30 +0900

    refactor : data.sql 을 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,2 @@
-CREATE TABLE Product (
-     id INT NOT NULL AUTO_INCREMENT,
-     name VARCHAR(50) NOT NULL,
-     price INT NOT NULL,
-     image_url LONGTEXT NOT NULL,
-     PRIMARY KEY (id)
-);
+INSERT INTO Product(name, price, image_url) VALUES('치킨', 10000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0ZReQ5GXoMIwMn5vMIHhRi2VlQoSPAL5d4w&usqp=CAU');
+INSERT INTO Product(name, price, image_url) VALUES('샐러드', 5000, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrogasOW7SXu7JTrirh7b1tFNLJi0EH5Uwho43tKayUvqV3sz8iRfqa2bbxU91TPVYP7g&usqp=CAU')
