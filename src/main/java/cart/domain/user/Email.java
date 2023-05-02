8f3d7a842bf60e0a75ff81f899731bdb24e53c17
70825 <hello70825@gmail.com>
2023-05-02 15:12:16 +0900

    feat: 이메일 객체 추가

---
+++ src/main/java/cart/domain/user/Email.java
@@ -0,0 +1,26 @@
+package cart.domain.user;
+
+import java.util.regex.Pattern;
+
+public class Email {
+
+    private static final Pattern PATTERN = Pattern.compile("^[a-zA-Z0-9+-\\_.]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$");
+
+    private final String email;
+
+    public Email(final String email) {
+        validate(email);
+
+        this.email = email;
+    }
+
+    private void validate(final String email) {
+        if (!PATTERN.matcher(email).matches()) {
+            throw new IllegalArgumentException("유효하지 않은 이메일 형식입니다");
+        }
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ src/test/java/cart/domain/user/EmailTest.java
@@ -0,0 +1,30 @@
+package cart.domain.user;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailTest {
+
+    @Test
+    @DisplayName("이메일을 정상적으로 등록할 수 있다")
+    void emailTest() {
+        final String input = "test123@test.com";
+
+        assertDoesNotThrow(() -> new Email(input));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"@test.com", "test@test", "test.com"})
+    @DisplayName("유효하지 않은 이메일 형식일 경우 예외가 발생한다")
+    void throwExceptionWhenInvalidEmail(final String input) {
+        Assertions.assertThatThrownBy(() -> new Email(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("유효하지 않은 이메일 형식입니다");
+    }
+
+}
