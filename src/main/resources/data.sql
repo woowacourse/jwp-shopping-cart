02be345df7313b301225c5f91baad825603f025c
greeng00se <thegreengoose7@gmail.com>
2023-05-02 22:54:32 +0900

    feat: data.sql 수정 및 schema.sql 파일 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,11 @@
-CREATE TABLE IF NOT EXISTS `product`
-(
-    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `name`       varchar(255)     NOT NULL,
-    `image`      varchar(1024)    NOT NULL,
-    `price`      long             NOT NULL,
-    `created_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    `updated_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+INSERT INTO PRODUCT (name, image, price)
+VALUES ('피자스쿨 치즈피자', 'https://t1.daumcdn.net/cfile/tistory/2647BE3754B7E8B733', 8900);
+INSERT INTO PRODUCT (name, image, price)
+VALUES ('도미노 치즈피자', 'https://cdn.dominos.co.kr/admin/upload/goods/20200311_TI57KvOH.jpg', 23900);
+INSERT INTO PRODUCT (name, image, price)
+VALUES ('서오릉 치즈피자', 'https://blog.kakaocdn.net/dn/bLtveJ/btqTJT1U7rX/sNzWENAmkZGyG5vOeknzzk/img.jpg', 20000);
+
+INSERT INTO MEMBER (email, password)
+VALUES ('pizza1@pizza.com', 'pizza');
+INSERT INTO MEMBER (email, password)
+VALUES ('pizza2@pizza.com', 'pizza')
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS `PRODUCT`
+(
+    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`       varchar(255)     NOT NULL,
+    `image`      varchar(1024)    NOT NULL,
+    `price`      long             NOT NULL,
+    `created_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `MEMBER`
+(
+    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `email`      varchar(255)     NOT NULL,
+    `password`   varchar(255)     NOT NULL,
+    `created_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `CART`
+(
+    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `member_id`  long             NOT NULL,
+    `product_id` long             NOT NULL,
+    `created_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `updated_at` timestamp        NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT CART_UNIQUE UNIQUE (`member_id`, `product_id`),
+    FOREIGN KEY (`member_id`) REFERENCES `MEMBER` (`id`),
+    FOREIGN KEY (`product_id`) REFERENCES `PRODUCT` (`id`)
+);
