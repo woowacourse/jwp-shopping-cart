418783c9f7895970685fca875bf2df1e532d0306
jaeyeon kim <jakind@naver.com>
2023-05-02 21:17:31 +0900

    feat : 원시값 포장 Price 객체 구현

---
+++ src/main/java/cart/vo/Price.java
@@ -0,0 +1,33 @@
+package cart.vo;
+
+public class Price {
+
+    private static final int PRICE_LOWER_BOUND_EXCLUSIVE = 1;
+    private static final int PRICE_UPPER_BOUND_EXCLUSIVE = 1_000_000_000;
+    private final int value;
+
+    private Price(int value) {
+        this.value = value;
+    }
+
+    public static Price of(int value) {
+        validateRange(value);
+        return new Price(value);
+    }
+
+    private static void validateRange(int value) {
+        if (isInvalidRange(value)) {
+            throw new IllegalStateException("올바르지 않은 가격입니다.");
+        }
+    }
+
+    private static boolean isInvalidRange(int value) {
+        return value < PRICE_LOWER_BOUND_EXCLUSIVE || PRICE_UPPER_BOUND_EXCLUSIVE < value;
+    }
+
+
+    public int getValue() {
+        return value;
+    }
+
+}
