151213891576618023c8bd625d28d2c5d1dfc8ca
jeonghoon <shb03323@naver.com>
2023-05-02 15:36:31 +0900

    feat: product, cart 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,22 @@
+CREATE TABLE IF NOT EXISTS users (
+    id          INT           AUTO_INCREMENT,
+    email       VARCHAR(100)  NOT NULL,
+    password    VARCHAR       NOT NULL,
+    name        VARCHAR(20)   DEFAULT '익명의 사용자',
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE IF NOT EXISTS product (
     id       INT           AUTO_INCREMENT,
     name     VARCHAR(20)   NOT NULL,
     image    VARCHAR       NOT NULL,
     price    INT           NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS cart (
+    id          INT       AUTO_INCREMENT,
+    user_id     INT       NOT NULL,
+    product_id  INT       NOT NULL,
     PRIMARY KEY (id)
 );
