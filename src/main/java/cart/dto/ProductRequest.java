f9e8e5a6cbd0064f9aff0325fe0ba36c4a32a4d1
Youngho <cyh6099@gmail.com>
2023-05-03 12:09:30 +0900

    refactor : 기본생성자 생성

--- src/main/java/cart/dto/ProductRequest.java
+++ src/main/java/cart/dto/ProductRequest.java
@@ -8,21 +8,24 @@ import javax.validation.constraints.Size;
 public class ProductRequest {
 
     @NotNull(message = "이미지 url은 비어있을 수 없습니다.")
-    private final String imageUrl;
+    private String imageUrl;
 
     @NotNull(message = "이름은 비어있을 수 없습니다.")
     @Size(min = 1, max = 50)
-    private final String name;
+    private String name;
 
     @NotNull(message = "가격은 비어있을 수 없습니다.")
     @Positive(message = "가격은 0 이상이어야 합니다.")
     @Max(value = 1_000_000_000, message = "가격은 최대 10억까지 가능합니다.")
-    private final Integer price;
+    private Integer price;
 
     public ProductRequest(String imageUrl, String name, Integer price) {
         this.imageUrl = imageUrl;
         this.name = name;
         this.price = price;
+    }
+
+    public ProductRequest() {
     }
 
     public String getImageUrl() {
