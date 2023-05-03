b3ceb020b9e37d6be2ee23e4df6ad5a7a5e6a683
seokhwan-an <asok369258@naver.com>
2023-05-03 15:30:48 +0900

    refactor: 테스트 케이스를 성공과 실패로 나누어서 관리

--- src/test/java/cart/controller/AdminProductAcceptanceTest.java
+++ src/test/java/cart/controller/AdminProductAcceptanceTest.java
@@ -10,9 +10,9 @@ import io.restassured.response.Response;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -31,11 +31,31 @@ class AdminProductAcceptanceTest {
         RestAssured.port = port;
     }
 
-    @DisplayName("상품을 등록할 때 성공하면 상태코드 200을 그렇지 않으면 상태코드 400을 반환한다.")
+    @DisplayName("상품을 등록할 때 성공하면 상태코드 201을 반환한다.")
     @Rollback
-    @MethodSource("createProductAndStatusCode")
+    @Test
+    void create_product_success() {
+        // givn
+        ProductRequest productAddRequest = new ProductRequest("연필", "이미지", 1000);
+
+        // when
+        ExtractableResponse<Response> result = given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(productAddRequest)
+            .when()
+            .post("admin/products")
+            .then()
+            .extract();
+
+        // then
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+    }
+
+    @DisplayName("상품을 등록할 때 잘못된 요청이 오면 상태코드 400을 반환한다.")
+    @Rollback
+    @MethodSource("createWrongProduct")
     @ParameterizedTest(name = "{displayName}")
-    void create_product_success(ProductRequest productRequest, int statusCode) {
+    void create_product_fail(ProductRequest productRequest) {
         // when
         ExtractableResponse<Response> result = given()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -46,35 +66,27 @@ class AdminProductAcceptanceTest {
             .extract();
 
         // then
-        assertThat(result.statusCode()).isEqualTo(statusCode);
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 
-    private static Stream<Arguments> createProductAndStatusCode() {
+    private static Stream<Arguments> createWrongProduct() {
         return Stream.of(
-            Arguments.arguments(new ProductRequest("연필", "이미지", 1000), HttpStatus.CREATED.value()),
-            Arguments.arguments(new ProductRequest("", "이미지", 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest("", "", 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest("", null, 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest(null, "이미지", 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest("연필", "이미지", -1), HttpStatus.BAD_REQUEST.value())
+            Arguments.arguments(new ProductRequest("", "이미지", 1000)),
+            Arguments.arguments(new ProductRequest("", "", 1000)),
+            Arguments.arguments(new ProductRequest("", null, 1000)),
+            Arguments.arguments(new ProductRequest(null, "이미지", 1000)),
+            Arguments.arguments(new ProductRequest("연필", "이미지", -1))
         );
     }
 
-    @DisplayName("상품 수정 요청을 보냈을 때 성공하면 상태코드 204를 반환하고 잘못된 요청을 보내면 400을 반환한다.")
+    @DisplayName("상품 수정 요청을 보냈을 때 성공하면 상태코드 204를 보내준다.")
     @Rollback
-    @MethodSource("updateProductAndStatusCode")
-    @ParameterizedTest
-    void update_product(ProductRequest productUpdateDto, int statusCode) {
+    @Test
+    void update_product_success() {
         // given
-        ProductRequest productRequest = new ProductRequest("지우개", "이미지", 2000);
-        ExtractableResponse<Response> add = given()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(productRequest)
-            .post("admin/products")
-            .then()
-            .extract();
-
-        Long id = Long.valueOf(add.header("Location").split("/")[1]);
+        ExtractableResponse<Response> addResult = addProduct();
+        Long id = Long.valueOf(addResult.header("Location").split("/")[1]);
+        ProductRequest productUpdateDto = new ProductRequest("지우개", "이미지 url", 2000);
 
         // when
         ExtractableResponse<Response> result = given()
@@ -87,31 +99,72 @@ class AdminProductAcceptanceTest {
             .extract();
 
         // then
-        assertThat(result.statusCode()).isEqualTo(statusCode);
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.NO_CONTENT.value());
     }
 
-    private static Stream<Arguments> updateProductAndStatusCode() {
+    @DisplayName("없는 상품에 대한 수정 요청을 보냈을 때는 상태코드 404를 보내준다.")
+    @Rollback
+    @Test
+    void update_not_exist_product() {
+        // given
+        ExtractableResponse<Response> addResult = addProduct();
+        Long id = Long.valueOf(addResult.header("Location").split("/")[1]);
+        ProductRequest productUpdateDto = new ProductRequest("지우개", "이미지 url", 2000);
+
+        // when
+        ExtractableResponse<Response> result = given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .queryParam("id", id + 1)
+            .body(productUpdateDto)
+            .when()
+            .put("admin/products/" + id + 1)
+            .then()
+            .extract();
+
+        // then
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+    @DisplayName("상품 수정 요청을 보냈을 때 잘못된 상품을 요청 보내면 상태코드 400을 반환한다.")
+    @Rollback
+    @MethodSource("updateWrongProduct")
+    @ParameterizedTest(name = "{displayName}")
+    void update_product_fail(ProductRequest productUpdateDto) {
+        // given
+        ExtractableResponse<Response> addResult = addProduct();
+        Long id = Long.valueOf(addResult.header("Location").split("/")[1]);
+
+        // when
+        ExtractableResponse<Response> result = given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .queryParam("id", id)
+            .body(productUpdateDto)
+            .when()
+            .put("admin/products/" + id)
+            .then()
+            .extract();
+
+        // then
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    private static Stream<Arguments> updateWrongProduct() {
         return Stream.of(
-            Arguments.arguments(new ProductRequest("연필", "이미지", 1000), HttpStatus.NO_CONTENT.value()),
-            Arguments.arguments(new ProductRequest("", "이미지", 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest("", "", 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest("", null, 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest(null, "이미지", 1000), HttpStatus.BAD_REQUEST.value()),
-            Arguments.arguments(new ProductRequest("연필", "이미지", -1), HttpStatus.BAD_REQUEST.value())
+            Arguments.arguments(new ProductRequest("", "이미지", 1000)),
+            Arguments.arguments(new ProductRequest("", "", 1000)),
+            Arguments.arguments(new ProductRequest("", null, 1000)),
+            Arguments.arguments(new ProductRequest(null, "이미지", 1000)),
+            Arguments.arguments(new ProductRequest("연필", "이미지", -1))
         );
     }
 
-    @DisplayName("상품 삭제 요청을 보냈을 때 성공하면 상태코드 200을 반환하고 잘못된 요청을 보내면 400을 반환한다.")
+    @DisplayName("상품 삭제 요청을 보냈을 때 성공하면 상태코드 200과 삭제된 상품의 아이디를 반환한다.")
     @Rollback
-    @CsvSource(value = {"1,200", "100,404"})
-    @ParameterizedTest
-    void delete_product(Long id, int statusCode) {
+    @Test
+    void delete_product_success() {
         // given
-        ProductRequest productRequest = new ProductRequest("지우개", "이미지", 2000);
-        given()
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .body(productRequest)
-            .post("admin/products");
+        ExtractableResponse<Response> addResult = addProduct();
+        Long id = Long.valueOf(addResult.header("Location").split("/")[1]);
 
         // when
         ExtractableResponse<Response> result = given()
@@ -123,6 +176,39 @@ class AdminProductAcceptanceTest {
             .extract();
 
         // then
-        assertThat(result.statusCode()).isEqualTo(statusCode);
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(result.body().as(Long.class)).isEqualTo(id);
+    }
+
+    @DisplayName("없는 상품에 대해서 삭제 요청을 보내면 상태코드 404를 반환한다.")
+    @Rollback
+    @Test
+    void delete_product_fail() {
+        // given
+        ExtractableResponse<Response> addResult = addProduct();
+        Long id = Long.valueOf(addResult.header("Location").split("/")[1]);
+
+        // when
+        ExtractableResponse<Response> result = given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .queryParam("id", id + 1)
+            .when()
+            .delete("admin/products/" + id + 1)
+            .then()
+            .extract();
+
+        // then
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.NOT_FOUND.value());
+    }
+
+
+    private ExtractableResponse<Response> addProduct() {
+        ProductRequest productRequest = new ProductRequest("지우개", "이미지", 2000);
+        return given()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(productRequest)
+            .post("admin/products")
+            .then()
+            .extract();
     }
 }
