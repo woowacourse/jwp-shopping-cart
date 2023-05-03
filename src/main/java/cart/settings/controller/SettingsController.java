9fc3611d92d47201560ea8d95e6bab18852bac61
echo <echo.backend@gmail.com>
2023-05-03 17:20:42 +0900

    feat: 사용자 설정 페이지 연동 기능 구현

---
+++ src/main/java/cart/settings/controller/SettingsController.java
@@ -0,0 +1,25 @@
+package cart.settings.controller;
+
+import cart.settings.service.SettingsService;
+import cart.user.dto.ResponseUserDTO;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SettingsController {
+    
+    private final SettingsService settingsService;
+    
+    public SettingsController(final SettingsService settingsService) {
+        this.settingsService = settingsService;
+    }
+    
+    @GetMapping("/settings")
+    public String settings(final Model model) {
+        final List<ResponseUserDTO> allUsers = this.settingsService.findAllUsers();
+        model.addAttribute("users", allUsers);
+        return "settings";
+    }
+}
