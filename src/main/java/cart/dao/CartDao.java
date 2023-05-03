ddd8bf6a7534602d77bd978c68128a316751d46b
Miseong Kim <miseongk@naver.com>
2023-05-04 00:51:26 +0900

    feat: cart id로 CartItem 조히하는 기능 구현

--- src/main/java/cart/dao/CartDao.java
+++ src/main/java/cart/dao/CartDao.java
@@ -2,15 +2,27 @@ package cart.dao;
 
 import cart.entity.CartItem;
 import cart.entity.Product;
+import cart.entity.Product.Builder;
 import java.sql.PreparedStatement;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class CartDao {
+
+    private final RowMapper<CartItem> cartItemRowMapper = (resultSet, rowNum) -> {
+        Product product = new Builder()
+                .name(resultSet.getString("name"))
+                .price(resultSet.getInt("price"))
+                .imageUrl(resultSet.getString("image_url"))
+                .build();
+        return new CartItem(resultSet.getInt("id"), product);
+    };
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -22,14 +34,7 @@ public class CartDao {
         String sqlForFindAll = "SELECT c.id, p.name, p.price, p.image_url FROM Cart c JOIN Product p "
                 + "WHERE c.product_id = p.id and c.member_email = ?";
 
-        return jdbcTemplate.query(sqlForFindAll, (resultSet, rowNum) -> {
-            Product product = new Product.Builder()
-                    .name(resultSet.getString("name"))
-                    .price(resultSet.getInt("price"))
-                    .imageUrl(resultSet.getString("image_url"))
-                    .build();
-            return new CartItem(resultSet.getInt("id"), product);
-        }, memberEmail);
+        return jdbcTemplate.query(sqlForFindAll, cartItemRowMapper, memberEmail);
     }
 
     public int save(int productId, String memberEmail) {
@@ -55,6 +60,18 @@ public class CartDao {
         return jdbcTemplate.query(sqlForFindByProductId, (resultSet, rowNum) ->
                         resultSet.getInt("id"),
                 productId, memberEmail);
+    }
+
+    public Optional<CartItem> findById(int cartId) {
+        String sqlForFindById = "SELECT c.id, p.name, p.price, p.image_url FROM Cart c JOIN Product p "
+                + "WHERE c.product_id = p.id and c.id = ?";
+        List<CartItem> cartItems = jdbcTemplate.query(sqlForFindById, cartItemRowMapper, cartId);
+
+        if (cartItems.isEmpty()) {
+            return Optional.empty();
+        }
+
+        return Optional.of(cartItems.get(0));
     }
 
 }
