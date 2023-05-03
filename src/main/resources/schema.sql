720b6c2437a77b6becf83b08d69351d59396342b
jeomxon <jeomxon@gmail.com>
2023-05-03 14:50:05 +0900

    feat: 장바구니에 대응하는 cart테이블 구현

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -14,3 +14,13 @@ CREATE TABLE member
     password varchar(50)  NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE cart
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) REFERENCES member (id),
+    FOREIGN KEY (product_id) REFERENCES product (id)
+);
