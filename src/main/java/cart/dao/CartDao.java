e6b3e76d476cfb99fcdd0d1a269df9d216786d28
jaeyeon kim <jakind@naver.com>
2023-05-03 23:15:39 +0900

    feat : Cart 를 삭제하는 기능 구현

--- src/main/java/cart/dao/CartDao.java
+++ src/main/java/cart/dao/CartDao.java
@@ -3,6 +3,7 @@ package cart.dao;
 import cart.entity.Cart;
 import cart.vo.Email;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,11 @@ import java.util.List;
 @Repository
 public class CartDao {
 
+    RowMapper<Cart> cartRowMapper = (resultSet, rowNum) -> new Cart.Builder()
+            .id(resultSet.getLong("id"))
+            .email(Email.from(resultSet.getString("member_email")))
+            .productId(resultSet.getLong("product_id"))
+            .build();
     private final JdbcTemplate jdbcTemplate;
 
     public CartDao(JdbcTemplate jdbcTemplate) {
@@ -22,17 +28,23 @@ public class CartDao {
         jdbcTemplate.update(sqlForSave, cart.getEmail(), cart.getProductId());
     }
 
+    public Cart findById(Long id) {
+        String sqlForFindById = "SELECT * FROM Cart WHERE id = ?";
+        return jdbcTemplate.queryForObject(sqlForFindById, cartRowMapper, id);
+    }
+
     public List<Cart> selectAll(String email) {
         String sqlForFindAll = "SELECT * FROM Cart WHERE member_email = ?";
 
         return jdbcTemplate.query(
                 sqlForFindAll,
-                (resultSet, rowNum) -> new Cart.Builder()
-                        .id(resultSet.getLong("id"))
-                        .email(Email.from(resultSet.getString("member_email")))
-                        .productId(resultSet.getLong("product_id"))
-                        .build(),
+                cartRowMapper,
                 email);
+    }
+
+    public void deleteById(Long id) {
+        String sqlForDeleteById = "DELETE FROM Cart WHERE id = ?";
+        jdbcTemplate.update(sqlForDeleteById, id);
     }
 
 }
