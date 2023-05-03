9ec24b2950d95068ce3c37d437d8bdee5ffbd5a0
cpot5620 <=cpot5620@naver.com>
2023-05-03 16:20:45 +0900

    feat(DB): User 테이블 초기 데이터 추가

--- src/main/resources/data-initial.sql
+++ src/main/resources/data-initial.sql
@@ -6,3 +6,6 @@ values ('치킨', 20000, 'FOOD',
      , ('고퍼우드 g110', 170000, 'INSTRUMENT',
         'https://m.gopherwood.co.kr/web/product/big/201910/48430668d3e313d9446d19a9b32afb83.jpg');
 
+INSERT INTO user (email, password)
+values ('test1@naver.com', 'test1'),
+       ('test2@google.com','test2')
