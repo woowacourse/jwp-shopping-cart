7e6758251728b3bd06f7c468df336d781d3a0e9b
kevstevie <jianna442@gmail.com>
2023-05-03 15:34:18 +0900

    chore: 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO member (email, password)
 VALUES ('user1@woowa.com', '123456'),
        ('user2@woowa.com', '1234');
+
+INSERT INTO product (name, price, image_url)
+VALUES ('product1', 1000, 'url.com'),
+       ('product2', 100, 'url2.com');
