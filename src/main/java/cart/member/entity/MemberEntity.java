6dcb842e94833c137cc5d50b4d493f8318f6c3b1
yujamint <woawoa0513@gmail.com>
2023-05-03 17:42:58 +0900

    refactor: Member id 타입 수정

--- src/main/java/cart/member/entity/MemberEntity.java
+++ src/main/java/cart/member/entity/MemberEntity.java
@@ -2,17 +2,17 @@ package cart.member.entity;
 
 public class MemberEntity {
 
-    private final int id;
+    private final Integer id;
     private final String email;
     private final String password;
 
-    public MemberEntity(final int id, final String email, final String password) {
+    public MemberEntity(final Integer id, final String email, final String password) {
         this.id = id;
         this.email = email;
         this.password = password;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
