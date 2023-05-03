411fb31a93dda475794984498884712560557212
Rosie <dev.kayoung@gmail.com>
2023-05-03 09:45:25 +0900

    refactor: exceptionHandler가 ResponseEntityExceptionHandler를 상속하도록 함

--- src/main/java/cart/advice/GlobalExceptionHandler.java
+++ src/main/java/cart/advice/GlobalExceptionHandler.java
@@ -1,22 +1,22 @@
 package cart.advice;
 
 import cart.dto.ApiErrorResponse;
-import cart.dto.ApiResponse;
 import cart.exception.InvalidPasswordException;
 import cart.exception.ProductNotFoundException;
 import cart.exception.UserNotFoundException;
 import java.util.Objects;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ProductNotFoundException.class, UserNotFoundException.class})
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
@@ -30,11 +30,12 @@ public class CustomControllerAdvice extends ResponseEntityExceptionHandler {
         return ApiErrorResponse.of(HttpStatus.UNAUTHORIZED, exception);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
-    public ApiErrorResponse handleMethodArgumentNotValidException(final MethodArgumentNotValidException exception) {
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException exception,
+                                                                  HttpHeaders headers, HttpStatus status,
+                                                                  WebRequest request) {
         String defaultMessage = Objects.requireNonNull(exception.getBindingResult().getFieldError()).getDefaultMessage();
-
-        return ApiErrorResponse.of(HttpStatus.BAD_REQUEST, defaultMessage);
+        ApiErrorResponse body = ApiErrorResponse.of(HttpStatus.BAD_REQUEST, defaultMessage);
+        return handleExceptionInternal(exception, body, headers, status, request);
     }
 }
