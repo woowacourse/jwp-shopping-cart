89bb79e88e5c5e1c984ae12849ed06890fa01d41
Be-student <presentsong@naver.com>
2023-05-03 22:50:15 +0900

    feat: findByUser 에서 Optional 로 반환 타입 변경

--- src/main/java/cart/repository/cart/CartRepository.java
+++ src/main/java/cart/repository/cart/CartRepository.java
@@ -2,12 +2,13 @@ package cart.repository.cart;
 
 import cart.domain.cart.Cart;
 import cart.domain.user.User;
+import java.util.Optional;
 
 public interface CartRepository {
 
     Cart save(Cart cart);
 
-    Cart update(Cart cart);
+    Optional<Cart> findByUser(User user);
 
-    Cart findByUser(User user);
+    Cart update(Cart cart);
 }
