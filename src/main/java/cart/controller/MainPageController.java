eb6ad493a89702130d9a9bf218efed6910eb5f43
dooboocookie <bkh3030@gmail.com>
2023-05-03 22:24:03 +0900

    feat: MainPageController /setting /cart 뷰 매핑

--- src/main/java/cart/controller/MainPageController.java
+++ src/main/java/cart/controller/MainPageController.java
@@ -1,7 +1,11 @@
 package cart.controller;
 
 import cart.controller.dto.ItemResponse;
+import cart.controller.dto.LoginUser;
+import cart.controller.dto.UserResponse;
+import cart.service.CartService;
 import cart.service.ItemService;
+import cart.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +16,11 @@ import java.util.List;
 public class MainPageController {
 
     private final ItemService itemService;
+    private final UserService userService;
 
-    public MainPageController(final ItemService itemService) {
+    public MainPageController(final ItemService itemService, final UserService userService) {
         this.itemService = itemService;
+        this.userService = userService;
     }
 
     @GetMapping("/")
@@ -29,5 +35,17 @@ public class MainPageController {
         List<ItemResponse> items = itemService.loadAllItem();
         model.addAttribute("products", items);
         return "admin";
+    }
+
+    @GetMapping("/settings")
+    public String showSetting(final Model model) {
+        List<UserResponse> users = userService.loadAllUser();
+        model.addAttribute("users", users);
+        return "settings";
+    }
+
+    @GetMapping("/cart")
+    public String showCart(final Model model) {
+        return "cart";
     }
 }
