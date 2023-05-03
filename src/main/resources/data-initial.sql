0dd70599146dca010fb0e2bdf720627e81ba8a85
yujamint <woawoa0513@gmail.com>
2023-05-03 17:23:40 +0900

    chore: 초기 데이터 DML 파일 생성

---
+++ src/main/resources/data-initial.sql
@@ -0,0 +1,8 @@
+INSERT INTO PRODUCTS (name, price, image) VALUES ('치킨', 1000, 'https://i.namu.wiki/i/pTVoWDp5G09PGTRUTbCy8raXo9CB47uF2wcuzdUYTlPwRjU6zjl0Reoih4MIXXRTnfxVl-yKlPjTQSVhAbfSxA.webp');
+INSERT INTO PRODUCTS (name, price, image) VALUES ('피자', 2000, 'https://cdn.dominos.co.kr/admin/upload/goods/20200311_x8StB1t3.jpg');
+INSERT INTO PRODUCTS (name, price, image) VALUES ('떡볶이', 3000, 'https://static.wtable.co.kr/image/production/service/recipe/1661/e8987193-0529-44c8-9740-56c0352f7184.jpg?size=1024x1024');
+
+INSERT INTO MEMBERS (email, password) VALUES ('yuja@naver.com', 'yuja1234');
+INSERT INTO MEMBERS (email, password) VALUES ('mint@google.com', 'min567');
+INSERT INTO MEMBERS (email, password) VALUES ('tea@kakao.com', 'tea890');
+
