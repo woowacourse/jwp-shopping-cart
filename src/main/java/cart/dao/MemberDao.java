1748e47f5de6bd2227a5bc0206ed1fbc8a46304b
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-05-03 14:04:02 +0900

    feat: 장바구니에 상품 추가

--- src/main/java/cart/dao/MemberDao.java
+++ src/main/java/cart/dao/MemberDao.java
@@ -2,12 +2,22 @@ package cart.dao;
 
 import cart.entity.MemberEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.Member;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class MemberDao {
+
+    private static final RowMapper<MemberEntity> rowMapper =
+            (rs, rowNum) -> new MemberEntity(
+                    rs.getLong(1),
+                    rs.getString(2),
+                    rs.getString(3)
+            );
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -31,4 +41,8 @@ public class MemberDao {
         );
     }
 
+    public Optional<MemberEntity> findByEmail(String email) {
+        String sql = "SELECT * FROM MEMBER WHERE email = ?";
+        return Optional.of(jdbcTemplate.queryForObject(sql, rowMapper, email));
+    }
 }
