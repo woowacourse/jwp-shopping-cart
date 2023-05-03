2ed8a0150ad6ae04127e37f5141929fbdb02180c
hong-sile <gurwns9325@naver.com>
2023-05-03 21:56:53 +0900

    refactor: 기본으로 들어가는 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,15 @@
+-- product
 insert into product (name, image_url, price)
 values ('사과', 'https://news.imaeil.com/photos/2021/10/19/2021101920550174833_l.jpg', 4000);
+insert into product (name, image_url, price)
+values ('치킨', 'https://data.0app0.com/diet/shop/goods/20210219/20210219151516_6436453018_2.jpg', 20000);
 
+-- member
 insert into member (email, password)
 values ('hongSile@wooteco.com', 'hongsile');
-
 insert into member (email, password)
 values ('seovalue@wooteco.com', 'seovalue');
 
+-- cart
 insert into cart (member_id, product_id)
 values (1L, 1L);
