6407274b4333b1822a411735f9b4a6b4279347a9
kdkdhoho <hkim4410@naver.com>
2023-05-03 19:07:52 +0900

    test(CartControllerTest): 통합 테스트 추가

---
+++ src/test/java/cart/controller/CartControllerTest.java
@@ -0,0 +1,87 @@
+package cart.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql({"/schema.sql", "/data.sql"})
+class CartControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 장바구니에_담겨진_상품을_가져올_때_인증정보가_포함되지_않으면_401에러_발생(final String value) {
+        RestAssured.given()
+                .header(new Header("Authorization", value))
+                .get("/cart/products")
+                .then()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void 장바구니에_담겨진_상품을_가져올_때_인증정보가_포함되면_성공() {
+        RestAssured.given()
+                .auth().preemptive().basic("userA@woowahan.com", "passwordA")
+                .when()
+                .get("/cart/products")
+                .then()
+                .contentType(ContentType.JSON)
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 장바구니에_상품을_등록할_때_인증정보가_포함되지_않으면_401에러_발생(final String value) {
+        RestAssured.given()
+                .header(new Header("Authorization", value))
+                .post("/cart/1")
+                .then()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void 장바구니에_상품을_가져올_때_인증정보가_포함되면_성공() {
+        RestAssured.given()
+                .auth().preemptive().basic("userB@woowahan.com", "passwordB")
+                .when()
+                .post("/cart/1")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " "})
+    void 장바구니에_상품을_삭제할_때_인증정보가_포함되지_않으면_401에러_발생(final String value) {
+        RestAssured.given()
+                .header(new Header("Authorization", value))
+                .delete("/cart/1")
+                .then()
+                .statusCode(HttpStatus.UNAUTHORIZED.value());
+    }
+
+    @Test
+    void 장바구니에_상품을_삭제할_때_인증정보가_포함되면_성공() {
+        RestAssured.given()
+                .auth().preemptive().basic("userB@woowahan.com", "passwordB")
+                .when()
+                .delete("/cart/2")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
