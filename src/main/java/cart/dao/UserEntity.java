a1e38f3c5e111d693991b681b23acdaf632fd15c
cpot5620 <=cpot5620@naver.com>
2023-05-03 14:50:16 +0900

    feat(DB): User 저장 및 조회 기능 구현

---
+++ src/main/java/cart/dao/UserEntity.java
@@ -0,0 +1,26 @@
+package cart.dao;
+
+public class UserEntity {
+    private final Long id;
+    private final String email;
+    private final String password;
+
+    public UserEntity(Long id, String email, String password) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
