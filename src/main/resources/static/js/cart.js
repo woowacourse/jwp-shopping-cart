55c7c8403d2e48ce0e774d4c1611f836ba55f0f5
Be-student <presentsong@naver.com>
2023-05-04 02:10:47 +0900

    feat: cartjs 작성

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -8,14 +8,21 @@ const addCartItem = (productId) => {
 
   // TODO: [2단계] 장바구니 CRUD API에 맞게 변경
   axios.request({
-    url: '',
+    url: '/carts',
+    method: 'POST',
+    data: {
+      productId
+    },
     headers: {
-      'Authorization': `Basic ${credentials}`
+      'Authorization': `Basic ${credentials}`,
+      'Content-type': 'application/json'
     }
   }).then((response) => {
     alert('장바구니에 담았습니다.');
   }).catch((error) => {
     console.error(error);
+    alert('사용자 정보가 없습니다.');
+    window.location.href = '/settings';
   });
 }
 
