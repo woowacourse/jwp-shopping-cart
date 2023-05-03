02d446efee9c879427dc2073985954ccd362098e
zillionme <beatus96@naver.com>
2023-05-03 17:17:38 +0900

    refactor: 상품 api 요청 매개변수 유효성 검사 기능 추가 및 반환 타입 수정

--- src/main/java/cart/controller/ProductApiController.java
+++ src/main/java/cart/controller/ProductApiController.java
@@ -1,11 +1,11 @@
 package cart.controller;
 
 import cart.dto.ProductDto;
-import cart.dto.ProductSaveRequestDto;
-import cart.dto.ProductUpdateRequestDto;
+import cart.dto.ProductRequestDto;
 import cart.service.ProductService;
 import java.net.URI;
 import java.util.List;
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,33 +18,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/products")
-public class ProductController {
+public class ProductApiController {
 
     private final ProductService productService;
 
-    public ProductController(ProductService productService) {
+    public ProductApiController(ProductService productService) {
         this.productService = productService;
     }
 
     //todo (질문1): (PageController)에서 admin 페이지를 보여주는 거랑 중복 코드인 것같은데, 삭제해야할까요?
     @GetMapping
     public ResponseEntity<List<ProductDto>> findAllProducts() {
-        List<ProductDto> products = productService.findAllProducts();
-        return ResponseEntity.ok(products);
+        List<ProductDto> allProducts = productService.findAllProducts();
+        return ResponseEntity.ok(allProducts);
     }
 
     //todo (질문3) : responseEntity의 status를 넘겨줄 때, uri 넘겨줘야하는 이유? 넘겨준 uri 정보로 리다이렉트 가능하다.
     @PostMapping
-    public ResponseEntity<Void> saveProduct(@RequestBody ProductSaveRequestDto productSaveRequestDto) {
-        long productId = productService.saveProduct(productSaveRequestDto);
-        return ResponseEntity.created(URI.create("products"+ productId)).build();
+    public ResponseEntity<ProductDto> saveProduct(@RequestBody @Valid ProductRequestDto productRequestDto) {
+        ProductDto savedProduct = productService.saveProduct(productRequestDto);
+        return ResponseEntity.created(URI.create("products"+ savedProduct.getProductId())).body(savedProduct);
     }
 
-    @PutMapping
-    public ResponseEntity<Void> updateProduct(@RequestBody ProductUpdateRequestDto productUpdateRequestDto) {
-        productService.updateProduct(productUpdateRequestDto);
-        return ResponseEntity.noContent().build();
+    @PutMapping("/{productId}")
+    public ResponseEntity<ProductDto> updateProduct(@PathVariable long productId, @RequestBody @Valid ProductRequestDto productRequestDto) {
+        ProductDto updatedProduct = productService.updateProduct(productId, productRequestDto);
+        return ResponseEntity.ok(updatedProduct);
     }
+
 
     @DeleteMapping("/{productId}")
     public ResponseEntity<Void> deleteProduct(@PathVariable long productId) {
