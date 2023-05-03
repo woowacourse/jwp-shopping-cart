20cd9dd6a411303bc61fab78b771b4d33ab7c8b6
yenawee <yaena0319@naver.com>
2023-05-03 14:55:04 +0900

    fix : DB 초기 설정 문제 해결하기

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO product(name, price, image_url)
-VALUES ('스프링', 1000, 'https://previews.123rf.com/images/marucyan/marucyan1211/marucyan121100106/16114832-새싹.jpg'),
-       ('빙봉', 10000,
+INSERT IGNORE INTO product(product_id, name, price, image_url)
+VALUES (1, '스프링', 1000, 'https://previews.123rf.com/images/marucyan/marucyan1211/marucyan121100106/16114832-새싹.jpg'),
+       (2, '빙봉', 10000,
         'https://mblogthumb-phinf.pstatic.net/MjAxNjEyMTNfMTk4/MDAxNDgxNjAwMDE1ODM1.mxPz7FBOV9DF5ryoeYEAcfZfBwMeFOYp-p03N7RAO68g.ICYGpmoaW0Q0wSenF5JQzQ4LZ3aqRWazx66DTqYgJFcg.JPEG.jeenyjin/Screenshot_2016-12-12-13-48-12-1.jpg?type=w800');
