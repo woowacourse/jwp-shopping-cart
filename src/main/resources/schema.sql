2b04697586761be81ae0841a1b7f5f34f8048733
jaeyeon kim <jakind@naver.com>
2023-05-03 21:23:57 +0900

    chore : Cart 테이블 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,8 +7,16 @@ CREATE TABLE Product (
 );
 
 CREATE TABLE Member (
-    id BIGINT AUTO_INCREMENT,
     email VARCHAR(30) UNIQUE NOT NULL,
     password VARCHAR(20) NOT NULL,
-    PRIMARY KEY (id)
-)
+    PRIMARY KEY (email)
+);
+
+CREATE TABLE Cart (
+    id BIGINT AUTO_INCREMENT,
+    member_email VARCHAR(30) NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT `cart_ibfk_1` FOREIGN KEY (member_email) REFERENCES Member (email) ON DELETE CASCADE,
+    CONSTRAINT `cart_ibfk_2` FOREIGN KEY (product_id) REFERENCES Product (id) ON DELETE CASCADE
+);
