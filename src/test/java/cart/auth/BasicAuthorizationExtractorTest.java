2b9da1bf94c25efff8c999431ab593c786e37341
jeonghoon <shb03323@naver.com>
2023-05-03 17:02:49 +0900

    test: Authorization 디코딩 테스트 진행

---
+++ src/test/java/cart/auth/BasicAuthorizationExtractorTest.java
@@ -0,0 +1,51 @@
+package cart.auth;
+
+import cart.auth.dto.AuthenticationDto;
+import cart.exception.AuthorizationException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class BasicAuthorizationExtractorTest {
+
+    @Test
+    @DisplayName("디코딩 성공 테스트")
+    void decode_success() {
+        // given
+        String authorizationHeader = "Basic ZGl0b29Ad29vdGVjby5jb206ZGl0b28xMjM0";
+
+        // when
+        AuthenticationDto authenticationDto = BasicAuthorizationExtractor.extract(authorizationHeader);
+
+        // then
+        assertAll(
+                () -> assertThat(authenticationDto.getEmail()).isEqualTo("ditoo@wooteco.com"),
+                () -> assertThat(authenticationDto.getPassword()).isEqualTo("ditoo1234")
+        );
+    }
+
+    @Test
+    @DisplayName("접두어가 Basic이 아닌 경우 예외가 발생한다.")
+    void decode_fail_invalid_prefix() {
+        // given
+        String authorizationHeader = "Ditoo ZGl0b29Ad29vdGVjby5jb206ZGl0b28xMjM0";
+
+        // when, then
+        assertThatThrownBy(() -> BasicAuthorizationExtractor.extract(authorizationHeader))
+                .isInstanceOf(AuthorizationException.class);
+    }
+
+    @Test
+    @DisplayName("':'으로 구분되는 값이 2개가 아니면 예외가 발생한다.")
+    void decode_fail_split_error() {
+        // given
+        String authorizationHeader = "Basic ZGl0b29Ad29vdGVjby5jb21kaXRvbzEyMzQ=";
+
+        // when, then
+        assertThatThrownBy(() -> BasicAuthorizationExtractor.extract(authorizationHeader))
+                .isInstanceOf(AuthorizationException.class);
+    }
+}
