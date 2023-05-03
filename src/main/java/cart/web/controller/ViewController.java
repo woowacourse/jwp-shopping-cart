8714b6fc364f0815236c12ee254a015840e23177
cpot5620 <=cpot5620@naver.com>
2023-05-03 16:23:10 +0900

    feat(Controller): User 전체 조회 요청 처리 기능 구현

--- src/main/java/cart/web/controller/ViewController.java
+++ src/main/java/cart/web/controller/ViewController.java
@@ -1,18 +1,23 @@
 package cart.web.controller;
 
 import cart.domain.service.ProductService;
+import cart.domain.service.UserService;
 import cart.domain.service.dto.ProductDto;
-import java.util.List;
+import cart.domain.service.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ViewController {
     private final ProductService productService;
+    private final UserService userService;
 
-    public ViewController(ProductService productService) {
+    public ViewController(ProductService productService, UserService userService) {
         this.productService = productService;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -31,5 +36,14 @@ public class ViewController {
         model.addAttribute("products", allProducts);
 
         return "admin";
+    }
+
+    @GetMapping("/settings")
+    public String loadSettingsPage(Model model) {
+        List<UserDto> allUsers = userService.getAllUsers();
+
+        model.addAttribute("users", allUsers);
+
+        return "settings";
     }
 }
