540ba7480a789d94ca089eab3512dc5e87024241
swonny <wonny921@gmail.com>
2023-05-03 14:56:56 +0900

    feat: 회원 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,13 @@ CREATE TABLE PRODUCT (
     image VARCHAR(2000) NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE MEMBER (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO MEMBER (email, password) VALUES ('a@a.com', 'password1');
+INSERT INTO MEMBER (email, password) VALUES ('b@b.com', 'password2');
