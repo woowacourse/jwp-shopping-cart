1a33b0c4b5523648261deb20e9abf0532f7c1ce9
woosung1223 <dntjd991223@naver.com>
2023-05-03 23:12:09 +0900

    feat: Logging Interceptor 생성

---
+++ src/main/java/cart/WebMvcConfiguration.java
@@ -0,0 +1,16 @@
+package cart;
+
+import cart.presentation.adapter.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor())
+                .addPathPatterns("/**");
+    }
+}
