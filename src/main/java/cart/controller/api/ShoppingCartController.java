a4ebb10da6a3be463bd36bb815840e778e9e888d
hong-sile <gurwns9325@naver.com>
2023-05-03 21:44:36 +0900

    feat: 장바구니에 있는 상품을 삭제하는 기능 추가

--- src/main/java/cart/controller/api/ShoppingCartController.java
+++ src/main/java/cart/controller/api/ShoppingCartController.java
@@ -1,15 +1,17 @@
 package cart.controller.api;
 
 import cart.controller.util.BasicAuthExtractor;
-import cart.entity.Product;
 import cart.service.ShoppingCartService;
+import cart.service.dto.CartResponse;
 import cart.service.dto.MemberInfo;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,10 +28,10 @@ public class ShoppingCartController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Product>> shoppingList(final HttpServletRequest httpServletRequest) {
+    public ResponseEntity<List<CartResponse>> shoppingList(final HttpServletRequest httpServletRequest) {
         final MemberInfo info = BasicAuthExtractor.extract(httpServletRequest);
-        final List<Product> allProduct = shoppingCartService.findAllProduct(info);
-        return ResponseEntity.ok().body(allProduct);
+        final List<CartResponse> cartResponses = shoppingCartService.findAllProduct(info);
+        return ResponseEntity.ok().body(cartResponses);
     }
 
     @PostMapping
@@ -38,5 +40,11 @@ public class ShoppingCartController {
         final MemberInfo info = BasicAuthExtractor.extract(httpServletRequest);
         shoppingCartService.addCartProduct(info, productId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> removeProduct(@PathVariable final Long id) {
+        shoppingCartService.removeProduct(id);
+        return ResponseEntity.noContent().build();
     }
 }
