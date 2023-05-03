228e49de288c40c11e2018facfb0af5f91faa4f3
hanueleee <ezzeezz@naver.com>
2023-05-03 14:33:07 +0900

    feat: Authrization 헤더 추출하는 기능 구현

---
+++ src/main/java/cart/infrastructure/BasicAuthorizationExtractor.java
@@ -0,0 +1,37 @@
+package cart.infrastructure;
+
+import cart.dto.AuthInfo;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.tomcat.util.codec.binary.Base64;
+
+public class BasicAuthorizationExtractor {
+    private static final String BASIC_TYPE = "Basic";
+    private static final String DELIMITER = ":";
+
+    String AUTHORIZATION = "Authorization";
+
+    public AuthInfo extract(HttpServletRequest request) {
+        String header = request.getHeader(AUTHORIZATION);
+
+        System.out.println(header);
+        if (header == null) {
+            return null;
+        }
+
+        if ((header.toLowerCase().startsWith(BASIC_TYPE.toLowerCase()))) {
+            String authHeaderValue = header.substring(BASIC_TYPE.length()).trim();
+            byte[] decodedBytes = Base64.decodeBase64(authHeaderValue);
+            String decodedString = new String(decodedBytes);
+
+            String[] credentials = decodedString.split(DELIMITER);
+            String email = credentials[0];
+            String password = credentials[1];
+
+            return new AuthInfo(email, password);
+        }
+
+        return null;
+    }
+
+
+}
