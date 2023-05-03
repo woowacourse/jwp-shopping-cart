048ef95c4f2508f80c7e2591b6c9f27472408dcd
kevstevie <jianna442@gmail.com>
2023-05-03 14:53:55 +0900

    refactor: responseEntityExceptionHandler 재적용

--- src/main/java/cart/controller/ExceptionController.java
+++ src/main/java/cart/controller/ExceptionController.java
@@ -6,9 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
-public class ExceptionController {
+public class ExceptionController extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handleProductNotFoundException(final ProductNotFoundException exception) {
