09704023e817a703040d9496561596c7be037501
jyeost <pig9619@naver.com>
2023-05-04 00:35:28 +0900

    feat: 초기 데이터 셋팅

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,6 +6,15 @@ CREATE TABLE IF NOT EXISTS PRODUCT
     IMAGE TEXT NOT NULL
 );
 
+INSERT INTO PRODUCT (NAME , PRICE, IMAGE)
+VALUES ('빙글빙글 케로로', 111111, 'https://mblogthumb-phinf.pstatic.net/MjAyMDA3MjJfMTcg/MDAxNTk1Mzg1Mzc4NDQ2.PweOjnlVywxRQO-OZIGFv9zVm1pzfTzxy-pGmYpT8pQg.AfGRhviDSKCoPIDG0CCXaugvYvLrPSMkr2fWzXT33ywg.GIF.mymetanight/%EB%B9%99%EA%B8%80%EB%B9%99%EA%B8%80.gif?type=w800');
+
+INSERT INTO PRODUCT (NAME , PRICE, IMAGE)
+VALUES ('파워 케로로', 22222222, 'https://mblogthumb-phinf.pstatic.net/MjAyMDA3MjJfOTcg/MDAxNTk1Mzg1NDUyMjY4.38gAyyC3t6JQ9cpnlQLMaX40qRsZxtmiKKmAAQS0C8og.Rg1Gy3YjlMy6J_B_ct2-ByTvYNcVOxsk5xbyg0ZLnagg.GIF.mymetanight/%EB%B9%9B%EB%82%98%EB%8A%94_%EB%AA%A8%EC%9E%90.gif?type=w800');
+
+INSERT INTO PRODUCT (NAME , PRICE, IMAGE)
+VALUES ('서브웨이', 33333333, 'https://m.subway.co.kr/upload/menu/Subway-Club%E2%84%A2_20211231095518589.png');
+
 CREATE TABLE IF NOT EXISTS MEMBER
 (
     EMAIL  VARCHAR(255)  NOT NULL PRIMARY KEY,
