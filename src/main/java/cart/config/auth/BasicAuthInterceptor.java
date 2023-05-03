dd226975fadfed597b95a2c13ef7dc49c6eddb5a
HyunSeo Park (Hyena) <goatsfish@gmail.com>
2023-05-03 13:39:40 +0900

    refactor: 인증용 헤더 확인 인터셉터 부모 변경

--- src/main/java/cart/config/auth/BasicAuthInterceptor.java
+++ src/main/java/cart/config/auth/BasicAuthInterceptor.java
@@ -1,24 +1,24 @@
 package cart.config.auth;
 
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Objects;
 
 @Component
-public class BasicAuthInterceptor extends HandlerInterceptorAdapter {
+public class BasicAuthInterceptor implements HandlerInterceptor {
     private static final String HEADER_AUTHORIZATION = "Authorization";
 
     @Override
-    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) {
         final String auth = request.getHeader(HEADER_AUTHORIZATION);
 
         if (Objects.isNull(auth)) {
             throw new BasicAuthException("토큰이 존재하지 않습니다.");
         }
 
-        return super.preHandle(request, response, handler);
+        return true;
     }
 }
