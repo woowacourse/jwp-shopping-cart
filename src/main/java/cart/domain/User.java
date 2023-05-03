0216ec5c24fb5050cba27019700fad7922d8c0e4
0chil <0@chll.it>
2023-05-03 13:20:50 +0900

    feat: 사용자 도메인 모델 객체 추가

---
+++ src/main/java/cart/domain/User.java
@@ -0,0 +1,12 @@
+package cart.domain;
+
+public class User {
+
+    private final String email;
+    private final String password;
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+}
