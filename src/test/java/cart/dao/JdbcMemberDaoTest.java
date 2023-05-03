6691a89e919fb2caa50299428bb053787e42548d
hectick <ch.yeondev@gmail.com>
2023-05-03 14:06:58 +0900

    feat: DB 에서 아이디와 비밀번호가 일치하는 사용자를 조회하는 기능 테스트

--- src/test/java/cart/dao/JdbcMemberDaoTest.java
+++ src/test/java/cart/dao/JdbcMemberDaoTest.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Import;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @Import({JdbcMemberDao.class, MemberInitializer.class})
@@ -44,6 +45,30 @@ class JdbcMemberDaoTest {
             final MemberEntity memberEntity = MemberEntity.of("hihi@email.com", "password3");
 
             assertDoesNotThrow(() -> memberDao.insert(memberEntity));
+        }
+    }
+
+    @Nested
+    @DisplayName("사용자를 이메일과 비밀번호로 조회하는 selectByEmailAndPassword 메서드 테스트")
+    class SelectByEmailAndPasswordTest {
+
+        @DisplayName("존재하는 사용자가 조회 되는지 확인한다")
+        @Test
+        void successTest() {
+            final MemberEntity memberEntity = MemberEntity.of("irene@email.com", "password1");
+            MemberEntity selectedMemberEntity = memberDao.selectByEmailAndPassword(memberEntity);
+            assertAll(
+                    () -> assertThat(selectedMemberEntity.getEmail()).isEqualTo(memberEntity.getEmail()),
+                    () -> assertThat(selectedMemberEntity.getPassword()).isEqualTo(memberEntity.getPassword())
+            );
+        }
+
+        @DisplayName("존재하지 않는 사용자를 조회할 경우 null을 반환하는지 확인한다")
+        @Test
+        void returnNullTest() {
+            final MemberEntity memberEntity = MemberEntity.of("hihi@email.com", "password3");
+
+            assertThat(memberDao.selectByEmailAndPassword(memberEntity)).isNull();
         }
     }
 
