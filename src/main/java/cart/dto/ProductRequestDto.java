d42a1c9a6faf870af93d1414cc881b8891aa8d1f
jaeyeon kim <jakind@naver.com>
2023-05-03 14:39:14 +0900

    refactor : RequestDto 검증을 강화

--- src/main/java/cart/dto/ProductRequestDto.java
+++ src/main/java/cart/dto/ProductRequestDto.java
@@ -4,19 +4,25 @@ import cart.entity.Product;
 import cart.vo.Name;
 import cart.vo.Price;
 import cart.vo.Url;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 
 public class ProductRequestDto {
 
-    @NotNull
+    @Length(min = 1, max = 100)
+    @NotBlank
     private final String name;
 
     @Positive
+    @Range(min = 1, max = 1_000_000_000)
     private final Integer price;
 
-    @NotNull
+    @NotEmpty
+    @NotBlank
     private final String imageUrl;
 
     public ProductRequestDto(String name, Integer price, String imageUrl) {
