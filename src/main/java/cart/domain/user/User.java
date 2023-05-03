c4db3507150006234a98a94e9b4384e4f5101748
junpakPark <junpak.park@gmail.com>
2023-05-03 20:50:26 +0900

    refactor : 불필요한 필드 및 getter 메서드 제거

--- src/main/java/cart/domain/user/User.java
+++ src/main/java/cart/domain/user/User.java
@@ -5,25 +5,20 @@ import java.time.LocalDateTime;
 public class User {
 
 	private final Long id;
-	private final String name;
 	private final String email;
 	private final String password;
-	private final String phoneNumber;
 	private final LocalDateTime createdAt;
 	private final LocalDateTime updatedAt;
 
 	public User(final String email, final String password) {
-		this(null, null, email, password, null, null, null);
+		this(null, email, password, null, null);
 	}
 
-	public User(final Long id, final String name, final String email, final String password, final String phoneNumber,
-		final LocalDateTime createdAt,
+	public User(final Long id, final String email, final String password, final LocalDateTime createdAt,
 		final LocalDateTime updatedAt) {
 		this.id = id;
-		this.name = name;
 		this.email = email;
 		this.password = password;
-		this.phoneNumber = phoneNumber;
 		this.createdAt = createdAt;
 		this.updatedAt = updatedAt;
 	}
@@ -32,20 +27,12 @@ public class User {
 		return id;
 	}
 
-	public String getName() {
-		return name;
-	}
-
 	public String getEmail() {
 		return email;
 	}
 
 	public String getPassword() {
 		return password;
-	}
-
-	public String getPhoneNumber() {
-		return phoneNumber;
 	}
 
 	public LocalDateTime getCreatedAt() {
