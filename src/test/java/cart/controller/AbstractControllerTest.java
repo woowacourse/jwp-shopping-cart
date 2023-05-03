20d9b719acdaa699dfb1df21e2bd4bb62f366506
Be-student <presentsong@naver.com>
2023-05-03 23:28:41 +0900

    fix: sql 실행으로 깨지는 테스트 수정

--- src/test/java/cart/controller/AbstractControllerTest.java
+++ src/test/java/cart/controller/AbstractControllerTest.java
@@ -3,6 +3,7 @@ package cart.controller;
 import cart.repository.user.UserRepository;
 import cart.service.product.ProductCommandService;
 import cart.service.product.ProductQueryService;
+import cart.service.user.UserQueryService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -16,6 +17,8 @@ public abstract class AbstractControllerTest {
     protected ProductQueryService productQueryService;
     @MockBean
     protected ProductCommandService productCommandService;
+    @MockBean
+    protected UserQueryService userQueryService;
     @Autowired
     protected ObjectMapper objectMapper;
     @Autowired
