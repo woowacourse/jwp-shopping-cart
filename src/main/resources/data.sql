c648ffb5a41aca7cce348b0332c96c8399d5159e
kdkdhoho <hkim4410@naver.com>
2023-05-03 15:17:12 +0900

    chore(data.sql): Cart에 해당하는 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,7 @@ VALUES (2, 'userB@woowahan.com', 'passwordB', 'userB', 'address2', 2);
 
 INSERT INTO PRODUCT VALUES (1, 'item1', 1000, 'https://picsum.photos/id/1/565/300');
 INSERT INTO PRODUCT VALUES (2, 'item2', 2000, 'https://picsum.photos/id/2/565/300');
+
+INSERT INTO CART VALUES (1, 1);
+INSERT INTO CART VALUES (1, 2);
+INSERT INTO CART VALUES (2, 2);
