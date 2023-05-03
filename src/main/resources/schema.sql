49bc8aa76f954138c7242a75b92e7934b4374e07
Youngho <cyh6099@gmail.com>
2023-05-03 13:22:00 +0900

    feat : 더미데이터 추가 및 테스트 환경에서 더미데이터 삭제

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+CREATE TABLE product
+(
+    id        BIGINT      NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name      VARCHAR(50) NOT NULL,
+    price     INT         NOT NULL,
+    image_url TEXT        NOT NULL
+);
+
+CREATE TABLE member
+(
+    id       INT AUTO_INCREMENT
+        PRIMARY KEY,
+    email    VARCHAR(255) NOT NULL,
+    password VARCHAR(15)  NOT NULL
+);
+
+CREATE TABLE cart
+(
+    id         INT AUTO_INCREMENT
+        PRIMARY KEY,
+    product_id INT NOT NULL,
+    member_id  INT NOT NULL,
+    CONSTRAINT member_id
+        foreign key (member_id) references member (id),
+    constraint product_id
+        foreign key (product_id) references product (id)
+);
