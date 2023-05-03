f8f5d4d1422232be9fa1a251a9eb97f47207b828
jjongwa <troas96@naver.com>
2023-05-03 22:18:18 +0900

    feat: 장바구니 정보를 저장하기 위한 테이블 생성 쿼리 작성, 더미 데이터 삽입

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,12 @@ values ('dino96@naver.com', 'jjongwa96');
 
 insert into member (email, password)
 values ('jeomxon@gmail.com', 'jeomxon00');
+
+insert into product (name, price, image)
+values ('케로로', 10000, 'https://i.namu.wiki/i/fXDC6tkjS6607gZSXSBdzFq_-12PLPWMcmOddg0dsqRq7Nl30Ek1r23BxxOTiERjGP4eyGmJuVPhxhSpOx2GDw.webp');
+
+insert into product (name, price, image)
+values ('기로로', 20000, 'https://i.namu.wiki/i/DPaC8VuH9-jXjF5Ol4C9HM6T7Dy1ak-7fKpXbMEJUgdMr_YIlZmBbLaEFtenyHhUJiLaNPSorIv7Ly6_9WXRDsNJwJK06xXoy8_jJpf5Kd7e8eIm9N_kLMS5VBqORgAfBVJW6gepvdK7kZkVYZGJ3A.webp');
+
+insert into cartItem(member_id, product_id)
+values (1,1);
