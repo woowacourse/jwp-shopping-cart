587d449d2388c7963546092028ceecd7469ffd1a
jaeyeon kim <jakind@naver.com>
2023-05-03 21:30:25 +0900

    feat : 구현한 Resolver 와 Interceptor 를 적용

---
+++ src/main/java/cart/config/WebMvcConfig.java
@@ -0,0 +1,35 @@
+package cart.config;
+
+import cart.authentication.AuthArgumentResolver;
+import cart.authentication.BasicLoginInterceptor;
+import cart.dao.MemberDao;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final MemberDao memberDao;
+
+    public WebMvcConfig(MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new BasicLoginInterceptor(memberDao))
+                .addPathPatterns("/admin/**")
+                .addPathPatterns("/cart/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthArgumentResolver());
+    }
+
+
+}
