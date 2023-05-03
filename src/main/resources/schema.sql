dd2ce259740d52e67b2de750a96128f70a1bb307
yenawee <yaena0319@naver.com>
2023-05-03 23:52:25 +0900

    feat : cart DB DDL 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,13 +5,27 @@ CREATE TABLE IF NOT EXISTS product
     price      INT          NOT NULL,
     image_url  VARCHAR(2048) NOT NULL,
 
-    PRIMARY KEY (product_id)
+    PRIMARY KEY (product_id),
+    UNIQUE(name)
 );
 
 CREATE TABLE IF NOT EXISTS user_info
 (
+    user_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
 
-    PRIMARY KEY (email)
+    PRIMARY KEY (user_id),
+    UNIQUE(email)
 );
+
+CREATE TABLE IF NOT EXISTS cart
+(
+    cart_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id BIGINT UNSIGNED NOT NULL,
+    product_id BIGINT UNSIGNED NOT NULL,
+
+    PRIMARY KEY (cart_id),
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id),
+    FOREIGN KEY (product_id) REFERENCES product(product_id)
+)
