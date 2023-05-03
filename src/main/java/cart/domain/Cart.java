7845a7498b628239c28c2fc4a3e183f425af4514
Rosie <dev.kayoung@gmail.com>
2023-05-03 09:33:50 +0900

    feat: 카트 도메인

--- src/main/java/cart/domain/Cart.java
+++ src/main/java/cart/domain/Cart.java
@@ -1,20 +1,39 @@
 package cart.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Cart {
-    private final List<CartItem> cartItems;
+    private final User user;
+    private final CartItems cartItems;
 
-    public Cart(List<CartItem> cartItems) {
-        this.cartItems = new ArrayList<>(cartItems);
+    public Cart(User user, CartItems cartItems) {
+        this.user = user;
+        this.cartItems = cartItems;
     }
 
-    public void add(CartItem newCartItem) {
-        cartItems.add(newCartItem);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Cart cart = (Cart) o;
+        return Objects.equals(user, cart.user);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user);
+    }
+
+    public User getUser() {
+        return user;
     }
 
     public List<CartItem> getCartItems() {
-        return new ArrayList<>(cartItems);
+        return cartItems.getCartItems();
     }
 }
--- src/test/java/cart/factory/CartFactory.java
+++ src/test/java/cart/factory/CartFactory.java
@@ -3,13 +3,13 @@ package cart.factory;
 import static cart.factory.ProductFactory.createOtherProduct;
 import static cart.factory.ProductFactory.createProduct;
 
-import cart.domain.Cart;
+import cart.domain.CartItems;
 import cart.domain.CartItem;
 import java.util.List;
 
 public class CartFactory {
-    public static Cart createCart() {
+    public static CartItems createCart() {
         List<CartItem> cartItems = List.of(new CartItem(createProduct()), new CartItem(createOtherProduct()));
-        return new Cart(cartItems);
+        return new CartItems(cartItems);
     }
 }
