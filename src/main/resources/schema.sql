43d542281b1caba37bff4d355d4bf8d60e0a6f50
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-03 16:44:47 +0900

    feat: CartUser 테이블 및 기본 생성 데이터 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,3 +8,31 @@ CREATE TABLE IF NOT EXISTS product
 
     PRIMARY KEY (product_id)
 );
+
+CREATE TABLE IF NOT EXISTS cart_user
+(
+    cart_user_id  BIGINT UNSIGNED AUTO_INCREMENT,
+    email         VARCHAR(40)  NOT NULL,
+    cart_password VARCHAR(100) NOT NULL,
+
+    PRIMARY KEY (cart_user_id)
+);
+
+CREATE TABLE IF NOT EXISTS cart_user_product
+(
+    cart_user_product_id BIGINT UNSIGNED AUTO_INCREMENT,
+    cart_user_id         BIGINT NOT NULL,
+    product_id           BIGINT NOT NULL,
+
+    PRIMARY KEY (cart_user_product_id)
+);
+
+ALTER TABLE cart_user_product
+    ADD CONSTRAINT cart_user_id
+        FOREIGN KEY (cart_user_id)
+            REFERENCES cart_user (cart_user_id);
+
+ALTER TABLE cart_user_product
+    ADD CONSTRAINT product_id
+        FOREIGN KEY (product_id)
+            REFERENCES product (product_id);
