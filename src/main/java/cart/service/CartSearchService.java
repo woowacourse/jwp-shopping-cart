13ba208a2ecef2081cf960d4f0e92272ecb6a87b
odo27 <odo27@naver.com>
2023-05-03 15:07:44 +0900

    feat: CartSearchService 구현

--- src/main/java/cart/service/CartSearchService.java
+++ src/main/java/cart/service/CartSearchService.java
@@ -3,15 +3,20 @@ package cart.service;
 import java.util.List;
 
 import cart.domain.product.Product;
+import cart.domain.user.Email;
+import cart.repository.CartRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CartSearchService {
 
+    private final CartRepository cartRepository;
+
+    public CartSearchService(final CartRepository cartRepository) {
+        this.cartRepository = cartRepository;
+    }
+
     public List<Product> findByEmail(final String email) {
-        return List.of(
-                new Product(1L, "odo", "url", 1),
-                new Product(2L, "nunu", "url", 1)
-        );
+        return cartRepository.findByEmail(new Email(email));
     }
 }
