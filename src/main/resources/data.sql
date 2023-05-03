da1adfd5ca238e2306ef36fc1582dd0c1a8a4c96
Miseong Kim <miseongk@naver.com>
2023-05-03 16:20:53 +0900

    chore: 초기 세팅 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
+INSERT INTO Product (id, name, price, image_url) VALUES (2, '치킨', 10000, 'https://pelicana.co.kr/resources/images/menu/original_menu01_200529.png');
+INSERT INTO Product (id, name, price, image_url) VALUES (3, '햄버거', 10000, 'https://pelicana.co.kr/resources/images/menu/original_menu01_200529.png');
+INSERT INTO Product (id, name, price, image_url) VALUES (4, '피자', 10000, 'https://pelicana.co.kr/resources/images/menu/original_menu01_200529.png');
+
 INSERT INTO Member (email, password) VALUES ('miseongk@gmail.com', 'miseongk');
 INSERT INTO Member (email, password) VALUES ('miseongk2@gmail.com', 'miseongk2');
+
+INSERT INTO Cart (member_email, product_id) VALUES ('miseongk@gmail.com', 2);
