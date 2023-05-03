dec8fefe553076af72d7337b429a89fd6f2d55f7
hyunseo <ohseo8813@gmail.com>
2023-05-03 11:34:46 +0900

    feat: MemberDao의 findByEmail 추가

--- src/main/java/cart/dao/MemberDao.java
+++ src/main/java/cart/dao/MemberDao.java
@@ -5,13 +5,11 @@ import java.util.List;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class MemberDao {
 
-    private final SimpleJdbcInsert insertActor;
     private final JdbcTemplate jdbcTemplate;
 
     private final RowMapper<Member> rowMapper = (resultSet, rowNum) -> new Member(
@@ -20,14 +18,16 @@ public class MemberDao {
             resultSet.getString("password"));
 
     public MemberDao(DataSource dataSource) {
-        this.insertActor = new SimpleJdbcInsert(dataSource)
-                .withTableName("member")
-                .usingGeneratedKeyColumns("id");
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public List<Member> findAll() {
         String sql = "select * from member";
         return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public Member findByEmail(String email) {
+        String sql = "select * from member where email = ?";
+        return jdbcTemplate.queryForObject(sql, rowMapper, email);
     }
 }
--- src/test/java/cart/dao/MemberDaoTest.java
+++ src/test/java/cart/dao/MemberDaoTest.java
@@ -1,6 +1,8 @@
 package cart.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import cart.domain.Member;
 import java.util.List;
@@ -11,6 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 
@@ -18,6 +21,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class MemberDaoTest {
 
+    private static final String EMAIL = "email@naver.com";
+    private static final String PASSWORD = "password";
     @Autowired
     private DataSource dataSource;
     @Autowired
@@ -30,20 +35,44 @@ class MemberDaoTest {
         memberDao = new MemberDao(dataSource);
     }
 
-    @DisplayName("저장된 모든 유저를 가져온다.")
     @Test
+    @DisplayName("저장된 모든 유저를 가져온다.")
     void find_all() {
         //given
-        saveByEmail("email@naver.com");
-        saveByEmail("email@kakao.com");
+        saveMember(new Member(EMAIL, PASSWORD));
+        saveMember(new Member("nado@kakao.com", "password"));
         //when
         List<Member> actual = memberDao.findAll();
         //then
         assertThat(actual.size()).isEqualTo(2);
     }
 
-    private void saveByEmail(String email) {
+    @Test
+    @DisplayName("이메일을 통해 멤버를 찾는다.")
+    void find_by_email_success() {
+        //given
+        Member given = new Member(EMAIL, PASSWORD);
+        saveMember(given);
+        //when
+        Member actual = memberDao.findByEmail(EMAIL);
+        //then
+        assertAll(
+                () -> assertThat(actual).usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(given),
+                () -> assertThat(actual.getId()).isPositive());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 이메일의 유저를 요청한다.")
+    void find_by_email_fail_by_wrong_email() {
+        //when && then
+        assertThatThrownBy(() -> memberDao.findByEmail(EMAIL))
+                .isInstanceOf(EmptyResultDataAccessException.class);
+    }
+
+    private void saveMember(Member member) {
         String sql = "insert into member(email, password) values (?,?)";
-        jdbcTemplate.update(sql, email, "password");
+        jdbcTemplate.update(sql, member.getEmail(), member.getPassword());
     }
 }
