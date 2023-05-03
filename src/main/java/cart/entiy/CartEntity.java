e2d5703a6bdcefb78632d733b98a8305ea5b8fe1
odo27 <odo27@naver.com>
2023-05-03 21:08:11 +0900

    feat: CartEntity cartId 추가

--- src/main/java/cart/entiy/CartEntity.java
+++ src/main/java/cart/entiy/CartEntity.java
@@ -2,12 +2,22 @@ package cart.entiy;
 
 public class CartEntity {
 
+    private final Long cartId;
     private final String email;
     private final Long productId;
 
     public CartEntity(final String email, final Long productId) {
+        this(null, email, productId);
+    }
+
+    public CartEntity(final Long cartId, final String email, final Long productId) {
+        this.cartId = cartId;
         this.email = email;
         this.productId = productId;
+    }
+
+    public Long getCartId() {
+        return cartId;
     }
 
     public String getEmail() {
