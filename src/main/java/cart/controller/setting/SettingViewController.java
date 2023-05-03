ff5539b26afa66111b0545f0971336a2a648ad0d
Be-student <presentsong@naver.com>
2023-05-03 23:15:41 +0900

    feat: 컨트롤러로 데이터 넘기는 과정 추가

--- src/main/java/cart/controller/setting/SettingViewController.java
+++ src/main/java/cart/controller/setting/SettingViewController.java
@@ -1,13 +1,29 @@
 package cart.controller.setting;
 
+import cart.dto.user.UserResponse;
+import cart.service.user.UserQueryService;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class SettingViewController {
 
+    private final UserQueryService userQueryService;
+
+    public SettingViewController(final UserQueryService userQueryService) {
+        this.userQueryService = userQueryService;
+    }
+
     @GetMapping("/settings")
-    public String getSettingsPage() {
+    public String getSettingsPage(final Model model) {
+        final List<UserResponse> userResponses = userQueryService.findAll()
+                .stream()
+                .map(UserResponse::from)
+                .collect(Collectors.toList());
+        model.addAttribute("members", userResponses);
         return "settings";
     }
 }
