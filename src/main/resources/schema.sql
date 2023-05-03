33805cfe0a1bb36f7877f33ff17791bb4bbaa0b3
woosung1223 <dntjd991223@naver.com>
2023-05-03 16:00:04 +0900

    feat: 장바구니 스키마 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,15 +1,22 @@
 CREATE TABLE PRODUCT (
-    id       INT             NOT NULL AUTO_INCREMENT,
-    name     VARCHAR(10)     NOT NULL,
-    url      VARCHAR(255)    NOT NULL,
-    price    INT             NOT NULL,
+    id          INT             NOT NULL AUTO_INCREMENT,
+    name        VARCHAR(10)     NOT NULL,
+    url         VARCHAR(255)    NOT NULL,
+    price       INT             NOT NULL,
     PRIMARY  KEY (id)
 );
 
 CREATE TABLE MEMBER (
-    id       INT             NOT NULL AUTO_INCREMENT,
-    email    VARCHAR(100)    NOT NULL,
-    password VARCHAR(10)     NOT NULL,
+    id          INT             NOT NULL AUTO_INCREMENT,
+    email       VARCHAR(100)    NOT NULL,
+    password    VARCHAR(10)     NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE CART (
+    id          INT             NOT NULL AUTO_INCREMENT,
+    product_id  INT             NOT NULL,
+    member_id   INT             NOT NULL,
     PRIMARY KEY (id)
 );
 
