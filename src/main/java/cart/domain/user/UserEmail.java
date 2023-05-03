be17a1fb6f8ebe6be811ce9b5899627f9a249ecf
LJW25 <dbjwlee@gmail.com>
2023-05-03 10:44:30 +0900

    feat(User*): 예외 검증 기능 구현

--- src/main/java/cart/domain/user/UserEmail.java
+++ src/main/java/cart/domain/user/UserEmail.java
@@ -1,13 +1,32 @@
 package cart.domain.user;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 public class UserEmail {
 
+    private static final int MIN_EMAIL_LENGTH = 1;
+    private static final int MAX_EMAIL_LENGTH = 32;
+    private static final String EMAIL_LENGTH_ERROR_MESSAGE = "이메일의 길이는 " + MIN_EMAIL_LENGTH + "자 이상 " + MAX_EMAIL_LENGTH + "자 이하입니다.";
+    private static final String EMAIL_PATTERN_ERROR_MESSAGE = "이메일의 형식이 올바르지 않습니다.";
+    private static final String EMAIL_REGEX = "^[a-zA-Z0-9._]+@[a-zA-Z0-9]+(\\.[a-zA-Z0-9]+)+$";
     private final String email;
 
-    public UserEmail(String email) {
+    public UserEmail(final String email) {
+        validate(email);
         this.email = email;
+    }
+
+    private void validate(final String email) {
+        if (email == null) {
+            throw new IllegalArgumentException(EMAIL_LENGTH_ERROR_MESSAGE);
+        }
+        if (email.length() < MIN_EMAIL_LENGTH || MAX_EMAIL_LENGTH < email.length()) {
+            throw new IllegalArgumentException(EMAIL_LENGTH_ERROR_MESSAGE);
+        }
+        if (!Pattern.matches(EMAIL_REGEX, email)) {
+            throw new IllegalArgumentException(EMAIL_PATTERN_ERROR_MESSAGE);
+        }
     }
 
     @Override
@@ -16,10 +35,10 @@ public class UserEmail {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        UserEmail userEmail = (UserEmail) o;
+        final UserEmail userEmail = (UserEmail) o;
         return Objects.equals(email, userEmail.email);
     }
 
--- src/main/java/cart/domain/user/UserPassword.java
+++ src/main/java/cart/domain/user/UserPassword.java
@@ -3,11 +3,24 @@ package cart.domain.user;
 import java.util.Objects;
 
 public class UserPassword {
+    private static final int MIN_EMAIL_LENGTH = 1;
+    private static final int MAX_EMAIL_LENGTH = 32;
+    private static final String PASSWORD_LENGTH_ERROR_MESSAGE = "비밀번호의 길이는 " + MIN_EMAIL_LENGTH + "자 이상 " + MAX_EMAIL_LENGTH + "자 이하입니다.";
 
     private final String password;
 
-    public UserPassword(String password) {
+    public UserPassword(final String password) {
+        validate(password);
         this.password = password;
+    }
+
+    private void validate(final String password) {
+        if (password == null) {
+            throw new IllegalArgumentException(PASSWORD_LENGTH_ERROR_MESSAGE);
+        }
+        if (password.length() < MIN_EMAIL_LENGTH || MAX_EMAIL_LENGTH < password.length()) {
+            throw new IllegalArgumentException(PASSWORD_LENGTH_ERROR_MESSAGE);
+        }
     }
 
     @Override
@@ -16,10 +29,10 @@ public class UserPassword {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        UserPassword that = (UserPassword) o;
+        final UserPassword that = (UserPassword) o;
         return Objects.equals(password, that.password);
     }
 
