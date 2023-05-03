dfbc7c3b4ec79df733644a729fb840f84a094baa
70825 <hello70825@gmail.com>
2023-05-03 16:11:19 +0900

    fix: 클릭 경로 수정

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -8,7 +8,7 @@ const addCartItem = (productId) => {
 
     // TODO: [2단계] 장바구니 CRUD API에 맞게 변경
     axios.request({
-        url: '/cart-products/' + productId,
+        url: '/cart/products/' + productId,
         headers: {
             'Authorization': `Basic ${credentials}`
         },
@@ -30,7 +30,7 @@ const removeCartItem = (id) => {
 
     // TODO: [2단계] 장바구니 CRUD API에 맞게 변경
     axios.request({
-        url: '/cart-products/' + id,
+        url: '/cart/products/' + id,
         headers: {
             'Authorization': `Basic ${credentials}`
         },
