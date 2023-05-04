f3b17924f73407487718616aa41c1ef858729c3a
kiarakim <kiarakim413@gmail.com>
2023-05-04 16:02:45 +0900

    chore: DDL, DML 파일 분리

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,28 @@
+CREATE TABLE IF NOT EXISTS products
+(
+id      BIGINT      NOT NULL AUTO_INCREMENT,
+name    VARCHAR(10) NOT NULL,
+price   DOUBLE      NOT NULL,
+image   TEXT        NOT NULL,
+
+PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS members
+(
+id          BIGINT      NOT NULL AUTO_INCREMENT,
+name        VARCHAR(10) NOT NULL,
+email       VARCHAR(30) NOT NULL,
+password    VARCHAR(16) NOT NULL,
+
+PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS carts
+(
+id          BIGINT NOT NULL AUTO_INCREMENT,
+memberId    BIGINT NOT NULL,
+productId   BIGINT NOT NULL,
+
+PRIMARY KEY(id)
+);
