500e760a046a452df799746232ce29ca58e7cab4
Youngho <cyh6099@gmail.com>
2023-05-04 15:36:56 +0900

    test : 더미데이터 제거

--- src/test/java/cart/dao/H2MemberDaoTest.java
+++ src/test/java/cart/dao/H2MemberDaoTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @SuppressWarnings("NonAsciiCharacters")
 @JdbcTest
+@Sql("/test.sql")
 public class H2MemberDaoTest {
 
     @Autowired
@@ -39,11 +41,11 @@ public class H2MemberDaoTest {
 
     @Test
     public void 전체_멤버를_조회한다() {
-//        Member member1 = new Member("cyh6099@gmail.com", "qwer1234");
-//        Member member2 = new Member("cyh6099@wooteco.com", "qwer1234");
-//
-//        memberDao.save(member1);
-//        memberDao.save(member2);
+        Member member1 = new Member("cyh6099@gmail.com", "qwer1234");
+        Member member2 = new Member("cyh6099@wooteco.com", "qwer1234");
+
+        memberDao.save(member1);
+        memberDao.save(member2);
 
         assertThat(memberDao.findAll()).hasSize(2);
     }
