6ca50d5936927d606daf63cfa398e71703329d39
JunDonghyuk <ehdgur4814@naver.com>
2023-05-04 13:53:44 +0900

    feat: 태스트 yml 및 테스트 테이블 설계

---
+++ src/test/resources/testSchema.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS PRODUCT (
+                                       id          BIGINT                            NOT NULL AUTO_INCREMENT,
+                                       name        VARCHAR(50)                       NOT NULL,
+    image_url   VARCHAR(200)                      NOT NULL,
+    price       INT                               NOT NULL,
+    PRIMARY KEY (id)
+    );
+
+CREATE TABLE IF NOT EXISTS MEMBER (
+                                      id           BIGINT                           NOT NULL AUTO_INCREMENT,
+                                      email        VARCHAR(50)                      NOT NULL,
+    name         VARCHAR(50)                      NOT NULL,
+    password     VARCHAR(100)                     NOT NULL,
+    PRIMARY KEY  (id),
+    UNIQUE KEY   member_email(email)
+    );
+
+CREATE TABLE IF NOT EXISTS CART (
+                                    id           BIGINT                           NOT NULL AUTO_INCREMENT,
+                                    member_id    BIGINT                           NOT NULL,
+                                    product_id   BIGINT                           NOT NULL,
+                                    PRIMARY KEY (id)
+);
