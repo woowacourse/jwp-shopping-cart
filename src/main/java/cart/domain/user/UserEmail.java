e0281155144c7b22669231d751b3df5dc7eb2605
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-05-05 01:05:46 +0900

    feat: 값 객체 동등성 추가

--- src/main/java/cart/domain/user/UserEmail.java
+++ src/main/java/cart/domain/user/UserEmail.java
@@ -1,5 +1,6 @@
 package cart.domain.user;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class UserEmail {
@@ -23,6 +24,23 @@ public class UserEmail {
 
     public static UserEmail from(final String email) {
         return new UserEmail(email);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final UserEmail userEmail = (UserEmail) o;
+        return Objects.equals(getEmail(), userEmail.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getEmail());
     }
 
     public String getEmail() {
