d481aeeeb369f1767f6b66a307abf562014a460d
SproutMj <mjk54565@gmail.com>
2023-05-04 23:04:12 +0900

    test: IndexControllerTest 작성

---
+++ src/test/java/cart/controller/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package cart.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IndexControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("장바구니 페이지 접속")
+    @Test
+    void cartPage() {
+        RestAssured.given().log().all()
+                .when().get("/")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
