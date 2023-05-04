78c13d0aba8b84a22f38c8ed51d1e8ca91b25f4b
xxeol2 <jhj01177@naver.com>
2023-05-04 15:51:56 +0900

    test: Member/Cart Dao 테스트 작성

---
+++ src/test/java/cart/dao/JdbcCartDaoTest.java
@@ -0,0 +1,100 @@
+package cart.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import cart.domain.cart.Cart;
+import cart.domain.member.Member;
+import cart.domain.product.Product;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+@JdbcTest
+class JdbcCartDaoTest {
+
+    private final RowMapper<Cart> cartRowMapper = (resultSet, rowNum) ->
+            new Cart(
+                    resultSet.getLong("id"),
+                    resultSet.getLong("member_id"),
+                    resultSet.getLong("product_id")
+            );
+
+    private JdbcMemberDao jdbcMemberDao;
+    private JdbcProductDao jdbcProductDao;
+    private JdbcCartDao jdbcCartDao;
+
+    public JdbcCartDaoTest(@Autowired final JdbcTemplate jdbcTemplate) {
+        this.jdbcMemberDao = new JdbcMemberDao(jdbcTemplate);
+        this.jdbcProductDao = new JdbcProductDao(jdbcTemplate);
+        this.jdbcCartDao = new JdbcCartDao(jdbcTemplate);
+    }
+
+    @Test
+    @DisplayName("Cart 삽입")
+    void insert() {
+        // given
+        Long memberId = jdbcMemberDao.insert(new Member("test@test.com", "password1", "애쉬"));
+        Long productId = jdbcProductDao.insert(new Product("피자", 1000, null));
+
+        // when
+        Long cartId = jdbcCartDao.insert(new Cart(memberId, productId));
+
+        // then
+        assertThat(cartId).isPositive();
+    }
+
+    @Test
+    @DisplayName("멤버별 Cart 조회")
+    void findAllByMemberId() {
+        // given
+        Long memberId = jdbcMemberDao.insert(new Member("test@test.com", "password1", "조개소년"));
+        Long memberId2 = jdbcMemberDao.insert(new Member("test2@test.com", "password1", "비버"));
+        Long productId = jdbcProductDao.insert(new Product("피자", 1000, null));
+        Long productId2 = jdbcProductDao.insert(new Product("햄버거", 2000, null));
+        jdbcCartDao.insert(new Cart(memberId, productId));
+        jdbcCartDao.insert(new Cart(memberId, productId2));
+        jdbcCartDao.insert(new Cart(memberId2, productId));
+
+        // when
+        List<Cart> carts = jdbcCartDao.findAllByMemberId(memberId);
+
+        // then
+        assertAll(
+                () -> assertThat(carts.size()).isEqualTo(2),
+                () -> assertThat(carts).extracting("memberId").containsExactly(memberId, memberId),
+                () -> assertThat(carts).extracting("productId").containsExactlyInAnyOrder(productId, productId2)
+        );
+    }
+
+    @Test
+    @DisplayName("멤버와 상품 id로 Cart 조회")
+    void findByMemberIdAndProductId() {
+        // given
+        Long memberId = jdbcMemberDao.insert(new Member("test@test.com", "password1", "조개소년"));
+        Long productId = jdbcProductDao.insert(new Product("피자", 1000, null));
+        jdbcCartDao.insert(new Cart(memberId, productId));
+
+        // when
+        Optional<Cart> cart = jdbcCartDao.findByMemberIdAndProductId(memberId, productId);
+
+        // then
+        assertAll(
+                () -> assertThat(cart).isPresent(),
+                () -> assertThat(cart.get())
+                        .usingRecursiveComparison()
+                        .ignoringFields("id")
+                        .isEqualTo(new Cart(memberId, productId))
+        );
+    }
+
+    @Test
+    @DisplayName("Name")
+    void t() {
+    }
+}
--- src/test/java/cart/dao/JdbcMemberDaoTest.java
+++ src/test/java/cart/dao/JdbcMemberDaoTest.java
@@ -1,8 +1,10 @@
 package cart.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import cart.domain.member.Member;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +30,34 @@ class JdbcMemberDaoTest {
     }
 
     @Test
-    @DisplayName("Member 조회 테스트")
-    void findAllTest() {
+    @DisplayName("Member 삽입")
+    void insert() {
+        Long id = jdbcMemberDao.insert(new Member("h@h.com", "password1", "홍길동"));
+        assertThat(id).isPositive();
+    }
+
+    @Test
+    @DisplayName("Member 조회")
+    void findAll() {
+        jdbcMemberDao.insert(new Member("m@h.com", "password1", "무민"));
+        jdbcMemberDao.insert(new Member("t@h.com", "password2", "누누"));
+        jdbcMemberDao.insert(new Member("n@h.com", "password3", "테오"));
+
         assertThat(jdbcMemberDao.findAll()).extracting("name")
-                .containsExactly("이오", "애쉬");
+                .contains("무민", "누누", "테오");
+    }
+
+    @Test
+    @DisplayName("이메일과 비밀번호로 Member 조회")
+    void findByEmailAndPassword() {
+        String email = "test@test.com";
+        String password = "testPassword";
+        jdbcMemberDao.insert(new Member(email, password, "홍길동"));
+
+        Optional<Member> member = jdbcMemberDao.findByEmailAndPassword(email, password);
+        assertAll(
+                () -> assertThat(member).isPresent(),
+                () -> assertThat(member.get()).extracting("name").isEqualTo("홍길동")
+        );
     }
 }
