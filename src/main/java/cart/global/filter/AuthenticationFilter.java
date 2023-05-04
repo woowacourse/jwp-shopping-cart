7f77b2e912a965c937491b0e1ac6b0ed6649655e
java-saeng <gusdn85554@naver.com>
2023-05-04 19:28:20 +0900

    refactor : 메서드 extract 및 exception message 반환

--- src/main/java/cart/global/filter/AuthenticationFilter.java
+++ src/main/java/cart/global/filter/AuthenticationFilter.java
@@ -1,7 +1,9 @@
 package cart.global.filter;
 
+import cart.global.exception.auth.CanNotFoundHeaderException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.util.PatternMatchUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -10,6 +12,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 public class AuthenticationFilter extends OncePerRequestFilter {
 
@@ -22,20 +26,43 @@ public class AuthenticationFilter extends OncePerRequestFilter {
 
         final String requestURI = request.getRequestURI();
 
-        if (hasHeaderCheckPath(requestURI)) {
-
-            final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
-
-            if (header == null) {
-                response.setStatus(HttpStatus.UNAUTHORIZED.value());
-                return;
-            }
+        if (isNotHeaderCheckPath(requestURI)) {
+            filterChain.doFilter(request, response);
         }
 
-        filterChain.doFilter(request, response);
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        try {
+            validateHeader(header);
+
+            filterChain.doFilter(request, response);
+        } catch (CanNotFoundHeaderException exception) {
+            writeErrorResponse(response, exception);
+        }
     }
 
-    private boolean hasHeaderCheckPath(final String requestURI) {
-        return PatternMatchUtils.simpleMatch(whiteList, requestURI);
+    private void writeErrorResponse(final HttpServletResponse response,
+                                    final CanNotFoundHeaderException exception) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        final PrintWriter printWriter = response.getWriter();
+        printWriter.write(exception.getMessage());
+        printWriter.flush();
+    }
+
+    private void validateHeader(final String header) {
+        if (hasNotHeader(header)) {
+            throw new CanNotFoundHeaderException();
+        }
+    }
+
+    private boolean hasNotHeader(final String header) {
+        return header == null;
+    }
+
+    private boolean isNotHeaderCheckPath(final String requestURI) {
+        return !PatternMatchUtils.simpleMatch(whiteList, requestURI);
     }
 }
