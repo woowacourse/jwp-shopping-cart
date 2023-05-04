732868f1ca6716c89c33e0e7ca8666d09a7f4f71
kokodak <kokodakadokok@gmail.com>
2023-05-04 17:24:35 +0900

    feat: 서비스 로직에 대해 트랜잭션 보장이 되도록 수정

--- src/main/java/cart/service/MemberService.java
+++ src/main/java/cart/service/MemberService.java
@@ -5,7 +5,9 @@ import cart.repository.dao.MemberDao;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @Service
 public class MemberService {
 
