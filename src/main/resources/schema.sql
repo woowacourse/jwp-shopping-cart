308332bbd8228e61af4ecd0062ed795de43c214b
kokodak <kokodakadokok@gmail.com>
2023-05-04 14:00:21 +0900

    feat: 사용자와 장바구니에 대한 DDL 코드 작성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,20 @@ CREATE TABLE IF NOT EXISTS product
     price      INT         NOT NULL,
     PRIMARY KEY (product_id)
 );
+
+CREATE TABLE IF NOT EXISTS member
+(
+    member_id BIGINT       NOT NULL AUTO_INCREMENT,
+    name      VARCHAR(50)  NOT NULL,
+    email     VARCHAR(255) NOT NULL,
+    password  VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (member_id)
+);
+
+CREATE TABLE IF NOT EXISTS cart
+(
+    cart_id    BIGINT NOT NULL AUTO_INCREMENT,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (cart_id)
+);
