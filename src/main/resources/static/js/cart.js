8a77cb167ba1f22e3783d38079f6c48133d3a5c2
wugawuga <dnr3655@naver.com>
2023-05-04 21:32:10 +0900

    refactor: member id 추가, url 수정

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -27,9 +27,9 @@ const removeCartItem = (id) => {
         return;
     }
 
-    // TODO: [2단계] 장바구니 CRUD API에 맞게 변경
     axios.request({
-        url: '',
+        url: '/carts/' + id,
+        method: 'DELETE',
         headers: {
             'Authorization': `Basic ${credentials}`
         }
--- src/main/resources/static/js/settings.js
+++ src/main/resources/static/js/settings.js
@@ -1,6 +1,6 @@
 const selectMember = (member) => {
-    const {email, password} = member;
-    const string = `${email}:${password}`;
+    const {id, email, password} = member;
+    const string = `${id}:${email}:${password}`;
     localStorage.setItem('credentials', btoa(string));
     alert(`${email} 사용자로 설정 했습니다.`);
 }
