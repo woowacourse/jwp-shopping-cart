78d5d1a52d7fd9576fdbf5b593d2dc8f17f9b9f3
iamjooon2 <iamjooon2@gmail.com>
2023-05-05 05:24:41 +0900

    chore: 테스트 더미 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO MEMBER (email, password) VALUES ('a@a.com', '1234');
 INSERT INTO MEMBER (email, password) VALUES ('b@b.com', '1234');
 
-INSERT INTO PRODUCT (name, price, image) VALUES ('가비', 1000, 'https://cdn.spotvnews.co.kr/news/photo/202211/566369_791169_1130.jpg가비사진');
-INSERT INTO PRODUCT (name, price, image) VALUES ('후추', 1000, 'https://ottogi.co.kr/pds/product/2017-05-17_484367826[1].jpg');
-INSERT INTO PRODUCT (name, price, image) VALUES ('메리', 1000, 'https://img.danawa.com/prod_img/500000/824/964/img/9964824_1.jpg?shrink=330:*&_v=20191205120907');
+INSERT INTO PRODUCT (name, price, image) VALUES ('가비', 1000, 'https://cdn.spotvnews.co.kr/news/photo/202211/566369_791169_1130.jpg');
+INSERT INTO PRODUCT (name, price, image) VALUES ('후추', 2000, 'https://ottogi.co.kr/pds/product/2017-05-17_484367826[1].jpg');
+INSERT INTO PRODUCT (name, price, image) VALUES ('메리', 3000, 'https://img.danawa.com/prod_img/500000/824/964/img/9964824_1.jpg?shrink=330:*&_v=20191205120907');
