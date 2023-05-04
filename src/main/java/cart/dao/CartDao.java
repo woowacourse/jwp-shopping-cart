196a76877846956e45748d7d75a973ca36c92a20
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-05-04 15:29:47 +0900

    feat: MemberId를 기준으로 cart에 저장된 값을 조회하는 기능 구현

--- src/main/java/cart/dao/CartDao.java
+++ src/main/java/cart/dao/CartDao.java
@@ -1,8 +1,13 @@
 package cart.dao;
 
+import cart.entity.Cart;
 import cart.entity.PutCart;
+
+import java.util.List;
 
 public interface CartDao {
 
     void save(PutCart putCart);
+
+    List<Cart> findByMemberId(Long id);
 }
--- src/main/java/cart/dao/JdbcCartDao.java
+++ src/main/java/cart/dao/JdbcCartDao.java
@@ -1,10 +1,13 @@
 package cart.dao;
 
+import cart.entity.Cart;
 import cart.entity.Item;
 import cart.entity.PutCart;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class JdbcCartDao implements CartDao {
@@ -20,5 +23,22 @@ public class JdbcCartDao implements CartDao {
         String sql = "insert into cart(member_id, item_id) values(?, ?)";
 
         jdbcTemplate.update(sql, putCart.getMemberId(), putCart.getProductId());
+    }
+
+    @Override
+    public List<Cart> findByMemberId(Long id) {
+        String sql = "select id, member_id, item_id where member_id = ?";
+
+        return jdbcTemplate.query(sql, mapRow());
+    }
+
+    private RowMapper<Cart> mapRow() {
+        return (rs, rowNum) -> {
+            Long id = rs.getLong(1);
+            Long memberId = rs.getLong(2);
+            Long itemId = rs.getLong(3);
+
+            return new Cart(id, memberId, itemId);
+        };
     }
 }
