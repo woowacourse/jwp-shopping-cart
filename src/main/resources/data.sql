e685c190ddd15eb469393b6d3929abb1bce773c5
mcodnjs <codnjs8989@gmail.com>
2023-05-04 09:33:56 +0900

    feat: cart 테이블 및 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,7 +7,7 @@ CREATE TABLE product
     PRIMARY KEY (id)
 );
 
-CREATE TABLE users
+CREATE TABLE member
 (
     id       BIGINT       NOT NULL AUTO_INCREMENT,
     email    VARCHAR(255) NOT NULL,
@@ -15,8 +15,34 @@ CREATE TABLE users
     PRIMARY KEY (id)
 );
 
-INSERT INTO users (email, password)
-VALUES ('a@a.com', 'password1');
-INSERT INTO users (email, password)
-VALUES ('b@b.com', 'password2');
+CREATE TABLE cart
+(
+    id         BIGINT NOT NULL AUTO_INCREMENT,
+    member_id  BIGINT NOT NULL,
+    product_id BIGINT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (member_id) references member (id),
+    FOREIGN KEY (product_id) references product (id)
+);
 
+INSERT INTO product (id, name, image_url, price)
+VALUES ('1',
+        '치킨',
+        'https://pelicana.co.kr/resources/images/menu/best_menu02_200824.jpg',
+        1000);
+
+INSERT INTO product (id, name, image_url, price)
+VALUES ('2',
+        '피자',
+        'https://grepp-programmers.s3.ap-northeast-2.amazonaws.com/files/production/d440b8f4-91c3-4272-8a81-876e9aaffb9c/RisingStarGraphBox.jpg',
+        10000);
+
+INSERT INTO member (id, email, password)
+VALUES ('1', 'a@a.com', 'password1');
+INSERT INTO member (id, email, password)
+VALUES ('2', 'b@b.com', 'password2');
+
+INSERT INTO cart (member_id, product_id)
+VALUES ('1', '1');
+INSERT INTO cart (member_id, product_id)
+VALUES ('1', '2');
