3d7d974b1991d89980209941bb26532cb7a72600
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-04 15:46:26 +0900

    feat: UserDao 전체 및 특정 user 조회 기능 추가

--- src/main/java/cart/dao/UserDao.java
+++ src/main/java/cart/dao/UserDao.java
@@ -1,16 +1,24 @@
 package cart.dao;
 
 import cart.domain.User;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class UserDao {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final RowMapper<User> actorRowMapper = (resultSet, rowNumber) -> new User(
+            resultSet.getString("email"),
+            resultSet.getString("password")
+    );
 
     public UserDao(final NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
@@ -22,5 +30,16 @@ public class UserDao {
         KeyHolder keyHolder = new GeneratedKeyHolder();
         namedParameterJdbcTemplate.update(sql, params, keyHolder);
         return (Long) keyHolder.getKey();
+    }
+
+    public List<User> findAll() {
+        final String sql = "SELECT id, email, password FROM users ";
+        return namedParameterJdbcTemplate.query(sql, actorRowMapper);
+    }
+
+    public User findBy(final Long userId) {
+        final String sql = "SELECT id, email, password FROM users WHERE id = :id";
+        MapSqlParameterSource param = new MapSqlParameterSource("id", userId);
+        return namedParameterJdbcTemplate.queryForObject(sql, param, actorRowMapper);
     }
 }
