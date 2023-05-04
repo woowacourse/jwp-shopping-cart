eb56cc0e2f878282168f9c046d7ed038f999d570
SproutMj <mjk54565@gmail.com>
2023-05-04 16:34:55 +0900

    test: AuthService 테스트 작성

---
+++ src/test/java/cart/service/AuthServiceTest.java
@@ -0,0 +1,56 @@
+package cart.service;
+
+import cart.controller.dto.request.LoginRequest;
+import cart.controller.dto.response.UserResponse;
+import cart.database.dao.UserDao;
+import cart.entity.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class AuthServiceTest {
+
+    private AuthService authService;
+    @Mock
+    private UserDao userDao;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        authService = new AuthService(userDao);
+    }
+
+    @DisplayName("로그인 정보 확인 테스트")
+    @Test
+    public void basicLogin() {
+        //given
+        String email = "test@example.com";
+        String password = "password";
+        UserEntity userEntity = new UserEntity(1L, email, password);
+        LoginRequest request = new LoginRequest(email, password);
+
+        when(userDao.findByEmailAndPassword(email, password)).thenReturn(userEntity);
+
+        UserResponse expectedResponse = new UserResponse(userEntity.getId(), email, password);
+
+        //when
+        UserResponse actualResponse = authService.basicLogin(request);
+
+        //then
+        assertAll(
+                () -> assertEquals(expectedResponse.getId(), actualResponse.getId()),
+                () -> assertEquals(expectedResponse.getEmail(), actualResponse.getEmail()),
+                () -> assertEquals(expectedResponse.getPassword(), actualResponse.getPassword())
+        );
+
+        verify(userDao).findByEmailAndPassword(email, password);
+    }
+}
+
