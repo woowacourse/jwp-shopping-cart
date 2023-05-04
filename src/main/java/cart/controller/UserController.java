f885d8106691e7e16ecd26e56661eb5f4bc109ba
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-04 15:47:03 +0900

    feat: UserController에 GET /user/{userId} 요청 매핑

--- src/main/java/cart/controller/UserController.java
+++ src/main/java/cart/controller/UserController.java
@@ -1,16 +1,16 @@
 package cart.controller;
 
 import cart.controller.dto.UserRequest;
+import cart.controller.dto.UserResponse;
 import cart.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("user")
@@ -28,5 +28,21 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED)
                              .location(URI.create("/settings"))
                              .build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> loadAllUser() {
+        List<UserResponse> users = userService.loadAllUser();
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body(users);
+    }
+
+    @GetMapping("{userId}")
+    public ResponseEntity<UserResponse> loadUser(@PathVariable final Long userId) {
+        UserResponse user = userService.loadUser(userId);
+        return ResponseEntity.status(HttpStatus.OK)
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body(user);
     }
 }
