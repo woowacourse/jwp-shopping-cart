958f10426770fd62095fd3ad795f0dfd132dec86
jeonghoon <shb03323@naver.com>
2023-05-04 14:11:24 +0900

    refactor: data, schema 분리

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,26 +1,3 @@
-CREATE TABLE IF NOT EXISTS users (
-    id          INT           AUTO_INCREMENT,
-    email       VARCHAR(100)  NOT NULL,
-    password    VARCHAR       NOT NULL,
-    name        VARCHAR(20)   DEFAULT '익명의 사용자',
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS product (
-    id       INT           AUTO_INCREMENT,
-    name     VARCHAR(20)   NOT NULL,
-    image    VARCHAR       NOT NULL,
-    price    INT           NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS cart (
-    id          INT       AUTO_INCREMENT,
-    user_id     INT       NOT NULL,
-    product_id  INT       NOT NULL,
-    PRIMARY KEY (id)
-);
-
 INSERT INTO users(email, password, name) VALUES ('ditoo@wooteco.com', 'ditoo1234', '디투'),
                          ('barrel@wooteco.com', 'barrel1234', '배럴'),
                          ('brown@wooteco.com', 'brown1234', '브라운');
