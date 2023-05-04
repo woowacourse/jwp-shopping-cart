0ca7a3f0b80fc08e4201ac21c9f8bfb841ccd605
cpot5620 <=cpot5620@naver.com>
2023-05-04 15:28:33 +0900

    feat(DB): 특정 회원 조회 기능 구현

--- src/main/java/cart/dao/user/UserDao.java
+++ src/main/java/cart/dao/user/UserDao.java
@@ -1,15 +1,25 @@
 package cart.dao.user;
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class UserDao {
+    private static final RowMapper<UserEntity> rowMapper =
+            (rs, rowNum) -> new UserEntity(
+                    rs.getLong("user_id"),
+                    rs.getString("email"),
+                    rs.getString("password")
+            );
+
     private final JdbcTemplate jdbcTemplate;
     private final SimpleJdbcInsert simpleInsert;
 
@@ -29,11 +39,24 @@ public class UserDao {
     public List<UserEntity> findAll() {
         String findAllQuery = "SELECT * FROM user";
 
-        return jdbcTemplate.query(findAllQuery, (rs, rowNum) ->
-                new UserEntity(
-                        rs.getLong("user_id"),
-                        rs.getString("email"),
-                        rs.getString("password")
-                ));
+        return jdbcTemplate.query(findAllQuery, rowMapper);
+    }
+
+    public Optional<UserEntity> findById(Long id) {
+        String findByIdQuery = "SELECT * FROM user WHERE user_id = ?";
+
+        try {
+            UserEntity userEntity = jdbcTemplate.queryForObject(findByIdQuery, rowMapper, id);
+
+            return Optional.of(userEntity);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    public void deleteById(Long id) {
+        String deleteByIdQuery = "DELETE FROM user WHERE user_id = ?";
+
+        jdbcTemplate.update(deleteByIdQuery, id);
     }
 }
