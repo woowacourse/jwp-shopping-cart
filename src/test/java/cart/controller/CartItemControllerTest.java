7917eb1490b4f63f3b1cd2fcf4bb3f0393cbcf2f
hectick <ch.yeondev@gmail.com>
2023-05-04 17:22:45 +0900

    test: 상품 가격 테스트

--- src/test/java/cart/controller/CartItemControllerTest.java
+++ src/test/java/cart/controller/CartItemControllerTest.java
@@ -1,15 +1,11 @@
 package cart.controller;
 
-import cart.dto.CartItemCreationDto;
 import cart.dto.request.CartItemCreationRequest;
-import cart.dto.request.ProductCreationRequest;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -107,6 +103,37 @@ class CartItemControllerTest {
             RestAssured.given().log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                     .when().get("/cart/cart-items")
+                    .then().log().all()
+                    .statusCode(HttpStatus.UNAUTHORIZED.value());
+        }
+    }
+
+    @Nested
+    @DisplayName("장바구니에 상품을 삭제하는 deleteCartItems 메서드 테스트")
+    class DeleteTest {
+
+        @BeforeEach
+        void setUp() {
+            RestAssured.port = port;
+        }
+
+        @DisplayName("정상 삭제가 되었다면 상태코드 201을 반환하는지 확인한다")
+        @Test
+        void successTest() {
+            RestAssured.given().log().all()
+                    .auth().preemptive().basic(EMAIL, PASSWORD)
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().delete("/cart/cart-items/1")
+                    .then().log().all()
+                    .statusCode(HttpStatus.NO_CONTENT.value());
+        }
+
+        @DisplayName("사용자 설정을 하지 않고 장바구니를 삭제하려하면 상태코드 401을 반환하는지 확인한다")
+        @Test
+        void failTest() {
+            RestAssured.given().log().all()
+                    .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .when().delete("/cart/cart-items/1")
                     .then().log().all()
                     .statusCode(HttpStatus.UNAUTHORIZED.value());
         }
