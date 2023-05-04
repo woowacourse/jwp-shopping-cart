8b73b6ecfc7ec9cc873fe8234ed4a8dae11fe3ad
SproutMj <mjk54565@gmail.com>
2023-05-04 13:52:57 +0900

    refactor: existById 테스트 추가 및 테스트 방식 변경에 따른 수정

--- src/test/java/cart/database/dao/ProductDaoTest.java
+++ src/test/java/cart/database/dao/ProductDaoTest.java
@@ -3,7 +3,6 @@ package cart.database.dao;
 import cart.controller.dto.request.ProductCreateRequest;
 import cart.controller.dto.request.ProductUpdateRequest;
 import cart.convertor.ProductEntityConvertor;
-import cart.database.dao.ProductDao;
 import cart.entity.ProductEntity;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -22,115 +21,138 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 @JdbcTest
 class ProductDaoTest {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-	private ProductEntityConvertor productEntityConvertor = new ProductEntityConvertor();
-	private ProductDao productDao;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    private ProductEntityConvertor productEntityConvertor = new ProductEntityConvertor();
+    private ProductDao productDao;
+    private String name;
+    private String imageUrl;
+    private int price;
 
-	@BeforeEach
-	void setUp () {
-		this.productDao = new ProductDao(jdbcTemplate);
-	}
+    @BeforeEach
+    void setUp() {
+        this.productDao = new ProductDao(jdbcTemplate);
 
-	@DisplayName("상품 생성 쿼리")
-	@Test
-	void create () {
-		// given
-		ProductCreateRequest request = new ProductCreateRequest("product", 5000, "image url");
+        name = "temp";
+        imageUrl = "temp_img";
+        price = 1000;
 
-		// when
-		productDao.create(productEntityConvertor.dtoToEntity(request));
+        jdbcTemplate.update(
+                "INSERT INTO PRODUCT (NAME, IMAGE_URL, PRICE) VALUES (?, ?, ?)",
+                name, imageUrl, price
+        );
+    }
 
-		// then
-		List<ProductEntity> responses = productDao.findAll();
-		assertAll(
-				() -> assertThat(responses).hasSize(1),
-				() -> assertThat(responses.get(0).getName()).isEqualTo("product"),
-				() -> assertThat(responses.get(0).getPrice()).isEqualTo(5000)
-		);
-	}
+    @DisplayName("상품 생성 쿼리")
+    @Test
+    void create() {
+        // given
+        ProductCreateRequest request = new ProductCreateRequest("product", 5000, "image url");
 
-	@DisplayName("상품 전체 조회 쿼리")
-	@Test
-	void findAll () {
-		// given
-		ProductCreateRequest request1 = new ProductCreateRequest("product1", 5000, "image url");
-		ProductCreateRequest request2 = new ProductCreateRequest("product2", 6000, "image url");
-		ProductCreateRequest request3 = new ProductCreateRequest("product3", 7000, "image url");
-		productDao.create(productEntityConvertor.dtoToEntity(request1));
-		productDao.create(productEntityConvertor.dtoToEntity(request2));
-		productDao.create(productEntityConvertor.dtoToEntity(request3));
+        // when
+        productDao.create(productEntityConvertor.dtoToEntity(request));
 
-		// when
-		List<ProductEntity> responses = productDao.findAll();
+        // then
+        List<ProductEntity> responses = productDao.findAll();
+        assertAll(
+                () -> assertThat(responses).hasSize(2),
+                () -> assertThat(responses.get(1).getName()).isEqualTo("product"),
+                () -> assertThat(responses.get(1).getPrice()).isEqualTo(5000)
+        );
+    }
 
-		// then
-		assertAll(
-				() -> assertThat(responses).hasSize(3),
-				() -> assertThat(responses).extracting(ProductEntity::getName)
-						.contains("product1", "product2", "product3"),
-				() -> assertThat(responses).extracting(ProductEntity::getPrice)
-						.contains(5000, 6000, 7000)
-		);
-	}
+    @DisplayName("상품 전체 조회 쿼리")
+    @Test
+    void findAll() {
+        // given
+        ProductCreateRequest request1 = new ProductCreateRequest("product1", 5000, "image url");
+        ProductCreateRequest request2 = new ProductCreateRequest("product2", 6000, "image url");
+        ProductCreateRequest request3 = new ProductCreateRequest("product3", 7000, "image url");
+        productDao.create(productEntityConvertor.dtoToEntity(request1));
+        productDao.create(productEntityConvertor.dtoToEntity(request2));
+        productDao.create(productEntityConvertor.dtoToEntity(request3));
 
-	@DisplayName("상품 수정 쿼리")
-	@Test
-	void updateById () {
-		// given
-		ProductCreateRequest createRequest = new ProductCreateRequest("product1", 5000, "image url");
-		ProductUpdateRequest updateRequest = new ProductUpdateRequest("update product", 2000, "img");
-		productDao.create(productEntityConvertor.dtoToEntity(createRequest));
+        // when
+        List<ProductEntity> responses = productDao.findAll();
 
-		// when
-		productDao.updateById(getResponseId(), productEntityConvertor.dtoToEntity(updateRequest));
+        // then
+        assertAll(
+                () -> assertThat(responses).hasSize(4),
+                () -> assertThat(responses).extracting(ProductEntity::getName)
+                        .contains("temp", "product1", "product2", "product3"),
+                () -> assertThat(responses).extracting(ProductEntity::getPrice)
+                        .contains(1000, 5000, 6000, 7000)
+        );
+    }
 
-		ProductEntity response = productDao.findAll().get(0);
+    @DisplayName("상품 수정 쿼리")
+    @Test
+    void updateById() {
+        // given
+        ProductCreateRequest createRequest = new ProductCreateRequest("product1", 5000, "image url");
+        ProductUpdateRequest updateRequest = new ProductUpdateRequest("update product", 2000, "img");
+        productDao.create(productEntityConvertor.dtoToEntity(createRequest));
 
-		// then
-		assertAll(
-				() -> assertThat(response.getName()).isEqualTo("update product"),
-				() -> assertThat(response.getPrice()).isEqualTo(2000)
-		);
-	}
+        // when
+        productDao.updateById(getResponseId(), productEntityConvertor.dtoToEntity(updateRequest));
 
-	private Long getResponseId () {
-		List<ProductEntity> responses = productDao.findAll();
-		ProductEntity response = responses.get(0);
-		return response.getId();
-	}
+        ProductEntity response = productDao.findAll().get(0);
+
+        // then
+        assertAll(
+                () -> assertThat(response.getName()).isEqualTo("update product"),
+                () -> assertThat(response.getPrice()).isEqualTo(2000)
+        );
+    }
+
+    private Long getResponseId() {
+        List<ProductEntity> responses = productDao.findAll();
+        ProductEntity response = responses.get(0);
+        return response.getId();
+    }
 
 
-	@DisplayName("상품 삭제 쿼리")
-	@Test
-	void deleteById () {
-		// given
-		ProductCreateRequest createRequest1 = new ProductCreateRequest("product1", 5000, "image url");
-		ProductCreateRequest createRequest2 = new ProductCreateRequest("product1", 5000, "image url");
-		ProductCreateRequest createRequest3 = new ProductCreateRequest("product1", 5000, "image url");
-		productDao.create(productEntityConvertor.dtoToEntity(createRequest1));
-		productDao.create(productEntityConvertor.dtoToEntity(createRequest2));
-		productDao.create(productEntityConvertor.dtoToEntity(createRequest3));
+    @DisplayName("상품 삭제 쿼리")
+    @Test
+    void deleteById() {
+        // given
+        ProductCreateRequest createRequest1 = new ProductCreateRequest("product1", 5000, "image url");
+        ProductCreateRequest createRequest2 = new ProductCreateRequest("product1", 5000, "image url");
+        ProductCreateRequest createRequest3 = new ProductCreateRequest("product1", 5000, "image url");
+        productDao.create(productEntityConvertor.dtoToEntity(createRequest1));
+        productDao.create(productEntityConvertor.dtoToEntity(createRequest2));
+        productDao.create(productEntityConvertor.dtoToEntity(createRequest3));
 
-		// when
-		int beforeSize = productDao.findAll().size();
-		productDao.deleteById(getResponseId());
-		int afterSize = productDao.findAll().size();
+        // when
+        int beforeSize = productDao.findAll().size();
+        productDao.deleteById(getResponseId());
+        int afterSize = productDao.findAll().size();
 
-		// then
-		assertThat(beforeSize - 1).isEqualTo(afterSize);
-	}
+        // then
+        assertThat(beforeSize - 1).isEqualTo(afterSize);
+    }
 
-	@DisplayName("이름이 32자 넘는 경우 요청 실패")
-	@Test
-	void create32OverNameLengthFail () {
-		// given
-		final String name = "012345678901234567890123456789012345";
-		ProductCreateRequest request = new ProductCreateRequest(name, 5000, "image url");
+    @DisplayName("이름이 32자 넘는 경우 요청 실패")
+    @Test
+    void create32OverNameLengthFail() {
+        // given
+        final String name = "012345678901234567890123456789012345";
+        ProductCreateRequest request = new ProductCreateRequest(name, 5000, "image url");
 
-		// when
+        // when
 
-		// then
-		assertThrows(ConstraintViolationException.class, () -> productDao.create(productEntityConvertor.dtoToEntity(request)));
-	}
+        // then
+        assertThrows(ConstraintViolationException.class, () -> productDao.create(productEntityConvertor.dtoToEntity(request)));
+    }
+
+    @DisplayName("Product 존재 여부 테스트")
+    @Test
+    void existById() {
+        //given
+
+        //when
+
+        //then
+        assertThat(productDao.existById(1L)).isTrue();
+    }
 }
