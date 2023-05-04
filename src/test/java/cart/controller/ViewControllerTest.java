7c04b68df5df83d4cc02090cf056257418fa1393
Miseong Kim <miseongk@naver.com>
2023-05-04 11:13:04 +0900

    test: 장바구니, 설정 페이지 호출 테스트

--- src/test/java/cart/controller/ViewControllerTest.java
+++ src/test/java/cart/controller/ViewControllerTest.java
@@ -3,7 +3,6 @@ package cart.controller;
 import static org.hamcrest.Matchers.containsString;
 
 import io.restassured.RestAssured;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -51,9 +50,26 @@ public class ViewControllerTest {
                 .body(containsString("<title>관리자 페이지</title>"));
     }
 
-    @AfterEach
-    void afterEach() {
-        jdbcTemplate.update("TRUNCATE TABLE product");
+    @Test
+    @DisplayName("설정 페이지를 잘 호출하는지 확인한다.")
+    void settings() {
+        RestAssured.given().log().all()
+                .accept(MediaType.TEXT_HTML_VALUE)
+                .when().get("/settings")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body(containsString("사용자 선택"));
+    }
+
+    @Test
+    @DisplayName("장바구니 페이지를 잘 호출하는지 확인한다.")
+    void cart() {
+        RestAssured.given().log().all()
+                .accept(MediaType.TEXT_HTML_VALUE)
+                .when().get("/cart")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .body(containsString("<title>장바구니</title>"));
     }
 
 }
