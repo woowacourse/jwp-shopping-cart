da49dd83e03fedecf4e3d5caaaa5e2a955743de0
SproutMj <mjk54565@gmail.com>
2023-05-04 23:16:19 +0900

    test: SettingControllerTest 작성

---
+++ src/test/java/cart/controller/SettingControllerTest.java
@@ -0,0 +1,36 @@
+package cart.controller;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+class SettingControllerTest {
+
+    @LocalServerPort
+    int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @DisplayName("세팅 페이지 접속")
+    @Test
+    void getSettingPage() {
+        ValidatableResponse response = RestAssured.given().log().all()
+                .when().get("/settings")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .contentType(MediaType.TEXT_HTML_VALUE);
+    }
+}
