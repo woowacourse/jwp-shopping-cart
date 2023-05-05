03074023f607385e7879b5b3f3bb147af8b9b798
thdwoqor <celebarte01@gmail.com>
2023-05-05 21:27:16 +0900

    chore : cart table 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,6 +11,15 @@ CREATE TABLE IF NOT EXISTS member
     email        varchar(30) PRIMARY KEY NOT NULL,
     name         varchar(30)             NOT NULL,
     phone_number varchar(30)             NOT NULL,
-    password     varchar(30)             NOT NULL,
-    cart         TEXT
+    password     varchar(30)             NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS cart
+(
+    member_email VARCHAR(255) NOT NULL,
+    item_id      INT          NOT NULL,
+    quantity     INT default 1,
+    PRIMARY KEY (member_email,item_id),
+    FOREIGN KEY (member_email) REFERENCES member (email) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (item_id) REFERENCES item (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
