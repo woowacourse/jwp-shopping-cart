8d44d2b06c7bfc6d9c91060495e6f1434f3daa17
Choi-JJunho <junho5336@gmail.com>
2023-05-05 14:25:38 +0900

    refactor: String 배열 List로 수정

--- src/main/java/cart/auth/CustomAuthResolver.java
+++ src/main/java/cart/auth/CustomAuthResolver.java
@@ -14,11 +14,17 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Component
 public class CustomAuthResolver implements HandlerMethodArgumentResolver {
+    private static final int BASIC_TOKEN_SIZE = 2;
+    private static final int BASIC_TOKEN_PASSWORD_INDEX = 1;
+
     private final MemberDao memberDao;
 
     public CustomAuthResolver(H2MemberDao memberDao) {
@@ -36,11 +42,11 @@ public class CustomAuthResolver implements HandlerMethodArgumentResolver {
         if (nativeRequest == null) {
             return null;
         }
-        String[] token = extractBasicToken(nativeRequest);
+        List<String> token = extractBasicToken(nativeRequest);
 
         validateToken(token);
 
-        MemberEntity member = memberDao.findByEmail(token[0])
+        MemberEntity member = memberDao.findByEmail(token.get(0))
                 .orElseThrow(() -> new AuthenticationException("잘못된 인증정보입니다."));
 
         validatePassword(token, member);
@@ -48,25 +54,26 @@ public class CustomAuthResolver implements HandlerMethodArgumentResolver {
         return member;
     }
 
-    private void validateToken(String[] token) {
-        if (token.length != 2) {
+    private void validateToken(List<String> token) {
+        if (token.size() != BASIC_TOKEN_SIZE) {
             throw new InvalidTokenException("유효하지 않은 토큰입니다.");
         }
     }
 
-    private void validatePassword(String[] token, MemberEntity member) {
-        if (!Objects.equals(token[1], member.getPassword())) {
+    private void validatePassword(List<String> token, MemberEntity member) {
+        if (!Objects.equals(token.get(BASIC_TOKEN_PASSWORD_INDEX), member.getPassword())) {
             throw new AuthenticationException("잘못된 인증정보입니다.");
         }
     }
 
-    private String[] extractBasicToken(HttpServletRequest nativeRequest) {
+    private List<String> extractBasicToken(HttpServletRequest nativeRequest) {
         String requestHeader = nativeRequest.getHeader("Authorization");
         String credential = StringUtils.delete(requestHeader, "Basic").strip();
 
         byte[] decode = Base64.getDecoder().decode(credential);
         String result = new String(decode);
 
-        return result.split(":");
+        return Arrays.stream(result.split(":"))
+                .collect(Collectors.toList());
     }
 }
