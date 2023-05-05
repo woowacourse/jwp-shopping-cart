e8f0e2c8d3b019d119fccae6b37aeb82e152fb2e
splitube <voicesplit@daum.net>
2023-05-05 15:38:35 +0900

    feat: "/cart" url을 처리하는 viewController 추가

--- src/main/java/cart/configuration/WebConfig.java
+++ src/main/java/cart/configuration/WebConfig.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -31,5 +32,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(basicAuthorizationArgumentResolver);
+    }
+
+    @Override
+    public void addViewControllers(final ViewControllerRegistry registry) {
+        registry.addViewController("/cart").setViewName("cart");
     }
 }
