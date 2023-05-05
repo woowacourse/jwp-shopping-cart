ec895c7a0307590887c4e91ff6397fe060e32af5
BGuga <tmdwns9913@gmail.com>
2023-05-05 21:21:21 +0900

    refactor: 정확한 예외처리 로직 작성

--- src/main/java/cart/common/GlobalExceptionHandler.java
+++ src/main/java/cart/common/GlobalExceptionHandler.java
@@ -4,28 +4,28 @@ import cart.excpetion.AuthException;
 import cart.excpetion.CartException;
 import cart.excpetion.ProductionRepoException;
 import cart.excpetion.ProductionServiceException;
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler({IllegalArgumentException.class})
-    public ResponseEntity<String> handle(IllegalArgumentException e) {
-        return ResponseEntity.badRequest().body(e.getMessage());
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<String> handle(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
     @ExceptionHandler({AuthException.class})
     public ResponseEntity<String> handle(AuthException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-    }
-
-    @ExceptionHandler({EmptyResultDataAccessException.class, IllegalStateException.class})
-    public ResponseEntity<String> handle(Exception e) {
-        return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler({ProductionServiceException.class, ProductionRepoException.class})
@@ -36,5 +36,11 @@ public class GlobalExceptionHandler {
     @ExceptionHandler({CartException.class})
     public ResponseEntity<String> handle(CartException e) {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler()
+    public ResponseEntity<String> handle(Exception e) {
+        logger.error(e.getMessage());
+        return ResponseEntity.internalServerError().build();
     }
 }
