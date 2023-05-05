bd8dd1d65b34bf72e6df92ec7f1fd56e32a30c47
jaeyeon kim <jakind@naver.com>
2023-05-05 18:57:36 +0900

    refactor : PasswordTest 에서 깨지던 테스트 수정

--- src/test/java/cart/vo/PasswordTest.java
+++ src/test/java/cart/vo/PasswordTest.java
@@ -9,18 +9,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PasswordTest {
 
-    @DisplayName("Password 로 형식에 맞지 않는 데이터를 입력 받았을 때 예외 발생 (7자, 특수문자 안들어감, 31자)")
+    @DisplayName("Password 로 형식에 맞지 않는 데이터를 입력 받았을 때 예외 발생 (7자, 특수문자 안들어감, 21자)")
     @ParameterizedTest
-    @ValueSource(strings = {"sksk11!", "sksksksk1", "ssssssssssssssssssssssssssss11!"})
+    @ValueSource(strings = {"sksk11!", "sksksksk1", "ssssssssssssssssss11!"})
     void createPasswordFail(String input) {
         assertThatThrownBy(() -> Password.from(input))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("올바르지 않은 비밀번호 형식입니다.");
     }
 
-    @DisplayName("Password 로 형식에 맞는 데이터를 입력 받았을 때 성공적으로 객체 생성 (8자, 30자)")
+    @DisplayName("Password 로 형식에 맞는 데이터를 입력 받았을 때 성공적으로 객체 생성 (8자, 20자)")
     @ParameterizedTest
-    @ValueSource(strings = {"sksk583!", "ssssssssssssssssssssssssssss1!"})
+    @ValueSource(strings = {"sksk583!", "ssssssssssssssssa11!"})
     void createPasswordSuccess(String input) {
         Password password = Password.from(input);
 
