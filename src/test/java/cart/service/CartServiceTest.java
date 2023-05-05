a7b160f6992366e310cd074af491adc8f86902f4
jaeyeon kim <jakind@naver.com>
2023-05-05 18:53:37 +0900

    test : CartService 의 removeById 를 테스트

--- src/test/java/cart/service/CartServiceTest.java
+++ src/test/java/cart/service/CartServiceTest.java
@@ -12,13 +12,15 @@ import cart.vo.Url;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.mockito.Mockito;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.BDDMockito.given;
 
 class CartServiceTest {
 
@@ -37,11 +39,11 @@ class CartServiceTest {
     @DisplayName("findAll 을 통해 ResponseDto 가 잘 반환되는지 확인한다.")
     void findAll() {
         Email email = Email.from("kpeel5839@a.com");
-        BDDMockito.given(cartDao.selectAll(email))
+        given(cartDao.selectAll(email))
                 .willReturn(new ArrayList<>(List.of(getCart(1L, email), getCart(2L, email))));
-        BDDMockito.given(productDao.findById(1L))
+        given(productDao.findById(1L))
                 .willReturn(getProduct(1L, "샐러드", 10000));
-        BDDMockito.given(productDao.findById(2L))
+        given(productDao.findById(2L))
                 .willReturn(getProduct(2L, "치킨", 10001));
 
         List<CartResponseDto> carts = cartService.findAll(email);
@@ -57,6 +59,29 @@ class CartServiceTest {
                         .getName()
                         .equals("치킨"))
         ).isTrue();
+    }
+
+    @Test
+    @DisplayName("다른 사용자가 Cart 에 접근하여 삭제하려 할 때 예외를 반환")
+    void removeByIdFail() {
+        Email legalUserEmail = Email.from("kpeel5839@a.com");
+        Email illegalUserEmail = Email.from("kpeel583@a.com");
+        given(cartDao.findById(1L))
+                .willReturn(getCart(1L, legalUserEmail));
+
+        assertThatThrownBy(() -> cartService.removeById(1L, illegalUserEmail))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("삭제할 권한이 없습니다.");
+    }
+
+    @Test
+    @DisplayName("정상적인 사용자가 Cart 에 접근하여 삭제하려 할 때 정상 삭제")
+    void removeByIdSuccess() {
+        Email email = Email.from("kpeel5839@a.com");
+        given(cartDao.findById(1L))
+                .willReturn(getCart(1L, email));
+
+        assertDoesNotThrow(() -> cartService.removeById(1L, email));
     }
 
     private Cart getCart(long id, Email email) {
