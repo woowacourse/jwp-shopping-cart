ceca4e9aba27ceb5d09aa7074f517f9e5b6c019d
ingpyo <juninpyo@naver.com>
2023-05-05 15:52:57 +0900

    feat: 웹으로 보내는 dto 생성

--- src/main/java/cart/controller/CartController.java
+++ src/main/java/cart/controller/CartController.java
@@ -4,6 +4,7 @@ import cart.authority.Authority;
 import cart.controller.dto.request.CartItemCreationRequest;
 import cart.controller.dto.request.CartItemDeleteRequest;
 import cart.controller.dto.request.MemberIdRequest;
+import cart.controller.dto.response.CartItemResponse;
 import cart.domain.dto.CartDto;
 import cart.domain.dto.ProductDto;
 import cart.service.CartService;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/cart/items")
@@ -36,9 +38,20 @@ public class CartController {
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductDto>> findIdMemberId(@Authority final MemberIdRequest memberId) {
+    public ResponseEntity<List<CartItemResponse>> findIdMemberId(@Authority final MemberIdRequest memberId) {
         final List<CartDto> cartDtos = cartService.getProducts(memberId);
-        return ResponseEntity.ok(productService.findById(cartDtos));
+        final List<ProductDto> productDtos = productService.findById(cartDtos);
+
+        List<CartItemResponse> cartItemResponses = productDtos.stream()
+                .map(productDto -> new CartItemResponse(
+                        productDto.getId(),
+                        productDto.getName(),
+                        productDto.getImage(),
+                        productDto.getPrice())
+                )
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(cartItemResponses);
     }
 
     @DeleteMapping
