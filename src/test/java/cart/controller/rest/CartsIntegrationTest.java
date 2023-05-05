e95d2f9b3f7ac486852dc57e9abefccae344d573
MoonJeWoong <munjin0201@naver.com>
2023-05-05 19:44:02 +0900

    test: 장바구니 api관련 통합 테스트 작성

---
+++ src/test/java/cart/controller/rest/CartsIntegrationTest.java
@@ -0,0 +1,88 @@
+package cart.controller.rest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import cart.domain.product.Product;
+import cart.persistence.CartDao;
+import cart.persistence.ProductsDao;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CartsIntegrationTest {
+
+    private static final Long MEMBER_ID = 1L;
+    private static final String EMAIL = "munjin0201@naver.com";
+    private static final String PASSWORD = "1234";
+
+    @LocalServerPort
+    int port;
+    @Autowired
+    ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    @Sql("/data.sql")
+    @DisplayName("/api/cart/items에 정상적인 post 요청이 들어오면 created 상태코드를 반환한다.")
+    void createItemTest() {
+        ProductsDao productsDao = applicationContext.getBean("h2ProductsDao", ProductsDao.class);
+        Long createdId = productsDao.create(new Product("테스트", 1000, "http://testtest"));
+
+        RestAssured
+                .given()
+                .auth().preemptive().basic(EMAIL, PASSWORD)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .queryParam("product-id", createdId)
+                .when()
+                .post("/api/cart/items")
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    @Sql("/data.sql")
+    @DisplayName("/api/cart/items에 정상적인 get 요청이 들어오면 ok 상태코드를 반환한다.")
+    void readItemsByMemberTest() {
+        RestAssured
+                .given()
+                .auth().preemptive().basic(EMAIL, PASSWORD)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .get("/api/cart/items")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    @Sql("/data.sql")
+    @DisplayName("/api/cart/items에 정상적인 delete 요청이 들어오면 no_content 상태코드를 반환한다.")
+    void deleteItemTest() {
+        ProductsDao productsDao = applicationContext.getBean("h2ProductsDao", ProductsDao.class);
+        Long createdProductId = productsDao.create(new Product("테스트", 1000, "http://testtest"));
+
+        CartDao cartDao = applicationContext.getBean("h2CartDao", CartDao.class);
+        Long createdItemId = cartDao.createItem(MEMBER_ID, createdProductId);
+
+        RestAssured.given()
+                .auth().preemptive().basic(EMAIL, PASSWORD)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when()
+                .delete("/api/cart/items/" + createdItemId)
+                .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+}
