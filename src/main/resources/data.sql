d749a78ffb6477aea4666dfe0bc750456c9d9e14
zillionme <beatus96@naver.com>
2023-05-05 09:28:29 +0900

    refactor: product 테이블 양수 옵션 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS PRODUCT (
-  product_id          BIGINT            NOT NULL   AUTO_INCREMENT,
-  name        VARCHAR(500)      NOT NULL,
-  image_url   TEXT              NOT NULL,
-  price       DECIMAL           NOT NULL,
+  product_id          BIGINT UNSIGNED       NOT NULL   AUTO_INCREMENT,
+  name                VARCHAR(500)          NOT NULL,
+  image_url           TEXT                  NOT NULL,
+  price               DECIMAL UNSIGNED      NOT NULL,
   PRIMARY KEY (product_id)
 );
