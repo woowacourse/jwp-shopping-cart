ec290d914c8f30e1ca4cf251e67027568141de5b
kevstevie <jianna442@gmail.com>
2023-05-05 20:03:27 +0900

    feat: Exception 처리 추가

--- src/main/java/cart/controller/ExceptionController.java
+++ src/main/java/cart/controller/ExceptionController.java
@@ -22,4 +22,10 @@ public class ExceptionController extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                 .body(exception.getMessage());
     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(final Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(exception.getMessage());
+    }
 }
