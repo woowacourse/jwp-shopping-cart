8e7fde212c4e07ed0e2a7d6fccf1abbcc9bb2101
seokhwan-an <asok369258@naver.com>
2023-05-05 20:19:48 +0900

    docs: 기본 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- product 더미 데이터
+INSERT INTO product(name, image_url, price)
+values ('연필', 'https://ppss.kr/wp-content/uploads/2018/07/01-65.jpg', 1000)
+, ('지우개', 'https://img.danawa.com/prod_img/500000/093/390/img/9390093_1.jpg?shrink=330:*&_v=20200824081645', 2000)
+, ('볼팬', 'https://img.danawa.com/prod_img/500000/665/448/img/1448665_1.jpg?shrink=330:*&_v=20200805125500', 1500);
+
+-- member 더미 데이터
+INSERT INTO member(email, password)
+values ('ako@wooteco.com', 'ako')
+,('oz@wooteco.com', 'oz');
