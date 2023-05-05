6104cae5a0edb59b539af6a03267c66de027574e
zillionme <beatus96@naver.com>
2023-05-05 16:02:06 +0900

    feat: member 테이블 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,16 @@
 CREATE TABLE IF NOT EXISTS PRODUCT (
-  product_id          BIGINT UNSIGNED     NOT NULL   AUTO_INCREMENT,
+  product_id          BIGINT              NOT NULL   AUTO_INCREMENT,
   name                VARCHAR(500)        NOT NULL,
   image_url           TEXT                NOT NULL,
   price               DECIMAL             NOT NULL   CHECK (price >= 0),
   PRIMARY KEY (product_id)
 );
+
+CREATE TABLE IF NOT EXISTS MEMBER (
+  member_id           BIGINT              NOT NULL   AUTO_INCREMENT,
+  name                VARCHAR(500),
+  email               VARCHAR(500)        NOT NULL,
+  password            VARCHAR(500)        NOT NULL   CHECK (4 <= LENGTH(password) AND LENGTH(password) <= 16),
+  PRIMARY KEY (member_id)
+);
+
