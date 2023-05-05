3b9f681ce5b18a974a7e5e8ad4a27d708da068fd
chaewon121 <cw121@naver.com>
2023-05-05 21:19:44 +0900

    refactor: 예외처리 범위와 메세지 수정

--- src/main/java/cart/controller/api/ExceptionControllerAdvice.java
+++ src/main/java/cart/controller/api/ExceptionControllerAdvice.java
@@ -1,13 +1,13 @@
 package cart.controller.api;
 
-import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Transactional
 public class ExceptionControllerAdvice {
 
     public static class ExceptionResponse {
@@ -22,13 +22,9 @@ public class ExceptionControllerAdvice {
         }
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ExceptionResponse> handleValidException(final MethodArgumentNotValidException e) {
-        final String errorMessage = e.getFieldErrors().stream()
-                .map(it -> it.getField() + " : " + it.getDefaultMessage())
-                .collect(Collectors.joining("\n"));
-
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleValidException(final Exception e) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ExceptionResponse(errorMessage));
+                .body(new ExceptionResponse(e.getMessage()));
     }
 }
