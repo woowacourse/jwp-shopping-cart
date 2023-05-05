764f226792cb46eb1a8fd2a8bdd99cf36ef38000
woosung1223 <dntjd991223@naver.com>
2023-05-05 22:51:34 +0900

    chore: 불필요한 어노테이션 제거

--- src/main/java/cart/business/repository/CartItemRepository.java
+++ src/main/java/cart/business/repository/CartItemRepository.java
@@ -1,11 +1,9 @@
 package cart.business.repository;
 
 import cart.business.domain.cart.CartItem;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface CartItemRepository {
 
     Integer save(CartItem cartItem);
--- src/main/java/cart/business/repository/MemberRepository.java
+++ src/main/java/cart/business/repository/MemberRepository.java
@@ -1,12 +1,10 @@
 package cart.business.repository;
 
 import cart.business.domain.member.Member;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface MemberRepository {
 
     List<Member> findAll();
