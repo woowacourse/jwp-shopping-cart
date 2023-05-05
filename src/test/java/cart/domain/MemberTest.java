40687ab135913fa499018f038c1943e4dda0de57
mcodnjs <codnjs8989@gmail.com>
2023-05-05 17:33:09 +0900

    test: member 테스트 추가

---
+++ src/test/java/cart/domain/MemberTest.java
@@ -0,0 +1,32 @@
+package cart.domain;
+
+import cart.exception.InvalidMemberException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class MemberTest {
+
+    @DisplayName("유효하지 않은 email이 들어올 시 InvalidMemberException을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "  ", "notEmailForm"})
+    void validateEmail(String email) {
+        assertThrows(
+                InvalidMemberException.class,
+                () -> new Member(1L, email, "password1")
+        );
+    }
+
+    @DisplayName("유효하지 않은 password가 들어올 시 InvalidMemberException을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "  ", "12345678", "abcdefgh", "a123456"})
+    void validatePassword(String password) {
+        assertThrows(
+                InvalidMemberException.class,
+                () -> new Member(1L, "a@a.com", password)
+        );
+    }
+
+}
