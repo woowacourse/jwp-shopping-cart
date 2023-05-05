e13017986fcb5616a1f2e25d27718cecb4338133
chaewon121 <cw121@naver.com>
2023-05-05 21:23:31 +0900

    feat: 회원 기능 구현

---
+++ src/main/java/cart/dao/MemberDao.java
@@ -0,0 +1,51 @@
+package cart.dao;
+
+import cart.domain.Member;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class MemberDao {
+
+    private JdbcTemplate template;
+    private SimpleJdbcInsert simpleJdbcInsert;
+    private RowMapper<Member> rowMapper = ((rs, rowNum) ->
+            new Member(
+                    rs.getLong(1),
+                    rs.getString(2),
+                    rs.getString(3)
+            ));
+
+    public MemberDao(final JdbcTemplate template) {
+        this.template = template;
+        this.simpleJdbcInsert = new SimpleJdbcInsert(template)
+                .withTableName("member")
+                .usingGeneratedKeyColumns("id");
+    }
+
+    public Long save(Member member) {
+        final SqlParameterSource source = new BeanPropertySqlParameterSource(member);
+        return simpleJdbcInsert.executeAndReturnKey(source).longValue();
+    }
+
+    public List<Member> findAll() {
+        final String sql = "select * from member";
+        return template.query(sql, rowMapper);
+    }
+
+    public Member findByEmail(String email){
+        final String sql = "select * from member where email = ?";
+        return template.query(sql,rowMapper,email).get(0);
+    }
+
+    public void deleteById(Long id) {
+        final String sql = "delete from member where id = ?";
+        template.update(sql, id);
+    }
+}
