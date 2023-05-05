82de01edffa519b9096f83aa6bc83a65532505d8
hgo641 <hgo641@gmail.com>
2023-05-06 05:01:19 +0900

    refactor: 테스트 fixture를 사용하게 변경

--- src/test/java/cart/controller/PageControllerTest.java
+++ src/test/java/cart/controller/PageControllerTest.java
@@ -1,11 +1,7 @@
 package cart.controller;
 
-import cart.dto.MemberResponse;
-import cart.dto.ProductResponse;
 import cart.service.MemberService;
 import cart.service.ProductService;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -14,6 +10,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static cart.fixture.MemberFixture.FIRST_MEMBER;
+import static cart.fixture.ProductFixture.FIRST_PRODUCT;
+import static cart.fixture.ProductFixture.SECOND_PRODUCT;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -34,55 +33,47 @@ public class PageControllerTest {
 
     @Test
     void 메인_페이지를_가져온다() throws Exception {
-        final ProductResponse firstProductResponse = new ProductResponse(1L, "홍고", "https://ca.slack-edge.com/TFELTJB7V-U04M4NFB5TN-e18b78fabe81-512", 1_000_000_000);
-        final ProductResponse secondProductResponse = new ProductResponse(2L, "아벨", "https://ca.slack-edge.com/TFELTJB7V-U04LMNLQ78X-a7ef923d5391-512", 1_000_000_000);
-
-        given(productService.findAll()).willReturn(List.of(firstProductResponse, secondProductResponse));
+        given(productService.findAll())
+                .willReturn(List.of(FIRST_PRODUCT.RESPONSE, SECOND_PRODUCT.RESPONSE));
 
         mockMvc.perform(get("/"))
                 .andExpectAll(
                         status().isOk(),
                         view().name("index"),
-                        model().attribute("products", equalTo(List.of(firstProductResponse, secondProductResponse)))
+                        model().attribute("products", equalTo(List.of(FIRST_PRODUCT.RESPONSE, SECOND_PRODUCT.RESPONSE)))
                 )
                 .andDo(print());
     }
 
     @Test
     void 관리자_페이지를_가져온다() throws Exception {
-        final ProductResponse firstProductResponse = new ProductResponse(1L, "홍고", "https://ca.slack-edge.com/TFELTJB7V-U04M4NFB5TN-e18b78fabe81-512", 1_000_000_000);
-        final ProductResponse secondProductResponse = new ProductResponse(2L, "아벨", "https://ca.slack-edge.com/TFELTJB7V-U04LMNLQ78X-a7ef923d5391-512", 1_000_000_000);
-
-        given(productService.findAll()).willReturn(List.of(firstProductResponse, secondProductResponse));
+        given(productService.findAll()).willReturn(List.of(FIRST_PRODUCT.RESPONSE, SECOND_PRODUCT.RESPONSE));
 
         mockMvc.perform(get("/admin"))
                 .andExpectAll(
                         status().isOk(),
                         view().name("admin"),
-                        model().attribute("products", equalTo(List.of(firstProductResponse, secondProductResponse)))
+                        model().attribute("products", equalTo(List.of(FIRST_PRODUCT.RESPONSE, SECOND_PRODUCT.RESPONSE)))
                 )
                 .andDo(print());
     }
 
     @Test
     void 세팅_페이지를_가져온다() throws Exception {
-        final MemberResponse firstMemberResponse = new MemberResponse(1L, "turtle@test.com", "pw1");
-        final MemberResponse secondMemberResponse = new MemberResponse(2L, "hongo@test.com", "pw2");
-
-        given(memberService.findAll()).willReturn(List.of(firstMemberResponse, secondMemberResponse));
+        given(memberService.findAll()).willReturn(List.of(FIRST_MEMBER.RESPONSE));
 
         mockMvc.perform(get("/settings"))
                 .andExpectAll(
                         status().isOk(),
                         view().name("settings"),
-                        model().attribute("members", equalTo(List.of(firstMemberResponse, secondMemberResponse)))
+                        model().attribute("members", equalTo(List.of(FIRST_MEMBER.RESPONSE)))
                 )
                 .andDo(print());
     }
 
     @Test
     void 장바구니_페이지를_가져온다() throws Exception {
-          mockMvc.perform(get("/cart"))
+        mockMvc.perform(get("/cart"))
                 .andExpectAll(
                         status().isOk(),
                         view().name("cart")
--- src/test/java/cart/controller/ProductControllerTest.java
+++ src/test/java/cart/controller/ProductControllerTest.java
@@ -1,7 +1,6 @@
 package cart.controller;
 
 import cart.dto.ProductRequest;
-import cart.dto.ProductResponse;
 import cart.service.ProductService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
@@ -10,10 +9,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.util.List;
 
+import static cart.fixture.ProductFixture.FIRST_PRODUCT;
+import static cart.fixture.ProductFixture.SECOND_PRODUCT;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -33,24 +33,18 @@ class ProductControllerTest {
 
     @Test
     void 상품을_조회한다() throws Exception {
-        final ProductResponse firstProductResponse = new ProductResponse(1L, "홍고", "https://ca.slack-edge.com/TFELTJB7V-U04M4NFB5TN-e18b78fabe81-512", 1_000_000_000);
-        final ProductResponse secondProductResponse = new ProductResponse(2L, "아벨", "https://ca.slack-edge.com/TFELTJB7V-U04LMNLQ78X-a7ef923d5391-512", 1_000_000_000);
-        given(productService.findAll()).willReturn(List.of(firstProductResponse, secondProductResponse));
+        given(productService.findAll()).willReturn(List.of(FIRST_PRODUCT.RESPONSE, SECOND_PRODUCT.RESPONSE));
 
-        final MvcResult mvcResult = mockMvc.perform(get("/products"))
+        mockMvc.perform(get("/products"))
                 .andExpect(status().isOk())
                 .andDo(print())
                 .andReturn();
-
-
     }
 
     @Test
     void 상품을_생성한다() throws Exception {
-        final ProductRequest productRequest = new ProductRequest("홍고", "aaaa", 10000);
-
         mockMvc.perform(post("/products")
-                        .content(objectMapper.writeValueAsString(productRequest))
+                        .content(objectMapper.writeValueAsString(FIRST_PRODUCT.REQUEST))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andDo(print());
