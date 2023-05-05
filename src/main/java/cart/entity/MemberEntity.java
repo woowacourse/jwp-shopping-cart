9b1a615c826040209e48a44fa3397858f362303a
BGuga <tmdwns9913@gmail.com>
2023-05-05 21:42:37 +0900

    refactor: MemberEntity validation 생성

--- src/main/java/cart/entity/MemberEntity.java
+++ src/main/java/cart/entity/MemberEntity.java
@@ -11,6 +11,15 @@ public class MemberEntity {
         this.password = password;
     }
 
+    private void validate(final String email, final String password) {
+        if (email == null) {
+            throw new IllegalArgumentException("DB 테이블의 email 은 not null 로 설정 되어 있습니다.");
+        }
+        if (password == null) {
+            throw new IllegalArgumentException("DB 테이블의 password 은 not null 로 설정 되어 있습니다.");
+        }
+    }
+
     public Integer getId() {
         return id;
     }
