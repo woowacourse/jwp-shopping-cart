3596f6cd680cf269c640cd5eaa997e17e673f675
splitube <voicesplit@daum.net>
2023-05-05 16:29:20 +0900

    refactor: Interceptor 리팩토링

--- src/main/java/cart/interceptor/AuthInterceptor.java
+++ src/main/java/cart/interceptor/AuthInterceptor.java
@@ -9,10 +9,11 @@ import org.springframework.web.servlet.HandlerInterceptor;
 @Component
 public class AuthInterceptor implements HandlerInterceptor {
 
-    private static final String BASIC = "Basic";
-    private static final int AUTHORIZATION_TYPE_INDEX = 0;
-    private static final int CREDENTIAL_INDEX = 1;
+    private static final String AUTHORIZATION_HEADER = "Authorization";
     private static final String AUTHORIZATION_DELIMITER = " ";
+    private static final int AUTHORIZATION_TYPE_INDEX = 0;
+    private static final String BASIC_TYPE = "Basic";
+    private static final int CREDENTIAL_INDEX = 1;
 
     @Override
     public boolean preHandle(
@@ -20,14 +21,13 @@ public class AuthInterceptor implements HandlerInterceptor {
         final HttpServletResponse response,
         final Object handler
     ) throws Exception {
-        final String authorization = request.getHeader("Authorization");
+        final String authorization = request.getHeader(AUTHORIZATION_HEADER);
         final String[] authorizationInfo = authorization.split(AUTHORIZATION_DELIMITER);
         final String authorizationType = authorizationInfo[AUTHORIZATION_TYPE_INDEX];
-        if (authorizationType.equals(BASIC)) {
+        if (authorizationType.equals(BASIC_TYPE)) {
             final String encodedCredential = authorizationInfo[CREDENTIAL_INDEX];
             final byte[] decodedByteArray = Base64.getDecoder().decode(encodedCredential);
-            final String decodedCredential = new String(decodedByteArray);
-            request.setAttribute("AuthInfo", decodedCredential);
+            request.setAttribute("AuthInfo", new String(decodedByteArray));
         }
         return HandlerInterceptor.super.preHandle(request, response, handler);
     }
