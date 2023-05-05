c2b186f30c595c76fb3bd63fb9f8ad9f58fd0ef9
LJW25 <dbjwlee@gmail.com>
2023-05-05 15:02:11 +0900

    feat(Cart*): 장바구니 객체 생성

---
+++ src/main/java/cart/domain/cart/CartItem.java
@@ -0,0 +1,31 @@
+package cart.domain.cart;
+
+public class CartItem {
+    private final Long id;
+    private final String productName;
+    private final int productPrice;
+    private final String productImage;
+
+    public CartItem(final Long id, final String productName, final int productPrice, final String productImage) {
+        this.id = id;
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productImage = productImage;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public String getProductImage() {
+        return productImage;
+    }
+}
