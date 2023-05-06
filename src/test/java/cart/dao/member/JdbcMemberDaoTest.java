ecba9b30c446d96b11ba9d23fc81552fa534aa67
Go-Jaecheol <gojaech@naver.com>
2023-05-06 15:27:39 +0900

    test: JdbcMemberDao 테스트 변경

--- src/test/java/cart/dao/member/JdbcMemberDaoTest.java
+++ src/test/java/cart/dao/member/JdbcMemberDaoTest.java
@@ -2,38 +2,48 @@ package cart.dao.member;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.sql.PreparedStatement;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 @JdbcTest
 @Import(JdbcMemberDao.class)
 class JdbcMemberDaoTest {
 
     @Autowired
-    private JdbcMemberDao jdbcMemberDao;
-    @Autowired
     private JdbcTemplate jdbcTemplate;
+    private MemberDao memberDao;
+
+    @BeforeEach
+    void setUp() {
+        memberDao = new JdbcMemberDao(jdbcTemplate);
+    }
+
+    private void insertMember() {
+        final SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("member")
+                .usingGeneratedKeyColumns("id");
+        final Map<String, Object> parameters = new HashMap<>();
+        parameters.put("email", "test@wooteco.com");
+        parameters.put("password", "test");
+        simpleJdbcInsert.executeAndReturnKey(parameters).longValue();
+    }
 
     @Test
     @DisplayName("저장된 회원 목록을 모두 조회할 수 있다.")
     void findAll() {
         // given
-        final int initialSize = jdbcMemberDao.findAll().size();
+        final int initialSize = memberDao.findAll().size();
         // when
-        String sql = "INSERT INTO member(email, password) VALUES(?, ?)";
-        jdbcTemplate.update(connection -> {
-            PreparedStatement ps = connection
-                    .prepareStatement(sql, new String[]{"id"});
-            ps.setString(1, "test@wooteco.com");
-            ps.setString(2, "test");
-            return ps;
-        });
-        final int result = jdbcMemberDao.findAll().size();
+        insertMember();
+        final int result = memberDao.findAll().size();
         // then
         assertThat(result).isEqualTo(initialSize + 1);
     }
