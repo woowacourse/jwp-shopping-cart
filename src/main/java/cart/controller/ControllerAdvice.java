19d67b6ed1a040e8c8093e94e43426491894c709
gitchan <eunkeeeee@gmail.com>
2023-05-06 16:45:38 +0900

    refactor(ControllerAdvice): 사용자 에러 처리

--- src/main/java/cart/controller/ControllerAdvice.java
+++ src/main/java/cart/controller/ControllerAdvice.java
@@ -5,11 +5,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestControllerAdvice
 public class ControllerAdvice {
     @ExceptionHandler
-    public ResponseEntity<String> handleException(MethodArgumentNotValidException e) {
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         return ResponseEntity
                 .badRequest()
                 .body(e.getBindingResult().getFieldError().getDefaultMessage());
@@ -27,5 +30,17 @@ public class ControllerAdvice {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body(e.getMessage());
+    }
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException e, HttpServletResponse response) {
+        if (e.getStatus() == HttpStatus.UNAUTHORIZED) {
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .body("Unauthorized");
+        }
+        return ResponseEntity
+                .status(e.getStatus())
+                .build();
     }
 }
