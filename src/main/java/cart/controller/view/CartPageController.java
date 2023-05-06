a1704caa8aa4217dd6bc031d55ad0d5614ee31e9
yeonkkk <tjddus0302@gmail.com>
2023-05-06 15:38:10 +0900

    feat: 데이터베이스 설정값 추가

---
+++ src/main/java/cart/controller/view/CartPageController.java
@@ -0,0 +1,12 @@
+package cart.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CartPageController {
+    @GetMapping("/cart")
+    public String cartPage() {
+        return "cart";
+    }
+}
