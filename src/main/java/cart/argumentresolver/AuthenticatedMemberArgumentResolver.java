7737a20fa625da33413d3bfb6d6e9c73d206ab2f
gitchan <eunkeeeee@gmail.com>
2023-05-07 00:53:36 +0900

    refactor(AuthenticatedMemberArgumentResolver): 기능 클래스 이동

--- src/main/java/cart/argumentresolver/AuthenticatedMemberArgumentResolver.java
+++ src/main/java/cart/argumentresolver/AuthenticatedMemberArgumentResolver.java
@@ -1,7 +1,11 @@
 package cart.argumentresolver;
 
 import cart.entity.member.Member;
+import cart.entity.member.MemberDao;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -12,6 +16,16 @@ import javax.servlet.http.HttpServletRequest;
 
 @Component
 public class AuthenticatedMemberArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private static final String BASIC_TYPE_PREFIX = "Basic";
+    private static final String DELIMITER = ":";
+
+    private final MemberDao memberDao;
+
+    public AuthenticatedMemberArgumentResolver(final MemberDao memberDao) {
+        this.memberDao = memberDao;
+    }
+
     @Override
     public boolean supportsParameter(final MethodParameter parameter) {
         return parameter.hasParameterAnnotation(AuthenticatedMember.class)
@@ -25,6 +39,32 @@ public class AuthenticatedMemberArgumentResolver implements HandlerMethodArgumen
             final NativeWebRequest webRequest,
             final WebDataBinderFactory binderFactory) throws Exception {
         final HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
-        return request.getAttribute("authenticatedMember");
+
+        final String authorization = request.getHeader("Authorization");
+        final boolean isBasicAuthentication = authorization != null && authorization.toLowerCase().startsWith(BASIC_TYPE_PREFIX.toLowerCase());
+
+        if (!isBasicAuthentication) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        final Member authenticatedMember = findMemberFromAuthentication(authorization);
+
+        if (authenticatedMember == null) {
+            return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+        }
+
+        return authenticatedMember;
+    }
+
+    private Member findMemberFromAuthentication(final String authorization) {
+        String authHeaderValue = authorization.substring(BASIC_TYPE_PREFIX.length()).trim();
+        byte[] decodedBytes = Base64.decodeBase64(authHeaderValue);
+        String decodedString = new String(decodedBytes);
+
+        String[] credentials = decodedString.split(DELIMITER);
+        String email = credentials[0];
+        String password = credentials[1];
+
+        return memberDao.findByEmailAndPassword(email, password).orElse(null);
     }
 }
--- src/main/java/cart/config/WebConfig.java
+++ src/main/java/cart/config/WebConfig.java
@@ -1,10 +1,7 @@
 package cart.config;
 
-import cart.argumentresolver.AuthenticatedMemberArgumentResolver;
-import cart.interceptor.BasicAuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -12,19 +9,14 @@ import java.util.List;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    private final BasicAuthInterceptor basicAuthInterceptor;
+    private final HandlerMethodArgumentResolver argumentResolver;
 
-    public WebConfig(final BasicAuthInterceptor basicAuthInterceptor) {
-        this.basicAuthInterceptor = basicAuthInterceptor;
-    }
-
-    @Override
-    public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(basicAuthInterceptor).addPathPatterns("/carts/**");
+    public WebConfig(final HandlerMethodArgumentResolver argumentResolver) {
+        this.argumentResolver = argumentResolver;
     }
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthenticatedMemberArgumentResolver());
+        resolvers.add(argumentResolver);
     }
 }
