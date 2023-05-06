fd49bafe98ac17b5a93ebb9fe6fcf501bce6e79a
yeonkkk <tjddus0302@gmail.com>
2023-05-06 15:35:56 +0900

    feat: cart 데이터베이스 설정값 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,17 +2,25 @@ DROP TABLE IF EXISTS product;
 
 CREATE TABLE product
 (
-    id        BIGINT       NOT NULL AUTO_INCREMENT,
-    name      VARCHAR(50)  NOT NULL,
-    price     INT          NOT NULL,
-    image_url VARCHAR(255) NOT NULL,
+    id        BIGINT         NOT NULL AUTO_INCREMENT,
+    name      VARCHAR(50)    NOT NULL,
+    price     INT            NOT NULL,
+    image_url VARCHAR(255)   NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE member
 (
-    id        BIGINT       NOT NULL AUTO_INCREMENT,
+    id        BIGINT         NOT NULL AUTO_INCREMENT,
     email      VARCHAR(100)  NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    password VARCHAR(100)    NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE cart
+(
+    id            BIGINT     NOT NULL AUTO_INCREMENT,
+    product_id    BIGINT     NOT NULL,
+    member_id     BIGINT     NOT NULL,
     PRIMARY KEY (id)
 );
