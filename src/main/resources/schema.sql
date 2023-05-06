ab4e752a06ede7779429bc0f6fd823542cea37dc
Woojin Lee <dhkep03@gmail.com>
2023-05-07 04:30:37 +0900

    chore: Member 테이블 DDL 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,13 @@ CREATE TABLE product
     image_url TEXT        NOT NULL,
     PRIMARY KEY (id)
 );
+
+CREATE TABLE member
+(
+    id        BIGINT      NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    name VARCHAR(255),
+    phone_number VARCHAR(50),
+    PRIMARY KEY (id)
+);
