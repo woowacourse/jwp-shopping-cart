0bbb7cbe3567864bce35e7b51d451d2cb8622aa9
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-06 18:08:42 +0900

    feat: 장바구니 페이지 연동 기능 추가

---
+++ src/main/java/cart/controller/config/WebMvcConfiguration.java
@@ -0,0 +1,31 @@
+package cart.controller.config;
+
+import cart.controller.auth.interceptor.LoginInterceptor;
+import cart.controller.auth.resolver.BasicAuthenticationPrincipalArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/cart")
+                .setViewName("cart");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .addPathPatterns("/carts/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new BasicAuthenticationPrincipalArgumentResolver());
+    }
+}
