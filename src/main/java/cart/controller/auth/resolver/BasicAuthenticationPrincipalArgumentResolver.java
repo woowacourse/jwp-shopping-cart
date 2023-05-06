594be51282f3fd923287afcaa0906aaff03a2b95
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-06 18:05:43 +0900

    feat: /carts/** 요청 시 로그인 확인 기능 추가

---
+++ src/main/java/cart/controller/auth/resolver/BasicAuthenticationPrincipalArgumentResolver.java
@@ -0,0 +1,26 @@
+package cart.controller.auth.resolver;
+
+import cart.controller.auth.BasicAuthorizationExtractor;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class BasicAuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final BasicAuthorizationExtractor basicAuthorizationExtractor = new BasicAuthorizationExtractor();
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(BasicAuthenticationPrincipal.class);
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        return basicAuthorizationExtractor.extract(request);
+    }
+}
