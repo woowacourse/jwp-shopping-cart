87c0b7dc895518995212d2f65980509932cec755
Combi153 <jcm04@naver.com>
2023-05-07 02:24:38 +0900

    feat: 인증 검증 로직 추가

--- src/main/java/cart/controller/authentication/AuthInfo.java
+++ src/main/java/cart/controller/authentication/AuthInfo.java
@@ -1,11 +1,16 @@
 package cart.controller.authentication;
 
+import cart.exception.AuthorizationException;
+
 public class AuthInfo {
 
     private final String email;
     private final String password;
 
     public AuthInfo(final String email, final String password) {
+        if (email == null || password == null) {
+            throw new AuthorizationException("회원 정보가 입력되지 않았습니다.");
+        }
         this.email = email;
         this.password = password;
     }
