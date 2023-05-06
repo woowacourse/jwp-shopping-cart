19a6c23c8db34dd4e20d0a7675a9ccf7fadc4147
kiarakim <kiarakim413@gmail.com>
2023-05-06 14:26:59 +0900

    feat: 장바구니 페이지 연동

---
+++ src/main/java/cart/controller/view/CartViewController.java
@@ -0,0 +1,12 @@
+package cart.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CartViewController {
+	@GetMapping("/cart")
+	public String showCarts() {
+		return "cart";
+	}
+}
