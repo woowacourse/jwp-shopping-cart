3ae975ab005ab3efce24dcc15af862b7c8b8e9b8
kiarakim <kiarakim413@gmail.com>
2023-05-06 15:07:06 +0900

    fix: 장바구니 상품 저장 및 조회 테스트 코드 수정

--- src/test/java/cart/repository/CartRepositoryTest.java
+++ src/test/java/cart/repository/CartRepositoryTest.java
@@ -1,5 +1,7 @@
 package cart.repository;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import cart.domain.cart.Cart;
-import cart.domain.cart.CartId;
 import cart.domain.member.Member;
 import cart.domain.member.MemberId;
 import cart.domain.product.Product;
@@ -36,25 +37,21 @@ class CartRepositoryTest {
 		productRepository = new ProductRepository(jdbcTemplate);
 	}
 
-	@DisplayName("장바구니 상품 저장 테스트")
+	@DisplayName("장바구니 상품 저장 및 조회 테스트")
 	@Test
 	void insert() {
 		// given
 		final MemberId memberId = memberRepository.insert(MEMBER);
 		final ProductId chickenId = productRepository.insert(CHICKEN);
+		final ProductId pizzaId = productRepository.insert(PIZZA);
 
 		// when
-		final CartId cartId = cartRepository.insert(memberId, chickenId);
+		cartRepository.insert(memberId, chickenId);
+		cartRepository.insert(memberId, pizzaId);
 
-		final Cart expectCart = cartRepository.findByCartId(cartId);
+		final List<Cart> carts = cartRepository.findAllByMemberId(memberId);
 
 		// then
-		Assertions.assertThat(expectCart)
-			.usingRecursiveComparison()
-			.isEqualTo(new Cart(
-				cartId,
-				memberId,
-				chickenId
-			));
+		Assertions.assertThat(carts.size()).isEqualTo(2);
 	}
 }
