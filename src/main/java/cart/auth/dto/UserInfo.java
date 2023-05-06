4573e8d3f8022f1225a9bbcf0a0235d1bd59dbf5
echo <echo.backend@gmail.com>
2023-05-07 08:36:40 +0900

    refactor: UserDTO 수정
    UserRequestDTO -> UserInfo
    UserResponseDTO: id 추가

--- src/main/java/cart/auth/dto/UserInfo.java
+++ src/main/java/cart/auth/dto/UserInfo.java
@@ -3,18 +3,18 @@ package cart.auth.dto;
 import cart.auth.domain.Email;
 import cart.auth.domain.Password;
 
-public class UserRequestDTO {
+public class UserInfo {
     
     private final Email email;
     private final Password password;
     
-    public UserRequestDTO(final Email email, final Password password) {
+    public UserInfo(final Email email, final Password password) {
         this.email = email;
         this.password = password;
     }
     
-    public static UserRequestDTO of(final String email, final String password) {
-        return new UserRequestDTO(new Email(email), new Password(password));
+    public static UserInfo of(final String email, final String password) {
+        return new UserInfo(new Email(email), new Password(password));
     }
     
     public Email getEmail() {
--- src/main/java/cart/auth/dto/UserResponseDTO.java
+++ src/main/java/cart/auth/dto/UserResponseDTO.java
@@ -6,20 +6,23 @@ public class UserResponseDTO {
     
     private final String email;
     private final String password;
+    private final long id;
     
-    public UserResponseDTO(final String email, final String password) {
+    public UserResponseDTO(final long id, final String email, final String password) {
+        this.id = id;
         this.email = email;
         this.password = password;
     }
     
     public static UserResponseDTO from(final User user) {
+        final long id = user.getId();
         final String email = user.getEmail().getValue();
         final String password = user.getPassword().getValue();
-        return new UserResponseDTO(email, password);
+        return new UserResponseDTO(id, email, password);
     }
     
-    public static UserResponseDTO from(final String email, final String password) {
-        return new UserResponseDTO(email, password);
+    public long getId() {
+        return this.id;
     }
     
     public String getEmail() {
