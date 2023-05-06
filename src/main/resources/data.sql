c96a8ac2d64b906befd908c375974db9bb81cb68
hyunseo <ohseo8813@gmail.com>
2023-05-06 15:38:48 +0900

    refactor: 테이블 생성과 기타 쿼리 실행을 schema.sql - data.sql로 분리

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,30 +1,3 @@
-CREATE TABLE if not exists PRODUCT
-(
-    id        BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
-    name      VARCHAR(100)                   NOT NULL,
-    price     INT                            NOT NULL,
-    image_url VARCHAR(255)                   NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE if not exists MEMBER
-(
-    id       BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
-    email    VARCHAR(100) unique            NOT NULL,
-    password VARCHAR(255)                   NOT NULL,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE if not exists CART_PRODUCT
-(
-    id         BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
-    member_id  BIGINT UNSIGNED                NOT NULL,
-    product_id BIGINT UNSIGNED                NOT NULL,
-    FOREIGN KEY (member_id) REFERENCES MEMBER (id) on delete CASCADE,
-    FOREIGN KEY (product_id) REFERENCES PRODUCT (id) on delete CASCADE
-);
-
-
 insert into MEMBER(email, password)
 values ('emailA@naver.com', 'passwordA'),
        ('emailB@kakao.com', 'passwordB');
