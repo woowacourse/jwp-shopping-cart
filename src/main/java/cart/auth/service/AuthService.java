195694a6d1f019f96613f9436a31c8947e4bba52
echo <echo.backend@gmail.com>
2023-05-07 08:39:05 +0900

    refactor: Settings 패키지의 User관련 클래스 auth로 패키지 변경

--- src/main/java/cart/auth/service/AuthService.java
+++ src/main/java/cart/auth/service/AuthService.java
@@ -1,18 +1,19 @@
-package cart.settings.service;
+package cart.auth.service;
 
 import cart.auth.dao.UserDAO;
 import cart.auth.domain.User;
+import cart.auth.dto.UserInfo;
 import cart.auth.dto.UserResponseDTO;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SettingsService {
+public class AuthService {
     
     private final UserDAO userDAO;
     
-    public SettingsService(final UserDAO userDAO) {
+    public AuthService(final UserDAO userDAO) {
         this.userDAO = userDAO;
     }
     
@@ -21,5 +22,10 @@ public class SettingsService {
         return users.stream()
                 .map(UserResponseDTO::from)
                 .collect(Collectors.toList());
+    }
+    
+    public UserResponseDTO findUser(final UserInfo userInfo) {
+        final User user = this.userDAO.find(userInfo);
+        return UserResponseDTO.from(user);
     }
 }
--- src/main/java/cart/settings/controller/SettingsController.java
+++ src/main/java/cart/settings/controller/SettingsController.java
@@ -1,7 +1,7 @@
 package cart.settings.controller;
 
 import cart.auth.dto.UserResponseDTO;
-import cart.settings.service.SettingsService;
+import cart.auth.service.AuthService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,15 +10,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SettingsController {
     
-    private final SettingsService settingsService;
+    private final AuthService authService;
     
-    public SettingsController(final SettingsService settingsService) {
-        this.settingsService = settingsService;
+    public SettingsController(final AuthService authService) {
+        this.authService = authService;
     }
     
     @GetMapping("/settings")
     public String settings(final Model model) {
-        final List<UserResponseDTO> allUsers = this.settingsService.findAllUsers();
+        final List<UserResponseDTO> allUsers = this.authService.findAllUsers();
         model.addAttribute("users", allUsers);
         return "settings";
     }
