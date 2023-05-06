838b7bea2a81db2d2c7a9e747be8495e37a73bf1
Woojin Lee <dhkep03@gmail.com>
2023-05-06 12:40:50 +0900

    feat: User에 id 필드 추가

--- src/main/java/cart/domain/user/User.java
+++ src/main/java/cart/domain/user/User.java
@@ -4,18 +4,24 @@ import java.util.Optional;
 
 public class User {
 
+    private final Long id;
     private final String email;
     private final String password;
     private final UserPrivacy userPrivacy;
 
-    private User(String email, String password, UserPrivacy userPrivacy) {
+    private User(Long id, String email, String password, UserPrivacy userPrivacy) {
+        this.id = id;
         this.email = email;
         this.password = password;
         this.userPrivacy = userPrivacy;
     }
 
-    public static User of(String email, String password, String name, String phoneNumber) {
-        return new User(email, password, new UserPrivacy(name, phoneNumber));
+    public static User create(Long id, String email, String password, String name, String phoneNumber) {
+        return new User(id, email, password, new UserPrivacy(name, phoneNumber));
+    }
+
+    public static User createToSave(String email, String password, String name, String phoneNumber) {
+        return new User(null, email, password, new UserPrivacy(name, phoneNumber));
     }
 
     public String getEmail() {
--- src/test/java/cart/domain/user/UserTest.java
+++ src/test/java/cart/domain/user/UserTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 
 class UserTest {
 
-    @DisplayName("이메일, 비밀번호, 이름, 휴대전화 번호를 가진 User를 생성한다.")
+    @DisplayName("ID, 이메일, 비밀번호, 이름, 휴대전화 번호를 가진 User를 생성한다.")
     @Test
     void shouldSuccessToCreateUserWhenRequest() {
-        assertDoesNotThrow(() -> User.of(
+        assertDoesNotThrow(() -> User.create(
+                1L,
                 "test@test.com",
                 "1234abcd!@",
                 "김철수",
@@ -18,10 +19,11 @@ class UserTest {
         ));
     }
 
-    @DisplayName("이메일, 비밀번호, 휴대전화 번호를 가진 User를 생성한다.")
+    @DisplayName("ID, 이메일, 비밀번호, 휴대전화 번호를 가진 User를 생성한다.")
     @Test
     void shouldSuccessToCreateUserWhenCreateWithoutName() {
-        assertDoesNotThrow(() -> User.of(
+        assertDoesNotThrow(() -> User.create(
+                1L,
                 "test@test.com",
                 "1234abcd!@",
                 null,
@@ -29,10 +31,11 @@ class UserTest {
         ));
     }
 
-    @DisplayName("이메일, 비밀번호, 이름을 가진 User를 생성한다.")
+    @DisplayName("ID, 이메일, 비밀번호, 이름을 가진 User를 생성한다.")
     @Test
     void shouldSuccessToCreateUserWhenCreateWithoutPhoneNumber() {
-        assertDoesNotThrow(() -> User.of(
+        assertDoesNotThrow(() -> User.create(
+                1L,
                 "test@test.com",
                 "1234abcd!@",
                 "김철수",
@@ -40,14 +43,26 @@ class UserTest {
         ));
     }
 
-    @DisplayName("이메일, 비밀번호를 가진 User를 생성한다.")
+    @DisplayName("ID, 이메일, 비밀번호를 가진 User를 생성한다.")
     @Test
     void shouldSuccessToCreateUserWhenCreateWithoutNameAndPhoneNumber() {
-        assertDoesNotThrow(() -> User.of(
+        assertDoesNotThrow(() -> User.create(
+                1L,
                 "test@test.com",
                 "1234abcd!@",
                 null,
                 null
+        ));
+    }
+
+    @DisplayName("ID가 없는 User를 생성한다.")
+    @Test
+    void shouldSuccessToCreateUserWhenCreateWithoutId() {
+        assertDoesNotThrow(() -> User.createToSave(
+                "test@test.com",
+                "1234abcd!@",
+                "김철수",
+                "01012341234"
         ));
     }
 }
