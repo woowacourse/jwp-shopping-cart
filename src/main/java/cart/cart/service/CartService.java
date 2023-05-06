1b383636cf2f9215dc7ec1be1b5d3d86c44d5c18
echo <echo.backend@gmail.com>
2023-05-07 08:39:44 +0900

    feat: CartService 구현

---
+++ src/main/java/cart/cart/service/CartService.java
@@ -0,0 +1,45 @@
+package cart.cart.service;
+
+import cart.cart.dao.CartDAO;
+import cart.cart.domain.Cart;
+import cart.cart.dto.CartRequestDTO;
+import cart.catalog.dao.CatalogDao;
+import cart.catalog.dto.ResponseProductDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartService {
+    
+    private final CartDAO cartDAO;
+    private final CatalogDao catalogDao;
+    
+    public CartService(final CartDAO cartDAO, final CatalogDao catalogDao) {
+        this.cartDAO = cartDAO;
+        this.catalogDao = catalogDao;
+    }
+    
+    public List<ResponseProductDto> findUserCart(final long userId) {
+        final List<Cart> carts = this.cartDAO.findUserCart(userId);
+        return carts.stream()
+                .map(Cart::getProductId)
+                .map(this.catalogDao::findByID)
+                .map(ResponseProductDto::create)
+                .collect(Collectors.toList());
+    }
+    
+    public void createCart(final CartRequestDTO cartRequestDTO) {
+        this.cartDAO.create(cartRequestDTO);
+    }
+    
+    public void deleteCart(final CartRequestDTO cartRequestDTO) {
+        final Cart cart = this.cartDAO.find(cartRequestDTO);
+        this.cartDAO.delete(cart);
+    }
+    
+    public void clearCart(final CartRequestDTO cartRequestDTO) {
+        final long userId = cartRequestDTO.getUserId();
+        this.cartDAO.clear(userId);
+    }
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -32,3 +32,7 @@ CREATE TABLE carts (
     ON DELETE CASCADE
 );
 
+INSERT INTO carts (product_id,user_id) VALUES(1,1);
+INSERT INTO carts (product_id,user_id) VALUES(2,1);
+INSERT INTO carts (product_id,user_id) VALUES(3,1);
+
