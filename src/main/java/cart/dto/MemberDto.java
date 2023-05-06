02404e0fc3be1e49acc7799b4454bb5f50cc566c
yeonkkk <tjddus0302@gmail.com>
2023-05-07 03:50:18 +0900

    test: 설정 페이지 테스트 추가

--- src/main/java/cart/dto/MemberDto.java
+++ src/main/java/cart/dto/MemberDto.java
@@ -2,10 +2,12 @@ package cart.dto;
 
 import cart.entity.MemberEntity;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public class MemberDto {
     private final long id;
