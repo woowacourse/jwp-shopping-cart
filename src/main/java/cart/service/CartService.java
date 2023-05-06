e9fc030515006c73776b0cdf71821d9d5ed56f04
kiarakim <kiarakim413@gmail.com>
2023-05-06 18:32:55 +0900

    feat: 장바구니 상풍 저장 및 조회 서비스 구현

--- src/main/java/cart/service/CartService.java
+++ src/main/java/cart/service/CartService.java
@@ -2,8 +2,13 @@ package cart.service;
 
 import java.util.List;
 
+import cart.domain.cart.CartId;
+import cart.domain.member.MemberId;
+import cart.domain.product.ProductId;
 import cart.service.response.CartResponse;
 
 public interface CartService {
+	CartId insert(MemberId memberId, ProductId productId);
+
 	List<CartResponse> findAllByEmail(String email);
 }
--- src/main/java/cart/service/CartServiceImpl.java
+++ src/main/java/cart/service/CartServiceImpl.java
@@ -1,15 +1,48 @@
 package cart.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import cart.domain.cart.CartId;
+import cart.domain.member.MemberId;
+import cart.domain.product.Product;
+import cart.domain.product.ProductId;
+import cart.repository.CartRepository;
+import cart.repository.ProductRepository;
 import cart.service.response.CartResponse;
 
 @Service
 public class CartServiceImpl implements CartService {
+	private final CartRepository cartRepository;
+	private final ProductRepository productRepository;
+
+	public CartServiceImpl(final CartRepository cartRepository, final ProductRepository productRepository) {
+		this.cartRepository = cartRepository;
+		this.productRepository = productRepository;
+	}
+
+	@Transactional
+	@Override
+	public CartId insert(final MemberId memberId, final ProductId productId) {
+		return cartRepository.insert(memberId, productId);
+	}
+
+	@Transactional
 	@Override
 	public List<CartResponse> findAllByEmail(final String email) {
-		return List.of(new CartResponse(1L, "https://img.freepik.com/premium-photo/red-apples-isolated-on-white-background-ripe-fresh-apples-clipping-path-apple-with-leaf_299651-595.jpg", "apple", 1000));
+		return cartRepository.findAllByEmail(email)
+			.stream()
+			.map(cart -> {
+				final Product product = productRepository.findByProductId(cart.getProductId());
+				return new CartResponse(cart.getId().getId(),
+					product.getImage(),
+					product.getName(),
+					product.getPrice()
+				);
+			})
+			.collect(Collectors.toList());
 	}
 }
