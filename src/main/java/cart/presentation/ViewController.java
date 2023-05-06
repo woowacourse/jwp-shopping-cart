c76241c0387a784c40f33ab31f9e99645ea401c0
eunbii0213 <eunbii0213@naver.com>
2023-05-07 03:27:27 +0900

    fix: 사용자 선택 페이지 최초 실행시에만 초기 사용자 생성

--- src/main/java/cart/presentation/ViewController.java
+++ src/main/java/cart/presentation/ViewController.java
@@ -1,6 +1,10 @@
 package cart.presentation;
 
+import cart.business.CartProductService;
 import cart.business.CartService;
+import cart.business.MemberService;
+import cart.business.ProductService;
+import cart.presentation.dto.CartResponse;
 import cart.presentation.dto.MemberRequest;
 import cart.presentation.dto.MemberResponse;
 import cart.presentation.dto.ProductResponse;
@@ -17,47 +21,49 @@ public class ViewController {
     //interface bean을 어떻게 주입하는가? 어떻게 알고 주입하는가?에 대한 내용임
     // 방법은 여러가지. 1. 우선순위주기 @Primary 2. 이름으로 주입, 3. @Qualifier
 
+    private final ProductService productService;
+    private final MemberService memberService;
     private final CartService cartService;
+    private final CartProductService cartProductService;
 
-    public ViewController(CartService productService) {
-        this.cartService = productService;
+    public ViewController(ProductService productService, MemberService memberService, CartService cartService, CartProductService cartProductService) {
+        this.productService = productService;
+        this.memberService = memberService;
+        this.cartService = cartService;
+        this.cartProductService = cartProductService;
     }
 
     @GetMapping("/")
     public String home(Model model) {
-        List<ProductResponse> products = cartService.readProduct().stream()
-                .map(product -> new ProductResponse(product.getId(),
-                        product.getName(),
-                        product.getUrl(),
-                        product.getPrice()))
-                .collect(Collectors.toList());
+        List<ProductResponse> products = productService.read().stream().map(product -> new ProductResponse(product.getId(), product.getName(), product.getUrl(), product.getPrice())).collect(Collectors.toList());
         model.addAttribute("products", products);
         return "index";
     }
 
     @GetMapping("/admin")
     public String admin(Model model) {
-        List<ProductResponse> products = cartService.readProduct().stream()
-                .map(product -> new ProductResponse(product.getId(),
-                        product.getName(),
-                        product.getUrl(),
-                        product.getPrice()))
-                .collect(Collectors.toList());
+        List<ProductResponse> products = productService.read().stream().map(product -> new ProductResponse(product.getId(), product.getName(), product.getUrl(), product.getPrice())).collect(Collectors.toList());
         model.addAttribute("products", products);
         return "admin";
     }
 
     @GetMapping("/settings")
     public String settings(Model model) {
-        cartService.createMember(new MemberRequest("coding_judith@gmail.com", "judy123"));
-        cartService.createMember(new MemberRequest("coding_teo@gmail.com", "teo123"));
+        if (memberService.read().size() == 0) {
+            memberService.create(new MemberRequest("coding_judith@gmail.com", "judy123"));
+            memberService.create(new MemberRequest("coding_teo@gmail.com", "teo123"));
+        }
 
-        List<MemberResponse> members = cartService.readMembers().stream()
-                .map(member -> new MemberResponse(member.getId(),
-                        member.getEmail(),
-                        member.getPassword()))
-                .collect(Collectors.toList());
+        List<MemberResponse> members = memberService.read().stream().map(member -> new MemberResponse(member.getId(), member.getEmail(), member.getPassword())).collect(Collectors.toList());
         model.addAttribute("members", members);
         return "settings";
+    }
+
+    @GetMapping("/cart")
+    public String cart(Model model) {
+
+        List<CartResponse> carts = cartService.read().stream().map(cart -> new CartResponse(cart.getId(), cart.getMemberId(), cartService.findProductsByMemberId(cart.getMemberId()))).collect(Collectors.toList());
+        model.addAttribute("cart", carts);
+        return "cart";
     }
 }
