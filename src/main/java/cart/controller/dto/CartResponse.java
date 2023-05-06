8adb312b1785537b33c4d8a5ce4bbec0f33d87f1
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-06 18:04:20 +0900

    feat: CartService 사용자 아이디에 따른 조회 기능 추가
    - CartResponse 반환 dto 생성
    - AuthInfo 계정 요청 dto 생성
    - Service에서 이메일이 아닌 AuthInfo를 받도록 수정

---
+++ src/main/java/cart/controller/dto/CartResponse.java
@@ -0,0 +1,43 @@
+package cart.controller.dto;
+
+import cart.domain.CartData;
+
+public class CartResponse {
+
+    private final Long id;
+    private final String name;
+    private final String imageUrl;
+    private final Integer price;
+
+    private CartResponse(Long id, String name, String imageUrl, Integer price) {
+        this.id = id;
+        this.name = name;
+        this.imageUrl = imageUrl;
+        this.price = price;
+    }
+
+    public static CartResponse from(CartData cartData) {
+        return new CartResponse(
+                cartData.getId(),
+                cartData.getName(),
+                cartData.getImageUrl(),
+                cartData.getPrice()
+        );
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+}
--- src/main/java/cart/service/CartService.java
+++ src/main/java/cart/service/CartService.java
@@ -1,24 +1,51 @@
 package cart.service;
 
+import cart.controller.auth.dto.AuthInfo;
 import cart.controller.dto.CartRequest;
+import cart.controller.dto.CartResponse;
 import cart.dao.CartDao;
+import cart.dao.UserDao;
 import cart.domain.Cart;
+import cart.domain.CartData;
+import cart.domain.User;
+import cart.exception.NotFoundResultException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class CartService {
 
     private final CartDao cartDao;
+    private final UserDao userDao;
 
-    public CartService(CartDao cartDao) {
+    public CartService(CartDao cartDao, UserDao userDao) {
         this.cartDao = cartDao;
+        this.userDao = userDao;
     }
 
-    public Long saveCart(final CartRequest cartRequest) {
+    public Long saveCart(final AuthInfo authInfo, final CartRequest cartRequest) {
+        Long userId = findIdByEmail(authInfo);
         Cart cart = new Cart.Builder()
-                .userId(cartRequest.getUserId())
+                .userId(userId)
                 .itemId(cartRequest.getItemId())
                 .build();
         return cartDao.save(cart);
+    }
+
+    public List<CartResponse> loadAllCart(final AuthInfo authInfo) {
+        Long userId = findIdByEmail(authInfo);
+        List<CartData> allCart = cartDao.findAll(userId);
+        return allCart.stream()
+                      .map(CartResponse::from)
+                      .collect(Collectors.toList());
+    }
+
+    private Long findIdByEmail(final AuthInfo authInfo) {
+        Optional<User> findUser = userDao.findByEmail(authInfo.getEmail());
+        User user = findUser.orElseThrow(() -> new NotFoundResultException("존재하지 않는 사용자 입니다."));
+        return user.getId();
     }
 }
