4838b1a2b68e6a4ab31f4871f22c403b28b25a57
rawfishthelgh <rg970604@naver.com>
2023-05-07 00:56:26 +0900

    refactor : 더미데이터 쿼리문 test로 이동

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,11 +28,3 @@ CREATE TABLE IF NOT EXISTS CART
     FOREIGN KEY (product_id) REFERENCES PRODUCT(id),
     FOREIGN KEY (member_id) REFERENCES MEMBER(id)
 );
-
-INSERT INTO MEMBER (email, password) VALUES ('rg970604@naver.com','password');
-INSERT INTO MEMBER (email, password) VALUES ('yimsh66@naver.com','password');
-
-INSERT INTO PRODUCT (name, image, price) VALUES ('치킨','https://nenechicken.com/17_new/images/menu/30005.jpg',18000);
-INSERT INTO PRODUCT (name, image, price) VALUES ('피자','https://cdn.dominos.co.kr/admin/upload/goods/20230117_97ySneQn.jpg?RS=350x350&SP=1',21000);
-INSERT INTO PRODUCT (name, image, price) VALUES ('막국수','https://gdimg.gmarket.co.kr/1797034760/still/600?ver=1621574924',9000);
-
