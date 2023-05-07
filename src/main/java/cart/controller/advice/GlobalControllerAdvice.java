20a672733fda16623b60773d45b2c1ed3f3310e5
Ethan <kim76744dev@gmail.com>
2023-05-07 13:18:56 +0900

    refactor: 예상치 못한 에러가 발생할 경우 메시지 변경

--- src/main/java/cart/controller/advice/GlobalControllerAdvice.java
+++ src/main/java/cart/controller/advice/GlobalControllerAdvice.java
@@ -18,14 +18,13 @@ import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 @RestControllerAdvice
 public class GlobalControllerAdvice {
 
-    private static final String UNEXPECTED_ERROR_MESSAGE = "네트워크 연결 상태가 좋지 않습니다 잠시 후 다시 시도해 주세요.";
+    private static final String UNEXPECTED_ERROR_MESSAGE = "서버가 응답할 수 없습니다.";
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @ExceptionHandler
     public ResponseEntity<ErrorResponse> handleUnexpectedException(final Exception exception) {
-        final String errorMessage = exception.getMessage();
-        logger.error("error : {}", errorMessage);
+        logger.error("error : {}", exception.getMessage());
 
         return ResponseEntity.internalServerError().body(new ErrorResponse(UNEXPECTED_ERROR_MESSAGE));
     }
