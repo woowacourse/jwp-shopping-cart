4823b77af1c5fb9e028ff732623a15d444f5a9f1
kokodak <kokodakadokok@gmail.com>
2023-05-07 11:31:50 +0900

    test: 어드민 컨트롤러에 대한 테스트 코드 작성

--- src/test/java/cart/controller/AdminControllerTest.java
+++ src/test/java/cart/controller/AdminControllerTest.java
@@ -1,96 +1,123 @@
 package cart.controller;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.doNothing;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import cart.dto.CreateProductRequest;
-import cart.dto.ProductDto;
 import cart.dto.UpdateProductRequest;
 import cart.service.ProductService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.ArrayList;
-import java.util.List;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.jdbc.Sql;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-@WebMvcTest(AdminController.class)
+@Sql("/init.sql")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class AdminControllerTest {
 
     @Autowired
-    private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    @MockBean
     private ProductService productService;
 
-    @Test
-    void 어드민_페이지를_조회한다() throws Exception {
-        List<ProductDto> products = new ArrayList<>();
-        given(productService.findAllProduct())
-                .willReturn(products);
+    @LocalServerPort
+    private int port;
 
-        mockMvc.perform(get("/admin"))
-                .andExpect(model().attributeExists("products"))
-                .andExpect(model().attribute("products", products))
-                .andExpect(status().isOk());
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = this.port;
     }
 
     @Test
-    void 상품을_추가한다() throws Exception {
-        final CreateProductRequest request = new CreateProductRequest("하디", "imageUrl", 100000);
-        final String requestBody = objectMapper.writeValueAsString(request);
-        doNothing()
-                .when(productService)
-                .addProduct(any());
-
-        mockMvc.perform(post("/admin/products")
-                        .content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isCreated());
+    void 어드민_페이지를_조회한다() {
+        RestAssured
+                .given()
+                .when().get("/admin")
+                .then()
+                .statusCode(HttpStatus.OK.value());
     }
 
     @Test
-    void 상품을_업데이트한다() throws Exception {
-        final Long id = 1L;
-        final UpdateProductRequest request = new UpdateProductRequest("코코닥", "imageUrl", 15000);
-        final String requestBody = objectMapper.writeValueAsString(request);
-        doNothing()
-                .when(productService)
-                .updateProduct(any(), any());
-
-        mockMvc.perform(patch("/admin/products/{product_id}", id)
-                        .content(requestBody)
-                        .contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isCreated());
+    void 유효한_추가_요청이라면_201_응답코드를_반환한다() {
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new CreateProductRequest("name", "imageUrl", 1000))
+                .when().post("/admin/products")
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
     }
 
     @Test
-    void 상품을_삭제한다() throws Exception {
-        final Long id = 1L;
+    void 유효하지_않은_추가_요청이라면_400_응답코드를_반환한다() {
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new CreateProductRequest("name", "imageUrl", -100))
+                .when().post("/admin/products")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
 
-        doNothing()
-                .when(productService)
-                .deleteProduct(anyLong());
+    @Test
+    void 유효한_수정_요청이라면_201_응답코드를_반환한다() {
+        productService.addProduct(new CreateProductRequest("name", "imageUrl", 1000));
 
-        mockMvc.perform(delete("/admin/products/{product_id}", id))
-                .andExpect(status().isOk());
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new UpdateProductRequest("name", "imageUrl", 50000))
+                .when().patch("/admin/products/" + 1L)
+                .then()
+                .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void 유효하지_않은_수정_요청이라면_400_응답코드를_반환한다() {
+        productService.addProduct(new CreateProductRequest("name", "imageUrl", 1000));
+
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new UpdateProductRequest("name", "imageUrl", -100))
+                .when().patch("/admin/products/" + 1L)
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    void 존재하지_않는_상품에_대한_수정_요청이라면_404_응답코드를_반환한다() {
+        RestAssured
+                .given()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(new UpdateProductRequest("name", "imageUrl", 50000))
+                .when().patch("/admin/products/" + 123L)
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    void 유효한_삭제_요청이라면_200_응답코드를_반환한다() {
+        productService.addProduct(new CreateProductRequest("name", "imageUrl", 1000));
+
+        RestAssured
+                .given()
+                .when().delete("/admin/products/" + 1L)
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    void 존재하지_않는_상품에_대한_삭제_요청이라면_404_응답코드를_반환한다() {
+        RestAssured
+                .given()
+                .when().delete("/admin/products/" + 1L)
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
     }
 }
