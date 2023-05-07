6cc0b12cd9a2cddee92075058e978da38f7a7e28
jjongwa <troas96@naver.com>
2023-05-08 00:06:59 +0900

    refactor: extract 내부 로직 메서드 분리

--- src/main/java/cart/auth/BasicAuthorizationExtractor.java
+++ src/main/java/cart/auth/BasicAuthorizationExtractor.java
@@ -2,29 +2,41 @@ package cart.auth;
 
 import org.apache.tomcat.util.codec.binary.Base64;
 
+import javax.naming.AuthenticationException;
+
 public class BasicAuthorizationExtractor {
 
     private static final String BASIC_TYPE = "Basic";
     private static final String DELIMITER = ":";
 
-    public AuthInfo extract(String header) {
+    public AuthInfo extract(String header) throws AuthenticationException {
         if (header == null) {
             return null;
         }
 
-        if ((header.toLowerCase().startsWith(BASIC_TYPE.toLowerCase()))) {
-            String authHeaderValue = header.substring(BASIC_TYPE.length()).trim();
-            byte[] decodedBytes = Base64.decodeBase64(authHeaderValue);
-            String decodedString = new String(decodedBytes);
-
-            String[] credentials = decodedString.split(DELIMITER);
-            String email = credentials[0];
-            String password = credentials[1];
-            System.out.println("email = " + email);
-            System.out.println("password = " + password);
-            return new AuthInfo(email, password);
+        if (isStartsWithBasic(header)) {
+            String decodedString = decodeHeader(header);
+            return makeAuthInfo(decodedString);
         }
 
-        return null;
+        throw new AuthenticationException("해당 정보의 유저가 없습니다.");
     }
+
+    private String decodeHeader(String header) {
+        String authHeaderValue = header.substring(BASIC_TYPE.length()).trim();
+        byte[] decodedBytes = Base64.decodeBase64(authHeaderValue);
+        return new String(decodedBytes);
+    }
+
+    private AuthInfo makeAuthInfo(String decodedString) {
+        String[] credentials = decodedString.split(DELIMITER);
+        String email = credentials[0];
+        String password = credentials[1];
+        return new AuthInfo(email, password);
+    }
+
+    private boolean isStartsWithBasic(String header) {
+        return header.toLowerCase().startsWith(BASIC_TYPE.toLowerCase());
+    }
+
 }
