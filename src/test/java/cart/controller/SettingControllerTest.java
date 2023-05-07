ce3473cd5e2b5da5f356ba9aa4dbad3b8af9e7c2
kokodak <kokodakadokok@gmail.com>
2023-05-07 11:32:33 +0900

    test: 설정 컨트롤러에 대한 테스트 코드 작성

---
+++ src/test/java/cart/controller/SettingControllerTest.java
@@ -0,0 +1,34 @@
+package cart.controller;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class SettingControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = this.port;
+    }
+
+    @Test
+    void 설정_페이지를_조회한다() {
+        RestAssured
+                .given()
+                .when().get("/settings")
+                .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+}
