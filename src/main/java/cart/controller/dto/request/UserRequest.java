c095191753914c0289ce66ab95d51a01c6147eac
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-07 18:20:04 +0900

    feat: UserRequest 검증 추가

--- src/main/java/cart/controller/dto/request/UserRequest.java
+++ src/main/java/cart/controller/dto/request/UserRequest.java
@@ -1,17 +1,18 @@
 package cart.controller.dto.request;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 public class UserRequest {
 
-    @NotNull(message = "이메일은 빈 값일 수 없습니다.")
     @NotEmpty(message = "이메일은 빈 값일 수 없습니다.")
+    @Length(max = 50, message = "이메일은 50자 이하로 입력해주세요.")
     @Email(message = "이메일 형식으로 입력해주세요.")
     private final String email;
-    @NotNull(message = "비밀번호는 빈 값일 수 없습니다.")
     @NotEmpty(message = "비밀번호는 빈 값일 수 없습니다.")
+    @Length(max = 20, message = "비밀번호는 20자 이하로 입력해주세요.")
     private final String password;
 
     public UserRequest(String email, String password) {
