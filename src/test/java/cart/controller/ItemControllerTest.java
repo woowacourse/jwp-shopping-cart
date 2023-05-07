98db79f1e96df0d9213c08046d883b4cac007a4b
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-07 11:49:23 +0900

    refactor: 요청 url을 user에서 users로 수정

--- src/test/java/cart/controller/ItemControllerTest.java
+++ src/test/java/cart/controller/ItemControllerTest.java
@@ -29,7 +29,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-@WebMvcTest(value = ItemController.class)
+@WebMvcTest(ItemController.class)
 class ItemControllerTest {
 
     @MockBean
