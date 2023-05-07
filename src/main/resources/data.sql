4bd251b65167166d094f26927ea688819af9a5a5
0chil <0@chll.it>
2023-05-07 15:43:11 +0900

    chore: 더미 데이터 업로드

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO USERS (email, password)
+VALUES ('0@chll.it', 'verysecurepassword');
+INSERT INTO USERS (email, password)
+VALUES ('beaver@wooteco.com', 'veryverysecurepassword');
+
+INSERT INTO PRODUCT(name, image, price)
+VALUES ('치킨', 'https://user-images.githubusercontent.com/39221443/235916727-e62f14ed-ad8a-49df-958d-8783d8820152.png', '23000');
+INSERT INTO PRODUCT(name, image, price)
+VALUES ('바닐라 라떼', 'https://globalassets.starbucks.com/assets/1b317a079984402895bb5cf4d2894940.jpg?impolicy=1by1_wide_topcrop_630', '3800');
