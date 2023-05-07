54352d40d040330783cee3fdb3fc82785e928a1d
dhkim <akdlzmf1123@gmail.com>
2023-05-07 19:28:58 +0900

    refactor: 메서드 분리

--- src/main/java/cart/authorization/BasicAuthorizationExtractor.java
+++ src/main/java/cart/authorization/BasicAuthorizationExtractor.java
@@ -12,25 +12,35 @@ public class BasicAuthorizationExtractor implements AuthorizationExtractor<Membe
 
     private static final String BASIC_TYPE = "Basic";
     private static final String DELIMITER = ":";
+    private static final int EMAIL_INDEX = 0;
+    private static final int PASSWORD_INDEX = 1;
 
     public MemberAuthorizationInfo extract(HttpServletRequest request) {
-        String header = request.getHeader(AUTHORIZATION);
-        if (header == null) {
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader == null) {
             return null;
         }
 
-        if ((header.toLowerCase().startsWith(BASIC_TYPE.toLowerCase()))) {
-            String authHeaderValue = header.substring(BASIC_TYPE.length()).trim();
-            byte[] decodedBytes = Base64.decodeBase64(authHeaderValue);
-            String decodedString = new String(decodedBytes);
+        if (isAuthorizationHeaderBasic(authorizationHeader)) {
+            String decodedString = decodeAuthorizationHeader(authorizationHeader);
 
             String[] credentials = decodedString.split(DELIMITER);
-            String email = credentials[0];
-            String password = credentials[1];
+            String email = credentials[EMAIL_INDEX];
+            String password = credentials[PASSWORD_INDEX];
 
             return new MemberAuthorizationInfo(email, password);
         }
 
         return null;
+    }
+
+    private boolean isAuthorizationHeaderBasic(String header) {
+        return header.toLowerCase().startsWith(BASIC_TYPE.toLowerCase());
+    }
+
+    private String decodeAuthorizationHeader(String authorizationHeader) {
+        String authHeaderValue = authorizationHeader.substring(BASIC_TYPE.length()).trim();
+        byte[] decodedBytes = Base64.decodeBase64(authHeaderValue);
+        return new String(decodedBytes);
     }
 }
