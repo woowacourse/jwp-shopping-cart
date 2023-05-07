c839129ed2f56d94a8eeda2803c850c35732a9d4
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-07 17:20:19 +0900

    refactor: UserRequest 예외 메시지 추가

--- src/main/java/cart/controller/dto/request/UserRequest.java
+++ src/main/java/cart/controller/dto/request/UserRequest.java
@@ -6,12 +6,12 @@ import javax.validation.constraints.NotNull;
 
 public class UserRequest {
 
-    @NotNull
-    @NotEmpty
-    @Email
+    @NotNull(message = "이메일은 빈 값일 수 없습니다.")
+    @NotEmpty(message = "이메일은 빈 값일 수 없습니다.")
+    @Email(message = "이메일 형식으로 입력해주세요.")
     private final String email;
-    @NotNull
-    @NotEmpty
+    @NotNull(message = "비밀번호는 빈 값일 수 없습니다.")
+    @NotEmpty(message = "비밀번호는 빈 값일 수 없습니다.")
     private final String password;
 
     public UserRequest(String email, String password) {
