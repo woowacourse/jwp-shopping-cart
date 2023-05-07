df58d10e4a1bd913feacb68a75d5f6bbfedf7f76
kiarakim <kiarakim413@gmail.com>
2023-05-08 00:50:47 +0900

    feat: email, password 유효성 검사 기능 구현

--- src/main/java/cart/auth/application/AuthService.java
+++ src/main/java/cart/auth/application/AuthService.java
@@ -11,6 +11,11 @@ public class AuthService {
 		this.memberRepository = memberRepository;
 	}
 
+	public boolean checkInvalidLogin(String principal, String credentials) {
+		final Member member = memberRepository.findByEmail(principal);
+		return !member.getEmail().equals(principal) || !member.getPassword().equals(credentials);
+	}
+
 	public MemberResponse findMember(final String email) {
 		final Member member = memberRepository.findByEmail(email);
 		return new MemberResponse(member.getId().getId(), member.getName(), member.getEmail(), member.getPassword());
