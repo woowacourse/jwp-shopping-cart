0436cfd3b3c8e92a240026fcdbcf846dba40d31e
songusika <sws981202@gmail.com>
2023-05-08 07:54:55 +0900

    feat: 장바구니 dao 구현

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -13,6 +13,17 @@ CREATE TABLE IF NOT EXISTS `member`
     `password` varchar(255)     NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS `cart_product`
+(
+    `id`         long PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `member_id`  long             NOT NULL,
+    `product_id` long             NOT NULL,
+    CONSTRAINT CART_UNIQUE UNIQUE (`member_id`, `product_id`),
+    FOREIGN KEY (`member_id`) REFERENCES `member` (`id`),
+    FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+);
+
+
 INSERT INTO member (email, password)
 VALUES ('herb@teco.com', 'herbPassword');
 INSERT INTO member (email, password)
