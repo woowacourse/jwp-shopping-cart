54d63713f9ee9f4fb98d670bd69f6832ad22b02a
SproutMj <mjk54565@gmail.com>
2023-05-08 01:01:21 +0900

    refactor: Basic 로그인 중 IllegalArgumentException이 발생하는 경우 테스트 추가

--- src/test/java/cart/util/BasicAuthExtractorTest.java
+++ src/test/java/cart/util/BasicAuthExtractorTest.java
@@ -55,4 +55,33 @@ class BasicAuthExtractorTest {
                 () -> assertNotEquals(password, loginRequest.getPassword())
         );
     }
+
+    @DisplayName("BASIC으로 올바르게 만들어지지 않은 헤더인 경우")
+    @Test
+    void basicLoginFailIfNotRightHeader() {
+        //given
+        String email = "abcd";
+        String password = "aaaa";
+        String fail = "fail";
+        String header = "Not Basic " + Base64.getEncoder().encodeToString((fail + ":" + "fail").getBytes());
+
+        //when
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.addHeader("Authorization", header);
+
+        BasicAuthExtractor extractor = new BasicAuthExtractor();
+
+        //then
+        assertThrows(IllegalArgumentException.class, ()->extractor.extract(request));
+    }
+
+    @DisplayName("헤더의 값이 null인 경우")
+    @Test
+    void basicLoginFailIfNullHeader() {
+        //given
+        MockHttpServletRequest request = new MockHttpServletRequest();
+
+        //when, then
+        assertThrows(IllegalArgumentException.class, ()->request.addHeader("Authorization", null));
+    }
 }
