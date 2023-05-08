c21ffe9279060bcd3f2570a2c3d340f8724aadea
Combi153 <jcm04@naver.com>
2023-05-08 14:45:25 +0900

    feat: AuthInfo 예외 처리 로직 추가

--- src/main/java/cart/controller/authentication/AuthInfo.java
+++ src/main/java/cart/controller/authentication/AuthInfo.java
@@ -8,11 +8,22 @@ public class AuthInfo {
     private final String password;
 
     public AuthInfo(final String email, final String password) {
-        if (email == null || password == null) {
-            throw new AuthorizationException("회원 정보가 입력되지 않았습니다.");
-        }
+        validateEmail(email);
+        validatePassword(password);
         this.email = email;
         this.password = password;
+    }
+
+    private void validateEmail(final String email) {
+        if (email == null || email.isBlank()) {
+            throw new AuthorizationException("회원 계정이 입력되지 않았습니다.");
+        }
+    }
+
+    private void validatePassword(final String password) {
+        if (password == null || password.isBlank()) {
+            throw new AuthorizationException("회원 비밀번호가 입력되지 않았습니다.");
+        }
     }
 
     public String getEmail() {
