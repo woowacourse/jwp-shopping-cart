8c4254373c6b6a9d404ad06b025497fb50d3f609
jeonghoon <shb03323@naver.com>
2023-05-08 14:29:17 +0900

    refactor: Interceptor 연결

--- src/main/java/cart/config/WebMvcConfiguration.java
+++ src/main/java/cart/config/WebMvcConfiguration.java
@@ -1,8 +1,10 @@
 package cart.config;
 
 import cart.auth.AuthArgumentResolver;
+import cart.auth.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -14,6 +16,12 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
 
     public WebMvcConfiguration(final AuthArgumentResolver authArgumentResolver) {
         this.authArgumentResolver = authArgumentResolver;
+    }
+
+    @Override
+    public void addInterceptors(final InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .addPathPatterns("/carts", "/cart");
     }
 
     @Override
