c61ccdcb2bae84e1c419206e9fd4825b3c8406c9
jeonghoon <shb03323@naver.com>
2023-05-08 14:00:08 +0900

    fix: AuthorizationException ControllerAdvice에 추가

--- src/main/java/cart/controller/GlobalControllerAdvice.java
+++ src/main/java/cart/controller/GlobalControllerAdvice.java
@@ -1,11 +1,14 @@
 package cart.controller;
 
+import cart.exception.AuthorizationException;
 import cart.exception.ProductNotFoundException;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.stream.Collectors;
 
@@ -28,5 +31,10 @@ public class GlobalControllerAdvice {
     @ExceptionHandler(ProductNotFoundException.class)
     public ResponseEntity<String> handle(ProductNotFoundException e) {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(AuthorizationException.class)
+    public ResponseEntity<String> handle(AuthorizationException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
     }
 }
