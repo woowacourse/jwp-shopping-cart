a20047869f18e6f95012ac463464b7d3ea75511f
odo27 <odo27@naver.com>
2023-05-08 18:56:12 +0900

    feat: CartSearchServiceTest 추가

---
+++ src/test/java/cart/service/CartSearchServiceTest.java
@@ -0,0 +1,28 @@
+package cart.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import cart.domain.cart.Cart;
+import cart.repository.StubCartRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CartSearchServiceTest {
+
+    private StubCartRepository stubCartRepository;
+    private CartSearchService cartSearchService;
+
+    @BeforeEach
+    void setUp() {
+        stubCartRepository = new StubCartRepository();
+        cartSearchService = new CartSearchService(stubCartRepository);
+    }
+
+    @Test
+    void 조회_테스트() {
+        final List<Cart> carts = cartSearchService.findByEmail("a@a.com");
+        assertThat(carts).isNull();
+    }
+}
