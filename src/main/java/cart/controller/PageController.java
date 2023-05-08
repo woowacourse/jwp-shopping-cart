89eb1483170216a7ce462c135e8147b23c8e96b1
This2sho <zx00018@naver.com>
2023-05-08 10:21:50 +0900

    feat: 추가 페이지 연동

--- src/main/java/cart/controller/PageController.java
+++ src/main/java/cart/controller/PageController.java
@@ -1,7 +1,9 @@
 package cart.controller;
 
-import cart.dao.ProductDao;
-import cart.dao.ProductEntity;
+import cart.controller.dto.MemberResponse;
+import cart.controller.dto.ProductResponse;
+import cart.service.MemberService;
+import cart.service.ProductService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,27 +12,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PageController {
 
-    private final ProductDao productDao;
+    private final ProductService productService;
+    private final MemberService memberService;
 
-    public PageController(ProductDao productDao) {
-        this.productDao = productDao;
+    public PageController(ProductService productService, MemberService memberService) {
+        this.productService = productService;
+        this.memberService = memberService;
     }
 
     @GetMapping
     public String loadHome(Model model) {
-        final List<ProductEntity> productEntities = productDao.findAll();
-        List<ProductResponse> products = ResponseMapper.from(productEntities);
-
+        final List<ProductResponse> products = productService.findAll();
         model.addAttribute("products", products);
         return "index";
     }
 
     @GetMapping("/admin")
-    public String loadAdmin(Model model){
-        final List<ProductEntity> productEntities = productDao.findAll();
-        List<ProductResponse> products = ResponseMapper.from(productEntities);
-
+    public String loadAdmin(Model model) {
+        final List<ProductResponse> products = productService.findAll();
         model.addAttribute("products", products);
         return "admin";
+    }
+
+    @GetMapping("/cart")
+    public String loadCart() {
+        return "cart";
+    }
+
+    @GetMapping("/settings")
+    public String loadMembers(Model model) {
+        final List<MemberResponse> memberResponses = memberService.findAll();
+        model.addAttribute("members", memberResponses);
+        return "settings";
     }
 }
