b4a8fe6679854cf183c40f3d4b32c6918d0d95ed
Combi153 <jcm04@naver.com>
2023-05-08 14:45:11 +0900

    feat: 예외 처리 로직 추가 및 Logger 적용

--- src/main/java/cart/controller/GlobalExceptionHandler.java
+++ src/main/java/cart/controller/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package cart.controller;
 
 import cart.exception.AuthorizationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,8 +21,11 @@ import java.util.Map;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<String> handleIllegalArgumentException(final IllegalArgumentException exception) {
+        logger.warn(exception.getMessage());
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
@@ -30,6 +38,7 @@ public class GlobalExceptionHandler {
             String fieldName = ((FieldError) error).getField();
             String message = error.getDefaultMessage();
             errorMessageByFields.put(fieldName, message);
+            logger.warn(message);
         });
 
         return ResponseEntity.badRequest().body(errorMessageByFields);
@@ -37,12 +46,31 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthorizationException.class)
     public ResponseEntity<String> handleAuthorizationException(final AuthorizationException exception) {
+        logger.warn(exception.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<String> handleHttpRequestMethodNotSupportedException(final HttpRequestMethodNotSupportedException exception) {
+        logger.warn(exception.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("요청 리소스에 맞는 메서드가 아닙니다.");
+    }
+
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleEmptyResultDataAccessException(final EmptyResultDataAccessException exception) {
+        logger.warn(exception.getMessage());
+        return ResponseEntity.badRequest().body("접근하려는 데이터가 존재하지 않습니다.");
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(final HttpMessageNotReadableException exception) {
+        logger.warn(exception.getMessage());
+        return ResponseEntity.badRequest().body("요청 body에서 문제가 발생했습니다. body를 확인해주세요.");
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(final Exception exception) {
-        exception.printStackTrace();
+        logger.error("예기치 못한 오류: " + exception);
         return ResponseEntity.internalServerError().body("예기치 못한 예외가 발생했습니다.");
     }
 }
