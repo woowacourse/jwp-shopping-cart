2c6399d172401bb140937e17ea2b55e78a8b0fa6
odo27 <odo27@naver.com>
2023-05-08 18:57:11 +0900

    feat: UserSearchServiceTest 추가

---
+++ src/test/java/cart/service/UserSearchServiceTest.java
@@ -0,0 +1,28 @@
+package cart.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import cart.domain.user.User;
+import cart.repository.StubUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class UserSearchServiceTest {
+
+    private StubUserRepository stubUserRepository;
+    private UserSearchService userSearchService;
+
+    @BeforeEach
+    void setUp() {
+        stubUserRepository = new StubUserRepository();
+        userSearchService = new UserSearchService(stubUserRepository);
+    }
+
+    @Test
+    void 전체_조회_테스트() {
+        final List<User> users = userSearchService.findAll();
+        assertThat(users).isNull();
+    }
+}
