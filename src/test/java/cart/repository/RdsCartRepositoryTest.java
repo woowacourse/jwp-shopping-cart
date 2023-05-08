8a563d0355f064214fa5154d3f4e13141059bccc
odo27 <odo27@naver.com>
2023-05-08 18:56:45 +0900

    feat: RdsCartRepositoryTest 추가

--- src/test/java/cart/repository/RdsCartRepositoryTest.java
+++ src/test/java/cart/repository/RdsCartRepositoryTest.java
@@ -1,14 +1,18 @@
 package cart.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
+import java.util.Optional;
 
+import cart.controller.dto.CartResponse;
 import cart.dao.CartDao;
 import cart.dao.ProductDao;
 import cart.dao.UserDao;
 import cart.domain.cart.Cart;
 import cart.domain.user.Email;
+import cart.entiy.CartEntity;
 import cart.entiy.ProductEntity;
 import cart.entiy.UserEntity;
 import org.junit.jupiter.api.BeforeEach;
@@ -52,19 +56,42 @@ class RdsCartRepositoryTest {
         @DisplayName("cart를 저장하면")
         class ContextSave {
 
+            private long cartId;
+
             @BeforeEach
             void setUp() {
-                rdsCartRepository.save(
+                final CartResponse cartResponse = rdsCartRepository.save(
                         "a@a.com",
                         productId
                 );
+                cartId = cartResponse.getCartId();
             }
 
             @Test
-            @DisplayName("조회할 수 있다")
-            void save() {
+            @DisplayName("이메일로 조회할 수 있다")
+            void findByEmail() {
                 final List<Cart> carts = rdsCartRepository.findByEmail(new Email("a@a.com"));
+                assertAll(
+                        () -> assertThat(carts).hasSize(1),
+                        () -> assertThat(carts.get(0).getCartId()).isEqualTo(cartId),
+                        () -> assertThat(carts.get(0).getProduct().getId()).isEqualTo(productId),
+                        () -> assertThat(carts.get(0).getProduct().getName().getValue()).isEqualTo("odo"),
+                        () -> assertThat(carts.get(0).getProduct().getImage().getValue()).isEqualTo("url"),
+                        () -> assertThat(carts.get(0).getProduct().getPrice().getValue()).isEqualTo(1)
+                );
                 assertThat(carts).hasSize(1);
+            }
+
+            @Test
+            @DisplayName("아이디로 조회할 수 있다")
+            void findById() {
+                final Optional<CartEntity> cartEntity = rdsCartRepository.findById(cartId);
+                assertAll(
+                        () -> assertThat(cartEntity).isPresent(),
+                        () -> assertThat(cartEntity.get().getCartId()).isPositive(),
+                        () -> assertThat(cartEntity.get().getEmail()).isEqualTo("a@a.com"),
+                        () -> assertThat(cartEntity.get().getProductId()).isEqualTo(productId)
+                );
             }
         }
     }
