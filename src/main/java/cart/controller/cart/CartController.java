563a9b056f2047708e08c7c7f8d0ddcc7be71255
JunDonghyuk <ehdgur4814@naver.com>
2023-05-08 14:39:04 +0900

    refactor: api 스펙 변경

--- src/main/java/cart/controller/cart/CartController.java
+++ src/main/java/cart/controller/cart/CartController.java
@@ -1,7 +1,7 @@
 package cart.controller.cart;
 
 import cart.config.auth.Auth;
-import cart.dto.ProductDto;
+import cart.dto.CartItemDto;
 import cart.service.CartService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,20 +20,20 @@ public class CartController {
     }
 
     @GetMapping("")
-    public ResponseEntity<List<ProductDto>> findProduct(@Auth final String email) {
-        List<ProductDto> products = cartService.findAllCartProductByEmail(email);
-        return ResponseEntity.ok().body(products);
+    public ResponseEntity<List<CartItemDto>> findProduct(@Auth final String email) {
+        List<CartItemDto> cartItems = cartService.findAllCartItemsByEmail(email);
+        return ResponseEntity.ok().body(cartItems);
     }
 
-    @PostMapping("/{product_id}")
-    public ResponseEntity<Void> addProduct(@PathVariable("product_id") final Long productId, @Auth final String email) {
-        final Long id = cartService.addProductInCart(productId, email);
+    @PostMapping("")
+    public ResponseEntity<Void> addProduct(@RequestBody CartItemDto cartItemDto, @Auth final String email) {
+        final Long id = cartService.addProductInCart(cartItemDto.getProductId(), email);
         return ResponseEntity.created(URI.create("/carts/" + id)).build();
     }
 
-    @DeleteMapping("/{product_id}")
-    public ResponseEntity<Void> deleteProduct(@PathVariable("product_id") final Long productId, @Auth final String email) {
-        cartService.deleteProductInCart(productId, email);
+    @DeleteMapping("/{cart_id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable("cart_id") final Long cartId) {
+        cartService.deleteProductByCartId(cartId);
         return ResponseEntity.noContent().build();
     }
 }
