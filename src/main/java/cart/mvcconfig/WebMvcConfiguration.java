84a6877dcceff350c65c90175ae05f8455ca1e0d
yoondgu <doyou2ing@naver.com>
2023-05-08 14:33:42 +0900

    fix: AuthorizationExtractor 의존성 주입 설정

--- src/main/java/cart/mvcconfig/WebMvcConfiguration.java
+++ src/main/java/cart/mvcconfig/WebMvcConfiguration.java
@@ -3,6 +3,7 @@ package cart.mvcconfig;
 import cart.domain.member.MemberService;
 import cart.infratstructure.AuthenticationInterceptor;
 import cart.infratstructure.AuthenticationPrincipalArgumentResolver;
+import cart.infratstructure.AuthorizationExtractor;
 import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -12,15 +13,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    private final AuthorizationExtractor authorizationExtractor;
     private final MemberService memberService;
 
-    public WebMvcConfiguration(final MemberService memberService) {
+    public WebMvcConfiguration(final AuthorizationExtractor authorizationExtractor, final MemberService memberService) {
+        this.authorizationExtractor = authorizationExtractor;
         this.memberService = memberService;
     }
 
     @Override
     public void addInterceptors(final InterceptorRegistry registry) {
-        registry.addInterceptor(new AuthenticationInterceptor(memberService))
+        registry.addInterceptor(new AuthenticationInterceptor(authorizationExtractor, memberService))
                 .addPathPatterns("/cart-items/**");
     }
 
