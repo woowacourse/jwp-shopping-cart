131bc8d3d514d4653a988cab0806c7ed35f0758f
Woojin Lee <dhkep03@gmail.com>
2023-05-08 15:26:09 +0900

    feat: 모든 요청 응답 Log 기록

--- src/main/java/cart/config/WebMvcConfiguration.java
+++ src/main/java/cart/config/WebMvcConfiguration.java
@@ -1,6 +1,7 @@
 package cart.config;
 
 import cart.config.auth.SignInInterceptor;
+import cart.config.log.LoggerInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,14 +9,19 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
+    private final LoggerInterceptor loggerInterceptor;
     private final SignInInterceptor signInInterceptor;
 
-    public WebMvcConfiguration(SignInInterceptor signInInterceptor) {
+    public WebMvcConfiguration(LoggerInterceptor loggerInterceptor, SignInInterceptor signInInterceptor) {
+        this.loggerInterceptor = loggerInterceptor;
         this.signInInterceptor = signInInterceptor;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggerInterceptor)
+                .addPathPatterns("/**");
+
         registry.addInterceptor(signInInterceptor)
                 .addPathPatterns("/products/**");
     }
