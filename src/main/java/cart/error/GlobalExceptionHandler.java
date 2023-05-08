e4f1e6493c790fdc62d1729bae69d50f2f069e0d
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-05-08 13:14:44 +0900

    refactor: 컨트롤러 계층 예외처리 클래스명 변경

--- src/main/java/cart/error/GlobalExceptionHandler.java
+++ src/main/java/cart/error/GlobalExceptionHandler.java
@@ -15,7 +15,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class ApiControllerHandler {
+public class GlobalExceptionHandler {
 
     private final Logger log = LoggerFactory.getLogger(getClass());
 
