aee5a6249d7b24f7faa9812541effba59e422f40
drunkenhw <drunkenhw@gmail.com>
2023-05-08 17:34:03 +0900

    refactor: authentication extractor 생성 방식 변경

--- src/main/java/cart/config/WebMvcConfig.java
+++ src/main/java/cart/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package cart.config;
 import cart.auth.AuthenticationArgumentResolver;
 import cart.auth.AuthenticationExtractor;
 import java.util.List;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,11 +12,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new AuthenticationArgumentResolver(authenticationExtractor()));
-    }
-
-    @Bean
-    public AuthenticationExtractor authenticationExtractor() {
-        return new AuthenticationExtractor();
+        resolvers.add(new AuthenticationArgumentResolver(new AuthenticationExtractor()));
     }
 }
