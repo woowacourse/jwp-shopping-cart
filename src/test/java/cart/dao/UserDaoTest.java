f122df4851e473efc523337ab8c9daa265b00c9d
odo27 <odo27@naver.com>
2023-05-08 18:54:56 +0900

    feat: UserDao 생성 테스트 추가

--- src/test/java/cart/dao/UserDaoTest.java
+++ src/test/java/cart/dao/UserDaoTest.java
@@ -1,6 +1,7 @@
 package cart.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,6 +20,19 @@ class UserDaoTest {
     @Autowired
     private void setUp(final JdbcTemplate jdbcTemplate) {
         userDao = new UserDao(jdbcTemplate);
+    }
+
+    @Test
+    void 생성_테스트() {
+        final UserEntity userEntity = userDao.insert(new UserEntity("a@a.com", "password1"));
+        final Optional<UserEntity> result = userDao.findByEmail("a@a.com");
+        assertAll(
+                () -> assertThat(userEntity.getEmail()).isEqualTo("a@a.com"),
+                () -> assertThat(userEntity.getPassword()).isEqualTo("password1"),
+                () -> assertThat(result).isPresent(),
+                () -> assertThat(result.get().getEmail()).isEqualTo("a@a.com"),
+                () -> assertThat(result.get().getPassword()).isEqualTo("password1")
+        );
     }
 
     @Test
