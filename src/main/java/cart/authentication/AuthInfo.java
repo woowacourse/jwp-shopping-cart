95710dc8d46fa82fab0a3dfef61c7c76ca685ccd
jaeyeon kim <jakind@naver.com>
2023-05-08 14:59:48 +0900

    refactor : AuthInfo 에서 password 를 삭제 (현재로서 사용하지 않는 값)

--- src/main/java/cart/authentication/AuthInfo.java
+++ src/main/java/cart/authentication/AuthInfo.java
@@ -3,19 +3,13 @@ package cart.authentication;
 public class AuthInfo {
 
     private final String email;
-    private final String password;
 
-    public AuthInfo(String email, String password) {
+    public AuthInfo(String email) {
         this.email = email;
-        this.password = password;
     }
 
     public String getEmail() {
         return email;
-    }
-
-    public String getPassword() {
-        return password;
     }
 
 }
