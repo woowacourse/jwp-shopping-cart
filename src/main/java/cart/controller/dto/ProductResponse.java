9ecf250effd161976d584c34fcfd17ca149fe3e3
This2sho <zx00018@naver.com>
2023-05-08 10:23:28 +0900

    refactor: Dto에서 Entity변환하도록 수정

---
+++ src/main/java/cart/controller/dto/ProductResponse.java
@@ -0,0 +1,43 @@
+package cart.controller.dto;
+
+import cart.dao.entity.ProductEntity;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductResponse {
+
+    private final Long id;
+    private final String name;
+    private final Integer price;
+    private final String imageUrl;
+
+    private ProductResponse(Long id, String name, Integer price, String imageUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.imageUrl = imageUrl;
+    }
+
+    public static List<ProductResponse> from(List<ProductEntity> productEntities) {
+        return productEntities.stream()
+            .map(entity -> new ProductResponse(entity.getId(), entity.getName(), entity.getPrice(),
+                entity.getImageUrl())).collect(
+                Collectors.toList());
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
