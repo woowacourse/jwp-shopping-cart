152f69e0b53c110a6dfa43b90aa093f2de6824e2
yoondgu <doyou2ing@naver.com>
2023-05-08 09:24:15 +0900

    refactor: ResController 확장성 고려하여 어노테이션 대신 ResponseEntity 사용

--- src/main/java/cart/controller/CartItemController.java
+++ src/main/java/cart/controller/CartItemController.java
@@ -6,12 +6,12 @@ import cart.dto.CartItemDto;
 import cart.infratstructure.AuthenticationPrincipal;
 import java.util.List;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,21 +24,21 @@ public class CartItemController {
         this.cartItemService = cartItemService;
     }
 
-    @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    public List<CartItemDto> get(@AuthenticationPrincipal Long loginMemberId) {
-        return cartItemService.findAllByMemberId(loginMemberId);
+    public ResponseEntity<List<CartItemDto>> get(@AuthenticationPrincipal Long loginMemberId) {
+        return ResponseEntity.ok(cartItemService.findAllByMemberId(loginMemberId));
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{productId}")
-    public void create(@PathVariable Long productId, @AuthenticationPrincipal Long loginMemberId) {
+    public ResponseEntity<Void> create(@PathVariable Long productId, @AuthenticationPrincipal Long loginMemberId) {
         cartItemService.add(new CartItem(loginMemberId, productId));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable(value = "id") Long cartItemId, @AuthenticationPrincipal Long loginMemberId) {
+    public ResponseEntity<Void> delete(@PathVariable(value = "id") Long cartItemId,
+                                       @AuthenticationPrincipal Long loginMemberId) {
         cartItemService.deleteById(loginMemberId, cartItemId);
+        return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/cart/controller/ProductController.java
+++ src/main/java/cart/controller/ProductController.java
@@ -7,6 +7,7 @@ import cart.dto.ProductRequest.UpdateDto;
 import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -29,30 +29,30 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public void create(@Valid @RequestBody AddDto productDto) {
+    public ResponseEntity<Void> create(@Valid @RequestBody AddDto productDto) {
         productService.add(new Product(
                 productDto.getName(),
                 productDto.getImageUrl(),
                 productDto.getPrice()
         ));
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping
-    public void update(@Valid @RequestBody UpdateDto productDto) {
+    public ResponseEntity<Void> update(@Valid @RequestBody UpdateDto productDto) {
         productService.update(new Product(
                 productDto.getId(),
                 productDto.getName(),
                 productDto.getImageUrl(),
                 productDto.getPrice()
         ));
+        return ResponseEntity.noContent().build();
     }
 
-    @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Long id) {
+    public ResponseEntity<Void> delete(@PathVariable("id") Long id) {
         productService.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 }
