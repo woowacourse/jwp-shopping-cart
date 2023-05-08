7ae8d9901ccedcbb84d1ec965a6c8b6bad878daa
yenawee <yaena0319@naver.com>
2023-05-08 09:56:14 +0900

    feat : 서버에서 에러 확인할 수 있게 하기

--- src/main/java/cart/exception/CartControllerAdvice.java
+++ src/main/java/cart/exception/CartControllerAdvice.java
@@ -1,5 +1,7 @@
 package cart.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -11,23 +13,30 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 public class CartControllerAdvice extends ResponseEntityExceptionHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(CartControllerAdvice.class);
+
     @ExceptionHandler({EmptyResultDataAccessException.class})
     public ResponseEntity<String> handleEmptyDataAccessException(final EmptyResultDataAccessException e) {
+        logger.error("An error occurred: {}", e.getMessage(), e);
         return ResponseEntity.badRequest().body("해당하는 상품이 없습니다.");
     }
 
     @ExceptionHandler({UserAuthenticationException.class})
     public ResponseEntity<String> handleAuthentificationException(final UserAuthenticationException e) {
+        logger.error("An error occurred: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
     }
 
     @ExceptionHandler({DataIntegrityViolationException.class})
     public ResponseEntity<String> handleDataIntegrityVioationException(final DataIntegrityViolationException e) {
+        logger.error("An error occurred: {}", e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("요청이 실패했습니다. 입력 값을 확인하고 다시 시도해주세요.");
     }
 
     @ExceptionHandler({Exception.class})
     public ResponseEntity<Object> handleOtherException(final Exception e, WebRequest request) {
+        logger.error("An error occurred: {}", e.getMessage(), e);
         try {
             return handleException(e, request);
         } catch (Exception exception) {
