6825216322e0eac30ffbeffc9a538eb3bb35c131
echo <echo.backend@gmail.com>
2023-05-09 09:16:53 +0900

    fix: admin view mapping 수정

--- src/main/java/cart/admin/view/AdminViewController.java
+++ src/main/java/cart/admin/view/AdminViewController.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminViewController {
+    
     private final CatalogService catalogService;
-
+    
     public AdminViewController(final CatalogService catalogService) {
         this.catalogService = catalogService;
     }
-
-    @GetMapping("")
+    
+    @GetMapping("/admin")
     public String renderAdminPage(final Model model) {
         model.addAttribute("products", this.catalogService.display());
         return "admin";
