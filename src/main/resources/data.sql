fe0c5f3e8bb9a06885195a9e158b812fbdd09ad9
echo <echo.backend@gmail.com>
2023-05-11 22:41:44 +0900

    chore: data.sql의 미리 생성된 cart들 제거

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -30,8 +30,3 @@ INSERT INTO users (email,password) VALUES('hello@test.com','#abcd1234');
 INSERT INTO products (name,image,price) VALUES('치킨','https://images.unsplash.com/photo-1626074353765-517a681e40be?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',15000);
 INSERT INTO products (name,image,price) VALUES('샐러드','https://images.unsplash.com/photo-1546793665-c74683f339c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80',9000);
 INSERT INTO products (name,image,price) VALUES('피자','https://images.unsplash.com/photo-1593560708920-61dd98c46a4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80',23000);
-
-INSERT INTO cart_items (product_id,user_id) VALUES(1,1);
-INSERT INTO cart_items (product_id,user_id) VALUES(2,1);
-INSERT INTO cart_items (product_id,user_id) VALUES(3,1);
-
