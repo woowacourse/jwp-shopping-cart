6055b0f6bd7905cd844edb203ac13cc6f8e055f5
echo <echo.backend@gmail.com>
2023-05-11 22:47:55 +0900

    refactor: 안쓰이는 코드 제거

--- src/main/java/cart/cart/controller/CartRestController.java
+++ src/main/java/cart/cart/controller/CartRestController.java
@@ -3,50 +3,45 @@ package cart.cart.controller;
 import cart.auth.dto.UserInfo;
 import cart.auth.dto.UserResponseDTO;
 import cart.auth.infrastructure.AuthenticationPrincipal;
-import cart.auth.infrastructure.BasicAuthorizationExtractor;
 import cart.auth.service.AuthService;
 import cart.cart.dto.CartRequestDTO;
 import cart.cart.service.CartService;
 import cart.catalog.dto.ProductResponseDTO;
-import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CartRestController {
-    
+
     private final CartService cartService;
     private final AuthService authService;
-    private final BasicAuthorizationExtractor basicAuthorizationExtractor = new BasicAuthorizationExtractor();
-    
+
     public CartRestController(final CartService cartService, final AuthService authService) {
         this.cartService = cartService;
         this.authService = authService;
     }
-    
+
     @GetMapping("/cart/items")
     public ResponseEntity<List<ProductResponseDTO>> getCart(@AuthenticationPrincipal final UserInfo userInfo) {
         final UserResponseDTO userResponseDTO = this.authService.findUser(userInfo);
         final List<ProductResponseDTO> userCart = this.cartService.findUserCart(userResponseDTO.getId());
         return ResponseEntity.ok().body(userCart);
     }
-    
+
     @PostMapping("/cart/items/{productId}")
     public ResponseEntity<Void> add(@AuthenticationPrincipal final UserInfo userInfo,
-            @PathVariable final Long productId) {
+                                    @PathVariable final Long productId) {
         final UserResponseDTO userResponseDTO = this.authService.findUser(userInfo);
         final CartRequestDTO cartRequestDTO = new CartRequestDTO(userResponseDTO.getId(), productId);
         this.cartService.createCart(cartRequestDTO);
         return ResponseEntity.ok().build();
     }
-    
+
     @DeleteMapping("/cart/items/{productId}")
     public ResponseEntity<Void> remove(@AuthenticationPrincipal final UserInfo userInfo,
-            @PathVariable final Long productId) {
+                                       @PathVariable final Long productId) {
         final UserResponseDTO userResponseDTO = this.authService.findUser(userInfo);
         final CartRequestDTO cartRequestDTO = new CartRequestDTO(userResponseDTO.getId(), productId);
         this.cartService.deleteCart(cartRequestDTO);
