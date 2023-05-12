6a70b291d0057a7aa42a90524c5c102223f93b71
Youngho <cyh6099@gmail.com>
2023-05-12 22:27:52 +0900

    refactor : 패키지 변경

--- src/main/java/cart/controller/product/ProductRestController.java
+++ src/main/java/cart/controller/product/ProductRestController.java
@@ -1,12 +1,19 @@
 package cart.controller.product;
 
 import cart.controller.product.dto.ProductRequest;
+import cart.controller.product.dto.ProductWebResponse;
 import cart.service.product.ProductService;
 import cart.service.product.dto.ProductResponse;
 import cart.service.product.dto.ProductServiceRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.net.URI;
