f49832cb89e492d451e89552963d08a71319201a
yeonkkk <tjddus0302@gmail.com>
2023-05-12 15:59:51 +0900

    refactor: 사용자 인증 정보에 대한 검증 추가

--- src/main/java/cart/auth/BasicAuthorizationExtractor.java
+++ src/main/java/cart/auth/BasicAuthorizationExtractor.java
@@ -1,6 +1,7 @@
 package cart.auth;
 
 import cart.dto.auth.AuthInfo;
+import cart.exception.AuthorizationException;
 import java.util.Base64;
 import javax.servlet.http.HttpServletRequest;
 
@@ -11,22 +12,29 @@ public class BasicAuthorizationExtractor implements AuthorizationExtractor<AuthI
     @Override
     public AuthInfo extract(final HttpServletRequest request) {
         String header = request.getHeader(AUTHORIZATION);
+        validateNull(header);
+        validateBasicType(header);
+
+        String value = header.substring(TYPE.length()).trim();
+        byte[] decodedBytes = Base64.getDecoder().decode(value);
+        String decodedString = new String(decodedBytes);
+
+        String[] credentials = decodedString.split(DELIMITER);
+        return AuthInfo.builder()
+                .email(credentials[0])
+                .password(credentials[1])
+                .build();
+    }
+
+    private void validateNull(final String header) {
         if (header == null) {
-            return null;
+            throw new AuthorizationException("사용자 인증 정보가 없습니다.");
         }
+    }
 
-        if (header.toLowerCase().startsWith(TYPE.toLowerCase())) {
-            String value = header.substring(TYPE.length()).trim();
-            byte[] decodedBytes = Base64.getDecoder().decode(value);
-            String decodedString = new String(decodedBytes);
-
-            String[] credentials = decodedString.split(DELIMITER);
-            return AuthInfo.builder()
-                    .email(credentials[0])
-                    .password(credentials[1])
-                    .build();
+    private void validateBasicType(final String header) {
+        if (!header.toLowerCase().startsWith(TYPE.toLowerCase())) {
+            throw new AuthorizationException(TYPE + " 타입의 인증 정보가 필요합니다.");
         }
-
-        return null;
     }
 }
