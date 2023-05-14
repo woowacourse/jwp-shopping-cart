f95b1f415f3fd2a4e7d8de6cd7481ef48b7f33ea
zillionme <beatus96@naver.com>
2023-05-14 17:45:15 +0900

    refactor: 요청 dto의 유효성 검사 주석 추가

--- src/main/java/cart/dto/CartItemRequest.java
+++ src/main/java/cart/dto/CartItemRequest.java
@@ -1,7 +1,10 @@
 package cart.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class CartItemRequest {
 
+    @NotBlank(message = "상품의 이름은 비어있을 수 없습니다.")
     private Long productId;
 
     public CartItemRequest(Long productId) {
--- src/main/java/cart/dto/MemberRequest.java
+++ src/main/java/cart/dto/MemberRequest.java
@@ -3,17 +3,20 @@ package cart.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 import org.springframework.lang.Nullable;
 
 public class MemberRequest {
 
     @Nullable
+    @Range(min = 1, max = 50, message = "회원 이름은 {min} ~ {max}자여야 합니다.")
     private final String name;
     @NotBlank (message = "회원의 이메일은 비어있을 수 없습니다.")
     @Email
     private final String email;
+
     @NotBlank (message = "회원의 비밀번호는 비어있을 수 없습니다.")
-    @Length(min = 4, max = 16)
+    @Length(min = 4, max = 16, message = "회원 비밀번호는 {min} ~ {max}자여야 합니다.")
     private final String password;
 
     public MemberRequest(String name, String email, String password) {
