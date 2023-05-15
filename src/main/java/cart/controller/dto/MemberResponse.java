eb81fe432ece784d242ac4a6df786ef1322937ed
kevstevie <jianna442@gmail.com>
2023-05-15 19:24:36 +0900

    refactor: polishing

--- src/main/java/cart/controller/dto/MemberResponse.java
+++ src/main/java/cart/controller/dto/MemberResponse.java
@@ -7,11 +7,6 @@ public class MemberResponse {
     private final String email;
     private final String password;
 
-    public MemberResponse(final String email, final String password) {
-        this.email = email;
-        this.password = password;
-    }
-
     public MemberResponse(final Member member) {
         this.email = member.getEmail();
         this.password = member.getPassword();
