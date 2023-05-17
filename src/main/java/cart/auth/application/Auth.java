8f4360d353e0a27e6c0e359e8c9c4c24a6464e93
kiarakim <kiarakim413@gmail.com>
2023-05-17 16:11:15 +0900

    style: 클래스 네이밍 변경

--- src/main/java/cart/auth/application/Auth.java
+++ src/main/java/cart/auth/application/Auth.java
@@ -1,4 +1,4 @@
-package cart.auth.infrastructure;
+package cart.auth.application;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface AuthenticationPrincipal {
+public @interface Auth {
 }
--- src/main/java/cart/auth/infrastructure/AuthenticationArgumentResolver.java
+++ src/main/java/cart/auth/infrastructure/AuthenticationArgumentResolver.java
@@ -8,21 +8,40 @@ import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import cart.auth.application.Auth;
+import cart.auth.application.AuthService;
+import cart.auth.application.AuthorizationException;
+import cart.auth.application.AuthorizationExtractor;
+import cart.auth.application.BasicAuthorizationExtractor;
 import cart.auth.dto.AuthInfo;
 
-public class AuthenticationPrincipalArgumentResolver implements HandlerMethodArgumentResolver {
+public class AuthenticationArgumentResolver implements HandlerMethodArgumentResolver {
 
 	private final AuthorizationExtractor<AuthInfo> extractor = new BasicAuthorizationExtractor();
+	private final AuthService authService;
+
+	public AuthenticationArgumentResolver(final AuthService authService) {
+		this.authService = authService;
+	}
 
 	@Override
 	public boolean supportsParameter(final MethodParameter parameter) {
-		return parameter.hasParameterAnnotation(AuthenticationPrincipal.class);
+		return parameter.hasParameterAnnotation(Auth.class);
 	}
 
 	@Override
 	public Object resolveArgument(final MethodParameter parameter, final ModelAndViewContainer mavContainer,
 		final NativeWebRequest webRequest, final WebDataBinderFactory binderFactory) throws Exception {
 		final HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
-		return extractor.extract(request);
+
+		final AuthInfo authInfo = extractor.extract(request);
+		final String email = authInfo.getEmail();
+		final String password = authInfo.getPassword();
+
+		if(authService.checkInvalidLogin(email, password)){
+			throw new AuthorizationException();
+		}
+
+		return authInfo;
 	}
 }
